0a2a5d0f36d53db3641341232e632f4ef3de675f
johannes hanika <hanatos@gmail.com>
2010-02-17 14:20:03 +0100

    manpage key list completed.

--- makeman.sh
+++ makeman.sh
@@ -3,7 +3,7 @@ output=darktable.1
 set -e; \
   d=`sed -n 's,/,-,g;s,.*\$$[D]ate: \(..........\).*,\1,p' $input`; \
   pod2man $input \
-  | sed 's/^\.TH .*/.TH DARKTABLE 1 "'"$$d"'" "darktable-0.4" "darktable"/' \
+  | sed 's/^\.TH .*/.TH DARKTABLE 1 "'"$$d"'" "darktable-0.5" "darktable"/' \
   | sed -e '/.*DREGGNAUTHORS.*/r AUTHORS' | sed -e '/.*DREGGNAUTHORS.*/d' \
   > tmp.$$$$ \
   && mv -f tmp.$$$$ $output
