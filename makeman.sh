bc916bf4a6a859c671d08583539d8670ebdf8656
johannes hanika <hanatos@gmail.com>
2010-02-17 14:00:54 +0100

    man page added.

---
+++ makeman.sh
@@ -0,0 +1,10 @@
+input=darktable.pod
+output=darktable.1
+set -e; \
+  d=`sed -n 's,/,-,g;s,.*\$$[D]ate: \(..........\).*,\1,p' $input`; \
+  pod2man $input \
+  | sed 's/^\.TH .*/.TH DARKTABLE 1 "'"$$d"'" "darktable-0.4" "darktable"/' \
+  | sed -e '/.*DREGGNAUTHORS.*/r AUTHORS' | sed -e '/.*DREGGNAUTHORS.*/d' \
+  > tmp.$$$$ \
+  && mv -f tmp.$$$$ $output
+
