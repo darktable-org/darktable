5830913c4ca39d35d47f7f249a2f4234cede9d70
johannes hanika <hanatos@gmail.com>
2010-04-02 21:42:28 +0200

    removed libraw denoise, along with some aliasing in greeneq

--- src/LibRaw/internal/dcraw_common.cpp
+++ src/LibRaw/internal/dcraw_common.cpp
@@ -3184,13 +3184,13 @@ void CLASS green_matching()
   const int margin = 3;
   int oj = 2, oi = 2;
   float f;
-  const float thr = 0.1f;
+  const float thr = 0.01f;
   if(FC(oj, oi) != 3) oj++;
   if(FC(oj, oi) != 3) oi++;
 
-  img = (ushort (*)[4]) calloc (iheight*iwidth, sizeof *image);
+  img = (ushort (*)[4]) calloc (height*width, sizeof *image);
   merror (img, "green_matching()");
-  memcpy(img,image,iheight*iwidth*sizeof *image);
+  memcpy(img,image,height*width*sizeof *image);
 
   for(j=oj;j<height-margin;j+=2)
     for(i=oi;i<width-margin;i+=2){
@@ -3200,22 +3200,18 @@ void CLASS green_matching()
       o1_4=img[(j+1)*width+i+1][1];
       o2_1=img[(j-2)*width+i][3];
       o2_2=img[(j+2)*width+i][3];
-      o2_3=img[j*width+i-2][3];
-      o2_4=img[j*width+i+2][3];
+      o2_3=img[j*width+i-2]  [3];
+      o2_4=img[j*width+i+2]  [3];
 
       m1=(o1_1+o1_2+o1_3+o1_4)/4.0;
       m2=(o2_1+o2_2+o2_3+o2_4)/4.0;
 
       c1=(abs(o1_1-o1_2)+abs(o1_1-o1_3)+abs(o1_1-o1_4)+abs(o1_2-o1_3)+abs(o1_3-o1_4)+abs(o1_2-o1_4))/6.0;
       c2=(abs(o2_1-o2_2)+abs(o2_1-o2_3)+abs(o2_1-o2_4)+abs(o2_2-o2_3)+abs(o2_3-o2_4)+abs(o2_2-o2_4))/6.0;
-      // ..??
-      // if((img[j*iwidth+i][3]<maximum*0.95)&&(m2>m1)&&(c1<maximum/threshold)&&(c2<maximum/threshold))
-        // image[j*iwidth+i][3]*=m1/m2;
-      // this looks better:
-      if((img[j*iwidth+i][3]<maximum*0.95)&&(c1<maximum*thr)&&(c2<maximum*thr))
+      if((img[j*width+i][3]<maximum*0.95)&&(c1<maximum*thr)&&(c2<maximum*thr))
       {
         f = image[j*width+i][3]*m1/m2;
-        image[j*iwidth+i][3]=f>0xffff?0xffff:f;
+        image[j*width+i][3]=f>0xffff?0xffff:f;
       }
     }
   free(img);
