75f7ae977b98d8fb6065520124b3d3787e6a7eb4
Henrik Andersson <dinamic@dinamic-desktop.(none)>
2010-04-07 17:40:47 +0200

    Updated priority of velvia and split toning plugin..

--- src/iop/splittoning.c
+++ src/iop/splittoning.c
@@ -428,7 +428,7 @@ void init(dt_iop_module_t *module)
   module->params = malloc(sizeof(dt_iop_splittoning_params_t));
   module->default_params = malloc(sizeof(dt_iop_splittoning_params_t));
   module->default_enabled = 0;
-  module->priority = 970;
+  module->priority = 968;
   module->params_size = sizeof(dt_iop_splittoning_params_t);
   module->gui_data = NULL;
   dt_iop_splittoning_params_t tmp = (dt_iop_splittoning_params_t){ 0,0.5,0.2,0.5, 0.5,33.0};
--- src/iop/velvia.c
+++ src/iop/velvia.c
@@ -184,7 +184,7 @@ void init(dt_iop_module_t *module)
   module->params = malloc(sizeof(dt_iop_velvia_params_t));
   module->default_params = malloc(sizeof(dt_iop_velvia_params_t));
   module->default_enabled = 0;
-  module->priority = 970;
+  module->priority = 967;
   module->params_size = sizeof(dt_iop_velvia_params_t);
   module->gui_data = NULL;
   dt_iop_velvia_params_t tmp = (dt_iop_velvia_params_t){50,50,.5};
