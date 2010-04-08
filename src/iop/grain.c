a27a2b7da66f171d2da953a3696ab52910020eee
Henrik Andersson <dinamic@dinamic-desktop.(none)>
2010-04-08 21:54:52 +0200

    Added simplex noise

--- src/iop/grain.c
+++ src/iop/grain.c
@@ -34,7 +34,8 @@
 #include <inttypes.h>
 
 #define GRAIN_LIGHTNESS_STRENGTH_SCALE 0.25
-#define GRAIN_HUE_STRENGTH_SCALE 0.25
+// (m_pi/2)/4 = half hue colorspan
+#define GRAIN_HUE_STRENGTH_SCALE 0.392699082
 #define GRAIN_SATURATION_STRENGTH_SCALE 0.25
 #define GRAIN_RGB_STRENGTH_SCALE 0.25
 
@@ -75,18 +76,134 @@ typedef struct dt_iop_grain_data_t
 }
 dt_iop_grain_data_t;
 
-double __perlin_noise(uint32_t x,uint32_t y) 
-{
-  uint32_t n = x + y * 57;
-  n = (n<<13) ^ n;
-  return ( 1.0 - (( (n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0));   
+
+static int grad3[12][3] = {{1,1,0},{-1,1,0},{1,-1,0},{-1,-1,0},
+                        {1,0,1},{-1,0,1},{1,0,-1},{-1,0,-1},
+                        {0,1,1},{0,-1,1},{0,1,-1},{0,-1,-1}};
+
+static int p[] = {151,160,137,91,90,15,
+131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
+190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
+77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
+135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
+129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
+251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
+49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180};
+
+static int perm[512];
+void _simplex_noise_init() { for(int i=0; i<512; i++) perm[i] = p[i & 255]; }
+double dot(int g[], double x, double y, double z) { return g[0]*x + g[1]*y + g[2]*z; }
+
+#define FASTFLOOR(x) ( x>0 ? (int)(x) : (int)(x)-1 )
+
+double _simplex_noise(double xin, double yin, double zin) {
+ double n0, n1, n2, n3; // Noise contributions from the four corners
+ // Skew the input space to determine which simplex cell we're in
+ double F3 = 1.0/3.0;
+ double s = (xin+yin+zin)*F3; // Very nice and simple skew factor for 3D
+ int i = FASTFLOOR(xin+s);
+ int j = FASTFLOOR(yin+s);
+ int k = FASTFLOOR(zin+s);
+ double G3 = 1.0/6.0; // Very nice and simple unskew factor, too
+ double t = (i+j+k)*G3;
+ double X0 = i-t; // Unskew the cell origin back to (x,y,z) space
+ double Y0 = j-t;
+ double Z0 = k-t;
+ double x0 = xin-X0; // The x,y,z distances from the cell origin
+ double y0 = yin-Y0;
+ double z0 = zin-Z0;
+ // For the 3D case, the simplex shape is a slightly irregular tetrahedron.
+ // Determine which simplex we are in.
+ int i1, j1, k1; // Offsets for second corner of simplex in (i,j,k) coords
+ int i2, j2, k2; // Offsets for third corner of simplex in (i,j,k) coords
+ if(x0>=y0) {
+   if(y0>=z0)
+     { i1=1; j1=0; k1=0; i2=1; j2=1; k2=0; } // X Y Z order
+     else if(x0>=z0) { i1=1; j1=0; k1=0; i2=1; j2=0; k2=1; } // X Z Y order
+     else { i1=0; j1=0; k1=1; i2=1; j2=0; k2=1; } // Z X Y order
+   }
+ else { // x0<y0
+   if(y0<z0) { i1=0; j1=0; k1=1; i2=0; j2=1; k2=1; } // Z Y X order
+   else if(x0<z0) { i1=0; j1=1; k1=0; i2=0; j2=1; k2=1; } // Y Z X order
+   else { i1=0; j1=1; k1=0; i2=1; j2=1; k2=0; } // Y X Z order
+ }
+ //  A step of (1,0,0) in (i,j,k) means a step of (1-c,-c,-c) in (x,y,z),
+ //  a step of (0,1,0) in (i,j,k) means a step of (-c,1-c,-c) in (x,y,z), and
+ //  a step of (0,0,1) in (i,j,k) means a step of (-c,-c,1-c) in (x,y,z), where
+ //  c = 1/6.
+  double   x1 = x0 - i1 + G3; // Offsets for second corner in (x,y,z) coords
+  double   y1 = y0 - j1 + G3;
+  double   z1 = z0 - k1 + G3;
+  double   x2 = x0 - i2 + 2.0*G3; // Offsets for third corner in (x,y,z) coords
+  double   y2 = y0 - j2 + 2.0*G3;
+  double   z2 = z0 - k2 + 2.0*G3;
+  double   x3 = x0 - 1.0 + 3.0*G3; // Offsets for last corner in (x,y,z) coords
+  double   y3 = y0 - 1.0 + 3.0*G3;
+  double   z3 = z0 - 1.0 + 3.0*G3;
+  // Work out the hashed gradient indices of the four simplex corners
+  int ii = i & 255;
+  int jj = j & 255;
+  int kk = k & 255;
+  int gi0 = perm[ii+perm[jj+perm[kk]]] % 12;
+  int gi1 = perm[ii+i1+perm[jj+j1+perm[kk+k1]]] % 12;
+  int gi2 = perm[ii+i2+perm[jj+j2+perm[kk+k2]]] % 12;
+  int gi3 = perm[ii+1+perm[jj+1+perm[kk+1]]] % 12;
+  // Calculate the contribution from the four corners
+  double t0 = 0.6 - x0*x0 - y0*y0 - z0*z0;
+  if(t0<0) n0 = 0.0;
+  else {
+    t0 *= t0;
+    n0 = t0 * t0 * dot(grad3[gi0], x0, y0, z0);
+  }
+  double t1 = 0.6 - x1*x1 - y1*y1 - z1*z1;
+  if(t1<0) n1 = 0.0;
+  else {
+    t1 *= t1;
+    n1 = t1 * t1 * dot(grad3[gi1], x1, y1, z1);
+  }
+  double t2 = 0.6 - x2*x2 - y2*y2 - z2*z2;
+  if(t2<0) n2 = 0.0;
+  else {
+    t2 *= t2;
+    n2 = t2 * t2 * dot(grad3[gi2], x2, y2, z2);
+  }
+  double t3 = 0.6 - x3*x3 - y3*y3 - z3*z3;
+  if(t3<0) n3 = 0.0;
+  else {
+    t3 *= t3;
+    n3 = t3 * t3 * dot(grad3[gi3], x3, y3, z3);
+  }
+  // Add contributions from each corner to get the final noise value.
+  // The result is scaled to stay just inside [-1,1]
+  return 32.0*(n0 + n1 + n2 + n3);
 }
 
-double __perline_smooth_noise(double x,double y) 
+
+
+
+
+#define PRIME_LEVELS 4
+uint64_t _low_primes[PRIME_LEVELS] ={ 12503,14029,15649, 11369 };
+//uint64_t _mid_primes[PRIME_LEVELS] ={ 784697,875783, 536461,639259};
+
+double __value_noise(uint32_t level,uint32_t x,uint32_t y) 
 {
-  double corners = ( __perlin_noise(x-1, y-1)+__perlin_noise(x+1, y-1)+__perlin_noise(x-1, y+1)+__perlin_noise(x+1, y+1) ) / 16;
-  double sides   = ( __perlin_noise(x-1, y)  +__perlin_noise(x+1, y)  +__perlin_noise(x, y-1)  +__perlin_noise(x, y+1) ) /  8;
-  double center  =  __perlin_noise(x, y) / 4;
+  //uint32_t lvl=level%PRIME_LEVELS;
+  uint32_t n = x + y * 57;
+  n = (n<<13) ^ n;
+  return ( 1.0 - (( (n * (n * n * 15731 + 789221) +1376312589) & 0x7fffffff) / 1073741824.0)); 
+}
+
+double __value_smooth_noise(uint32_t level,double x,double y) 
+{
+  double corners = ( __value_noise(level,x-1, y-1)+__value_noise(level,x+1, y-1)+__value_noise(level,x-1, y+1)+__value_noise(level,x+1, y+1) ) / 16;
+  double sides   = ( __value_noise(level,x-1, y)  +__value_noise(level,x+1, y)  +__value_noise(level,x, y-1)  +__value_noise(level,x, y+1) ) /  8;
+  double center  =  __value_noise(level,x, y) / 4;
   return corners + sides + center;
 }
 
@@ -97,34 +214,44 @@ double __preline_cosine_interpolate(double a,double b,double x)
 	return  a*(1-f) + b*f;
 }
 
-double __perlin_interpolate(double x,double y) 
+double __value_interpolate(uint32_t level,double x,double y) 
 {
   double fx = x - (uint32_t)x;
   double fy = y - (uint32_t)y;
 
-  double v1 = __perline_smooth_noise((uint32_t)x,     (uint32_t)y);
-  double v2 = __perline_smooth_noise((uint32_t)x + 1, (uint32_t)y);
-  double v3 = __perline_smooth_noise((uint32_t)x,     (uint32_t)y + 1);
-  double v4 = __perline_smooth_noise((uint32_t)x + 1, (uint32_t)y + 1);
+  double v1 = __value_smooth_noise(level,(uint32_t)x,     (uint32_t)y);
+  double v2 = __value_smooth_noise(level,(uint32_t)x + 1, (uint32_t)y);
+  double v3 = __value_smooth_noise(level,(uint32_t)x,     (uint32_t)y + 1);
+  double v4 = __value_smooth_noise(level,(uint32_t)x + 1, (uint32_t)y + 1);
 
   double i1 = __preline_cosine_interpolate(v1 , v2 , fx);
   double i2 = __preline_cosine_interpolate(v3 , v4 , fx);
 
   return __preline_cosine_interpolate(i1 , i2 , fy);
 }
-
-double _perlin_2d_noise(double x,double y,uint32_t octaves,double persistance) 
+double _perlin_2d_noise(double x,double y,uint32_t octaves,double persistance,double z) 
 {
-  double f,a,total=0;
+  double f=1,a=1,total=0;
   
-  for(int o=1;o<=octaves;o++) {
+  for(int o=0;o<octaves;o++) {
+    total+= (__value_interpolate(o,x*f/z,y*f/z)*a);
     f=2*o;
     a=persistance*o;
-    total+= (__perlin_interpolate(x*f,y*f)*a);
   }
   return total;
 }
 
+double _simplex_2d_noise(double x,double y,uint32_t octaves,double persistance,double z) 
+{
+  double f=1,a=1,total=0;
+  
+  for(int o=0;o<octaves;o++) {
+    total+= (_simplex_noise(x*f/z,y*f/z,o)*a);
+    f=2*o;
+    a=persistance*o;
+  }
+  return total;
+}
 
 void rgb2hsl(float r,float g,float b,float *h,float *s,float *l) 
 {
@@ -189,10 +316,18 @@ void process (struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, void 
   double strength=(data->strength/100.0);
   for(int j=0;j<roi_out->height;j++) for(int i=0;i<roi_out->width;i++)
   {
-    double x=(i/(double)roi_out->width) * (128+(256*(data->scale/100.0)));
-    double y=(j/(double)roi_out->height) * (128+(256*(data->scale/100.0)));
-    double octaves=4;
-    double noise=_perlin_2d_noise(x,y,octaves,0.25);
+    double xscale=piece->buf_in.width>piece->buf_in.height?(piece->buf_in.width/piece->buf_in.height):1.0;
+    double yscale=piece->buf_in.width<piece->buf_in.height?(piece->buf_in.height/piece->buf_in.width):1.0;
+	  
+    double x=((roi_out->x+i)/(double)(roi_out->x+roi_out->width)) *512;	// * (32+(256*(data->scale/100.0)));
+    double y=((roi_out->y+j)/(double)(roi_out->y+roi_out->height)) *512;	// * (32+(256*(data->scale/100.0)));
+    x*=xscale;
+    y*=yscale;
+	  
+    double octaves=2;
+    double zoom=8*(data->scale/100.0);
+  //  double noise=_perlin_2d_noise(x, y, octaves,0.25, zoom)*1.5;
+    double noise=_simplex_2d_noise(x, y, octaves,0.5, zoom);
     if(data->channel==DT_GRAIN_CHANNEL_LIGHTNESS || data->channel==DT_GRAIN_CHANNEL_SATURATION) 
     {
       rgb2hsl(in[0],in[1],in[2],&h,&s,&l);
@@ -303,6 +438,7 @@ void gui_update(struct dt_iop_module_t *self)
 
 void init(dt_iop_module_t *module)
 {
+  _simplex_noise_init();
   module->params = malloc(sizeof(dt_iop_grain_params_t));
   module->default_params = malloc(sizeof(dt_iop_grain_params_t));
   module->default_enabled = 0;
