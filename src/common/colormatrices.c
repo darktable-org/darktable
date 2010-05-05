9d19ce9d75b0e711e757097f23eaacb01462ce73
Pascal de Bruijn <pmjdebruijn@pcode.nl>
2010-05-05 15:23:50 +0200

    Let our american and asian friends benefit from the EOS enhanced matrices too

--- src/common/colormatrices.c
+++ src/common/colormatrices.c
@@ -18,7 +18,7 @@
 
 /**
  * this is a collection of custom measured color matrices, profiled
- * for darktable (darktable.sf.net), so far all contributed by Pascal de Bruijn.
+ * for darktable (darktable.sf.net), so far all calculated by Pascal de Bruijn.
  */
 typedef struct dt_profiled_colormatrix_t
 {
@@ -27,18 +27,46 @@ typedef struct dt_profiled_colormatrix_t
 }
 dt_profiled_colormatrix_t;
 
+  // image submitter, chart type, illuminant, comments
 static dt_profiled_colormatrix_t dt_profiled_colormatrices[] = {
- { "Canon EOS 30D",          { 840195, 148773, -67017}, { 112915, 1104553, -369720}, {240005,  -19562, 1468338}, {827255, 873337, 715317}},   // experimental
- { "Canon EOS 40D",          { 845901, 325760, -13077}, { 110809,  960724, -213577}, { 82230, -218063, 1110229}, {837906, 868393, 705704}},
- { "Canon EOS 50D",          {1035110, 365005,  -8057}, {-192184,  930511, -477417}, {189545, -233353, 1360870}, {863983, 888763, 730026}},
- { "Canon EOS 350D DIGITAL", { 784348, 329681, -18875}, { 227249, 1001602, -115692}, { 23834, -270844, 1011185}, {861252, 886368, 721420}},
- { "Canon EOS 400D DIGITAL", { 743546, 283783, -16647}, { 256531, 1035355, -117432}, { 36560, -256836, 1013535}, {855698, 880066, 726181}},
- { "Canon EOS 450D",         { 960098, 404968,  22842}, { -85114,  855072, -310928}, {159851, -194611, 1164276}, {851379, 871506, 711823}},
- { "Canon PowerShot S90",    { 866531, 231995,  55756}, {  76965, 1067474, -461502}, {106369, -243286, 1314529}, {807449, 855270, 690750}},
- { "NIKON D60",              { 756927, 309906, -13000}, { 323959, 1038269,  -66376}, { 51697, -178635, 1044418}, {609894, 636215, 537338}},   // experimental
- { "NIKON D200",             { 878922, 352966,   2914}, { 273575, 1048141, -116302}, { 61661, -171021, 1126297}, {691483, 727142, 615204}},
- { "SONY DSLR-A200",         { 846786, 366302, -22858}, { 311584, 1046249, -107056}, { 54596, -192993, 1191406}, {708405, 744507, 596771}}
- };
+
+  // Roy Niswanger, ColorChecker DC, direct sunlight, experimental
+  { "Canon EOS 30D",                { 840195, 148773, -67017}, { 112915, 1104553, -369720}, {240005,  -19562, 1468338}, {827255, 873337, 715317}},
+
+  // Pascal de Bruijn, CMP Digital Target 3, strobe, well lit
+  { "Canon EOS 40D",                { 845901, 325760, -13077}, { 110809,  960724, -213577}, { 82230, -218063, 1110229}, {837906, 868393, 705704}},
+
+  // Pascal de Bruijn, CMP Digital Target 3, strobe, well lit
+  { "Canon EOS 50D",                {1035110, 365005,  -8057}, {-192184,  930511, -477417}, {189545, -233353, 1360870}, {863983, 888763, 730026}},
+
+  // Pascal de Bruijn, CMP Digital Target 3, strobe, well lit
+  { "Canon EOS 350D DIGITAL",       { 784348, 329681, -18875}, { 227249, 1001602, -115692}, { 23834, -270844, 1011185}, {861252, 886368, 721420}},
+  { "Canon EOS DIGITAL REBEL XT",   { 784348, 329681, -18875}, { 227249, 1001602, -115692}, { 23834, -270844, 1011185}, {861252, 886368, 721420}},
+  { "Canon EOS Kiss Digital N",     { 784348, 329681, -18875}, { 227249, 1001602, -115692}, { 23834, -270844, 1011185}, {861252, 886368, 721420}},
+
+  // Pascal de Bruijn, CMP Digital Target 3, strobe, well lit
+  { "Canon EOS 400D DIGITAL",       { 743546, 283783, -16647}, { 256531, 1035355, -117432}, { 36560, -256836, 1013535}, {855698, 880066, 726181}},
+  { "Canon EOS DIGITAL REBEL XTi",  { 743546, 283783, -16647}, { 256531, 1035355, -117432}, { 36560, -256836, 1013535}, {855698, 880066, 726181}},
+  { "Canon EOS Kiss Digital X",     { 743546, 283783, -16647}, { 256531, 1035355, -117432}, { 36560, -256836, 1013535}, {855698, 880066, 726181}},
+
+  // Pascal de Bruijn, CMP Digital Target 3, strobe, well lit
+  { "Canon EOS 450D",               { 960098, 404968,  22842}, { -85114,  855072, -310928}, {159851, -194611, 1164276}, {851379, 871506, 711823}},
+  { "Canon EOS DIGITAL REBEL XSi",  { 960098, 404968,  22842}, { -85114,  855072, -310928}, {159851, -194611, 1164276}, {851379, 871506, 711823}},
+  { "Canon EOS Kiss Digital X2",    { 960098, 404968,  22842}, { -85114,  855072, -310928}, {159851, -194611, 1164276}, {851379, 871506, 711823}},
+
+  // Pascal de Bruijn, CMP Digital Target 3, strobe, well lit
+  { "Canon PowerShot S90",          { 866531, 231995,  55756}, {  76965, 1067474, -461502}, {106369, -243286, 1314529}, {807449, 855270, 690750}},
+
+  // Richard Hughes, Homebrew ColorChecker, strobe, experimental
+  { "NIKON D60",                    { 756927, 309906, -13000}, { 323959, 1038269,  -66376}, { 51697, -178635, 1044418}, {609894, 636215, 537338}},
+
+  // Rolf Steinort, Wolf Faust IT8, direct sunlight, well lit
+  { "NIKON D200",                   { 878922, 352966,   2914}, { 273575, 1048141, -116302}, { 61661, -171021, 1126297}, {691483, 727142, 615204}},
+
+  // Alexander Rabtchevich, Wolf Faust IT8, direct sunlight, well lit
+  { "SONY DSLR-A200",               { 846786, 366302, -22858}, { 311584, 1046249, -107056}, { 54596, -192993, 1191406}, {708405, 744507, 596771}}
+
+};
 
 static const int dt_profiled_colormatrix_cnt = sizeof(dt_profiled_colormatrices)/sizeof(dt_profiled_colormatrix_t);
 
