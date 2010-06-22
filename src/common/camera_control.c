d047a71f3de5572ff2a6bfbdea49fde91411abf5
Henrik Andersson <dinamic@dinamic-desktop.(none)>
2010-06-22 22:57:54 +0200

    more fixes out of the blue...

--- src/common/camera_control.c
+++ src/common/camera_control.c
@@ -620,12 +620,17 @@ void dt_camctl_tether_mode(const dt_camctl_t *c, const dt_camera_t *cam,gboolean
 const char *dt_camctl_camera_get_model(const dt_camctl_t *c,const dt_camera_t *cam)
 {
   dt_camctl_t *camctl=(dt_camctl_t *)c;
+  gdk_threads_enter();
   if( !cam && ( (cam = camctl->active_camera) == NULL  || (cam = camctl->wanted_camera) == NULL))
   {
     dt_print(DT_DEBUG_CAMCTL,"[camera_control] Failed to get model of camera, camera==NULL\n"); 
+    gdk_threads_leave();
     return NULL;
   }
-  return g_strdup(cam->model);	
+  gchar *crap=g_strdup(cam->model);	
+  gdk_threads_leave();
+    
+  return crap;
 }
 
 void dt_camctl_camera_set_property(const dt_camctl_t *c,const dt_camera_t *cam,const char *property_name, const char *value) {
