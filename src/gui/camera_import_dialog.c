f60138e217e67aa961d33286edf4b263d92d6af6
Henrik Andersson <dinamic@dinamic-desktop.(none)>
2010-06-23 12:43:49 +0200

    Fixed freeing of model data when dialog finishes..

--- src/gui/camera_import_dialog.c
+++ src/gui/camera_import_dialog.c
@@ -448,18 +448,15 @@ void _camera_storage_image_filename(const dt_camera_t *camera,const char *filena
   
   // filename\n 1/60 f/2.8 24mm iso 160
   sprintf(file_info,"%s%c%s",filename,strlen(exif_info)?'\n':'\0',strlen(exif_info)?exif_info:"");
-  gdk_threads_enter(); 
   gtk_list_store_append(data->store,&iter);
   gtk_list_store_set(data->store,&iter,0,thumb,1,file_info,-1);
-  gdk_flush();
-  gdk_threads_enter(); 
   if(pixbuf) g_object_unref(pixbuf);
   if(thumb) g_object_ref(thumb);
 }
 
 void _camera_import_dialog_free(_camera_import_dialog_t *data) {
   gtk_list_store_clear( data->store );
-  g_free( data->store );
+  g_object_unref( data->store );
   g_free( data->vp );
 }
 
