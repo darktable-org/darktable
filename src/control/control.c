5526767dc005add9eb8f83e99fe08f3f10e77162
Pascal de Bruijn <pmjdebruijn@pcode.nl>
2010-07-17 20:18:01 +0200

    Directories in XDG/Pictures should be capitalized

--- src/control/control.c
+++ src/control/control.c
@@ -80,7 +80,7 @@ void dt_ctl_settings_default(dt_control_t *c)
   dt_conf_set_int  ("ui_last/combo_filter",   DT_LIB_FILTER_STAR_1);
 
   // Import settings
-  dt_conf_set_string ("capture/camera/storage/basedirectory", "$(PICTURES_FOLDER)/darktable");
+  dt_conf_set_string ("capture/camera/storage/basedirectory", "$(PICTURES_FOLDER)/Darktable");
   dt_conf_set_string ("capture/camera/storage/subpath", "$(YEAR)$(MONTH)$(DAY)_$(JOBCODE)");
   dt_conf_set_string ("capture/camera/storage/namepattern", "$(YEAR)$(MONTH)$(DAY)_$(SEQUENCE).$(FILE_EXTENSION)");
   dt_conf_set_string ("capture/camera/import/jobcode", "noname");
