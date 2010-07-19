e51d367554f59b18e20939c164ffab9020c253ac
johannes hanika <hanatos@gmail.com>
2010-07-19 16:36:24 +0200

    histogram now always displayed correctly for cached buffers.

--- src/develop/pixelpipe_hb.c
+++ src/develop/pixelpipe_hb.c
@@ -222,7 +222,8 @@ int dt_dev_pixelpipe_process_rec(dt_dev_pixelpipe_t *pipe, dt_develop_t *dev, vo
   {
     // if(module) printf("found valid buf pos %d in cache for module %s %s %lu\n", pos, module->op, pipe == dev->preview_pipe ? "[preview]" : "", hash);
     (void) dt_dev_pixelpipe_cache_get(&(pipe->cache), hash, output);
-    return 0;
+    // go to post-collect directly:
+    goto post_process_collect_info;
   }
 
   // if history changed or exit event, abort processing?
@@ -404,6 +405,7 @@ int dt_dev_pixelpipe_process_rec(dt_dev_pixelpipe_t *pipe, dt_develop_t *dev, vo
     }
 #endif
 
+post_process_collect_info:
     // final histogram:
     if(dev->gui_attached && pipe == dev->preview_pipe && (strcmp(module->op, "gamma") == 0))
     {
