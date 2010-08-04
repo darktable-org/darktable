b421457a6b8148981667ed6263fae1a81481345d
johannes hanika <hanatos@gmail.com>
2010-08-04 09:42:53 +0200

    make manpage respect config.h version.

--- makeman.sh
+++ makeman.sh
@@ -1,9 +1,10 @@
 input=darktable.pod
 output=darktable.1
+version=$(cat config.h | grep PACKAGE_VERSION | cut -f 3 -d" " | tr -d "\"")
 set -e; \
   d=`sed -n 's,/,-,g;s,.*\$$[D]ate: \(..........\).*,\1,p' $input`; \
   pod2man $input \
-  | sed 's/^\.TH .*/.TH DARKTABLE 1 "'"$$d"'" "darktable-0.5" "darktable"/' \
+  | sed 's/^\.TH .*/.TH DARKTABLE 1 "'"$$d"'" "darktable-'$version'" "darktable"/' \
   | sed -e '/.*DREGGNAUTHORS.*/r AUTHORS' | sed -e '/.*DREGGNAUTHORS.*/d' \
   > tmp.$$$$ \
   && mv -f tmp.$$$$ $output
