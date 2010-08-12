33991f1031bae074de7935c99a1dc0fea48c869e
Henrik Andersson <henrik.4e@gmail.com>
2010-08-12 19:06:06 +0200

    Scaled down the filmstrip to a better size..

--- src/dtgtk/paint.c
+++ src/dtgtk/paint.c
@@ -223,8 +223,8 @@ void dtgtk_cairo_paint_filmstrip(cairo_t *cr,gint x,gint y,gint w,gint h,gint fl
   gint s=w<h?w:h;
   cairo_translate (cr, x+(w/2.0)-(s/2.0), y+(h/2.0)-(s/2.0));
   cairo_scale (cr,s,s);
-  cairo_scale (cr,0.8,0.8);
-  cairo_translate (cr,0.1,0.1);
+  cairo_scale (cr,0.7,0.7);
+  cairo_translate (cr,0.15,0.15);
   
   cairo_set_line_cap (cr,CAIRO_LINE_CAP_ROUND);
  
