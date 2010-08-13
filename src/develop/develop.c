2dd28b0b78d3c9eba6b7db788e81734e3c52b8be
johannes hanika <hanatos@gmail.com>
2010-08-13 17:03:23 +0200

    ops.

--- src/develop/develop.c
+++ src/develop/develop.c
@@ -654,7 +654,7 @@ void dt_dev_add_history_item(dt_develop_t *dev, dt_iop_module_t *module)
   if(dev->gui_attached)
   {
     // update history (on) (off) annotation
-    dt_control_clear_history_items(dev->history_end-1);
+    dt_control_clear_history_items(dev->history_end);
     dt_control_queue_draw_all();
   }
 }
