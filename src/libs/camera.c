64dea1acee222d8a91b20849be9e84075cd245e6
Henrik Andersson <henrik.4e@gmail.com>
2010-09-07 14:04:24 +0200

    Added build of property menu for user selection of tethering properties.

--- src/libs/camera.c
+++ src/libs/camera.c
@@ -54,7 +54,10 @@ typedef struct dt_lib_camera_t
     
       GtkBox *pvbox1,*pvbox2;                  // propertylabel,widget    
       GtkWidget *plabel,*pname;                  // propertylabel,widget    
-      GList *properties;    // a list of dt_lib_camera_property_t
+      GList *properties;                            // a list of dt_lib_camera_property_t
+    
+      GtkMenu *properties_menu;                     // available properties 
+    
   } gui;
   
   /** Data part of the module */
@@ -212,6 +215,19 @@ static void _osd_button_clicked(GtkWidget *widget, gpointer user_data) {
   dt_control_gui_queue_draw();
 }
 
+static void _property_choice_callback(GtkMenuItem *item, gpointer user_data) {
+  dt_lib_camera_t *lib=(dt_lib_camera_t *)user_data;
+  gtk_entry_set_text(GTK_ENTRY (lib->gui.pname), gtk_menu_item_get_label(item));
+}
+
+
+static void 
+_show_property_popupmenu_clicked(GtkWidget *widget, gpointer user_data) 
+{
+  dt_lib_camera_t *lib=(dt_lib_camera_t *)user_data;
+  gtk_menu_popup (lib->gui.properties_menu, NULL, NULL, NULL, NULL, 1,  gtk_get_current_event_time());
+}
+
 static void
 _add_property_button_clicked (GtkWidget *widget, gpointer user_data)
 {
@@ -347,6 +363,9 @@ gui_init (dt_lib_module_t *self)
   lib->data.listener->camera_property_accessibility_changed=_camera_property_accessibility_changed;
   lib->data.listener->request_image_path=_camera_tethered_request_image_path;
   lib->data.listener->request_image_filename=_camera_tethered_request_image_filename;
+  
+  /* build the propertymenu */
+  dt_camctl_camera_build_property_menu (darktable.camctl,NULL,&lib->gui.properties_menu,G_CALLBACK(_property_choice_callback),lib);
   
   // Setup gui
   self->widget = gtk_vbox_new(FALSE, 5);
@@ -538,17 +557,24 @@ gui_init (dt_lib_module_t *self)
   gtk_box_pack_start (GTK_BOX (self->widget), dtgtk_label_new (_("additional properties"),DARKTABLE_LABEL_TAB|DARKTABLE_LABEL_ALIGN_RIGHT), TRUE, TRUE, 5);
   vbox1 = GTK_BOX (gtk_vbox_new (TRUE, 0));
   vbox2 = GTK_BOX (gtk_vbox_new (TRUE, 0));
+  
   gtk_box_pack_start (vbox1, GTK_WIDGET ( gtk_label_new (_("label"))), TRUE, TRUE, 0);
   lib->gui.plabel = gtk_entry_new ();
   gtk_box_pack_start (vbox2, GTK_WIDGET ( lib->gui.plabel ), TRUE, TRUE, 0);
+  
+  hbox = GTK_BOX(gtk_hbox_new(FALSE, 0));
   gtk_box_pack_start (vbox1, GTK_WIDGET ( gtk_label_new (_("property"))), TRUE, TRUE, 0);
+  GtkWidget *widget = gtk_button_new_with_label("O");
+  g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (_show_property_popupmenu_clicked), lib);
   lib->gui.pname = gtk_entry_new ();
-  gtk_box_pack_start (vbox2, GTK_WIDGET ( lib->gui.pname ), TRUE, TRUE, 0);
+  gtk_box_pack_start (hbox, GTK_WIDGET ( lib->gui.pname ), TRUE, TRUE, 0);
+  gtk_box_pack_start (hbox, GTK_WIDGET ( widget ), TRUE, FALSE, 0);
+  gtk_box_pack_start (vbox2, GTK_WIDGET ( hbox ), TRUE, TRUE, 0);
   
   hbox = GTK_BOX (gtk_hbox_new(FALSE, 0));
   gtk_box_pack_start (GTK_BOX (hbox), GTK_WIDGET (vbox1), FALSE, FALSE, 5);
   gtk_box_pack_start (GTK_BOX (hbox), GTK_WIDGET (vbox2), TRUE, TRUE, 5);
-  GtkWidget *widget = gtk_button_new_with_label (_("add user property"));
+  widget = gtk_button_new_with_label (_("add user property"));
   g_signal_connect (G_OBJECT (widget), "clicked", G_CALLBACK (_add_property_button_clicked), lib);
   gtk_widget_show(widget);
   gtk_box_pack_start (GTK_BOX (self->widget), GTK_WIDGET(hbox), FALSE, FALSE, 5);
