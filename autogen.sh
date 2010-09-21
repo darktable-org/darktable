114c6c2ebd6583239f284de770b4fec46aea0780
johannes hanika <hanatos@gmail.com>
2010-09-21 18:01:50 +0200

    more pretty print (no origin/ etc)

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-dt_decoration=$(git log master^..master --decorate | head -1 | cut -d' ' -f4 | cut -f1 -d')')
+dt_decoration=$(git log master^..master --decorate | head -1 | cut -d' ' -f4 | cut -f1 -d')' | sed -e 's/,//' -e 's/origin\///' )
 dt_sha1sum=$(git log master^..master | head -1 | cut -f2 -d ' ' | cut -c -8)
 sed -e "s/REPLACE_WITH_DT_VERSION/$dt_sha1sum/" < configure.ac.in > configure.ac
 autoreconf --install --force
--- tools/create_version_h.sh
+++ tools/create_version_h.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-dt_decoration=$(git log master^..master --decorate | head -1 | cut -d' ' -f4 | cut -f1 -d')')
+dt_decoration=$(git log master^..master --decorate | head -1 | cut -d' ' -f4 | cut -f1 -d')' | sed -e 's/,//' -e 's/origin\///'
+ )
 dt_sha1sum=$(git log master^..master | head -1 | cut -f2 -d ' ' | cut -c -8)
 
 cat > common/version.h << EOF
