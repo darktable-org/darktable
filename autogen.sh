09230d86b97e4b385529715789315df0ac99f6f8
johannes hanika <hanatos@gmail.com>
2010-09-21 17:50:56 +0200

    make distcheck fixes.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dt_decoration=$(git log master^..master --decorate | head -1 | cut -d' ' -f4 | cut -f1 -d')')
 dt_sha1sum=$(git log master^..master | head -1 | cut -f2 -d ' ' | cut -c -8)
-sed -e "s/REPLACE_WITH_DT_VERSION/$dt_decoration-$dt_sha1sum/" < configure.ac.in > configure.ac
+sed -e "s/REPLACE_WITH_DT_VERSION/$dt_sha1sum/" < configure.ac.in > configure.ac
 autoreconf --install --force
 intltoolize --copy --force --automake
