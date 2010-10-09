6ccadf7cbdb1e41d5324e5406d210f3151114b51
David Bremner <bremner@unb.ca>
2010-10-09 20:13:25 -0300

    Update autogen.sh and tools/create_version_h.sh
    - use more idiomatic version extraction git commands.
    - Remove dependence on master branch.
    - remove duplication of code
    - Allow caching of version information without full autoreconf

--- tools/create_version_h.sh
+++ tools/create_version_h.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-dt_decoration=$(git log master^..master --decorate | head -1 | cut -d' ' -f4 | cut -f1 -d')' | sed -e 's/,//' -e 's/origin\///'
- )
-dt_sha1sum=$(git log master^..master | head -1 | cut -f2 -d ' ' | cut -c -8)
+if [ ! -f version.sh ]; then
+    ./tools/create_version_sh.sh
+fi
+. version.sh
 
 cat > common/version.h << EOF
 #ifndef DT_VERSION_H
