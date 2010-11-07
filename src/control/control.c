6ee0f267c0d6995b25c136f422381bafada887ea
Henrik Andersson <henrik.4e@gmail.com>
2010-11-07 23:32:00 +0100

    Fixes errors of opaque pthread_t in MacOSX build..

--- src/control/control.c
+++ src/control/control.c
@@ -412,11 +412,11 @@ void dt_control_init(dt_control_t *s)
   s->running = 1;
   pthread_mutex_unlock(&s->run_mutex);
   for(k=0;k<s->num_threads;k++)
-    pthread_create(s->thread + k, NULL, dt_control_work, s);
+    pthread_create(s->thread[k], NULL, dt_control_work, s);
   for(k=0;k<DT_CTL_WORKER_RESERVED;k++)
   {
     s->new_res[k] = 0;
-    pthread_create(s->thread_res + k, NULL, dt_control_work_res, s);
+    pthread_create(s->thread_res[k], NULL, dt_control_work_res, s);
   }
   s->button_down = 0;
   s->button_down_which = 0;
