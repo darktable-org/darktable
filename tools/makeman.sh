a30e88eb06fcd755e86e30a0bb243a0f63a899ff
johannes hanika <hanatos@gmail.com>
2010-11-07 17:08:43 +0100

    manpage creation now with authors list.

--- tools/makeman.sh
+++ tools/makeman.sh
@@ -1,11 +1,13 @@
-input=darktable.pod
-output=darktable.1
-version=$(cat config.h | grep PACKAGE_VERSION | cut -f 3 -d" " | tr -d "\"")
+input=$1
+config=$2
+authors=$3
+output=$4
+version=$(cat $config | grep PACKAGE_VERSION | head -1 | cut -f 3 -d" " | tr -d "\"")
 set -e; \
   d=`sed -n 's,/,-,g;s,.*\$$[D]ate: \(..........\).*,\1,p' $input`; \
   pod2man $input \
   | sed 's/^\.TH .*/.TH DARKTABLE 1 "'"$$d"'" "darktable-'$version'" "darktable"/' \
-  | sed -e '/.*DREGGNAUTHORS.*/r AUTHORS' | sed -e '/.*DREGGNAUTHORS.*/d' \
+  | sed -e '/.*DREGGNAUTHORS.*/r '"$authors" | sed -e '/.*DREGGNAUTHORS.*/d' \
   > tmp.$$$$ \
   && mv -f tmp.$$$$ $output
 
