955d2c39fa776dc1554b703efdb60bf2a080562c
johannes hanika <hanatos@gmail.com>
2010-12-14 21:21:03 +0100

    minor adjustments.

--- src/common/image.c
+++ src/common/image.c
@@ -95,7 +95,7 @@ const char *
 dt_image_film_roll_name(const char *path)
 {
   const char *folder = path + strlen(path);
-  int numparts = dt_conf_get_int("show_folder_levels");
+  int numparts = CLAMPS(dt_conf_get_int("show_folder_levels"), 1, 5);
   int count = 0;
   if (numparts < 1)
     numparts = 1;
