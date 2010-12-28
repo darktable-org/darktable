eb1170b44a6663f01f6e95487f188cbab79c8d3e
Jochen Schroeder <cycomanic@gmail.com>
2010-12-29 10:56:19 +1100

    initial version of local contrast based on nonlinear yeni filter

--- src/iop/localc.c
+++ src/iop/localc.c
@@ -47,31 +47,31 @@ DT_MODULE(1)
 
 // TODO: some build system to support dt-less compilation and translation!
 
-typedef struct dt_iop_anlfyeni_params_t
+typedef struct dt_iop_localc_params_t
 {
   float alpha, scale, strength;
 }
-dt_iop_anlfyeni_params_t;
+dt_iop_localc_params_t;
 
-typedef struct dt_iop_anlfyeni_gui_data_t
+typedef struct dt_iop_localc_gui_data_t
 {
   GtkDarktableSlider *scale1, *scale2, *scale3; // this is needed by gui_update
   GtkVBox   *vbox1, *vbox2;
 }
-dt_iop_anlfyeni_gui_data_t;
+dt_iop_localc_gui_data_t;
 
-typedef struct dt_iop_anlfyeni_data_t
+typedef struct dt_iop_localc_data_t
 {
   float alpha, scale, strength; // in our case, no precomputation or
                      // anything is possible, so this is just a copy.
 }
-dt_iop_anlfyeni_data_t;
+dt_iop_localc_data_t;
 
-typedef struct dt_iop_anlfyeni_global_data_t
+typedef struct dt_iop_localc_global_data_t
 {
   // we don't need it for this example (as for most dt plugins)
 }
-dt_iop_anlfyeni_global_data_t;
+dt_iop_localc_global_data_t;
 
 // this returns a translatable name
 const char *name()
@@ -88,8 +88,7 @@ groups ()
 
 static float lambda(float x, float mu, float alpha)
 {
-    //return pow((1-abs(mu-x)/100.),alpha);
-    return exp(-abs(mu-x)*alpha);
+    return pow((1-abs(mu-x)/100.),alpha);
 }
 
 static float yeni(float x, float mu, float alpha)
@@ -100,19 +99,18 @@ static float yeni(float x, float mu, float alpha)
 
 static float gain(float x, float a, float b, float c, float K)
 {
-    if(abs(x)<=a) return 0.;
-    if(abs(x)<=b) return K*(cosf(M_PI+(x-a)*(0.5*M_PI)/(b-a))+1);
-    if(abs(x)<=c) return K*cosf((x-b)*(0.5*M_PI/(c-b)));
+    if(x<=a) return 0.;
+    if(x<=b) return K*(cosf(M_PI+(x-a)*(0.5*M_PI)/(b-a))+1);
+    if(x<=c) return K*cosf((x-b)*(0.5*M_PI/(c-b)));
     return 0.;
 }
 
 /** process, all real work is done here. */
 void process (struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, void *ivoid, void *ovoid, const dt_iop_roi_t *roi_in, const dt_iop_roi_t *roi_out)
 {
-  dt_iop_anlfyeni_params_t *d = (dt_iop_anlfyeni_params_t *)piece->data;
+  dt_iop_localc_params_t *d = (dt_iop_localc_params_t *)piece->data;
   // how many colors in our buffer?
   const int ch = piece->colors;
-  const int ch_width_1 = ch * (roi_out->width-1);
   //int j,k,i;
   // TODO: check if I can find better default values for a, b, c
   const float a = d->scale*1.;
@@ -125,40 +123,39 @@ void process (struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, void 
    //optional: parallelize it!
    //TODO: IMPORTANT!! I am not 100% sure if this can actually be parallised. I
    //should check that omp really only parallises the outer loop.
-  #pragma omp parallel for default(none) schedule(static) shared(ivoid,ovoid,roi_in,roi_out,d, piece) private(mu_f, mu_b, highpass)
+  #pragma omp parallel for default(none) schedule(static) shared(ivoid,ovoid,roi_in,roi_out,d) private(mu_f, mu_b, highpass)
 #endif
-  for(int j=0;j<roi_out->height;j++)
-  {
-    int i;
-    const float *in  = ((float *)ivoid) + ch*roi_in->width *j;
-    float *out = ((float *)ovoid) + ch*roi_out->width*j;
-    const float *inp0 = in;
-    const float *inp1 = in + ch_width_1;
-    mu_f[0] = yeni(inp0[0], inp0[0], d->alpha/roi_in->scale*piece->iscale);
-    mu_b[0] = yeni(inp1[0], inp1[0], d->alpha/roi_in->scale*piece->iscale);
-    out[1] = in[1];
-    out[2] = in[2];
-    inp0 += ch;
-    inp1 -= ch;
-    float *outp = out + ch;
-    for(i=1;i<roi_out->width/2;i++,outp+=ch,inp0+=ch,inp1-=ch)
+    for(int j=0;j<roi_out->height;j++)
     {
-      mu_f[i] = yeni(inp0[0], mu_f[i-1], d->alpha/roi_in->scale*piece->iscale);
-      mu_b[i] = yeni(inp1[0], mu_b[i-1], d->alpha/roi_in->scale*piece->iscale);
-      outp[1] = inp0[1];
-      outp[2] = inp0[2];
+      int i,k;
+      float *in  = ((float *)ivoid) + ch*roi_in->width *j;
+      float *out = ((float *)ovoid) + ch*roi_out->width*j;
+      mu_f[0] = yeni(in[0], in[0], d->alpha);
+      mu_b[0] = yeni(in[ch*(roi_out->width-1)+0], 
+             in[ch*(roi_out->width -1) + 0], d->alpha);
+      out[1] = in[1];
+      out[2] = in[2];
+    for(i=1;i<roi_out->width/2;i++)
+    {
+      mu_f[i] = yeni(in[ch*i+0], mu_f[i-1], d->alpha);
+      mu_b[i] = yeni(in[ch*(roi_out->width-(1+i))+0], 
+              mu_b[i-1], d->alpha);
+      out[ch*i+1] = in[ch*i+1];
+      out[ch*i+2] = in[ch*i+2];
     }
-    float *outp1 = out + ch*(roi_out->width-(1+i));
-    for(;i<roi_out->width;i++,outp+=ch,outp1-=ch,inp0+=ch,inp1-=ch)
+    for(k=i;k<roi_out->width;k++)
     {
-      outp[1] = inp0[1];
-      outp[2] = inp0[2];
-      mu_f[i] = yeni(inp0[0], mu_f[i-1], d->alpha/roi_in->scale*piece->iscale);
-      mu_b[i] = yeni(inp1[0], mu_b[i-1], d->alpha/roi_in->scale*piece->iscale);
-      highpass = inp0[0] - (mu_f[i]+mu_b[roi_out->width-(1+i)])/2;
-      outp[0] = inp0[0]+gain(highpass, a, b, c, d->strength)*highpass;
-      highpass = inp1[0] - (mu_f[roi_out->width-(1+i)]+ mu_b[i])/2;
-      outp1[0] = inp1[0] + gain(highpass, a, b, c, d->strength)*highpass;
+      out[ch*k+1] = in[ch*k+1];
+      out[ch*k+2] = in[ch*k+2];
+      mu_f[k] = yeni(in[ch*k+0], mu_f[k-1], d->alpha);
+      mu_b[k] = yeni(in[ch*(roi_out->width-(1+k))+0], 
+              mu_b[k-1], d->alpha);
+      highpass = in[ch*k+0] - (mu_f[k]+mu_b[roi_out->width-(1+k)])/2;
+      out[ch*k+0] = in[ch*k+0]+gain(highpass, a, b, c, d->strength)*highpass;
+      highpass = in[ch*(roi_out->width-(1+k))+0] - 
+                 (mu_f[roi_out->width-(1+k)]+ mu_b[k])/2;
+      out[ch*(roi_out->width-(1+k))+0] = in[ch*(roi_out->width-(1+k))+0] +
+                gain(highpass, a, b, c, d->strength)*highpass;
     }
   }
 }
@@ -167,17 +164,17 @@ void process (struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, void 
 void init(dt_iop_module_t *module)
 {
   module->data = NULL; 
-  module->params = malloc(sizeof(dt_iop_anlfyeni_params_t));
-  module->default_params = malloc(sizeof(dt_iop_anlfyeni_params_t));
+  module->params = malloc(sizeof(dt_iop_localc_params_t));
+  module->default_params = malloc(sizeof(dt_iop_localc_params_t));
   module->default_enabled = 0;
   module->priority = 858; //TODO: need to check a good place in the pipe
-  module->params_size = sizeof(dt_iop_anlfyeni_params_t);
+  module->params_size = sizeof(dt_iop_localc_params_t);
   module->gui_data = NULL;
   // TODO: check the defaults if there's better ones
-  dt_iop_anlfyeni_params_t tmp = (dt_iop_anlfyeni_params_t){0.01f, 1.0f, 1.0f};
+  dt_iop_localc_params_t tmp = (dt_iop_localc_params_t){5.0f, 1.0f, 1.0f};
 
-  memcpy(module->params, &tmp, sizeof(dt_iop_anlfyeni_params_t));
-  memcpy(module->default_params, &tmp, sizeof(dt_iop_anlfyeni_params_t));
+  memcpy(module->params, &tmp, sizeof(dt_iop_localc_params_t));
+  memcpy(module->default_params, &tmp, sizeof(dt_iop_localc_params_t));
 }
 
 void cleanup(dt_iop_module_t *module)
@@ -192,8 +189,8 @@ void cleanup(dt_iop_module_t *module)
 
 void commit_params (struct dt_iop_module_t *self, dt_iop_params_t *params, dt_dev_pixelpipe_t *pipe, dt_dev_pixelpipe_iop_t *piece)
 {
-  dt_iop_anlfyeni_params_t *p = (dt_iop_anlfyeni_params_t *)params;
-  dt_iop_anlfyeni_data_t *d = (dt_iop_anlfyeni_data_t *)piece->data;
+  dt_iop_localc_params_t *p = (dt_iop_localc_params_t *)params;
+  dt_iop_localc_data_t *d = (dt_iop_localc_data_t *)piece->data;
   d->alpha = p->alpha;
   d->scale = p->scale;
   d->strength = p->strength;
@@ -201,7 +198,7 @@ void commit_params (struct dt_iop_module_t *self, dt_iop_params_t *params, dt_de
 
 void init_pipe     (struct dt_iop_module_t *self, dt_dev_pixelpipe_t *pipe, dt_dev_pixelpipe_iop_t *piece)
 {
-  piece->data = malloc(sizeof(dt_iop_anlfyeni_data_t));
+  piece->data = malloc(sizeof(dt_iop_localc_data_t));
   self->commit_params(self, self->default_params, pipe, piece);
 }
 
@@ -215,11 +212,11 @@ alpha_callback(GtkRange *range, dt_iop_module_t *self)
 {
   // this is important to avoid cycles!
   if(darktable.gui->reset) return;
-  dt_iop_anlfyeni_gui_data_t *g = (dt_iop_anlfyeni_gui_data_t *)self->gui_data;
-  dt_iop_anlfyeni_params_t *p = (dt_iop_anlfyeni_params_t *)self->params;
+  dt_iop_localc_gui_data_t *g = (dt_iop_localc_gui_data_t *)self->gui_data;
+  dt_iop_localc_params_t *p = (dt_iop_localc_params_t *)self->params;
   p->alpha = dtgtk_slider_get_value(g->scale1);
   // let core know of the changes
-  dt_dev_add_history_item(darktable.develop, self, TRUE);
+  dt_dev_add_history_item(darktable.develop, self);
 }
 
 static void
@@ -227,11 +224,11 @@ scale_callback(GtkRange *range, dt_iop_module_t *self)
 {
   // this is important to avoid cycles!
   if(darktable.gui->reset) return;
-  dt_iop_anlfyeni_gui_data_t *g = (dt_iop_anlfyeni_gui_data_t *)self->gui_data;
-  dt_iop_anlfyeni_params_t *p = (dt_iop_anlfyeni_params_t *)self->params;
-  p->scale = dtgtk_slider_get_value(g->scale2);
+  dt_iop_localc_gui_data_t *g = (dt_iop_localc_gui_data_t *)self->gui_data;
+  dt_iop_localc_params_t *p = (dt_iop_localc_params_t *)self->params;
+  p->strength = dtgtk_slider_get_value(g->scale2);
   // let core know of the changes
-  dt_dev_add_history_item(darktable.develop, self, TRUE);
+  dt_dev_add_history_item(darktable.develop, self);
 }
 
 static void
@@ -239,19 +236,19 @@ strength_callback(GtkRange *range, dt_iop_module_t *self)
 {
   // this is important to avoid cycles!
   if(darktable.gui->reset) return;
-  dt_iop_anlfyeni_gui_data_t *g = (dt_iop_anlfyeni_gui_data_t *)self->gui_data;
-  dt_iop_anlfyeni_params_t *p = (dt_iop_anlfyeni_params_t *)self->params;
+  dt_iop_localc_gui_data_t *g = (dt_iop_localc_gui_data_t *)self->gui_data;
+  dt_iop_localc_params_t *p = (dt_iop_localc_params_t *)self->params;
   p->strength = dtgtk_slider_get_value(g->scale3);
   // let core know of the changes
-  dt_dev_add_history_item(darktable.develop, self, TRUE);
+  dt_dev_add_history_item(darktable.develop, self);
 }
  
 /** gui callbacks, these are needed. */
 void gui_update    (dt_iop_module_t *self)
 {
   // let gui slider match current parameters:
-  dt_iop_anlfyeni_gui_data_t *g = (dt_iop_anlfyeni_gui_data_t *)self->gui_data;
-  dt_iop_anlfyeni_params_t *p = (dt_iop_anlfyeni_params_t *)self->params;
+  dt_iop_localc_gui_data_t *g = (dt_iop_localc_gui_data_t *)self->gui_data;
+  dt_iop_localc_params_t *p = (dt_iop_localc_params_t *)self->params;
   dtgtk_slider_set_value(g->scale1, p->alpha);
   dtgtk_slider_set_value(g->scale2, p->scale);
   dtgtk_slider_set_value(g->scale3, p->strength);
@@ -260,9 +257,9 @@ void gui_update    (dt_iop_module_t *self)
 void gui_init(struct dt_iop_module_t *self)
 {
   // init the slider (more sophisticated layouts are possible with gtk tables and boxes):
-  self->gui_data = malloc(sizeof(dt_iop_anlfyeni_gui_data_t));
-  dt_iop_anlfyeni_gui_data_t *g = (dt_iop_anlfyeni_gui_data_t *)self->gui_data;
-  dt_iop_anlfyeni_params_t *p = (dt_iop_anlfyeni_params_t *)self->params;
+  self->gui_data = malloc(sizeof(dt_iop_localc_gui_data_t));
+  dt_iop_localc_gui_data_t *g = (dt_iop_localc_gui_data_t *)self->gui_data;
+  dt_iop_localc_params_t *p = (dt_iop_localc_params_t *)self->params;
 
   self->widget = GTK_WIDGET(gtk_hbox_new(FALSE, 0));
   g->vbox1 = GTK_VBOX(gtk_vbox_new(FALSE, DT_GUI_IOP_MODULE_CONTROL_SPACING));
@@ -278,8 +275,8 @@ void gui_init(struct dt_iop_module_t *self)
   widget = dtgtk_reset_label_new(_("strength"), self, &p->strength, sizeof(float));
   gtk_box_pack_start(GTK_BOX(g->vbox1), widget, TRUE, TRUE, 0);
 
-  g->scale1 = DTGTK_SLIDER(dtgtk_slider_new_with_range(DARKTABLE_SLIDER_BAR,0.001, 0.07, 0.001, p->alpha, 3));
-  g->scale2 = DTGTK_SLIDER(dtgtk_slider_new_with_range(DARKTABLE_SLIDER_BAR,0.0, 6.0000, 0.010, p->scale, 3));
+  g->scale1 = DTGTK_SLIDER(dtgtk_slider_new_with_range(DARKTABLE_SLIDER_BAR,0.0, 30.0000, 0.100, p->alpha, 3));
+  g->scale2 = DTGTK_SLIDER(dtgtk_slider_new_with_range(DARKTABLE_SLIDER_BAR,0.0, 4.0000, 0.010, p->scale, 3));
   g->scale3 = DTGTK_SLIDER(dtgtk_slider_new_with_range(DARKTABLE_SLIDER_BAR,0.0, 10.0000, 0.001, p->strength, 3));
   gtk_box_pack_start(GTK_BOX(g->vbox2), GTK_WIDGET(g->scale1), TRUE, TRUE, 0);
   gtk_box_pack_start(GTK_BOX(g->vbox2), GTK_WIDGET(g->scale2), TRUE, TRUE, 0);
