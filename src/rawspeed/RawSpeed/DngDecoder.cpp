b7f324cbaf7482a29b8f5933d08820f9d61e42d6
johannes hanika <hanatos@gmail.com>
2011-01-18 12:06:42 +0100

    rawspeed: also load float hdr dng

--- src/rawspeed/RawSpeed/DngDecoder.cpp
+++ src/rawspeed/RawSpeed/DngDecoder.cpp
@@ -83,7 +83,7 @@ RawImage DngDecoder::decodeRaw() {
   try {
     mRaw->dim.x = raw->getEntry(IMAGEWIDTH)->getInt();
     mRaw->dim.y = raw->getEntry(IMAGELENGTH)->getInt();
-    mRaw->bpp = 2;
+    mRaw->bpp = raw->getEntry(BITSPERSAMPLE)->getShort()/8;
   } catch (TiffParserException) {
     ThrowRDE("DNG Decoder: Could not read basic image information.");
   }
