9099eb16bfdc8bd60c7084c093645b92b2c729bc
Henrik Andersson <hean01@users.sourceforge.net>
2011-02-18 00:29:55 +0100

    Added a size check of blend_params blob copy
    Added temporary debug messages...

--- src/develop/develop.c
+++ src/develop/develop.c
@@ -587,6 +587,9 @@ int dt_dev_write_history_item(dt_image_t *image, dt_dev_history_item_t *h, int32
   DT_DEBUG_SQLITE3_BIND_INT(stmt, 5, image->id);
   DT_DEBUG_SQLITE3_BIND_INT(stmt, 6, num);
   DT_DEBUG_SQLITE3_BIND_BLOB(stmt, 7, h->blend_params, sizeof(dt_develop_blend_params_t), SQLITE_TRANSIENT);
+  
+  fprintf(stderr,"Write: mode %d, opacity %f\n",h->blend_params->mode,h->blend_params->opacity);
+  
   sqlite3_step (stmt);
   sqlite3_finalize (stmt);
   return 0;
@@ -843,8 +846,10 @@ void dt_dev_read_history(dt_develop_t *dev)
       memcpy(hist->params, sqlite3_column_blob(stmt, 4), hist->module->params_size);
     }
         
-    memcpy(hist->blend_params, sqlite3_column_blob(stmt, 6), sizeof(dt_develop_blend_params_t));
-  
+    if(sqlite3_column_bytes(stmt, 6) == sizeof(dt_develop_blend_params_t))
+      memcpy(hist->blend_params, sqlite3_column_blob(stmt, 6), sizeof(dt_develop_blend_params_t));
+    
+    fprintf(stderr,"Read: mode %d, opacity %f\n",hist->blend_params->mode,hist->blend_params->opacity);
     // memcpy(hist->module->params, hist->params, hist->module->params_size);
     // hist->module->enabled = hist->enabled;
     // printf("[dev read history] img %d number %d for operation %d - %s params %f %f\n", sqlite3_column_int(stmt, 0), sqlite3_column_int(stmt, 1), instance, hist->module->op, *(float *)hist->params, *(((float*)hist->params)+1));
