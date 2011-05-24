6fdfd515daae932b37b01eeff28dc7730d64638b
johannes hanika <hanatos@gmail.com>
2011-05-24 11:22:35 +0200

    fixed a small bug in blendop hash calculation. this should re-enable caching in dr mode.

--- src/develop/pixelpipe_hb.c
+++ src/develop/pixelpipe_hb.c
@@ -288,7 +288,6 @@ dt_dev_pixelpipe_process_rec(dt_dev_pixelpipe_t *pipe, dt_develop_t *dev, void *
   }
   else dt_pthread_mutex_unlock(&pipe->busy_mutex);
 
-
   // 2) if history changed or exit event, abort processing?
   // preview pipe: abort on all but zoom events (same buffer anyways)
   if(dt_iop_breakpoint(dev, pipe)) return 1;
