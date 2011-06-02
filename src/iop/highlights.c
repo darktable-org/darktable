e8c4721fb9ac942d6eedd5a9115b5eaec6e6aebd
Henrik Andersson <hean01@users.sourceforge.net>
2011-06-02 12:29:51 +0200

    Moved highlight clip/reconstruct module just before tonemapper so tonemapper is working on fixed data preventing pink highlights..

--- src/iop/highlights.c
+++ src/iop/highlights.c
@@ -354,7 +354,7 @@ void init(dt_iop_module_t *module)
   // module->data = malloc(sizeof(dt_iop_highlights_data_t));
   module->params = malloc(sizeof(dt_iop_highlights_params_t));
   module->default_params = malloc(sizeof(dt_iop_highlights_params_t));
-  module->priority = 256;
+  module->priority = 249;
   module->default_enabled = 1;
   module->params_size = sizeof(dt_iop_highlights_params_t);
   module->gui_data = NULL;
