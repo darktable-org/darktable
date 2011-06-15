ef50b7ab1e272a7b1d8038912af09fe35e6a8b21
Henrik Andersson <hean01@users.sourceforge.net>
2011-06-15 21:41:27 +0200

    Initial stub for signalframework.

---
+++ src/control/signal.h
@@ -0,0 +1,48 @@
+/*
+    This file is part of darktable,
+    copyright (c) 2011 Henrik Andersson.
+
+    darktable is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    darktable is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with darktable.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef DT_CONTROL_SIGNAL
+#define DT_CONTROL_SIGNAL
+
+#include <glib-object.h>
+
+/** \brief enum of signals to listen for in darktable.
+    \note To add a new signal, first off add a enum and 
+    document what it's used for, then add a matching signal string
+    name to _strings in signal.c
+*/
+typedef enum dt_signal_t {
+
+  /** \brief This signal is raised when mouse hovers over image thumbs
+      both on lighttable and in the filmstrip.
+   */
+  DT_SIGNAL_MOUSE_OVER_IMAGE_CHANGE,
+
+
+  /* do not touch !*/
+  DT_SIGNAL_COUNT
+} dt_signal_t;
+
+/* intitialize the signal framework */
+struct dt_control_signal_t *dt_control_signal_init();
+/* raises a signal */
+void dt_control_signal_raise(const struct dt_control_signal_t *ctlsig, const dt_signal_t signal);
+/* connects a callback to a signal */
+void dt_control_signal_connect(const struct dt_control_signal_t *ctlsig,const dt_signal_t signal, GCallback *cb, gpointer user_data); 
+
+#endif
