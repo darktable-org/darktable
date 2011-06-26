d69b53f9887a86c58816149c97107803dc670ea1
johannes hanika <hanatos@gmail.com>
2011-06-26 14:54:02 +1200

    colorcorrection module is now tracked in the dependency system
    and auto-assigned a place in the pipe by tools/iop_dependencies.py,
    as the rest of the modules.

--- src/iop/clipping.c
+++ src/iop/clipping.c
@@ -660,7 +660,7 @@ void init(dt_iop_module_t *module)
   module->default_enabled = 0;
   module->params_size = sizeof(dt_iop_clipping_params_t);
   module->gui_data = NULL;
-  module->priority = 386;
+  module->priority = 377; // module order created by iop_dependencies.py, do not edit!
 }
 
 void cleanup(dt_iop_module_t *module)
--- src/iop/demosaic.c
+++ src/iop/demosaic.c
@@ -823,7 +823,7 @@ void init(dt_iop_module_t *module)
   module->params = malloc(sizeof(dt_iop_demosaic_params_t));
   module->default_params = malloc(sizeof(dt_iop_demosaic_params_t));
   module->default_enabled = 1;
-  module->priority = 136;
+  module->priority = 133; // module order created by iop_dependencies.py, do not edit!
   module->hide_enable_button = 1;
   module->params_size = sizeof(dt_iop_demosaic_params_t);
   module->gui_data = NULL;
