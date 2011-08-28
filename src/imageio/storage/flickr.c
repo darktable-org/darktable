ab38ac0615268794c323840cd2eceec6ad9f8001
Jose Carlos Garcia Sogo <jsogo@debian.org>
2011-08-28 11:21:49 +0200

    Flickr upload   + Update copyright info

--- src/imageio/storage/flickr.c
+++ src/imageio/storage/flickr.c
@@ -1,6 +1,6 @@
 /*
     This file is part of darktable,
-    copyright (c) 2010 Jose Carlos Garcia Sogo
+    copyright (c) 2010-2011 Jose Carlos Garcia Sogo
 
     darktable is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
