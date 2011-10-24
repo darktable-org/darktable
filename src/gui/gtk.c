13f49ded9432e081626807d1a4c37c7dda63f869
Henrik Andersson <hean01@users.sourceforge.net>
2011-10-24 22:44:48 +0200

    Revert "Revert the spacing added when adding modules into containers"
    This reverts commit 30591f57af6abf662ecd8c37401f04b53ef78974.

--- src/gui/gtk.c
+++ src/gui/gtk.c
@@ -1084,7 +1084,7 @@ void dt_ui_container_add_widget(dt_ui_t *ui, const dt_ui_container_t c, GtkWidge
     case DT_UI_CONTAINER_PANEL_TOP_RIGHT:
     case DT_UI_CONTAINER_PANEL_CENTER_TOP_RIGHT:
     case DT_UI_CONTAINER_PANEL_CENTER_BOTTOM_RIGHT:
-      gtk_box_pack_end(GTK_BOX(ui->containers[c]),w,FALSE,FALSE,0);
+      gtk_box_pack_end(GTK_BOX(ui->containers[c]),w,FALSE,FALSE,2);
       break;
 
     /* if box is center we want it to fill as much as it can */
@@ -1092,12 +1092,12 @@ void dt_ui_container_add_widget(dt_ui_t *ui, const dt_ui_container_t c, GtkWidge
     case DT_UI_CONTAINER_PANEL_CENTER_TOP_CENTER:
     case DT_UI_CONTAINER_PANEL_CENTER_BOTTOM_CENTER:
     case DT_UI_CONTAINER_PANEL_BOTTOM:
-      gtk_box_pack_start(GTK_BOX(ui->containers[c]),w,TRUE,TRUE,0);
+      gtk_box_pack_start(GTK_BOX(ui->containers[c]),w,TRUE,TRUE,2);
       break;
 
     default:
     {
-      gtk_box_pack_start(GTK_BOX(ui->containers[c]),w,FALSE,FALSE,0);
+      gtk_box_pack_start(GTK_BOX(ui->containers[c]),w,FALSE,FALSE,2);
     }  break;
   }
   gtk_widget_show_all(w);
