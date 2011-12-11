bcf49d2482e0320fab183d7f13e49791a731352e
Pascal de Bruijn <pmjdebruijn@pcode.nl>
2011-12-11 18:25:39 +0100

    Increase the unsharp mask threshold to 0.1

--- src/iop/sharpen.c
+++ src/iop/sharpen.c
@@ -504,7 +504,7 @@ void init(dt_iop_module_t *module)
   module->gui_data = NULL;
   dt_iop_sharpen_params_t tmp = (dt_iop_sharpen_params_t)
   {
-    2.0, 0.5, 0.004
+    2.0, 0.5, 0.1
   };
   memcpy(module->params, &tmp, sizeof(dt_iop_sharpen_params_t));
   memcpy(module->default_params, &tmp, sizeof(dt_iop_sharpen_params_t));
