2ef88d9131da76c6b0c928280a9e01e1621050eb
johannes hanika <hanatos@gmail.com>
2012-01-21 21:06:59 +1300

    ran iop_dependencies with the new plugin.

--- src/iop/bloom.c
+++ src/iop/bloom.c
@@ -304,7 +304,7 @@ void init(dt_iop_module_t *module)
   module->params = malloc(sizeof(dt_iop_bloom_params_t));
   module->default_params = malloc(sizeof(dt_iop_bloom_params_t));
   module->default_enabled = 0;
-  module->priority = 448; // module order created by iop_dependencies.py, do not edit!
+  module->priority = 460; // module order created by iop_dependencies.py, do not edit!
   module->params_size = sizeof(dt_iop_bloom_params_t);
   module->gui_data = NULL;
   dt_iop_bloom_params_t tmp = (dt_iop_bloom_params_t)
--- src/iop/monochrome.c
+++ src/iop/monochrome.c
@@ -155,7 +155,7 @@ void init(dt_iop_module_t *module)
   module->params = malloc(sizeof(dt_iop_monochrome_params_t));
   module->default_params = malloc(sizeof(dt_iop_monochrome_params_t));
   module->default_enabled = 0;
-  module->priority = 551; // module order created by iop_dependencies.py, do not edit!
+  module->priority = 560; // module order created by iop_dependencies.py, do not edit!
   module->params_size = sizeof(dt_iop_monochrome_params_t);
   module->gui_data = NULL;
   dt_iop_monochrome_params_t tmp = (dt_iop_monochrome_params_t)
