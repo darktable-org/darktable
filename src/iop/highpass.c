e4889115f2a7fda121f28eee797ee21301e45f11
Ulrich Pegelow <ulrich.pegelow@tongareva.de>
2012-03-13 21:40:02 +0100

    opencl: replace remnants of clReleaseMemObject by call to abstraction function dt_opencl_release_mem_object

--- src/iop/highpass.c
+++ src/iop/highpass.c
@@ -258,7 +258,7 @@ process_cl (struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, cl_mem 
   err = dt_opencl_enqueue_kernel_2d(devid, gd->kernel_highpass_mix, sizes);
   if(err != CL_SUCCESS) goto error;
 
-  clReleaseMemObject(dev_m);
+  if (dev_m != NULL) dt_opencl_release_mem_object(dev_m);
   return TRUE;
 
 error:
