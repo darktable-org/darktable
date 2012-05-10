198d2b9ce477397f1774180345617af5ab472c44
johannes hanika <hanatos@gmail.com>
2012-05-10 19:00:41 +1200

    curves: catmull rom now actually means it.

--- src/common/curve_tools.c
+++ src/common/curve_tools.c
@@ -500,12 +500,12 @@ float *catmull_rom_set ( int n, float x[], float y[])
   //nc_merror(m, "spline_cubic_set");
 
   //calculate the slopes
-  m[0] = (y[1]-y[0]);///(x[1]-x[0]);
+  m[0] = (y[1]-y[0])/(x[1]-x[0]);
   for (i=1; i<n-1; i++)
   {
-    m[i] = (y[i+1]-y[i-1])/2.0;//*(x[i+1]-x[i-1]));
+    m[i] = (y[i+1]-y[i-1])/(x[i+1]-x[i-1]);
   }
-  m[n-1] = (y[n-1]-y[n-2]);//(x[n-1]-y[n-2]);
+  m[n-1] = (y[n-1]-y[n-2])/(x[n-1]-y[n-2]);
 
   return m;
 }
