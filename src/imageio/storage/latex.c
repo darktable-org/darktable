68f2d2c6f24e08ccc62f753e1b45add4efc66498
johannes hanika <hanatos@gmail.com>
2012-05-14 21:07:13 +1200

    latex: book export now with correct trim box and nice centering + text layout possibilities.

--- src/imageio/storage/latex.c
+++ src/imageio/storage/latex.c
@@ -298,11 +298,14 @@ store (dt_imageio_module_data_t *sdata, const int imgid, dt_imageio_module_forma
     snprintf(relfilename, 256, "%s", c);
 
     snprintf(pair->line, 4096,
-        "\\vbox to \\imgheight {\n"
+        "\\begin{minipage}{\\imgwidth}%%\n"
+        "\\drawtrimcorners%%\n"
+        "\\vskip0pt plus 1filll\n"
+        "\\begin{minipage}{\\imgwidth}%%\n"
         " \\hfil\\includegraphics[width=\\imgwidth,height=\\imgheight,keepaspectratio]{%s}\\hfil\n"
-        "  \\vfil\n"
-        "}\n"
-        "\\drawtrimcorners\n"
+        "  %% put text under image here\n"
+        "\\end{minipage}\n"
+        "\\end{minipage}\n"
         "\\newpage\n\n", relfilename);
 
     pair->pos = num;
@@ -380,6 +383,8 @@ finalize_store(dt_imageio_module_storage_t *self, void *dd)
     "\\newcommand{\\dtsubject}{the matter}\n"
     "\\newcommand{\\dtkeywords}{this, that}\n"
     "\\documentclass{photobook} %% use [draftmode] for preview\n"
+    "\\color{white}\n"
+    "\\pagecolor{black}\n"
     "\\begin{document}\n"
     "\\maketitle\n"
     "\\pagestyle{empty}\n",
