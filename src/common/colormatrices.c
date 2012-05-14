c81562d93fa8e33f8c6da865cd38b28436366f66
Pascal de Bruijn <pmjdebruijn@pcode.nl>
2012-05-14 19:53:33 +0200

    Canon EOS 5D Mark III enhanced color matrix thanks for Russell Harrison

--- src/common/colormatrices.c
+++ src/common/colormatrices.c
@@ -49,6 +49,9 @@ static dt_profiled_colormatrix_t dt_profiled_colormatrices[] =
   // Xavier Besse, CMP Digital Target 3, direct sunlight, well lit
   { "Canon EOS 5D Mark II",         { 967590, 399139,  36026}, { -52094,  819046, -232071}, {144455, -143158, 1069305}, {864227, 899139, 741547}},
 
+  // Russell Harrison, Wolf Faust IT8, direct sunlight, well lit
+  { "Canon EOS 5D Mark III",        { 947891, 312958,  -7126}, { 163071, 1301834, -276596}, { 75928, -363388, 1272232}, {741272, 757050, 662430}},
+
   // Deacon MacMillan, Kodak Q60 (IT8), strobe, well lit
   { "Canon EOS 5D",                 { 971420, 386429,   5753}, { 176849, 1141586, -137955}, { 81909, -284790, 1198090}, {753662, 783997, 645142}},
 
