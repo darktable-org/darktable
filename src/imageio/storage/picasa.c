2529247bc3232f150dc016a75b8ec2c07a8f571c
Simon Spannagel <simonspa@kth.se>
2012-05-27 23:28:00 +0200

    Revert "clean up picasa code a bit."
    This reverts commit 03b35023fd96843e2b716fb5eec2b8f7d31370eb.

--- src/imageio/storage/picasa.c
+++ src/imageio/storage/picasa.c
@@ -898,6 +898,7 @@ store (dt_imageio_module_data_t *sdata, const int imgid, dt_imageio_module_forma
   int result=1;
   dt_storage_picasa_params_t *p=(dt_storage_picasa_params_t *)sdata;
 
+  int fail = 0;
 #ifdef _OPENMP // synch parallel store
   #pragma omp critical
 #endif
@@ -905,8 +906,10 @@ store (dt_imageio_module_data_t *sdata, const int imgid, dt_imageio_module_forma
     if( _picasa_api_create_album( p->picasa_api ) != 201 )
     {
       dt_control_log("failed to create picasa album");
-      return 1;
+      fail = 1;
     }
+
+  if(fail) return 1;
 
   const char *ext = format->extension(fdata);
 
