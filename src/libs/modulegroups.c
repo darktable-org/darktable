3e2abf8d80562d658a2e1c293b6d4e75baf46caa
Pascal de Bruijn <pmjdebruijn@darkstar.(none)>
2012-07-11 21:02:08 +0200

    Revert "Make module group buttons flat"
    This reverts commit fd7a7fa4192474a9566dbb33bccb5b8120e0670a.
    
    I think I initially asked for this to be changed, but it looks oddish.
    
    I guess bauhaus is best confined to to IOPs for the time being.

--- src/libs/modulegroups.c
+++ src/libs/modulegroups.c
@@ -100,7 +100,7 @@ void gui_init(dt_lib_module_t *self)
 
   self->widget = gtk_hbox_new(TRUE,2);
 
-  dtgtk_cairo_paint_flags_t pf = CPF_STYLE_FLAT;
+  dtgtk_cairo_paint_flags_t pf = CPF_NONE;
 
   /* favorites */
   d->buttons[DT_MODULEGROUP_FAVORITES] = dtgtk_togglebutton_new(dtgtk_cairo_paint_modulegroup_favorites, pf);
