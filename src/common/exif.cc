53f6487159145ac9c3ccb567a5d5d17475dbce59
johannes hanika <hanatos@gmail.com>
2012-09-06 21:08:13 +1200

    autoapply: flag handling (auto-apply presets already applied?)
    this should make its way through xmp/db and be inited to 0
    for all newly imported images as well as the already imported
    current collection.

--- src/common/exif.cc
+++ src/common/exif.cc
@@ -1193,6 +1193,26 @@ int dt_exif_xmp_read (dt_image_t *img, const char* filename, const int history_o
       img->legacy_flip.legacy = 0;
     }
 
+    // GPS data
+    if ((pos=xmpData.findKey(Exiv2::XmpKey("Xmp.exif.GPSLatitude"))) != xmpData.end() )
+    {
+      img->latitude = _gps_string_to_number(pos->toString().c_str());
+    }
+
+    if ((pos=xmpData.findKey(Exiv2::XmpKey("Xmp.exif.GPSLongitude"))) != xmpData.end() )
+    {
+      img->longitude = _gps_string_to_number(pos->toString().c_str());
+    }
+
+    if ((pos=xmpData.findKey(Exiv2::XmpKey("Xmp.darktable.auto_presets_applied"))) != xmpData.end() )
+    {
+      int32_t i = pos->toLong();
+      // set or clear bit in image struct
+      if(i == 1) img->flags |= DT_IMAGE_AUTO_PRESETS_APPLIED;
+      if(i == 0) img->flags &= ~DT_IMAGE_AUTO_PRESETS_APPLIED;
+    } // not found means 0 (old xmp)
+    else img->flags &= ~DT_IMAGE_AUTO_PRESETS_APPLIED;
+
     // history
     Exiv2::XmpData::iterator ver;
     Exiv2::XmpData::iterator en;
@@ -1381,6 +1401,11 @@ dt_exif_xmp_read_data(Exiv2::XmpData &xmpData, const int imgid)
 
   xmpData["Xmp.darktable.xmp_version"] = xmp_version;
   xmpData["Xmp.darktable.raw_params"] = raw_params;
+
+  if(stars & DT_IMAGE_AUTO_PRESETS_APPLIED)
+    xmpData["Xmp.darktable.auto_presets_applied"] = 1;
+  else
+    xmpData["Xmp.darktable.auto_presets_applied"] = 0;
 
   // get tags from db, store in dublin core
   Exiv2::Value::AutoPtr v1 = Exiv2::Value::create(Exiv2::xmpSeq); // or xmpBag or xmpAlt.
