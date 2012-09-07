0a0bb9af19ff86e7581042b543a549aa6f237142
Frédéric Grollier <fred.grollier@gmail.com>
2012-09-07 10:28:16 +0200

    Update the 'active' group of modules automatically when history stack changes.

--- src/common/history.c
+++ src/common/history.c
@@ -138,7 +138,10 @@ dt_history_copy_and_paste_on_image (int32_t imgid, int32_t dest_imgid, gboolean 
 
   /* if current image in develop reload history */
   if (dt_dev_is_current_image(darktable.develop, dest_imgid))
+  {
     dt_dev_reload_history_items (darktable.develop);
+    dt_dev_modulegroups_set(darktable.develop, dt_dev_modulegroups_get(darktable.develop));
+  }
 
   /* update xmp file */
   dt_image_synch_xmp(dest_imgid);
