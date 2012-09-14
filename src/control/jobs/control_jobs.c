02b9e3ea41899d794c369f17389a7fa227a331f2
Frédéric Grollier <fred.grollier@gmail.com>
2012-09-14 16:10:01 +0200

    Add safety check on 'newimgid'

--- src/control/jobs/control_jobs.c
+++ src/control/jobs/control_jobs.c
@@ -523,7 +523,7 @@ int32_t dt_control_duplicate_images_job_run(dt_job_t *job)
   {
     imgid = (long int)t->data;
     newimgid = dt_image_duplicate(imgid);
-    dt_history_copy_and_paste_on_image(imgid, newimgid, FALSE);
+    if(newimgid != -1) dt_history_copy_and_paste_on_image(imgid, newimgid, FALSE);
     t = g_list_delete_link(t, t);
     fraction=1.0/total;
     dt_control_backgroundjobs_progress(darktable.control, jid, fraction);
