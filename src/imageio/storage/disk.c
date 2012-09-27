9f2e1e460c0dc32e6e7b15a42d4c65205598174f
Tobias Ellinghaus <me@houz.org>
2012-09-27 12:08:52 +0200

    Cosmetics

--- src/imageio/storage/disk.c
+++ src/imageio/storage/disk.c
@@ -331,7 +331,7 @@ gui_reset (dt_imageio_module_storage_t *self)
 {
   disk_t *d = (disk_t *)self->gui_data;
   // global default can be annoying:
-  // gtk_entry_set_text(GTK_ENTRY(d->entry), "$(FILE_FOLDER)/darktable_exported/$(FILE_NAME)");
+  // gtk_entry_set_text(GTK_ENTRY(d->entry), "$(FILE_FOLDER)/darktable_exported/img_$(SEQUENCE)");
   dt_conf_set_string("plugins/imageio/storage/disk/file_directory", gtk_entry_get_text(d->entry));
 }
 
