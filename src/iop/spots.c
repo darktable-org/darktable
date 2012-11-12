darktable-org/darktable

commit 8625aa76b6793c61152d9dbb351c2da6a4b94dd6

fix for #9044: avoid division by zero in spot removal plugin
this happened for heavily scaled down images/thumbnails

src/iop/spots.c
@@ -92,17 +92,28 @@ void process (struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, void 
   for(int i=0; i<d->num_spots; i++)
   {
     // convert from world space:
-    const int x  = (d->spot[i].x *piece->buf_in.width)/scale - roi_in->x, y  = (d->spot[i].y *piece->buf_in.height)/scale - roi_in->y;
-    const int xc = (d->spot[i].xc*piece->buf_in.width)/scale - roi_in->x, yc = (d->spot[i].yc*piece->buf_in.height)/scale - roi_in->y;
+    const int x  = (d->spot[i].x *piece->buf_in.width)/scale - roi_in->x;
+    const int y  = (d->spot[i].y *piece->buf_in.height)/scale - roi_in->y;
+    const int xc = (d->spot[i].xc*piece->buf_in.width)/scale - roi_in->x;
+    const int yc = (d->spot[i].yc*piece->buf_in.height)/scale - roi_in->y;
     const int rad = d->spot[i].radius * MIN(piece->buf_in.width, piece->buf_in.height)/scale;
-    const int um = MIN(rad, MIN(x, xc)), uM = MIN(rad, MIN(roi_in->width -1-xc, roi_in->width -1-x));
-    const int vm = MIN(rad, MIN(y, yc)), vM = MIN(rad, MIN(roi_in->height-1-yc, roi_in->height-1-y));
+    const int um = CLAMP(MIN(x, xc), 0, rad);
+    const int uM = CLAMP(MIN(roi_in->width-1-xc, roi_in->width-1-x), 0, rad);
+    const int vm = CLAMP(MIN(y, yc), 0, rad);
+    const int vM = CLAMP(MIN(roi_in->height-1-yc, roi_in->height-1-y), 0, rad);
     float filter[2*rad + 1];
     // for(int k=-rad; k<=rad; k++) filter[rad + k] = expf(-k*k*2.f/(rad*rad));
-    for(int k=-rad; k<=rad; k++)
+    if(rad > 0)
     {
-      const float kk = 1.0f - fabsf(k/(float)rad);
-      filter[rad + k] = kk*kk*(3.0f - 2.0f*kk);
+      for(int k=-rad; k<=rad; k++)
+      {
+        const float kk = 1.0f - fabsf(k/(float)rad);
+        filter[rad + k] = kk*kk*(3.0f - 2.0f*kk);
+      }
+    }
+    else
+    {
+      filter[0] = 1.0f;
     }
     for(int u=-um; u<=uM; u++) for(int v=-vm; v<=vM; v++)
       {

commit 06042e422f181205bca8c69c794db368f75d797f

fix bug introduced with commit 8625aa76b6793c61152d9dbb351c2da6a4b94dd6

src/iop/spots.c
@@ -97,10 +97,10 @@ void process (struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, void 
     const int xc = (d->spot[i].xc*piece->buf_in.width)/scale - roi_in->x;
     const int yc = (d->spot[i].yc*piece->buf_in.height)/scale - roi_in->y;
     const int rad = d->spot[i].radius * MIN(piece->buf_in.width, piece->buf_in.height)/scale;
-    const int um = CLAMP(MIN(x, xc), 0, rad);
-    const int uM = CLAMP(MIN(roi_in->width-1-xc, roi_in->width-1-x), 0, rad);
-    const int vm = CLAMP(MIN(y, yc), 0, rad);
-    const int vM = CLAMP(MIN(roi_in->height-1-yc, roi_in->height-1-y), 0, rad);
+    const int um = MIN(rad, MIN(x, xc));
+    const int uM = MIN(rad, MIN(roi_in->width-1-xc, roi_in->width-1-x));
+    const int vm = MIN(rad, MIN(y, yc));
+    const int vM = MIN(rad, MIN(roi_in->height-1-yc, roi_in->height-1-y));
     float filter[2*rad + 1];
     // for(int k=-rad; k<=rad; k++) filter[rad + k] = expf(-k*k*2.f/(rad*rad));
     if(rad > 0)