a3ef59fe703a59cfbe7bf16f7065f57c72aab50c
Ulrich Pegelow <ulrich.pegelow@tongareva.de>
2012-12-06 21:03:27 +0100

    opencl: implement config parameter to omit opencl for module whitebalance
    this is supposed to be a more elegant way to deal with #9086

--- src/iop/temperature.c
+++ src/iop/temperature.c
@@ -366,6 +366,7 @@ void commit_params (struct dt_iop_module_t *self, dt_iop_params_t *p1, dt_dev_pi
   dt_iop_temperature_params_t *p = (dt_iop_temperature_params_t *)p1;
   dt_iop_temperature_data_t *d = (dt_iop_temperature_data_t *)piece->data;
   for(int k=0; k<3; k++) d->coeffs[k]  = p->coeffs[k];
+  piece->process_cl_ready = !(darktable.opencl->omit_whitebalance);
 }
 
 void init_pipe (struct dt_iop_module_t *self, dt_dev_pixelpipe_t *pipe, dt_dev_pixelpipe_iop_t *piece)
