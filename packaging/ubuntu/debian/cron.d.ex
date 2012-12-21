d2f57e8ce58b53b81cc8655f2d96c776a966315d
Pascal de Bruijn <pmjdebruijn@pcode.nl>
2012-12-21 14:31:21 +0100

    Remove confusing and outdated Debian/Ubuntu packaging

--- packaging/ubuntu/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the darktable package
-#
-0 4	* * *	root	[ -x /usr/bin/darktable_maintenance ] && /usr/bin/darktable_maintenance
--- packaging/ubuntu/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(darktable):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="darktable" command="/usr/bin/darktable"
