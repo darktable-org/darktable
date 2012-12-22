f1c6906f3682b48e29d13924f9b5a8a4c9c801ce
Ulrich Pegelow <ulrich.pegelow@tongareva.de>
2012-12-22 17:53:01 +0100

    opencl: remove configuration parameter opencl_omit_whitebalance

--- src/common/opencl.c
+++ src/common/opencl.c
@@ -46,7 +46,6 @@ void dt_opencl_init(dt_opencl_t *cl, const int argc, char *argv[])
   cl->enabled = 0;
   cl->use_events = dt_conf_get_bool("opencl_use_events");
   cl->avoid_atomics = dt_conf_get_bool("opencl_avoid_atomics");
-  cl->omit_whitebalance = dt_conf_get_bool("opencl_omit_whitebalance");
   cl->dlocl = NULL;
   int exclude_opencl = 0;
 
