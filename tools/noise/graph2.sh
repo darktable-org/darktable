0dabcd2c50cea8f0ae016c4a6e98734833e421cf
johannes hanika <hanatos@gmail.com>
2013-01-03 20:50:29 +1300

    noise profile: cross-iso plots for all cameras.

---
+++ tools/noise/graph2.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# remove whitespace and braces:
+# cat src/iop/denoiseprofile.c | grep '{"'  | sed 's/\s*,\s*/,/g' | tr " " "_" | tr -d "{}()\"" | tr "/" "_" > trim.txt
+
+# filter out panasonic and powershot
+cat src/iop/denoiseprofile.c | grep '{"'  | sed 's/\s*,\s*/,/g' | tr " " "_" | tr -d "{}()\"" | tr "/" "_" | grep -v "PowerShot" | grep -v "Panasonic" | grep -v "DYNAX" | grep -v "NEX-C3" | grep -v "pentax_k-x" > trim.txt
+
+# get all:
+# filter="cat"
+
+# only canon:
+filter="grep Canon"
+
+# get list of cameras (use exif model only):
+# cams=$(cat trim.txt | awk -F, "{ print \$3; }" | sort | uniq | tr "\n" " ") # could print \$2,\$3 to get maker and model
+# cams=$(cat trim.txt | awk -F, "{ print \$1; }" | sed 's/_iso_.*$//' | sort | uniq | tr "/" "_" | tr "\n" " ") # use first field to get commented double measurements as separate data points
+# filtered:
+cams=$(cat trim.txt | $filter | awk -F, "{ print \$1; }" | sed 's/_iso_.*$//' | sort | uniq | tr "/" "_" | tr "\n" " ")
+
+# is actually num cams + 1, because the first column is iso
+num_cams=$[ $(echo $cams | wc -w) + 1]
+
+# get sorted list of iso values:
+isos=$(cat trim.txt | $filter | awk -F, "{ print \$4; }" | sort -g | uniq)
+# isos="200 400 800 1600"
+
+
+# we want output files to look like:
+# iso  cam1 cam2 cam3 cam4
+# iso1 ..
+# iso2   ...
+#
+# so we first output the header:
+echo "iso ${cams}" > data.txt
+
+for iso in $isos
+do
+  echo -n "$iso " >> data.txt
+  echo "collecting iso $iso .."
+  for cam in $cams
+  do
+    # collect green poissonian value for this camera and iso ($8)
+    a=$(cat trim.txt | awk -F, "{if (\$1 ~ /$cam/ && \$4 == $iso) { print \$8; } }" | tr -d "\n")
+    if [ "$a" == "" ]
+    then
+      # echo "no value found for $cam iso $iso"
+      a="?"
+    fi
+    echo -n "$a " >> data.txt
+  done
+  echo "" >> data.txt
+done
+
+# now for some plotting pleasure:
+gnuplot << EOF
+set term pdf fontscale 0.5 size 10, 10
+set output 'poissonian.pdf'
+set mxtics 10
+set grid mxtics xtics ytics
+set logscale xy
+# set yrange [0:1e-5]
+set datafile missing "?"
+set key autotitle columnhead
+plot for [i=2:${num_cams}] './data.txt' u 1:(column(i)) w lp title column(i)
+EOF
+
