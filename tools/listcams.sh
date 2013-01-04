1e11a1ee78e5b20ead039ac4f6c5c5a451fe9669
johannes hanika <hanatos@gmail.com>
2013-01-05 07:43:08 +1300

    noise profile: fix listcams.sh

--- tools/listcams.sh
+++ tools/listcams.sh
@@ -2,4 +2,4 @@
 
 # TODO: extract basecurves, color matrices etc and create a complete html table for the web:
 echo "cameras with profiled presets for denoising:"
-cat src/iop/denoiseprofile.c | grep -E '^{"' | grep "iso 400\"" | tr -d "{}()\"" | cut -d '_' -f2 | cut -d ',' -f1 | tr " " "_" | sed -e "s/_iso_400//" | tr [:upper:] [:lower:] | tr "_" " " | sort | sed 's/$/<\/li>/' | sed 's/^/<li>/'
+cat src/common/noiseprofiles.h | grep -E '^{"' | grep "iso 400\"" | tr -d "{}()\"" | cut -d '_' -f2 | cut -d ',' -f1 | tr " " "_" | sed -e "s/_iso_400//" | tr [:upper:] [:lower:] | tr "_" " " | sort | sed 's/$/<\/li>/' | sed 's/^/<li>/'
