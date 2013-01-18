36575404f5baa29e0d81b227a0d1294f5da17307
Dennis Gnad <dennis@xorn.org>
2013-01-19 00:32:46 +0100

    put dt_control_queue_redraw() at a more reasonable position

--- src/iop/tonecurve.c
+++ src/iop/tonecurve.c
@@ -430,6 +430,8 @@ pick_toggled(GtkToggleButton *togglebutton, dt_iop_module_t *self)
   /* set the area sample size*/
   if (self->request_color_pick)
     dt_lib_colorpicker_set_point(darktable.lib, 0.5, 0.5);
+  else
+    dt_control_queue_redraw();
 
   if(self->off) gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(self->off), 1);
   dt_iop_request_focus(self);
@@ -819,7 +821,6 @@ finally:
   cairo_paint(cr_pixmap);
   cairo_destroy(cr_pixmap);
   cairo_surface_destroy(cst);
-  dt_control_queue_redraw();
   return TRUE;
 }
 
