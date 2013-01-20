d442acea34a8f9f8d54dfeff5f6d8ed719ea64fc
johannes hanika <hanatos@gmail.com>
2013-01-20 19:58:06 +1300

    opencl: disable sm_20 for nvidia gts 240

--- src/common/nvidia_gpus.h
+++ src/common/nvidia_gpus.h
@@ -134,6 +134,7 @@ static const char *nvidia_gpus[] = {
 "GeForce GT 430*","2.1",
 "GeForce GT 420*","1.0",
 "GeForce GT 240","1.2",
+"GeForce GTS 240","1.2",
 "GeForce GT 220*","1.2",
 "GeForce GT 220","1.2",
 "GeForce 210","1.2",
