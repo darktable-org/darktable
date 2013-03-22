7a1a24de3aece26f6eaefe66bf8a0abaef2efc28
johannes hanika <hanatos@gmail.com>
2013-03-23 07:26:47 +1300

    bauhaus: less tolerance when mouse leaves the popup.

--- src/bauhaus/bauhaus.c
+++ src/bauhaus/bauhaus.c
@@ -269,7 +269,7 @@ dt_bauhaus_root_motion_notify(GtkWidget *widget, GdkEventMotion *event, gpointer
 {
   if(darktable.bauhaus->current)
   {
-    const float tol = 100;
+    const float tol = 5;
     gint wx, wy;
     GtkWidget *widget = darktable.bauhaus->popup_window;
     gdk_window_get_origin (gtk_widget_get_window (widget), &wx, &wy);
