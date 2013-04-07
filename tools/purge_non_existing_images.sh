5d9059a2009477cc6176c645d517d834b1cc4f22
johannes hanika <hanatos@gmail.com>
2013-04-07 23:27:34 +1200

    purge images: tony arnold's version. (cherry picked from commit 63f06d2bbe31753088271682317e35961a4add6c)

--- tools/purge_non_existing_images.sh
+++ tools/purge_non_existing_images.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 DBFILE=~/.config/darktable/library.db
+TMPFILE=`mktemp`
 QUERY="select A.id,B.folder,A.filename from images as A join film_rolls as B on A.film_id = B.id"
-sqlite3 $DBFILE "$QUERY"| while read result
+sqlite3 $DBFILE "$QUERY" > $TMPFILE
+cat $TMPFILE | while read result
 do
   ID=$(echo "$result" | cut -f1 -d"|")
   FD=$(echo "$result" | cut -f2 -d"|")
   FL=$(echo "$result" | cut -f3 -d"|")
   if ! [ -f "$FD/$FL" ];
   then
-    echo "removing non existent file $FD/$FL"
-    # uncomment this dangerous line to actually do it:
-    # sqlite3 $DBFILE "delete from images where id=$ID"
+    echo "removing non existent file $FD/$FL with ID = $ID"
+    sqlite3 $DBFILE "delete from images where id=$ID"
   fi
 done
+rm $TMPFILE
