77c64f8a5c84eab9bc392e35e88e47a83cb75d52
Jérémy Rosen <jeremy.rosen@openwide.fr>
2013-04-17 18:05:26 +0200

    lua : add lua preferences
    adds three function to the API
    
    * darktable.preferences.register(script,name,type,label,tooltip,default,[min,max])
      creates a new preference entry in the lua tab of pre
      script : invisible prefix to guarentee unicity of preferences
      name : the name of the preference to display
      type : one of "string" "bool" "integer"
      tooltip : the tooltip to display
      default : default value
      min : (int only) minimum value
      max : (int only) maximum value
    
    * darktable.preferences.read(script,name,type) : value
      reads a value from a lua preference
      script : an invisible prefix
      name : name of the preference to read
      type : the type of the preference to read
      returns : the value of the preference
    
    * darktable.preferences.write(script,name,type,value)
      writes a value to a lua preference
      script : an invisible prefix
      name : name of the preference to set
      type : type of the preference
      value : value to set

--- src/lua/init.c
+++ src/lua/init.c
@@ -19,6 +19,7 @@
 #include "lua/init.h"
 #include "lua/call.h"
 #include "lua/configuration.h"
+#include "lua/preferences.h"
 #include "lua/print.h"
 #include "common/darktable.h"
 #include "common/file_location.h"
@@ -40,6 +41,7 @@ static int dt_luacleanup(lua_State*L) {
 static lua_CFunction init_funcs[] = {
   dt_lua_init_print,
   dt_lua_init_configuration,
+  dt_lua_init_preferences,
   NULL
 };
 
