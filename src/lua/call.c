a26448113d17a0964e1cff5dad604e34ace0bbe5
Jérémy Rosen <jeremy.rosen@openwide.fr>
2013-04-17 09:32:06 +0200

    lua : add wrappers to call lua from DT
    these function allow to load a file or a string, call it as a lua content and treat errors correctly

---
+++ src/lua/call.c
@@ -0,0 +1,73 @@
+/*
+   This file is part of darktable,
+   copyright (c) 2012 Jeremy Rosen
+
+   darktable is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   darktable is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with darktable.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "lua/lua.h"
+#include "lua/call.h"
+#include "control/control.h"
+
+static int dump_error(lua_State *L) {
+	const char * message = lua_tostring(L,-1);
+	if(darktable.unmuted & DT_DEBUG_LUA)
+		luaL_traceback(L,L,message,0);
+	// else : the message is already on the top of the stack, don't touch
+	return 1;
+}
+
+static int dt_lua_do_chunk(lua_State *L,int loadresult,int nargs,int nresults) {
+  int result;
+  if(loadresult){
+    dt_print(DT_DEBUG_LUA,"LUA ERROR %s\n",lua_tostring(L,-1));
+    lua_pop(L,1);
+    return 0;
+  }
+  lua_pushcfunction(L,dump_error);
+  lua_insert(L,lua_gettop(L)-(nargs+1));
+  result = lua_gettop(L)-(nargs+1); // remember the stack size to findout the number of results in case of multiret
+  if(lua_pcall(L, nargs, nresults,result)) {
+    dt_print(DT_DEBUG_LUA,"LUA ERROR %s\n",lua_tostring(L,-1));
+    lua_pop(L,2);
+    if(nresults !=LUA_MULTRET) {
+      for(int i= 0 ; i < nresults; i++) {
+        lua_pushnil(L);
+      }
+    }
+  }
+  lua_remove(L,result); // remove the error handler
+  result= lua_gettop(L) -result;
+
+  lua_gc(L,LUA_GCCOLLECT,0);
+  if(darktable.gui!=NULL) dt_control_queue_redraw();
+  return result;
+}
+
+void dt_lua_protect_call(lua_State *L,lua_CFunction func) {
+  lua_pushcfunction(L,func);
+  dt_lua_do_chunk(L,0,0,0);
+}
+void dt_lua_dostring(lua_State *L,const char* command) {
+  dt_lua_do_chunk(L,luaL_loadstring(darktable.lua_state, command),0,0);
+}
+
+void dt_lua_dofile(lua_State *L,const char* filename) {
+  dt_lua_do_chunk(L,luaL_loadfile(darktable.lua_state, filename),0,0);
+}
+
+// closed on GC of the dt lib, usually when the lua interpreter closes
+
+// modelines: These editor modelines have been set for all relevant files by tools/update_modelines.sh
+// vim: shiftwidth=2 expandtab tabstop=2 cindent
+// kate: tab-indents: off; indent-width 2; replace-tabs on; indent-mode cstyle; remove-trailing-space on;
---
+++ src/lua/call.h
@@ -0,0 +1,34 @@
+/*
+   This file is part of darktable,
+   copyright (c) 2012 Jeremy Rosen
+
+   darktable is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   darktable is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with darktable.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef DT_LUA_CALL_H
+#define DT_LUA_CALL_H
+
+
+/** runs a command in the DT lua environement, command in any valid lua string */
+void dt_lua_dostring(lua_State *L,const char* command);
+
+/** executes a CFunction through dt_lua_do_chunk, no parameter, no result */
+void dt_lua_protect_call(lua_State *L,lua_CFunction func);
+
+/** runs the content of a file */
+void dt_lua_dofile(lua_State *L,const char* filename);
+
+#endif
+// modelines: These editor modelines have been set for all relevant files by tools/update_modelines.sh
+// vim: shiftwidth=2 expandtab tabstop=2 cindent
+// kate: tab-indents: off; indent-width 2; replace-tabs on; indent-mode cstyle; remove-trailing-space on;
