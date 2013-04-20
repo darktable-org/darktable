a278fae5dd7e4fb6e1a0e23f4fc805a87ea8ab58
Boucman <jeremy.rosen@enst-bretagne.fr>
2013-04-20 19:31:54 +0200

    lua : introduce glist helpers
    this allows function to easily push and pull a glist of lautoc-typed objects
    
    the list is converted into a numerically-indexed table of converted objects
    
    when converting back all objects are taken independently of their index

---
+++ src/lua/glist.c
@@ -0,0 +1,61 @@
+/*
+   This file is part of darktable,
+   copyright (c) 2012 Jeremy Rosen
+
+   darktable is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   darktable is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with darktable.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "lua/glist.h"
+#include <lauxlib.h>
+#include <stdlib.h>
+
+void dt_lua_push_glist_typeid(lua_State *L,GList * list, luaA_Type elt_type)
+{
+  GList * elt = list;
+  lua_newtable(L);
+  while(elt)
+  {
+    luaA_push_typeid(L,elt_type,elt->data);
+    luaL_ref(L,-2);
+    elt = g_list_next(elt);
+  }
+}
+
+GList* dt_lua_to_glist_typeid(lua_State *L, luaA_Type elt_type,int index)
+{
+  // recreate list of images
+  GList * list = NULL;
+  size_t type_size = luaA_type_size(elt_type);
+  lua_pushnil(L);  /* first key */
+  while (lua_next(L, index -1) != 0)
+  {
+    /* uses 'key' (at index -2) and 'value' (at index -1) */
+    void*obj = malloc(type_size);
+    luaA_to_typeid(L,elt_type,obj,-1);
+    lua_pop(L,1);
+    list = g_list_prepend(list,(gpointer)obj);
+  }
+  list = g_list_reverse(list);
+  return list;
+}
+
+int dt_lua_init_glist(lua_State * L)
+{
+  return 0;
+}
+
+
+// modelines: These editor modelines have been set for all relevant files by tools/update_modelines.sh
+// vim: shiftwidth=2 expandtab tabstop=2 cindent
+// kate: tab-indents: off; indent-width 2; replace-tabs on; indent-mode cstyle; remove-trailing-space on;
