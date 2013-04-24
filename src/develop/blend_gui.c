cdf0f75772cb580f2eadd695a11e907768c03f48
Ulrich Pegelow <ulrich.pegelow@tongareva.de>
2013-04-24 19:23:28 +0200

    masks and blending: some wording changes

--- src/develop/blend_gui.c
+++ src/develop/blend_gui.c
@@ -544,7 +544,6 @@ static void
 _blendop_blendif_reset(GtkButton *button, dt_iop_module_t *module)
 {
   module->blend_params->blendif = module->default_blendop_params->blendif;
-  module->blend_params->radius = module->default_blendop_params->radius;
   memcpy(module->blend_params->blendif_parameters, module->default_blendop_params->blendif_parameters, 4*DEVELOP_BLENDIF_SIZE*sizeof(float));
 
   dt_iop_gui_update_blendif(module);
@@ -1084,24 +1083,24 @@ void dt_iop_gui_init_blending(GtkWidget *iopw, dt_iop_module_t *module)
 
     if(bd->masks_support)
     {
-      dt_bauhaus_combobox_add(bd->masks_modes_combo, _("using mask"));
+      dt_bauhaus_combobox_add(bd->masks_modes_combo, _("with drawn mask"));
       bd->masks_modes = g_list_append(bd->masks_modes, GUINT_TO_POINTER(DEVELOP_MASK_ENABLED | DEVELOP_MASK_MASK));
     }
 
     if(bd->blendif_support)
     {
-      dt_bauhaus_combobox_add(bd->masks_modes_combo, _("conditional"));
+      dt_bauhaus_combobox_add(bd->masks_modes_combo, _("with parametric mask"));
       bd->masks_modes = g_list_append(bd->masks_modes, GUINT_TO_POINTER(DEVELOP_MASK_ENABLED | DEVELOP_MASK_CONDITIONAL));
     }
 
     if(bd->blendif_support && bd->masks_support)
     {
-      dt_bauhaus_combobox_add(bd->masks_modes_combo, _("using mask & conditional"));
+      dt_bauhaus_combobox_add(bd->masks_modes_combo, _("with drawn & parametric mask"));
       bd->masks_modes = g_list_append(bd->masks_modes, GUINT_TO_POINTER(DEVELOP_MASK_ENABLED | DEVELOP_MASK_BOTH));
     }
 
     dt_bauhaus_combobox_set(bd->masks_modes_combo, 0);
-    gtk_object_set(GTK_OBJECT(bd->masks_modes_combo), "tooltip-text", _("activate blending: uniformly, with mask, conditional, or combination of both"), (char *)NULL);
+    gtk_object_set(GTK_OBJECT(bd->masks_modes_combo), "tooltip-text", _("activate blending: uniformly, with drawn mask, parametric mask, or combination of both"), (char *)NULL);
     g_signal_connect (G_OBJECT (bd->masks_modes_combo), "value-changed",
                       G_CALLBACK (_blendop_masks_mode_callback), bd);
 
@@ -1192,20 +1191,20 @@ void dt_iop_gui_init_blending(GtkWidget *iopw, dt_iop_module_t *module)
     bd->masks_combine_combo = dt_bauhaus_combobox_new(module);
     dt_bauhaus_widget_set_label(bd->masks_combine_combo, _("combine masks"));
 
-    dt_bauhaus_combobox_add(bd->masks_combine_combo, _("substractive-normal"));
+    dt_bauhaus_combobox_add(bd->masks_combine_combo, _("substractive"));
     bd->masks_combine = g_list_append(bd->masks_combine, GUINT_TO_POINTER(DEVELOP_COMBINE_NORM_EXCL));
 
-    dt_bauhaus_combobox_add(bd->masks_combine_combo, _("substractive-inverted"));
-    bd->masks_combine = g_list_append(bd->masks_combine, GUINT_TO_POINTER(DEVELOP_COMBINE_INV_EXCL));
-
-    dt_bauhaus_combobox_add(bd->masks_combine_combo, _("additive-normal"));
+    dt_bauhaus_combobox_add(bd->masks_combine_combo, _("additive"));
     bd->masks_combine = g_list_append(bd->masks_combine, GUINT_TO_POINTER(DEVELOP_COMBINE_NORM_INCL));
 
-    dt_bauhaus_combobox_add(bd->masks_combine_combo, _("additive-inverted"));
+    dt_bauhaus_combobox_add(bd->masks_combine_combo, _("substractive & inverted"));
+    bd->masks_combine = g_list_append(bd->masks_combine, GUINT_TO_POINTER(DEVELOP_COMBINE_INV_EXCL));
+
+    dt_bauhaus_combobox_add(bd->masks_combine_combo, _("additive & inverted"));
     bd->masks_combine = g_list_append(bd->masks_combine, GUINT_TO_POINTER(DEVELOP_COMBINE_INV_INCL));
 
     dt_bauhaus_combobox_set(bd->masks_combine_combo, 0);
-    gtk_object_set(GTK_OBJECT(bd->masks_combine_combo), "tooltip-text", _("how to combine individual conditional masks and painted masks"), (char *)NULL);
+    gtk_object_set(GTK_OBJECT(bd->masks_combine_combo), "tooltip-text", _("how to combine individual drawn masks and parametric masks"), (char *)NULL);
     g_signal_connect (G_OBJECT (bd->masks_combine_combo), "value-changed",
                       G_CALLBACK (_blendop_masks_combine_callback), bd);
 
@@ -1225,7 +1224,7 @@ void dt_iop_gui_init_blending(GtkWidget *iopw, dt_iop_module_t *module)
 
 
     bd->suppress = dtgtk_togglebutton_new(dtgtk_cairo_paint_eye_toggle, CPF_STYLE_FLAT|CPF_DO_NOT_USE_BORDER);
-    g_object_set(G_OBJECT(bd->suppress), "tooltip-text", _("temporarily switch off blend mask. only for module in focus."), (char *)NULL);
+    g_object_set(G_OBJECT(bd->suppress), "tooltip-text", _("temporarily switch off blend mask. only for module in focus"), (char *)NULL);
     g_signal_connect (G_OBJECT(bd->suppress), "toggled",
                       G_CALLBACK (_blendop_blendif_suppress_toggled), module);
 
