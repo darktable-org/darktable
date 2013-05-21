7a194eb4777caa8f8dd5f9944924911cbd248a0b
Pascal de Bruijn <pmjdebruijn@pcode.nl>
2013-05-21 18:20:15 +0200

    temperature: kelvin is high, introduce kludge factor

--- src/iop/temperature.c
+++ src/iop/temperature.c
@@ -40,6 +40,8 @@ DT_MODULE(2)
 #define DT_IOP_LOWEST_TEMPERATURE     2000
 #define DT_IOP_HIGHEST_TEMPERATURE   23000
 
+#define DT_IOP_KLUDGE_FACTOR 1.6
+
 typedef struct dt_iop_temperature_params_t
 {
   float temp_out;
@@ -371,7 +373,7 @@ void gui_update (struct dt_iop_module_t *self)
   dt_bauhaus_slider_set(g->scale_r, p->coeffs[0]);
   dt_bauhaus_slider_set(g->scale_g, p->coeffs[1]);
   dt_bauhaus_slider_set(g->scale_b, p->coeffs[2]);
-  dt_bauhaus_slider_set(g->scale_k, temp);
+  dt_bauhaus_slider_set(g->scale_k, temp / DT_IOP_KLUDGE_FACTOR);
   dt_bauhaus_slider_set(g->scale_tint, tint);
   if(fabsf(p->coeffs[0]-fp->coeffs[0]) + fabsf(p->coeffs[1]-fp->coeffs[1]) + fabsf(p->coeffs[2]-fp->coeffs[2]) < 0.01)
     dt_bauhaus_combobox_set(g->presets, 0);
@@ -456,7 +458,7 @@ void reload_defaults(dt_iop_module_t *module)
         float temp, tint, mul[3];
         for(int k=0; k<3; k++) mul[k] = g->daylight_wb[k]/tmp.coeffs[k];
         convert_rgb_to_k(mul, &temp, &tint);
-        dt_bauhaus_slider_set_default(g->scale_k,    temp);
+        dt_bauhaus_slider_set_default(g->scale_k,    temp / DT_IOP_KLUDGE_FACTOR);
         dt_bauhaus_slider_set_default(g->scale_tint, tint);
       }
 
@@ -517,7 +519,7 @@ gui_update_from_coeffs (dt_iop_module_t *self)
   convert_rgb_to_k(mul, &temp, &tint);
 
   darktable.gui->reset = 1;
-  dt_bauhaus_slider_set(g->scale_k,    temp);
+  dt_bauhaus_slider_set(g->scale_k,    temp / DT_IOP_KLUDGE_FACTOR);
   dt_bauhaus_slider_set(g->scale_tint, tint);
   dt_bauhaus_slider_set(g->scale_r, p->coeffs[0]);
   dt_bauhaus_slider_set(g->scale_g, p->coeffs[1]);
@@ -555,7 +557,7 @@ temp_changed(dt_iop_module_t *self)
   dt_iop_temperature_gui_data_t *g = (dt_iop_temperature_gui_data_t *)self->gui_data;
   dt_iop_temperature_params_t *p  = (dt_iop_temperature_params_t *)self->params;
 
-  const float temp = dt_bauhaus_slider_get(g->scale_k);
+  const float temp = dt_bauhaus_slider_get(g->scale_k) * DT_IOP_KLUDGE_FACTOR;
   const float tint = dt_bauhaus_slider_get(g->scale_tint);
 
   convert_k_to_rgb(temp, p->coeffs);
