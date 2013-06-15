269b544b3dcea7a5b3578dde42ca7d78052ac42b
Ulrich Pegelow <ulrich.pegelow@tongareva.de>
2013-06-15 10:06:17 +0200

    opencl: set locale to "C" during initialization. fixes #9467 (cherry picked from commit 092e3cc6b758be46409726dfc1b357e414ccbddb)
    Conflicts:
    	src/common/opencl.c

--- src/common/opencl.c
+++ src/common/opencl.c
@@ -29,6 +29,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <assert.h>
+#include <locale.h>
 
 #include <sys/stat.h>
 #include <errno.h>
@@ -48,6 +49,11 @@ void dt_opencl_init(dt_opencl_t *cl, const int argc, char *argv[])
   cl->avoid_atomics = dt_conf_get_bool("opencl_avoid_atomics");
   cl->dlocl = NULL;
   int exclude_opencl = 0;
+
+  // work-around to fix a bug in some AMD OpenCL compilers, which would fail parsing certain numerical constants if locale is different from "C".
+  // we save the current locale, set locale to "C", and restore the previous setting after OpenCL is initialized
+  char *locale = setlocale(LC_ALL, NULL);
+  setlocale(LC_ALL, "C");
 
   // user selectable parameter defines minimum requirement on GPU memory
   // default is 768MB
@@ -341,6 +347,7 @@ finally:
     cl->bilateral = dt_bilateral_init_cl_global();
     cl->gaussian = dt_gaussian_init_cl_global();
   }
+  if(locale) setlocale(LC_ALL, locale);
   return;
 }
 
