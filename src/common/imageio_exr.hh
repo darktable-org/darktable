cfaefa2e0ebe48737b62316d62bd98d8b0db2def
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2013-09-26 22:23:15 +0200

    std::tr1::shared_ptr isn't supported in Clang, only std::shared_ptr is
    Also use std::shared_ptr when C++11 standard is enabled.
    
    This fixes the build with Clang.

--- src/common/imageio_exr.hh
+++ src/common/imageio_exr.hh
@@ -21,7 +21,11 @@
 #include "common/image.h"
 #include "common/mipmap_cache.h"
 
+#if __cplusplus >= 201103L || defined(__clang__)
+#include <memory>
+#else
 #include <tr1/memory>
+#endif
 
 #include <OpenEXR/ImfFrameBuffer.h>
 #include <OpenEXR/ImfTestFile.h>
@@ -56,7 +60,11 @@ public:
   }
 
   uint32_t size;
+#if __cplusplus >= 201103L || defined(__clang__)
+  std::shared_ptr<uint8_t> data;
+#else
   std::tr1::shared_ptr<uint8_t> data;
+#endif
 };
 
 
