d40e4bb1d96e38b970b728f742adf4cd59632221
Richard Levitte <richard@levitte.org>
2013-10-18 09:50:24 +0200

    Add a script to update po/POTFILES.in. Make sure to respect the contents of po/POTFILES.skip.

---
+++ tools/update_potfiles_in.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SOURCES=$(
+    (
+	find src/ -name "*.c" -or -name "*.cc" -or -name "*.h" | grep -v src/external | grep -v gegl-operations
+	find build/src/ -name '*.c' -o -name '*.cc' -o -name '*.h'
+    ) | fgrep -v -f po/POTFILES.skip | sort
+)
+
+fgrep -l '_(' $SOURCES > po/POTFILES.in.2
+if diff po/POTFILES.in po/POTFILES.in.2 >/dev/null 2>&1 ; then
+    rm -f po/POTFILES.in.2
+else
+    mv po/POTFILES.in.2 po/POTFILES.in
+fi
