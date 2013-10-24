a9cc7d510c21c09aeaeda505a08ba1f9bc8991a2
Dennis Gnad <dennis@xorn.org>
2013-10-24 22:48:29 +0200

    defringe: 1. speedup by fibonacci precalculation + 2. expose more internal values to GUI

--- src/iop/defringe.c
+++ src/iop/defringe.c
@@ -24,6 +24,7 @@
 #include "bauhaus/bauhaus.h"
 #include "common/darktable.h"
 #include "develop/imageop.h"
+#include "dtgtk/label.h"
 #include "dtgtk/slider.h"
 #include "gui/gtk.h"
 #include <gtk/gtk.h>
@@ -36,13 +37,20 @@ DT_MODULE(1)
 #define MODE_LOCAL_AVERAGE 1
 #define MODE_STATIC 2
 
+#define EXPERT_OPTIONS
+
 typedef struct dt_iop_defringe_params_t
 {
   float radius;
   float thresh;
   uint32_t op_mode;
-  float a_bias;
+  float g_bias;
+  float m_bias;
   float b_bias;
+  float y_bias;
+#ifdef EXPERT_OPTIONS
+  float x,y,z;
+#endif
 }
 dt_iop_defringe_params_t;
 
@@ -54,8 +62,13 @@ typedef struct dt_iop_defringe_gui_data_t
   GtkWidget *mode_select;
   GtkWidget *radius_scale;
   GtkWidget *thresh_scale;
-  GtkWidget *a_bias_scale;
+  GtkWidget *g_bias_scale;
+  GtkWidget *m_bias_scale;
   GtkWidget *b_bias_scale;
+  GtkWidget *y_bias_scale;
+#ifdef EXPERT_OPTIONS
+  GtkWidget * x,*y,*z;
+#endif
 }
 dt_iop_defringe_gui_data_t;
 
@@ -163,6 +176,91 @@ void process (struct dt_iop_module_t *module, dt_dev_pixelpipe_iop_t *piece, voi
   dt_gaussian_blur(gauss, in, out);
   dt_gaussian_free(gauss);
 
+  // Pre-Compute Fibonacci Lattices
+  int *tmp;
+
+  int samples_wish = radius*radius*0.8;
+  int sampleidx_avg;
+  // select samples by fibonacci number
+  // too slow with 144 samples:
+  //  if (samples_wish > 89) {
+  //    sampleidx = 12; // 144 samples
+  if (samples_wish > 55) {
+    sampleidx_avg = 11; // 89 samples
+  } else if (samples_wish > 34) {
+    sampleidx_avg = 10; // ..you get the idea
+  } else if (samples_wish > 21) {
+    sampleidx_avg = 9;
+  } else if (samples_wish > 13) {
+    sampleidx_avg = 8;
+  } else { // don't use less than 13 samples
+    sampleidx_avg = 7;
+  }
+  const int sampleidx_small = sampleidx_avg-1;
+  // larger area when using local averaging
+#ifdef EXPERT_OPTIONS
+  const int radius_avg = ceil(radius*10*p->y);
+#else
+  const int radius_avg = ceil(radius*10);
+#endif
+  // smaller area for artifact filter
+  const int radius_artifact_filter = radius/2;
+
+  const int samples_small = fib[sampleidx_small];
+  const int samples_avg = fib[sampleidx_avg];
+  const int samples_artifact = samples_avg;
+
+  int * xy_avg;
+  if ((xy_avg = g_malloc(2 * sizeof(int) * samples_avg)))
+  {
+    tmp = xy_avg;
+    for (int u=0; u < samples_avg; u++)
+    {
+      int dx,dy;
+      fib_latt(&dx,&dy,radius_avg,u,sampleidx_avg);
+      *tmp++ = dx;
+      *tmp++ = dy;
+    }
+  }
+  else
+  {
+    return;
+  }
+
+  int * xy_small;
+  if ((xy_small = g_malloc(2 * sizeof(int) * samples_small)))
+  {
+    tmp = xy_small;
+    for (int u=0; u < samples_small; u++)
+    {
+      int dx,dy;
+      fib_latt(&dx,&dy,radius,u,sampleidx_small);
+      *tmp++ = dx;
+      *tmp++ = dy;
+    }
+  }
+  else
+  {
+    return;
+  }
+
+  int * xy_artifact;
+  if ((xy_artifact = g_malloc(2 * sizeof(int) * samples_small)))
+  {
+    tmp = xy_artifact;
+    for (int u=0; u < samples_small; u++)
+    {
+      int dx,dy;
+      fib_latt(&dx,&dy,radius_artifact_filter,u,sampleidx_small);
+      *tmp++ = dx;
+      *tmp++ = dy;
+    }
+  }
+  else
+  {
+    return;
+  }
+
 #ifdef _OPENMP
 #pragma omp parallel for default(none) shared(width,height,p) reduction(+:avg_edge_chroma) schedule(static)
 #endif
@@ -175,22 +273,30 @@ void process (struct dt_iop_module_t *module, dt_dev_pixelpipe_iop_t *piece, voi
       float a = in[v*width*ch + t*ch +1] - out[v*width*ch + t*ch +1];
       float b = in[v*width*ch + t*ch +2] - out[v*width*ch + t*ch +2];
 
+      float ab_edge_prop;
+#ifdef EXPERT_OPTIONS
+      ab_edge_prop = p->x;
+#else
+      ab_edge_prop = 0.6;
+#endif
+
       // color biasing
       if (a > 0.0)
-        a = a*(0.1+p->a_bias); // magenta bias
+        a = a*(0.1 + p->m_bias); // magenta bias
       else
-        a = a*(0.1+(1-p->a_bias)); // green bias
+        a = a*(0.1 + p->g_bias); // green bias
 
       if (b > 0.0)
-        b = b*(0.1+p->b_bias); // yellow bias
+        b = b*(0.1 + p->y_bias); // yellow bias
       else
-        b = b*(0.1+(1-p->b_bias)); // blue bias
+        b = b*(0.1 + p->b_bias); // blue bias
 
       float edge = (a*a+b*b); //range up to 2 * (1.1*256)^2 -> approx. 0 to 158598
       float saturation = fabs(in[v*width*ch + t*ch +1]) + fabs(in[v*width*ch + t*ch +2]);
+      float luminosity = in[v*width*ch + t*ch];
       // save local edge chroma in out[.. +3] , this is later compared with threshold
       // partially dependent on luminosity / saturation
-      out[v*width*ch + t*ch +3] = edge * 0.6 + edge * 0.4 * saturation/255.0 * in[v*width*ch + t*ch]/255.0;
+      out[v*width*ch + t*ch +3] = edge * ab_edge_prop + edge * (1.0-ab_edge_prop) * saturation/255.0 * luminosity/255.0;
       // the average chroma of the edge-layer in the roi
       if (MODE_GLOBAL_AVERAGE == p->op_mode) avg_edge_chroma += edge;
     }
@@ -209,32 +315,9 @@ void process (struct dt_iop_module_t *module, dt_dev_pixelpipe_iop_t *piece, voi
     thresh = 6.0 * p->thresh;
   }
 
-  // precompute stuff
-  int samples_wish = radius*radius*0.8;
-  int sampleidx;
-  // select samples by fibonacci number
-  if (samples_wish > 89) {
-    sampleidx = 12; // 144 samples
-  } else if (samples_wish > 55) {
-    sampleidx = 11; // 89 samples
-  } else if (samples_wish > 34) {
-    sampleidx = 10; // ..you get the idea
-  } else if (samples_wish > 21) {
-    sampleidx = 9;
-  } else if (samples_wish > 13) {
-    sampleidx = 8;
-  } else { // don't use less than 13 samples
-    sampleidx = 7;
-  }
-  const int sampleidx_small = sampleidx-1;
-  // larger area when using local averaging
-  const int radius_avg = ceil(radius*10);
-  // smaller area for artifact filter
-  const int radius_artifact_filter = radius/2;
-
 #ifdef _OPENMP
 // dynamically scheduled due to possible uneven edge-chroma distribution (thanks to rawtherapee code for this hint!)
-#pragma omp parallel for default(none) shared(width,height,p,sampleidx) firstprivate(thresh,avg_edge_chroma) schedule(dynamic,32)
+#pragma omp parallel for default(none) shared(width,height,p,xy_small,xy_avg,xy_artifact) firstprivate(thresh,avg_edge_chroma) schedule(dynamic,32)
 #endif
   for (int v=0; v<height; v++)
   {
@@ -254,22 +337,24 @@ void process (struct dt_iop_module_t *module, dt_dev_pixelpipe_iop_t *piece, voi
         {
           float local_avg = 0.0;
           // use some and not all values from the neigbourhood to speed things up
-          for (int u=0; u < fib[sampleidx]; u++)
+          const int *tmp = xy_avg;
+          for (int u=0; u < samples_avg; u++)
           {
-            int dx,dy;
-            fib_latt(&dx,&dy,radius_avg,u,sampleidx);
+            int dx = *tmp++;
+            int dy = *tmp++;
             int x = MAX(0,MIN(height,v+dx));
             int y = MAX(0,MIN(width,t+dy));
             local_avg += out[x*width*ch + y*ch +3];
           }
-          local_avg /= (float)fib[sampleidx]*2.0;
+          local_avg /= (float)samples_avg*2.0;
           avg_edge_chroma = local_avg;
         }
         // use some neighbourhood pixels for lowest chroma average
-        for (int u=0; u < fib[sampleidx_small]; u++)
+        const int *tmp = xy_small;
+        for (int u=0; u < samples_small; u++)
         {
-          int dx,dy;
-          fib_latt(&dx,&dy,radius,u,sampleidx_small);
+          int dx = *tmp++;
+          int dy = *tmp++;
           int x = MAX(0,MIN(height,v+dx));
           int y = MAX(0,MIN(width,t+dy));
           // inverse chroma weighted average of neigbouring pixels inside window
@@ -300,10 +385,11 @@ void process (struct dt_iop_module_t *module, dt_dev_pixelpipe_iop_t *piece, voi
         float norm=0;
         float weight;
         // dup'ed code from above:
-        for (int u=0; u < fib[sampleidx_small]; u++)
+        const int *tmp = xy_artifact;
+        for (int u=0; u < samples_artifact; u++)
         {
-          int dx,dy;
-          fib_latt(&dx,&dy,radius_artifact_filter,u,sampleidx_small);
+          int dx = *tmp++;
+          int dy = *tmp++;
           int x = MAX(0,MIN(height,v+dx));
           int y = MAX(0,MIN(width,t+dy));
           // inverse chroma weighted average of neigbouring pixels inside window
@@ -335,16 +421,26 @@ void process (struct dt_iop_module_t *module, dt_dev_pixelpipe_iop_t *piece, voi
       out[v*width*ch + t*ch +3] = in[v*width*ch + t*ch +3];
     }
   }
+  g_free(xy_artifact);
+  g_free(xy_small);
+  g_free(xy_avg);
 }
 
 void reload_defaults(dt_iop_module_t *module)
 {
   module->default_enabled = 0;
-  ((dt_iop_defringe_params_t *)module->default_params)->radius = 4.0;
-  ((dt_iop_defringe_params_t *)module->default_params)->thresh = 20;
+  ((dt_iop_defringe_params_t *)module->default_params)->radius = 5.0;
+  ((dt_iop_defringe_params_t *)module->default_params)->thresh = 10;
   ((dt_iop_defringe_params_t *)module->default_params)->op_mode = MODE_GLOBAL_AVERAGE;
-  ((dt_iop_defringe_params_t *)module->default_params)->a_bias = +0.2; // more reddish -> "purple fringe"
-  ((dt_iop_defringe_params_t *)module->default_params)->b_bias = -0.3; // more blueish -> purple
+  ((dt_iop_defringe_params_t *)module->default_params)->m_bias = +1.0; // magenta
+  ((dt_iop_defringe_params_t *)module->default_params)->g_bias = +1.0; // green
+  ((dt_iop_defringe_params_t *)module->default_params)->b_bias = +1.0; // blue
+  ((dt_iop_defringe_params_t *)module->default_params)->y_bias = +1.0; // yellow
+#ifdef EXPERT_OPTIONS
+  ((dt_iop_defringe_params_t *)module->default_params)->x = 0.6;
+  ((dt_iop_defringe_params_t *)module->default_params)->y = 1.0;
+  ((dt_iop_defringe_params_t *)module->default_params)->z = 0.0;
+#endif
   memcpy(module->params, module->default_params, sizeof(dt_iop_defringe_params_t));
 }
 
@@ -388,22 +484,38 @@ thresh_slider_callback (GtkWidget *w, dt_iop_module_t *module)
 }
 
 static void
-a_bias_slider_callback (GtkWidget *w, dt_iop_module_t *module)
+bias_slider_callback (GtkWidget *w, dt_iop_module_t *module)
 {
   if(darktable.gui->reset) return;
+  dt_iop_defringe_gui_data_t *g = (dt_iop_defringe_gui_data_t *)module->gui_data;
   dt_iop_defringe_params_t *p = (dt_iop_defringe_params_t *)module->params;
-  p->a_bias = dt_bauhaus_slider_get(w);
+  if (w == g->g_bias_scale)
+    p->g_bias = dt_bauhaus_slider_get(w);
+  else if (w == g->m_bias_scale)
+    p->m_bias = dt_bauhaus_slider_get(w);
+  else if (w == g->b_bias_scale)
+    p->b_bias = dt_bauhaus_slider_get(w);
+  else if (w == g->y_bias_scale)
+    p->y_bias = dt_bauhaus_slider_get(w);
   dt_dev_add_history_item(darktable.develop, module, TRUE);
 }
 
+#ifdef EXPERT_OPTIONS
 static void
-b_bias_slider_callback (GtkWidget *w, dt_iop_module_t *module)
+xyz_cb (GtkWidget *w, dt_iop_module_t *module)
 {
   if(darktable.gui->reset) return;
   dt_iop_defringe_params_t *p = (dt_iop_defringe_params_t *)module->params;
-  p->b_bias = dt_bauhaus_slider_get(w);
+  dt_iop_defringe_gui_data_t *g = (dt_iop_defringe_gui_data_t *)module->gui_data;
+  if (w == g->x)
+    p->x = dt_bauhaus_slider_get(w);
+  else if (w == g->y)
+    p->y = dt_bauhaus_slider_get(w);
+  else if (w == g->z)
+    p->z = dt_bauhaus_slider_get(w);
   dt_dev_add_history_item(darktable.develop, module, TRUE);
 }
+#endif
 
 static void
 mode_callback (GtkWidget *w, dt_iop_module_t *module)
@@ -450,26 +562,69 @@ void gui_init (dt_iop_module_t *module)
                    G_CALLBACK(radius_slider_callback), module);
   g_signal_connect(G_OBJECT(g->thresh_scale), "value-changed",
                    G_CALLBACK(thresh_slider_callback), module);
+
   /* color bias/shift/whatever sliders */
-  g->a_bias_scale = dt_bauhaus_slider_new_with_range_and_feedback(module, -1.0, 1.0, 0.01, p->a_bias, 2, 0);
-  dt_bauhaus_widget_set_label(g->a_bias_scale, NULL, _("green / magenta balance"));
-  dt_bauhaus_slider_set_stop(g->a_bias_scale, 0.0f, 0.0f, 1.0f, 0.0f);
-  dt_bauhaus_slider_set_stop(g->a_bias_scale, 1.0f, 1.0f, 0.0f, 1.0f);
-  g_object_set(G_OBJECT(g->a_bias_scale), "tooltip-text", _("move between stronger green or magenta sensitivity"), (char *)NULL);
+  g->g_bias_scale = dt_bauhaus_slider_new_with_range(module, 0.0, 2.0, 0.01, p->g_bias, 2);
+  dt_bauhaus_widget_set_label(g->g_bias_scale, NULL, _("green bias"));
+  dt_bauhaus_slider_set_stop(g->g_bias_scale, 0.0f, 0.0f, 0.0f, 0.0f);
+  dt_bauhaus_slider_set_stop(g->g_bias_scale, 1.0f, 0.0f, 1.0f, 0.0f);
+  g_object_set(G_OBJECT(g->g_bias_scale), "tooltip-text", _("green sensitivity"), (char *)NULL);
 
-  g->b_bias_scale = dt_bauhaus_slider_new_with_range_and_feedback(module, -1.0, 1.0, 0.01, p->b_bias, 2, 0);
-  dt_bauhaus_widget_set_label(g->b_bias_scale, NULL, _("blue / yellow balance"));
-  dt_bauhaus_slider_set_stop(g->b_bias_scale, 0.0f, 0.0f, 0.0f, 1.0f);
-  dt_bauhaus_slider_set_stop(g->b_bias_scale, 1.0f, 1.0f, 1.0f, 0.0f);
-  g_object_set(G_OBJECT(g->b_bias_scale), "tooltip-text", _("move between stronger blue or yellow sensitivity"), (char *)NULL);
+  g->m_bias_scale = dt_bauhaus_slider_new_with_range(module, 0.0, 2.0, 0.01, p->m_bias, 2);
+  dt_bauhaus_widget_set_label(g->m_bias_scale, NULL, _("magenta bias"));
+  dt_bauhaus_slider_set_stop(g->m_bias_scale, 0.0f, 0.0f, 0.0f, 0.0f);
+  dt_bauhaus_slider_set_stop(g->m_bias_scale, 1.0f, 1.0f, 0.0f, 1.0f);
+  g_object_set(G_OBJECT(g->m_bias_scale), "tooltip-text", _("magenta sensitivity"), (char *)NULL);
 
-  g_signal_connect(G_OBJECT(g->a_bias_scale), "value-changed",
-                   G_CALLBACK(a_bias_slider_callback), module);
+  g->b_bias_scale = dt_bauhaus_slider_new_with_range(module, 0.0, 2.0, 0.01, p->b_bias, 2);
+  dt_bauhaus_widget_set_label(g->b_bias_scale, NULL, _("blue bias"));
+  dt_bauhaus_slider_set_stop(g->b_bias_scale, 0.0f, 0.0f, 0.0f, 0.0f);
+  dt_bauhaus_slider_set_stop(g->b_bias_scale, 1.0f, 0.0f, 0.0f, 1.0f);
+  g_object_set(G_OBJECT(g->b_bias_scale), "tooltip-text", _("blue sensitivity"), (char *)NULL);
+
+  g->y_bias_scale = dt_bauhaus_slider_new_with_range(module, 0.0, 2.0, 0.01, p->y_bias, 2);
+  dt_bauhaus_widget_set_label(g->y_bias_scale, NULL, _("yellow bias"));
+  dt_bauhaus_slider_set_stop(g->y_bias_scale, 0.0f, 0.0f, 0.0f, 0.0f);
+  dt_bauhaus_slider_set_stop(g->y_bias_scale, 1.0f, 1.0f, 1.0f, 0.0f);
+  g_object_set(G_OBJECT(g->y_bias_scale), "tooltip-text", _("yellow sensitivity"), (char *)NULL);
+
+  g_signal_connect(G_OBJECT(g->g_bias_scale), "value-changed",
+                   G_CALLBACK(bias_slider_callback), module);
+  g_signal_connect(G_OBJECT(g->m_bias_scale), "value-changed",
+                   G_CALLBACK(bias_slider_callback), module);
   g_signal_connect(G_OBJECT(g->b_bias_scale), "value-changed",
-                   G_CALLBACK(b_bias_slider_callback), module);
+                   G_CALLBACK(bias_slider_callback), module);
+  g_signal_connect(G_OBJECT(g->y_bias_scale), "value-changed",
+                   G_CALLBACK(bias_slider_callback), module);
 
-  gtk_box_pack_start(GTK_BOX(module->widget), GTK_WIDGET(g->a_bias_scale), TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(module->widget), GTK_WIDGET(dtgtk_label_new(_("edge chroma bias"),DARKTABLE_LABEL_TAB|DARKTABLE_LABEL_ALIGN_RIGHT)), FALSE, FALSE, 5);
+
+  gtk_box_pack_start(GTK_BOX(module->widget), GTK_WIDGET(g->g_bias_scale), TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(module->widget), GTK_WIDGET(g->m_bias_scale), TRUE, TRUE, 0);
   gtk_box_pack_start(GTK_BOX(module->widget), GTK_WIDGET(g->b_bias_scale), TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(module->widget), GTK_WIDGET(g->y_bias_scale), TRUE, TRUE, 0);
+
+  /* expert options / internals */
+#ifdef EXPERT_OPTIONS
+  g->x = dt_bauhaus_slider_new_with_range(module, 0.0, 1.0, 0.01, p->x, 2);
+  dt_bauhaus_widget_set_label(g->x, NULL, _("ab edge proportion vs lum/sat"));
+
+  g->y = dt_bauhaus_slider_new_with_range(module, 0.0, 5.0, 0.01, p->y, 2);
+  dt_bauhaus_widget_set_label(g->y, NULL, _("local avg radius bias"));
+
+  g->z = dt_bauhaus_slider_new_with_range(module, 0.0, 1.0, 0.01, p->z, 2);
+  dt_bauhaus_widget_set_label(g->z, NULL, _("-unused-"));
+
+  g_signal_connect(G_OBJECT(g->x), "value-changed", G_CALLBACK(xyz_cb), module);
+  g_signal_connect(G_OBJECT(g->y), "value-changed", G_CALLBACK(xyz_cb), module);
+  g_signal_connect(G_OBJECT(g->z), "value-changed", G_CALLBACK(xyz_cb), module);
+
+  gtk_box_pack_start(GTK_BOX(module->widget), GTK_WIDGET(dtgtk_label_new(_("expert options"),DARKTABLE_LABEL_TAB|DARKTABLE_LABEL_ALIGN_RIGHT)), FALSE, FALSE, 5);
+
+  gtk_box_pack_start(GTK_BOX(module->widget), GTK_WIDGET(g->x), TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(module->widget), GTK_WIDGET(g->y), TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(module->widget), GTK_WIDGET(g->z), TRUE, TRUE, 0);
+#endif  
 }
 
 void gui_update (dt_iop_module_t *module)
@@ -479,8 +634,15 @@ void gui_update (dt_iop_module_t *module)
   dt_bauhaus_combobox_set(g->mode_select, p->op_mode);
   dt_bauhaus_slider_set(g->radius_scale, p->radius);
   dt_bauhaus_slider_set(g->thresh_scale, p->thresh);
-  dt_bauhaus_slider_set(g->a_bias_scale, p->a_bias);
+  dt_bauhaus_slider_set(g->g_bias_scale, p->g_bias);
+  dt_bauhaus_slider_set(g->m_bias_scale, p->m_bias);
   dt_bauhaus_slider_set(g->b_bias_scale, p->b_bias);
+  dt_bauhaus_slider_set(g->y_bias_scale, p->y_bias);
+#ifdef EXPERT_OPTIONS
+  dt_bauhaus_slider_set(g->x, p->x);
+  dt_bauhaus_slider_set(g->y, p->y);
+  dt_bauhaus_slider_set(g->z, p->z);
+#endif
 }
 
 void gui_cleanup (dt_iop_module_t *module)
