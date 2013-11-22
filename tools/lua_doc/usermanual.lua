97ebf687c41b4eb33956763ea6ceaf20ba4b01b1
Boucman <jeremy.rosen@enst-bretagne.fr>
2013-11-22 19:10:18 +0100

    usermanual : various changes to the luaAPI generation

--- tools/lua_doc/usermanual.lua
+++ tools/lua_doc/usermanual.lua
@@ -36,22 +36,22 @@ local function get_reported_type(node,simple)
 	if rtype == "documentation node" then rtype = nil end
 	if rtype == "dt_singleton" then rtype = nil end
 	if( rtype and not simple and doc.get_attribute(node,"signature")) then
-		if(doc.get_attribute(node,"ret_val")) then
-			rtype = "<funcsynopsis><funcprototype><funcdef>"..get_reported_type(doc.get_attribute(node,"ret_val",true)).." <function>"..rtype.."</function></funcdef>\n"
-		else
-			rtype = "<funcsynopsis><funcprototype><funcdef><function>"..rtype.."</function></funcdef>\n"
-		end
+		rtype = rtype.."( "
 		local sig = doc.get_attribute(node,"signature")
 		for k,v in pairs(sig) do
 			if(doc.get_attribute(v,"optional")) then
-				rtype = rtype..'<paramdef choice="opt">'..get_reported_type(v,true).." <parameter><optional>"..doc.get_short_name(v).."</optional></parameter></paramdef>"
+				rtype = rtype.."[ <emphasis>"..doc.get_short_name(v).."</emphasis> ]"
 			else
-				rtype = rtype.."<paramdef>"..get_reported_type(v,true).." <parameter>"..doc.get_short_name(v).."</parameter></paramdef>"
+				rtype = rtype.."<emphasis>"..doc.get_short_name(v).."</emphasis>"
+			end
+			if next(sig,k) then
+				rtype = rtype..", "
 			end
 		end
-		rtype = rtype.."</funcprototype></funcsynopsis>"
-	elseif rtype ~=nil and not simple then
-		rtype = "<synopsis>"..rtype.."</synopsis>"
+		rtype = rtype.." )"
+	end
+	if(not simple and doc.get_attribute(node,"ret_val")) then
+		rtype = rtype.." : "..get_reported_type(doc.get_attribute(node,"ret_val",true))
 	end
 	return rtype
 end
@@ -66,7 +66,7 @@ local function print_content(node)
 	local rtype = get_reported_type(node)
 	local result = ""
 	if  rtype then
-		result = result ..rtype.."\n"
+		result = result .."<synopsis>"..rtype.."</synopsis>\n"
 	end
 	result = result ..parse_text(node).."\n"
 	local concat=""
@@ -123,6 +123,9 @@ parse_doc_node = function(node,parent,prev_name)
 	end
 
 	local depth = depth(node);
+	if depth > 5 then 
+		error("too many tree depth for usermanual")
+	end
 
 	if(node._luadoc_type == "param") then
 		local tmp_node = doc.get_main_parent(node)
@@ -137,12 +140,12 @@ parse_doc_node = function(node,parent,prev_name)
 			result = result ..'<varlistentry><term id="'..doc.get_name(node)..'">'..doc.get_short_name(node).."</term><listitem>\n"
 		end
 	elseif depth ~= 0 then
-		result = result..'<section status="draft" '
+		result = result..'<sect'..(depth+1)..' status="draft" '
 		if(doc.is_main_parent(node,parent,prev_name) ) then
 			result = result..'id="'..doc.get_name(node)..'"'
 		end
 		result = result..'>\n'
-		result = result..'<title>'..node:get_short_name()..'</title>\n'
+		result = result..'<title>'..doc.get_name(node)..'</title>\n'
 		result = result..'<indexterm>\n'
 		result = result..'<primary>Lua API</primary>\n'
 		result = result..'<secondary>'..node:get_short_name()..'</secondary>\n'
@@ -157,7 +160,7 @@ parse_doc_node = function(node,parent,prev_name)
 	if(node._luadoc_type == "param") then
 		result = result..'</listitem></varlistentry>\n'
 	elseif depth ~= 0 then
-		result = result..'</section>\n'
+		result = result..'</sect'..(depth+1)..'>\n'
 	end
 	return result;
 end
