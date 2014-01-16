4dd53db20ce436c202a3de57bf5b040370f96588
Jérémy Rosen <jeremy.rosen@enst-bretagne.fr>
2014-01-16 12:38:32 +0100

    lua : api, enable external formating functions
    this should solve most of the problems of wiki vs usermanual syntax

--- tools/lua_doc/content.lua
+++ tools/lua_doc/content.lua
@@ -7,7 +7,7 @@ attributes = doc.toplevel.attributes
 local tmp_node
 
 
-for _,v in pairs({"node_to_string"})   do -- add para, add a way to make list, add a way to have code sections
+for _,v in pairs({"node_to_string","para","startlist","listel","endlist","code"})   do -- add p a way to have code sections
 	if _ENV[v]== nil then
 		error("function '"..v.."' not defined when requiring content")
 	end
@@ -34,14 +34,10 @@ types.dt_imageio_module_storage_data_email:set_text([[TBSL undocumented, force f
 ----------------------
 --  TOPLEVEL        --
 ----------------------
-doc.toplevel:set_text([[
-To access the darktable specific functions you must load the darktable environement:
-
-darktable = require "darktable"
-
-All functions and data are accessed through the darktable module.
-
-This documentation for API version ]]..real_darktable.configuration.api_version_string..[[.]])
+doc.toplevel:set_text([[To access the darktable specific functions you must load the darktable environement:]]..
+code([[darktable = require "darktable"]])..
+[[All functions and data are accessed through the darktable module.]]..para()..
+[[This documentation for API version ]]..real_darktable.configuration.api_version_string..[[.]])
 ----------------------
 --  DARKTABLE       --
 ----------------------
@@ -52,15 +48,14 @@ darktable.print:add_parameter("message","string",[[The string to display which s
 darktable.print_error:set_text([[This function will print its parameter if the Lua logdomain is activated. Start darktable with the "-d lua" command line option to enable the Lua logdomain.]])
 darktable.print_error:add_parameter("message","string",[[The string to display.]])
 
-darktable.register_event:set_text([[This function registers a callback to be called when a given event happens.
-
-Events are documented in the event section.]])
+darktable.register_event:set_text([[This function registers a callback to be called when a given event happens.]]..para()..
+[[Events are documented ]]..node_to_string(events,[[in the event section.]]))
 darktable.register_event:add_parameter("event_type","string",[[The name of the event to register to.]])
 darktable.register_event:add_parameter("callback","function",[[The function to call on event. The signature of the function depends on the type of event.]])
-darktable.register_event:add_parameter("...","variable",[[Some events need extra parameters at registraion time; these must be specified here.]])
+darktable.register_event:add_parameter("...","variable",[[Some events need extra parameters at registration time; these must be specified here.]])
 
-darktable.register_storage:set_text([[This function will add a new storage implemented in Lua. 
-A storage is a module that is responsible for handling images once they have been generated during export. Examples of core storages include filesystem, e-mail, facebook...]])
+darktable.register_storage:set_text([[This function will add a new storage implemented in Lua.]]..para()..
+[[A storage is a module that is responsible for handling images once they have been generated during export. Examples of core storages include filesystem, e-mail, facebook...]])
 darktable.register_storage:add_parameter("plugin_name","string",[[A Unique name for the plugin.]])
 darktable.register_storage:add_parameter("name","string",[[A human readable name for the plugin.]])
 tmp_node = darktable.register_storage:add_parameter("store","function",[[This function is called once for each exported image. Images can be exported in parallel but the calls to this function will be serialized.]])
@@ -72,33 +67,29 @@ tmp_node:add_parameter("filename","string",[[The name of a temporary file where 
 tmp_node:add_parameter("number","integer",[[The number of the image out of the export series.]])
 tmp_node:add_parameter("total","integer",[[The total number of images in the export series.]])
 tmp_node:add_parameter("high_quality","boolean",[[True if the export is high quality.]])
-tmp_node:add_parameter("extra_data","table",[[An empty Lua table to take extra data. This table is common to all calls to store and the call to finalize in a given export series.]])
+tmp_node:add_parameter("extra_data","table",[[An empty Lua table to take extra data. This table is common to the initialize, store and finalize calls in an export serie.]])
 tmp_node = darktable.register_storage:add_parameter("finalize","function",[[This function is called once all images are processed and all store calls are finished.]])
 tmp_node:set_attribute("optional",true)
 tmp_node:add_parameter("storage",tostring(types.dt_imageio_module_storage_t),[[The storage object used for the export.]])
 tmp_node:add_parameter("image_table","table",[[A table keyed by the exported image objects and valued with the corresponding temporary export filename.]])
 tmp_node:add_parameter("extra_data","table",[[An empty Lua table to store extra data. This table is common to all calls to store and the call to finalize in a given export series.]])
-tmp_node = darktable.register_storage:add_parameter("supported","function",[[A function called to check if a given image format is supported by the Lua storage; this is used to build the dropdown format list for the GUI.
-
-Nate that the parameters in the format are the ones currently set in the GUI; the user might change them before export.]])
+tmp_node = darktable.register_storage:add_parameter("supported","function",[[A function called to check if a given image format is supported by the Lua storage; this is used to build the dropdown format list for the GUI.]]..para()..
+[[Note that the parameters in the format are the ones currently set in the GUI; the user might change them before export.]])
 tmp_node:set_attribute("optional",true)
 tmp_node:add_parameter("storage",tostring(types.dt_imageio_module_storage_t),[[The storage object tested.]])
 tmp_node:add_parameter("format",tostring(types.dt_imageio_module_format),[[The format object to report about.]])
 tmp_node:add_return("boolean",[[True if the corresponding format is supported.]])
-tmp_node = darktable.register_storage:add_parameter("initialize","function",[[A function called before storage happens
-
-This function can change the list of exported functions]])
+tmp_node = darktable.register_storage:add_parameter("initialize","function",[[A function called before storage happens]]..para().. 
+[[This function can change the list of exported functions]])
 tmp_node:set_attribute("optional",true)
 tmp_node:add_parameter("storage",tostring(types.dt_imageio_module_storage_t),[[The storage object tested.]])
 tmp_node:add_parameter("format",tostring(types.dt_imageio_module_format),[[The format object to report about.]])
-tmp_node:add_parameter("images","table of dt_lua_image_t",[[A table containing images to be exported.]])
+tmp_node:add_parameter("images","table of "..tostring(dt_lua_image_t),[[A table containing images to be exported.]])
 tmp_node:add_parameter("high_quality","boolean",[[True if the export is high quality.]])
-tmp_node:add_parameter("extra_data","table",[[An empty Lua table to take extra data. This table is common to all calls to store and the call to finalize in a given export series.]])
-tmp_node:add_return("table or nil",[[The modified table of images to export or nil
-
-If nil (or nothing) is returned, the original list of images will be exported
-
-If a table of images is returned, that table will be used instead. The table can be empty. The images parameter can be modified and returned]])
+tmp_node:add_parameter("extra_data","table",[[An empty Lua table to take extra data. This table is common to the initialize, store and finalize calls in an export serie.]])
+tmp_node:add_return("table or nil",[[The modified table of images to export or nil]]..para()..
+[[If nil (or nothing) is returned, the original list of images will be exported]]..para()..
+[[If a table of images is returned, that table will be used instead. The table can be empty. The images parameter can be modified and returned]])
 tmp_node:add_version_info([[This parameter was added]])
 
 
@@ -107,14 +98,13 @@ darktable.films['#']:set_text([[Each film has a numeric entry in the database.]]
 ----------------------
 --  DARKTABLE.GUI   --
 ----------------------
-darktable.gui:set_text([[This subtable contains function and data to manipulate the darktable user interface with Lua.
+darktable.gui:set_text([[This subtable contains function and data to manipulate the darktable user interface with Lua.]]..para()..
+[[Most of these function won't do anything if the GUI is not enabled (i.e you are using the command line version darktabl-cli instead of darktable).]])
 
-Most of these function won't do anything if the GUI is not enabled (i.e you are using the command line version darktabl-cli instead of darktable).]])
+darktable.gui.action_images:set_text([[A table of ]]..tostring(types.dt_lua_image_t)..[[ on which the user expects UI actions to happen.]]..para()..
+[[It is based on both the hovered image and the selection and is consistent with the way darktable works.]]..para()..
+[[It is recommended to use this table to implement Lua actions rather than dt.gui.hovered or dt.gui.selected to be consistant with darktable's GUI.]])
 
-darktable.gui.action_images:set_text([[A table of images on which the user expects us to act.
-It is based on both the hovered image and the selection and is consistent with the way darktable works.
-
-It is recommended to use this table to implement Lua actions rather than dt.gui.hovered or dt.gui.selected to be consistant with darktable's GUI.]])
 for k, v in darktable.gui.action_images:all_children() do
 	v:remove_parent(darktable.gui.action_images)
 	darktable.gui.action_images[k] = nil
@@ -122,8 +112,8 @@ end
 
 darktable.gui.hovered:set_text([[The image under the cursor or nil if no image is hovered.]])
 darktable.gui.selection:set_text([[Allows to change the set of selected images.]])
-darktable.gui.selection:add_parameter("selection","table",[[A table of images which will define the selected images. If this parameter is not given the selection will be untouched. If an empty table is given the selection will be emptied.]]):set_attribute("optional",true)
-darktable.gui.selection:add_return("table",[[A table containing the selection as it was before the function was called.]])
+darktable.gui.selection:add_parameter("selection","table of "..tostring(types.dt_lua_image_t),[[A table of images which will define the selected images. If this parameter is not given the selection will be untouched. If an empty table is given the selection will be emptied.]]):set_attribute("optional",true)
+darktable.gui.selection:add_return("table of "..tostring(types.dt_lua_image_t),[[A table containing the selection as it was before the function was called.]])
 darktable.gui.selection:set_attribute("implicit_yield",true)
 
 ----------------------
@@ -150,7 +140,7 @@ darktable.tags.detach:add_parameter("image",tostring(types.dt_lua_image_t),[[The
 darktable.tags.detach:set_main_parent(darktable.tags)
 darktable.tags.get_tags:set_text([[Gets all tags attached to an image.]])
 darktable.tags.get_tags:add_parameter("image",tostring(types.dt_lua_image_t),[[The image to get the tags from.]])
-darktable.tags.get_tags:add_return("table",[[A table of tags that are attached to the image.]])
+darktable.tags.get_tags:add_return("table of "..tostring(types.dt_lua_tag_t),[[A table of tags that are attached to the image.]])
 darktable.tags.get_tags:set_main_parent(darktable.tags)
 
 ------------------------------
@@ -177,11 +167,9 @@ darktable.configuration.api_version_string:add_version_info([[field added]])
 -----------------------------
 --  DARKTABLE.PREFERENCES  --
 -----------------------------
-darktable.preferences:set_text([[Lua allows you do manipulate preferences. Lua has its own namespace for preferences and you can't access nor write normal darktable preferences.
-
-Preference handling functions take a _script_ parameter. This is a string used to avoid name collision in preferences (i.e namespace). Set it to something unique, usually the name of the script handling the preference.
-
-Preference handling functions can't guess the type of a parameter. You must pass the type of the preference you are handling. Allowed values are the following strings
+darktable.preferences:set_text([[Lua allows you do manipulate preferences. Lua has its own namespace for preferences and you can't access nor write normal darktable preferences.]]..para()..
+[[Preference handling functions take a _script_ parameter. This is a string used to avoid name collision in preferences (i.e namespace). Set it to something unique, usually the name of the script handling the preference.]]..para()..
+[[Preference handling functions can't guess the type of a parameter. You must pass the type of the preference you are handling. Allowed values are the following strings
 
 * string
 * bool
@@ -214,7 +202,7 @@ darktable.preferences.write:add_parameter("value","depends on type",[[The value 
 --  DARKTABLE.STYLES --
 -----------------------
 
-darktable.styles:set_text([[This pseude table allows you to access and manipulate styles.]])
+darktable.styles:set_text([[This pseudo table allows you to access and manipulate styles.]])
 
 darktable.styles["#"]:set_text([[Each existing style has a numeric index; you can iterate them using ipairs.]])
 
@@ -370,9 +358,9 @@ types.dt_lua_image_t.group_with:add_parameter("image",tostring(types.dt_lua_imag
 types.dt_lua_image_t.group_with:add_parameter("image2",tostring(types.dt_lua_image_t),[[The image we want to group with.]]):set_attribute("optional",true)
 types.dt_lua_image_t.make_group_leader:set_text([[Makes the image the leader of its group.]])
 types.dt_lua_image_t.make_group_leader:add_parameter("image",tostring(types.dt_lua_image_t),[[The image we want as the leader.]])
-types.dt_lua_image_t.get_group_members:set_text([[Returns a table containing all images of the group. The group leader is both at a numeric key and at the "leader" special key (so you probably want to use ipairs to iterate through that table).]])
+types.dt_lua_image_t.get_group_members:set_text([[Returns a table containing all ]]..tostring(types.dt_lua_image_t)..[[ of the group. The group leader is both at a numeric key and at the "leader" special key (so you probably want to use ipairs to iterate through that table).]])
 types.dt_lua_image_t.get_group_members:add_parameter("image",tostring(types.dt_lua_image_t),[[The image whose group we are querying.]])
-types.dt_lua_image_t.get_group_members:add_return("table",[[A table of image objects containing all images that are in the same group as the image.]])
+types.dt_lua_image_t.get_group_members:add_return("table of "..tostring(types.dt_lua_image_t),[[A table of image objects containing all images that are in the same group as the image.]])
 darktable.tags.attach:set_alias(types.dt_lua_image_t.attach_tag)
 types.dt_lua_image_t.group_leader:set_text([[The image which is the leader of the group this image is a member of.]])
 
@@ -523,14 +511,16 @@ attributes.implicit_yield:set_text([[This call will release the Lua lock while e
 doc.toplevel.system = doc.create_documentation_node(nil,doc.toplevel,"system")
 local system = doc.toplevel.system
 system:set_text([[This section documents changes to system functions.]])
-system.yield = doc.document_function(nil,system,"yield");
-system.yield:set_real_name("yield")
-system.yield:set_text([[Lua functions can yield at any point. The parameters and return types depend on why we want to yield.
-A callback that is yielding allows other Lua code to run.
 
-* wait_ms: one extra parameter; the execution will pause for that many miliseconds; yield returns nothing;
-* file_readable: an opened file from a call to the OS library; will return when the file is readable; returns nothing;
-* run_command: a command to be run by "sh -c"; will return when the command terminates; returns the return code of the execution.]])
-system.yield:add_parameter("type","string",[[The type of yield; can be one of "wait_ms", "file_readable", "run_command".]])
-system.yield:add_parameter("extra","variable",[[An extra parameter: integer for "wait_ms", open file for "file_readable", string for "run_command".]])
-system.yield:add_return("variable",[[Nothing for "wait_ms" and "file_readable"; the returned code of the command for "run_command".]])
+doc.toplevel.system.coroutine = doc.create_documentation_node(nil,doc.toplevel.system,"coroutine")
+system.coroutine.yield = doc.document_function(nil,system.coroutine,"yield");
+system.coroutine.yield:set_real_name("coroutine.yield")
+system.coroutine.yield:set_text([[Lua functions can yield at any point. The parameters and return types depend on why we want to yield.]]..para()..
+[[A callback that is yielding allows other Lua code to run.]]..startlist()..
+listel("wait_ms: one extra parameter; the execution will pause for that many miliseconds; yield returns nothing;")..
+listel("file_readable: an opened file from a call to the OS library; will return when the file is readable; returns nothing;")..
+listel([[* run_command: a command to be run by "sh -c"; will return when the command terminates; returns the return code of the execution.]])..
+endlist())
+system.coroutine.yield:add_parameter("type","string",[[The type of yield; can be one of "wait_ms", "file_readable", "run_command".]])
+system.coroutine.yield:add_parameter("extra","variable",[[An extra parameter: integer for "wait_ms", open file for "file_readable", string for "run_command".]])
+system.coroutine.yield:add_return("variable",[[Nothing for "wait_ms" and "file_readable"; the returned code of the command for "run_command".]])
--- tools/lua_doc/wiki.lua
+++ tools/lua_doc/wiki.lua
@@ -4,9 +4,22 @@ local function get_node_with_link(node,name)
 	return "\""..name.."\":"..page_name.."#"..node:get_name()
 end
 
-para = function() return "\n" end
-node_to_string = function(node) return get_node_with_link(node,node:get_name()) end
+para = function() return "\n\n" end
+node_to_string = function(node,name) 
+	if name then
+		return get_node_with_link(node,name)
+	else
+		return get_node_with_link(node,node:get_name())
+	end
+end
 
+code = function(text) 
+	return [[\n\n<pre><code class="lua">]]..text.."</pre></code>\n\n"
+end
+
+startlist = function() return "\n\n" end
+endlist = function() return "" end
+listel = function(text) return "\n* "..text end
 
 
 require "content"
@@ -171,7 +184,7 @@ function M.get_doc()
 
 
 This documentation was generated with darktable version ]]..real_darktable.configuration.version..[[.]])
-	return "{{toc}}\n\n"..parse_doc_node(doc.toplevel,nil,"")
+	return "{{>toc}}\n\n"..parse_doc_node(doc.toplevel,nil,"")
 end
 
 
