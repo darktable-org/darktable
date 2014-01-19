7dbd7fef4dfdf31a5f46d2b3b1cbe7ea51e0e88f
Henrik Andersson <hean01@users.sourceforge.net>
2014-01-19 23:08:28 +0100

    Cleanup and add check for gphoto2 to prevent breakage at buildtime.

--- src/libs/session.c
+++ src/libs/session.c
@@ -16,14 +16,10 @@
     along with darktable.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "common/darktable.h"
-#include "common/camera_control.h"
-#include "control/jobs.h"
-#include "control/control.h"
 #include "control/conf.h"
 #include "libs/lib.h"
 #include "gui/gtk.h"
 #include "gui/gtkentry.h"
-#include "dtgtk/label.h"
 #include <gdk/gdkkeysyms.h>
 
 DT_MODULE(1)
@@ -81,7 +77,9 @@ create_callback(GtkButton *button, gpointer user_data)
   dt_lib_session_t *lib=self->data;
 
   dt_conf_set_string("plugins/session/jobcode", gtk_entry_get_text(lib->gui.entry1) );
+#ifdef HAVE_GPHOTO2
   dt_view_tethering_set_job_code(darktable.view_manager, gtk_entry_get_text( lib->gui.entry1 ) );
+#endif
 }
 
 void
@@ -99,7 +97,6 @@ gui_init (dt_lib_module_t *self)
   GtkBox *hbox, *vbox1, *vbox2;
 
   // Session settings
-  //gtk_box_pack_start(GTK_BOX(self->widget), dtgtk_label_new("session settings",DARKTABLE_LABEL_TAB|DARKTABLE_LABEL_ALIGN_RIGHT), TRUE, TRUE, 0);
   hbox = GTK_BOX(gtk_hbox_new(FALSE, 5));
   vbox1 = GTK_BOX(gtk_vbox_new(TRUE, 5));
   vbox2 = GTK_BOX(gtk_vbox_new(TRUE, 5));
