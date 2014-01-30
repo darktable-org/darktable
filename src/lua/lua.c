f768c823c5d030c0cc7a681c454b81d4e3c2072a
Jérémy Rosen <jeremy.rosen@enst-bretagne.fr>
2014-01-30 14:57:49 +0100

    lua : check that the table to dump is really a table (in debug code

--- src/lua/lua.c
+++ src/lua/lua.c
@@ -32,6 +32,10 @@ void dt_lua_debug_table_internal(lua_State * L,int t,const char* function,int li
 {
   /* table is in the stack at index 't' */
   printf("lua table at index %d at %s:%d\n",t,function,line);
+  if(lua_type(L,t) != LUA_TTABLE) {
+    printf("\tnot a table: %s\n",lua_typename(L,lua_type(L,t)));
+    return;
+  }
   lua_pushnil(L);  /* first key */
   while (lua_next(L, t-1) != 0)
   {
