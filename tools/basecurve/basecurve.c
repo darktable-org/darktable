119b56cd6b3faff73dd856010a9c29d95ea56538
Edouard Gomez <ed.gomez@free.fr>
2014-02-04 00:46:32 +0100

    basecurve tool: add tentative tonecurve support, still incomplete

--- tools/basecurve/basecurve.c
+++ tools/basecurve/basecurve.c
@@ -85,7 +85,7 @@ read_ppm8(const char *filename, int *wd, int *ht)
   return p;
 }
 
-static inline float get_error(CurveData *c, CurveSample *csample, float (*basecurve)[3], int (*cnt)[3])
+static inline float get_error(CurveData *c, CurveSample *csample, float* basecurve, int* cnt)
 {
   CurveDataSample(c, csample);
   float sqrerr = 0.0f;
@@ -93,9 +93,9 @@ static inline float get_error(CurveData *c, CurveSample *csample, float (*basecu
   for(int k=0; k<0x10000; k++)
   {
     // too few samples? no error if we ignore it.
-    if(cnt[k][1] > 8)
+    if(cnt[k] > 8)
     {
-      float d = (basecurve[k][1] - (min + (max-min)*csample->m_Samples[k]*(1.0f/0x10000)));
+      float d = (basecurve[k] - (min + (max-min)*csample->m_Samples[k]*(1.0f/0x10000)));
       // way more error for lower values of x:
       d *= 0x10000-k;
       if(k < 655) d *= 100;
@@ -105,7 +105,7 @@ static inline float get_error(CurveData *c, CurveSample *csample, float (*basecu
   return sqrerr;
 }
 
-static inline void mutate(CurveData *c, CurveData *t, float (*basecurve)[3])
+static inline void mutate(CurveData *c, CurveData *t, float* basecurve)
 {
   for(int k=1;k<c->m_numAnchors-1;k++)
   {
@@ -116,12 +116,12 @@ static inline void mutate(CurveData *c, CurveData *t, float (*basecurve)[3])
     if(pos >= 0x10000) pos = 0xffff;
     if(pos < 0) pos = 0;
     t->m_anchors[k].x = x;
-    t->m_anchors[k].y = basecurve[pos][1];
+    t->m_anchors[k].y = basecurve[pos];
   }
   t->m_anchors[0].x = 0.0f;
-  t->m_anchors[0].y = 0.0f;//basecurve[0][1];
+  t->m_anchors[0].y = 0.0f;//basecurve[0];
   t->m_anchors[t->m_numAnchors-1].x = 1.0f;
-  t->m_anchors[t->m_numAnchors-1].y = 1.0f;//basecurve[0xffff][1];
+  t->m_anchors[t->m_numAnchors-1].y = 1.0f;//basecurve[0xffff];
 }
 
 static inline float linearize_sRGB(float val)
@@ -135,6 +135,167 @@ static inline float linearize_sRGB(float val)
     val = powf(((val + 0.055f)/1.055f), 2.4f);
   }
   return val;
+}
+
+#define SQUARE(a) ((a)*(a))
+#define CUBIC(a) ((a)*SQUARE((a)))
+
+static inline float Lab(float val)
+{
+  static const float threshold = CUBIC(6.f) / CUBIC(29.f);
+  if (val>threshold)
+  {
+    val = powf(val, 1.f / 3.f);
+  }
+  else
+  {
+    val = 4.f / 29.f + SQUARE(29.f) / (3.f * SQUARE(6.f)) * val;
+  }
+  return val;
+}
+
+static inline float RGB_to_L(float r, float g, float b)
+{
+  float y = 0.2126f*r + 0.7152f*g + 0.0722f*b;
+  return (116.f*Lab(y) - 16.f) / 100.f;
+}
+
+enum module_type
+{
+  MODULE_BASECURVE = 0,
+  MODULE_TONECURVE = 1,
+  MODULE_MAX
+};
+
+void
+build_channel_basecurve(
+  int width_jpeg, int height_jpeg, uint8_t* buf_jpeg,
+  int offx_raw, int offy_raw, int width_raw, uint16_t* buf_raw,
+  int ch, float* curve, int* cnt)
+{
+  for(int j=0;j<height_jpeg;j++)
+  {
+    for(int i=0;i<width_jpeg;i++)
+    {
+      // raw coordinate is offset:
+      const int ri = offx_raw + i;
+      const int rj = offy_raw + j;
+
+      // grab channel from JPEG first
+      float val = buf_jpeg[3*(width_jpeg*j + i) + ch]/255.f;
+
+      // linearize the sRGB value
+      // XXX: this supposes it is sRGB, support arbitrary colorspace with ICC lib ?
+      float jpegVal = linearize_sRGB(val);
+
+      // grab RGB from RAW
+      float rawVal = (float)buf_raw[3*(width_raw*rj + ri) + ch]/65535.f;
+
+      uint16_t raw = (uint16_t)((rawVal*65535.f) + 0.5f);
+      curve[raw] = (curve[raw]*cnt[raw] + jpegVal)/(cnt[raw] + 1.0f);
+      cnt[raw]++;
+    }
+  }
+}
+
+void
+build_tonecurve(
+  int width_jpeg, int height_jpeg, uint8_t* buf_jpeg,
+  int offx_raw, int offy_raw, int width_raw, uint16_t* buf_raw,
+  int ch, float* curve, int* cnt)
+{
+  for(int j=0;j<height_jpeg;j++)
+  {
+    for(int i=0;i<width_jpeg;i++)
+    {
+      // raw coordinate is offset:
+      const int ri = offx_raw + i;
+      const int rj = offy_raw + j;
+
+      // grab RGB from JPEG first
+      float r = buf_jpeg[3*(width_jpeg*j + i) + 0]/255.f;
+      float g = buf_jpeg[3*(width_jpeg*j + i) + 1]/255.f;
+      float b = buf_jpeg[3*(width_jpeg*j + i) + 2]/255.f;
+
+      // linearize the sRGB value (this supposes it is sRGB, TODO support arbitrary colorspace with ICC lib ?)
+      r = linearize_sRGB(r);
+      g = linearize_sRGB(g);
+      b = linearize_sRGB(b);
+
+      // Compute the JPEG L val
+      float jpegL = RGB_to_L(r, g, b);
+
+      // grab RGB from RAW
+      r = (float)buf_raw[3*(width_raw*rj + ri) + 0]/65535.f;
+      g = (float)buf_raw[3*(width_raw*rj + ri) + 1]/65535.f;
+      b = (float)buf_raw[3*(width_raw*rj + ri) + 2]/65535.f;
+
+      // Compute the RAW L val
+      float rawL = RGB_to_L(r, g, b);
+
+      uint16_t raw = (uint16_t)((rawL*65535.f) + 0.5f);
+      curve[raw] = (curve[raw]*cnt[raw] + jpegL)/(cnt[raw] + 1.0f);
+      cnt[raw]++;
+    }
+  }
+}
+
+void fit_curve(CurveData* best, int* nopt, float* minsqerr, CurveSample* csample, int num_nodes, float* curve, int* cnt)
+{
+  // now do the fitting:
+  CurveData curr, tent;
+
+  // type = 2 (monotone hermite)
+  curr.m_spline_type = 2;
+  curr.m_numAnchors = num_nodes;
+  curr.m_min_x = 0.0;
+  curr.m_max_x = 1.0;
+  curr.m_min_y = 0.0;
+  curr.m_max_y = 1.0;
+
+  *best = tent = curr;
+  *nopt = 0;
+  *minsqerr = FLT_MAX;
+
+  const float p_large = .0f;
+  float curr_m = FLT_MIN;
+
+  const int samples = 1000;
+  for(int i=0;i<samples;i++)
+  {
+    if(i == 0 || drand48() < p_large)
+    { // large step
+      for(int k=0;k<tent.m_numAnchors;k++)
+      {
+        float x = k/(tent.m_numAnchors-1.0f);
+        x *= x*x; // move closer to 0
+        uint32_t pos = x*0x10000;
+        if(pos >= 0x10000) pos = 0xffff;
+        if(pos < 0) pos = 0;
+        tent.m_anchors[k].x = x;
+        tent.m_anchors[k].y = curve[pos];
+      }
+    }
+    else
+    { // mutate
+      mutate(&curr, &tent, curve);
+    }
+    float m = get_error(&tent, csample, curve, cnt);
+    if(m < *minsqerr)
+    {
+      (*nopt)++;
+      *minsqerr = m;
+      *best = tent;
+
+    }
+    // fittness: 1/MSE
+    const float a = curr_m/m;
+    if(drand48() < a || i == 0)
+    { // accept new state
+      curr = tent;
+      curr_m = m;
+    }
+  }
 }
 
 int main(int argc, char *argv[])
@@ -155,9 +316,15 @@ int main(int argc, char *argv[])
     exit(1);
   }
   int num_nodes = 8;
+  enum module_type module = MODULE_BASECURVE;
+
   if(argc > 3)
     num_nodes = atol(argv[3]);
+  if (argc > 4)
+    module = atol(argv[4]);
+
   if(num_nodes > 20) num_nodes = 20; // basecurve doesn't support more than that.
+  module = (module < 0) ? 0 : (module > MODULE_MAX) ? MODULE_MAX : module;
   int wd, ht, jpgwd, jpght;
   uint16_t *img = read_ppm16(argv[1], &wd, &ht);
   if(!img) exit(1);
@@ -166,13 +333,18 @@ int main(int argc, char *argv[])
   uint8_t *jpg = read_ppm8(argv[2], &jpgwd, &jpght);
   if(!jpg) exit(1);
 
-  // basecurve is three-channel rgb and has 16-bit entries (raw) and 8-bit output (jpg).
-  // the output is averaged a couple of times, so we want to keep it as floats.
-  float basecurve[0x10000][3];
-  // keep a counter of how many samples are in each bin.
-  int cnt[0x10000][3];
-  memset(basecurve, 0, sizeof(basecurve));
-  memset(cnt, 0, sizeof(cnt));
+  int ncurves = module == MODULE_BASECURVE ? 3 : 1;
+  float* curve = calloc(1, 0x10000*sizeof(float)*ncurves);
+  if (!curve) {
+    fprintf(stderr, "error: failed allocating tonal curve\n");
+    exit(1);
+  }
+
+  int* cnt = calloc(1, 0x10000*sizeof(int)*ncurves);
+  if (!cnt) {
+    fprintf(stderr, "error: failed allocating histogram\n");
+    exit(1);
+  }
 
   int offx = (wd - jpgwd)/2;
   int offy = (ht - jpght)/2;
@@ -184,129 +356,113 @@ int main(int argc, char *argv[])
     exit(1);
   }
 
-  for(int j=0;j<jpght;j++)
-  {
-    for(int i=0;i<jpgwd;i++)
-    {
-      // raw coordinate is offset:
-      const int ri = offx + i, rj = offy + j;
-      for(int k=0;k<3;k++)
-      {
-        // un-gamma the jpg file:
-        float rgb = jpg[3*(jpgwd*j + i) + k]/255.0f;
-        rgb = linearize_sRGB(rgb);
-        // grab the raw color at this position:
-        const uint16_t raw = img[3*(wd*rj + ri) + k];
-        // accum the histogram:
-        basecurve[raw][k] = (basecurve[raw][k]*cnt[raw][k] + rgb)/(cnt[raw][k] + 1.0f);
-        cnt[raw][k]++;
-      }
-    }
-  }
+  float* curve_to_approximate = NULL;
+  int* cnt_for_approximation = NULL;
 
-  // output the histograms:
-  fprintf(fb, "# basecurve-red basecurve-green basecurve-blue basecurve-avg cnt-red cnt-green cnt-blue\n");
-  for(int k=0;k<0x10000;k++)
+  if (module == MODULE_BASECURVE)
   {
-    fprintf(fb, "%f %f %f %f %d %d %d\n", basecurve[k][0], basecurve[k][1], basecurve[k][2], (basecurve[k][0] + basecurve[k][1] + basecurve[k][2])/3.0f, cnt[k][0], cnt[k][1], cnt[k][2]);
+    for (int ch=0; ch<3; ch++)
+    {
+      build_channel_basecurve(jpgwd, jpght, jpg, offx, offy, wd, img, ch, curve+ch*0x10000, cnt+ch*0x10000);
+    }
+
+    // output the histograms:
+    fprintf(fb, "# basecurve-red basecurve-green basecurve-blue basecurve-avg cnt-red cnt-green cnt-blue\n");
+    for(int k=0;k<0x10000;k++)
+    {
+      float ch0 = curve[k + 0*0x10000];
+      float ch1 = curve[k + 1*0x10000];
+      float ch2 = curve[k + 2*0x10000];
+      int c0 = cnt[k + 0*0x10000];
+      int c1 = cnt[k + 1*0x10000];
+      int c2 = cnt[k + 2*0x10000];
+      fprintf(fb, "%f %f %f %f %d %d %d\n", ch0, ch1, ch2, (ch0 + ch1 + ch2)/3.0f, c0, c1, c2);
+    }
+
+    // for now it seems more stable to work on G channel alone
+    curve_to_approximate = curve + 0x10000;
+    cnt_for_approximation = cnt + 0x10000;
+  }
+  else if (module == MODULE_TONECURVE)
+  {
+    build_tonecurve(jpgwd, jpght, jpg, offx, offy, wd, img, 1, curve, cnt);
+
+    // output the histogram
+    fprintf(fb, "# tonecurve-L cnt-L\n");
+    for(int k=0;k<0x10000;k++)
+    {
+      fprintf(fb, "%f %d\n", curve[k], cnt[k]);
+    }
+
+    // for now it seems more stable to work on G channel alone
+    curve_to_approximate = curve;
+    cnt_for_approximation = cnt;
   }
 
   free(img);
   free(jpg);
 
-  // now do the fitting:
-  CurveData curr, tent, best;
+  CurveData fit;
+  int accepts;
+  float sqerr;
   CurveSample csample;
   const int res = 0x10000;
   csample.m_samplingRes = res;
   csample.m_outputRes = 0x10000;
-  csample.m_Samples = (uint16_t *)malloc(sizeof(uint16_t)*0x10000);
+  csample.m_Samples = (uint16_t *)calloc(1, sizeof(uint16_t)*0x10000);
+  fit_curve(&fit, &accepts, &sqerr, &csample, num_nodes, curve_to_approximate, cnt_for_approximation);
 
-  // type = 2 (monotone hermite)
-  curr.m_spline_type = 2;
-  curr.m_numAnchors = num_nodes;
-  curr.m_min_x = 0.0;
-  curr.m_max_x = 1.0;
-  curr.m_min_y = 0.0;
-  curr.m_max_y = 1.0;
-
-  best = tent = curr;
-
-  float min = FLT_MAX;
-  const float p_large = .0f;
-  float curr_m = FLT_MIN;
-  int accepts = 0;
-
-  const int samples = 1000;
-  for(int i=0;i<samples;i++)
+  if (module == MODULE_BASECURVE)
   {
-    if(i == 0 || drand48() < p_large)
-    { // large step
-      for(int k=0;k<tent.m_numAnchors;k++)
-      {
-        float x = k/(tent.m_numAnchors-1.0f);
-        x *= x*x; // move closer to 0
-        uint32_t pos = x*0x10000;
-        if(pos >= 0x10000) pos = 0xffff;
-        if(pos < 0) pos = 0;
-        tent.m_anchors[k].x = x;
-        tent.m_anchors[k].y = basecurve[pos][1];
-      }
-    }
-    else
-    { // mutate
-      mutate(&curr, &tent, basecurve);
-    }
-    float m = get_error(&tent, &csample, basecurve, cnt);
-    if(m < min)
+    fprintf(ff, "# err %f improved %d times\n", sqerr, accepts);
+    fprintf(ff, "# copy paste into iop/basecurve.c (be sure to insert name, maker, model, and set the last 0 to 1 if happy to filter it):\n");
+    fprintf(ff, "# { \"new measured basecurve\", \"insert maker\", \"insert model\", 0, 51200,                        {{{");
+    for(int k=0;k<fit.m_numAnchors;k++)
+      fprintf(ff, "{%f, %f}%s", fit.m_anchors[k].x, fit.m_anchors[k].y, k<fit.m_numAnchors-1?", ":"}}, ");
+    fprintf(ff, "{%d}, {m}}, 0, 0},\n", fit.m_numAnchors);
+    CurveDataSample(&fit, &csample);
+    for(int k=0; k<0x10000; k++)
+      fprintf(ff, "%f %f\n", k*(1.0f/0x10000), 0.0 + (1.0f-0.0f)*csample.m_Samples[k]*(1.0f/0x10000));
+
+    char encoded[2048];
+
+    dt_iop_basecurve_params_t params;
+    memset(&params, 0, sizeof(params));
+    for(int k=0;k<fit.m_numAnchors;k++)
     {
-      accepts ++;
-      best = tent;
-      min = m;
+      params.basecurve[0][k].x = fit.m_anchors[k].x;
+      params.basecurve[0][k].y = fit.m_anchors[k].y;
     }
-    // fittness: 1/MSE
-    const float a = curr_m/m;
-    if(drand48() < a || i == 0)
-    { // accept new state
-      curr = tent;
-      curr_m = m;
-    }
+    params.basecurve_nodes[0] = fit.m_numAnchors;
+    params.basecurve_type[0] = MONOTONE_HERMITE;
+
+    text_encode ((uint8_t *)&params, encoded, sizeof(params));
+
+    fprintf(stdout, "to test your new basecurve, copy/paste the following line into your shell.\n");
+    fprintf(stdout, "note that it is a smart idea to backup your database before messing with it on this level.\n");
+    fprintf(stdout, "(you have been warned :) )\n\n");
+    // the big binary blob is a canonical blend mode option (switched off).
+    fprintf(stdout, "echo \"INSERT INTO presets VALUES('measured basecurve','','basecurve',2,X'%s',1,X'00000000180000000000C842000000000000000000000000000000000000000000000000000000000000000000000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F',7,0,'','%%','%%','%%',0.0,51200.0,0.0,10000000.0,0.0,100000000.0,0.0,1000.0,0,0,0,0,2);\" | sqlite3 ~/.config/darktable/library.db\n", encoded);
   }
-
-  // our best state is in `best'
-
-  fprintf(ff, "# err %f improved %d times\n", min, accepts);
-  fprintf(ff, "# copy paste into iop/basecurve.c (be sure to insert name, maker, model, and set the last 0 to 1 if happy to filter it):\n");
-  fprintf(ff, "# { \"new measured basecurve\", \"insert maker\", \"insert model\", 0, 51200,                        {{{");
-  for(int k=0;k<best.m_numAnchors;k++)
-    fprintf(ff, "{%f, %f}%s", best.m_anchors[k].x, best.m_anchors[k].y, k<best.m_numAnchors-1?", ":"}}, ");
-  fprintf(ff, "{%d}, {m}}, 0, 0},\n", best.m_numAnchors);
-  CurveDataSample(&best, &csample);
-  for(int k=0; k<0x10000; k++)
-    fprintf(ff, "%f %f\n", k*(1.0f/0x10000), 0.0 + (1.0f-0.0f)*csample.m_Samples[k]*(1.0f/0x10000));
+  else if (module == MODULE_TONECURVE)
+  {
+    fprintf(ff, "# err %f improved %d times\n", sqerr, accepts);
+    fprintf(ff, "# ");
+    for(int k=0;k<fit.m_numAnchors;k++)
+      fprintf(ff, "{%f, %f}%s", fit.m_anchors[k].x, fit.m_anchors[k].y, k<fit.m_numAnchors-1?", ":"\n");
+    fprintf(ff, "# {%d}\n", fit.m_numAnchors);
+    CurveDataSample(&fit, &csample);
+    for(int k=0; k<0x10000; k++)
+      fprintf(ff, "%f %f\n", k*(1.0f/0x10000), 0.0 + (1.0f-0.0f)*csample.m_Samples[k]*(1.0f/0x10000));
+    fprintf(stdout, "# still WIP, a dump of best fit curve in is fit.dat\n");
+  }
 
   fclose(fb);
   fclose(ff);
-  // TODO: output basecurve as sqlite3 command
-  char encoded[2048];
 
-  dt_iop_basecurve_params_t params;
-  memset(&params, 0, sizeof(params));
-  for(int k=0;k<best.m_numAnchors;k++)
-  {
-    params.basecurve[0][k].x = best.m_anchors[k].x;
-    params.basecurve[0][k].y = best.m_anchors[k].y;
-  }
-  params.basecurve_nodes[0] = best.m_numAnchors;
-  params.basecurve_type[0] = MONOTONE_HERMITE;
+  free(csample.m_Samples);
+  free(curve);
+  free(cnt);
 
-  text_encode ((uint8_t *)&params, encoded, sizeof(params));
-
-  fprintf(stdout, "to test your new basecurve, copy/paste the following line into your shell.\n");
-  fprintf(stdout, "note that it is a smart idea to backup your database before messing with it on this level.\n");
-  fprintf(stdout, "(you have been warned :) )\n\n");
-  // the big binary blob is a canonical blend mode option (switched off).
-  fprintf(stdout, "echo \"INSERT INTO presets VALUES('measured basecurve','','basecurve',2,X'%s',1,X'00000000180000000000C842000000000000000000000000000000000000000000000000000000000000000000000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F00000000000000000000803F0000803F',7,0,'','%%','%%','%%',0.0,51200.0,0.0,10000000.0,0.0,100000000.0,0.0,1000.0,0,0,0,0,2);\" | sqlite3 ~/.config/darktable/library.db\n", encoded);
-
-  exit(0);
+  return 0;
 }
