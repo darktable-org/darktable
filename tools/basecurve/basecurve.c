2c1331d3966562cb9156d984c22b84613b8c95ba
Edouard Gomez <ed.gomez@free.fr>
2014-02-05 22:10:46 +0100

    basecurve tool: add license header

--- tools/basecurve/basecurve.c
+++ tools/basecurve/basecurve.c
@@ -1,3 +1,23 @@
+/*
+    This file is part of darktable,
+    copyright (c) 2013 Thorsten
+                  2013 Johaness Hanika
+                  2014 Edouard Gomez
+
+    darktable is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    darktable is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with darktable.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #define _XOPEN_SOURCE 600
 #include <stdio.h>
 #include <stdlib.h>
