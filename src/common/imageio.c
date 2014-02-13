95ba358ef0613518164476e7a9bce3087c00a2ab
Jérémy Rosen <jeremy.rosen@enst-bretagne.fr>
2014-02-13 12:13:22 +0100

    check that the malloced memory is not null
    should help with bug #9808

--- src/common/imageio.c
+++ src/common/imageio.c
@@ -84,7 +84,7 @@ int dt_imageio_large_thumbnail(const char *filename, uint8_t **buffer, int32_t *
     dt_imageio_jpeg_t jpg;
     if(dt_imageio_jpeg_decompress_header(image->data, image->data_size, &jpg)) goto libraw_fail;
     *buffer = (uint8_t *)malloc((size_t)sizeof(uint8_t)*jpg.width*jpg.height*4);
-    if(!buffer) goto libraw_fail;
+    if(!*buffer) goto libraw_fail;
     *width = jpg.width;
     *height = jpg.height;
     if(dt_imageio_jpeg_decompress(&jpg, *buffer))
