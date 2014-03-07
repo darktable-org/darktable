11e8a1149a113672caec990ab92dee741b41b31b
Roman Lebedev <lebedev.ri@gmail.com>
2014-03-07 23:18:02 +0400

    Convert some #defines with values like 2**x to enums with bitshifts (cherry picked from commit fa763973f75b1502c70e2184956bf607cb4b872b)

--- src/develop/masks.h
+++ src/develop/masks.h
@@ -32,24 +32,29 @@
 typedef enum dt_masks_type_t
 {
   DT_MASKS_NONE = 0, // keep first
-  DT_MASKS_CIRCLE = 1,
-  DT_MASKS_PATH = 2,
-  DT_MASKS_GROUP = 4,
-  DT_MASKS_CLONE = 8,
-  DT_MASKS_GRADIENT = 16,
-  DT_MASKS_ELLIPSE = 32,
-  DT_MASKS_BRUSH = 64
+  DT_MASKS_CIRCLE = 1<<0,
+  DT_MASKS_PATH = 1<<1,
+  DT_MASKS_GROUP = 1<<2,
+  DT_MASKS_CLONE = 1<<3,
+  DT_MASKS_GRADIENT = 1<<4,
+  DT_MASKS_ELLIPSE = 1<<5,
+  DT_MASKS_BRUSH = 1<<6
 }
 dt_masks_type_t;
 
-#define	DT_MASKS_STATE_NONE 0
-#define	DT_MASKS_STATE_USE 1
-#define	DT_MASKS_STATE_SHOW 2
-#define	DT_MASKS_STATE_INVERSE 4
-#define	DT_MASKS_STATE_UNION 8
-#define	DT_MASKS_STATE_INTERSECTION 16
-#define	DT_MASKS_STATE_DIFFERENCE 32
-#define	DT_MASKS_STATE_EXCLUSION 64
+/**masts states */
+typedef enum dt_masks_state_t
+{
+  DT_MASKS_STATE_NONE = 0,
+  DT_MASKS_STATE_USE = 1<<0,
+  DT_MASKS_STATE_SHOW = 1<<1,
+  DT_MASKS_STATE_INVERSE = 1<<2,
+  DT_MASKS_STATE_UNION = 1<<3,
+  DT_MASKS_STATE_INTERSECTION = 1<<4,
+  DT_MASKS_STATE_DIFFERENCE = 1<<5,
+  DT_MASKS_STATE_EXCLUSION = 1<<6
+}
+dt_masks_state_t;
 
 typedef enum dt_masks_points_states_t
 {
