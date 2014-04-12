13965d37cada221e46ec83b29de6a0909eab1ba9
Roman Lebedev <lebedev.ri@gmail.com>
2014-04-13 00:52:44 +0400

    Convert last(?) enum's to bitshifts

--- src/develop/pixelpipe_hb.h
+++ src/develop/pixelpipe_hb.h
@@ -61,11 +61,11 @@ dt_dev_pixelpipe_iop_t;
 
 typedef enum dt_dev_pixelpipe_change_t
 {
-  DT_DEV_PIPE_UNCHANGED   = 0,  // no event
-  DT_DEV_PIPE_TOP_CHANGED = 1,  // only params of top element changed
-  DT_DEV_PIPE_REMOVE      = 2,  // possibly elements of the pipe have to be removed
-  DT_DEV_PIPE_SYNCH       = 4,  // all nodes up to end need to be synched, but no removal of module pieces is necessary
-  DT_DEV_PIPE_ZOOMED      = 8   // zoom event, preview pipe does not need changes
+  DT_DEV_PIPE_UNCHANGED   = 0,    // no event
+  DT_DEV_PIPE_TOP_CHANGED = 1<<0, // only params of top element changed
+  DT_DEV_PIPE_REMOVE      = 1<<1, // possibly elements of the pipe have to be removed
+  DT_DEV_PIPE_SYNCH       = 1<<2, // all nodes up to end need to be synched, but no removal of module pieces is necessary
+  DT_DEV_PIPE_ZOOMED      = 1<<3  // zoom event, preview pipe does not need changes
 }
 dt_dev_pixelpipe_change_t;
 
