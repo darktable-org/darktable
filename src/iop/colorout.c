41374c8fba5610e2ecd12faa221cf47a2b25476d
Roman Lebedev <lebedev.ri@gmail.com>
2014-04-16 03:29:49 +0400

    Tune gamutchech - was partially broken by bad alpha in 05a558e474897293f9e36f64eb629687fcebccef

--- src/iop/colorout.c
+++ src/iop/colorout.c
@@ -498,9 +498,13 @@ process (struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, void *ivoi
         for (int j=0; j<roi_out->width; j++,rgbptr+=4,out+=4)
         {
           const __m128 pixel = _mm_load_ps(rgbptr);
-          const __m128 ingamut = _mm_cmpge_ps(pixel, _mm_setzero_ps());
-          const __m128 result = _mm_or_ps(_mm_andnot_ps(ingamut, outofgamutpixel),
-                                          _mm_and_ps(ingamut, pixel));
+          __m128 ingamut = _mm_cmplt_ps(pixel, _mm_setzero_ps());
+
+          ingamut = _mm_or_ps(_mm_unpacklo_ps(ingamut, ingamut), _mm_unpackhi_ps(ingamut, ingamut));
+          ingamut = _mm_or_ps(_mm_unpacklo_ps(ingamut, ingamut), _mm_unpackhi_ps(ingamut, ingamut));
+
+          const __m128 result = _mm_or_ps(_mm_and_ps(ingamut, outofgamutpixel),
+                                          _mm_andnot_ps(ingamut, pixel));
           _mm_stream_ps(out, result);
         }
         dt_free_align(rgb);
