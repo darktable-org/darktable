e2503c7bf1766761d30eedb1ed018429462a4573
Jose Carlos Garcia Sogo <jcsogo@gmail.com>
2014-04-25 00:28:20 +0200

    Avoid flickering of stars and drop shadow for zoom == 1
    This commit avoids the nasty flickring of the stars and the drop shadow
    when scrolling through images at zoom level 1 or full preview (z)

--- src/views/view.c
+++ src/views/view.c
@@ -772,13 +772,13 @@ dt_view_image_expose(
 
   const dt_image_t *img = dt_image_cache_read_testget(darktable.image_cache, imgid);
 
-  if(selected == 1)
+  if(selected == 1 && zoom != 1) // If zoom == 1 there is no need to set colors here
   {
     outlinecol = 0.4;
     bgcol = 0.6;
     fontcol = 0.5;
   }
-  if(imgsel == imgid)
+  if(imgsel == imgid || zoom == 1)
   {
     bgcol = 0.8;  // mouse over
     fontcol = 0.7;
@@ -883,7 +883,7 @@ dt_view_image_expose(
   // border around image
   const float border = zoom == 1 ? 16/scale : 2/scale;
   cairo_set_source_rgb(cr, bordercol, bordercol, bordercol);
-  if(buf.buf && selected)
+  if(buf.buf && (selected || zoom == 1))
   {
     cairo_set_line_width(cr, 1./scale);
     if(zoom == 1)
@@ -924,8 +924,9 @@ dt_view_image_expose(
   if(buf.buf)
     dt_mipmap_cache_read_release(darktable.mipmap_cache, &buf);
 
+  cairo_save(cr);
   const float fscale = fminf(width, height);
-  if(imgsel == imgid || full_preview || darktable.gui->show_overlays)
+  if(imgsel == imgid || full_preview || darktable.gui->show_overlays || zoom == 1)
   {
     if (width > DECORATION_SIZE_LIMIT)
     {
@@ -960,7 +961,8 @@ dt_view_image_expose(
           {
             dt_view_star(cr, x, y, r1, r2);
             // Only draw hovering effects in stars for the hovered image
-            if((imgsel == imgid) && ((px - x)*(px - x) + (py - y)*(py - y) < r1*r1))
+            //printf ("Image selected: %d - Image processed: %d\n", imgsel, imgid);
+            if((imgsel == imgid || zoom == 1) && ((px - x)*(px - x) + (py - y)*(py - y) < r1*r1))
             {
               *image_over = DT_VIEW_STAR_1 + k;
               cairo_fill(cr);
@@ -984,7 +986,7 @@ dt_view_image_expose(
         cairo_set_source_rgb(cr, 1., 0., 0.);
 
       // Only draw hovering effects in stars for the hovered image
-      if((imgsel == imgid) && ((px - x)*(px - x) + (py - y)*(py - y) < r1*r1))
+      if((imgsel == imgid || zoom == 1) && ((px - x)*(px - x) + (py - y)*(py - y) < r1*r1))
       {
         *image_over = DT_VIEW_REJECT; //mouse sensitive
         cairo_new_sub_path(cr);
@@ -1094,6 +1096,7 @@ dt_view_image_expose(
       }
     }
   }
+  cairo_restore(cr);
 
   // kill all paths, in case img was not loaded yet, or is blocked:
   cairo_new_path(cr);
