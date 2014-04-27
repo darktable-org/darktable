918889e51c583c8702f2651bb82e49359a217422
Pascal Obry <pascal@obry.net>
2014-04-27 12:44:14 +0200

    No need to handle tagxtag since we are using triggers.

--- tools/purge_unused_tags.sh
+++ tools/purge_unused_tags.sh
@@ -11,14 +11,10 @@ DBFILE=~/.config/darktable/library.db
 Q1C="SELECT name FROM tags WHERE id NOT IN (SELECT tagid FROM tagged_images);"
 Q1="DELETE FROM tags WHERE id NOT IN (SELECT tagid FROM tagged_images);"
 
-# tagxtag for missing tags
-Q2="DELETE FROM tagxtag WHERE id1 NOT IN (SELECT id FROM TAGS) OR id2 NOT IN (SELECT id FROM tags);"
-
 if [ "$1" = "-p" ]; then
     echo Purging tags...
     echo "$Q1C" | sqlite3 $DBFILE
     echo "$Q1" | sqlite3 $DBFILE
-    echo "$Q2" | sqlite3 $DBFILE
 else
     echo "$Q1C" | sqlite3 $DBFILE
 fi
