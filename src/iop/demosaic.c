4ec9dd050ee7317f952ca295850b9c24c510bbb0
Dan Torop <dant@pnym.net>
2014-05-06 01:28:19 -0500

    VNG demosaic uses floats and openmp
    the dcraw code uses 16-bit ints, convert to using floats;
    OpenMP makes VNG much less sluggish

--- src/iop/demosaic.c
+++ src/iop/demosaic.c
@@ -29,6 +29,7 @@
 #include "external/adobe_coeff.c"
 #include <memory.h>
 #include <stdlib.h>
+#include <math.h>
 #include <string.h>
 
 // we assume people have -msee support.
@@ -967,95 +968,85 @@ void xtrans_vng_interpolate(
   dt_iop_roi_t *roi_out, const dt_iop_roi_t *roi_in,
   const uint8_t xtrans[6][6])
 {
-  static const signed char *cp, terms[] = {
-    -2,-2,+0,-1,0,0x01, -2,-2,+0,+0,1,0x01, -2,-1,-1,+0,0,0x01,
-    -2,-1,+0,-1,0,0x02, -2,-1,+0,+0,0,0x03, -2,-1,+0,+1,1,0x01,
-    -2,+0,+0,-1,0,0x06, -2,+0,+0,+0,1,0x02, -2,+0,+0,+1,0,0x03,
-    -2,+1,-1,+0,0,0x04, -2,+1,+0,-1,1,0x04, -2,+1,+0,+0,0,0x06,
-    -2,+1,+0,+1,0,0x02, -2,+2,+0,+0,1,0x04, -2,+2,+0,+1,0,0x04,
-    -1,-2,-1,+0,0,0x80, -1,-2,+0,-1,0,0x01, -1,-2,+1,-1,0,0x01,
-    -1,-2,+1,+0,1,0x01, -1,-1,-1,+1,0,0x88, -1,-1,+1,-2,0,0x40,
-    -1,-1,+1,-1,0,0x22, -1,-1,+1,+0,0,0x33, -1,-1,+1,+1,1,0x11,
-    -1,+0,-1,+2,0,0x08, -1,+0,+0,-1,0,0x44, -1,+0,+0,+1,0,0x11,
-    -1,+0,+1,-2,1,0x40, -1,+0,+1,-1,0,0x66, -1,+0,+1,+0,1,0x22,
-    -1,+0,+1,+1,0,0x33, -1,+0,+1,+2,1,0x10, -1,+1,+1,-1,1,0x44,
-    -1,+1,+1,+0,0,0x66, -1,+1,+1,+1,0,0x22, -1,+1,+1,+2,0,0x10,
-    -1,+2,+0,+1,0,0x04, -1,+2,+1,+0,1,0x04, -1,+2,+1,+1,0,0x04,
-    +0,-2,+0,+0,1,0x80, +0,-1,+0,+1,1,0x88, +0,-1,+1,-2,0,0x40,
-    +0,-1,+1,+0,0,0x11, +0,-1,+2,-2,0,0x40, +0,-1,+2,-1,0,0x20,
-    +0,-1,+2,+0,0,0x30, +0,-1,+2,+1,1,0x10, +0,+0,+0,+2,1,0x08,
-    +0,+0,+2,-2,1,0x40, +0,+0,+2,-1,0,0x60, +0,+0,+2,+0,1,0x20,
-    +0,+0,+2,+1,0,0x30, +0,+0,+2,+2,1,0x10, +0,+1,+1,+0,0,0x44,
-    +0,+1,+1,+2,0,0x10, +0,+1,+2,-1,1,0x40, +0,+1,+2,+0,0,0x60,
-    +0,+1,+2,+1,0,0x20, +0,+1,+2,+2,0,0x10, +1,-2,+1,+0,0,0x80,
-    +1,-1,+1,+1,0,0x88, +1,+0,+1,+2,0,0x08, +1,+0,+2,-1,0,0x40,
-    +1,+0,+2,+1,0,0x10
+  static const signed char terms[] = {
+    -2,-2,+0,-1,1,0x01, -2,-2,+0,+0,2,0x01, -2,-1,-1,+0,1,0x01,
+    -2,-1,+0,-1,1,0x02, -2,-1,+0,+0,1,0x03, -2,-1,+0,+1,2,0x01,
+    -2,+0,+0,-1,1,0x06, -2,+0,+0,+0,2,0x02, -2,+0,+0,+1,1,0x03,
+    -2,+1,-1,+0,1,0x04, -2,+1,+0,-1,2,0x04, -2,+1,+0,+0,1,0x06,
+    -2,+1,+0,+1,1,0x02, -2,+2,+0,+0,2,0x04, -2,+2,+0,+1,1,0x04,
+    -1,-2,-1,+0,1,0x80, -1,-2,+0,-1,1,0x01, -1,-2,+1,-1,1,0x01,
+    -1,-2,+1,+0,2,0x01, -1,-1,-1,+1,1,0x88, -1,-1,+1,-2,1,0x40,
+    -1,-1,+1,-1,1,0x22, -1,-1,+1,+0,1,0x33, -1,-1,+1,+1,2,0x11,
+    -1,+0,-1,+2,1,0x08, -1,+0,+0,-1,1,0x44, -1,+0,+0,+1,1,0x11,
+    -1,+0,+1,-2,2,0x40, -1,+0,+1,-1,1,0x66, -1,+0,+1,+0,2,0x22,
+    -1,+0,+1,+1,1,0x33, -1,+0,+1,+2,2,0x10, -1,+1,+1,-1,2,0x44,
+    -1,+1,+1,+0,1,0x66, -1,+1,+1,+1,1,0x22, -1,+1,+1,+2,1,0x10,
+    -1,+2,+0,+1,1,0x04, -1,+2,+1,+0,2,0x04, -1,+2,+1,+1,1,0x04,
+    +0,-2,+0,+0,2,0x80, +0,-1,+0,+1,2,0x88, +0,-1,+1,-2,1,0x40,
+    +0,-1,+1,+0,1,0x11, +0,-1,+2,-2,1,0x40, +0,-1,+2,-1,1,0x20,
+    +0,-1,+2,+0,1,0x30, +0,-1,+2,+1,2,0x10, +0,+0,+0,+2,2,0x08,
+    +0,+0,+2,-2,2,0x40, +0,+0,+2,-1,1,0x60, +0,+0,+2,+0,2,0x20,
+    +0,+0,+2,+1,1,0x30, +0,+0,+2,+2,2,0x10, +0,+1,+1,+0,1,0x44,
+    +0,+1,+1,+2,1,0x10, +0,+1,+2,-1,2,0x40, +0,+1,+2,+0,1,0x60,
+    +0,+1,+2,+1,1,0x20, +0,+1,+2,+2,1,0x10, +1,-2,+1,+0,1,0x80,
+    +1,-1,+1,+1,1,0x88, +1,+0,+1,+2,1,0x08, +1,+0,+2,-1,1,0x40,
+    +1,+0,+2,+1,1,0x10
   }, chood[] = { -1,-1, -1,0, -1,+1, 0,+1, +1,+1, +1,0, +1,-1, 0,-1 };
-  unsigned short (*brow[5])[4], *pix;
-  int prow=6, pcol=6, *ip, *code[6][6], gval[8], gmin, gmax, sum[3];
-  int row, col, x, y, x1, x2, y1, y2, t, weight, grads, color, diag;
-  int g, diff, thold, num, c;
-
-  int width = roi_out->width;
-  int height = roi_out->height;
+  int *ip, *code[6][6];
+  const int width = roi_out->width, height = roi_out->height;
 
   xtrans_lin_interpolate(out, in, roi_out, roi_in, xtrans);
 
-  // FIXME: for first pass at this, convert data into a buffer of unsigned shorts as dcraw does -- make this work with floats instead, and perhaps not so much copying
-  unsigned short (*image)[4];
-
-  image = (unsigned short (*)[4]) calloc(height, width*sizeof *image);
-  for (row=0; row < height; row++)
-    for (col=0; col < width; col++)
-      for (c=0; c<4; c++)
-        image[row*width+col][c] = 0xffff * out[4*(row*width+col)+c];
-
-  // FIXME: this should be based off of sizeof *int to work 32/64 bit?
-  ip = (int *) calloc (prow*pcol, 1280);
-  // FIXME: handle calloc error here and in image calloc above; and should dt_alloc_align()?
-  //merror (ip, "vng_interpolate()");
-  for (row=0; row < prow; row++)		/* Precalculate for VNG */
-    for (col=0; col < pcol; col++) {
+  ip = (int *) dt_alloc_align(16, (size_t)sizeof(*ip) * 6*6 * 320);
+  // FIXME: handle alloc error here?
+  for (int row=0; row < 6; row++)		/* Precalculate for VNG */
+    for (int col=0; col < 6; col++) {
       code[row][col] = ip;
-      for (cp=terms, t=0; t < 64; t++) {
-	y1 = *cp++;  x1 = *cp++;
-	y2 = *cp++;  x2 = *cp++;
-	weight = *cp++;
-	grads = *cp++;
-	color = fcol(row+y1,col+x1);
+      const signed char *cp = terms;
+      for (int t=0; t < 64; t++) {
+	int y1 = *cp++, x1 = *cp++;
+	int y2 = *cp++, x2 = *cp++;
+	int weight = *cp++;
+	int grads = *cp++;
+	int color = fcol(row+y1,col+x1);
 	if (fcol(row+y2,col+x2) != color) continue;
-	diag = (fcol(row,col+1) == color && fcol(row+1,col) == color) ? 2:1;
+	int diag = (fcol(row,col+1) == color && fcol(row+1,col) == color) ? 2:1;
 	if (abs(y1-y2) == diag && abs(x1-x2) == diag) continue;
 	*ip++ = (y1*width + x1)*4 + color;
 	*ip++ = (y2*width + x2)*4 + color;
 	*ip++ = weight;
-	for (g=0; g < 8; g++)
+	for (int g=0; g < 8; g++)
 	  if (grads & 1<<g) *ip++ = g;
 	*ip++ = -1;
       }
       *ip++ = INT_MAX;
-      for (cp=chood, g=0; g < 8; g++) {
-	y = *cp++;  x = *cp++;
+      cp=chood;
+      for (int g=0; g < 8; g++) {
+	int y = *cp++, x = *cp++;
 	*ip++ = (y*width + x) * 4;
-	color = fcol(row,col);
+	int color = fcol(row,col);
 	if (fcol(row+y,col+x) != color && fcol(row+y*2,col+x*2) == color)
 	  *ip++ = (y*width + x) * 8 + color;
 	else
 	  *ip++ = 0;
       }
     }
-  // FIXME: handle calloc error
-  brow[4] = (unsigned short (*)[4]) calloc (width*3, sizeof **brow);
-  //merror (brow[4], "vng_interpolate()");
-  for (row=0; row < 3; row++)
+  float (*brow[5])[4];
+  // FIXME: handle alloc error
+  brow[4] = (float (*)[4]) dt_alloc_align(16, (size_t)sizeof(**brow)*width*3);
+  for (int row=0; row < 3; row++)
     brow[row] = brow[4] + row*width;
-  for (row=2; row < height-2; row++) {		/* Do VNG interpolation */
-    for (col=2; col < width-2; col++) {
-      pix = image[row*width+col];
-      ip = code[row % prow][col % pcol];
-      memset (gval, 0, sizeof gval);
+  for (int row=2; row < height-2; row++) {	/* Do VNG interpolation */
+#ifdef _OPENMP
+    #pragma omp parallel for default(none) shared(row, out, code, brow, xtrans) private(ip) schedule(static)
+#endif
+    for (int col=2; col < width-2; col++) {
+      int g;
+      float gval[8] = { 0.0f };
+      float *pix = out + 4*(row*width+col);
+      ip = code[row%6][col%6];
       while ((g = ip[0]) != INT_MAX) {		/* Calculate gradients */
-	diff = ABS(pix[g] - pix[ip[1]]) << ip[2];
+	float diff = fabsf(pix[g] - pix[ip[1]]) * ip[2];
 	gval[ip[3]] += diff;
 	ip += 5;
 	if ((g = ip[-1]) == -1) continue;
@@ -1064,50 +1055,45 @@ void xtrans_vng_interpolate(
 	  gval[g] += diff;
       }
       ip++;
-      gmin = gmax = gval[0];			/* Choose a threshold */
+      float gmin = gval[0], gmax = gval[0];	/* Choose a threshold */
       for (g=1; g < 8; g++) {
 	if (gmin > gval[g]) gmin = gval[g];
 	if (gmax < gval[g]) gmax = gval[g];
       }
       if (gmax == 0) {
-	memcpy (brow[2][col], pix, sizeof *image);
+	memcpy (brow[2][col], pix, 4 * sizeof *out);
 	continue;
       }
-      thold = gmin + (gmax >> 1);
-      memset (sum, 0, sizeof sum);
-      color = fcol(row,col);
-      for (num=g=0; g < 8; g++,ip+=2) {		/* Average the neighbors */
+      float thold = gmin + (gmax * 0.5f);
+      float sum[3] = { 0.0f };
+      int color = fcol(row,col);
+      int num=0;
+      for (g=0; g < 8; g++,ip+=2) {		/* Average the neighbors */
 	if (gval[g] <= thold) {
-	  for (c=0; c < 3; c++)
+	  for (int c=0; c < 3; c++)
 	    if (c == color && ip[1])
-	      sum[c] += (pix[c] + pix[ip[1]]) >> 1;
+	      sum[c] += (pix[c] + pix[ip[1]]) * 0.5f;
 	    else
 	      sum[c] += pix[ip[0] + c];
 	  num++;
 	}
       }
-      for (c=0; c < 3; c++) {			/* Save to buffer */
-	t = pix[color];
+      for (int c=0; c < 3; c++) {		/* Save to buffer */
+	float tot = pix[color];
 	if (c != color)
-	  t += (sum[c] - sum[color]) / num;
-	brow[2][col][c] = CLIP(t);
+	  tot += (sum[c] - sum[color]) / num;
+	brow[2][col][c] = LIM(tot,0.0f,1.0f);
       }
     }
     if (row > 3)				/* Write buffer to image */
-      memcpy (image[(row-2)*width+2], brow[0]+2, (width-4)*sizeof *image);
-    for (g=0; g < 4; g++)
+      memcpy (out + 4*((row-2)*width+2), brow[0]+2, (width-4)*4*sizeof *out);
+    for (int g=0; g < 4; g++)
       brow[(g-1) & 3] = brow[g];
   }
-  memcpy (image[(row-2)*width+2], brow[0]+2, (width-4)*sizeof *image);
-  memcpy (image[(row-1)*width+2], brow[1]+2, (width-4)*sizeof *image);
+  memcpy (out + (4*((height-4)*width+2)), brow[0]+2, (width-4)*4*sizeof *out);
+  memcpy (out + (4*((height-3)*width+2)), brow[1]+2, (width-4)*4*sizeof *out);
   free (brow[4]);
   free (code[0][0]);
-
-  for (row=0; row < height; row++)
-    for (col=0; col < width; col++)
-      for (c=0; c<4; c++)
-        out[4*(row*width+col)+c]=(float)image[row*width+col][c]/0xffff;
-  free(image);
 }
 
 
