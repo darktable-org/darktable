9d96c1a0fe1d2a9150f7934c87cd02024a0224d1
Pedro Côrte-Real <pedro@pedrocr.net>
2014-05-15 03:36:50 +0100

    Add the code for the 7HI

--- src/external/rawspeed/RawSpeed/MrwDecoder.cpp
+++ src/external/rawspeed/RawSpeed/MrwDecoder.cpp
@@ -51,6 +51,7 @@ static mrw_camera_t mrw_camera_table[] = {
   {"27820001", "DIMAGE A1"},
   {"27200001", "DIMAGE A2"},
   {"27790001", "DIMAGE 7I"},
+  {"27780001", "DIMAGE 7HI"},
   {"21810002", "DYNAX 7D"},
   {"21860002", "DYNAX 5D"},
 };
