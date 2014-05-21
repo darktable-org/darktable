9764d54275965326503f2b99f8654f5d3ef791f6
Pascal de Bruijn <pmjdebruijn@pcode.nl>
2014-05-21 23:39:16 +0200

    exif: legacy panasonic RAWs have basic exif in the makernote
    i worked on this for an hour before noticing this was bleeding
    obvious.
    
    this really is one of those cases where you really think:
    what were they thinking

--- src/common/exif.cc
+++ src/common/exif.cc
@@ -605,6 +605,14 @@ static bool dt_exif_read_exif_data(dt_image_t *img, Exiv2::ExifData &exifData)
           break;
         }
     }
+
+    if (!strlen(img->exif_maker))
+    {
+      if ( (pos=exifData.findKey(Exiv2::ExifKey("Exif.PanasonicRaw.Make")))
+           != exifData.end() && pos->size())
+        dt_strlcpy_to_utf8(img->exif_maker, sizeof(img->exif_maker), pos, exifData);
+    }
+
     if ( (pos=exifData.findKey(Exiv2::ExifKey("Exif.Image.Model")))
          != exifData.end() && pos->size())
     {
@@ -615,6 +623,14 @@ static bool dt_exif_read_exif_data(dt_image_t *img, Exiv2::ExifData &exifData)
           break;
         }
     }
+
+    if (!strlen(img->exif_model))
+    {
+      if ( (pos=exifData.findKey(Exiv2::ExifKey("Exif.PanasonicRaw.Model")))
+           != exifData.end() && pos->size())
+        dt_strlcpy_to_utf8(img->exif_model, sizeof(img->exif_model), pos, exifData);
+    }
+
     if ( (pos=exifData.findKey(Exiv2::ExifKey("Exif.Image.DateTimeOriginal")))
          != exifData.end() && pos->size())
     {
