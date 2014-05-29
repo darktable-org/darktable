e3670fb791aa5331e133f24a6228ef839bbb3fb5
Pedro Côrte-Real <pedro@pedrocr.net>
2014-05-29 20:07:06 +0100

    Blacklist the .SR2 extension from libraw

--- src/common/imageio.c
+++ src/common/imageio.c
@@ -309,7 +309,7 @@ return_label:
 static gboolean
 _blacklisted_ext(const gchar *filename)
 {
-  const char *extensions_blacklist[] = { "dng", "cr2", "nef", "nrw", "orf", "rw2", "pef", "srw", "arw", "raf", "mrw", "raw", NULL };
+  const char *extensions_blacklist[] = { "dng", "cr2", "nef", "nrw", "orf", "rw2", "pef", "srw", "arw", "raf", "mrw", "raw", "sr2", NULL };
   gboolean supported = TRUE;
   char *ext = g_strrstr(filename, ".");
   if(!ext) return FALSE;
