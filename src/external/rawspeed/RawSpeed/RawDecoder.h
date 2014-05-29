5affcfccc58af7264935edd3f07c62c26757b73e
Pedro Côrte-Real <pedro@pedrocr.net>
2014-05-30 00:00:10 +0100

    Add a shifted big-endian unpacked decoder

--- src/external/rawspeed/RawSpeed/RawDecoder.h
+++ src/external/rawspeed/RawSpeed/RawDecoder.h
@@ -173,6 +173,9 @@ protected:
   /* Faster version for reading unpacked 12 bit MSB data */
   void Decode12BitRawBEunpacked(ByteStream &input, uint32 w, uint32 h);
 
+  /* Faster version for reading unpacked 12 bit MSB data that is left aligned (needs >> 4 shift) */
+  void Decode12BitRawBEunpackedLeftAligned(ByteStream &input, uint32 w, uint32 h);
+
   /* Faster version for reading unpacked 12 bit LSB data */
   void Decode12BitRawUnpacked(ByteStream &input, uint32 w, uint32 h);
 
