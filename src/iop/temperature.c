5060007149b112200734ae6f67f40fc22ed0d548
Roman Lebedev <lebedev.ri@gmail.com>
2014-07-15 14:47:57 +0400

    Temperature iop: Fix process (X-Trans)

--- src/iop/temperature.c
+++ src/iop/temperature.c
@@ -219,14 +219,14 @@ void process (struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, void 
   uint8_t (*const xtrans)[6] = self->dev->image_storage.xtrans;
   dt_iop_temperature_data_t *d = (dt_iop_temperature_data_t *)piece->data;
   if(!dt_dev_pixelpipe_uses_downsampled_input(piece->pipe) && filters == 9u && piece->pipe->image.bpp != 4)
-  {  // xtrans int mosaiced
-    const float coeffsi[3] = {d->coeffs[0]/65535.0f, d->coeffs[1]/65535.0f, d->coeffs[2]/65535.0f};
+  {  // xtrans float mosaiced
+    const float coeffsi[3] = {d->coeffs[0], d->coeffs[1], d->coeffs[2]};
 #ifdef _OPENMP
     #pragma omp parallel for default(none) shared(roi_out, ivoid, ovoid, d) schedule(static)
 #endif
     for(int j=0; j<roi_out->height; j++)
     {
-      const uint16_t *in = ((uint16_t *)ivoid) + (size_t)j*roi_out->width;
+      const float *in = ((float *)ivoid) + (size_t)j*roi_out->width;
       float *out = ((float*)ovoid) + (size_t)j*roi_out->width;
       for(int i=0; i<roi_out->width; i++,out++,in++)
         *out = *in * coeffsi[FCxtrans(j,i,roi_out,xtrans)];
