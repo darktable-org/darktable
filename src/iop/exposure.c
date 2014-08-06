darktable-org/darktable

commit 441984a871c5c1edb3937b490c437ab5c0b7a693

Exposure: disable piece if no-op

src/iop/exposure.c
@@ -483,6 +483,10 @@ void commit_params (struct dt_iop_module_t *self, dt_iop_params_t *p1, dt_dev_pi
   {
     d->mode = EXPOSURE_MODE_MANUAL;
   }
+
+  if(d->mode != EXPOSURE_MODE_DEFLICKER &&
+      d->exposure == 0.0f && d->black == 0.0f)
+    piece->enabled = 0;
 }
 
 void init_pipe (struct dt_iop_module_t *self, dt_dev_pixelpipe_t *pipe, dt_dev_pixelpipe_iop_t *piece)

commit 84beaa88c2b0338cdd1f20689ecdacaa6f7dd2c3

Exposure IOP: fix autoexp
Was broken by 441984a871c5c1edb3937b490c437ab5c0b7a693

src/iop/exposure.c
@@ -485,6 +485,7 @@ void commit_params (struct dt_iop_module_t *self, dt_iop_params_t *p1, dt_dev_pi
   }
 
   if(d->mode != EXPOSURE_MODE_DEFLICKER &&
+      self->request_color_pick != DT_REQUEST_COLORPICK_MODULE && //autoexp
       d->exposure == 0.0f && d->black == 0.0f)
     piece->enabled = 0;
 }