b6a102aac21cbb713bd4a2e883ccfe8d20f38c8f
Roman Lebedev <lebedev.ri@gmail.com>
2014-09-10 17:55:48 +0400

    dt_imageio_open_rawspeed_sraw(): break everything! Stop scaling (B/W).

--- src/common/imageio_rawspeed.cc
+++ src/common/imageio_rawspeed.cc
@@ -248,10 +248,6 @@ dt_imageio_retval_t dt_imageio_open_rawspeed_sraw(dt_image_t *img, RawImage r, d
   for (int i=0; i<3; i++)
     img->wb_coeffs[i] = r->metadata.wbCoeffs[i];
 
-  /* needed by Deflicker */
-  img->raw_black_level = r->blackLevel;
-  img->raw_white_point = r->whitePoint;
-
   size_t raw_width = r->dim.x;
   size_t raw_height = r->dim.y;
 
@@ -268,16 +264,11 @@ dt_imageio_retval_t dt_imageio_open_rawspeed_sraw(dt_image_t *img, RawImage r, d
   void *buf = dt_mipmap_cache_alloc(mbuf, img);
   if(!buf) return DT_IMAGEIO_CACHE_FULL;
 
-  int black = r->blackLevel;
-  int white = r->whitePoint;
-
   uint16_t *raw_img = (uint16_t *)r->getDataUncropped(0, 0);
-
-  const float scale = (float)(white - black);
 
 #ifdef _OPENMP
 #pragma omp parallel for default(none) schedule(static) shared(raw_width, raw_height, raw_img, img,          \
-                                                               dimUncropped, cropTL, buf, black)
+                                                               dimUncropped, cropTL, buf)
 #endif
   for(size_t row = 0; row < raw_height; row++)
   {
@@ -296,7 +287,7 @@ dt_imageio_retval_t dt_imageio_open_rawspeed_sraw(dt_image_t *img, RawImage r, d
            * we need to copy data from only channel to each of 3 channels
            */
 
-          out[k] = MAX(0.0f, (((float)(*in)) - black) / scale);
+          out[k] = (float)*in / (float)UINT16_MAX;
         }
         else
         {
@@ -305,7 +296,7 @@ dt_imageio_retval_t dt_imageio_open_rawspeed_sraw(dt_image_t *img, RawImage r, d
            * just copy 3 ch to 3 ch
            */
 
-          out[k] = MAX(0.0f, (((float)(in[k])) - black) / scale);
+          out[k] = (float)in[k] / (float)UINT16_MAX;
         }
       }
     }
