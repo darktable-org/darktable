e3a7ff73fc24e1ed16e7644e09c70cfa8951f4de
Pedro Côrte-Real <pedro@pedrocr.net>
2014-09-15 23:44:19 +0100

    Allocate large enough buffer to hold full f700 raw and then ignore the second part of the raw (the darker version of the same photo from the Super CCD sensor)

--- src/external/rawspeed/RawSpeed/RafDecoder.cpp
+++ src/external/rawspeed/RawSpeed/RafDecoder.cpp
@@ -84,12 +84,17 @@ RawImage RafDecoder::decodeRawInternal() {
   // x-trans sensors report 14bpp, but data isn't packed so read as 16bpp
   if (bps == 14) bps = 16;
 
-  mRaw->dim = iPoint2D(width, height);
+  // Some fuji SuperCCD cameras include a second raw image next to the first one
+  // that is identical but darker to the first. The two combined can produce
+  // a higher dynamic range image. Right now we're ignoring it.
+  bool double_width = hints.find("double_width_unpacked") != hints.end();
+
+  mRaw->dim = iPoint2D(width*(double_width ? 2 : 1), height);
   mRaw->createData();
   ByteStream input(mFile->getData(off), mFile->getSize() - off);
   iPoint2D pos(0, 0);
 
-  if (hints.find("double_width_unpacked") != hints.end()) {
+  if (double_width) {
     Decode16BitRawUnpacked(input, width*2, height);
   } else if (mRootIFD->endian == big) {
     Decode16BitRawBEunpacked(input, width, height);
@@ -146,9 +151,12 @@ void RafDecoder::decodeMetaDataInternal(CameraMetaData *meta) {
   if (applyCrop) {
     new_size = cam->cropSize;
     crop_offset = cam->cropPos;
+    bool double_width = hints.find("double_width_unpacked") != hints.end();
     // If crop size is negative, use relative cropping
     if (new_size.x <= 0)
-      new_size.x = mRaw->dim.x - cam->cropPos.x + new_size.x;
+      new_size.x = mRaw->dim.x / (double_width ? 2 : 1) - cam->cropPos.x + new_size.x;
+    else
+      new_size.x /= (double_width ? 2 : 1);
 
     if (new_size.y <= 0)
       new_size.y = mRaw->dim.y - cam->cropPos.y + new_size.y;
