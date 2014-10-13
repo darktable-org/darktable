12f230d21d72157f8bd030d9866335f57b7fde50
Roman Lebedev <lebedev.ri@gmail.com>
2014-10-14 00:38:09 +0400

    iop_dependencies.py: add rotatepixels Run iop_dependencies.py after merge!

--- tools/iop_dependencies.py
+++ tools/iop_dependencies.py
@@ -86,12 +86,15 @@ def add_edges(gr):
   
   # fix mad sensor designs: NIKON D1X have rectangular pixels
   gr.add_edge(('scalepixels', 'demosaic'))
+  # fix mad sensor designs: some Fuji have their Bayer pattern rotated by -45deg
+  gr.add_edge(('rotatepixels', 'demosaic'))
 
   # flip is a distortion plugin, and as such has to go after spot removal
   # and lens correction, which depend on original input buffers.
   # and after buffer has been downscaled/demosaiced
   gr.add_edge(('flip', 'demosaic'))
   gr.add_edge(('flip', 'scalepixels'))
+  gr.add_edge(('flip', 'rotatepixels'))
   gr.add_edge(('flip', 'lens'))
   gr.add_edge(('flip', 'spots'))
   # plus, it confuses crop/rotate, vignetting and graduated density
@@ -161,6 +164,7 @@ def add_edges(gr):
   # and needs to be before geometric distortions:
   gr.add_edge(('spots', 'demosaic'))
   gr.add_edge(('scalepixels', 'spots'))
+  gr.add_edge(('rotatepixels', 'spots'))
   gr.add_edge(('lens', 'spots'))
   gr.add_edge(('borders', 'spots'))
   gr.add_edge(('clipping', 'spots'))
@@ -382,6 +386,7 @@ gr.add_nodes([
 'rawdenoise',
 'relight',
 'scalepixels',
+'rotatepixels',
 'shadhi',
 'sharpen',
 'soften',
