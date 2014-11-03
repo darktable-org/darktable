darktable-org/darktable

commit c0a900978328d7812048ce071cc31cd5b94d2f4e

Fix #10120: Don't put tags more than once into XMP
This includes some rewrites of helper functions

src/common/variables.c
@@ -219,8 +219,7 @@ gboolean _variable_get_value(dt_variables_params_t *params, gchar *variable,gcha
   else if( g_strcmp0(variable,"$(LABELS)") == 0 && (got_value=TRUE) )
   {
     //TODO: currently we concatenate all the color labels with a ',' as a separator. Maybe it's better to only use the first/last label?
-    unsigned int count = 0;
-    GList *res = dt_metadata_get(params->imgid, "Xmp.darktable.colorlabels", &count);
+    GList *res = dt_metadata_get(params->imgid, "Xmp.darktable.colorlabels", NULL);
     res = g_list_first(res);
     if(res != NULL)
     {
@@ -230,7 +229,8 @@ gboolean _variable_get_value(dt_variables_params_t *params, gchar *variable,gcha
         labels = g_list_append(labels, (char *)(_(dt_colorlabels_to_string(GPOINTER_TO_INT(res->data)))));
       }
       while((res=g_list_next(res)) != NULL);
-      char* str = dt_util_glist_to_str(",", labels, count);
+      char* str = dt_util_glist_to_str(",", labels);
+      g_list_free_full(labels, g_free);
       snprintf(value, value_len,  "%s", str);
       g_free(str);
     }
@@ -242,8 +242,7 @@ gboolean _variable_get_value(dt_variables_params_t *params, gchar *variable,gcha
   }
   else if( g_strcmp0(variable,"$(TITLE)") == 0 && params->filename && (got_value=TRUE) )
   {
-    unsigned int count = 0;
-    GList *res = dt_metadata_get(params->imgid, "Xmp.dc.title", &count);
+    GList *res = dt_metadata_get(params->imgid, "Xmp.dc.title", NULL);
     res = g_list_first(res);
     if(res != NULL)
     {

src/common/exif.cc
@@ -1850,27 +1850,18 @@ dt_exif_xmp_read_data(Exiv2::XmpData &xmpData, const int imgid)
   Exiv2::Value::AutoPtr v1 = Exiv2::Value::create(Exiv2::xmpSeq); // or xmpBag or xmpAlt.
   Exiv2::Value::AutoPtr v2 = Exiv2::Value::create(Exiv2::xmpSeq); // or xmpBag or xmpAlt.
 
-  gchar *tags = NULL;
-  gchar *hierarchical = NULL;
-
-  tags = dt_tag_get_list(imgid, ",");
-  char *beg = tags;
-  while(beg)
+  GList *tags = dt_tag_get_list(imgid);
+  while(tags)
   {
-    char *next = strstr(beg, ",");
-    if(next) *(next++) = 0;
-    v1->read(beg);
-    beg = next;
+    v1->read((char*)tags->data);
+    tags = g_list_next(tags);
   }
 
-  hierarchical = dt_tag_get_hierarchical(imgid, ",");
-  beg = hierarchical;
-  while(beg)
+  GList *hierarchical = dt_tag_get_hierarchical(imgid);
+  while(hierarchical)
   {
-    char *next = strstr(beg, ",");
-    if(next) *(next++) = 0;
-    v2->read(beg);
-    beg = next;
+    v2->read((char*)hierarchical->data);
+    hierarchical = g_list_next(hierarchical);
   }
 
   if(v1->count() > 0)
@@ -2050,8 +2041,8 @@ dt_exif_xmp_read_data(Exiv2::XmpData &xmpData, const int imgid)
     num ++;
   }
   sqlite3_finalize (stmt);
-  g_free(tags);
-  g_free(hierarchical);
+  g_list_free_full(tags, g_free);
+  g_list_free_full(hierarchical, g_free);
 }
 
 int dt_exif_xmp_attach (const int imgid, const char* filename)

src/common/history.c
@@ -294,7 +294,6 @@ dt_history_get_items_as_string(int32_t imgid)
 {
   GList *items = NULL;
   const char *onoff[2] = {_("off"), _("on")};
-  unsigned int count = 0;
   sqlite3_stmt *stmt;
   DT_DEBUG_SQLITE3_PREPARE_V2(dt_database_get(darktable.db), "select operation, enabled, multi_name from history where imgid=?1 order by num desc", -1, &stmt, NULL);
   DT_DEBUG_SQLITE3_BIND_INT(stmt, 1, imgid);
@@ -314,9 +313,10 @@ dt_history_get_items_as_string(int32_t imgid)
                        NULL);
     items = g_list_append(items, name);
     g_free(multi_name);
-    count++;
   }
-  return dt_util_glist_to_str("\n", items, count);
+  char *result = dt_util_glist_to_str("\n", items);
+  g_list_free_full(items, g_free);
+  return result;
 }
 
 int

src/common/styles.c
@@ -654,18 +654,18 @@ dt_styles_get_item_list_as_string(const char *name)
   if (items)
   {
     GList* names = NULL;
-    unsigned int count = 0;
     do
     {
       dt_style_item_t *item=(dt_style_item_t *)items->data;
       names = g_list_append(names, g_strdup(item->name));
       g_free(item->name);
       g_free(item);
-      count++;
     }
     while ((items=g_list_next(items)));
 
-    return dt_util_glist_to_str("\n", names, count);
+    char *result = dt_util_glist_to_str("\n", names);
+    g_list_free_full(names, g_free);
+    return result;
   }
   return NULL;
 }

src/common/tags.c
@@ -268,7 +268,7 @@ void dt_tag_detach_by_string(const char *name, gint imgid)
 }
 
 
-uint32_t dt_tag_get_attached(gint imgid,GList **result)
+uint32_t dt_tag_get_attached(gint imgid, GList **result, gboolean ignore_dt_tags)
 {
   sqlite3_stmt *stmt;
   if(imgid > 0)
@@ -277,17 +277,25 @@ uint32_t dt_tag_get_attached(gint imgid,GList **result)
     snprintf(query, sizeof(query),
              "SELECT DISTINCT T.id, T.name FROM tagged_images "
              "JOIN tags T on T.id = tagged_images.tagid "
-             "WHERE tagged_images.imgid = %d", imgid);
+             "WHERE tagged_images.imgid = %d %s ORDER BY T.name", imgid, ignore_dt_tags?"AND NOT T.name LIKE \"darktable|%\"":"");
     DT_DEBUG_SQLITE3_PREPARE_V2(dt_database_get(darktable.db), query, -1,
                                 &stmt, NULL);
   }
   else
   {
-    DT_DEBUG_SQLITE3_PREPARE_V2(dt_database_get(darktable.db),
-                                "SELECT DISTINCT T.id, T.name "
-                                "FROM tagged_images,tags as T "
-                                "WHERE tagged_images.imgid in (select imgid from selected_images)"
-                                "  AND T.id = tagged_images.tagid", -1, &stmt, NULL);
+    if(ignore_dt_tags)
+      DT_DEBUG_SQLITE3_PREPARE_V2(dt_database_get(darktable.db),
+                                  "SELECT DISTINCT T.id, T.name "
+                                  "FROM tagged_images,tags as T "
+                                  "WHERE tagged_images.imgid in (select imgid from selected_images)"
+                                  "  AND T.id = tagged_images.tagid AND NOT T.name LIKE \"darktable|%\" ORDER BY T.name", -1, &stmt, NULL);
+    else
+      DT_DEBUG_SQLITE3_PREPARE_V2(dt_database_get(darktable.db),
+                                  "SELECT DISTINCT T.id, T.name "
+                                  "FROM tagged_images,tags as T "
+                                  "WHERE tagged_images.imgid in (select imgid from selected_images)"
+                                  "  AND T.id = tagged_images.tagid ORDER BY T.name", -1, &stmt, NULL);
+
   }
 
   // Create result
@@ -304,82 +312,65 @@ uint32_t dt_tag_get_attached(gint imgid,GList **result)
   return count;
 }
 
-gchar* dt_tag_get_list(gint imgid, const gchar *separator)
+GList* dt_tag_get_list(gint imgid)
 {
-  gchar *result = NULL;
   GList *taglist = NULL;
-  GList *tags = NULL;
-  dt_tag_t *t;
-  gchar *value = NULL;
-  gchar **pch;
+  GList *tags    = NULL;
 
-  int count = dt_tag_get_attached (imgid, &taglist);
+  uint32_t count = dt_tag_get_attached(imgid, &taglist, TRUE);
 
-  if (count < 1)
+  if(count < 1)
     return NULL;
 
-  for (guint i = 0; i < g_list_length(taglist); i++)
+  while(taglist)
   {
+    dt_tag_t *t = (dt_tag_t*)taglist->data;
+    gchar *value = t->tag;
 
-    t = g_list_nth_data (taglist, i);
-    value = g_strdup(t->tag);
-    if (g_strrstr(value, "|") && !g_str_has_prefix(value, "darktable|"))
+    size_t j = 0;
+    gchar **pch = g_strsplit(value, "|", -1);
+
+    if(pch != NULL)
     {
-      size_t j = 0;
-      pch = g_strsplit(value, "|", -1);
-
-      if (pch != NULL)
+      while(pch[j] != NULL)
       {
-        while (pch[j] != NULL)
-        {
-          tags = g_list_prepend(tags, g_strdup(pch[j]));
-          j++;
-        }
-        g_strfreev(pch);
+        tags = g_list_prepend(tags, g_strdup(pch[j]));
+        j++;
       }
+      g_strfreev(pch);
     }
-    else if (!g_str_has_prefix(value, "darktable|"))
-      tags = g_list_prepend(tags, g_strdup(value));
-    g_free (t);
-    g_free (value);
+
+    taglist = g_list_next(taglist);
   }
 
-  g_list_free (taglist);
+  g_list_free_full(taglist, g_free);
 
-  result = dt_util_glist_to_str (separator, tags, g_list_length(tags));
-
-  return result;
+  return dt_util_glist_uniq(tags);
 }
 
-gchar *dt_tag_get_hierarchical(gint imgid, const gchar *separator)
+GList *dt_tag_get_hierarchical(gint imgid)
 {
   GList *taglist = NULL;
-  GList *tags = NULL;
-  gchar *result = NULL;
+  GList *tags    = NULL;
 
-  int count = dt_tag_get_attached (imgid, &taglist);
+  int count = dt_tag_get_attached(imgid, &taglist, TRUE);
 
-  if (count < 1)
+  if(count < 1)
     return NULL;
 
-  for (guint i=0; i<g_list_length(taglist); i++)
+  while(taglist)
   {
-    dt_tag_t *t;
-    gchar *value = NULL;
+    dt_tag_t *t = (dt_tag_t*)taglist->data;
 
-    t = g_list_nth_data (taglist, i);
-    value = g_strdup(t->tag);
+    tags = g_list_prepend(tags, t->tag);
 
-    /* return all tags, but omit the internal darktable ones: */
-    if (!g_str_has_prefix(value, "darktable|"))
-      tags = g_list_prepend(tags, value);
-
-    g_free (t);
+    taglist = g_list_next(taglist);
   }
 
-  result = dt_util_glist_to_str (separator, tags, g_list_length(tags));
+  g_list_free_full(taglist, g_free);
 
-  return result;
+  tags = g_list_reverse(tags);
+  return tags;
 }
 
 /*

src/common/tags.h
@@ -58,14 +58,16 @@ void dt_tag_detach(guint tagid,gint imgid);
 /** detach tags from images that matches name, it is valid to use % to match tag */
 void dt_tag_detach_by_string(const char *name, gint imgid);
 
-/** retrieves a list of tags of specified imgid \param[out] result a list of dt_tag_t. */
-uint32_t dt_tag_get_attached(gint imgid,GList **result);
+/** retrieves a list of tags of specified imgid \param[out] result a list of dt_tag_t, sorted by tag. */
+uint32_t dt_tag_get_attached(gint imgid,GList **result, gboolean ignore_dt_tags);
 
-/** get a string of tags, all serialized, and separated with separator */
-gchar* dt_tag_get_list(gint imgid, const gchar *separator);
+/** get a list of tags, call dt_util_glist_to_str() to make it into a string.
+ *  the difference to dt_tag_get_attached() is that this one splits at '|' and filters out the "darktable|" tags. */
+GList* dt_tag_get_list(gint imgid);
 
-/** get a string of only hierarchical tags, all serialized, and separated with separator */
-gchar *dt_tag_get_hierarchical(gint imgid, const gchar *separator);
+/** get a flat list of only hierarchical tags, call dt_util_glist_to_str() to make it into a string.
+ *  the difference to dt_tag_get_attached() is that this one filters out the "darktable|" tags. */
+GList *dt_tag_get_hierarchical(gint imgid);
 
 /** retrieves a list of suggested tags matching keyword. \param[in] keyword the keyword to search \param[out] result a pointer to list populated with result. \return the count \note the limit of result is decided by conf value "xxx" */
 uint32_t dt_tag_get_suggestions(const gchar *keyword, GList **result);

src/common/utility.c
@@ -108,16 +108,17 @@ gchar* dt_util_str_replace(const gchar* string, const gchar* pattern, const gcha
   return nstring;
 }
 
-gchar* dt_util_glist_to_str(const gchar* separator, GList * items, const unsigned int count)
+gchar* dt_util_glist_to_str(const gchar* separator, GList * items)
 {
-  if(count == 0)
+  if(items == NULL)
     return NULL;
 
+  const unsigned int count = g_list_length(items);
   gchar *result = NULL;
 
   // add the entries to an char* array
   items = g_list_first(items);
-  gchar** strings = g_malloc_n(count+1, sizeof(gchar *));
+  gchar** strings = g_malloc0_n(count+1, sizeof(gchar *));
   if(items != NULL)
   {
     int i = 0;
@@ -126,27 +127,45 @@ gchar* dt_util_glist_to_str(const gchar* separator, GList * items, const unsigne
       strings[i++] = items->data;
     }
     while((items=g_list_next(items)) != NULL);
-    strings[i] = NULL;
   }
 
   // join them into a single string
   result = g_strjoinv(separator, strings);
 
-  // free the GList and the array
-  items = g_list_first(items);
-  if(items != NULL)
-  {
-    do
-    {
-      g_free(items->data);
-    }
-    while((items=g_list_next(items)) != NULL);
-  }
-  g_list_free(items);
+  // free the array
   g_free(strings);
 
   return result;
 }
+
+GList* dt_util_glist_uniq(GList * items)
+{
+  if(!items) return NULL;
+
+  gchar *last = NULL;
+  GList *last_item = NULL;
+
+  items = g_list_sort(items, (GCompareFunc)g_strcmp0);
+  GList *iter = items;
+  while(iter)
+  {
+    gchar *value = (gchar*)iter->data;
+    if(!g_strcmp0(last, value))
+    {
+      g_free(value);
+      items = g_list_delete_link(items, iter);
+      iter = last_item;
+    }
+    else
+    {
+      last = value;
+      last_item = iter;
+    }
+    iter = g_list_next(iter);
+  }
+  return items;
+}
+
 
 gchar* dt_util_fix_path(const gchar* path)
 {

src/common/utility.h
@@ -29,8 +29,10 @@ gchar *dt_util_dstrcat(gchar *str,const gchar *format, ... );
 gchar* dt_util_str_replace(const gchar* string, const gchar* pattern, const gchar* substitute);
 /** count the number of occurrences of needle in haystack */
 guint dt_util_str_occurence(const gchar *haystack,const gchar *needle);
-/** generate a string from the elements of the list, separated by separator. the list is freed, the result has to be freed. */
-gchar* dt_util_glist_to_str(const gchar* separator, GList * items, const unsigned int count);
+/** generate a string from the elements of the list, separated by separator. the result has to be freed. */
+gchar* dt_util_glist_to_str(const gchar* separator, GList * items);
+/** take a list of strings and remove all duplicates. the result will be sorted. */
+GList* dt_util_glist_uniq(GList * items);
 /** fixes the given path by replacing a possible tilde with the correct home directory */
 gchar* dt_util_fix_path(const gchar* path);
 size_t dt_utf8_strlcpy(char *dest, const char *src, size_t n);

commit db519b5d22b6c1579cc523c9777e7bd768fcb2a9

Fix crash on export to filename with $(LABELS) GETTEXT(3): The resulting string is statically allocated and must not be modified or freed.
Was broken by c0a900978328d7812048ce071cc31cd5b94d2f4e
Bad commit was found using git bisect.

Thanks to Stéphane Gourichon for initial bugreport (on IRC)

src/common/variables.c
@@ -214,7 +214,7 @@ gboolean _variable_get_value(dt_variables_params_t *params, gchar *variable,gcha
       }
       while((res=g_list_next(res)) != NULL);
       char* str = dt_util_glist_to_str(",", labels);
-      g_list_free_full(labels, g_free);
+      g_list_free(labels);
       snprintf(value, value_len,  "%s", str);
       g_free(str);
     }