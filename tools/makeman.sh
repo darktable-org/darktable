68143108796a87658de4269bbdf8f45b5db2d7f2
Tobias Ellinghaus <me@houz.org>
2014-11-25 22:25:37 +0100

    Some more quotes, maybe now it runs through?

--- tools/makeman.sh
+++ tools/makeman.sh
@@ -1,13 +1,13 @@
-input=$1
-config=$2
-authors=$3
-output=$4
-version=$(cat $config | grep PACKAGE_VERSION | head -1 | cut -f 3 -d" " | tr -d "\"")
+input="$1"
+config="$2"
+authors="$3"
+output="$4"
+version=$(cat "$config" | grep PACKAGE_VERSION | head -1 | cut -f 3 -d" " | tr -d "\"")
 set -e; \
-  d=`sed -n 's,/,-,g;s,.*\$$[D]ate: \(..........\).*,\1,p' $input`; \
-  pod2man $input \
+  d=`sed -n 's,/,-,g;s,.*\$$[D]ate: \(..........\).*,\1,p' "$input"`; \
+  pod2man "$input" \
   | sed 's/^\.TH .*/.TH DARKTABLE 1 "'"$$d"'" "darktable-'$version'" "darktable"/' \
   | sed -e '/.*DREGGNAUTHORS.*/r '"$authors" | sed -e '/.*DREGGNAUTHORS.*/d' \
   > tmp.$$$$ \
-  && mv -f tmp.$$$$ $output
+  && mv -f tmp.$$$$ "$output"
 
