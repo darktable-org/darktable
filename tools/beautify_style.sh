8e572dea906ac4ea2413e2098e966efa7a65edb9
Tobias Ellinghaus <me@houz.org>
2014-12-03 15:40:19 +0100

    Ignore some more files when formatting code

--- tools/beautify_style.sh
+++ tools/beautify_style.sh
@@ -1,6 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 # this uses clang-format to standardize our indentation/braces etc
 
-find src | grep -v "^src/external" | egrep "\.h$|\.hh$|\.c$|\.cc$" | xargs clang-format-3.6 -i
+# change if your executable is named different
+CLANG_FORMAT=clang-format-3.6
 
+
+# add all the files and directories that may not be reformatted, relative to src/
+IGNORE_SET=(
+    external
+    iop/wb_presets.c
+    common/noiseprofiles.h
+    common/colormatrices.c
+)
+
+####################################################################################
+
+function join { local IFS="$1"; shift; echo "$*"; }
+
+IGNORE_SET=(${IGNORE_SET[@]/#/^src/})
+IGNORE_STRING=$(join \| "${IGNORE_SET[@]}")
+
+SOURCES=$(find src | egrep -v ${IGNORE_STRING} | egrep "\.h$|\.hh$|\.c$|\.cc$")
+
+for FILE in $SOURCES
+do
+  ${CLANG_FORMAT} -i $FILE
+done
