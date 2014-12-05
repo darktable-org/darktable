2396fcdc9cf699105ccf49565f4b902f3ee63b9a
Serhii Marchuk <serhii.marchuk@gmail.com>
2014-12-05 17:47:07 +0200

    Remove unnesesary define

--- src/external/LibRaw/internal/dcraw_common.cpp
+++ src/external/LibRaw/internal/dcraw_common.cpp
@@ -21,7 +21,6 @@ it under the terms of the one of three licenses as you choose:
    for more information
 */
 
-#define _GNU_SOURCE
 #include <math.h>
 #define CLASS LibRaw::
 #include "libraw/libraw_types.h"
