darktable-org/darktable

commit 0a00f6126a95984b959c19f5e4a1ecc89c8e4fed

Remove thumbnail_width/height settings

src/cli/main.c
@@ -71,7 +71,7 @@ static void generate_thumbnail_cache()
 
   // go through all images:
   DT_DEBUG_SQLITE3_PREPARE_V2(dt_database_get(darktable.db), "select id from images", -1, &stmt, 0);
-  uint8_t *tmp = (uint8_t *)dt_alloc_align(16, darktable.thumbnail_width*darktable.thumbnail_height*4);
+  uint8_t *tmp = (uint8_t *)dt_alloc_align(16, 1280*720*4);
   const int cache_quality = MIN(100, MAX(10, dt_conf_get_int("database_cache_quality")));
   while(sqlite3_step(stmt) == SQLITE_ROW)
   {

data/darktableconfig.xml.in
@@ -491,20 +491,6 @@
     <longdescription>if set to true, thumbnails will be processed by first downscaling rather than demosaicing the full image. this can result in much faster processing times and blurrier images, especially when you cropped a lot.</longdescription>
   </dtconfig>
   <dtconfig prefs="gui">
-    <name>plugins/lighttable/thumbnail_width</name>
-    <type>int</type>
-    <default>1300</default>
-    <shortdescription>maximum width of image drawing area</shortdescription>
-    <longdescription>maximum width of the image drawing area in darkroom mode. adjust to your screen\n(needs a restart and will invalidate current thumbnail caches).</longdescription>
-  </dtconfig>
-  <dtconfig prefs="gui">
-    <name>plugins/lighttable/thumbnail_height</name>
-    <type>int</type>
-    <default>1000</default>
-    <shortdescription>maximum height of image drawing area</shortdescription>
-    <longdescription>maximum height of the image drawing area in darkroom mode. adjust to your screen\n(needs a restart and will invalidate current thumbnail caches).</longdescription>
-  </dtconfig>
-  <dtconfig prefs="gui">
     <name>pressure_sensitivity</name>
     <type>
       <enum>

src/common/darktable.c
@@ -733,15 +733,6 @@ int dt_init(int argc, char *argv[], const int init_gui, lua_State *L)
   // Initialize the filesystem watcher
   darktable.fswatch = dt_fswatch_new();
 
-  // get max lighttable thumbnail size:
-  darktable.thumbnail_width = CLAMPS(dt_conf_get_int("plugins/lighttable/thumbnail_width"), 200, 3000);
-  darktable.thumbnail_height = CLAMPS(dt_conf_get_int("plugins/lighttable/thumbnail_height"), 200, 3000);
-  // and make sure it can be mip-mapped all the way from mip4 to mip0
-  darktable.thumbnail_width /= 16;
-  darktable.thumbnail_width *= 16;
-  darktable.thumbnail_height /= 16;
-  darktable.thumbnail_height *= 16;
-
   // FIXME: move there into dt_database_t
   dt_pthread_mutex_init(&(darktable.db_insert), NULL);
   dt_pthread_mutex_init(&(darktable.plugin_threadsafe), NULL);
@@ -1065,8 +1056,6 @@ void dt_configure_defaults()
     fprintf(stderr, "[defaults] setting high quality defaults\n");
     dt_conf_set_int("worker_threads", 8);
     dt_conf_set_int64("cache_memory", 1u << 30);
-    dt_conf_set_int("plugins/lighttable/thumbnail_width", 1300);
-    dt_conf_set_int("plugins/lighttable/thumbnail_height", 1000);
     dt_conf_set_bool("plugins/lighttable/low_quality_thumbnails", FALSE);
   }
   if(mem < (1u << 20) || threads <= 2 || bits < 64 || atom_cores > 0)
@@ -1076,8 +1065,6 @@ void dt_configure_defaults()
     dt_conf_set_int64("cache_memory", 200u << 20);
     dt_conf_set_int("host_memory_limit", 500);
     dt_conf_set_int("singlebuffer_limit", 8);
-    dt_conf_set_int("plugins/lighttable/thumbnail_width", 800);
-    dt_conf_set_int("plugins/lighttable/thumbnail_height", 500);
     dt_conf_set_string("plugins/darkroom/demosaic/quality", "always bilinear (fast)");
     dt_conf_set_bool("plugins/lighttable/low_quality_thumbnails", TRUE);
   }

src/common/darktable.h
@@ -171,7 +171,6 @@ typedef struct darktable_t
   uint32_t cpu_flags;
   int32_t num_openmp_threads;
 
-  int32_t thumbnail_width, thumbnail_height;
   int32_t unmuted;
   GList *iop;
   GList *collection_listeners;

src/common/focus.h
@@ -262,8 +262,7 @@ void dt_focus_draw_clusters(cairo_t *cr, int width, int height, int imgid, int b
     dt_dev_cleanup(&dev);
   }
 
-  const float scale = fminf(fminf(darktable.thumbnail_width, width) / (float)wd,
-                            fminf(darktable.thumbnail_height, height) / (float)ht);
+  const float scale = fminf(width / (float)wd, height / (float)ht);
   cairo_scale(cr, scale, scale);
 
   cairo_translate(cr, -wd / 2.0f, -ht / 2.0f);

src/develop/pixelpipe_hb.c
@@ -102,16 +102,18 @@ int dt_dev_pixelpipe_init_dummy(dt_dev_pixelpipe_t *pipe, int32_t width, int32_t
 
 int dt_dev_pixelpipe_init_preview(dt_dev_pixelpipe_t *pipe)
 {
+  // Use 720p as a decent initial value
   int res = dt_dev_pixelpipe_init_cached(
-      pipe, 4 * sizeof(float) * darktable.thumbnail_width * darktable.thumbnail_height, 5);
+      pipe, 4 * sizeof(float) * 1280 * 720, 5);
   pipe->type = DT_DEV_PIXELPIPE_PREVIEW;
   return res;
 }
 
 int dt_dev_pixelpipe_init(dt_dev_pixelpipe_t *pipe)
 {
+  // Use 720p as a decent initial value
   int res = dt_dev_pixelpipe_init_cached(
-      pipe, 4 * sizeof(float) * darktable.thumbnail_width * darktable.thumbnail_height, 5);
+      pipe, 4 * sizeof(float) * 1280 * 720, 5);
   pipe->type = DT_DEV_PIXELPIPE_FULL;
   return res;
 }

src/views/tethering.c
@@ -208,20 +208,12 @@ static void _expose_tethered_mode(dt_view_t *self, cairo_t *cr, int32_t width, i
 }
 
 
-void expose(dt_view_t *self, cairo_t *cri, int32_t width_i, int32_t height_i, int32_t pointerx,
+void expose(dt_view_t *self, cairo_t *cri, int32_t width, int32_t height, int32_t pointerx,
             int32_t pointery)
 {
-  const int32_t capwd = darktable.thumbnail_width;
-  const int32_t capht = darktable.thumbnail_height;
-  int32_t width = MIN(width_i, capwd);
-  int32_t height = MIN(height_i, capht);
-
   cairo_set_source_rgb(cri, .2, .2, .2);
-  cairo_rectangle(cri, 0, 0, width_i, height_i);
+  cairo_rectangle(cri, 0, 0, width, height);
   cairo_fill(cri);
-
-  if(width_i > capwd) cairo_translate(cri, -(capwd - width_i) * .5f, 0.0f);
-  if(height_i > capht) cairo_translate(cri, 0.0f, -(capht - height_i) * .5f);
 
   // Expose tethering center view
   cairo_save(cri);

src/views/view.c
@@ -840,8 +840,7 @@ void dt_view_image_expose(dt_view_image_over_t *image_over, uint32_t imgid, cair
                                                   stride);
     if(zoom == 1)
     {
-      scale = fminf(fminf(darktable.thumbnail_width, width) / (float)buf.width,
-                    fminf(darktable.thumbnail_height, height) / (float)buf.height);
+      scale = fminf(width / (float)buf.width, height / (float)buf.height);
     }
     else
       scale = fminf(width * imgwd / (float)buf.width, height * imgwd / (float)buf.height);

commit 5c1f427fe06a0c3d368f56da016a2977286138ca

cli: fix buffer allocation bug
introduced in 0a00f6126a95984b959c19f5e4a1ecc89c8e4fed

src/cli/main.c
@@ -71,7 +71,8 @@ static void generate_thumbnail_cache()
 
   // go through all images:
   DT_DEBUG_SQLITE3_PREPARE_V2(dt_database_get(darktable.db), "select id from images", -1, &stmt, 0);
-  uint8_t *tmp = (uint8_t *)dt_alloc_align(16, 1280*720*4);
+  // could only alloc max_mip-1, but would need to detect the special case that max==0.
+  uint8_t *tmp = (uint8_t *)dt_alloc_align(16, 4*darktable.mipmap_cache->max_width[max_mip]*darktable.mipmap_cache->max_height[max_mip]);
   if(!tmp)
   {
     fprintf(stderr, "couldn't allocate temporary memory!\n");