darktable-org/darktable

commit e5d487183ffc553edc894c0a338ae28f4bdeac36

CLI: prevent XMP file overriding

src/cli/main.c
@@ -140,6 +140,8 @@ int main(int argc, char *arg[])
   m_arg[m_argc++] = "darktable-cli";
   m_arg[m_argc++] = "--library";
   m_arg[m_argc++] = ":memory:";
+  m_arg[m_argc++] = "--conf";
+  m_arg[m_argc++] = "write_sidecar_files=FALSE";
   for(; k < argc; k++) m_arg[m_argc++] = arg[k];
   m_arg[m_argc] = NULL;
 

commit 5b4e2ce77c4057119950329c8628a2819bf43852

Fix clang scan-build issues: Out-of-bound array access
Introduced in e5d487183ffc553edc894c0a338ae28f4bdeac36

src/cli/main.c
@@ -235,7 +235,7 @@ int main(int argc, char *arg[])
   }
 
   int m_argc = 0;
-  char *m_arg[4 + argc - k];
+  char *m_arg[5 + argc - k];
   m_arg[m_argc++] = "darktable-cli";
   m_arg[m_argc++] = "--library";
   m_arg[m_argc++] = ":memory:";