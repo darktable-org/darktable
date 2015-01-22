8adb000b17604b7d253cbe863b2b7351cb28dfac
johannes hanika <hanatos@gmail.com>
2015-01-23 05:59:38 +1300

    pfm: silence exiv2 warnings.

--- src/common/exif.cc
+++ src/common/exif.cc
@@ -967,6 +967,8 @@ int dt_exif_write_blob(uint8_t *blob, uint32_t size, const char *path)
   }
   catch(Exiv2::AnyError &e)
   {
+    const char *c = path + strlen(path) - 4;
+    if(c >= path && !strcmp(c, ".pfm")) return 0;
     std::string s(e.what());
     std::cerr << "[exiv2] " << s << std::endl;
     return 0;
@@ -1218,6 +1220,8 @@ int dt_exif_read_blob(uint8_t *buf, const char *path, const int imgid, const int
   catch(Exiv2::AnyError &e)
   {
     // std::cerr.rdbuf(savecerr);
+    const char *c = path + strlen(path) - 4;
+    if(c >= path && !strcmp(c, ".pfm")) return 1;
     std::string s(e.what());
     std::cerr << "[exiv2] " << s << std::endl;
     return 0;
