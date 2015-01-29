dc9f1725662ec23da683ced78fa7e56245a71cd0
Roman Lebedev <lebedev.ri@gmail.com>
2015-01-29 21:56:35 +0300

    RawSpeed: CrwDecoder: WB: support some EOS cameras
    NOTE: WB coeffs are slightly different from those from dcraw/libraw
    It seems, they do not average 2 green levels, but use the first one exclusively.

--- src/external/rawspeed/RawSpeed/CiffTag.h
+++ src/external/rawspeed/RawSpeed/CiffTag.h
@@ -29,6 +29,8 @@ namespace RawSpeed {
 typedef enum {
   CIFF_NULL         = 0x0000,
   CIFF_MAKEMODEL    = 0x080a,
+  CIFF_SHOTINFO     = 0x102a,
+  CIFF_WHITEBALANCE = 0x10a9,
   CIFF_SENSORINFO   = 0x1031,
   CIFF_IMAGEINFO    = 0x1810,
   CIFF_DECODERTABLE = 0x1835,
