62f562101e03db2939e953080b5ef638f5649fc5
JohnnyRun <giannicarabelli@gmail.com>
2015-02-13 09:13:38 +0100

    up/down fix on iir=1 + shift fix on iir=1

--- src/views/lighttable.c
+++ src/views/lighttable.c
@@ -2006,8 +2006,12 @@ int key_pressed(dt_view_t *self, guint key, guint state)
     else if(layout == 1)
     {
       if (zoom == 1)
+      {
         move_view(lib, UP);
-      else {
+        lib->using_arrows = 0;
+      }
+      else
+      {
         lib->using_arrows = 1;
         lib->key_jump_offset = -1;
         lib->track = -1;
@@ -2026,8 +2030,12 @@ int key_pressed(dt_view_t *self, guint key, guint state)
     else if(layout == 1)
     {
       if (zoom == 1)
+      {
         move_view(lib, DOWN);
-      else  {
+        lib->using_arrows = 0;
+      }
+      else
+      {
         lib->using_arrows = 1;
         lib->key_jump_offset = 1;
         lib->track = -1;
@@ -2044,9 +2052,15 @@ int key_pressed(dt_view_t *self, guint key, guint state)
       lib->track = -DT_LIBRARY_MAX_ZOOM;
     else if(layout == 1)
     {
-      lib->using_arrows = 1;
-      lib->key_jump_offset = zoom*-1;
-      //move_view(lib, UP);
+      if (zoom == 1)
+      {
+        move_view(lib, UP);
+        lib->using_arrows = 0;
+      }
+      else {
+        lib->using_arrows = 1;
+        lib->key_jump_offset = zoom*-1;
+      }
     }
     else
       lib->track = -DT_LIBRARY_MAX_ZOOM;
@@ -2059,9 +2073,16 @@ int key_pressed(dt_view_t *self, guint key, guint state)
       lib->track = +DT_LIBRARY_MAX_ZOOM;
     else if(layout == 1) 
     {
-      lib->using_arrows = 1;
-      lib->key_jump_offset = zoom;
-      //move_view(lib, DOWN);
+      if (zoom == 1)
+      {
+        move_view(lib, DOWN);
+        lib->using_arrows = 0;
+      }
+      else
+      {
+        lib->using_arrows = 1;
+        lib->key_jump_offset = zoom;
+      }
     }
     else
       lib->track = DT_LIBRARY_MAX_ZOOM;
