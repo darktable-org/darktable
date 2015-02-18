275dd5ad7381a38eda58009b2288542ee734d80b
Pascal Obry <pascal@obry.net>
2015-02-18 22:38:59 +0100

    Properly disconnect _print_settings_filmstrip_activate_callback from signal.
    Fixes #10332.

--- src/libs/print_settings.c
+++ src/libs/print_settings.c
@@ -1563,6 +1563,10 @@ gui_cleanup (dt_lib_module_t *self)
 {
   dt_lib_print_settings_t *ps = (dt_lib_print_settings_t *)self->data;
 
+  dt_control_signal_disconnect(darktable.signals,
+                               G_CALLBACK(_print_settings_filmstrip_activate_callback),
+                               self);
+
   g_list_free_full(ps->profiles, g_free);
   g_list_free_full(ps->paper_list, g_free);
   free(self->data);
