fd495f06cdd8a8b1bd62f97a6c6b5b6fb633fec6
Tobias Ellinghaus <me@houz.org>
2015-06-17 12:17:19 +0200

    Correctly set history_end when copying history
    This fixes #10520.

--- src/common/history.c
+++ src/common/history.c
@@ -265,7 +265,7 @@ int dt_history_copy_and_paste_on_image(int32_t imgid, int32_t dest_imgid, gboole
     sqlite3_finalize(stmt);
 
     DT_DEBUG_SQLITE3_PREPARE_V2(dt_database_get(darktable.db),
-                                "SELECT IFNULL(MAX(num), -1) FROM history WHERE imgid = ?1", -1, &stmt, NULL);
+                                "SELECT IFNULL(MAX(num), -1)+1 FROM history WHERE imgid = ?1", -1, &stmt, NULL);
     DT_DEBUG_SQLITE3_BIND_INT(stmt, 1, dest_imgid);
     if(sqlite3_step(stmt) == SQLITE_ROW) offs = sqlite3_column_int(stmt, 0);
   }
@@ -319,11 +319,12 @@ int dt_history_copy_and_paste_on_image(int32_t imgid, int32_t dest_imgid, gboole
   sqlite3_finalize(stmt);
 
   /* copy the history items into the history of the dest image */
+  /* note: rowid starts at 1 while num has to start at 0! */
   DT_DEBUG_SQLITE3_PREPARE_V2(dt_database_get(darktable.db),
                               "INSERT INTO history "
                               "(imgid,num,module,operation,op_params,enabled,blendop_params,blendop_"
                               "version,multi_priority,multi_name) SELECT "
-                              "?1,?2+rowid,module,operation,op_params,enabled,blendop_params,blendop_"
+                              "?1,?2+rowid-1,module,operation,op_params,enabled,blendop_params,blendop_"
                               "version,multi_priority,multi_name FROM MEMORY.style_items",
                               -1, &stmt, NULL);
   DT_DEBUG_SQLITE3_BIND_INT(stmt, 1, dest_imgid);
