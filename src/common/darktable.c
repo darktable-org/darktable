4be3e08ff55e1f398c2c4caf20e483d8a2e80c94
Bruce Guenter <bruce@untroubled.org>
2015-07-11 11:06:00 -0600

    Add printf format attribute to dprintf in darktable.c

--- src/common/darktable.c
+++ src/common/darktable.c
@@ -141,7 +141,7 @@ static dt_signal_handler_t *_dt_sigsegv_old_handler = NULL;
 
 #if(defined(__FreeBSD_version) && (__FreeBSD_version < 800071)) || (defined(OpenBSD) && (OpenBSD < 201305))  \
     || defined(__SUNOS__)
-static int dprintf(int fd, const char *fmt, ...)
+static int dprintf(int fd, const char *fmt, ...) __attribute__((format(printf, 2, 3)))
 {
   va_list ap;
   FILE *f = fdopen(fd, "a");
