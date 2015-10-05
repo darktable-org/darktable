8fdf5e7b458986ede9e2188fe04f297952f0bb5a
Tobias Ellinghaus <me@houz.org>
2015-10-05 22:29:35 +0200

    Update --version of dt-cli
    (cherry picked from commit 0828093eac04763df83152215253ee80e0d768a4)

--- src/cli/main.c
+++ src/cli/main.c
@@ -25,6 +25,7 @@
  *  - profit
  */
 
+#include "version.h"
 #include "common/darktable.h"
 #include "common/debug.h"
 #include "common/collection.h"
@@ -77,7 +78,7 @@ int main(int argc, char *arg[])
       }
       else if(!strcmp(arg[k], "--version"))
       {
-        printf("this is darktable-cli\ncopyright (c) 2012-2014 johannes hanika, tobias ellinghaus\n");
+        printf("this is darktable-cli " PACKAGE_VERSION  "\ncopyright (c) 2012-2015 johannes hanika, tobias ellinghaus\n");
         exit(1);
       }
       else if(!strcmp(arg[k], "--width"))
