206d149e08349441c371ec749ca922d68f54b509
Pedro Côrte-Real <pedro@pedrocr.net>
2015-12-12 23:12:51 +0000

    tools: update main include file

--- tools/regression_tests/src/lib/all.rb
+++
@@ -1,4 +0,0 @@
-require File.expand_path "common.rb", File.dirname(__FILE__)
-require File.expand_path "exporter.rb", File.dirname(__FILE__)
-require File.expand_path "runner.rb", File.dirname(__FILE__)
-require File.expand_path "../web/builder.rb", File.dirname(__FILE__)
