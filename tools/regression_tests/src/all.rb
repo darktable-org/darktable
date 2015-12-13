42aa52601f257bdff6ab238ba1ba22b38fc97ad7
Pedro Côrte-Real <pedro@pedrocr.net>
2015-12-13 01:04:49 +0000

    tools: make fetching samples a manual step
    So as to not hit rawsamples.ch too hard make downloading
    samples a manual step. To do a first run you now need to do:
    
    $ ./fetch-samples
    $ ./compare darktable-1.6.x master
    
    After this first run fetch-samples only needs to be run
    whenever we want to fetch whatever is new in rawsamples.ch

--- tools/regression_tests/src/all.rb
+++ tools/regression_tests/src/all.rb
@@ -1,5 +1,4 @@
 require File.expand_path "lib/common.rb", File.dirname(__FILE__)
 require File.expand_path "lib/runner.rb", File.dirname(__FILE__)
 require File.expand_path "lib/build.rb", File.dirname(__FILE__)
-require File.expand_path "lib/samples.rb", File.dirname(__FILE__)
 require File.expand_path "web/builder.rb", File.dirname(__FILE__)
