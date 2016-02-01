b6934d0c1d7755336d653b8e32c2b288337d4ef3
Roman Lebedev <lebedev.ri@gmail.com>
2016-02-01 13:35:35 +0300

    Imageio rawspeed: fail to load CYGM/RGBE sensor images

--- src/common/imageio_rawspeed.cc
+++ src/common/imageio_rawspeed.cc
@@ -245,6 +245,11 @@ dt_imageio_retval_t dt_imageio_open_rawspeed(dt_image_t *img, const char *filena
 
     img->bpp = r->getBpp();
     img->filters = r->cfa.getDcrawFilter();
+    if(img->filters == 0xb4b4b4b4 || img->filters == 0x9c9c9c9c)
+    {
+      printf("[rawspeed] detected CYGM or RGBE sensor camera. Not supported in this version!\n");
+      return DT_IMAGEIO_FILE_CORRUPTED;
+    }
     if(img->filters)
     {
       img->flags &= ~DT_IMAGE_LDR;
