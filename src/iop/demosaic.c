046709d59ae324972a6ac7f6c6deba6facbfa305
Roman Lebedev <lebedev.ri@gmail.com>
2016-02-18 15:34:31 +0300

    demosaic: s/yoff/xoff/
    Fix typo which snuck into 8a5d0a8. This will clean up edges on CPU
    version of 3-pass Markesteijn demosaic.
    
    (cherry picked from commit 385801d7e6643790f3fddf437856e8c759b41dd2)

--- src/iop/demosaic.c
+++ src/iop/demosaic.c
@@ -543,7 +543,7 @@ static void xtrans_markesteijn_interpolate(float *out, const float *const in,
           else
           {
             // mirror a border pixel if beyond image edge
-            const int c = FCxtrans(row + yoff + 18, col + yoff + 18, xtrans);
+            const int c = FCxtrans(row + yoff + 18, col + xoff + 18, xtrans);
             for(int cc = 0; cc < 3; cc++)
               if(cc != c)
                 pix[cc] = 0.0f;
