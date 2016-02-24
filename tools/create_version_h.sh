91783ac002fffab6db8dfd026dab53d53a1dc0d7
Roman Lebedev <lebedev.ri@gmail.com>
2016-02-24 15:35:47 +0300

    create_version_h.sh: properly handle filenames with spaces
    $ ninja
    [1/1] Updating version string
    /home/lebedevri/darktable/tools/create_version_h.sh: 18: [: /home/lebedevri/darktable/odd: unexpected operator
    
    (cherry picked from commit 1b0616755aa247b35bc68b5fdd4657faec5b05db)

--- tools/create_version_h.sh
+++ tools/create_version_h.sh
@@ -15,13 +15,13 @@ if [ -n  "`echo -e $NEW_VERSION | grep  Format`" ]; then
 fi
 
 # version.h exists => check if it containts the up-to-date version
-if [ -f ${H_FILE} ]; then
-  OLD_VERSION=`sed 's/.*"\(.*\)".*/\1/' < ${H_FILE}`
+if [ -f "$H_FILE" ]; then
+  OLD_VERSION=`sed 's/.*"\(.*\)".*/\1/' < "$H_FILE"`
   if [ "${OLD_VERSION}" = "${NEW_VERSION}" ]; then
     VERSION_H_NEEDS_UPDATE=0
   fi
 fi
 
 if [ $VERSION_H_NEEDS_UPDATE -eq 1 ]; then
-  echo "#define PACKAGE_VERSION \"${NEW_VERSION}\"" > ${H_FILE}
+  echo "#define PACKAGE_VERSION \"${NEW_VERSION}\"" > "$H_FILE"
 fi
