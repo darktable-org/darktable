darktable-org/darktable

commit bc9b599f60cb14b29fd5ec280c93a61038a3f31a

Modulelist lib: get_params(): when realloc() fails, orig ptr is intact. do not leak it.

src/libs/modulelist.c
@@ -323,11 +323,16 @@ void *get_params(dt_lib_module_t *self, int *size)
     if(dt_iop_so_is_hidden(module) || (module->flags() & IOP_FLAGS_DEPRECATED)) continue;
     int op_len = strlen(module->op) + 1;
     int new_len = len + 1 + op_len;
-    params = realloc(params, new_len);
-    if(!params)
+    char *tmp = realloc(params, new_len);
+    if(!tmp)
     {
+      free(params);
       len = 0;
       break;
+    }
+    else
+    {
+      params = tmp;
     }
     memcpy(params + len, module->op, op_len);
     params[new_len - 1] = (char)module->state;

commit 028e9d5bb863571d86e174a7995d6e81e6e1e435

Modulelist lib: get_params(): oops, do not endorse use-after-free :)
I broke it in bc9b599f60cb14b29fd5ec280c93a61038a3f31a.

src/libs/modulelist.c
@@ -327,6 +327,7 @@ void *get_params(dt_lib_module_t *self, int *size)
     if(!tmp)
     {
       free(params);
+      params = NULL;
       len = 0;
       break;
     }