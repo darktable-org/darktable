840793ec4b2a00c88a570ea4f97013ed5da9f3cc
johannes hanika <hanika@kit.edu>
2016-03-17 11:06:11 +0100

    iop: run dependency script

--- src/iop/colortransfer.c
+++ src/iop/colortransfer.c
@@ -543,7 +543,7 @@ void init(dt_iop_module_t *module)
   module->params = calloc(1, sizeof(dt_iop_colortransfer_params_t));
   module->default_params = calloc(1, sizeof(dt_iop_colortransfer_params_t));
   module->default_enabled = 0;
-  module->priority = 450; // module order created by iop_dependencies.py, do not edit!
+  module->priority = 476; // module order created by iop_dependencies.py, do not edit!
   module->params_size = sizeof(dt_iop_colortransfer_params_t);
   module->gui_data = NULL;
   dt_iop_colortransfer_params_t tmp;
