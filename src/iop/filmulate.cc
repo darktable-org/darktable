811e09c918a1bf8a4f78786c2df9b03dc60d1cd7
CarVac <c.lo.to.da.down.lo@gmail.com>
2016-03-22 13:03:57 -0400

    filmulate.cc: add working color space selector

--- src/iop/filmulate.cc
+++ src/iop/filmulate.cc
@@ -38,7 +38,7 @@ extern "C"
 
 // this is the version of the modules parameters,
 // and includes version information about compile-time dt
-DT_MODULE_INTROSPECTION(1, dt_iop_filmulate_params_t)
+DT_MODULE_INTROSPECTION(2, dt_iop_filmulate_params_t)
 
 typedef struct dt_iop_filmulate_params_t
 {
@@ -50,6 +50,7 @@ typedef struct dt_iop_filmulate_params_t
   // to a minimum. if you have to change this struct, it will break
   // users data bases, and you should increment the version
   // of DT_MODULE(VERSION) above!
+  int color_space_size;
   float rolloff_boundary;
   float film_area;
   float layer_mix_const;
@@ -58,7 +59,7 @@ typedef struct dt_iop_filmulate_params_t
 
 typedef struct dt_iop_filmulate_gui_data_t
 {
-  GtkWidget *rolloff_boundary, *film_area, *drama, *overdrive;
+  GtkWidget *color_space_size, *rolloff_boundary, *film_area, *drama, *overdrive;
 } dt_iop_filmulate_gui_data_t;
 
 typedef struct dt_iop_filmulate_global_data_t
@@ -94,6 +95,7 @@ void commit_params(struct dt_iop_module_t *self, dt_iop_params_t *p1, dt_dev_pix
   dt_iop_filmulate_params_t *p = (dt_iop_filmulate_params_t *)p1;
   dt_iop_filmulate_params_t *d = (dt_iop_filmulate_params_t *)piece->data;
 
+  d->color_space_size = p->color_space_size;
   d->rolloff_boundary = p->rolloff_boundary*65535.0f;
   d->film_area = powf(p->film_area,2.0f);
   d->layer_mix_const = p->layer_mix_const/100.0f;
@@ -108,17 +110,6 @@ void modify_roi_out(struct dt_iop_module_t *self, struct dt_dev_pixelpipe_iop_t 
                     dt_iop_roi_t *roi_out, const dt_iop_roi_t *roi_in)
 {
   *roi_out = *roi_in;
-  cout << "modify roi out ==============================" << endl;
-  cout << "roi in x: " << roi_in->x << endl;
-  cout << "roi in y: " << roi_in->y << endl;
-  cout << "roi in width: " << roi_in->width << endl;
-  cout << "roi in height: " << roi_in->height << endl;
-  cout << "roi in scale: " << roi_in->scale << endl;
-  cout << "roi out x: " << roi_out->x << endl;
-  cout << "roi out y: " << roi_out->y << endl;
-  cout << "roi out width: " << roi_out->width << endl;
-  cout << "roi out height: " << roi_out->height << endl;
-  cout << "roi out scale: " << roi_out->scale << endl;
 }
 */
 
@@ -133,50 +124,41 @@ void modify_roi_out(struct dt_iop_module_t *self, struct dt_dev_pixelpipe_iop_t 
 void modify_roi_in(struct dt_iop_module_t *self, struct dt_dev_pixelpipe_iop_t *piece,
                    const dt_iop_roi_t *roi_out, dt_iop_roi_t *roi_in)
 {
-  /*
-  cout << "modify roi in ==============================" << endl;
-  cout << "roi in x: " << roi_in->x << endl;
-  cout << "roi in y: " << roi_in->y << endl;
-  cout << "roi in width: " << roi_in->width << endl;
-  cout << "roi in height: " << roi_in->height << endl;
-  cout << "roi in scale: " << roi_in->scale << endl;
-  cout << "roi out x: " << roi_out->x << endl;
-  cout << "roi out y: " << roi_out->y << endl;
-  cout << "roi out width: " << roi_out->width << endl;
-  cout << "roi out height: " << roi_out->height << endl;
-  cout << "roi out scale: " << roi_out->scale << endl;
-  */
-  //roi_in->x = roi_out->x;
-  //roi_in->y = roi_out->y;
-  //roi_in->width = roi_out->width;
-  //roi_in->height = roi_out->height;
   roi_in->scale = roi_out->scale;
 
   roi_in->x = 0;
   roi_in->y = 0;
-  //cout << "unrounded width: " << piece->buf_in.width * roi_out->scale << endl;
-  //cout << "unrounded height: " << piece->buf_in.height * roi_out->scale << endl;
   roi_in->width = round(piece->buf_in.width * roi_out->scale);
   roi_in->height = round(piece->buf_in.height * roi_out->scale);
-  //roi_in->width = floor(piece->buf_in.width * roi_out->scale);
-  //roi_in->height = floor(piece->buf_in.height * roi_out->scale);
-  //cout << "roi in x: " << roi_in->x << endl;
-  //cout << "roi in y: " << roi_in->y << endl;
-  //cout << "roi in width: " << roi_in->width << endl;
-  //cout << "roi in height: " << roi_in->height << endl;
-  //cout << "roi in scale: " << roi_in->scale << endl;
-  //*roi_in = *roi_out;
 }
 
 /** process, all real work is done here. */
 void process(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, const void *const i, void *const o,
              const dt_iop_roi_t *const roi_in, const dt_iop_roi_t *const roi_out)
 {
+  // Get the data struct.
+  dt_iop_filmulate_params_t *d = (dt_iop_filmulate_params_t *)piece->data;
+  const int color_space_size = d->color_space_size;
+  const float rolloff_boundary = d->rolloff_boundary;
+  const float film_area = d->film_area;
+  const float layer_mix_const = d->layer_mix_const;
+  const int agitate_count = d->agitate_count;
   dt_iop_color_intent_t intent = DT_INTENT_PERCEPTUAL;
+
   const cmsHPROFILE Lab = dt_colorspaces_get_profile(DT_COLORSPACE_LAB, "", DT_PROFILE_DIRECTION_ANY)->profile;
   const cmsHPROFILE Rec2020 = dt_colorspaces_get_profile(DT_COLORSPACE_LIN_REC2020, "", DT_PROFILE_DIRECTION_ANY)->profile;
-  cmsHTRANSFORM transform_lab_to_lin_rec2020 = cmsCreateTransform(Lab, TYPE_LabA_FLT, Rec2020, TYPE_RGBA_FLT, intent, 0);
-  cmsHTRANSFORM transform_lin_rec2020_to_lab = cmsCreateTransform(Rec2020, TYPE_RGBA_FLT, Lab, TYPE_LabA_FLT, intent, 0);
+  const cmsHPROFILE Rec709 = dt_colorspaces_get_profile(DT_COLORSPACE_LIN_REC709, "", DT_PROFILE_DIRECTION_ANY)->profile;
+  cmsHTRANSFORM transform_lab_to_lin_rgba, transform_lin_rgba_to_lab;
+  if(0 == color_space_size)
+  {
+    transform_lab_to_lin_rgba = cmsCreateTransform(Lab, TYPE_LabA_FLT, Rec2020, TYPE_RGBA_FLT, intent, 0);
+    transform_lin_rgba_to_lab = cmsCreateTransform(Rec2020, TYPE_RGBA_FLT, Lab, TYPE_LabA_FLT, intent, 0);
+  }
+  else
+  {
+    transform_lab_to_lin_rgba = cmsCreateTransform(Lab, TYPE_LabA_FLT, Rec709, TYPE_RGBA_FLT, intent, 0);
+    transform_lin_rgba_to_lab = cmsCreateTransform(Rec709, TYPE_RGBA_FLT, Lab, TYPE_LabA_FLT, intent, 0);
+  }
 
   const int width_in = roi_in->width;
   const int height_in = roi_in->height;
@@ -191,22 +173,15 @@ void process(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, const 
 
   //Turn Lab into linear Rec2020
 #ifdef _OPENMP
-#pragma omp parallel for schedule(static) default(none) shared(rgbbufin, transform_lab_to_lin_rec2020)
+#pragma omp parallel for schedule(static) default(none) shared(rgbbufin, transform_lab_to_lin_rgba)
 #endif
   for(int y = 0; y < height_in; y++)
   {
     const float *in = (float*)i + y * width_in * 4;
     float *out = rgbbufin + y * width_in * 4;
-    cmsDoTransform(transform_lab_to_lin_rec2020, in, out, width_in);
+    cmsDoTransform(transform_lab_to_lin_rgba, in, out, width_in);
   }
 
-  // this is called for preview and full pipe separately, each with its own pixelpipe piece.
-  // Get the data struct.
-  dt_iop_filmulate_params_t *d = (dt_iop_filmulate_params_t *)piece->data;
-  float rolloff_boundary = d->rolloff_boundary;
-  float film_area = d->film_area;
-  float layer_mix_const = d->layer_mix_const;
-  int agitate_count = d->agitate_count;
 
   //Filmulate things!
   filmulate(rgbbufin, rgbbufout,
@@ -221,13 +196,13 @@ void process(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, const 
   free(rgbbufin);
   //Turn back to Lab
 #ifdef _OPENMP
-#pragma omp parallel for schedule(static) default(none) shared(rgbbufout, transform_lin_rec2020_to_lab)
+#pragma omp parallel for schedule(static) default(none) shared(rgbbufout, transform_lin_rgba_to_lab)
 #endif
   for(int y = 0; y < height_out; y++)
   {
     const float *in = rgbbufout + y * width_out * 4;
     float *out = (float*)o + y * width_out * 4;
-    cmsDoTransform(transform_lin_rec2020_to_lab, in, out, width_out);
+    cmsDoTransform(transform_lin_rgba_to_lab, in, out, width_out);
   }
   free(rgbbufout);
 }
@@ -256,7 +231,7 @@ void init(dt_iop_module_t *module)
   module->params_size = sizeof(dt_iop_filmulate_params_t);
   module->gui_data = NULL;
   // init defaults:
-  dt_iop_filmulate_params_t tmp = (dt_iop_filmulate_params_t){51275.0f/65535.0f, sqrtf(864.0f), 20.0f, 1};
+  dt_iop_filmulate_params_t tmp = (dt_iop_filmulate_params_t){0, 51275.0f/65535.0f, sqrtf(864.0f), 20.0f, 1};
 
   memcpy(module->params, &tmp, sizeof(dt_iop_filmulate_params_t));
   memcpy(module->default_params, &tmp, sizeof(dt_iop_filmulate_params_t));
@@ -271,6 +246,21 @@ void cleanup(dt_iop_module_t *module)
 }
 
 /** put your local callbacks here, be sure to make them static so they won't be visible outside this file! */
+static void color_space_size_callback(GtkWidget *w, dt_iop_module_t *self)
+{
+  //This is important to avoid cycles!
+  if(darktable.gui->reset)
+  {
+    return;
+  }
+  dt_iop_filmulate_params_t *p = (dt_iop_filmulate_params_t *)self->params;
+
+  //If overdrive is off, we agitate once. If overdrive is on, we don't agitate.
+  p->color_space_size = dt_bauhaus_combobox_get(w);
+  //Let core know of the changes
+  dt_dev_add_history_item(darktable.develop, self, TRUE);
+}
+
 static void rolloff_boundary_callback(GtkWidget *w, dt_iop_module_t *self)
 {
   //This is important to avoid cycles!
@@ -351,10 +341,7 @@ static void drama_callback(GtkWidget *w, dt_iop_module_t *self)
   dt_iop_filmulate_params_t *p = (dt_iop_filmulate_params_t *)self->params;
 
   //Drama goes from 0 to 100, but the relevant parameter in the backend is 0 to 1.
-//  p->layer_mix_const = dt_bauhaus_slider_get(w)/100.0f;
   p->layer_mix_const = dt_bauhaus_slider_get(w);
-  cout << "layer mix const at callback: " << p->layer_mix_const << endl;
-  cout << "layer mix const at callback2: " << dt_bauhaus_slider_get(w) << endl;
   //Let core know of the changes
   dt_dev_add_history_item(darktable.develop, self, TRUE);
 }
@@ -398,6 +385,7 @@ void gui_update(dt_iop_module_t *self)
   // let gui slider match current parameters:
   dt_iop_filmulate_gui_data_t *g = (dt_iop_filmulate_gui_data_t *)self->gui_data;
   dt_iop_filmulate_params_t *p = (dt_iop_filmulate_params_t *)self->params;
+  dt_bauhaus_combobox_set(g->color_space_size, p->color_space_size);
   dt_bauhaus_slider_set(g->rolloff_boundary, p->rolloff_boundary);
   dt_bauhaus_slider_set(g->film_area, p->film_area);
   dt_bauhaus_slider_set(g->drama, p->layer_mix_const);
@@ -413,33 +401,43 @@ void gui_init(dt_iop_module_t *self)
   //Create the widgets
   self->widget = gtk_box_new(GTK_ORIENTATION_VERTICAL, DT_BAUHAUS_SPACE);
 
+  g->color_space_size = dt_bauhaus_combobox_new(self);
   g->rolloff_boundary = dt_bauhaus_slider_new_with_range(self, 1.0f, 65535.0f, 0, 51275.0f, 2);
   g->film_area = dt_bauhaus_slider_new_with_range(self, 1.2f, 6.0f, 0.001f, logf(sqrtf(864.0f)), 2);
   g->drama = dt_bauhaus_slider_new_with_range(self, 0.0f, 1.0f, 0.0f, 0.2f, 2);
   g->overdrive = dt_bauhaus_combobox_new(self);
 
+  //Scaling things for the sliders
   dt_bauhaus_slider_set_callback(g->rolloff_boundary, rolloff_boundary_scaled_callback);
   dt_bauhaus_slider_set_callback(g->film_area, film_dimensions_callback);
   dt_bauhaus_slider_set_callback(g->drama, drama_scaled_callback);
+
+  //Values for the comboboxes
+  dt_bauhaus_combobox_add(g->color_space_size, _("Rec2020"));
+  dt_bauhaus_combobox_add(g->color_space_size, _("Rec709"));
   dt_bauhaus_combobox_add(g->overdrive, _("off"));
   dt_bauhaus_combobox_add(g->overdrive, _("on"));
   
+  dt_bauhaus_widget_set_label(g->color_space_size, NULL, _("color space size"));
+  gtk_widget_set_tooltip_text(g->color_space_size, _("filmulation works in RGB. Rec2020 is a bigger space, good if you're using larger output spaces. Rec709 is good for sRGB output color space, and helps attenuate the value of bright colors naturally."));
   dt_bauhaus_widget_set_label(g->rolloff_boundary, NULL, _("rolloff boundary"));
   gtk_widget_set_tooltip_text(g->rolloff_boundary, _("sets the point above which the highlights gently stop getting brighter. if you've got completely unclipped highlights before filmulation, raise this to 1."));
   dt_bauhaus_widget_set_label(g->film_area, NULL, _("film size"));
-  gtk_widget_set_tooltip_text(g->film_area, _("larger sizes emphasize smaller details and overall flatten the image. smaller sizes emphasize larger regional contrasts. don't use larger sizes with high drama or you'll get the HDR look."));
+  gtk_widget_set_tooltip_text(g->film_area, _("larger sizes emphasize smaller details and overall flatten the image. smaller sizes emphasize larger regional contrasts. don't use larger sizes with high drama or you'll get the hdr look."));
   dt_bauhaus_widget_set_label(g->drama, NULL, _("drama"));
   gtk_widget_set_tooltip_text(g->drama, _("pulls down highlights to retain detail. this is the real \"filmy\" effect. this not only helps bring down highlights, but can rescue extremely saturated regions such as flowers."));
   dt_bauhaus_widget_set_label(g->overdrive, NULL, _("overdrive mode"));
   gtk_widget_set_tooltip_text(g->overdrive, _("in case of emergency, break glass and press this button. this increases the filminess, in case 100 Drama was not enough for you."));
 
   //Add widgets to the gui
+  gtk_box_pack_start(GTK_BOX(self->widget), g->color_space_size, TRUE, TRUE, 0);
   gtk_box_pack_start(GTK_BOX(self->widget), g->rolloff_boundary, TRUE, TRUE, 0);
   gtk_box_pack_start(GTK_BOX(self->widget), g->film_area, TRUE, TRUE, 0);
   gtk_box_pack_start(GTK_BOX(self->widget), g->drama, TRUE, TRUE, 0);
   gtk_box_pack_start(GTK_BOX(self->widget), g->overdrive, TRUE, TRUE, 0);
 
   //Connect to the signals when widgets are changed
+  g_signal_connect(G_OBJECT(g->color_space_size), "value-changed", G_CALLBACK(color_space_size_callback), self);
   g_signal_connect(G_OBJECT(g->rolloff_boundary), "value-changed", G_CALLBACK(rolloff_boundary_callback), self);
   g_signal_connect(G_OBJECT(g->film_area), "value-changed", G_CALLBACK(film_area_callback), self);
   g_signal_connect(G_OBJECT(g->drama), "value-changed", G_CALLBACK(drama_callback), self);
