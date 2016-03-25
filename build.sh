45dffe0063bead4c7bf2720a6e2b7973ecaf039e
Edouard Gomez <ed.gomez@free.fr>
2016-03-26 00:06:08 +0100

    build: simplified, updated args parsing and help related stuff

--- build.sh
+++ build.sh
@@ -19,18 +19,19 @@ ADDRESS_SANITIZER=0
 PRINT_HELP=0
 
 OPT_FLICKR=-1
+OPT_LIBSECRET=-1
 OPT_KWALLET=-1
 OPT_OPENMP=-1
 OPT_OPENCL=-1
 OPT_UNITY=-1
 OPT_TETHERING=-1
-OPT_GEO=-1
 OPT_LUA=-1
+OPT_GEO=-1
 OPT_OPENEXR=-1
 OPT_WEBP=-1
 
 # ---------------------------------------------------------------------------
-# Parse options
+# Parsing functions
 # ---------------------------------------------------------------------------
 
 parse_feature()
@@ -39,38 +40,8 @@ parse_feature()
 	value=$2
 
 	case $feature in
-	flickr)
-		OPT_FLICKR=$value
-		;;
-	libsecret)
-		OPT_LIBSECRET=$value
-		;;
-	kwallet)
-		OPT_KWALLET=$value
-		;;
-	openmp)
-		OPT_OPENMP=$value
-		;;
-	opencl)
-		OPT_OPENCL=$value
-		;;
-	unity)
-		OPT_UNITY=$value
-		;;
-	tethering)
-		OPT_TETHERING=$value
-		;;
-	lua)
-		OPT_LUA=$value
-		;;
-	geo)
-		OPT_GEO=$value
-		;;
-	openexr)
-		OPT_OPENEXR=$value
-		;;
-	webp)
-		OPT_WEBP=$value
+	flickr|libsecret|kwallet|openmp|opencl|unity|tethering|lua|geo|openexr|webp)
+		eval "OPT_$(printf $feature|tr a-z A-Z)"=$value
 		;;
 	*)
 		echo "warning: unknown feature '$feature'"
@@ -78,73 +49,79 @@ parse_feature()
 	esac
 }
 
-while [ $# -ge 1 ] ; do
-	option="$1"
-	case $option in
-	--prefix)
-		INSTALL_PREFIX="$2"
+parse_args()
+{
+	while [ "$#" -ge 1 ] ; do
+		option="$1"
+		case $option in
+		--prefix)
+			INSTALL_PREFIX="$2"
+			shift
+			;;
+		--build-type)
+			BUILD_TYPE="$2"
+			shift
+			;;
+		--build-dir)
+			BUILD_DIR="$2"
+			shift
+			;;
+		--build-generator)
+			BUILD_GENERATOR="$2"
+			shift
+			;;
+		-j|--jobs)
+			MAKE_TASKS="$2"
+			shift
+			;;
+		--enable-*)
+			feature=${option#--enable-}
+			parse_feature "$feature" 1
+			;;
+		--disable-*)
+			feature=${option#--disable-}
+			parse_feature "$feature" 0
+			;;
+		--asan)
+			ADDRESS_SANITIZER=1
+			;;
+		-h|--help)
+			PRINT_HELP=1
+			;;
+		*)
+			echo "warning: ignoring unknown option $option"
+			;;
+		esac
 		shift
-		;;
-	--buildtype)
-		BUILD_TYPE="$2"
-		shift
-		;;
-	--builddir)
-		BUILD_DIR="$2"
-		shift
-		;;
-	--buildgenerator)
-		BUILD_GENERATOR="$2"
-		shift
-		;;
-	-j|--jobs)
-		MAKE_TASKS="$2"
-		shift
-		;;
-	--enable-*)
-		feature=${option#--enable-}
-		parse_feature "$feature" 1
-		;;
-	--disable-*)
-		feature=${option#--disable-}
-		parse_feature "$feature" 0
-		;;
-	--asan)
-		ADDRESS_SANITIZER=1
-		;;
-	-h|--help)
-		PRINT_HELP=1
-		;;
-	*)
-		echo "warning: ignoring unknown option $option"
-		;;
-	esac
-	shift
-done
+	done
+}
 
 # ---------------------------------------------------------------------------
-# Process user wishes
+# Help
 # ---------------------------------------------------------------------------
 
-if [ $PRINT_HELP -ne 0 ] ; then
-		cat <<EOF
-build.sh [OPTIONS]
+print_help()
+{
+	cat <<EOF
+$(basename $0) [OPTIONS]
 
 Options:
 Installation:
-   --prefix <string>     Install directory prefix (default: /opt/darktable)
+   --prefix         <string>  Install directory prefix (default: /opt/darktable)
 
 Build:
-   --builddir <string>       Building directory (default: $DT_SRC_DIR/build)
-   --buildtype <string>      Build type (Release, Debug, RelWithDebInfo)
-                                        (default:RelWithDebInfo)
-   --buildgenerator <string> Build tool (default: Unix Makefiles)
--j --jobs <integer>          Number of tasks (default: number of CPUs)
+   --build-dir      <string>  Building directory (default: $DT_SRC_DIR/build)
+   --build-type     <string>  Build type (Release, Debug, RelWithDebInfo)
+                              (default:RelWithDebInfo)
+   --build-generator <string> Build tool (default: Unix Makefiles)
+-j --jobs <integer>           Number of tasks (default: number of CPUs)
 
 Features:
-All these options have a --disable-* equivalent. By default they are set
-so that the cmake script autodetects features.
+By default cmake will enabel the features it autodetects on the build machine.
+Specifying the option on the command line forces the feature on or off.
+All these options have a --disable-* equivalent. 
    --enable-flickr
+   --enable-libsecret
    --enable-kwallet
    --enable-openmp
    --enable-opencl
@@ -158,6 +135,17 @@ so that the cmake script autodetects features.
 Extra:
 -h --help                Print help message
 EOF
+
+}
+
+# ---------------------------------------------------------------------------
+# Let's process the user's wishes
+# ---------------------------------------------------------------------------
+
+parse_args "$@"
+
+if [ $PRINT_HELP -ne 0 ] ; then
+	print_help
 	exit 1
 fi
 
