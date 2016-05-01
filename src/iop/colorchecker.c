9dfcaf489d4d2477cd934ce3c8d642648c944120
Roman Lebedev <lebedev.ri@gmail.com>
2016-05-01 14:26:28 +0300

    Colorchecker iop: fix build with openmp without simd

--- src/iop/colorchecker.c
+++ src/iop/colorchecker.c
@@ -258,8 +258,8 @@ fastlog (float x)
 // }
 
 // thinplate spline kernel \phi(r)
-#ifdef _OPENMP
-#pragma omp declare simd
+#if defined(_OPENMP) && defined(OPENMP_SIMD_)
+#pragma omp declare SIMD()
 #endif
 static inline float kernel(const float *x, const float *y)
 {
@@ -301,8 +301,8 @@ void process(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, const 
       out[2] += data->coeff_b[data->num_patches+1] * in[0] +
                 data->coeff_b[data->num_patches+2] * in[1] +
                 data->coeff_b[data->num_patches+3] * in[2];
-#ifdef _OPENMP // <== nice try, i don't think this does anything here
-#pragma omp simd
+#if defined(_OPENMP) && defined(OPENMP_SIMD_) // <== nice try, i don't think this does anything here
+#pragma omp SIMD()
 #endif
       for(int k=0;k<data->num_patches;k++)
       { // rbf from thin plate spline
