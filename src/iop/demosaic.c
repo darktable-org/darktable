13a207b4105e0368f02cf40f8929e1cd375a34aa
Roman Lebedev <lebedev.ri@gmail.com>
2016-05-15 14:07:30 +0300

    dt_dev_pixelpipe_uses_downsampled_input() is no more.
    We do need to keep 4f part of rawprepare because of sRAW's
    and DNG's.

--- src/iop/demosaic.c
+++ src/iop/demosaic.c
@@ -3584,7 +3584,7 @@ void commit_params(struct dt_iop_module_t *self, dt_iop_params_t *params, dt_dev
 {
   dt_iop_demosaic_params_t *p = (dt_iop_demosaic_params_t *)params;
   dt_iop_demosaic_data_t *d = (dt_iop_demosaic_data_t *)piece->data;
-  if(!(pipe->image.flags & DT_IMAGE_RAW) || dt_dev_pixelpipe_uses_downsampled_input(pipe)) piece->enabled = 0;
+  if(!(pipe->image.flags & DT_IMAGE_RAW)) piece->enabled = 0;
   d->green_eq = p->green_eq;
   d->color_smoothing = p->color_smoothing;
   d->median_thrs = p->median_thrs;
