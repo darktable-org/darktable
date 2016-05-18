13e5f298b305d30d4a79575176aaf0b6619f5be3
Tobias Ellinghaus <me@houz.org>
2016-05-18 12:51:02 +0200

    Add geolocation to watermark variables
    This adds the following watermark variables:
    - $(GPS.LATITUDE)
    - $(GPS.LONGITUDE)
    - $(GPS.ELEVATION)
    - $(GPS.LOCATION)
    
    Fixes #10796.

--- src/iop/watermark.c
+++ src/iop/watermark.c
@@ -717,6 +717,58 @@ static gchar *_watermark_get_svgdoc(dt_iop_module_t *self, dt_iop_watermark_data
     {
       g_list_free_full(res, &g_free);
     }
+
+    // geolocation
+    gchar *latitude = NULL, *longitude = NULL, *elevation = NULL;
+    if(dt_conf_get_bool("plugins/lighttable/metadata_view/pretty_location"))
+    {
+      latitude = dt_util_latitude_str(image->latitude);
+      longitude = dt_util_longitude_str(image->longitude);
+      elevation = dt_util_elevation_str(image->elevation);
+    }
+    else
+    {
+      const gchar NS = image->latitude < 0 ? 'S' : 'N';
+      const gchar EW = image->longitude < 0 ? 'W' : 'E';
+      if(image->latitude) latitude = g_strdup_printf("%c %09.6f", NS, fabs(image->latitude));
+      if(image->longitude) longitude = g_strdup_printf("%c %010.6f", EW, fabs(image->longitude));
+      if(image->elevation) elevation = g_strdup_printf("%.2f %s", image->elevation, _("m"));
+    }
+    gchar *parts[4] = { 0 };
+    int i = 0;
+    if(latitude) parts[i++] = latitude;
+    if(longitude) parts[i++] = longitude;
+    if(elevation) parts[i++] = elevation;
+    gchar *location = g_strjoinv(", ", parts);
+    svgdoc = _string_substitute(svgdata, "$(GPS.LATITUDE)", (latitude ? latitude : "-"));
+    if(svgdoc != svgdata)
+    {
+      g_free(svgdata);
+      svgdata = svgdoc;
+    }
+    svgdoc = _string_substitute(svgdata, "$(GPS.LONGITUDE)", (longitude ? longitude : "-"));
+    if(svgdoc != svgdata)
+    {
+      g_free(svgdata);
+      svgdata = svgdoc;
+    }
+    svgdoc = _string_substitute(svgdata, "$(GPS.ELEVATION)", (elevation ? elevation : "-"));
+    if(svgdoc != svgdata)
+    {
+      g_free(svgdata);
+      svgdata = svgdoc;
+    }
+    svgdoc = _string_substitute(svgdata, "$(GPS.LOCATION)", location);
+    if(svgdoc != svgdata)
+    {
+      g_free(svgdata);
+      svgdata = svgdoc;
+    }
+    g_free(latitude);
+    g_free(longitude);
+    g_free(elevation);
+    g_free(location);
+
   }
   return svgdoc;
 }
