48b1e05950e341fc283963b701b43c4e731eaaf6
Roman Lebedev <lebedev.ri@gmail.com>
2016-06-28 21:24:46 +0300

    FindOpenJPEG.cmake: pick right include path if multiple headers.
    At least in debian, libopenjpeg-dev 1:1.5.2-3.1 installs openjpeg.h
    not just as /usr/include/openjpeg-1.5/openjpeg.h, but also as
    /usr/include/openjpeg.h
    
    Without NO_DEFAULT_PATH, cmake does not find
    the right openjpeg.h in /usr/include/openjpeg-2.1/openjpeg.h,
    and does not set OpenJPEG_INCLUDE_DIR to /usr/include/openjpeg-2.1,
    but to /usr/include...

--- cmake/modules/FindOpenJPEG.cmake
+++ cmake/modules/FindOpenJPEG.cmake
@@ -14,7 +14,15 @@ libfind_pkg_check_modules(OpenJPEG_PKGCONF libopenjp2)
 find_path(OpenJPEG_INCLUDE_DIR
   NAMES openjpeg.h
   PATHS ${OpenJPEG_PKGCONF_INCLUDE_DIRS}
+  NO_DEFAULT_PATH
 )
+
+# at least in debian, libopenjpeg-dev 1:1.5.2-3.1 installs openjpeg.h
+# not just as /usr/include/openjpeg-1.5/openjpeg.h, but also as
+# /usr/include/openjpeg.h; without NO_DEFAULT_PATH, cmake does not find
+# the right openjpeg.h in /usr/include/openjpeg-2.1/openjpeg.h,
+# and does not set OpenJPEG_INCLUDE_DIR to /usr/include/openjpeg-2.1,
+# but to /usr/include...
 
 # Finally the library itself
 find_library(OpenJPEG_LIBRARY
