e5d746b18603b5cbb542cfde7df43d0204f7641c
Hans Vanpée <hans@vanpee.be>
2016-06-29 11:01:57 +0200

    Commit upstream changes

---
+++ src/external/rawspeed/CMakeFiles/rawspeed.dir/DependInfo.cmake
@@ -0,0 +1,84 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/AriDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/AriDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/ArwDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/ArwDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/BitPumpJPEG.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/BitPumpJPEG.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/BitPumpMSB.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/BitPumpMSB.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/BitPumpMSB16.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/BitPumpMSB16.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/BitPumpMSB32.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/BitPumpMSB32.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/BitPumpPlain.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/BitPumpPlain.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/BlackArea.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/BlackArea.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/ByteStream.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/ByteStream.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/ByteStreamSwap.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/ByteStreamSwap.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/Camera.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/Camera.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/CameraMetaData.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/CameraMetaData.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/CameraMetadataException.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/CameraMetadataException.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/CameraSensorInfo.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/CameraSensorInfo.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/CiffEntry.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/CiffEntry.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/CiffIFD.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/CiffIFD.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/CiffParser.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/CiffParser.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/CiffParserException.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/CiffParserException.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/ColorFilterArray.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/ColorFilterArray.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/Common.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/Common.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/Cr2Decoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/Cr2Decoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/CrwDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/CrwDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/DcrDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/DcrDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/DcsDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/DcsDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/DngDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/DngDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/DngDecoderSlices.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/DngDecoderSlices.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/DngOpcodes.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/DngOpcodes.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/ErfDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/ErfDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/FileIOException.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/FileIOException.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/FileMap.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/FileMap.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/FileReader.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/FileReader.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/FileWriter.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/FileWriter.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/HasselbladDecompressor.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/HasselbladDecompressor.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/IOException.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/IOException.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/KdcDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/KdcDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/LJpegDecompressor.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/LJpegDecompressor.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/LJpegPlain.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/LJpegPlain.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/MefDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/MefDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/MosDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/MosDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/MrwDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/MrwDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/NakedDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/NakedDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/NefDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/NefDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/NikonDecompressor.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/NikonDecompressor.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/OrfDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/OrfDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/PefDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/PefDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/PentaxDecompressor.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/PentaxDecompressor.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/RafDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/RafDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/RawDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/RawDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/RawDecoderException.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/RawDecoderException.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/RawImage.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/RawImage.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/RawImageDataFloat.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/RawImageDataFloat.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/RawImageDataU16.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/RawImageDataU16.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/RawParser.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/RawParser.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/Rw2Decoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/Rw2Decoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/SrwDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/SrwDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/StdAfx.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/StdAfx.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/ThreefrDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/ThreefrDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/TiffEntry.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/TiffEntry.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/TiffEntryBE.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/TiffEntryBE.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/TiffIFD.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/TiffIFD.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/TiffIFDBE.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/TiffIFDBE.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/TiffParser.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/TiffParser.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/TiffParserException.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/TiffParserException.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/X3fDecoder.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/X3fDecoder.cpp.o"
+  "/home/hans/repos/darktable/src/external/rawspeed/RawSpeed/X3fParser.cpp" "/home/hans/repos/darktable/src/external/rawspeed/CMakeFiles/rawspeed.dir/RawSpeed/X3fParser.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
