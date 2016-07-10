ce98adcbe8d3c47d65b0eb47918c9d224bdea87a
Roman Lebedev <lebedev.ri@gmail.com>
2016-07-10 21:10:26 +0300

    dt-cli: do not even try to open display, we don't need it.
    Refs #9473, 37e8c23eaa082457b8fea4857b8a9ae466ddc3a6
    
    Internally, gtk_init_check() first does gtk_parse_args(),
    and then tries to open display.
    
    For cli we really don't need nor want display,
    and do not want to see any warnings about failing to
    open such display when running cli.
    
    This removes following warning when running dt-cli without
    X/with unset DISPLAY env variable:
      Unable to init server: Could not connect: Connection refused
    
    FIXME: are there any more warnings?
    (cherry picked from commit 772538fdbf25c71b18b5551944134b7e81d01e4a)

--- src/cli/main.c
+++ src/cli/main.c
@@ -59,7 +59,7 @@ int main(int argc, char *arg[])
   bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
   textdomain(GETTEXT_PACKAGE);
 
-  gtk_init_check(&argc, &arg);
+  if(!gtk_parse_args(&argc, &arg)) exit(1);
 
   // parse command line arguments
   char *image_filename = NULL;
