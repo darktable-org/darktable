darktable-org/darktable

commit ddd0ee7c71c1ece96a97bc0fe3b7ba73b5f6cf3b

Fix #9181: only run one export at a time

src/control/jobs.c
@@ -53,6 +53,8 @@ typedef struct _dt_job_t
 
   dt_job_state_change_callback state_changed_cb;
 
+  dt_progress_t *progress;
+
   char description[DT_CONTROL_DESCRIPTION_LEN];
 } _dt_job_t;
 
@@ -76,6 +78,11 @@ static void dt_control_job_set_state(_dt_job_t *job, dt_job_state_t state)
 {
   if(!job) return;
   dt_pthread_mutex_lock(&job->state_mutex);
+  if(state >= DT_JOB_STATE_FINISHED  && job->state != DT_JOB_STATE_RUNNING && job->progress)
+  {
+    dt_control_progress_destroy(darktable.control, job->progress);
+    job->progress = NULL;
+  }
   job->state = state;
   /* pass state change to callback */
   if(job->state_changed_cb) job->state_changed_cb(job, state);
@@ -135,6 +142,7 @@ dt_job_t *dt_control_job_create(dt_job_execute_callback execute, const char *msg
 void dt_control_job_dispose(_dt_job_t *job)
 {
   if(!job) return;
+  if(job->progress) dt_control_progress_destroy(darktable.control, job->progress);
   dt_control_job_set_state(job, DT_JOB_STATE_DISPOSED);
   if(job->params_destroy) job->params_destroy(job->params);
   dt_pthread_mutex_destroy(&job->state_mutex);
@@ -235,6 +243,7 @@ static _dt_job_t *dt_control_schedule_job(dt_control_t *control)
   for(int i = 0; i < DT_JOB_QUEUE_MAX; i++)
   {
     if(control->queues[i] == NULL) continue;
+    if(control->export_scheduled && i == DT_JOB_QUEUE_USER_EXPORT) continue;
     _dt_job_t *_job = (_dt_job_t *)control->queues[i]->data;
     if(_job->priority > max_priority)
     {
@@ -258,6 +267,7 @@ static _dt_job_t *dt_control_schedule_job(dt_control_t *control)
   GList **queue = &control->queues[winner_queue];
   *queue = g_list_delete_link(*queue, *queue);
   control->queue_length[winner_queue]--;
+  if(winner_queue == DT_JOB_QUEUE_USER_EXPORT) control->export_scheduled = TRUE;
 
   // and place it in scheduled job array (for job deduping)
   control->job[dt_control_get_threadid()] = job;
@@ -307,6 +317,7 @@ static int32_t dt_control_run_job(dt_control_t *control)
   // remove the job from scheduled job array (for job deduping)
   dt_pthread_mutex_lock(&control->queue_mutex);
   control->job[dt_control_get_threadid()] = NULL;
+  if(job->queue == DT_JOB_QUEUE_USER_EXPORT) control->export_scheduled = FALSE;
   dt_pthread_mutex_unlock(&control->queue_mutex);
 
   // and free it
@@ -434,7 +445,9 @@ int dt_control_add_job(dt_control_t *control, dt_job_queue_t queue_id, _dt_job_t
   else
   {
     // the rest are FIFOs
-    if(queue_id == DT_JOB_QUEUE_USER_BG || queue_id == DT_JOB_QUEUE_SYSTEM_BG)
+    if(queue_id == DT_JOB_QUEUE_USER_BG ||
+       queue_id == DT_JOB_QUEUE_USER_EXPORT ||
+       queue_id == DT_JOB_QUEUE_SYSTEM_BG)
       job->priority = 0;
     else
       job->priority = DT_CONTROL_FG_PRIORITY;
@@ -533,6 +546,35 @@ static void *dt_control_work(void *ptr)
   }
   return NULL;
 }
+
+// convenience functions to have a progress bar for the job.
+// this allows to show the gui indicator of the job even before it got scheduled
+void dt_control_job_add_progress(dt_job_t *job, const char *message, gboolean cancellable)
+{
+  if(!job) return;
+  job->progress = dt_control_progress_create(darktable.control, TRUE, message);
+  if(cancellable)
+    dt_control_progress_attach_job(darktable.control, job->progress, job);
+}
+
+void dt_control_job_set_progress_message(dt_job_t *job, const char *message)
+{
+  if(!job || !job->progress) return;
+  dt_control_progress_set_message(darktable.control, job->progress, message);
+}
+
+void dt_control_job_set_progress(dt_job_t *job, double value)
+{
+  if(!job || !job->progress) return;
+  dt_control_progress_set_progress(darktable.control, job->progress, value);
+}
+
+double dt_control_job_get_progress(dt_job_t *job)
+{
+  if(!job || !job->progress) return -1.0;
+  return dt_control_progress_get_progress(job->progress);
+}
+
 
 // moved out of control.c to be able to make some helper functions static
 void dt_control_jobs_init(dt_control_t *control)

src/common/film.c
@@ -20,7 +20,6 @@
 #include "control/control.h"
 #include "control/conf.h"
 #include "control/jobs.h"
-#include "control/progress.h"
 #include "common/film.h"
 #include "common/dtpthread.h"
 #include "common/collection.h"
@@ -341,7 +340,7 @@ int _film_filename_cmp(gchar *a, gchar *b)
   return ret;
 }
 
-void dt_film_import1(dt_film_t *film)
+void dt_film_import1(dt_job_t *job, dt_film_t *film)
 {
   gboolean recursive = dt_conf_get_bool("ui_last/import_recursive");
 
@@ -406,7 +405,7 @@ void dt_film_import1(dt_film_t *film)
   guint total = g_list_length(images);
   g_snprintf(message, sizeof(message) - 1, ngettext("importing %d image", "importing %d images", total),
              total);
-  dt_progress_t *progress = dt_control_progress_create(darktable.control, TRUE, message);
+  dt_control_job_set_progress_message(job, message);
 
 
   /* loop thru the images and import to current film roll */
@@ -465,7 +464,7 @@ void dt_film_import1(dt_film_t *film)
     dt_image_import(cfr->id, (const gchar *)image->data, FALSE);
 
     fraction += 1.0 / total;
-    dt_control_progress_set_progress(darktable.control, progress, fraction);
+    dt_control_job_set_progress(job, fraction);
 
 
   } while((image = g_list_next(image)) != NULL);
@@ -476,7 +475,6 @@ void dt_film_import1(dt_film_t *film)
   dt_control_queue_redraw_center();
   dt_control_signal_raise(darktable.signals, DT_SIGNAL_TAG_CHANGED);
 
-  dt_control_progress_destroy(darktable.control, progress);
   dt_control_signal_raise(darktable.signals, DT_SIGNAL_FILMROLLS_IMPORTED, film->id);
 
   // FIXME: maybe refactor into function and call it?

src/common/film.h
@@ -24,6 +24,10 @@
 #include <inttypes.h>
 #include <glib.h>
 
+// ugly, but the only way to avoid a circular include dependecy :-(
+struct _dt_job_t;
+typedef struct _dt_job_t dt_job_t;
+
 /**
  * film roll.
  * this is one directory of images on disk.
@@ -52,7 +56,7 @@ int dt_film_open_recent(const int32_t num);
  * are respected). */
 int dt_film_import(const char *dirname);
 /** helper for import threads. */
-void dt_film_import1(dt_film_t *film);
+void dt_film_import1(dt_job_t *job, dt_film_t *film);
 /** constructs the lighttable/query setting for this film, respecting stars and filters. */
 void dt_film_set_query(const int32_t id);
 /** removes this film and all its images from db. */

src/control/control.h
@@ -177,6 +177,7 @@ typedef struct dt_control_t
 
   // job management
   int32_t running;
+  gboolean export_scheduled;
   dt_pthread_mutex_t queue_mutex, cond_mutex, run_mutex;
   pthread_cond_t cond;
   int32_t num_threads;
@@ -206,6 +207,8 @@ typedef struct dt_control_t
       void (*cancellable)(dt_lib_module_t *self, struct dt_lib_backgroundjob_element_t *instance,
                           dt_progress_t *progress);
       void (*updated)(dt_lib_module_t *self, struct dt_lib_backgroundjob_element_t *instance, double value);
+      void (*message_updated)(dt_lib_module_t *self, struct dt_lib_backgroundjob_element_t *instance,
+                              const char *message);
     } proxy;
 
   } progress_system;

src/control/jobs.h
@@ -23,6 +23,7 @@
 
 #include <inttypes.h>
 #include <stddef.h>
+#include <glib.h>
 
 #define DT_CONTROL_DESCRIPTION_LEN 256
 // reserved workers
@@ -43,11 +44,12 @@ typedef enum dt_job_state_t
 
 typedef enum dt_job_queue_t
 {
-  DT_JOB_QUEUE_USER_FG = 0,   // gui actions, ...
-  DT_JOB_QUEUE_SYSTEM_FG = 1, // thumbnail creation, ..., may be pushed out of the queue
-  DT_JOB_QUEUE_USER_BG = 2,   // exports, ...
-  DT_JOB_QUEUE_SYSTEM_BG = 3, // some lua stuff that may not be pushed out of the queue, ...
-  DT_JOB_QUEUE_MAX = 4
+  DT_JOB_QUEUE_USER_FG = 0,     // gui actions, ...
+  DT_JOB_QUEUE_SYSTEM_FG = 1,   // thumbnail creation, ..., may be pushed out of the queue
+  DT_JOB_QUEUE_USER_BG = 2,     // imports, ...
+  DT_JOB_QUEUE_USER_EXPORT = 3, // exports. only one of these jobs will ever be scheduled at a time
+  DT_JOB_QUEUE_SYSTEM_BG = 4,   // some lua stuff that may not be pushed out of the queue, ...
+  DT_JOB_QUEUE_MAX = 5
 } dt_job_queue_t;
 
 struct _dt_job_t;
@@ -76,6 +78,11 @@ void dt_control_job_set_params_with_size(dt_job_t *job, void *params, size_t par
                                          dt_job_destroy_callback callback);
 /** get job params. WARNING: you must not free them. dt_control_job_dispose() will take care of that */
 void *dt_control_job_get_params(const dt_job_t *job);
+
+void dt_control_job_add_progress(dt_job_t *job, const char *message, gboolean cancellable);
+void dt_control_job_set_progress_message(dt_job_t *job, const char *message);
+void dt_control_job_set_progress(dt_job_t *job, double value);
+double dt_control_job_get_progress(dt_job_t *job);
 
 struct dt_control_t;
 void dt_control_jobs_init(struct dt_control_t *control);

src/control/jobs/camera_jobs.c
@@ -22,7 +22,6 @@
 #include "control/conf.h"
 #include "control/jobs/camera_jobs.h"
 #include "control/jobs/image_jobs.h"
-#include "control/progress.h"
 #include "gui/gtk.h"
 
 #include <stdio.h>
@@ -63,7 +62,7 @@ typedef struct dt_camera_import_t
 
   GList *images;
   struct dt_camera_t *camera;
-  dt_progress_t *progress;
+  dt_job_t *job;
   double fraction;
   uint32_t import_count;
 } dt_camera_import_t;
@@ -85,6 +84,8 @@ static int32_t dt_camera_capture_job_run(dt_job_t *job)
 
   total = params->brackets ? params->count * params->brackets : params->count;
   snprintf(message, sizeof(message), ngettext("capturing %d image", "capturing %d images", total), total);
+
+  dt_control_job_set_progress_message(job, message);
 
   /* try to get exp program mode for nikon */
   char *expprogram = (char *)dt_camctl_camera_get_property(darktable.camctl, NULL, "expprogram");
@@ -121,9 +122,6 @@ static int32_t dt_camera_capture_job_run(dt_job_t *job)
     }
   }
 
-  /* create the bgjob plate */
-  dt_progress_t *progress = dt_control_progress_create(darktable.control, TRUE, message);
-
   GList *current_value = g_list_find(values, original_value);
   for(uint32_t i = 0; i < params->count; i++)
   {
@@ -159,7 +157,7 @@ static int32_t dt_camera_capture_job_run(dt_job_t *job)
       dt_camctl_camera_capture(darktable.camctl, NULL);
 
       fraction += 1.0 / total;
-      dt_control_progress_set_progress(darktable.control, progress, fraction);
+      dt_control_job_set_progress(job, fraction);
     }
 
     // lets reset to original value before continue
@@ -169,9 +167,6 @@ static int32_t dt_camera_capture_job_run(dt_job_t *job)
       dt_camctl_camera_set_property_string(darktable.camctl, NULL, "shutterspeed", current_value->data);
     }
   }
-
-  /* cleanup */
-  dt_control_progress_destroy(darktable.control, progress);
 
   // free values
   if(values)
@@ -212,6 +207,7 @@ dt_job_t *dt_camera_capture_job_create(const char *jobcode, uint32_t delay, uint
     dt_control_job_dispose(job);
     return NULL;
   }
+  dt_control_job_add_progress(job, _("capture images"), FALSE);
   dt_control_job_set_params(job, params, dt_camera_capture_cleanup);
 
   dt_import_session_set_name(params->shared.session, jobcode);
@@ -288,7 +284,7 @@ void _camera_import_image_downloaded(const dt_camera_t *camera, const char *file
 
   t->fraction += 1.0 / g_list_length(t->images);
 
-  dt_control_progress_set_progress(darktable.control, t->progress, t->fraction);
+  dt_control_job_set_progress(t->job, t->fraction);
 
   t->import_count++;
 }
@@ -331,7 +327,7 @@ static int32_t dt_camera_import_job_run(dt_job_t *job)
   char message[512] = { 0 };
   snprintf(message, sizeof(message),
            ngettext("importing %d image from camera", "importing %d images from camera", total), total);
-  params->progress = dt_control_progress_create(darktable.control, TRUE, message);
+  dt_control_job_set_progress_message(job, message);
 
   // Switch to new filmroll
   dt_film_open(dt_import_session_film_id(params->shared.session));
@@ -348,7 +344,6 @@ static int32_t dt_camera_import_job_run(dt_job_t *job)
   dt_camctl_register_listener(darktable.camctl, &listener);
   dt_camctl_import(darktable.camctl, params->camera, params->images);
   dt_camctl_unregister_listener(darktable.camctl, &listener);
-  dt_control_progress_destroy(darktable.control, params->progress);
 
   return 0;
 }
@@ -385,6 +380,7 @@ dt_job_t *dt_camera_import_job_create(const char *jobcode, GList *images, struct
     dt_control_job_dispose(job);
     return NULL;
   }
+  dt_control_job_add_progress(job, _("import images from camera"), FALSE);
   dt_control_job_set_params(job, params, dt_camera_import_cleanup);
 
   /* intitialize import session for camera import job */
@@ -395,6 +391,7 @@ dt_job_t *dt_camera_import_job_create(const char *jobcode, GList *images, struct
   params->images = g_list_copy(images);
   params->camera = camera;
   params->import_count = 0;
+  params->job = job;
   return job;
 }
 

commit 40b109df80ec8a55c8681ca97c11d9b81cb62e4a

dt_control_job_create(): set progress to NULL after destroying it. Fixes #11079
Was broken in ddd0ee7c71c1ece96a97bc0fe3b7ba73b5f6cf3b.

Avoids heap-use-after-free after exporting image:

=================================================================
==15123==ERROR: AddressSanitizer: heap-use-after-free on address 0x61d000210a08 at pc 0x7eff46cdba15 bp 0x7eff2d9579f0 sp 0x7eff2d9579e8
READ of size 8 at 0x61d000210a08 thread T4
    0 0x7eff46cdba14 in dt_control_progress_destroy /home/lebedevri/darktable/src/control/progress.c:91
    1 0x7eff46cc9f3d in dt_control_job_set_state /home/lebedevri/darktable/src/control/jobs.c:83
    2 0x7eff46cca5f4 in dt_control_job_dispose /home/lebedevri/darktable/src/control/jobs.c:146
    3 0x7eff46ccb3d0 in dt_control_run_job /home/lebedevri/darktable/src/control/jobs.c:324
    4 0x7eff46ccc3d4 in dt_control_work /home/lebedevri/darktable/src/control/jobs.c:539
    5 0x7eff4264a463 in start_thread (/lib/x86_64-linux-gnu/libpthread.so.0+0x7463)
    6 0x7eff3eb8a30c in clone (/lib/x86_64-linux-gnu/libc.so.6+0xe730c)

0x61d000210a08 is located 1928 bytes inside of 1952-byte region [0x61d000210280,0x61d000210a20)
freed by thread T4 here:
    0 0x7eff472aca00 in free (/usr/lib/x86_64-linux-gnu/libasan.so.3+0xc1a00)
    1 0x7eff46cdbb5f in dt_control_progress_destroy /home/lebedevri/darktable/src/control/progress.c:118
    2 0x7eff46cca5e3 in dt_control_job_dispose /home/lebedevri/darktable/src/control/jobs.c:145
    3 0x7eff46ccb3d0 in dt_control_run_job /home/lebedevri/darktable/src/control/jobs.c:324
    4 0x7eff46ccc3d4 in dt_control_work /home/lebedevri/darktable/src/control/jobs.c:539
    5 0x7eff4264a463 in start_thread (/lib/x86_64-linux-gnu/libpthread.so.0+0x7463)

previously allocated by thread T0 here:
    0 0x7eff472acec0 in calloc (/usr/lib/x86_64-linux-gnu/libasan.so.3+0xc1ec0)
    1 0x7eff46cdb6ed in dt_control_progress_create /home/lebedevri/darktable/src/control/progress.c:52

src/control/jobs.c
@@ -143,6 +143,7 @@ void dt_control_job_dispose(_dt_job_t *job)
 {
   if(!job) return;
   if(job->progress) dt_control_progress_destroy(darktable.control, job->progress);
+  job->progress = NULL;
   dt_control_job_set_state(job, DT_JOB_STATE_DISPOSED);
   if(job->params_destroy) job->params_destroy(job->params);
   dt_pthread_mutex_destroy(&job->state_mutex);