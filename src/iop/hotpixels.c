74edcf2716e5a7624e8ba5dd7096a252e1733ea4
Dan Torop <dant@pnym.net>
2016-07-22 16:54:52 -0400

    hotpixels: better marking of fixed x-trans pixels
    Mark the pixels with an indicator of the same color as the fixed pixel,
    as in the Bayer code.

--- src/iop/hotpixels.c
+++ src/iop/hotpixels.c
@@ -179,10 +179,21 @@ static int process_xtrans(const void *const ivoid, void *const ovoid,
           fixed++;
           if(markfixed)
           {
-            // cheat and mark all colors of pixels
-            // FIXME: use offsets
-            for(int i = -2; i >= -10 && i >= -col; --i) out[i] = *in;
-            for(int i = 2; i <= 10 && i < width - col; ++i) out[i] = *in;
+            const uint8_t c = FCxtrans(row, col, roi_out, xtrans);
+            for(int i = -2; i >= -10 && i >= -col; --i)
+            {
+              if(c == FCxtrans(row, col+i, roi_out, xtrans))
+              {
+                out[i] = *in;
+              }
+            }
+            for(int i = 2; i <= 10 && i < width - col; ++i)
+            {
+              if(c == FCxtrans(row, col+i, roi_out, xtrans))
+              {
+                out[i] = *in;
+              }
+            }
           }
         }
       }
