e18092cc7acbabebeb4940c07ac5269961653f50
Ulrich Pegelow <ulrich.pegelow@tongareva.de>
2016-07-24 16:57:59 +0200

    opencl: add kernel for highlights reconstruction Lch (fixes #11092)

--- src/iop/highlights.c
+++ src/iop/highlights.c
@@ -871,9 +871,6 @@ void commit_params(struct dt_iop_module_t *self, dt_iop_params_t *p1, dt_dev_pix
 
   // no OpenCL for DT_IOP_HIGHLIGHTS_INPAINT yet.
   if(d->mode == DT_IOP_HIGHLIGHTS_INPAINT) piece->process_cl_ready = 0;
-
-  // no OpenCL for DT_IOP_HIGHLIGHTS_LCH yet.
-  if(d->mode == DT_IOP_HIGHLIGHTS_LCH) piece->process_cl_ready = 0;
 }
 
 void init_global(dt_iop_module_so_t *module)
