9be1f4c050a9ebb51ec53ae9d3f47b715a499818
Roman Lebedev <lebedev.ri@gmail.com>
2016-09-07 12:14:13 +0300

    Nikon D7100: set proper whitelevels, enable 12bit mode. Fixes #11139
    Also, according to the camera owner, there is no fully uncompressed mode.

--- tools/rawspeed-check-nikon-modes.rb
+++ tools/rawspeed-check-nikon-modes.rb
@@ -28,7 +28,8 @@ IGNORE_ONLY_MODE = {
   ["NIKON CORPORATION", "NIKON 1 V1"] => "compressed",
   ["NIKON CORPORATION", "NIKON D600"] => "compressed",
   ["NIKON CORPORATION", "NIKON D3200"] => "compressed",
-  ["NIKON CORPORATION", "NIKON D7000"] => "compressed"
+  ["NIKON CORPORATION", "NIKON D7000"] => "compressed",
+  ["NIKON CORPORATION", "NIKON D7100"] => "compressed"
 }
 
 require 'nokogiri'
