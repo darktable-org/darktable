1292f9a1ff26d15782f154288ede9c19ebb285b9
Roman Lebedev <lebedev.ri@gmail.com>
2016-09-12 15:38:15 +0300

    Nikon D3000: proper whitelevel. Fixes #11148
    According to usermanual, only has 12bit compressed raw.

--- tools/rawspeed-check-nikon-modes.rb
+++ tools/rawspeed-check-nikon-modes.rb
@@ -30,6 +30,7 @@ IGNORE_ONLY_MODE = {
   ["NIKON CORPORATION", "NIKON 1 S1"] => "compressed",
   ["NIKON CORPORATION", "NIKON 1 V1"] => "compressed",
   ["NIKON CORPORATION", "NIKON D600"] => "compressed",
+  ["NIKON CORPORATION", "NIKON D3000"] => "compressed",
   ["NIKON CORPORATION", "NIKON D3200"] => "compressed",
   ["NIKON CORPORATION", "NIKON D7000"] => "compressed",
   ["NIKON CORPORATION", "NIKON D7100"] => "compressed",
