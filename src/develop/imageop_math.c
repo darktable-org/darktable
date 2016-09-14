6b5493f626d0bae42bac4057019fd70a65b12fae
Roman Lebedev <lebedev.ri@gmail.com>
2016-09-15 00:21:09 +0300

    X-Trans float: implement a bit better downscaling
    (just like the bayer) prone to confetti colours,
    does not do sub-sampling.
    But at least not as pixellated :)
    
    Simple adaptation from dt_iop_clip_and_zoom_demosaic_third_size_xtrans_f()

--- src/develop/imageop_math.c
+++ src/develop/imageop_math.c
@@ -1122,6 +1122,57 @@ void dt_iop_clip_and_zoom_mosaic_third_size_xtrans(uint16_t *const out, const ui
   }
 }
 
+void dt_iop_clip_and_zoom_mosaic_third_size_xtrans_f(float *const out, const float *const in,
+                                                     const dt_iop_roi_t *const roi_out,
+                                                     const dt_iop_roi_t *const roi_in, const int32_t out_stride,
+                                                     const int32_t in_stride, const uint8_t (*const xtrans)[6])
+{
+  const float px_footprint = 1.f / roi_out->scale;
+  const int samples = MAX(1, (int)floorf(px_footprint / 3));
+
+  // A slightly different algorithm than
+  // dt_iop_clip_and_zoom_demosaic_half_size_f() which aligns to 2x2
+  // Bayer grid and hence most pull additional data from all edges
+  // which don't align with CFA. Instead align to a 3x3 pattern (which
+  // is semi-regular in X-Trans CFA). If instead had aligned the
+  // samples to the full 6x6 X-Trans CFA, wouldn't need to perform a
+  // CFA lookup, but then would only work at 1/6 scale or less. This
+  // code doesn't worry about fractional pixel offset of top/left of
+  // pattern nor oversampling by non-integer number of samples.
+
+  // X-Trans RGB weighting averages to 2:5:2 for each 3x3 cell
+  static const float div[3] = { 2.0, 5.0, 2.0 };
+
+#ifdef _OPENMP
+#pragma omp parallel for default(none) schedule(static)
+#endif
+  for(int y = 0; y < roi_out->height; y++)
+  {
+    float *outc = out + out_stride * y;
+    const int py = CLAMPS((int)round((y + roi_out->y - 0.5f) * px_footprint), 0, roi_in->height - 3);
+    const int ymax = MIN(roi_in->height - 3, py + 3 * samples);
+
+    for(int x = 0; x < roi_out->width; x++, outc++)
+    {
+      float col[3] = { 0.0f };
+      int num = 0;
+      const int px = CLAMPS((int)round((x + roi_out->x - 0.5f) * px_footprint), 0, roi_in->width - 3);
+      const int xmax = MIN(roi_in->width - 3, px + 3 * samples);
+      for(int yy = py; yy <= ymax; yy += 3)
+        for(int xx = px; xx <= xmax; xx += 3)
+        {
+          for(int j = 0; j < 3; ++j)
+            for(int i = 0; i < 3; ++i)
+              col[FCxtrans(yy + j, xx + i, roi_in, xtrans)] += in[xx + i + in_stride * (yy + j)];
+          num++;
+        }
+
+      const int c = FCxtrans(y, x, roi_out, xtrans);
+      *outc = col[c] / ((float)num * div[c]);
+    }
+  }
+}
+
 void dt_iop_clip_and_zoom_demosaic_passthrough_monochrome_plain(float *const out, const uint16_t *const in,
                                                                 const dt_iop_roi_t *const roi_out,
                                                                 const dt_iop_roi_t *const roi_in,
