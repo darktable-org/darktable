darktable-org/darktable

commit f09750ab9069549619c14dd151f6b196c2f0457f

pixelpipe: duplicte module name into local variable in dt_dev_pixelpipe_process_rec()
aim: in case of crashes within dt_dev_pixelpipe_process_rec() we get easier access to the
the module context in which it happened

src/develop/pixelpipe_hb.c
@@ -724,6 +724,7 @@ static int dt_dev_pixelpipe_process_rec(dt_dev_pixelpipe_t *pipe, dt_develop_t *
 {
   dt_iop_roi_t roi_in = *roi_out;
 
+  char module_name[256] = { 0 };
   void *input = NULL;
   void *cl_mem_input = NULL;
   *cl_mem_output = NULL;
@@ -740,6 +741,7 @@ static int dt_dev_pixelpipe_process_rec(dt_dev_pixelpipe_t *pipe, dt_develop_t *
                                           g_list_previous(modules), g_list_previous(pieces), pos - 1);
   }
 
+  if(module && module->op) strncpy(module_name, module->op, sizeof(module_name));
   get_output_format(module, pipe, piece, dev, *out_format);
   const size_t bpp = dt_iop_buffer_dsc_to_bpp(*out_format);
   const size_t bufsize = (size_t)bpp * roi_out->width * roi_out->height;

commit 9b0e0bde7248828a540364d2aa5d04d15f1c98ae

dt_dev_pixelpipe_process_rec(): fix clang build.
In file included from darktable/src/develop/pixelpipe.c:22:
darktable/src/develop/pixelpipe_hb.c:744:24: error: address of array 'module->op' will always evaluate to 'true' [-Werror,-Wpointer-bool-conversion]
  if(module && module->op) strncpy(module_name, module->op, sizeof(module_name));
            ~~ ~~~~~~~~^~

Was broken in f09750ab9069549619c14dd151f6b196c2f0457f.

src/develop/pixelpipe_hb.c
@@ -741,7 +741,7 @@ static int dt_dev_pixelpipe_process_rec(dt_dev_pixelpipe_t *pipe, dt_develop_t *
                                           g_list_previous(modules), g_list_previous(pieces), pos - 1);
   }
 
-  if(module && module->op) strncpy(module_name, module->op, sizeof(module_name));
+  if(module) g_strlcpy(module_name, module->op, MIN(sizeof(module_name), sizeof(module->op)));
   get_output_format(module, pipe, piece, dev, *out_format);
   const size_t bpp = dt_iop_buffer_dsc_to_bpp(*out_format);
   const size_t bufsize = (size_t)bpp * roi_out->width * roi_out->height;