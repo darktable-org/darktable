6c6dcf815d90d85e91be72f3360e20efc7b4aae8
Roman Lebedev <lebedev.ri@gmail.com>
2016-10-03 15:13:42 +0300

    Soften iop: process_cl(): don't do unbound stack-based VLA's

--- src/iop/soften.c
+++ src/iop/soften.c
@@ -424,7 +424,8 @@ int process_cl(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, cl_m
   const float sigma = sqrt((radius * (radius + 1) * BOX_ITERATIONS + 2) / 3.0f);
   const int wdh = ceilf(3.0f * sigma);
   const int wd = 2 * wdh + 1;
-  float mat[wd];
+  const size_t mat_size = sizeof(float) * wd;
+  float *mat = malloc(mat_size);
   float *m = mat + wdh;
   float weight = 0.0f;
 
@@ -469,7 +470,7 @@ int process_cl(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, cl_m
   dev_tmp = dt_opencl_alloc_device(devid, width, height, 4 * sizeof(float));
   if(dev_tmp == NULL) goto error;
 
-  dev_m = dt_opencl_copy_host_to_device_constant(devid, (size_t)sizeof(float) * wd, mat);
+  dev_m = dt_opencl_copy_host_to_device_constant(devid, mat_size, mat);
   if(dev_m == NULL) goto error;
 
   /* overexpose image */
@@ -542,11 +543,13 @@ int process_cl(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, cl_m
 
   if(dev_m != NULL) dt_opencl_release_mem_object(dev_m);
   if(dev_tmp != NULL) dt_opencl_release_mem_object(dev_tmp);
+  free(mat);
   return TRUE;
 
 error:
   if(dev_m != NULL) dt_opencl_release_mem_object(dev_m);
   if(dev_tmp != NULL) dt_opencl_release_mem_object(dev_tmp);
+  free(mat);
   dt_print(DT_DEBUG_OPENCL, "[opencl_soften] couldn't enqueue kernel! %d\n", err);
   return FALSE;
 }
