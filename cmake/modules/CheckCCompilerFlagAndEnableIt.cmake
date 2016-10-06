77a39bd6354c288de559cc9c408ff98b1364beef
Roman Lebedev <lebedev.ri@gmail.com>
2016-10-06 16:32:07 +0300

    CMake: fine-tune CHECK_COMPILER_FLAG_AND_ENABLE_IT()
    Acc. to #11199, in order to check some flags, we need
    to make sure to set other flags too.

--- cmake/modules/CheckCCompilerFlagAndEnableIt.cmake
+++ cmake/modules/CheckCCompilerFlagAndEnableIt.cmake
@@ -3,6 +3,8 @@ include(CheckCCompilerFlag)
 macro (CHECK_C_COMPILER_FLAG_AND_ENABLE_IT _FLAG)
   set(_RESULT "C_COMPILER_UNDERSTANDS_${_FLAG}")
 
+  set(CMAKE_REQUIRED_FLAGS "${CMAKE_C_FLAGS}")
+
   CHECK_C_COMPILER_FLAG("${_FLAG}" ${_RESULT})
 
   if(${${_RESULT}})
