ee18b950775b0dd24f77d2702190f6f777628036
Roman Lebedev <lebedev.ri@gmail.com>
2016-10-06 14:03:42 +0300

    pixelpipe_picker_cl(): roi_copy: passthrough proper x,y
    Should be important for mosaiced picker.

--- src/develop/pixelpipe_hb.c
+++ src/develop/pixelpipe_hb.c
@@ -540,7 +540,7 @@ static void pixelpipe_picker_cl(int devid, dt_iop_module_t *module, dt_iop_buffe
   box[2] = region[0];
   box[3] = region[1];
 
-  dt_iop_roi_t roi_copy = (dt_iop_roi_t){.x = 0, .y = 0, .width = region[0], .height = region[1] };
+  dt_iop_roi_t roi_copy = (dt_iop_roi_t){.x = roi->x, .y = roi->x, .width = region[0], .height = region[1] };
 
   dt_color_picker_helper(dsc, pixel, &roi_copy, box, picked_color, picked_color_min, picked_color_max);
 
