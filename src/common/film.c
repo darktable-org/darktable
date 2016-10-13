darktable-org/darktable

commit eab7a7d248e50f58c574f54e820856572b67f4ae

Clean folder name before opening as filmroll
Before it could happen that the initial string ended in '/' which would
trigger the creation of a new filmroll during import as it's there
compared to a version without the slash. So let's clean the initial
string, too.

src/common/film.c
@@ -276,7 +276,9 @@ int dt_film_import(const char *dirname)
 
   /* at last put import film job on queue */
   film->last_loaded = 0;
-  g_strlcpy(film->dirname, dirname, sizeof(film->dirname));
+  gchar *clean_dirname = g_path_get_dirname(dirname);
+  g_strlcpy(film->dirname, clean_dirname, sizeof(film->dirname));
+  g_free(clean_dirname);
   film->dir = g_dir_open(film->dirname, 0, NULL);
   dt_control_add_job(darktable.control, DT_JOB_QUEUE_USER_BG, dt_film_import1_create(film));
 

commit dcc6cdc7c56fea8457d84d08a0621133f6886fd4

Fix import of folders, don't import parent folder
This was broken in eab7a7d248e50f58c574f54e820856572b67f4ae and was
triggered when passing a folder name not ending in '/'.
Thanks pobry for spotting this.

src/common/film.c
@@ -279,9 +279,9 @@ int dt_film_import(const char *dirname)
 
   /* at last put import film job on queue */
   film->last_loaded = 0;
-  gchar *clean_dirname = g_path_get_dirname(dirname);
-  g_strlcpy(film->dirname, clean_dirname, sizeof(film->dirname));
-  g_free(clean_dirname);
+  g_strlcpy(film->dirname, dirname, sizeof(film->dirname));
+  char *last = &film->dirname[strlen(film->dirname) - 1];
+  if(*last == '/' && last != film->dirname) *last = '\0'; // remove the closing /, unless it's also the start
   film->dir = g_dir_open(film->dirname, 0, NULL);
   dt_control_add_job(darktable.control, DT_JOB_QUEUE_USER_BG, dt_film_import1_create(film));
 