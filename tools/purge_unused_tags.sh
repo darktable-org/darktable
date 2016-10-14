da986fc094d6fa7da23fe4f876d66116e48c979c
Pascal Obry <pascal@obry.net>
2016-10-14 23:42:38 +0200

    Migrate purge_unused_tags script to new db layout.

--- tools/purge_unused_tags.sh
+++ tools/purge_unused_tags.sh
@@ -5,18 +5,38 @@
 #        -p  do the purge, otherwise only display unused tags
 #
 
-DBFILE=~/.config/darktable/library.db
+LIBDB=$HOME/.config/darktable/library.db
+DATADB=$HOME/.config/darktable/data.db
 
 # tags not used
-Q1C="SELECT name FROM tags WHERE id NOT IN (SELECT tagid FROM tagged_images);"
-Q1="DELETE FROM tags WHERE id NOT IN (SELECT tagid FROM tagged_images);"
+Q1C="
+ATTACH DATABASE \"$LIBDB\" as lib;
+ATTACH DATABASE \"$DATADB\" as data;
+SELECT name FROM data.tags WHERE id NOT IN (SELECT tagid FROM tagged_images);
+"
+
+Q1="
+ATTACH DATABASE \"$LIBDB\" as lib;
+ATTACH DATABASE \"$DATADB\" as data;
+DELETE FROM data.tags WHERE id NOT IN (SELECT tagid FROM tagged_images);
+"
+
+if [ ! -f $LIBDB ]; then
+    echo missing \"$LIBDB\" file
+    exit 1
+fi
+
+if [ ! -f $DATADB ]; then
+    echo missing \"$DATADB\" file
+    exit 1
+fi
 
 if [ "$1" = "-p" ]; then
     echo Purging tags...
-    echo "$Q1C" | sqlite3 $DBFILE
-    echo "$Q1" | sqlite3 $DBFILE
+    echo "$Q1C" | sqlite3
+    echo "$Q1" | sqlite3
 else
-    echo "$Q1C" | sqlite3 $DBFILE
+    echo "$Q1C" | sqlite3
     echo
     echo to really purge from the database call:
     echo $0 -p
