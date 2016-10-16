darktable-org/darktable

commit 7d41b03e8403e21d8e07f4a6297b9998bf1ec12f

pixelpipe: duplicte module name into local variable in dt_dev_pixelpipe_process_rec()
aim: in case of crashes within dt_dev_pixelpipe_process_rec() we get easier access to the
the module context in which it happened

(cherry picked from commit ce77e71f8b698230b76832071408a2e90284655b)

src/develop/pixelpipe_hb.c
@@ -722,6 +722,7 @@ static int dt_dev_pixelpipe_process_rec(dt_dev_pixelpipe_t *pipe, dt_develop_t *
 {
   dt_iop_roi_t roi_in = *roi_out;
 
+  char module_name[256] = { 0 };
   void *input = NULL;
   void *cl_mem_input = NULL;
   *cl_mem_output = NULL;
@@ -738,6 +739,7 @@ static int dt_dev_pixelpipe_process_rec(dt_dev_pixelpipe_t *pipe, dt_develop_t *
                                           g_list_previous(modules), g_list_previous(pieces), pos - 1);
   }
 
+  if(module && module->op) strncpy(module_name, module->op, sizeof(module_name));
   const int bpp = get_output_bpp(module, pipe, piece, dev);
   *out_bpp = bpp;
   const size_t bufsize = (size_t)bpp * roi_out->width * roi_out->height;

commit 1aa17269dc132288a712bbd8a58cd00a9d9e9412

dt_dev_pixelpipe_process_rec(): fix clang build.
In file included from darktable/src/develop/pixelpipe.c:22:
darktable/src/develop/pixelpipe_hb.c:744:24: error: address of array 'module->op' will always evaluate to 'true' [-Werror,-Wpointer-bool-conversion]
  if(module && module->op) strncpy(module_name, module->op, sizeof(module_name));
            ~~ ~~~~~~~~^~

Was broken in 7d41b03e8403e21d8e07f4a6297b9998bf1ec12f ce77e71f8b698230b76832071408a2e90284655b.

(cherry picked from commit c2a424b2790e8e3059c50f4a1f93640019649f2b)

src/develop/pixelpipe_hb.c
@@ -739,7 +739,7 @@ static int dt_dev_pixelpipe_process_rec(dt_dev_pixelpipe_t *pipe, dt_develop_t *
                                           g_list_previous(modules), g_list_previous(pieces), pos - 1);
   }
 
-  if(module && module->op) strncpy(module_name, module->op, sizeof(module_name));
+  if(module) strncpy(module_name, module->op, sizeof(module_name));
   const int bpp = get_output_bpp(module, pipe, piece, dev);
   *out_bpp = bpp;
   const size_t bufsize = (size_t)bpp * roi_out->width * roi_out->height;