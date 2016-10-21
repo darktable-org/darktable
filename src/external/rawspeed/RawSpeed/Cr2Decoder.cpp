38d36de18dc3f40cd61aead3db80eead390edc94
Roman Lebedev <lebedev.ri@gmail.com>
2016-10-21 20:28:49 +0300

    Rawspeed: Cr2Decoder: bump version: 80D m/s-RAW
    Forgot about it in https://github.com/darktable-org/darktable/pull/1294
    
    FIXME: how are we supposed to keep track of this?!

--- src/external/rawspeed/RawSpeed/Cr2Decoder.cpp
+++ src/external/rawspeed/RawSpeed/Cr2Decoder.cpp
@@ -29,7 +29,7 @@ namespace RawSpeed {
 
 Cr2Decoder::Cr2Decoder(TiffIFD *rootIFD, FileMap* file) :
     RawDecoder(file), mRootIFD(rootIFD) {
-  decoderVersion = 6;
+  decoderVersion = 7;
 }
 
 Cr2Decoder::~Cr2Decoder(void) {
