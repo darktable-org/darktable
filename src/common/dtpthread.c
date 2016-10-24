e1b62a9b57d081da24f5cf40034d0026c0a77605
peterbud <peterbudai@hotmail.com>
2016-10-24 12:55:04 +0200

    Cleaning up BUILD.txt for Windows/MSYS2

--- src/common/dtpthread.c
+++ src/common/dtpthread.c
@@ -35,7 +35,7 @@
 int dt_pthread_create(pthread_t *thread, void *(*start_routine)(void *), void *arg)
 {
   int ret;
-  
+
   struct rlimit rlim = { 0 };
 
   ret = getrlimit(RLIMIT_STACK, &rlim);
@@ -58,7 +58,7 @@ int dt_pthread_create(pthread_t *thread, void *(*start_routine)(void *), void *a
 
     rlim.rlim_cur = WANTED_STACK_SIZE;
   }
-  
+
   pthread_attr_t attr;
 
   ret = pthread_attr_init(&attr);
@@ -90,7 +90,7 @@ int dt_pthread_create(pthread_t *thread, void *(*start_routine)(void *), void *a
       fprintf(stderr, "[dt_pthread_create] error: pthread_attr_setstacksize() returned %i\n", ret);
     }
   }
-  
+
   ret = pthread_create(thread, &attr, start_routine, arg);
 
   pthread_attr_destroy(&attr);
