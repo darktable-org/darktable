a114c7db429d6fb1e585c01ad6cc4bb792cdbc4f
peterbud <peterbudai@hotmail.com>
2016-10-25 23:10:22 +0200

    Also adding modelines

--- src/win/getdelim.h
+++ src/win/getdelim.h
@@ -24,3 +24,7 @@ ssize_t getdelim(char **lineptr, size_t *n, int delimiter, FILE *fp);
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 
 #endif //__GETDELIM_H__
+
+// modelines: These editor modelines have been set for all relevant files by tools/update_modelines.sh
+// vim: shiftwidth=2 expandtab tabstop=2 cindent
+// kate: tab-indents: off; indent-width 2; replace-tabs on; indent-mode cstyle; remove-trailing-spaces modified;
