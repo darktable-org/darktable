d7e5b6e91e4eb28be620838e46a18ce61557c133
peterbud <peterbudai@hotmail.com>
2016-10-25 19:54:03 +0200

    Clang formatting - getdelim

--- src/win/getdelim.c
+++ src/win/getdelim.c
@@ -1,28 +1,47 @@
-/* getdelim.c --- Implementation of replacement getdelim function.
-   Copyright (C) 1994, 1996-1998, 2001, 2003, 2005-2011 Free Software
-   Foundation, Inc.
+/*
+    This file is part of darktable,
+    copyright (c) 2009--2012 johannes hanika.
+    copyright (c) 2010--2012 tobias ellinghaus.
 
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 2, or (at
-   your option) any later version.
+    darktable is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-   This program is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
+    darktable is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-   02110-1301, USA.  */
+    You should have received a copy of the GNU General Public License
+    along with darktable.  If not, see <http://www.gnu.org/licenses/>.
+
+    getdelim.c --- Implementation of replacement getdelim function.
+
+    Copyright (C) 1994, 1996-1998, 2001, 2003, 2005-2011 Free Software
+    Foundation, Inc.
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 2, or (at
+    your option) any later version.
+
+    This program is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.  */
 
 /* Ported from glibc by Simon Josefsson. */
 
 #include <config.h>
 
 /* Don't use __attribute__ __nonnull__ in this compilation unit.  Otherwise gcc
-   optimizes away the lineptr == NULL || n == NULL || fp == NULL tests below.  */
+   optimizes away the lineptr == NULL || n == NULL || fp == NULL tests below. */
 #define _GL_ARG_NONNULL(params)
 
 #include <stdio.h>
@@ -33,20 +52,20 @@
 #include <errno.h>
 
 #ifndef SSIZE_MAX
-# define SSIZE_MAX ((ssize_t) (SIZE_MAX / 2))
+#define SSIZE_MAX ((ssize_t)(SIZE_MAX / 2))
 #endif
 
 #if USE_UNLOCKED_IO
-# include "unlocked-io.h"
-# define getc_maybe_unlocked(fp)        getc(fp)
+#include "unlocked-io.h"
+#define getc_maybe_unlocked(fp) getc(fp)
 #elif !HAVE_FLOCKFILE || !HAVE_FUNLOCKFILE || !HAVE_DECL_GETC_UNLOCKED
-# undef flockfile
-# undef funlockfile
-# define flockfile(x) ((void) 0)
-# define funlockfile(x) ((void) 0)
-# define getc_maybe_unlocked(fp)        getc(fp)
+#undef flockfile
+#undef funlockfile
+#define flockfile(x) ((void)0)
+#define funlockfile(x) ((void)0)
+#define getc_maybe_unlocked(fp) getc(fp)
 #else
-# define getc_maybe_unlocked(fp)        getc_unlocked(fp)
+#define getc_maybe_unlocked(fp) getc_unlocked(fp)
 #endif
 
 /* Read up to (and including) a DELIMITER from FP into *LINEPTR (and
@@ -55,89 +74,77 @@
    necessary.  Returns the number of characters read (not including
    the null terminator), or -1 on error or EOF.  */
 
-ssize_t
-getdelim (char **lineptr, size_t *n, int delimiter, FILE *fp)
-{
+ssize_t getdelim(char **lineptr, size_t *n, int delimiter, FILE *fp) {
   ssize_t result;
   size_t cur_len = 0;
 
-  if (lineptr == NULL || n == NULL || fp == NULL)
-    {
-      errno = EINVAL;
-      return -1;
+  if (lineptr == NULL || n == NULL || fp == NULL) {
+    errno = EINVAL;
+    return -1;
+  }
+
+  flockfile(fp);
+
+  if (*lineptr == NULL || *n == 0) {
+    char *new_lineptr;
+    *n = 120;
+    new_lineptr = (char *)realloc(*lineptr, *n);
+    if (new_lineptr == NULL) {
+      result = -1;
+      goto unlock_return;
+    }
+    *lineptr = new_lineptr;
+  }
+
+  for (;;) {
+    int i;
+
+    i = getc_maybe_unlocked(fp);
+    if (i == EOF) {
+      result = -1;
+      break;
     }
 
-  flockfile (fp);
-
-  if (*lineptr == NULL || *n == 0)
-    {
+    /* Make enough space for len+1 (for final NUL) bytes.  */
+    if (cur_len + 1 >= *n) {
+      size_t needed_max =
+          SSIZE_MAX < SIZE_MAX ? (size_t)SSIZE_MAX + 1 : SIZE_MAX;
+      size_t needed = 2 * *n + 1; /* Be generous. */
       char *new_lineptr;
-      *n = 120;
-      new_lineptr = (char *) realloc (*lineptr, *n);
-      if (new_lineptr == NULL)
-        {
-          result = -1;
-          goto unlock_return;
-        }
+
+      if (needed_max < needed)
+        needed = needed_max;
+      if (cur_len + 1 >= needed) {
+        result = -1;
+        errno = EOVERFLOW;
+        goto unlock_return;
+      }
+
+      new_lineptr = (char *)realloc(*lineptr, needed);
+      if (new_lineptr == NULL) {
+        result = -1;
+        goto unlock_return;
+      }
+
       *lineptr = new_lineptr;
+      *n = needed;
     }
 
-  for (;;)
-    {
-      int i;
+    (*lineptr)[cur_len] = i;
+    cur_len++;
 
-      i = getc_maybe_unlocked (fp);
-      if (i == EOF)
-        {
-          result = -1;
-          break;
-        }
-
-      /* Make enough space for len+1 (for final NUL) bytes.  */
-      if (cur_len + 1 >= *n)
-        {
-          size_t needed_max =
-            SSIZE_MAX < SIZE_MAX ? (size_t) SSIZE_MAX + 1 : SIZE_MAX;
-          size_t needed = 2 * *n + 1;   /* Be generous. */
-          char *new_lineptr;
-
-          if (needed_max < needed)
-            needed = needed_max;
-          if (cur_len + 1 >= needed)
-            {
-              result = -1;
-              errno = EOVERFLOW;
-              goto unlock_return;
-            }
-
-          new_lineptr = (char *) realloc (*lineptr, needed);
-          if (new_lineptr == NULL)
-            {
-              result = -1;
-              goto unlock_return;
-            }
-
-          *lineptr = new_lineptr;
-          *n = needed;
-        }
-
-      (*lineptr)[cur_len] = i;
-      cur_len++;
-
-      if (i == delimiter)
-        break;
-    }
+    if (i == delimiter)
+      break;
+  }
   (*lineptr)[cur_len] = '\0';
   result = cur_len ? cur_len : result;
 
- unlock_return:
-  funlockfile (fp); /* doesn't set errno */
+unlock_return:
+  funlockfile(fp); /* doesn't set errno */
 
   return result;
 }
 
-ssize_t
-getline (char **lineptr, size_t *n, FILE *stream)
-{
-  return getdelim (lineptr, n, '\n', stream);
+ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
+  return getdelim(lineptr, n, '\n', stream);
 }
--- src/win/getdelim.h
+++ src/win/getdelim.h
@@ -1,7 +1,26 @@
+/*
+    This file is part of darktable,
+    copyright (c) 2009--2012 johannes hanika.
+    copyright (c) 2010--2012 tobias ellinghaus.
+
+    darktable is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    darktable is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with darktable.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef __GETDELIM_H__
 #define __GETDELIM_H__
 
-ssize_t getdelim (char **lineptr, size_t *n, int delimiter, FILE *fp);
-ssize_t getline (char **lineptr, size_t *n, FILE *stream);
+ssize_t getdelim(char **lineptr, size_t *n, int delimiter, FILE *fp);
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 
 #endif //__GETDELIM_H__
