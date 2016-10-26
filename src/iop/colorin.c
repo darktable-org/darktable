darktable-org/darktable

commit b54d7f3418fd34ae3bc6539cdd28bf18137d304f

image: add a new flag for 4 color bayer images
Instead of testing the filter pattern everywhere add a new
flag to signify that the image is a 4 color bayer and thus
is handled differently throughout the pipe.

src/iop/colorin.c
@@ -1042,7 +1042,7 @@ void reload_defaults(dt_iop_module_t *module)
     use_eprofile = TRUE; // the image has a profile assigned
   dt_image_cache_write_release(darktable.image_cache, img, DT_IMAGE_CACHE_RELAXED);
 
-  if(img->filters == 0xb4b4b4b4 || img->filters == 0x9c9c9c9c)
+  if(img->flags & DT_IMAGE_4BAYER) // 4Bayer images have been pre-converted to rec2020
     tmp.type = DT_COLORSPACE_LIN_REC2020;
   else if(use_eprofile)
     tmp.type = DT_COLORSPACE_EMBEDDED_ICC;
@@ -1120,8 +1120,7 @@ static void update_profile_list(dt_iop_module_t *self)
   else
     dt_dcraw_adobe_coeff(self->dev->image_storage.camera_makermodel, (float(*)[12])cam_xyz);
 
-  if(!isnan(cam_xyz[0]) && self->dev->image_storage.filters != 0xb4b4b4b4
-                        && self->dev->image_storage.filters != 0x9c9c9c9c)
+  if(!isnan(cam_xyz[0]) && !(self->dev->image_storage.flags & DT_IMAGE_4BAYER))
   {
     prof = (dt_colorspaces_color_profile_t *)calloc(1, sizeof(dt_colorspaces_color_profile_t));
     g_strlcpy(prof->name, dt_colorspaces_get_name(DT_COLORSPACE_STANDARD_MATRIX, ""), sizeof(prof->name));

src/common/image.h
@@ -65,7 +65,9 @@ typedef enum
   // image has an associated .txt file for overlay
   DT_IMAGE_HAS_TXT = 4096,
   // image has an associated wav file
-  DT_IMAGE_HAS_WAV = 8192
+  DT_IMAGE_HAS_WAV = 8192,
+  // image is a bayer pattern with 4 colors (e.g., CYGM or RGBE)
+  DT_IMAGE_4BAYER = 16384,
 } dt_image_flags_t;
 
 typedef enum dt_image_colorspace_t

src/common/imageio_rawspeed.cc
@@ -245,6 +245,8 @@ dt_imageio_retval_t dt_imageio_open_rawspeed(dt_image_t *img, const char *filena
 
     img->bpp = r->getBpp();
     img->filters = r->cfa.getDcrawFilter();
+    if (img->filters == 0xb4b4b4b4 || img->filters == 0x9c9c9c9c)
+      img->flags |= DT_IMAGE_4BAYER;
     if(img->filters)
     {
       img->flags &= ~DT_IMAGE_LDR;

src/iop/demosaic.c
@@ -1561,7 +1561,7 @@ void process(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, void *
       piece->pipe->type == DT_DEV_PIXELPIPE_EXPORT ||
       uhq_thumb ||
       roi_out->scale > (data->filters == 9u ? 0.333f : 0.5f) ||
-      img->filters == 0xb4b4b4b4 || img->filters == 0x9c9c9c9c;
+      (img->flags & DT_IMAGE_4BAYER); // half_size_f doesn't support 4bayer images
 
   // we check if we can stop at the linear interpolation step in VNG
   // instead of going the full way
@@ -1607,7 +1607,7 @@ void process(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, void *
     {
       float *in = (float *)pixels;
 
-      if(img->filters != 0xb4b4b4b4 && img->filters != 0x9c9c9c9c && data->green_eq != DT_IOP_GREEN_EQ_NO)
+      if(!(img->flags & DT_IMAGE_4BAYER) && data->green_eq != DT_IOP_GREEN_EQ_NO)
       {
         in = (float *)dt_alloc_align(16, (size_t)roi_in->height * roi_in->width * sizeof(float));
         switch(data->green_eq)
@@ -1631,10 +1631,10 @@ void process(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, void *
 
       if(demosaicing_method == DT_IOP_DEMOSAIC_PASSTHROUGH_MONOCHROME)
         passthrough_monochrome(tmp, in, &roo, &roi);
-      else if(demosaicing_method == DT_IOP_DEMOSAIC_VNG4 || img->filters == 0xb4b4b4b4 || img->filters == 0x9c9c9c9c)
+      else if(demosaicing_method == DT_IOP_DEMOSAIC_VNG4 || (img->flags & DT_IMAGE_4BAYER))
       {
         vng_interpolate(tmp, in, &roo, &roi, data->filters, img->xtrans, only_vng_linear);
-        if (img->filters == 0xb4b4b4b4 || img->filters == 0x9c9c9c9c)
+        if (img->flags & DT_IMAGE_4BAYER)
           dt_colorspaces_cygm_to_rgb(tmp, roo.width*roo.height, img->camera_makermodel);
       }
       else if(demosaicing_method != DT_IOP_DEMOSAIC_AMAZE)

commit 282e594dfc17380ca20d8c5bed1983c911085c0f

Colorin iop: reload_defaults() really don't acess img after releasing it.
Was broken in b54d7f3418fd34ae3bc6539cdd28bf18137d304f.

src/iop/colorin.c
@@ -1754,7 +1754,6 @@ void reload_defaults(dt_iop_module_t *module)
   }
   else
     use_eprofile = TRUE; // the image has a profile assigned
-  dt_image_cache_write_release(darktable.image_cache, img, DT_IMAGE_CACHE_RELAXED);
 
   if(img->flags & DT_IMAGE_4BAYER) // 4Bayer images have been pre-converted to rec2020
     tmp.type = DT_COLORSPACE_LIN_REC709;
@@ -1768,6 +1767,8 @@ void reload_defaults(dt_iop_module_t *module)
     tmp.type = DT_COLORSPACE_SRGB;
   else if(!isnan(module->dev->image_storage.d65_color_matrix[0]))
     tmp.type = DT_COLORSPACE_EMBEDDED_MATRIX;
+
+  dt_image_cache_write_release(darktable.image_cache, img, DT_IMAGE_CACHE_RELAXED);
 
 end:
   memcpy(module->params, &tmp, sizeof(dt_iop_colorin_params_t));