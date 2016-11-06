7f194ff5d3755d868f59c8b315dcd45afe6f0262
Matthieu Moy <Matthieu.Moy@imag.fr>
2016-11-06 18:56:31 +0100

    drawn masks: make equidistant/proportional feathering discoverable
    The "shift+click" shortcut is hard to discover, let dt hint the user as
    it does for other similar shortcuts. Keep the message short and just
    talk about "feathering mode" without mentionning "equidistant" and
    "proportional", which users probably wouldn't understand without further
    explanations. Just let users either learn by experiment or read the
    manual for details.

--- src/develop/masks/masks.c
+++ src/develop/masks/masks.c
@@ -64,7 +64,7 @@ static void _set_hinter_message(dt_masks_form_gui_t *gui, dt_masks_type_t formty
     if(gui->point_selected >= 0)
       g_strlcat(msg, _("ctrl+click to rotate"), sizeof(msg));
     else if(gui->form_selected)
-      g_strlcat(msg, _("ctrl+scroll to set shape opacity"), sizeof(msg));
+      g_strlcat(msg, _("shift+click to switch feathering mode, ctrl+scroll to set shape opacity"), sizeof(msg));
   }
   else if(formtype & DT_MASKS_BRUSH)
   {
