ca91fbc06355ace7e7e5f275b1a23d2ecc5bcab2
Roman Lebedev <lebedev.ri@gmail.com>
2016-11-07 20:46:05 +0300

    _darkroom_ui_apply_style_popupmenu(): gtk_menu_popup() deprecated

--- src/views/darkroom.c
+++ src/views/darkroom.c
@@ -950,7 +950,11 @@ static void _darkroom_ui_apply_style_popupmenu(GtkWidget *w, gpointer user_data)
   /* if we got any styles, lets popup menu for selection */
   if(menu)
   {
+#if GTK_CHECK_VERSION(3, 22, 0)
+    gtk_menu_popup_at_pointer(GTK_MENU(menu), NULL);
+#else
     gtk_menu_popup(GTK_MENU(menu), NULL, NULL, NULL, NULL, 0, 0);
+#endif
   }
   else
     dt_control_log(_("no styles have been created yet"));
