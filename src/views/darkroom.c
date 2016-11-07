5b5f8257f4466987d1fb943a9aac1cac56994b80
Roman Lebedev <lebedev.ri@gmail.com>
2016-11-07 20:45:04 +0300

    _darkroom_ui_favorite_presets_popupmenu(): gtk_menu_popup() deprecated

--- src/views/darkroom.c
+++ src/views/darkroom.c
@@ -885,7 +885,12 @@ static void _darkroom_ui_favorite_presets_popupmenu(GtkWidget *w, gpointer user_
   if(darktable.gui->presets_popup_menu)
   {
     gtk_widget_show_all(GTK_WIDGET(darktable.gui->presets_popup_menu));
+
+#if GTK_CHECK_VERSION(3, 22, 0)
+    gtk_menu_popup_at_pointer(darktable.gui->presets_popup_menu, NULL);
+#else
     gtk_menu_popup(darktable.gui->presets_popup_menu, NULL, NULL, NULL, NULL, 0, 0);
+#endif
   }
   else
     dt_control_log(_("no userdefined presets for favorite modules were found"));
