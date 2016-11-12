ee5d3fe55493bc7481e3b08f86d03b0481b550db
peterbud <peterbudai@hotmail.com>
2016-11-12 16:21:54 +0100

    Windows build: renaming CI script To clearly mark the designated purpose

--- ci-script-windows.sh
+++ ci-script-windows.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+#    This file is part of darktable.
+#
+#    darktable is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    darktable is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with darktable.  If not, see <http://www.gnu.org/licenses/>.
 
-# Continuous Integration Library for darktable
+# Continuous Integration script for darktable
 # Author: Peter Budai <peterbudai@hotmail.com>
+
+# it is supposed to be run by appveyor-ci
 
 # Enable colors
 normal=$(tput sgr0)
