3bd91095a3e574b5cbbb5b0dc6618a69e1a0ea48
parafin <parafin@paraf.in>
2016-12-07 02:46:06 +0300

    fix tools/parse_version_c.sh on Mac (incompatible sed)

--- tools/parse_version_c.sh
+++ tools/parse_version_c.sh
@@ -6,7 +6,7 @@ H_FILE=$1
 
 # version.c exists => check if it containts the up-to-date version
 if [ -f "$H_FILE" ]; then
-  OLD_VERSION=`cat "$H_FILE" | tr '\n' ' ' | sed s/\"/\\\n/g | sed '4q;d'`
+  OLD_VERSION=`tr '"' '\n' < "$H_FILE" | sed '7q;d'`
 
   echo $OLD_VERSION
 fi
