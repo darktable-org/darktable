darktable-org/darktable

commit 12c55131cbff42eda4fd0a3f7ae69c47f72c690c

histogram: use all 8-bit of the output, not just 6.

src/develop/pixelpipe_hb.c
@@ -2018,7 +2018,7 @@ static int dt_dev_pixelpipe_process_rec(dt_dev_pixelpipe_t *pipe, dt_develop_t *
         box[3] = roi_out->height - 1;
       }
       dev->histogram_max = 0;
-      memset(dev->histogram, 0, sizeof(uint32_t) * 4 * 64);
+      memset(dev->histogram, 0, sizeof(uint32_t) * 4 * 256);
 
       {
         uint8_t *pixel = (uint8_t *)*output;
@@ -2026,7 +2026,7 @@ static int dt_dev_pixelpipe_process_rec(dt_dev_pixelpipe_t *pipe, dt_develop_t *
           for(int i = box[0]; i <= box[2]; i += 4)
           {
             uint8_t rgb[3];
-            for(int k = 0; k < 3; k++) rgb[k] = pixel[4 * j * roi_out->width + 4 * i + 2 - k] >> 2;
+            for(int k = 0; k < 3; k++) rgb[k] = pixel[4 * j * roi_out->width + 4 * i + 2 - k];
 
             for(int k = 0; k < 3; k++) dev->histogram[4 * rgb[k] + k]++;
             uint8_t lum = MAX(MAX(rgb[0], rgb[1]), rgb[2]);
@@ -2035,7 +2035,7 @@ static int dt_dev_pixelpipe_process_rec(dt_dev_pixelpipe_t *pipe, dt_develop_t *
       }
 
       // don't count <= 0 pixels
-      for(int k = 19; k < 4 * 64; k += 4)
+      for(int k = 19; k < 4 * 256; k += 4)
         dev->histogram_max = dev->histogram_max > dev->histogram[k] ? dev->histogram_max : dev->histogram[k];
 
       // calculate the waveform histogram. since this is drawn pixel by pixel we have to do it in the correct

src/gui/draw.h
@@ -227,8 +227,8 @@ static inline int dt_draw_curve_add_point(dt_draw_curve_t *c, const float x, con
 static inline void dt_draw_histogram_8_linear(cairo_t *cr, uint32_t *hist, int32_t channel)
 {
   cairo_move_to(cr, 0, 0);
-  for(int k = 0; k < 64; k++) cairo_line_to(cr, k, hist[4 * k + channel]);
-  cairo_line_to(cr, 63, 0);
+  for(int k = 0; k < 256; k++) cairo_line_to(cr, k, hist[4 * k + channel]);
+  cairo_line_to(cr, 255, 0);
   cairo_close_path(cr);
   cairo_fill(cr);
 }
@@ -236,8 +236,8 @@ static inline void dt_draw_histogram_8_linear(cairo_t *cr, uint32_t *hist, int32
 static inline void dt_draw_histogram_8_log(cairo_t *cr, uint32_t *hist, int32_t channel)
 {
   cairo_move_to(cr, 0, 0);
-  for(int k = 0; k < 64; k++) cairo_line_to(cr, k, logf(1.0 + hist[4 * k + channel]));
-  cairo_line_to(cr, 63, 0);
+  for(int k = 0; k < 256; k++) cairo_line_to(cr, k, logf(1.0 + hist[4 * k + channel]));
+  cairo_line_to(cr, 255, 0);
   cairo_close_path(cr);
   cairo_fill(cr);
 }

src/libs/histogram.c
@@ -376,7 +376,7 @@ static gboolean _lib_histogram_draw_callback(GtkWidget *widget, cairo_t *crf, gp
     {
       // cairo_set_antialias(cr, CAIRO_ANTIALIAS_NONE);
       cairo_translate(cr, 0, height);
-      cairo_scale(cr, width / 63.0, -(height - 10) / hist_max);
+      cairo_scale(cr, width / 255.0, -(height - 10) / hist_max);
       cairo_set_operator(cr, CAIRO_OPERATOR_ADD);
       // cairo_set_operator(cr, CAIRO_OPERATOR_OVER);
       cairo_set_line_width(cr, 1.);

commit 0f7a641d81c635298a3de6ef8dcd880e86f44bd9

Histogram params: default to 256 bins. Fixes #11376
Was broken by 12c55131cbff42eda4fd0a3f7ae69c47f72c690c.

src/develop/pixelpipe_hb.c
@@ -217,7 +217,7 @@ void dt_dev_pixelpipe_create_nodes(dt_dev_pixelpipe_t *pipe, dt_develop_t *dev)
       piece->enabled = module->enabled;
       piece->request_histogram = DT_REQUEST_ONLY_IN_GUI;
       piece->histogram_params.roi = NULL;
-      piece->histogram_params.bins_count = 64;
+      piece->histogram_params.bins_count = 256;
       piece->histogram_stats.bins_count = 0;
       piece->histogram_stats.pixels = 0;
       piece->colors

src/iop/levels.c
@@ -395,7 +395,7 @@ void commit_params(dt_iop_module_t *self, dt_iop_params_t *p1, dt_dev_pixelpipe_
 
   piece->request_histogram |= (DT_REQUEST_ONLY_IN_GUI);
 
-  piece->histogram_params.bins_count = 64;
+  piece->histogram_params.bins_count = 256;
 
   if(p->mode == LEVELS_MODE_AUTOMATIC)
   {