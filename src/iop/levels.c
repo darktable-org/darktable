darktable-org/darktable

commit 0f7a641d81c635298a3de6ef8dcd880e86f44bd9

Histogram params: default to 256 bins. Fixes #11376
Was broken by 12c55131cbff42eda4fd0a3f7ae69c47f72c690c.

src/iop/levels.c
@@ -395,7 +395,7 @@ void commit_params(dt_iop_module_t *self, dt_iop_params_t *p1, dt_dev_pixelpipe_
 
   piece->request_histogram |= (DT_REQUEST_ONLY_IN_GUI);
 
-  piece->histogram_params.bins_count = 64;
+  piece->histogram_params.bins_count = 256;
 
   if(p->mode == LEVELS_MODE_AUTOMATIC)
   {

src/develop/pixelpipe_hb.c
@@ -217,7 +217,7 @@ void dt_dev_pixelpipe_create_nodes(dt_dev_pixelpipe_t *pipe, dt_develop_t *dev)
       piece->enabled = module->enabled;
       piece->request_histogram = DT_REQUEST_ONLY_IN_GUI;
       piece->histogram_params.roi = NULL;
-      piece->histogram_params.bins_count = 64;
+      piece->histogram_params.bins_count = 256;
       piece->histogram_stats.bins_count = 0;
       piece->histogram_stats.pixels = 0;
       piece->colors

commit 5c6f920ddbe5fa7ce69dc31dd815be47e43021bc

Levels iop: actually use all 255 bins
Was broken in 0f7a641d81c635298a3de6ef8dcd880e86f44bd9
and 12c55131cbff42eda4fd0a3f7ae69c47f72c690c.

src/iop/levels.c
@@ -1,7 +1,7 @@
 /*
     This file is part of darktable,
     copyright (c) 2009--2011 johannes hanika.
-    copyright (c) 2014-2015 LebedevRI.
+    copyright (c) 2014-2016 Roman Lebedev.
 
     darktable is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -156,20 +156,20 @@ static void dt_iop_levels_compute_levels_manual(const uint32_t *histogram, float
   if(!histogram) return;
 
   // search histogram for min (search from bottom)
-  for(int k = 0; k <= 4 * 63; k += 4)
+  for(int k = 0; k <= 4 * 255; k += 4)
   {
     if(histogram[k] > 1)
     {
-      levels[0] = ((float)(k) / (4 * 64));
+      levels[0] = ((float)(k) / (4 * 256));
       break;
     }
   }
   // then for max (search from top)
-  for(int k = 4 * 63; k >= 0; k -= 4)
+  for(int k = 4 * 255; k >= 0; k -= 4)
   {
     if(histogram[k] > 1)
     {
-      levels[2] = ((float)(k) / (4 * 64));
+      levels[2] = ((float)(k) / (4 * 256));
       break;
     }
   }
@@ -841,7 +841,7 @@ static gboolean dt_iop_levels_area_draw(GtkWidget *widget, cairo_t *crf, gpointe
     if(hist && hist_max > 0.0f)
     {
       cairo_save(cr);
-      cairo_scale(cr, width / 63.0, -(height - DT_PIXEL_APPLY_DPI(5)) / hist_max);
+      cairo_scale(cr, width / 255.0, -(height - DT_PIXEL_APPLY_DPI(5)) / hist_max);
       cairo_set_source_rgba(cr, .2, .2, .2, 0.5);
       dt_draw_histogram_8(cr, hist, 0, dev->histogram_type == DT_DEV_HISTOGRAM_LINEAR); // TODO: make draw
                                                                                         // handle waveform