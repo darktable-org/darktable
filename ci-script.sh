162cf6eaee60d9300ba85ec8e4e48c1e155e9198
Roman Lebedev <lebedev.ri@gmail.com>
2017-04-12 09:36:44 +0300

    CI: linux clang-3.9 build: without OpenMP, without OpenCL.
    Would have caught the bug that was fixed in previous commit.

--- ci-script.sh
+++ ci-script.sh
@@ -23,6 +23,7 @@
 #   SRC_DIR - read-only directory with git checkout to compile
 #   CC, CXX, CFLAGS, CXXFLAGS are not required, should make sense too
 #   TARGET - either build or usermanual
+#   ECO - some other flags for cmake
 
 set -ex
 
@@ -52,7 +53,7 @@ du -hcs "$BUILD_DIR"
 du -hcs "$INSTALL_PREFIX"
 
 cd "$BUILD_DIR"
-cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DVALIDATE_APPDATA_FILE=On "$SRC_DIR" || (cat "$BUILD_DIR"/CMakeFiles/CMakeOutput.log; cat "$BUILD_DIR"/CMakeFiles/CMakeError.log)
+cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo $ECO -DVALIDATE_APPDATA_FILE=On "$SRC_DIR" || (cat "$BUILD_DIR"/CMakeFiles/CMakeOutput.log; cat "$BUILD_DIR"/CMakeFiles/CMakeError.log)
 
 case "$TARGET" in
   "usermanual")
