82805876e76ace64cd0dca011be71e35493c9d01
Roman Lebedev <lebedev.ri@gmail.com>
2017-05-04 22:08:21 +0300

    parse_version_c.sh: update according to changes in eec27ddf7cd4643b01eef6b22015d092fea68cf8
    Fixes constant rebuilds when NO changes at all happened.

--- tools/parse_version_c.sh
+++ tools/parse_version_c.sh
@@ -6,7 +6,7 @@ H_FILE=$1
 
 # version.c exists => check if it containts the up-to-date version
 if [ -f "$H_FILE" ]; then
-  OLD_VERSION=`tr '"' '\n' < "$H_FILE" | sed '7q;d'`
+  OLD_VERSION=`tr '"' '\n' < "$H_FILE" | sed '8q;d'`
 
   echo $OLD_VERSION
 fi
