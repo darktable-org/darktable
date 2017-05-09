db096f6947b344a4a9e0549adb3828c2363ccca3
Tobias Ellinghaus <me@houz.org>
2017-05-09 12:48:52 +0200

    Simplify finding somem system paths
    Instead of setting pathnames for temp and cache folders from cmake we
    now use the defaults as provided by glib. Same for config dir. On Linux
    that should give the same results (*), on Windows however it behaves
    nicer as it won't add .* to the user directory. Instead we write to
    %AppData%\Local\darktable\.
    
    (*) There is one difference: We no longer use the non-standard
    ~/.local/tmp/ as our temp dir. Instead we honour the system settings.

--- src/config.cmake.h
+++ src/config.cmake.h
@@ -14,8 +14,6 @@ extern const char darktable_last_commit_year[];
 
 #define GETTEXT_PACKAGE "darktable"
 #define DARKTABLE_LOCALEDIR "@CMAKE_INSTALL_FULL_LOCALEDIR@"
-#define DARKTABLE_TMPDIR "@TMP_DIR@"
-#define DARKTABLE_CACHEDIR "@CACHE_DIR@"
 
 #define DARKTABLE_LIBDIR "@CMAKE_INSTALL_FULL_LIBDIR@/darktable"
 #define DARKTABLE_DATADIR "@CMAKE_INSTALL_FULL_DATAROOTDIR@/darktable"
