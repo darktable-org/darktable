9e158056e2d92498a1a2332f11881483e72d7669
Roman Lebedev <lebedev.ri@gmail.com>
2017-05-10 19:28:32 +0300

    git version string .sh: some shellcheck/checkbashisms, fix building in path with spaces

--- tools/get_last_commit_year.sh
+++ tools/get_last_commit_year.sh
@@ -3,7 +3,7 @@
 DT_SRC_DIR=$(dirname "$0")
 DT_SRC_DIR=$(cd "$DT_SRC_DIR/../" && pwd -P)
 
-LAST_COMMIT_YEAR=`git --git-dir="${DT_SRC_DIR}/.git" log -n1 --pretty=%ci`
+LAST_COMMIT_YEAR=$(git --git-dir="${DT_SRC_DIR}/.git" log -n1 --pretty=%ci)
 
 if [ $? -eq 0 ]; then
   echo "${LAST_COMMIT_YEAR}" | cut -b 1-4
