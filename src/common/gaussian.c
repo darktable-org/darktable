a7a0a31951d352d5cf8c2b8bd30ff8e0c7085e16
Ulrich Pegelow <ulrich.pegelow@tongareva.de>
2017-06-21 06:55:08 +0200

    opencl: some cleanup

--- src/common/gaussian.c
+++ src/common/gaussian.c
@@ -31,7 +31,7 @@
 #define MMCLAMPPS(a, mn, mx) (_mm_min_ps((mx), _mm_max_ps((a), (mn))))
 #endif
 
-#define BLOCKSIZE 32
+#define BLOCKSIZE 64
 
 static void compute_gauss_params(const float sigma, dt_gaussian_order_t order, float *a0, float *a1,
                                  float *a2, float *a3, float *b1, float *b2, float *coefp, float *coefn)
