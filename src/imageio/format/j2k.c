darktable-org/darktable

commit 5f13d2527398eb82591fea828bbee7f13f1cc8f7

Add a way to control style handling in the export module.
It is now possible to select whether the style module are to be
appended to the current picture history or as before just replacing
the corresponding module in the history.

src/imageio/format/j2k.c
@@ -82,7 +82,7 @@ typedef enum
 #define DOWNSAMPLE_FLOAT_TO_16BIT(_val)                                                                      \
   (_val) <= 0.0f ? 0 : ((_val) >= 1.0f ? 65535 : (int)(65535.0f * (_val)))
 
-DT_MODULE(1)
+DT_MODULE(2)
 
 typedef enum
 {
@@ -97,6 +97,7 @@ typedef struct dt_imageio_j2k_t
   int max_width, max_height;
   int width, height;
   char style[128];
+  gboolean style_append;
   int bpp;
   dt_imageio_j2k_format_t format;
   dt_imageio_j2k_preset_t preset;
@@ -527,6 +528,43 @@ int write_image(dt_imageio_module_data_t *j2k_tmp, const char *filename, const v
 size_t params_size(dt_imageio_module_format_t *self)
 {
   return sizeof(dt_imageio_j2k_t);
+}
+
+void *legacy_params(dt_imageio_module_format_t *self, const void *const old_params,
+                    const size_t old_params_size, const int old_version, const int new_version,
+                    size_t *new_size)
+{
+  if(old_version == 1 && new_version == 2)
+  {
+    typedef struct dt_imageio_j2k_v1_t
+    {
+      int max_width, max_height;
+      int width, height;
+      char style[128];
+      gboolean style_append;
+      int bpp;
+      dt_imageio_j2k_format_t format;
+      dt_imageio_j2k_preset_t preset;
+      int quality;
+    } dt_imageio_j2k_v1_t;
+
+    dt_imageio_j2k_v1_t *o = (dt_imageio_j2k_v1_t *)old_params;
+    dt_imageio_j2k_t *n = (dt_imageio_j2k_t *)malloc(sizeof(dt_imageio_j2k_t));
+
+    n->max_width = o->max_width;
+    n->max_height = o->max_height;
+    n->width = o->width;
+    n->height = o->height;
+    g_strlcpy(n->style, o->style, sizeof(o->style));
+    n->style_append = 0;
+    n->bpp = o->bpp;
+    n->format = o->format;
+    n->preset = o->preset;
+    n->quality = o->quality;
+    *new_size = self->params_size(self);
+    return n;
+  }
+  return NULL;
 }
 
 void *get_params(dt_imageio_module_format_t *self)

src/imageio/format/png.c
@@ -36,13 +36,14 @@
 #include "dtgtk/slider.h"
 #include "common/imageio_format.h"
 
-DT_MODULE(1)
+DT_MODULE(2)
 
 typedef struct dt_imageio_png_t
 {
   int max_width, max_height;
   int width, height;
   char style[128];
+  gboolean style_append;
   int bpp;
   FILE *f;
   png_structp png_ptr;
@@ -350,6 +351,42 @@ size_t params_size(dt_imageio_module_format_t *self)
   return sizeof(dt_imageio_module_data_t) + sizeof(int);
 }
 
+void *legacy_params(dt_imageio_module_format_t *self, const void *const old_params,
+                    const size_t old_params_size, const int old_version, const int new_version,
+                    size_t *new_size)
+{
+  if(old_version == 1 && new_version == 2)
+  {
+    typedef struct dt_imageio_png_v1_t
+    {
+      int max_width, max_height;
+      int width, height;
+      char style[128];
+      gboolean style_append;
+      int bpp;
+      FILE *f;
+      png_structp png_ptr;
+      png_infop info_ptr;
+    } dt_imageio_png_v1_t;
+
+    dt_imageio_png_v1_t *o = (dt_imageio_png_v1_t *)old_params;
+    dt_imageio_png_t *n = (dt_imageio_png_t *)malloc(sizeof(dt_imageio_png_t));
+
+    n->max_width = o->max_width;
+    n->max_height = o->max_height;
+    n->width = o->width;
+    n->height = o->height;
+    g_strlcpy(n->style, o->style, sizeof(o->style));
+    n->style_append = 0;
+    n->bpp = o->bpp;
+    n->f = o->f;
+    n->png_ptr = o->png_ptr;
+    n->info_ptr = o->info_ptr;
+    *new_size = self->params_size(self);
+    return n;
+  }
+  return NULL;
+}
 void *get_params(dt_imageio_module_format_t *self)
 {
   dt_imageio_png_t *d = (dt_imageio_png_t *)calloc(1, sizeof(dt_imageio_png_t));

src/imageio/format/tiff.c
@@ -30,13 +30,14 @@
 #include "common/imageio_format.h"
 #define DT_TIFFIO_STRIPE 64
 
-DT_MODULE(1)
+DT_MODULE(2)
 
 typedef struct dt_imageio_tiff_t
 {
   int max_width, max_height;
   int width, height;
   char style[128];
+  gboolean style_append;
   int bpp;
   int compress;
   TIFF *handle;
@@ -278,6 +279,40 @@ size_t params_size(dt_imageio_module_format_t *self)
   return sizeof(dt_imageio_tiff_t) - sizeof(TIFF *);
 }
 
+void *legacy_params(dt_imageio_module_format_t *self, const void *const old_params,
+                    const size_t old_params_size, const int old_version, const int new_version,
+                    size_t *new_size)
+{
+  if(old_version == 1 && new_version == 2)
+  {
+    typedef struct dt_imageio_tiff_v1_t
+    {
+      int max_width, max_height;
+      int width, height;
+      char style[128];
+      gboolean style_append;
+      int bpp;
+      int compress;
+      TIFF *handle;
+    } dt_imageio_tiff_v1_t;
+
+    dt_imageio_tiff_v1_t *o = (dt_imageio_tiff_v1_t *)old_params;
+    dt_imageio_tiff_t *n = (dt_imageio_tiff_t *)malloc(sizeof(dt_imageio_tiff_t));
+
+    n->max_width = o->max_width;
+    n->max_height = o->max_height;
+    n->width = o->width;
+    n->height = o->height;
+    g_strlcpy(n->style, o->style, sizeof(o->style));
+    n->style_append = 0;
+    n->bpp = o->bpp;
+    n->compress = o->compress;
+    n->handle = o->handle;
+    *new_size = self->params_size(self);
+    return n;
+  }
+  return NULL;
+}
 void *get_params(dt_imageio_module_format_t *self)
 {
   dt_imageio_tiff_t *d = (dt_imageio_tiff_t *)calloc(1, sizeof(dt_imageio_tiff_t));

src/imageio/format/webp.c
@@ -32,7 +32,7 @@
 
 #include <webp/encode.h>
 
-DT_MODULE(1)
+DT_MODULE(2)
 
 typedef enum
 {
@@ -55,6 +55,7 @@ typedef struct dt_imageio_webp_t
   int max_width, max_height;
   int width, height;
   char style[128];
+  gboolean style_append;
   int comp_type;
   int quality;
   int hint;
@@ -181,6 +182,40 @@ size_t params_size(dt_imageio_module_format_t *self)
   return sizeof(dt_imageio_webp_t);
 }
 
+void *legacy_params(dt_imageio_module_format_t *self, const void *const old_params,
+                    const size_t old_params_size, const int old_version, const int new_version,
+                    size_t *new_size)
+{
+  if(old_version == 1 && new_version == 2)
+  {
+    typedef struct dt_imageio_webp_v1_t
+    {
+      int max_width, max_height;
+      int width, height;
+      char style[128];
+      gboolean style_append;
+      int comp_type;
+      int quality;
+      int hint;
+    } dt_imageio_webp_v1_t;
+
+    dt_imageio_webp_v1_t *o = (dt_imageio_webp_v1_t *)old_params;
+    dt_imageio_webp_t *n = (dt_imageio_webp_t *)malloc(sizeof(dt_imageio_webp_t));
+
+    n->max_width = o->max_width;
+    n->max_height = o->max_height;
+    n->width = o->width;
+    n->height = o->height;
+    g_strlcpy(n->style, o->style, sizeof(o->style));
+    n->style_append = 0;
+    n->comp_type = o->comp_type;
+    n->quality = o->quality;
+    n->hint = o->hint;
+    *new_size = self->params_size(self);
+    return n;
+  }
+  return NULL;
+}
 void *get_params(dt_imageio_module_format_t *self)
 {
   dt_imageio_webp_t *d = (dt_imageio_webp_t *)calloc(1, sizeof(dt_imageio_webp_t));

src/cli/main.c
@@ -274,6 +274,7 @@ int main(int argc, char *arg[])
   fdata->max_width = (w != 0 && fdata->max_width > w) ? w : fdata->max_width;
   fdata->max_height = (h != 0 && fdata->max_height > h) ? h : fdata->max_height;
   fdata->style[0] = '\0';
+  fdata->style_append = 0;
 
   if(storage->initialize_store)
   {

src/common/imageio.c
@@ -676,13 +676,20 @@ int dt_imageio_export_with_flags(const uint32_t imgid, const char *filename,
         if(strncmp(m->op, s->name, strlen(m->op)) == 0)
         {
           dt_dev_history_item_t *h = malloc(sizeof(dt_dev_history_item_t));
+          dt_iop_module_t *sty_module = m;
+
+          if (format_params->style_append && !(m->flags() & IOP_FLAGS_ONE_INSTANCE))
+          {
+            sty_module = dt_dev_module_duplicate(m->dev, m, 0);
+            if(!sty_module) return 1;
+          }
 
           h->params = s->params;
           h->blend_params = s->blendop_params;
           h->enabled = s->enabled;
-          h->module = m;
+          h->module = sty_module;
           h->multi_priority = 1;
-          g_strlcpy(h->multi_name, "", sizeof(h->multi_name));
+          g_strlcpy(h->multi_name, "<style>", sizeof(h->multi_name));
 
           if(m->legacy_params && (s->module_version != m->version()))
           {

src/common/imageio_module.h
@@ -52,6 +52,7 @@ typedef struct dt_imageio_module_data_t
   int max_width, max_height;
   int width, height;
   char style[128];
+  gboolean style_append;
 } dt_imageio_module_data_t;
 
 struct dt_imageio_module_format_t;

src/control/jobs/control_jobs.c
@@ -957,6 +957,7 @@ static int32_t dt_control_export_job_run(dt_job_t *job)
     fdata->max_width = (w != 0 && fdata->max_width > w) ? w : fdata->max_width;
     fdata->max_height = (h != 0 && fdata->max_height > h) ? h : fdata->max_height;
     g_strlcpy(fdata->style, settings->style, sizeof(fdata->style));
+    fdata->style_append = settings->style_append;
     guint num = 0;
     // Invariant: the tagid for 'darktable|changed' will not change while this function runs. Is this a
     // sensible assumption?
@@ -1275,7 +1276,7 @@ void dt_control_reset_local_copy_images()
 }
 
 void dt_control_export(GList *imgid_list, int max_width, int max_height, int format_index, int storage_index,
-                       gboolean high_quality, char *style)
+                       gboolean high_quality, char *style, gboolean style_append)
 {
   dt_job_t *job = dt_control_job_create(&dt_control_export_job_run, "export");
   if(!job) return;
@@ -1309,6 +1310,7 @@ void dt_control_export(GList *imgid_list, int max_width, int max_height, int for
   data->sdata = sdata;
   data->high_quality = high_quality;
   g_strlcpy(data->style, style, sizeof(data->style));
+  data->style_append = style_append;
   params->data = data;
   dt_control_signal_raise(darktable.signals, DT_SIGNAL_IMAGE_EXPORT_MULTIPLE, params);
   dt_control_add_job(darktable.control, DT_JOB_QUEUE_USER_FG, job);

commit 6e02365774027322c6ddb5b6d40893e56ad9c692

Fix broken legacy_params in some imageio formats
This fixes a bug introduced in 5f13d2527398eb82591fea828bbee7f13f1cc8f7
which most likely broke people's old export presets. Given how old it is
and the lack of complaints it's probably only a minor issue though. :-)

src/imageio/format/j2k.c
@@ -531,7 +531,6 @@ void *legacy_params(dt_imageio_module_format_t *self, const void *const old_para
       int max_width, max_height;
       int width, height;
       char style[128];
-      gboolean style_append;
       int bpp;
       dt_imageio_j2k_format_t format;
       dt_imageio_j2k_preset_t preset;

src/imageio/format/png.c
@@ -364,7 +364,6 @@ void *legacy_params(dt_imageio_module_format_t *self, const void *const old_para
       int max_width, max_height;
       int width, height;
       char style[128];
-      gboolean style_append;
       int bpp;
       FILE *f;
       png_structp png_ptr;

src/imageio/format/tiff.c
@@ -269,7 +269,6 @@ void *legacy_params(dt_imageio_module_format_t *self, const void *const old_para
       int max_width, max_height;
       int width, height;
       char style[128];
-      gboolean style_append;
       int bpp;
       int compress;
       TIFF *handle;
@@ -292,6 +291,7 @@ void *legacy_params(dt_imageio_module_format_t *self, const void *const old_para
   }
   return NULL;
 }
+
 void *get_params(dt_imageio_module_format_t *self)
 {
   dt_imageio_tiff_t *d = (dt_imageio_tiff_t *)calloc(1, sizeof(dt_imageio_tiff_t));

src/imageio/format/webp.c
@@ -191,7 +191,6 @@ void *legacy_params(dt_imageio_module_format_t *self, const void *const old_para
       int max_width, max_height;
       int width, height;
       char style[128];
-      gboolean style_append;
       int comp_type;
       int quality;
       int hint;
@@ -214,6 +213,7 @@ void *legacy_params(dt_imageio_module_format_t *self, const void *const old_para
   }
   return NULL;
 }
+
 void *get_params(dt_imageio_module_format_t *self)
 {
   dt_imageio_webp_t *d = (dt_imageio_webp_t *)calloc(1, sizeof(dt_imageio_webp_t));