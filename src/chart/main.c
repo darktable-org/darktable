6c7c791bd5dac5f44118c1879ccaaa1d7966410f
Heiko Bauke <heiko.bauke@mail.de>
2017-09-12 12:02:44 +0200

    allow input of gray color patches in arbitrary order

--- src/chart/main.c
+++ src/chart/main.c
@@ -868,20 +868,48 @@ static char *encode_colorchecker(int num, const double *point, const double **ta
   return dt_exif_xmp_encode_internal((uint8_t *)&params, sizeof(params), NULL, FALSE);
 }
 
-static void process_data(dt_lut_t *self, double *target_L, double *target_a, double *target_b,
-                         double *colorchecker_Lab, int N, int num_tonecurve, int sparsity)
+static int compare_L_source(const void *x_, const void *y_)
 {
-  if (num_tonecurve < 2)
-    num_tonecurve = 2;
+  const double x = *(const double *)x_;
+  const double y = *(const double *)y_;
+  return x < y ? -1 : (x > y ? +1 : 0);
+}
+
+static void process_data(dt_lut_t *self, double *target_L, double *target_a, double *target_b,
+                         double *colorchecker_Lab, int N, int num_grays, int sparsity)
+{
+  // sort all gray patches according to the L channel of the source colors
+  double *grays = malloc(sizeof(double) * 6 * num_grays);
+  for(int k = 0; k < num_grays; k++)
+  {
+    grays[k * 6 + 0] = colorchecker_Lab[(k + N - num_grays) * 3 + 0];
+    grays[k * 6 + 1] = colorchecker_Lab[(k + N - num_grays) * 3 + 1];
+    grays[k * 6 + 2] = colorchecker_Lab[(k + N - num_grays) * 3 + 2];
+    grays[k * 6 + 3] = target_L[k + N - num_grays];
+    grays[k * 6 + 4] = target_a[k + N - num_grays];
+    grays[k * 6 + 5] = target_b[k + N - num_grays];
+  }
+  qsort(grays, num_grays, sizeof(double) * 6, compare_L_source);
+  for(int k = 0; k < num_grays; k++)
+  {
+    colorchecker_Lab[(k + N - num_grays) * 3 + 0] = grays[k * 6 + 0];
+    colorchecker_Lab[(k + N - num_grays) * 3 + 1] = grays[k * 6 + 1];
+    colorchecker_Lab[(k + N - num_grays) * 3 + 2] = grays[k * 6 + 2];
+    target_L[k + N - num_grays] = grays[k * 6 + 3];
+    target_a[k + N - num_grays] = grays[k * 6 + 4];
+    target_b[k + N - num_grays] = grays[k * 6 + 5];
+  }
+  free(grays);
+
+  // construct a tone curve from the grays plus pure black and pure white
+  int num_tonecurve = num_grays + 2;
   tonecurve_t tonecurve;
   double *cx = malloc(sizeof(double) * num_tonecurve);
   double *cy = malloc(sizeof(double) * num_tonecurve);
-  cx[0] = cy[0] = 0.0;                                   // fix black
-  cx[num_tonecurve - 1] = cy[num_tonecurve - 1] = 100.0; // fix white
-  for(int k = 1; k < num_tonecurve - 1; k++)
-    cx[num_tonecurve - 1 - k] = colorchecker_Lab[3 * (N - num_tonecurve + 2 + k - 1)];
-  for(int k = 1; k < num_tonecurve - 1; k++)
-    cy[num_tonecurve - 1 - k] = target_L[N - num_tonecurve + 2 + k - 1];
+  cx[0] = cy[0] = 0.0;                           // fix black
+  cx[num_grays + 1] = cy[num_grays + 1] = 100.0; // fix white
+  for(int k = 0; k < num_grays; k++) cx[k + 1] = colorchecker_Lab[3 * (N - num_grays + k)];
+  for(int k = 0; k < num_grays; k++) cy[k + 1] = target_L[N - num_grays + k];
   tonecurve_create(&tonecurve, cx, cy, num_tonecurve);
 
   cy = NULL;
@@ -889,8 +917,8 @@ static void process_data(dt_lut_t *self, double *target_L, double *target_a, dou
 
 #if 0 // quiet.
   for(int k = 0; k < num_tonecurve; k++)
-    fprintf(stderr, "L[%g] = %g\n", 100.0 * k / (num_tonecurve - 1.0f),
-            tonecurve_apply(&tonecurve, 100.0f * k / (num_tonecurve - 1.0f)));
+    fprintf(stderr, "L[%g] = %g\n", 100.0 * k / (num_tonecurve - 1.0),
+            tonecurve_apply(&tonecurve, 100.0 * k / (num_tonecurve - 1.0)));
 #endif
 
 #if 0 // Lab tonecurve on L only
@@ -900,24 +928,24 @@ static void process_data(dt_lut_t *self, double *target_L, double *target_a, dou
   tonecurve_t rgbcurve;
   cx = malloc(sizeof(double) * num_tonecurve);
   cy = malloc(sizeof(double) * num_tonecurve);
-  cx[0] = cy[0] = 0.0;                                   // fix black
-  cx[num_tonecurve - 1] = cy[num_tonecurve - 1] = 100.0; // fix white
-  for(int k = 1; k < num_tonecurve - 1; k++)
+  cx[0] = cy[0] = 0.0;                           // fix black
+  cx[num_grays + 1] = cy[num_grays + 1] = 100.0; // fix white
+  for(int k = 0; k < num_grays; k++)
   {
-    float rgb[3], Lab[3] = {0.0f};
-    Lab[0] = colorchecker_Lab[3 * (N - num_tonecurve + 2 + k - 1)];
+    float rgb[3], Lab[3] = { 0.0f, 0.0f, 0.0f };
+    Lab[0] = colorchecker_Lab[3 * (N - num_grays + k)];
     dt_Lab_to_prophotorgb(Lab, rgb);
-    cx[num_tonecurve - 1 - k] = rgb[0];
+    cx[k + 1] = rgb[0];
     Lab[0] = tonecurve_apply(&tonecurve, Lab[0]);
     dt_Lab_to_prophotorgb(Lab, rgb);
-    cy[num_tonecurve - 1 - k] = rgb[0];
+    cy[k + 1] = rgb[0];
   }
   tonecurve_create(&rgbcurve, cx, cy, num_tonecurve);
 
   // now unapply the curve:
   for(int k = 0; k < N; k++)
   {
-    float rgb[3], Lab[3] = {0.0f};
+    float rgb[3], Lab[3] = { 0.0f, 0.0f, 0.0f };
     Lab[0] = target_L[k];
     Lab[1] = target_a[k];
     Lab[2] = target_b[k];
