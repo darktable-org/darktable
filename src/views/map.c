98468844dc973a94a4888410107a06345cc676dd
Sven Claussner <scl.gplus@gmail.com>
2017-11-03 17:46:24 +0100

    Show copyright info in the map widget. Fixes #9586.
    Show the map provider's copyright info in the map.
    Requires libosmgpsmap>1.1.0

--- src/views/map.c
+++ src/views/map.c
@@ -411,7 +411,7 @@ void init(dt_view_t *self)
     gtk_box_pack_start(GTK_BOX(parent), GTK_WIDGET(lib->map), TRUE, TRUE, 0);
 
     lib->osd = g_object_new(OSM_TYPE_GPS_MAP_OSD, "show-scale", TRUE, "show-coordinates", TRUE, "show-dpad",
-                            TRUE, "show-zoom", TRUE, NULL);
+                            TRUE, "show-zoom", TRUE, "show-copyright", TRUE, NULL);
 
     if(dt_conf_get_bool("plugins/map/show_map_osd"))
     {
