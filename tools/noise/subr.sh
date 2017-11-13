601bcbf570ac5be213daf514b194f2e36c0667ad
Tobias Ellinghaus <me@houz.org>
2017-11-13 18:06:14 +0100

    noise: Fix exposure check
    IM seems to require an insane amount of disk cache when assembling the
    color channels for exposure check computation. With /tmp/ on a memory
    disk that often fails, resulting in confusing error messages and no
    checks to be actually computed. This moves the cache into the image
    folder, hoping for enough disk space.

--- tools/noise/subr.sh
+++ tools/noise/subr.sh
@@ -499,15 +499,16 @@ export_thumbnail() {
 }
 
 check_exposure() {
-	local orig input over under ret convert_flags
+	local orig input inputdir over under ret convert_flags
 	orig=$1
 	input=$2
+	inputdir=$(dirname $input)
 
 	ret=0
 
 	# See: http://www.imagemagick.org/discourse-server/viewtopic.php?f=1&t=19805
-
-	convert_flags="-channel RGB -threshold 99% -separate -append"
+	# and https://www.imagemagick.org/script/architecture.php#tera-pixel for the temporary-path thing
+	convert_flags="-define registry:temporary-path=${inputdir}/tmp -channel RGB -threshold 99% -separate -append"
 
 	over=$(convert "$input" $convert_flags -format "%[mean]" info: | cut -f1 -d.)
 	if [ "$over" -a "$over" -lt 80 ]; then
