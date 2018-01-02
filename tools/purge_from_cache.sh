darktable-org/darktable

commit 18086e1ff114e855ac6a926b96ce5b745f079a2b

fix various issues and POSIX incompatibilities in shell scripts

tools/purge_from_cache.sh
@@ -30,7 +30,7 @@ cache_base="${HOME}/.cache/darktable"
 dryrun=1
 
 # remember the command line to show it in the end when not purging
-commandline="$0 $@"
+commandline="$0 $*"
 
 # handle command line arguments
 while [ "$#" -ge 1 ] ; do
@@ -81,7 +81,7 @@ if [ ! -f "${library}" ]; then
 fi
 
 # the mipmap directory matching the selected library
-cache_dir="${cache_base}/mipmaps-$(echo -n "${library}" | sha1sum | cut --delimiter=" " --fields=1).d"
+cache_dir="${cache_base}/mipmaps-$(echo "${library}" | sha1sum | cut --delimiter=" " --fields=1).d"
 
 if [ ! -d "${cache_dir}" ]; then
   echo "cache directory '${cache_dir}' doesn't exist"
@@ -93,7 +93,7 @@ id_list=$(mktemp -t darktable-tmp.XXXXXX)
 sqlite3 "${library}" "select id from images order by id" > "${id_list}"
 
 # iterate over cached mipmaps and check for each if the image is in the db
-find "${cache_dir}" -type f | while read mipmap; do
+find "${cache_dir}" -type f | while read -r mipmap; do
   # get the image id from the filename
   id=$(echo "${mipmap}" | sed 's,.*/\([0-9]*\).*,\1,')
   # ... and delete it if it's not in the library

tools/authors_h.sh
@@ -32,7 +32,7 @@ echo "" >> "$H_FILE"
 
 echo "static const char *authors[] = {" >> "$H_FILE"
 
-cat "$AUTHORS" | sed "s/^/\"/g" | sed "s/$/\",/g" >> "$H_FILE"
+sed -e "s/^/\"/g" -e "s/$/\",/g" "$AUTHORS" >> "$H_FILE"
 
 echo "NULL };" >> "$H_FILE"
 

tools/create_control_svg.sh
@@ -68,7 +68,7 @@ EOF
 
 
 # input file
-log=$1
+log="$1"
 
 # output file
 output="control.svg"
@@ -77,10 +77,10 @@ output="control.svg"
 echo "$header" > $output
 
 # collect thread ids:
-ids=$(grep -E "^\[run_job" $log | cut -f 2 -d ' ' | sort | uniq)
+ids=$(grep -E '^\[run_job' "$log" | cut -f 2 -d ' ' | sort | uniq)
 
 # start time:
-start_time=$(grep -E "^\[run_job" $log | cut -f 3 -d ' ' | head -1 | sed "s/,/./g")
+start_time=$(grep -E '^\[run_job' "$log" | cut -f 3 -d ' ' | head -1 | sed "s/,/./g")
 
 # calculate the radical inverse in posix bc:
 def_ri=$(cat << EOF
@@ -111,15 +111,15 @@ for id in $ids
 do
   # get index to start sort (begin of time)
   len=${#id}
-  start=$(awk -v a="$(head -1 $log)" -v b="$id" 'BEGIN{print index(a,b)}')
-  offs=$(($len + $start))
+  start=$(awk -v a="$(head -1 "$log")" -v b="$id" 'BEGIN{print index(a,b)}')
+  offs=$((len + start))
 
   # get sorted outputs by time (should already be sorted, in fact)
-  numlines=$(cat $log | grep -E '^\[run_job.\] '$id | sort -n -k $offs | wc -l)
-  for i in $(seq 0 2 $(($numlines-1)))
+  numlines=$(grep -E '^\[run_job.\] '$id "$log" | sort -n -k $offs | wc -l)
+  for i in $(seq 0 2 $((numlines - 1)))
   do
-    line1=$(cat $log | grep -E '^\[run_job.\] '$id | sort -n -k $offs | tail -$(($numlines - $i)) | head -1 | sed "s/,/./g")
-    line2=$(cat $log | grep -E '^\[run_job.\] '$id | sort -n -k $offs | tail -$(($numlines - $i - 1)) | head -1 | sed "s/,/./g")
+    line1=$(grep -E '^\[run_job.\] '$id "$log" | sort -n -k $offs | tail -$((numlines - i)) | head -1 | sed "s/,/./g")
+    line2=$(grep -E '^\[run_job.\] '$id "$log" | sort -n -k $offs | tail -$((numlines - i - 1)) | head -1 | sed "s/,/./g")
 
     # get two lines, assert +- and job description string
     descr=$(echo $line1 | cut -f 4- -d" ")
@@ -127,9 +127,9 @@ do
     off=$(echo $line2 | cut -f 3 -d" ")
     x_on=$(echo "100 * ($on - $start_time)" | bc -l)
     x_wd=$(echo "100 * ($off - $start_time) - $x_on" | bc -l)
-    y=$(($offset * 30))
+    y=$((offset * 30))
     ht=20
-    yt=$(($y+10))
+    yt=$((y + 10))
 
     # choose color by radical inverse of the image id, if 'image XXXX' is given
     imgid=$(awk -v a="$line1" -v b="image" 'BEGIN{print substr(a,index(a,b)+6,4)}' | grep -v -E '[^0-9]')

tools/create_metadata.sh
@@ -5,7 +5,7 @@ outputdir="$2"
 outputheader=metadata_gen.h
 outputbody=metadata_gen.c
 
-headerdefine=__$(printf $outputheader | tr '[:lower:].' '[:upper:]_')__
+headerdefine=__$(printf "%s" "$outputheader" | tr '[:lower:].' '[:upper:]_')__
 # header of the .h file
 cat > "$outputdir/$outputheader" << EOF
 /** generated file, do not edit! */
@@ -30,13 +30,13 @@ EOF
 
 # iterate over the input
 first=0
-for line in $(cat "$inputfile" | grep -v "^#"); do
-    enum=DT_METADATA_$(printf $line | tr '[:lower:].' '[:upper:]_')
-    length=$(printf $line | wc -c)
+grep -v "^#" "$inputfile" | while IFS= read -r line; do
+    enum=DT_METADATA_$(printf "%s" "$line" | tr '[:lower:].' '[:upper:]_')
+    length=$(printf "%s" "$line" | wc -c)
     if [ "$first" -ne 0 ]; then
         printf ",\n" >> "$outputdir/$outputheader"
     fi
-    printf "    $enum" >> "$outputdir/$outputheader"
+    printf "%s" "    $enum" >> "$outputdir/$outputheader"
     first=1
 
     cat >> "$outputdir/$outputbody" << EOF

tools/create_release.sh
@@ -3,25 +3,25 @@
 DT_SRC_DIR=$(dirname "$0")
 DT_SRC_DIR=$(cd "$DT_SRC_DIR/../" && pwd -P)
 
-cd $DT_SRC_DIR
+cd "$DT_SRC_DIR" || exit
 
 git shortlog -sne release-2.2.0..HEAD
 
 echo "are you sure these guys received proper credit in the about dialog?"
 echo "HINT: $ tools/generate_authors.rb release-2.2.0..HEAD > AUTHORS"
-read answer
+read -r answer
 
 # prefix rc with ~, so debian thinks its less than
 echo "* archiving git tree"
 
-dt_decoration=$(git describe --tags $branch | sed 's,^release-,,;s,-,+,;s,-,~,;' | sed 's/rc/~rc/')
+dt_decoration=$(git describe --tags | sed -e 's,^release-,,;s,-,+,;s,-,~,;' -e 's/rc/~rc/')
 
 echo "* * creating root archive"
-git archive --format tar HEAD --prefix=darktable-$dt_decoration/ -o darktable-$dt_decoration.tar
+git archive --format tar HEAD --prefix=darktable-"$dt_decoration"/ -o darktable-"$dt_decoration".tar
 
 echo "* * creating submodule archives"
 # for each of git submodules append to the root archive
-git submodule foreach --recursive 'git archive --format tar --verbose --prefix="darktable-'$dt_decoration'/$path/" HEAD --output "'$DT_SRC_DIR'/darktable-sub-$sha1.tar"'
+git submodule foreach --recursive 'git archive --format tar --verbose --prefix="darktable-'"$dt_decoration"'/$path/" HEAD --output "'"$DT_SRC_DIR"'/darktable-sub-$sha1.tar"'
 
 if [ $(ls "$DT_SRC_DIR/darktable-sub-"*.tar | wc -l) != 0  ]; then
   echo "* * appending submodule archives, combining all tars"
@@ -33,22 +33,22 @@ fi
 
 echo "* * done creating archive"
 
-TMPDIR=`mktemp -d -t darktable-XXXXXX`
-cd "$TMPDIR"
+TMPDIR=$(mktemp -d -t darktable-XXXXXX)
+cd "$TMPDIR" || exit
 
 tar xf "$DT_SRC_DIR/darktable-$dt_decoration.tar"
 
 # create version header for non-git tarball:
 echo "* creating version header"
-"$DT_SRC_DIR/tools/create_version_c.sh" darktable-$dt_decoration/src/version_gen.c $dt_decoration
+"$DT_SRC_DIR/tools/create_version_c.sh" "darktable-$dt_decoration/src/version_gen.c" "$dt_decoration"
 
 # remove usermanual, that's > 80 MB and released separately
 echo "* removing usermanual"
-rm -rf darktable-$dt_decoration/doc/usermanual
+rm -rf darktable-"$dt_decoration"/doc/usermanual
 
 # drop regression_tests. for internal use, and need git anyway
 echo "* removing tools/regression_tests"
-rm -rf darktable-$dt_decoration/tools/regression_tests
+rm -rf darktable-"$dt_decoration"/tools/regression_tests
 
 # ... and also remove RELEASE_NOTES. that file is just for internal use
 #echo "* removing RELEASE_NOTES"
@@ -56,16 +56,16 @@ rm -rf darktable-$dt_decoration/tools/regression_tests
 
 # wrap it up again
 echo "* creating final tarball"
-tar cf darktable-$dt_decoration.tar darktable-$dt_decoration/
+tar cf "darktable-$dt_decoration.tar$ darktable-$dt_decoration/"
 rm "$DT_SRC_DIR/darktable-$dt_decoration.tar"
-xz -z -v -9 -e darktable-$dt_decoration.tar
-cp darktable-$dt_decoration.tar.xz "$DT_SRC_DIR"
+xz -z -v -9 -e "darktable-$dt_decoration.tar"
+cp "darktable-$dt_decoration.tar.xz" "$DT_SRC_DIR"
 
 # now test the build:
 echo "* test compiling"
-rm -rf darktable-$dt_decoration/
-tar xf darktable-$dt_decoration.tar.xz
-cd darktable-$dt_decoration/
+rm -rf "darktable-$dt_decoration/"
+tar xf "darktable-$dt_decoration.tar.xz"
+cd "darktable-$dt_decoration/" || exit
 ./build.sh --prefix "$TMPDIR/darktable/"
 
 echo
@@ -73,6 +73,3 @@ echo "to actually test this build you should do:"
 echo "cd $TMPDIR/darktable-$dt_decoration/build && make install"
 echo "then run darktable from:"
 echo "$TMPDIR/darktable/bin/darktable"
-
-
-

tools/create_version_c.sh
@@ -42,21 +42,23 @@ LAST_COMMIT_YEAR=$("${DT_SRC_DIR}/tools/get_last_commit_year.sh")
 
 if [ $VERSION_C_NEEDS_UPDATE -eq 1 ]; then
 # when changing format, you must also update tools/get_git_version_string.sh !!!
-  echo "#ifndef RC_BUILD" > "$C_FILE"
-  echo "  #ifdef HAVE_CONFIG_H" >> "$C_FILE"
-  echo "    #include \"config.h\"" >> "$C_FILE"
-  echo "  #endif" >> "$C_FILE"
+  {
+    echo "#ifndef RC_BUILD"
+    echo "  #ifdef HAVE_CONFIG_H"
+    echo "    #include \"config.h\""
+    echo "  #endif"
 
-  echo "  const char darktable_package_version[] = \"${NEW_VERSION}\";" >> "$C_FILE"
-  echo "  const char darktable_package_string[] = PACKAGE_NAME \" ${NEW_VERSION}\";" >> "$C_FILE"
-  echo "  const char darktable_last_commit_year[] = \"${LAST_COMMIT_YEAR}\";" >> "$C_FILE"
-  echo "#else" >> "$C_FILE"
-  echo "  #define DT_MAJOR ${MAJOR_VERSION}" >> "$C_FILE"
-  echo "  #define DT_MINOR ${MINOR_VERSION}" >> "$C_FILE"
-  echo "  #define DT_PATCH ${PATCH_VERSION}" >> "$C_FILE"
-  echo "  #define DT_N_COMMITS ${N_COMMITS}" >> "$C_FILE"
-  echo "  #define LAST_COMMIT_YEAR \"${LAST_COMMIT_YEAR}\"" >> "$C_FILE"
-  echo "#endif" >> "$C_FILE"
+    echo "  const char darktable_package_version[] = \"${NEW_VERSION}\";"
+    echo "  const char darktable_package_string[] = PACKAGE_NAME \" ${NEW_VERSION}\";"
+    echo "  const char darktable_last_commit_year[] = \"${LAST_COMMIT_YEAR}\";"
+    echo "#else"
+    echo "  #define DT_MAJOR ${MAJOR_VERSION}"
+    echo "  #define DT_MINOR ${MINOR_VERSION}"
+    echo "  #define DT_PATCH ${PATCH_VERSION}"
+    echo "  #define DT_N_COMMITS ${N_COMMITS}"
+    echo "  #define LAST_COMMIT_YEAR \"${LAST_COMMIT_YEAR}\""
+    echo "#endif";
+  } > "$C_FILE"
 
 fi
 

tools/dngmeta.sh
@@ -24,9 +24,9 @@
 TOOLS_DIR=$(dirname "$0")
 TOOLS_DIR=$(cd "$TOOLS_DIR" && pwd -P)
 
-. $TOOLS_DIR/noise/subr.sh
+. "$TOOLS_DIR"/noise/subr.sh
 
-AUTHOR="$(getent passwd $USER | awk -F ':' '{print $5}' | awk -F ',' '{print $1}') <$USER@$HOSTNAME>"
+AUTHOR="$(getent passwd "$USER" | awk -F ':' '{print $5}' | awk -F ',' '{print $1}') <$USER@$HOSTNAME>"
 
 DNG="$1"
 
@@ -43,16 +43,16 @@ UNIQUE_CAMERA_MODEL=$(exiv2 -Pkt "$DNG" 2>/dev/null | grep 'Exif.Image.UniqueCam
 ISO=$(get_image_iso "$DNG")
 
 # This doesn't work with two name makes but there aren't any active ones
-ID_MAKE=${MAKE:0:1}$(echo ${MAKE:1} | cut -d " " -f 1 | tr "[A-Z]" "[a-z]")
+ID_MAKE=${MAKE:0:1}$(echo "${MAKE:1}" | cut -d " " -f 1 | tr "[:upper:]" "[:lower:]")
 
 ID_MODEL=$MODEL
-first_maker=$(echo $MAKE | cut -d " " -f 1)
-first_model=$(echo $MODEL | cut -d " " -f 1)
+first_maker=$(echo "$MAKE" | cut -d " " -f 1)
+first_model=$(echo "$MODEL" | cut -d " " -f 1)
 if [ "$first_maker" = "$first_model" ]; then
-  ID_MODEL=$(echo $MODEL | cut -d " " -f 2-)
+  ID_MODEL=$(echo "$MODEL" | cut -d " " -f 2-)
 fi
 
-MANGLED_MAKE_MODEL=$(echo $MAKE $MODEL | sed 's# CORPORATION##gi' | sed 's#Canon Canon#Canon#g' | sed 's#NIKON NIKON#NIKON#g' | sed 's#PENTAX PENTAX#PENTAX#g' | sed 's#OLYMPUS IMAGING CORP.#OLYMPUS#g' | sed 's#OLYMPUS OPTICAL CO.,LTD#OLYMPUS#g' | sed 's# EASTMAN KODAK COMPANY#KODAK#g')
+MANGLED_MAKE_MODEL=$(echo "$MAKE" "$MODEL" | sed 's# CORPORATION##gi' | sed 's#Canon Canon#Canon#g' | sed 's#NIKON NIKON#NIKON#g' | sed 's#PENTAX PENTAX#PENTAX#g' | sed 's#OLYMPUS IMAGING CORP.#OLYMPUS#g' | sed 's#OLYMPUS OPTICAL CO.,LTD#OLYMPUS#g' | sed 's# EASTMAN KODAK COMPANY#KODAK#g')
 
 SOFTWARE=$(exiv2 -Pkt "$DNG" 2>/dev/null | grep 'Exif.Image.Software ' | awk '{print $2 " " $3 " " $4 " " $5 " " $6}')
 
@@ -69,8 +69,6 @@ MATRIX_ZB=$(exiv2 -Pkt "$DNG" 2>/dev/null | grep 'Exif.Image.ColorMatrix2 ' | se
 
 WHITE=$(exiv2 -Pkt "$DNG" 2>/dev/null | grep 'Exif.SubImage1.WhiteLevel ' | awk '{print $2}' | bc)
 BLACK=$(exiv2 -Pkt "$DNG" 2>/dev/null | grep 'Exif.SubImage1.BlackLevel ' | awk '{print $2}' | bc)
-
-WHITE_HEX="0x$(echo "ibase=10;obase=16;$WHITE" | bc | tr 'A-Z' 'a-z')"
 
 CFA_PATTERN_WIDTH=$(exiv2 -Pkt "$DNG" 2>/dev/null | grep 'Exif.SubImage1.CFARepeatPatternDim ' | awk '{print $2}')
 CFA_PATTERN_HEIGHT=$(exiv2 -Pkt "$DNG" 2>/dev/null | grep 'Exif.SubImage1.CFARepeatPatternDim ' | awk '{print $3}')
@@ -92,11 +90,11 @@ MODE=""
 SENSOR_ISO=""
 
 if [[ $MAKE == Panasonic ]]; then
-  if [[ $((100 * $IMG_LONG / $IMG_SHORT)) -gt 164 ]]; then
+  if [[ $((100 * IMG_LONG / IMG_SHORT)) -gt 164 ]]; then
     MODE=" mode=\"16:9\""
-  elif [[ $((100 * $IMG_LONG / $IMG_SHORT)) -gt 142 ]]; then
+  elif [[ $((100 * IMG_LONG / IMG_SHORT)) -gt 142 ]]; then
     MODE=" mode=\"3:2\""
-  elif [[ $((100 * $IMG_LONG / $IMG_SHORT)) -gt 116 ]]; then
+  elif [[ $((100 * IMG_LONG / IMG_SHORT)) -gt 116 ]]; then
     MODE=" mode=\"4:3\""
   else
     MODE=" mode=\"1:1\""

tools/keybinding2docbook.sh
@@ -4,7 +4,7 @@ git grep -A 2 -h gtk_accel_map_add_entry | tr -d '\n' | \
 sed 's/gtk_accel_map_add_entry *( *\(\"[^\"]\+\"\) *, *\([^ ,]\+\) *, *\([^,)]\+\)) *;/%~\1;\2;\3%~/g' | \
 tr '%~' '\n' | \
 awk -f 'tools/keybinding2docbook.awk' | \
-gcc `pkg-config --cflags --libs gtk+-2.0` -o tmp -xc -
+gcc $(pkg-config --cflags --libs gtk+-2.0) -o tmp -xc -
 
 ./tmp > ./doc/usermanual/addendum/keymappings.xml
 rm ./tmp

commit c2ca36e78fa96d066736ae6352edfe1cbde32299

Fix finding mipmaps cache folder in purge script
This was broken in 18086e1ff114e855ac6a926b96ce5b745f079a2b when -n was
removed from the echo call.

tools/purge_from_cache.sh
@@ -81,7 +81,7 @@ if [ ! -f "${library}" ]; then
 fi
 
 # the mipmap directory matching the selected library
-cache_dir="${cache_base}/mipmaps-$(echo "${library}" | sha1sum | cut --delimiter=" " --fields=1).d"
+cache_dir="${cache_base}/mipmaps-$(printf "%s" "${library}" | sha1sum | cut --delimiter=" " --fields=1).d"
 
 if [ ! -d "${cache_dir}" ]; then
   echo "cache directory '${cache_dir}' doesn't exist"