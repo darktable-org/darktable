81d213bfad23b0ff5cd581a01f4a86b088c65ffc
Tobias Ellinghaus <me@houz.org>
2018-01-11 18:52:58 +0100

    Add global function to get final image size
    This moves a helper function from cups_print.c to image.c. Almost the
    same code is also in focus.h, but not exactly.

--- src/common/image.h
+++ src/common/image.h
@@ -229,6 +229,8 @@ int32_t dt_image_duplicate(const int32_t imgid);
 void dt_image_flip(const int32_t imgid, const int32_t cw);
 void dt_image_set_flip(const int32_t imgid, const dt_image_orientation_t user_flip);
 dt_image_orientation_t dt_image_get_orientation(const int imgid);
+/** get max width and height of the final processed image with its current hisotry stack */
+gboolean dt_image_get_final_size(const int32_t imgid, int *width, int *height);
 /** set image location lon/lat */
 void dt_image_set_location(const int32_t imgid, double lon, double lat);
 /** set image location lon/lat/ele */
