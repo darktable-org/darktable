darktable-org/darktable

commit 27e5861b7d1e7f110ecbb33131bc83f1e8c832ac

Fixed colorpicker behavior wrt post-colorout modules.

src/develop/pixelpipe_hb.c
@@ -751,7 +751,7 @@ post_process_collect_info:
     // Picking RGB for the live samples
     if(dev->gui_attached
        && pipe == dev->preview_pipe
-       && (strcmp(module->op, "colorout") == 0) // only colorout provides meaningful RGB data
+       && (strcmp(module->op, "gamma") == 0) // only colorout provides meaningful RGB data
        && darktable.lib->proxy.colorpicker.live_samples) // samples to pick
     {
       dt_colorpicker_sample_t *sample = NULL;
@@ -761,18 +761,24 @@ post_process_collect_info:
       {
         sample = samples->data;
 
-        float *pixel = (float*)*output;
+        uint8_t *pixel = (uint8_t*)*output;
 
-        for(int k=0; k<3; k++) sample->picked_color_rgb_min[k] =  666.0f;
-        for(int k=0; k<3; k++) sample->picked_color_rgb_max[k] = -666.0f;
+        for(int k=0; k<3; k++) sample->picked_color_rgb_min[k] =  255;
+        for(int k=0; k<3; k++) sample->picked_color_rgb_max[k] = 0;
         int box[4];
         int point[2];
         float rgb[3];
         for(int k=0; k<3; k++) rgb[k] = 0.0f;
-        for(int k=0; k<4; k+=2) box[k] = MIN(roi_out->width -1, MAX(0, sample->box[k]*roi_out->width));
-        for(int k=1; k<4; k+=2) box[k] = MIN(roi_out->height-1, MAX(0, sample->box[k]*roi_out->height));
-        point[0] = MIN(roi_out->width -1, MAX(0, sample->point[0] * roi_out->width));
-        point[1] = MIN(roi_out->height -1, MAX(0, sample->point[1] * roi_out->height));
+        for(int k=0; k<4; k+=2)
+          box[k] = MIN(roi_out->width -1,
+                       MAX(0, sample->box[k]*roi_out->width));
+        for(int k=1; k<4; k+=2)
+          box[k] = MIN(roi_out->height-1,
+                       MAX(0, sample->box[k]*roi_out->height));
+        point[0] = MIN(roi_out->width -1,
+                       MAX(0, sample->point[0] * roi_out->width));
+        point[1] = MIN(roi_out->height -1,
+                       MAX(0, sample->point[1] * roi_out->height));
         const float w = 1.0/((box[3]-box[1]+1)*(box[2]-box[0]+1));
         if(sample->size == DT_COLORPICKER_SIZE_BOX)
         {
@@ -780,9 +786,13 @@ post_process_collect_info:
           {
             for(int k=0; k<3; k++)
             {
-              sample->picked_color_rgb_min[k] = fminf(sample->picked_color_rgb_min[k], pixel[4*(roi_out->width*j + i) + k]);
-              sample->picked_color_rgb_max[k] = fmaxf(sample->picked_color_rgb_max[k], pixel[4*(roi_out->width*j + i) + k]);
-              rgb[k] += w*pixel[4*(roi_out->width*j + i) + k];
+              sample->picked_color_rgb_min[k] =
+                  MIN(sample->picked_color_rgb_min[k],
+                      pixel[4*(roi_out->width*j + i) + 2-k]);
+              sample->picked_color_rgb_max[k] =
+                  MAX(sample->picked_color_rgb_max[k],
+                      pixel[4*(roi_out->width*j + i) + 2-k]);
+              rgb[k] += w*pixel[4*(roi_out->width*j + i) + 2-k];
             }
           }
           for(int k=0; k<3; k++) sample->picked_color_rgb_mean[k] = rgb[k];
@@ -793,7 +803,7 @@ post_process_collect_info:
             sample->picked_color_rgb_mean[i]
                 = sample->picked_color_rgb_min[i]
                   = sample->picked_color_rgb_max[i]
-                    = pixel[4*(roi_out->width*point[1] + point[0]) + i];
+                    = pixel[4*(roi_out->width*point[1] + point[0]) + 2-i];
         }
 
         samples = g_slist_next(samples);
@@ -802,22 +812,38 @@ post_process_collect_info:
     //Picking RGB for primary colorpicker output
     if(dev->gui_attached
        && pipe == dev->preview_pipe
-       && (strcmp(module->op, "colorout") == 0) // only colorout provides meaningful RGB data
-       && module->request_color_pick
+       && (strcmp(module->op, "gamma") == 0) // only gamma provides meaningful RGB data
+       && dev->gui_module
+       && !strcmp(dev->gui_module->op, "colorout")
+       && dev->gui_module->request_color_pick
        && darktable.lib->proxy.colorpicker.picked_color_mean) // colorpicker module active
     {
-      float *pixel = (float*)*output;
+      uint8_t *pixel = (uint8_t*)*output;
 
-      for(int k=0; k<3; k++) darktable.lib->proxy.colorpicker.picked_color_min[k] =  666.0f;
-      for(int k=0; k<3; k++) darktable.lib->proxy.colorpicker.picked_color_max[k] = -666.0f;
+      for(int k=0; k<3; k++)
+        darktable.lib->proxy.colorpicker.picked_color_min[k] =  255;
+      for(int k=0; k<3; k++)
+        darktable.lib->proxy.colorpicker.picked_color_max[k] = 0;
       int box[4];
       int point[2];
       float rgb[3];
       for(int k=0; k<3; k++) rgb[k] = 0.0f;
-      for(int k=0; k<4; k+=2) box[k] = MIN(roi_out->width -1, MAX(0, module->color_picker_box[k]*roi_out->width));
-      for(int k=1; k<4; k+=2) box[k] = MIN(roi_out->height-1, MAX(0, module->color_picker_box[k]*roi_out->height));
-      point[0] = MIN(roi_out->width -1, MAX(0, module->color_picker_point[0] * roi_out->width));
-      point[1] = MIN(roi_out->height -1, MAX(0, module->color_picker_point[1] * roi_out->height));
+      for(int k=0; k<4; k+=2)
+        box[k] =
+            MIN(roi_out->width -1,
+                MAX(0, dev->gui_module->color_picker_box[k]
+                    * roi_out->width));
+      for(int k=1; k<4; k+=2)
+        box[k] =
+            MIN(roi_out->height-1,
+                MAX(0, dev->gui_module->color_picker_box[k]
+                    * roi_out->height));
+      point[0] = MIN(roi_out->width -1,
+                     MAX(0, dev->gui_module->color_picker_point[0]
+                         * roi_out->width));
+      point[1] = MIN(roi_out->height -1,
+                     MAX(0, dev->gui_module->color_picker_point[1]
+                         * roi_out->height));
       const float w = 1.0/((box[3]-box[1]+1)*(box[2]-box[0]+1));
       if(darktable.lib->proxy.colorpicker.size)
       {
@@ -825,12 +851,17 @@ post_process_collect_info:
         {
           for(int k=0; k<3; k++)
           {
-            darktable.lib->proxy.colorpicker.picked_color_min[k] = fminf(darktable.lib->proxy.colorpicker.picked_color_min[k], pixel[4*(roi_out->width*j + i) + k]);
-            darktable.lib->proxy.colorpicker.picked_color_max[k] = fmaxf(darktable.lib->proxy.colorpicker.picked_color_max[k], pixel[4*(roi_out->width*j + i) + k]);
-            rgb[k] += w*pixel[4*(roi_out->width*j + i) + k];
+            darktable.lib->proxy.colorpicker.picked_color_min[k] =
+                MIN(darktable.lib->proxy.colorpicker.picked_color_min[2-k],
+                    pixel[4*(roi_out->width*j + i) + 2-k]);
+            darktable.lib->proxy.colorpicker.picked_color_max[k] =
+                MAX(darktable.lib->proxy.colorpicker.picked_color_max[k],
+                    pixel[4*(roi_out->width*j + i) + 2-k]);
+            rgb[k] += w*pixel[4*(roi_out->width*j + i) + 2-k];
           }
         }
-        for(int k=0; k<3; k++) darktable.lib->proxy.colorpicker.picked_color_mean[k] = rgb[k];
+        for(int k=0; k<3; k++)
+          darktable.lib->proxy.colorpicker.picked_color_mean[k] = rgb[k];
       }
       else
       {
@@ -838,7 +869,7 @@ post_process_collect_info:
           darktable.lib->proxy.colorpicker.picked_color_mean[i]
               = darktable.lib->proxy.colorpicker.picked_color_min[i]
                 = darktable.lib->proxy.colorpicker.picked_color_max[i]
-                  = pixel[4*(roi_out->width*point[1] + point[0]) + i];
+                  = pixel[4*(roi_out->width*point[1] + point[0]) + 2-i];
       }
       dt_pthread_mutex_unlock(&pipe->busy_mutex);
       

src/libs/colorpicker.c
@@ -77,7 +77,8 @@ static void _update_picker_output(dt_lib_module_t *self)
     // always adjust picked color:
     int m = dt_conf_get_int("ui_last/colorpicker_mode");
     float fallback_col[] = {0,0,0};
-    float *rgb = fallback_col;
+    uint8_t fallback_rgb[] = {0,0,0};
+    uint8_t *rgb = fallback_rgb;
     float *lab = fallback_col;
     switch(m)
     {
@@ -97,8 +98,7 @@ static void _update_picker_output(dt_lib_module_t *self)
     switch(input_color)
     {
     case 0: // rgb
-      snprintf(colstring, 512, "(%d, %d, %d)", (int)(255 * rgb[0]),
-               (int)(255 * rgb[1]), (int)(255 * rgb[2]));
+      snprintf(colstring, 512, "(%d, %d, %d)", rgb[0], rgb[1], rgb[2]);
       break;
     case 1: // Lab
       snprintf(colstring, 512, "(%.03f, %.03f, %.03f)", lab[0], lab[1], lab[2]);
@@ -107,9 +107,9 @@ static void _update_picker_output(dt_lib_module_t *self)
     gtk_label_set_label(GTK_LABEL(data->output_label), colstring);
 
     // Setting the button color
-    c.red = rgb[0] * 65535;
-    c.green = rgb[1] * 65535;
-    c.blue = rgb[2] * 65535;
+    c.red = rgb[0] * 65535 / 255;
+    c.green = rgb[1] * 65535 / 255;
+    c.blue = rgb[2] * 65535 / 255;
     gtk_widget_modify_bg(data->output_button, GTK_STATE_INSENSITIVE, &c);
   }
 }
@@ -176,9 +176,9 @@ static gboolean _history_button_enter(GtkWidget *widget, GdkEvent *event,
   {
     // Then set RGB
     snprintf(text, 512, "(%d, %d, %d)",
-             (int)(255 * data->history_rgb[n][0]),
-             (int)(255 * data->history_rgb[n][1]),
-             (int)(255 * data->history_rgb[n][2]));
+             data->history_rgb[n][0],
+             data->history_rgb[n][1],
+             data->history_rgb[n][2]);
   }
   else
   {
@@ -224,7 +224,8 @@ static void _history_button_clicked(GtkButton *button, gpointer self)
   // Saving the current picker data
   m = dt_conf_get_int("ui_last/colorpicker_mode");
   float fallback_col[] = {0,0,0};
-  float *rgb = fallback_col;
+  uint8_t fallback_rgb[] = {0,0,0};
+  uint8_t *rgb = fallback_rgb;
   float *lab = fallback_col;
   switch(m)
   {
@@ -247,9 +248,9 @@ static void _history_button_clicked(GtkButton *button, gpointer self)
   for(i = 0; i < 3; i++)
     data->history_lab[n][i] = lab[i];
 
-  c.red = rgb[0] * 65535;
-  c.green = rgb[1] * 65535;
-  c.blue = rgb[2] * 65535;
+  c.red = rgb[0] * 65535 / 255;
+  c.green = rgb[1] * 65535 / 255;
+  c.blue = rgb[2] * 65535 / 255;
   gtk_widget_modify_bg(GTK_WIDGET(button), GTK_STATE_NORMAL, &c);
   gtk_widget_modify_bg(GTK_WIDGET(button), GTK_STATE_PRELIGHT, &c);
   gtk_widget_modify_bg(GTK_WIDGET(button), GTK_STATE_ACTIVE, &c);
@@ -261,7 +262,8 @@ static void _history_button_clicked(GtkButton *button, gpointer self)
 static void _update_samples_output(dt_lib_module_t *self)
 {
   float fallback[] = {0., 0., 0.};
-  float *rgb = fallback;
+  uint8_t fallback_rgb[] = {0,0,0};
+  uint8_t *rgb = fallback_rgb;
   float *lab = fallback;
   char text[1024];
   GSList *samples = darktable.lib->proxy.colorpicker.live_samples;
@@ -294,9 +296,9 @@ static void _update_samples_output(dt_lib_module_t *self)
     }
 
     // Setting the output button
-    c.red = rgb[0] * 65535;
-    c.green = rgb[1] * 65535;
-    c.blue = rgb[2] * 65535;
+    c.red = rgb[0] * 65535 / 255;
+    c.green = rgb[1] * 65535 / 255;
+    c.blue = rgb[2] * 65535 / 255;
     gtk_widget_modify_bg(sample->output_button, GTK_STATE_NORMAL, &c);
     gtk_widget_modify_bg(sample->output_button, GTK_STATE_PRELIGHT, &c);
     gtk_widget_modify_bg(sample->output_button, GTK_STATE_ACTIVE, &c);
@@ -306,10 +308,7 @@ static void _update_samples_output(dt_lib_module_t *self)
     {
     case 0:
       // RGB
-      snprintf(text, 1024, "(%d, %d, %d)",
-               (int)(rgb[0] * 255),
-               (int)(rgb[1] * 255),
-               (int)(rgb[2] * 255));
+      snprintf(text, 1024, "(%d, %d, %d)", rgb[0], rgb[1], rgb[2]);
       break;
 
     case 1:
@@ -490,11 +489,13 @@ void gui_init(dt_lib_module_t *self)
       dt_conf_get_int("ui_last/colorpicker_display_samples");
   darktable.lib->proxy.colorpicker.live_samples = NULL;
   darktable.lib->proxy.colorpicker.picked_color_mean =
-      (float*)malloc(sizeof(float) * 3);
+      (uint8_t*)malloc(sizeof(uint8_t) * 3);
   darktable.lib->proxy.colorpicker.picked_color_min =
-      (float*)malloc(sizeof(float) * 3);
+      (uint8_t*)malloc(sizeof(uint8_t) * 3);
   darktable.lib->proxy.colorpicker.picked_color_max =
-      (float*)malloc(sizeof(float) * 3);
+      (uint8_t*)malloc(sizeof(uint8_t) * 3);
+  for(i = 0; i < 3; i++)
+    darktable.lib->proxy.colorpicker.picked_color_mean[i] = 0;
   darktable.lib->proxy.colorpicker.update_panel =  _update_picker_output;
   darktable.lib->proxy.colorpicker.update_samples = _update_samples_output;
 

src/libs/colorpicker.h
@@ -19,6 +19,7 @@
 #ifndef COLORPICKER_H
 #define COLORPICKER_H
 
+#include <inttypes.h>
 #include <gtk/gtk.h>
 
 #define DT_COLORPICKER_SIZE_POINT 0
@@ -41,7 +42,7 @@ typedef struct dt_lib_colorpicker_t
   GtkWidget *add_sample_button;
   GtkWidget *display_samples_check_box;
 
-  float history_rgb[5][3];
+  uint8_t history_rgb[5][3];
   float history_lab[5][3];
 
 } dt_lib_colorpicker_t;
@@ -56,9 +57,9 @@ typedef struct dt_colorpicker_sample_t
   int size;
 
   /** The actual picked colors */
-  float picked_color_rgb_mean[3];
-  float picked_color_rgb_min[3];
-  float picked_color_rgb_max[3];
+  uint8_t picked_color_rgb_mean[3];
+  uint8_t picked_color_rgb_min[3];
+  uint8_t picked_color_rgb_max[3];
 
   float picked_color_lab_mean[3];
   float picked_color_lab_min[3];

src/libs/lib.h
@@ -39,9 +39,9 @@ typedef struct dt_lib_t
     struct
     {
       struct dt_lib_module_t *module;
-      float *picked_color_mean;
-      float *picked_color_min;
-      float *picked_color_max;
+      uint8_t *picked_color_mean;
+      uint8_t *picked_color_min;
+      uint8_t *picked_color_max;
       GSList *live_samples;
       struct dt_colorpicker_sample_t *selected_sample;
       int size;

commit 25050d71b482d595b84267d87c95ba97434b3073

Fix #11975: Min color picker was broken
This fixes a typo, introduced in
27e5861b7d1e7f110ecbb33131bc83f1e8c832ac 6.5 years ago ...

src/develop/pixelpipe_hb.c
@@ -1925,7 +1925,7 @@ static int dt_dev_pixelpipe_process_rec(dt_dev_pixelpipe_t *pipe, dt_develop_t *
             for(int k = 0; k < 3; k++)
             {
               darktable.lib->proxy.colorpicker.picked_color_rgb_min[k]
-                  = MIN(darktable.lib->proxy.colorpicker.picked_color_rgb_min[2 - k],
+                  = MIN(darktable.lib->proxy.colorpicker.picked_color_rgb_min[k],
                         pixel[4 * (roi_out->width * j + i) + 2 - k]);
               darktable.lib->proxy.colorpicker.picked_color_rgb_max[k]
                   = MAX(darktable.lib->proxy.colorpicker.picked_color_rgb_max[k],