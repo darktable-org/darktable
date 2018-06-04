0711dbc7dd6f4c94053b7ce30f0c85d4be0c7cbc
Heiko Bauke <heiko.bauke@mail.de>
2018-06-04 23:14:25 +0200

    blend: perform all arithmetic floating pint operations in single precision

---
+++ src/common/math.h
@@ -0,0 +1,22 @@
+/*
+ *    This file is part of darktable,
+ *    copyright (c) 2018 Heiko Bauke.
+ *
+ *    darktable is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    darktable is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with darktable.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define DT_M_PI_F (3.14159265358979324f)
+#define DT_M_PI (3.14159265358979324)
