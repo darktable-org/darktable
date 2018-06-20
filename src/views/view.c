darktable-org/darktable

commit 06316e693325523207cebb6dfd346a4a6113b5a2

Correct abs function should be used (clang -Wabsolute-value)

src/views/view.c
@@ -1016,7 +1016,7 @@ dt_view_image_expose(
         else x = (.04+8*0.04-1.9*.04)*fscale;
         dt_view_draw_audio(cr, x, y, s);
         // mouse is over the audio icon
-        if(abs(px-x) <= 1.2*s && abs(py-y) <= 1.2*s)
+        if(fabsf(px-x) <= 1.2*s && fabsf(-y) <= 1.2*s)
           *image_over = DT_VIEW_AUDIO;
       }
 #endif
@@ -1058,7 +1058,7 @@ dt_view_image_expose(
         dtgtk_cairo_paint_grouping(cr, _x, _y, s, s, 23);
         cairo_restore(cr);
         // mouse is over the grouping icon
-        if(img && abs(px-_x-.5*s) <= .8*s && abs(py-_y-.5*s) <= .8*s)
+        if(img && fabs(px-_x-.5*s) <= .8*s && fabs(py-_y-.5*s) <= .8*s)
           *image_over = DT_VIEW_GROUP;
       }
 
@@ -1085,7 +1085,7 @@ dt_view_image_expose(
         else x = (.04+8*0.04)*fscale;
         dt_view_draw_altered(cr, x, y, s);
         //g_print("px = %d, x = %.4f, py = %d, y = %.4f\n", px, x, py, y);
-        if(img && abs(px-x) <= 1.2*s && abs(py-y) <= 1.2*s) // mouse hovers over the altered-icon -> history tooltip!
+        if(img && fabsf(px-x) <= 1.2*s && fabsf(py-y) <= 1.2*s) // mouse hovers over the altered-icon -> history tooltip!
         {
           darktable.gui->center_tooltip = 1;
         }

src/CMakeLists.txt
@@ -623,6 +623,17 @@ add_definitions("-Werror=implicit-function-declaration")
 
 ###### GTK+3 port ######
 
+###### Some good warnings ######
+# Clang-specific warnings:
+if(CMAKE_C_COMPILER_ID STREQUAL "Clang")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Werror=absolute-value")
+endif(CMAKE_C_COMPILER_ID STREQUAL "Clang")
+
+# Generic (gcc) warnings:
+#set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ")
+###### Some good warnings ######
+
+
 target_link_libraries(lib_darktable ${LIBS})
 
 if(WIN32)

src/iop/amaze_demosaic_RT.cc
@@ -878,7 +878,7 @@ amaze_demosaic_RT(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, c
 
             //if both agree on interpolation direction, choose the one with strongest directional discrimination;
             //otherwise, choose the u/d and l/r difference fluctuation weights
-            if ((0.5-varwt)*(0.5-diffwt)>0 && fabsf(0.5-diffwt)<fabsf(0.5-varwt))
+            if ((0.5-varwt)*(0.5-diffwt)>0 && fabsf(0.5f-diffwt)<fabsf(0.5f-varwt))
             {
               hvwt[indx>>1]=varwt;
             }
@@ -1000,7 +1000,7 @@ amaze_demosaic_RT(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, c
 //					hvwtalt = 0.25*(hvwt[(indx-m1)>>1]+hvwt[(indx+p1)>>1]+hvwt[(indx-p1)>>1]+hvwt[(indx+m1)>>1]);
 //					vo=fabsf(0.5-hvwt[indx>>1]);
 //					ve=fabsf(0.5-hvwtalt);
-            if (fabsf(0.5-hvwt[indx>>1])<fabsf(0.5-hvwtalt))
+            if (fabsf(0.5f-hvwt[indx>>1])<fabsf(0.5f-hvwtalt))
             {
               hvwt[indx>>1]=hvwtalt; //a better result was obtained from the neighbors
             }
@@ -1182,7 +1182,7 @@ amaze_demosaic_RT(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, c
             pmwtalt = xdivf(pmwt[(indx-m1)>>1]+pmwt[(indx+p1)>>1]+pmwt[(indx-p1)>>1]+pmwt[(indx+m1)>>1],2);
 //					vo=fabsf(0.5-pmwt[indx1]);
 //					ve=fabsf(0.5-pmwtalt);
-            if (fabsf(0.5-pmwt[indx1])<fabsf(0.5-pmwtalt))
+            if (fabsf(0.5f-pmwt[indx1])<fabsf(0.5f-pmwtalt))
             {
               pmwt[indx1]=pmwtalt; //a better result was obtained from the neighbors
             }
@@ -1195,7 +1195,7 @@ amaze_demosaic_RT(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, c
           for (cc=12+(FC(rr,2,filters)&1),indx=rr*TS+cc,indx1=indx>>1; cc<cc1-12; cc+=2,indx+=2,indx1++)
           {
 
-            if (fabsf(0.5-pmwt[indx>>1])<fabsf(0.5-hvwt[indx>>1]) ) continue;
+            if (fabsf(0.5f-pmwt[indx>>1])<fabsf(0.5f-hvwt[indx>>1]) ) continue;
 
             //now interpolate G vertically/horizontally using R+B values
             //unfortunately, since G interpolation cannot be done diagonally this may lead to color shifts

src/iop/atrous.c
@@ -966,7 +966,7 @@ area_enter_notify(GtkWidget *widget, GdkEventCrossing *event, gpointer user_data
 {
   dt_iop_module_t *self = (dt_iop_module_t *)user_data;
   dt_iop_atrous_gui_data_t *c = (dt_iop_atrous_gui_data_t *)self->gui_data;
-  if(!c->dragging) c->mouse_y = fabsf(c->mouse_y);
+  if(!c->dragging) c->mouse_y = fabs(c->mouse_y);
   gtk_widget_queue_draw(widget);
   return TRUE;
 }
@@ -976,7 +976,7 @@ area_leave_notify(GtkWidget *widget, GdkEventCrossing *event, gpointer user_data
 {
   dt_iop_module_t *self = (dt_iop_module_t *)user_data;
   dt_iop_atrous_gui_data_t *c = (dt_iop_atrous_gui_data_t *)self->gui_data;
-  if(!c->dragging) c->mouse_y = -fabsf(c->mouse_y);
+  if(!c->dragging) c->mouse_y = -fabs(c->mouse_y);
   gtk_widget_queue_draw(widget);
   return TRUE;
 }
@@ -1284,10 +1284,10 @@ area_motion_notify(GtkWidget *widget, GdkEventMotion *event, gpointer user_data)
   {
     // move x-positions
     c->x_move = 0;
-    float dist = fabsf(p->x[c->channel][0] - c->mouse_x);
+    float dist = fabs(p->x[c->channel][0] - c->mouse_x);
     for(int k=1; k<BANDS; k++)
     {
-      float d2 = fabsf(p->x[c->channel][k] - c->mouse_x);
+      float d2 = fabs(p->x[c->channel][k] - c->mouse_x);
       if(d2 < dist)
       {
         c->x_move = k;
@@ -1302,10 +1302,10 @@ area_motion_notify(GtkWidget *widget, GdkEventMotion *event, gpointer user_data)
     float dist = 1000000.0f;
     for(int k=0; k<BANDS; k++)
     {
-      float d2 = fabsf(p->x[c->channel][k] - c->mouse_x);
+      float d2 = fabs(p->x[c->channel][k] - c->mouse_x);
       if(d2 < dist)
       {
-        if(fabsf(c->mouse_y - p->y[ch][k]) < fabsf(c->mouse_y - p->y[ch2][k])) c->channel2 = ch;
+        if(fabs(c->mouse_y - p->y[ch][k]) < fabs(c->mouse_y - p->y[ch2][k])) c->channel2 = ch;
         else c->channel2 = ch2;
         dist = d2;
       }

src/iop/basecurve.c
@@ -415,8 +415,8 @@ dt_iop_basecurve_enter_notify(GtkWidget *widget, GdkEventCrossing *event, gpoint
 {
   dt_iop_module_t *self = (dt_iop_module_t *)user_data;
   dt_iop_basecurve_gui_data_t *c = (dt_iop_basecurve_gui_data_t *)self->gui_data;
-  c->mouse_x = fabsf(c->mouse_x);
-  c->mouse_y = fabsf(c->mouse_y);
+  c->mouse_x = fabs(c->mouse_x);
+  c->mouse_y = fabs(c->mouse_y);
   gtk_widget_queue_draw(widget);
   return TRUE;
 }
@@ -427,8 +427,8 @@ dt_iop_basecurve_leave_notify(GtkWidget *widget, GdkEventCrossing *event, gpoint
   dt_iop_module_t *self = (dt_iop_module_t *)user_data;
   dt_iop_basecurve_gui_data_t *c = (dt_iop_basecurve_gui_data_t *)self->gui_data;
   // sign swapping for fluxbox
-  c->mouse_x = -fabsf(c->mouse_x);
-  c->mouse_y = -fabsf(c->mouse_y);
+  c->mouse_x = -fabs(c->mouse_x);
+  c->mouse_y = -fabs(c->mouse_y);
   gtk_widget_queue_draw(widget);
   return TRUE;
 }

src/iop/colorzones.c
@@ -848,10 +848,10 @@ colorzones_motion_notify(GtkWidget *widget, GdkEventMotion *event, gpointer user
   else if(event->y > height)
   {
     c->x_move = 0;
-    float dist = fabsf(p->equalizer_x[c->channel][0] - c->mouse_x);
+    float dist = fabs(p->equalizer_x[c->channel][0] - c->mouse_x);
     for(int k=1; k<DT_IOP_COLORZONES_BANDS; k++)
     {
-      float d2 = fabsf(p->equalizer_x[c->channel][k] - c->mouse_x);
+      float d2 = fabs(p->equalizer_x[c->channel][k] - c->mouse_x);
       if(d2 < dist)
       {
         c->x_move = k;
@@ -921,7 +921,7 @@ colorzones_enter_notify(GtkWidget *widget, GdkEventCrossing *event, gpointer use
 {
   dt_iop_module_t *self = (dt_iop_module_t *)user_data;
   dt_iop_colorzones_gui_data_t *c = (dt_iop_colorzones_gui_data_t *)self->gui_data;
-  c->mouse_y = fabsf(c->mouse_y);
+  c->mouse_y = fabs(c->mouse_y);
   gtk_widget_queue_draw(widget);
   return TRUE;
 }
@@ -932,7 +932,7 @@ colorzones_leave_notify(GtkWidget *widget, GdkEventCrossing *event, gpointer use
   dt_iop_module_t *self = (dt_iop_module_t *)user_data;
   dt_iop_colorzones_gui_data_t *c = (dt_iop_colorzones_gui_data_t *)self->gui_data;
   // for fluxbox
-  c->mouse_y = -fabsf(c->mouse_y);
+  c->mouse_y = -fabs(c->mouse_y);
   gtk_widget_queue_draw(widget);
   return TRUE;
 }

src/iop/grain.c
@@ -376,7 +376,7 @@ void process (struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, void 
   // double zoom=1.0+(8*(data->scale/100.0));
   const double wd = fminf(piece->buf_in.width, piece->buf_in.height);
   const double zoom=(1.0+8*data->scale/100)/800.0;
-  const int filter = fabsf(roi_out->scale - 1.0) > 0.01;
+  const int filter = fabsf(roi_out->scale - 1.0f) > 0.01;
   // filter width depends on world space (i.e. reverse wd norm and roi->scale, as well as buffer input to pixelpipe iscale)
   const double filtermul = piece->iscale/(roi_out->scale*wd);
 #ifdef _OPENMP

src/iop/lowlight.c
@@ -681,10 +681,10 @@ lowlight_motion_notify(GtkWidget *widget, GdkEventMotion *event, gpointer user_d
   else if(event->y > height)
   {
     c->x_move = 0;
-    float dist = fabsf(p->transition_x[0] - c->mouse_x);
+    float dist = fabs(p->transition_x[0] - c->mouse_x);
     for(int k=1; k<DT_IOP_LOWLIGHT_BANDS; k++)
     {
-      float d2 = fabsf(p->transition_x[k] - c->mouse_x);
+      float d2 = fabs(p->transition_x[k] - c->mouse_x);
       if(d2 < dist)
       {
         c->x_move = k;

commit 2ce1b34d0ed2d13dca319881e66c899e03992b9a

Fix playback of audio notes
This got broken back in 2014 with 06316e6. Not many people missed the
feature. :-)

src/views/view.c
@@ -1211,7 +1211,7 @@ int dt_view_image_expose(dt_view_image_over_t *image_over, uint32_t imgid, cairo
             x = (.04 + 8 * 0.04 - 1.9 * .04) * fscale;
           dt_view_draw_audio(cr, x, y, s);
           // mouse is over the audio icon
-          if(fabsf(px - x) <= 1.2 * s && fabsf(-y) <= 1.2 * s) *image_over = DT_VIEW_AUDIO;
+          if(fabsf(px - x) <= 1.2 * s && fabsf(py - y) <= 1.2 * s) *image_over = DT_VIEW_AUDIO;
         }
       }
 