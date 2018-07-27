24445d363e92d4d395c09d629fcd502c86e4d5a2
Tobias Ellinghaus <me@houz.org>
2018-07-27 12:18:45 +0200

    bauhaus: Add widget to populate function callback

--- src/bauhaus/bauhaus.h
+++ src/bauhaus/bauhaus.h
@@ -288,7 +288,7 @@ int dt_bauhaus_combobox_get(GtkWidget *w);
 const GList *dt_bauhaus_combobox_get_labels(GtkWidget *w);
 void dt_bauhaus_combobox_clear(GtkWidget *w);
 void dt_bauhaus_combobox_set_default(GtkWidget *widget, int def);
-void dt_bauhaus_combobox_add_populate_fct(GtkWidget *widget, void (*fct)(struct dt_iop_module_t **module));
+void dt_bauhaus_combobox_add_populate_fct(GtkWidget *widget, void (*fct)(GtkWidget *w, struct dt_iop_module_t **module));
 
 // key accel parsing:
 // execute a line of input
