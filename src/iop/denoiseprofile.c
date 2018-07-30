ff058274618324c7e0b68bb80e5abe722e9e2eb6
Tobias Ellinghaus <me@houz.org>
2018-07-30 11:50:41 +0200

    profiled denoise: Update preset blending if needed

--- src/iop/denoiseprofile.c
+++ src/iop/denoiseprofile.c
@@ -23,6 +23,7 @@
 #include "common/opencl.h"
 #include "common/exif.h"
 #include "control/control.h"
+#include "develop/blend.h"
 #include "develop/imageop.h"
 #include "develop/imageop_math.h"
 #include "develop/tiling.h"
@@ -146,33 +147,50 @@ int flags()
   return IOP_FLAGS_SUPPORTS_BLENDING | IOP_FLAGS_ALLOW_TILING;
 }
 
-void init_presets(dt_iop_module_so_t *self)
+static void add_preset(dt_iop_module_so_t *self, const char *name, const char *pi, const char *bpi, const int blendop_version)
 {
   int len, blen;
-  // this blob was exported as dtstyle and copied from there:
-  const char *p0i  = "0000803f0000803f000080bf05a32a3636106236c01b58341f1609b446faddb301000000";
-  const char *bp0i = "gz12eJxjZGBgEGYAgRNODESDBnsIHll8AM62GP8=";
-  uint8_t *p0  = dt_exif_xmp_decode(p0i, strlen(p0i), &len);
+  uint8_t *p  = dt_exif_xmp_decode(pi, strlen(pi), &len);
   assert(len == sizeof(dt_iop_denoiseprofile_params_t));
-  uint8_t *bp0 = dt_exif_xmp_decode(bp0i, strlen(bp0i), &blen);
-  assert(blen == sizeof(dt_develop_blend_params_t));
-  dt_gui_presets_add_with_blendop(
-      _("chroma (use on 1st instance)"), self->op, self->version(),
-      p0, len, bp0, 1);
-  free(bp0);
-  free(p0);
-  
-  const char *p1i = "000080400000003f000080bf38c54438c0d7b83828ff8934230e0c344a216d3400000000";
-  const char *bp1i = "gz12eJxjZGBgEGAAgWlODESDBnsIHll8AJKaGMo=";
-  uint8_t *p1  = dt_exif_xmp_decode(p1i, strlen(p1i), &len);
-  assert(len == sizeof(dt_iop_denoiseprofile_params_t));
-  uint8_t *bp1 = dt_exif_xmp_decode(bp1i, strlen(bp1i), &blen);
-  assert(blen == sizeof(dt_develop_blend_params_t));
-  dt_gui_presets_add_with_blendop(
-      _("luma (use on 2nd instance)"), self->op, self->version(),
-      p1, len, bp1, 1);
-  free(bp1);
-  free(p1);
+  uint8_t *bp = dt_exif_xmp_decode(bpi, strlen(bpi), &blen);
+  if(blendop_version != dt_develop_blend_version())
+  {
+    // update to current blendop params format
+    void *bp_new = malloc(sizeof(dt_develop_blend_params_t));
+
+    if(dt_develop_blend_legacy_params_from_so(self, bp, blendop_version, bp_new, dt_develop_blend_version(),
+      blen) == 0)
+    {
+      free(bp);
+      bp = bp_new;
+      blen = sizeof(dt_develop_blend_params_t);
+    }
+    else
+    {
+      free(bp);
+      free(bp_new);
+      bp = NULL;
+    }
+  }
+
+  if(p && bp)
+    dt_gui_presets_add_with_blendop(name, self->op, self->version(),
+                                    p, len, bp, 1);
+  free(bp);
+  free(p);
+}
+
+void init_presets(dt_iop_module_so_t *self)
+{
+  // these blobs were exported as dtstyle and copied from there:
+  add_preset(self,
+             _("chroma (use on 1st instance)"),
+             "0000803f0000803f000080bf05a32a3636106236c01b58341f1609b446faddb301000000",
+             "gz12eJxjZGBgEGYAgRNODESDBnsIHll8AM62GP8=", 7);
+  add_preset(self,
+             _("luma (use on 2nd instance)"),
+             "000080400000003f000080bf38c54438c0d7b83828ff8934230e0c344a216d3400000000",
+             "gz12eJxjZGBgEGAAgWlODESDBnsIHll8AJKaGMo=", 7);
 }
 
 typedef union floatint_t
