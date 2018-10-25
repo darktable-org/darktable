aee7c699ebc820fd1f9ab9121a9e821c240fcba2
rawfiner <rawfiner@gmail.com>
2018-10-25 22:16:59 +0200

    Contextual help: clang-format

--- src/libs/navigation.c
+++ src/libs/navigation.c
@@ -106,7 +106,7 @@ void gui_init(dt_lib_module_t *self)
 
   /* create drawingarea */
   self->widget = gtk_drawing_area_new();
-  dt_gui_add_help_link(self->widget,dt_get_help_url(self->plugin_name));
+  dt_gui_add_help_link(self->widget, dt_get_help_url(self->plugin_name));
   gtk_widget_set_events(self->widget, GDK_EXPOSURE_MASK | GDK_POINTER_MOTION_MASK
                                       | GDK_POINTER_MOTION_HINT_MASK | GDK_BUTTON_PRESS_MASK
                                       | GDK_BUTTON_RELEASE_MASK | GDK_STRUCTURE_MASK);
