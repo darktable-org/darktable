6286938b3f040e20063869be82566fdf5a5669e0
johannes hanika <hanatos@gmail.com>
2018-10-27 09:50:19 +1300

    darktable-chart: enable process button for it8 ref values, too

--- src/chart/main.c
+++ src/chart/main.c
@@ -444,6 +444,7 @@ static gboolean open_it8(dt_lut_t *self, const char *filename)
   {
     free(self->reference_filename);
     self->reference_filename = get_filename_base(filename);
+    gtk_widget_set_sensitive(self->process_button, TRUE);
   }
   gtk_widget_queue_draw(self->source.drawing_area);
 
