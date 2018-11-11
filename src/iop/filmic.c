05df1a4e82247859a511e78a8201a0265c99e15a
Pascal Obry <pascal@obry.net>
2018-11-11 11:39:58 +0100

    filmic: fix color pick.
    The only issue remaining is that if you press the auto-tune color picker
    the second time to release it the icon stays active. It is not, but the
    status is not reset. If you click on the image it is disabled. Not sure
    why!

--- src/iop/filmic.c
+++ src/iop/filmic.c
@@ -77,7 +77,8 @@ typedef enum dt_iop_filmic_pickcolor_type_t
   DT_PICKPROFLOG_NONE = 0,
   DT_PICKPROFLOG_GREY_POINT = 1,
   DT_PICKPROFLOG_BLACK_POINT = 2,
-  DT_PICKPROFLOG_WHITE_POINT = 3
+  DT_PICKPROFLOG_WHITE_POINT = 3,
+  DT_PICKPROFLOG_AUTOTUNE = 4
 } dt_iop_filmic_pickcolor_type_t;
 
 typedef struct dt_iop_filmic_params_t
@@ -491,6 +492,7 @@ static void disable_colorpick(struct dt_iop_module_t *self)
   dt_iop_filmic_gui_data_t *g = (dt_iop_filmic_gui_data_t *)self->gui_data;
   self->request_color_pick = DT_REQUEST_COLORPICK_OFF;
   g->which_colorpicker = DT_PICKPROFLOG_NONE;
+  g->apply_picked_color = 0;
 }
 
 static void set_colorpick_state(dt_iop_filmic_gui_data_t *g, const int which_colorpicker)
@@ -498,6 +500,7 @@ static void set_colorpick_state(dt_iop_filmic_gui_data_t *g, const int which_col
   dt_bauhaus_widget_set_quad_active(g->grey_point_source, which_colorpicker == DT_PICKPROFLOG_GREY_POINT);
   dt_bauhaus_widget_set_quad_active(g->black_point_source, which_colorpicker == DT_PICKPROFLOG_BLACK_POINT);
   dt_bauhaus_widget_set_quad_active(g->white_point_source, which_colorpicker == DT_PICKPROFLOG_WHITE_POINT);
+  dt_bauhaus_widget_set_quad_active(g->auto_button, which_colorpicker == DT_PICKPROFLOG_AUTOTUNE);
 }
 
 static void security_threshold_callback(GtkWidget *slider, gpointer user_data)
@@ -530,27 +533,11 @@ static void security_threshold_callback(GtkWidget *slider, gpointer user_data)
   dt_dev_add_history_item(darktable.develop, self, TRUE);
 }
 
-static void optimize_button_pressed_callback(GtkWidget *button, gpointer user_data)
+static void apply_autotune(dt_iop_module_t *self/*GtkWidget *button, gpointer user_data*/)
 {
-  dt_iop_module_t *self = (dt_iop_module_t *)user_data;
+  if(darktable.gui->reset) return;
+
   dt_iop_filmic_gui_data_t *g = (dt_iop_filmic_gui_data_t *)self->gui_data;
-  if(self->dt->gui->reset) return;
-  if(self->off) gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(self->off), 1);
-
-  disable_colorpick(self);
-  set_colorpick_state(g, g->which_colorpicker);
-
-  dt_iop_request_focus(self);
-  dt_lib_colorpicker_set_area(darktable.lib, 0.99);
-  dt_control_queue_redraw();
-  self->request_color_pick = DT_REQUEST_COLORPICK_MODULE;
-  dt_dev_reprocess_all(self->dev);
-
-  if(self->request_color_pick != DT_REQUEST_COLORPICK_MODULE || self->picked_color_max[0] < 0.0f)
-  {
-    dt_control_log(_("wait for the preview to be updated."));
-    return;
-  }
 
   dt_iop_filmic_params_t *p = (dt_iop_filmic_params_t *)self->params;
 
@@ -584,8 +571,6 @@ static void optimize_button_pressed_callback(GtkWidget *button, gpointer user_da
   darktable.gui->reset = 0;
 
   sanitize_latitude(p, g);
-
-  self->request_color_pick = DT_REQUEST_COLORPICK_OFF;
 
   dt_dev_add_history_item(darktable.develop, self, TRUE);
 }
@@ -729,6 +714,9 @@ static int call_apply_picked_color(struct dt_iop_module_t *self, dt_iop_filmic_g
      case DT_PICKPROFLOG_WHITE_POINT:
        apply_auto_white_point_source(self);
        break;
+     case DT_PICKPROFLOG_AUTOTUNE:
+       apply_autotune(self);
+       break;
      default:
        handled = 0;
        break;
@@ -746,6 +734,8 @@ static int get_colorpick_from_button(GtkWidget *button, dt_iop_filmic_gui_data_t
     which_colorpicker = DT_PICKPROFLOG_BLACK_POINT;
   else if(button == g->white_point_source)
     which_colorpicker = DT_PICKPROFLOG_WHITE_POINT;
+  else if(button == g->auto_button)
+    which_colorpicker = DT_PICKPROFLOG_AUTOTUNE;
 
   return which_colorpicker;
 }
@@ -806,6 +796,8 @@ static void color_picker_callback(GtkWidget *button, dt_iop_module_t *self)
   }
 
   set_colorpick_state(g, g->which_colorpicker);
+  dt_iop_request_focus(self);
+  dt_control_queue_redraw();
 }
 
 void gui_focus(struct dt_iop_module_t *self, gboolean in)
@@ -1188,6 +1180,13 @@ void cleanup_global(dt_iop_module_so_t *module)
   module->data = NULL;
 }
 
+void gui_reset(dt_iop_module_t *self)
+{
+  dt_iop_filmic_gui_data_t *g = (dt_iop_filmic_gui_data_t *)self->gui_data;
+  g->apply_picked_color = 0;
+  disable_colorpick(self);
+}
+
 void gui_init(dt_iop_module_t *self)
 {
   self->gui_data = malloc(sizeof(dt_iop_filmic_gui_data_t));
@@ -1244,11 +1243,14 @@ void gui_init(dt_iop_module_t *self)
   gtk_widget_set_tooltip_text(g->security_factor, _("enlarge or shrink the computed dynamic range"));
   g_signal_connect(G_OBJECT(g->security_factor), "value-changed", G_CALLBACK(security_threshold_callback), self);
 
-  g->auto_button = gtk_button_new_with_label(_("auto tune source"));
+  g->auto_button = dt_bauhaus_combobox_new(self);
+  dt_bauhaus_widget_set_label(g->auto_button, NULL, _("auto tune source"));
+  dt_bauhaus_widget_set_quad_paint(g->auto_button, dtgtk_cairo_paint_colorpicker,
+                                   CPF_STYLE_FLAT | CPF_DO_NOT_USE_BORDER, NULL);
+  dt_bauhaus_widget_set_quad_toggle(g->auto_button, TRUE);
+  g_signal_connect(G_OBJECT(g->auto_button), "quad-pressed", G_CALLBACK(color_picker_callback), self);
   gtk_widget_set_tooltip_text(g->auto_button, _("make an optimization with some guessing"));
   gtk_box_pack_start(GTK_BOX(self->widget), g->auto_button, TRUE, TRUE, 0);
-  g_signal_connect(G_OBJECT(g->auto_button), "clicked", G_CALLBACK(optimize_button_pressed_callback), self);
-
 
   gtk_box_pack_start(GTK_BOX(self->widget), dt_ui_section_label_new(_("filmic S curve")), FALSE, FALSE, 5);
 
