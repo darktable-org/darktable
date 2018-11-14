38fd1e50dc723f9e507e148b3f3621fc41254eee
Aurélien PIERRE <aurelien@aurelienpierre.com>
2018-11-14 01:17:40 -0500

    make UI labels more explicit

--- src/iop/tonecurve.c
+++ src/iop/tonecurve.c
@@ -1154,10 +1154,10 @@ void gui_init(struct dt_iop_module_t *self)
 
   c->autoscale_ab = dt_bauhaus_combobox_new(self);
   dt_bauhaus_widget_set_label(c->autoscale_ab, NULL, _("color space"));
-  dt_bauhaus_combobox_add(c->autoscale_ab, _("Lab, bounded channels"));
+  dt_bauhaus_combobox_add(c->autoscale_ab, _("Lab, linked channels"));
   dt_bauhaus_combobox_add(c->autoscale_ab, _("Lab, independant channels"));
-  dt_bauhaus_combobox_add(c->autoscale_ab, _("XYZ, bounded channels"));
-  dt_bauhaus_combobox_add(c->autoscale_ab, _("RGB, bounded channels"));
+  dt_bauhaus_combobox_add(c->autoscale_ab, _("XYZ, linked channels"));
+  dt_bauhaus_combobox_add(c->autoscale_ab, _("RGB, linked channels"));
   gtk_box_pack_start(GTK_BOX(self->widget), c->autoscale_ab, TRUE, TRUE, 0);
   gtk_widget_set_tooltip_text(c->autoscale_ab, _("if set to auto, a and b curves have no effect and are "
                                                  "not displayed. chroma values (a and b) of each pixel are "
