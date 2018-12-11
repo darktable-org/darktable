a2757c495fa660d7d7b43bbec51c458b6c1d4967
Matthieu Moy <git@matthieu-moy.fr>
2018-12-11 22:34:54 +0100

    lighttable: colors customizable through CSS
    Most colors used to display thumbnails were hardcoded values. For a
    white background theme, the colors were rather dark. With this patch,
    one can get a light theme for the lighttable with eg.
    
    @define-color thumbnail_bg_color #eee;
    @define-color thumbnail_selected_bg_color shade(@thumbnail_bg_color, .9);
    @define-color thumbnail_hover_bg_color shade(@thumbnail_bg_color, .8);
    @define-color thumbnail_outline_color #7f7f7f;
    @define-color thumbnail_selected_outline_color shade(@thumbnail_outline_color, .8);
    @define-color thumbnail_hover_outline_color shade(@thumbnail_outline_color, .6);
    @define-color thumbnail_font_color #aaa;
    @define-color thumbnail_selected_font_color shade(@thumbnail_font_color, .8);
    @define-color thumbnail_hover_font_color shade(@thumbnail_font_color, .6);
    @define-color thumbnail_border_color #222;
    @define-color thumbnail_selected_border_color #eee;

--- src/gui/gtk.c
+++ src/gui/gtk.c
@@ -1177,6 +1177,17 @@ int dt_gui_gtk_init(dt_gui_gtk_t *gui)
     [DT_GUI_COLOR_LIGHTTABLE_PREVIEW_BG] = { "lighttable_preview_bg_color", { .1, .1, .1, 1.0 } },
     [DT_GUI_COLOR_BRUSH_CURSOR] = { "brush_cursor", { 1., 1., 1., 0.9 } },
     [DT_GUI_COLOR_BRUSH_TRACE] = { "brush_trace", { 0., 0., 0., 0.8 } },
+    [DT_GUI_COLOR_THUMBNAIL_BG] = { "thumbnail_bg_color", { 0.4, 0.4, 0.4, 1.0 } },
+    [DT_GUI_COLOR_THUMBNAIL_SELECTED_BG] = { "thumbnail_selected_bg_color", { 0.6, 0.6, 0.6, 1.0 } },
+    [DT_GUI_COLOR_THUMBNAIL_HOVER_BG] = { "thumbnail_hover_bg_color", { 0.8, 0.8, 0.8, 1.0 } },
+    [DT_GUI_COLOR_THUMBNAIL_OUTLINE] = { "thumbnail_outline_color", { 0.2, 0.2, 0.2, 1.0 } },
+    [DT_GUI_COLOR_THUMBNAIL_SELECTED_OUTLINE] = { "thumbnail_selected_outline_color", { 0.4, 0.4, 0.4, 1.0 } },
+    [DT_GUI_COLOR_THUMBNAIL_HOVER_OUTLINE] = { "thumbnail_hover_outline_color", { 0.6, 0.6, 0.6, 1.0 } },
+    [DT_GUI_COLOR_THUMBNAIL_FONT] = { "thumbnail_font_color", { 0.425, 0.425, 0.425, 1.0 } },
+    [DT_GUI_COLOR_THUMBNAIL_SELECTED_FONT] = { "thumbnail_selected_font_color", { 0.5, 0.5, 0.5, 1.0 } },
+    [DT_GUI_COLOR_THUMBNAIL_HOVER_FONT] = { "thumbnail_hover_font_color", { 0.7, 0.7, 0.7, 1.0 } },
+    [DT_GUI_COLOR_THUMBNAIL_BORDER] = { "thumbnail_border_color", { 0.1, 0.1, 0.1, 1.0 } },
+    [DT_GUI_COLOR_THUMBNAIL_SELECTED_BORDER] = { "thumbnail_selected_border_color", { 0.9, 0.9, 0.9, 1.0 } },
   };
 
   for(int i = 0; i < DT_GUI_COLOR_LAST; i++)
