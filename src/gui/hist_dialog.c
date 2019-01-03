ce558953debf0d9976cf2526905de0e46683d0d7
Pascal Obry <pascal@obry.net>
2019-01-03 18:41:29 +0100

    [history|styles]_dialog: fix build on Gtk+ < 3.22

--- src/gui/hist_dialog.c
+++ src/gui/hist_dialog.c
@@ -149,7 +149,8 @@ int dt_gui_hist_dialog_new(dt_gui_hist_dialog_t *d, int imgid, gboolean iscopy)
   GtkWidget *scroll = gtk_scrolled_window_new(NULL, NULL);
   gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scroll), GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
   gtk_scrolled_window_set_min_content_height(GTK_SCROLLED_WINDOW(scroll), DT_PIXEL_APPLY_DPI(300));
-  gtk_scrolled_window_set_max_content_height(GTK_SCROLLED_WINDOW(scroll), DT_PIXEL_APPLY_DPI(700));
+//  only available in 3.22, and not making the expected job anyway
+//  gtk_scrolled_window_set_max_content_height(GTK_SCROLLED_WINDOW(scroll), DT_PIXEL_APPLY_DPI(700));
   gtk_scrolled_window_set_propagate_natural_height(GTK_SCROLLED_WINDOW(scroll), TRUE);
   gtk_widget_set_margin_start(scroll, DT_PIXEL_APPLY_DPI(5));
   gtk_widget_set_margin_end(scroll, DT_PIXEL_APPLY_DPI(5));
