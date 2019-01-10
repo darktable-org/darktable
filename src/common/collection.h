de8171fefdc893ba6078d2b01bc8219582961d34
Tobias Ellinghaus <me@houz.org>
2019-01-10 14:28:14 +0100

    'operator' is keyword in C++, don't use it in header

--- src/common/collection.h
+++ src/common/collection.h
@@ -226,11 +226,9 @@ void dt_collection_deserialize(char *buf);
 int dt_collection_serialize(char *buf, int bufsize);
 
 /* splits an input string into a number part and an optional operator part */
-void dt_collection_split_operator_number(const gchar *input, char **number1, char **number2, char **operator);
-void dt_collection_split_operator_datetime(const gchar *input, char **number1, char **number2,
-                                           char **operator);
-void dt_collection_split_operator_exposure(const gchar *input, char **number1, char **number2,
-                                           char **operator);
+void dt_collection_split_operator_number(const gchar *input, char **number1, char **number2, char **op);
+void dt_collection_split_operator_datetime(const gchar *input, char **number1, char **number2, char **op);
+void dt_collection_split_operator_exposure(const gchar *input, char **number1, char **number2, char **op);
 
 int64_t dt_collection_get_image_position(const int32_t image_id);
 void dt_collection_shift_image_positions(const unsigned int length, const int64_t image_position);
