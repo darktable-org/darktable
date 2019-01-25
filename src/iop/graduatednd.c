500b07805f0851a87e11bd31f3b37e14326ddd07
Pascal Obry <pascal@obry.net>
2019-01-25 17:03:30 +0100

    graduadednd: add color picker.
    Fixing 6 years old #9095

--- src/iop/graduatednd.c
+++ src/iop/graduatednd.c
@@ -34,6 +34,7 @@
 #include "develop/tiling.h"
 #include "dtgtk/gradientslider.h"
 #include "gui/accelerators.h"
+#include "gui/color_picker_proxy.h"
 #include "gui/gtk.h"
 #include "gui/presets.h"
 #include "iop/iop_api.h"
@@ -45,6 +46,12 @@
 #define CLIP(x) ((x < 0.0f) ? 0.0f : (x > 1.0f) ? 1.0f : x)
 
 DT_MODULE_INTROSPECTION(1, dt_iop_graduatednd_params_t)
+
+typedef enum dt_iop_graduatednd_pickcolor_type_t
+{
+  DT_GRADUATEDND_NONE = 0,
+  DT_GRADUATEDND_HUESAT = 1,
+} dt_iop_graduatednd_pickcolor_type_t;
 
 typedef struct dt_iop_graduatednd_params_t
 {
@@ -116,6 +123,9 @@ typedef struct dt_iop_graduatednd_gui_data_t
   GtkWidget *label1, *label2, *label3, *label5, *label6; // density, compression, rotation, hue, saturation
   GtkWidget *scale1, *scale2, *scale3;                   // density, compression, rotation
   GtkWidget *gslider1, *gslider2;                        // hue, saturation
+
+  dt_iop_color_picker_t color_picker;
+  dt_iop_graduatednd_pickcolor_type_t which_colorpicker;
 
   int selected;
   int dragging;
@@ -393,6 +403,72 @@ static int set_points_from_grad(struct dt_iop_module_t *self, float *xa, float *
   *xb = pts[2] / self->dev->preview_pipe->backbuf_width;
   *yb = pts[3] / self->dev->preview_pipe->backbuf_height;
   return 1;
+}
+
+static inline void update_saturation_slider_end_color(GtkWidget *slider, float hue)
+{
+  float rgb[3];
+  hsl2rgb(rgb, hue, 1.0, 0.5);
+  dt_bauhaus_slider_set_stop(slider, 1.0, rgb[0], rgb[1], rgb[2]);
+}
+
+static int _iop_color_picker_get_set(dt_iop_module_t *self, GtkWidget *button)
+{
+  dt_iop_graduatednd_gui_data_t *g = (dt_iop_graduatednd_gui_data_t *)self->gui_data;
+
+  const dt_iop_graduatednd_pickcolor_type_t current_picker = g->which_colorpicker;
+
+  g->which_colorpicker = DT_GRADUATEDND_HUESAT;
+
+  if (current_picker == g->which_colorpicker)
+    return ALREADY_SELECTED;
+  else
+    return g->which_colorpicker;
+}
+
+static void _iop_color_picker_apply(struct dt_iop_module_t *self)
+{
+  dt_iop_graduatednd_gui_data_t *g = (dt_iop_graduatednd_gui_data_t *)self->gui_data;
+  dt_iop_graduatednd_params_t *p = (dt_iop_graduatednd_params_t *)self->params;
+
+  // convert picker RGB 2 HSL
+  float H = .0f, S = .0f, L = .0f;
+  rgb2hsl(self->picked_color, &H, &S, &L);
+
+  if(fabsf(p->hue - H) < 0.0001f && fabsf(p->saturation - S) < 0.0001f)
+  {
+    // interrupt infinite loops
+    return;
+  }
+
+  p->hue        = H;
+  p->saturation = S;
+
+  darktable.gui->reset = 1;
+  dt_bauhaus_slider_set(g->gslider1, p->hue);
+  dt_bauhaus_slider_set(g->gslider2, p->saturation);
+  update_saturation_slider_end_color(g->gslider2, p->hue);
+  darktable.gui->reset = 0;
+
+  dt_dev_add_history_item(darktable.develop, self, TRUE);
+}
+
+static void _iop_color_picker_update(dt_iop_module_t *self)
+{
+  dt_iop_graduatednd_gui_data_t *g = (dt_iop_graduatednd_gui_data_t *)self->gui_data;
+  dt_bauhaus_widget_set_quad_active(g->gslider1, g->which_colorpicker == DT_GRADUATEDND_HUESAT);
+}
+
+static void _iop_color_picker_reset(struct dt_iop_module_t *self)
+{
+  dt_iop_graduatednd_gui_data_t *g = (dt_iop_graduatednd_gui_data_t *)self->gui_data;
+  g->which_colorpicker = DT_GRADUATEDND_NONE;
+}
+
+void gui_reset(struct dt_iop_module_t *self)
+{
+  dt_iop_graduatednd_gui_data_t *g = (dt_iop_graduatednd_gui_data_t *)self->gui_data;
+  dt_iop_color_picker_reset(&g->color_picker, TRUE);
 }
 
 void gui_post_expose(struct dt_iop_module_t *self, cairo_t *cr, int32_t width, int32_t height,
@@ -1060,18 +1136,14 @@ void cleanup_pipe(struct dt_iop_module_t *self, dt_dev_pixelpipe_t *pipe, dt_dev
   piece->data = NULL;
 }
 
-static inline void update_saturation_slider_end_color(GtkWidget *slider, float hue)
-{
-  float rgb[3];
-  hsl2rgb(rgb, hue, 1.0, 0.5);
-  dt_bauhaus_slider_set_stop(slider, 1.0, rgb[0], rgb[1], rgb[2]);
-}
-
 void gui_update(struct dt_iop_module_t *self)
 {
   dt_iop_module_t *module = (dt_iop_module_t *)self;
   dt_iop_graduatednd_gui_data_t *g = (dt_iop_graduatednd_gui_data_t *)self->gui_data;
   dt_iop_graduatednd_params_t *p = (dt_iop_graduatednd_params_t *)module->params;
+
+  dt_iop_color_picker_reset(&g->color_picker, TRUE);
+
   dt_bauhaus_slider_set(g->scale1, p->density);
   dt_bauhaus_slider_set(g->scale2, p->compression);
   dt_bauhaus_slider_set(g->scale3, p->rotation);
@@ -1181,8 +1253,9 @@ void gui_init(struct dt_iop_module_t *self)
   dt_bauhaus_slider_set_stop(g->gslider1, 1.0f, 1.0f, 0.0f, 0.0f);
   gtk_widget_set_tooltip_text(g->gslider1, _("select the hue tone of filter"));
   g_signal_connect(G_OBJECT(g->gslider1), "value-changed", G_CALLBACK(hue_callback), self);
-
-  gtk_box_pack_start(GTK_BOX(self->widget), g->gslider1, TRUE, TRUE, 0);
+  dt_bauhaus_widget_set_quad_paint(g->gslider1, dtgtk_cairo_paint_colorpicker, CPF_STYLE_FLAT | CPF_DO_NOT_USE_BORDER, NULL);
+  dt_bauhaus_widget_set_quad_toggle(g->gslider1, TRUE);
+  g_signal_connect(G_OBJECT(g->gslider1), "quad-pressed", G_CALLBACK(dt_iop_color_picker_callback), &g->color_picker);
 
   /* saturation slider */
   g->gslider2 = dt_bauhaus_slider_new_with_range(self, 0.0f, 1.0f, 0.01f, 0.0f, 2);
@@ -1192,10 +1265,20 @@ void gui_init(struct dt_iop_module_t *self)
   gtk_widget_set_tooltip_text(g->gslider2, _("select the saturation of filter"));
   g_signal_connect(G_OBJECT(g->gslider2), "value-changed", G_CALLBACK(saturation_callback), self);
 
-  gtk_box_pack_start(GTK_BOX(self->widget), g->gslider2, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(self->widget), GTK_WIDGET(g->gslider1), TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(self->widget), GTK_WIDGET(g->gslider2), TRUE, TRUE, 0);
+
   g->selected = 0;
   g->dragging = 0;
   g->define = 0;
+
+  init_picker(&g->color_picker,
+              self,
+              DT_COLOR_PICKER_POINT,
+              _iop_color_picker_get_set,
+              _iop_color_picker_apply,
+              _iop_color_picker_reset,
+              _iop_color_picker_update);
 }
 
 void gui_cleanup(struct dt_iop_module_t *self)
