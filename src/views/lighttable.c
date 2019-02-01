3c2da71f17f58384966f1fdcd1510f3a54d97a30
Pascal Obry <pascal@obry.net>
2019-02-01 23:22:34 +0100

    lighttable-filemanager: fix some navigation glitches.

--- src/views/lighttable.c
+++ src/views/lighttable.c
@@ -261,7 +261,6 @@ static void move_view(dt_library_t *lib, dt_lighttable_direction_t dir)
     case DIRECTION_UP:
     {
       if(lib->offset >= 1) lib->offset = lib->offset - iir;
-      while(lib->offset < 0) lib->offset += iir;
     }
     break;
     case DIRECTION_DOWN:
@@ -273,7 +272,7 @@ static void move_view(dt_library_t *lib, dt_lighttable_direction_t dir)
     case DIRECTION_PGUP:
     {
       lib->offset -= (lib->max_rows - 1) * iir;
-      while(lib->offset < 0) lib->offset += iir;
+      while(lib->offset <= -iir) lib->offset += iir;
     }
     break;
     case DIRECTION_PGDOWN:
@@ -336,7 +335,6 @@ static void zoom_around_image(dt_library_t *lib, double pointerx, double pointer
   lib->offset = zoom_anchor_image - pi - (pj * new_images_in_row);
   lib->first_visible_filemanager = lib->offset;
   lib->offset_changed = TRUE;
-  lib->images_in_row = new_images_in_row;
 }
 
 static void _view_lighttable_collection_listener_callback(gpointer instance, gpointer user_data)
@@ -466,9 +464,15 @@ static void _update_collected_images(dt_view_t *self)
 static void _set_position(dt_view_t *self, uint32_t pos)
 {
   dt_library_t *lib = (dt_library_t *)self->data;
-  lib->first_visible_filemanager = lib->first_visible_zoomable = lib->offset = pos;
-  lib->offset_changed = TRUE;
-  dt_control_queue_redraw_center();
+  // only reset position when not already with a changed offset, this is because if the offset is
+  // already changed it means that we are about to change the display (zoom in or out for example).
+  // And in this case a new offset is already positioned and we don't want to reset it.
+  if(!lib->offset_changed)
+  {
+    lib->first_visible_filemanager = lib->first_visible_zoomable = lib->offset = pos;
+    lib->offset_changed = TRUE;
+    dt_control_queue_redraw_center();
+  }
 }
 
 static uint32_t _get_position(dt_view_t *self)
@@ -863,7 +867,7 @@ end_query_cache:
 
             // if we have moved the view we need to expose again all pictures as the first row or last one need to
             // be redrawn properly. for this we just record the missing thumbs.
-            if(lib->offset_changed && mouse_over_id != -1)
+            if(offset_changed && mouse_over_id != -1)
             {
               missing += iir;
             }
@@ -1125,8 +1129,6 @@ after_drawing:
     free(imgids);
   }
 
-  lib->offset_changed = FALSE;
-
   free(query_ids);
   // oldpan = pan;
   if(darktable.unmuted & DT_DEBUG_CACHE) dt_mipmap_cache_print(darktable.mipmap_cache);
@@ -1145,6 +1147,8 @@ after_drawing:
     darktable.gui->center_tooltip = 0;
     gtk_widget_set_tooltip_text(dt_ui_center(darktable.gui->ui), "");
   }
+
+  lib->offset_changed = FALSE;
 
   return missing;
 }
