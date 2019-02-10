2b8fefe6c332c05a15cde9754c396b3339ba7db0
mihkel-t <47502080+mihkel-t@users.noreply.github.com>
2019-02-10 20:01:02 +0200

    Add string context

--- src/iop/retouch.c
+++ src/iop/retouch.c
@@ -2824,7 +2824,7 @@ void gui_init(dt_iop_module_t *self)
   g->cmb_fill_mode = dt_bauhaus_combobox_new(self);
   dt_bauhaus_widget_set_label(g->cmb_fill_mode, NULL, _("fill mode"));
   dt_bauhaus_combobox_add(g->cmb_fill_mode, _("erase"));
-  dt_bauhaus_combobox_add(g->cmb_fill_mode, _("color"));
+  dt_bauhaus_combobox_add(g->cmb_fill_mode, C_("fill mode", "color"));
   g_object_set(g->cmb_fill_mode, "tooltip-text", _("erase the detail or fills with chosen color"), (char *)NULL);
   g_signal_connect(G_OBJECT(g->cmb_fill_mode), "value-changed", G_CALLBACK(rt_fill_mode_callback), self);
 
