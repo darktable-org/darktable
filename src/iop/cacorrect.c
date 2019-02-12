1e9e710ba23768f64566d2605b1130c1566ff06b
Pascal Obry <pascal@obry.net>
2019-02-12 07:49:38 +0100

    Ensure that module without gui_data are properly refreshed.
    First we remove the call to gui_update() for module with a NULL
    gui_data. This was to fix #12458. So now we add a fake gui_data
    to ensure that gui_update() will be called for those modules.
    
    Fixes #12594.

--- src/iop/cacorrect.c
+++ src/iop/cacorrect.c
@@ -46,14 +46,7 @@ typedef struct dt_iop_cacorrect_gui_data_t
 {
 } dt_iop_cacorrect_gui_data_t;
 
-typedef struct dt_iop_cacorrect_data_t
-{
-} dt_iop_cacorrect_data_t;
-
-typedef struct dt_iop_cacorrect_global_data_t
-{
-} dt_iop_cacorrect_global_data_t;
-
+dt_iop_cacorrect_gui_data_t dummy;
 
 // this returns a translatable name
 const char *name()
@@ -1545,21 +1538,17 @@ void cleanup(dt_iop_module_t *module)
 void commit_params(struct dt_iop_module_t *self, dt_iop_params_t *params, dt_dev_pixelpipe_t *pipe,
                    dt_dev_pixelpipe_iop_t *piece)
 {
-  // dt_iop_cacorrect_params_t *p = (dt_iop_cacorrect_params_t *)params;
-  // dt_iop_cacorrect_data_t *d = (dt_iop_cacorrect_data_t *)piece->data;
   dt_image_t *img = &pipe->image;
   if(!(img->flags & DT_IMAGE_RAW) || dt_image_is_monochrome(img)) piece->enabled = 0;
 }
 
 void init_pipe(struct dt_iop_module_t *self, dt_dev_pixelpipe_t *pipe, dt_dev_pixelpipe_iop_t *piece)
 {
-  piece->data = malloc(sizeof(dt_iop_cacorrect_data_t));
   self->commit_params(self, self->default_params, pipe, piece);
 }
 
 void cleanup_pipe(struct dt_iop_module_t *self, dt_dev_pixelpipe_t *pipe, dt_dev_pixelpipe_iop_t *piece)
 {
-  free(piece->data);
   piece->data = NULL;
 }
 
@@ -1578,9 +1567,9 @@ void gui_update(dt_iop_module_t *self)
 
 void gui_init(dt_iop_module_t *self)
 {
-  self->gui_data = NULL;
   self->widget = gtk_label_new("");
   gtk_widget_set_halign(self->widget, GTK_ALIGN_START);
+  self->gui_data = &dummy;
   dt_gui_add_help_link(self->widget, dt_get_help_url(self->op));
 }
 
