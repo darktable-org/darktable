d81a5b7d000a2dde214cbee76a8e8a3f4b0a91e1
mihkel-t <47502080+mihkel-t@users.noreply.github.com>
2019-02-17 13:16:33 +0200

    Add string context

--- src/iop/denoiseprofile.c
+++ src/iop/denoiseprofile.c
@@ -2730,7 +2730,7 @@ void gui_init(dt_iop_module_t *self)
   g->channel_tabs = GTK_NOTEBOOK(gtk_notebook_new());
 
   gtk_notebook_append_page(GTK_NOTEBOOK(g->channel_tabs), GTK_WIDGET(gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0)),
-                           gtk_label_new(_("all")));
+                           gtk_label_new(C_("color channels", "all")));
   gtk_notebook_append_page(GTK_NOTEBOOK(g->channel_tabs), GTK_WIDGET(gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0)),
                            gtk_label_new(_("R")));
   gtk_notebook_append_page(GTK_NOTEBOOK(g->channel_tabs), GTK_WIDGET(gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0)),
