darktable-org/darktable

commit 8bb6f8fb9f8fe0b5ed4fb7af5a49d980d06b3b11

Define dt_image_geoloc_t for longitude/latitude/elevation and use it.
Just a code refactoring to ease undo/redo implementation for the
geoloc libs in the lighttable.

src/lua/image.c
@@ -514,9 +514,9 @@ int dt_lua_init_image(lua_State *L)
   luaA_struct_member(L, dt_image_t, filename, const char_filename_length);
   luaA_struct_member(L, dt_image_t, width, const int32_t);
   luaA_struct_member(L, dt_image_t, height, const int32_t);
-  luaA_struct_member(L, dt_image_t, longitude, protected_double); // set to NAN if value is not set
-  luaA_struct_member(L, dt_image_t, latitude, protected_double); // set to NAN if value is not set
-  luaA_struct_member(L, dt_image_t, elevation, protected_double); // set to NAN if value is not set
+  luaA_struct_member(L, dt_image_t, geoloc.longitude, protected_double); // set to NAN if value is not set
+  luaA_struct_member(L, dt_image_t, geoloc.latitude, protected_double); // set to NAN if value is not set
+  luaA_struct_member(L, dt_image_t, geoloc.elevation, protected_double); // set to NAN if value is not set
 
   dt_lua_init_int_type(L, dt_lua_image_t);
 

src/common/image.c
@@ -347,28 +347,28 @@ void dt_image_print_exif(const dt_image_t *img, char *line, size_t line_len)
              (int)img->exif_focal_length, (int)img->exif_iso);
 }
 
-void dt_image_set_location(const int32_t imgid, double lon, double lat)
+void dt_image_set_location(const int32_t imgid, dt_image_geoloc_t *geoloc)
 {
   /* fetch image from cache */
   dt_image_t *image = dt_image_cache_get(darktable.image_cache, imgid, 'w');
 
   /* set image location */
-  image->longitude = lon;
-  image->latitude = lat;
+  image->geoloc.longitude = geoloc->longitude;
+  image->geoloc.latitude = geoloc->latitude;
 
   /* store */
   dt_image_cache_write_release(darktable.image_cache, image, DT_IMAGE_CACHE_SAFE);
 }
 
-void dt_image_set_location_and_elevation(const int32_t imgid, double lon, double lat, double ele)
+void dt_image_set_location_and_elevation(const int32_t imgid, dt_image_geoloc_t *geoloc)
 {
   /* fetch image from cache */
   dt_image_t *image = dt_image_cache_get(darktable.image_cache, imgid, 'w');
 
   /* set image location and elevation */
-  image->longitude = lon;
-  image->latitude = lat;
-  image->elevation = ele;
+  image->geoloc.longitude = geoloc->longitude;
+  image->geoloc.latitude = geoloc->latitude;
+  image->geoloc.elevation = geoloc->elevation;
 
   /* store */
   dt_image_cache_write_release(darktable.image_cache, image, DT_IMAGE_CACHE_SAFE);
@@ -1218,9 +1218,9 @@ void dt_image_init(dt_image_t *img)
   img->exif_iso = 0;
   img->exif_focal_length = 0;
   img->exif_focus_distance = 0;
-  img->latitude = NAN;
-  img->longitude = NAN;
-  img->elevation = NAN;
+  img->geoloc.latitude = NAN;
+  img->geoloc.longitude = NAN;
+  img->geoloc.elevation = NAN;
   img->raw_black_level = 0;
   for(uint8_t i = 0; i < 4; i++) img->raw_black_level_separate[i] = 0;
   img->raw_white_point = 16384; // 2^14

src/common/exif.cc
@@ -96,7 +96,7 @@ const char *dt_xmp_keys[]
         "Xmp.darktable.blendop_version", "Xmp.darktable.multi_priority", "Xmp.darktable.multi_name", "Xmp.darktable.iop_order",
         "Xmp.darktable.xmp_version", "Xmp.darktable.raw_params", "Xmp.darktable.auto_presets_applied",
         "Xmp.darktable.mask_id", "Xmp.darktable.mask_type", "Xmp.darktable.mask_name",
-        "Xmp.darktable.masks_history", "Xmp.darktable.mask_num", "Xmp.darktable.mask_points", 
+        "Xmp.darktable.masks_history", "Xmp.darktable.mask_num", "Xmp.darktable.mask_points",
         "Xmp.darktable.mask_version", "Xmp.darktable.mask", "Xmp.darktable.mask_nb", "Xmp.darktable.mask_src",
         "Xmp.dc.creator", "Xmp.dc.publisher", "Xmp.dc.title", "Xmp.dc.description", "Xmp.dc.rights",
         "Xmp.xmpMM.DerivedFrom" };
@@ -326,12 +326,12 @@ static bool dt_exif_read_xmp_data(dt_image_t *img, Exiv2::XmpData &xmpData, int 
     /* read gps location */
     if(FIND_XMP_TAG("Xmp.exif.GPSLatitude"))
     {
-      img->latitude = dt_util_gps_string_to_number(pos->toString().c_str());
+      img->geoloc.latitude = dt_util_gps_string_to_number(pos->toString().c_str());
     }
 
     if(FIND_XMP_TAG("Xmp.exif.GPSLongitude"))
     {
-      img->longitude = dt_util_gps_string_to_number(pos->toString().c_str());
+      img->geoloc.longitude = dt_util_gps_string_to_number(pos->toString().c_str());
     }
 
     if(FIND_XMP_TAG("Xmp.exif.GPSAltitude"))
@@ -343,7 +343,7 @@ static bool dt_exif_read_xmp_data(dt_image_t *img, Exiv2::XmpData &xmpData, int 
         const char *sign = sign_str.c_str();
         double elevation = 0.0;
         if(dt_util_gps_elevation_to_number(pos->toRational(0).first, pos->toRational(0).second, sign[0], &elevation))
-          img->elevation = elevation;
+          img->geoloc.elevation = elevation;
       }
     }
 
@@ -699,7 +699,7 @@ static bool dt_exif_read_exif_data(dt_image_t *img, Exiv2::ExifData &exifData)
         if(dt_util_gps_rationale_to_number(pos->toRational(0).first, pos->toRational(0).second,
                                            pos->toRational(1).first, pos->toRational(1).second,
                                            pos->toRational(2).first, pos->toRational(2).second, sign[0], &latitude))
-          img->latitude = latitude;
+          img->geoloc.latitude = latitude;
       }
     }
 
@@ -714,7 +714,7 @@ static bool dt_exif_read_exif_data(dt_image_t *img, Exiv2::ExifData &exifData)
         if(dt_util_gps_rationale_to_number(pos->toRational(0).first, pos->toRational(0).second,
                                            pos->toRational(1).first, pos->toRational(1).second,
                                            pos->toRational(2).first, pos->toRational(2).second, sign[0], &longitude))
-          img->longitude = longitude;
+          img->geoloc.longitude = longitude;
       }
     }
 
@@ -727,7 +727,7 @@ static bool dt_exif_read_exif_data(dt_image_t *img, Exiv2::ExifData &exifData)
         const char *sign = sign_str.c_str();
         double elevation = 0.0;
         if(dt_util_gps_elevation_to_number(pos->toRational(0).first, pos->toRational(0).second, sign[0], &elevation))
-          img->elevation = elevation;
+          img->geoloc.elevation = elevation;
       }
     }
 
@@ -1381,16 +1381,16 @@ int dt_exif_read_blob(uint8_t **buf, const char *path, const int imgid, const in
 
       // GPS data
       const dt_image_t *cimg = dt_image_cache_get(darktable.image_cache, imgid, 'r');
-      if(!std::isnan(cimg->longitude) && !std::isnan(cimg->latitude))
+      if(!std::isnan(cimg->geoloc.longitude) && !std::isnan(cimg->geoloc.latitude))
       {
         exifData["Exif.GPSInfo.GPSVersionID"] = "02 02 00 00";
-        exifData["Exif.GPSInfo.GPSLongitudeRef"] = (cimg->longitude < 0) ? "W" : "E";
-        exifData["Exif.GPSInfo.GPSLatitudeRef"] = (cimg->latitude < 0) ? "S" : "N";
+        exifData["Exif.GPSInfo.GPSLongitudeRef"] = (cimg->geoloc.longitude < 0) ? "W" : "E";
+        exifData["Exif.GPSInfo.GPSLatitudeRef"] = (cimg->geoloc.latitude < 0) ? "S" : "N";
 
-        long long_deg = (int)floor(fabs(cimg->longitude));
-        long lat_deg = (int)floor(fabs(cimg->latitude));
-        long long_min = (int)floor((fabs(cimg->longitude) - floor(fabs(cimg->longitude))) * 60000000);
-        long lat_min = (int)floor((fabs(cimg->latitude) - floor(fabs(cimg->latitude))) * 60000000);
+        long long_deg = (int)floor(fabs(cimg->geoloc.longitude));
+        long lat_deg = (int)floor(fabs(cimg->geoloc.latitude));
+        long long_min = (int)floor((fabs(cimg->geoloc.longitude) - floor(fabs(cimg->geoloc.longitude))) * 60000000);
+        long lat_min = (int)floor((fabs(cimg->geoloc.latitude) - floor(fabs(cimg->geoloc.latitude))) * 60000000);
         gchar *long_str = g_strdup_printf("%ld/1 %ld/1000000 0/1", long_deg, long_min);
         gchar *lat_str = g_strdup_printf("%ld/1 %ld/1000000 0/1", lat_deg, lat_min);
         exifData["Exif.GPSInfo.GPSLongitude"] = long_str;
@@ -1398,12 +1398,12 @@ int dt_exif_read_blob(uint8_t **buf, const char *path, const int imgid, const in
         g_free(long_str);
         g_free(lat_str);
       }
-      if(!std::isnan(cimg->elevation))
+      if(!std::isnan(cimg->geoloc.elevation))
       {
         exifData["Exif.GPSInfo.GPSVersionID"] = "02 02 00 00";
-        exifData["Exif.GPSInfo.GPSAltitudeRef"] = (cimg->elevation < 0) ? "1" : "0";
+        exifData["Exif.GPSInfo.GPSAltitudeRef"] = (cimg->geoloc.elevation < 0) ? "1" : "0";
 
-        long ele_dm = (int)floor(fabs(10.0 * cimg->elevation));
+        long ele_dm = (int)floor(fabs(10.0 * cimg->geoloc.elevation));
         gchar *ele_str = g_strdup_printf("%ld/10", ele_dm);
         exifData["Exif.GPSInfo.GPSAltitude"] = ele_str;
         g_free(ele_str);
@@ -2177,7 +2177,7 @@ static void add_mask_entry_to_db(int imgid, mask_entry_t *entry)
   entry->already_added = TRUE;
 
   const int mask_num = 0;
-  
+
   sqlite3_stmt *stmt;
   DT_DEBUG_SQLITE3_PREPARE_V2(
     dt_database_get(darktable.db),
@@ -2293,14 +2293,14 @@ static int history_v1_to_v3(const int imgid)
       if(multi_priority_max >= 0)
       {
         const float iop_order = ((float)(multi_priority_max + 1 - multi_priority) / 1000.0) + prior_v1->iop_order;
-  
+
         DT_DEBUG_SQLITE3_PREPARE_V2(dt_database_get(darktable.db),
                                     "UPDATE main.history SET iop_order = ?2 WHERE imgid = ?1 AND num = ?3", -1,
                                     &stmt, NULL);
         DT_DEBUG_SQLITE3_BIND_INT(stmt, 1, imgid);
         DT_DEBUG_SQLITE3_BIND_DOUBLE(stmt, 2, iop_order);
         DT_DEBUG_SQLITE3_BIND_INT(stmt, 3, num);
-  
+
         if(sqlite3_step(stmt) != SQLITE_DONE)
         {
           fprintf(stderr, "[exif] error updating pipe order in history for image %d\n", imgid);
@@ -2392,12 +2392,12 @@ int dt_exif_xmp_read(dt_image_t *img, const char *filename, const int history_on
     {
       iop_order_version = pos->toLong();
     }
-    
+
 
     // masks
     GHashTable *mask_entries = NULL;
     GList *mask_entries_v3 = NULL;
-    
+
     // clean all old masks for this image
     DT_DEBUG_SQLITE3_PREPARE_V2(dt_database_get(darktable.db), "DELETE FROM main.masks_history WHERE imgid = ?1", -1,
                                 &stmt, NULL);
@@ -2424,9 +2424,9 @@ int dt_exif_xmp_read(dt_image_t *img, const char *filename, const int history_on
       while(m_entries)
       {
         mask_entry_t *mask_entry = (mask_entry_t *)m_entries->data;
-        
+
         add_mask_entry_to_db(img->id, mask_entry);
-        
+
         m_entries = g_list_next(m_entries);
       }
     }
@@ -2540,7 +2540,7 @@ int dt_exif_xmp_read(dt_image_t *img, const char *filename, const int history_on
       if(sqlite3_step(stmt) == SQLITE_ROW)
         num_masks = sqlite3_column_int(stmt, 0);
       sqlite3_finalize(stmt);
-      
+
       if(num_masks > 0)
       {
         // make room for mask_manager entry
@@ -2570,11 +2570,11 @@ int dt_exif_xmp_read(dt_image_t *img, const char *filename, const int history_on
           goto end;
         }
         sqlite3_finalize(stmt);
-        
+
         num++;
       }
     }
-    
+
     // we shouldn't change history_end when no history was read!
     if((pos = xmpData.findKey(Exiv2::XmpKey("Xmp.darktable.history_end"))) != xmpData.end() && num > 0)
     {

src/common/gpx.c
@@ -134,8 +134,7 @@ void dt_gpx_destroy(struct dt_gpx_t *gpx)
   g_free(gpx);
 }
 
-gboolean dt_gpx_get_location(struct dt_gpx_t *gpx, GTimeVal *timestamp, gdouble *lon, gdouble *lat,
-                             gdouble *ele)
+gboolean dt_gpx_get_location(struct dt_gpx_t *gpx, GTimeVal *timestamp, dt_image_geoloc_t *geoloc)
 {
   g_assert(gpx != NULL);
 
@@ -153,9 +152,9 @@ gboolean dt_gpx_get_location(struct dt_gpx_t *gpx, GTimeVal *timestamp, gdouble 
        closest location value start or end point */
     if((!item->next && timestamp->tv_sec >= tp->time.tv_sec) || (timestamp->tv_sec <= tp->time.tv_sec))
     {
-      *lon = tp->longitude;
-      *lat = tp->latitude;
-      *ele = tp->elevation;
+      geoloc->longitude = tp->longitude;
+      geoloc->latitude = tp->latitude;
+      geoloc->elevation = tp->elevation;
       return FALSE;
     }
 
@@ -163,9 +162,9 @@ gboolean dt_gpx_get_location(struct dt_gpx_t *gpx, GTimeVal *timestamp, gdouble 
     if(timestamp->tv_sec >= tp->time.tv_sec
        && timestamp->tv_sec <= ((_gpx_track_point_t *)item->next->data)->time.tv_sec)
     {
-      *lon = tp->longitude;
-      *lat = tp->latitude;
-      *ele = tp->elevation;
+      geoloc->longitude = tp->longitude;
+      geoloc->latitude = tp->latitude;
+      geoloc->elevation = tp->elevation;
       return TRUE;
     }
 

src/common/gpx.h
@@ -19,6 +19,7 @@
 #pragma once
 
 #include <glib.h>
+#include "common/image.h"
 
 struct dt_gpx_t;
 
@@ -29,7 +30,7 @@ void dt_gpx_destroy(struct dt_gpx_t *);
 /* fetch the lon,lat coords for time t, if within time range
   of gpx record return TRUE, FALSE is returned if out of time frame
   and closest record of lon,lat is filled */
-gboolean dt_gpx_get_location(struct dt_gpx_t *, GTimeVal *timestamp, gdouble *lon, gdouble *lat, gdouble *ele);
+gboolean dt_gpx_get_location(struct dt_gpx_t *, GTimeVal *timestamp, dt_image_geoloc_t *geoloc);
 
 // modelines: These editor modelines have been set for all relevant files by tools/update_modelines.sh
 // vim: shiftwidth=2 expandtab tabstop=2 cindent

src/common/image.h
@@ -118,6 +118,11 @@ typedef enum dt_image_loader_t
   LOADER_PNM = 10,
 } dt_image_loader_t;
 
+typedef struct dt_image_geoloc_t
+{
+  double longitude, latitude, elevation;
+} dt_image_geoloc_t;
+
 struct dt_cache_entry_t;
 // TODO: add color labels and such as cachable
 // __attribute__ ((aligned (128)))
@@ -165,9 +170,7 @@ typedef struct dt_image_t
   dt_image_raw_parameters_t legacy_flip; // unfortunately needed to convert old bits to new flip module.
 
   /* gps coords */
-  double longitude;
-  double latitude;
-  double elevation;
+  dt_image_geoloc_t geoloc;
 
   /* needed in exposure iop for Deflicker */
   uint16_t raw_black_level;
@@ -233,9 +236,9 @@ dt_image_orientation_t dt_image_get_orientation(const int imgid);
 /** get max width and height of the final processed image with its current hisotry stack */
 gboolean dt_image_get_final_size(const int32_t imgid, int *width, int *height);
 /** set image location lon/lat */
-void dt_image_set_location(const int32_t imgid, double lon, double lat);
+void dt_image_set_location(const int32_t imgid, dt_image_geoloc_t *geoloc);
 /** set image location lon/lat/ele */
-void dt_image_set_location_and_elevation(const int32_t imgid, double lon, double lat, double ele);
+void dt_image_set_location_and_elevation(const int32_t imgid, dt_image_geoloc_t *geoloc);
 /** returns 1 if there is history data found for this image, 0 else. */
 int dt_image_altered(const uint32_t imgid);
 /** set the image final/cropped aspect ratio */

src/common/image_cache.c
@@ -77,17 +77,17 @@ void dt_image_cache_allocate(void *data, dt_cache_entry_t *entry)
     uint32_t tmp = sqlite3_column_int(stmt, 18);
     memcpy(&img->legacy_flip, &tmp, sizeof(dt_image_raw_parameters_t));
     if(sqlite3_column_type(stmt, 19) == SQLITE_FLOAT)
-      img->longitude = sqlite3_column_double(stmt, 19);
+      img->geoloc.longitude = sqlite3_column_double(stmt, 19);
     else
-      img->longitude = NAN;
+      img->geoloc.longitude = NAN;
     if(sqlite3_column_type(stmt, 20) == SQLITE_FLOAT)
-      img->latitude = sqlite3_column_double(stmt, 20);
+      img->geoloc.latitude = sqlite3_column_double(stmt, 20);
     else
-      img->latitude = NAN;
+      img->geoloc.latitude = NAN;
     if(sqlite3_column_type(stmt, 21) == SQLITE_FLOAT)
-      img->elevation = sqlite3_column_double(stmt, 21);
+      img->geoloc.elevation = sqlite3_column_double(stmt, 21);
     else
-      img->elevation = NAN;
+      img->geoloc.elevation = NAN;
     const void *color_matrix = sqlite3_column_blob(stmt, 22);
     if(color_matrix)
       memcpy(img->d65_color_matrix, color_matrix, sizeof(img->d65_color_matrix));
@@ -242,9 +242,9 @@ void dt_image_cache_write_release(dt_image_cache_t *cache, dt_image_t *img, dt_i
   DT_DEBUG_SQLITE3_BIND_INT(stmt, 15, img->orientation);
   DT_DEBUG_SQLITE3_BIND_INT(stmt, 16, *(uint32_t *)(&img->legacy_flip));
   DT_DEBUG_SQLITE3_BIND_INT(stmt, 17, img->group_id);
-  DT_DEBUG_SQLITE3_BIND_DOUBLE(stmt, 18, img->longitude);
-  DT_DEBUG_SQLITE3_BIND_DOUBLE(stmt, 19, img->latitude);
-  DT_DEBUG_SQLITE3_BIND_DOUBLE(stmt, 20, img->elevation);
+  DT_DEBUG_SQLITE3_BIND_DOUBLE(stmt, 18, img->geoloc.longitude);
+  DT_DEBUG_SQLITE3_BIND_DOUBLE(stmt, 19, img->geoloc.latitude);
+  DT_DEBUG_SQLITE3_BIND_DOUBLE(stmt, 20, img->geoloc.elevation);
   DT_DEBUG_SQLITE3_BIND_BLOB(stmt, 21, &img->d65_color_matrix, sizeof(img->d65_color_matrix), SQLITE_STATIC);
   DT_DEBUG_SQLITE3_BIND_INT(stmt, 22, img->colorspace);
   DT_DEBUG_SQLITE3_BIND_INT(stmt, 23, img->raw_black_level);

src/common/variables.c
@@ -117,9 +117,9 @@ static void init_expansion(dt_variables_params_t *params, gboolean iterate)
     params->data->exif_focal_length = img->exif_focal_length;
     if(!isnan(img->exif_focus_distance) && fpclassify(img->exif_focus_distance) != FP_ZERO)
       params->data->exif_focus_distance = img->exif_focus_distance;
-    if(!isnan(img->longitude)) params->data->longitude = img->longitude;
-    if(!isnan(img->latitude)) params->data->latitude = img->latitude;
-    if(!isnan(img->elevation)) params->data->elevation = img->elevation;
+    if(!isnan(img->geoloc.longitude)) params->data->longitude = img->geoloc.longitude;
+    if(!isnan(img->geoloc.latitude)) params->data->latitude = img->geoloc.latitude;
+    if(!isnan(img->geoloc.elevation)) params->data->elevation = img->geoloc.elevation;
 
     dt_image_cache_read_release(darktable.image_cache, img);
   }

commit 38006e5f743a163d7725c9c52eacc3c2ccf102a1

New version of Lua struct binding with takes a specific name.
Use it for the geoloc struct in dt_image_t. Fix backward compatibility
issue introduced in 8bb6f8f.

Fixes #2218.

src/external/LuaAutoC/lautoc.h
@@ -120,6 +120,7 @@ void luaA_to_void_ptr(lua_State* L, luaA_Type,  void* c_out, int index);
 
 #define luaA_struct(L, type) luaA_struct_type(L, luaA_type(L, type))
 #define luaA_struct_member(L, type, member, member_type) luaA_struct_member_type(L, luaA_type(L, type), #member, luaA_type(L, member_type), offsetof(type, member))
+#define luaA_struct_member_name(L, type, member, member_type, member_name) luaA_struct_member_type(L, luaA_type(L, type), #member_name, luaA_type(L, member_type), offsetof(type, member))
 
 #define luaA_struct_push(L, type, c_in) luaA_struct_push_type(L, luaA_type(L, type), c_in)
 #define luaA_struct_push_member(L, type, member, c_in) luaA_struct_push_member_offset_type(L, luaA_type(L, type), offsetof(type, member), c_in)

src/lua/image.c
@@ -514,9 +514,9 @@ int dt_lua_init_image(lua_State *L)
   luaA_struct_member(L, dt_image_t, filename, const char_filename_length);
   luaA_struct_member(L, dt_image_t, width, const int32_t);
   luaA_struct_member(L, dt_image_t, height, const int32_t);
-  luaA_struct_member(L, dt_image_t, geoloc.longitude, protected_double); // set to NAN if value is not set
-  luaA_struct_member(L, dt_image_t, geoloc.latitude, protected_double); // set to NAN if value is not set
-  luaA_struct_member(L, dt_image_t, geoloc.elevation, protected_double); // set to NAN if value is not set
+  luaA_struct_member_name(L, dt_image_t, geoloc.longitude, protected_double, longitude); // set to NAN if value is not set
+  luaA_struct_member_name(L, dt_image_t, geoloc.latitude, protected_double, latitude); // set to NAN if value is not set
+  luaA_struct_member_name(L, dt_image_t, geoloc.elevation, protected_double, elevation); // set to NAN if value is not set
 
   dt_lua_init_int_type(L, dt_lua_image_t);
 