b54ccb7c886466b2edfc0ea97b246528ff9219a9
Pascal Obry <pascal@obry.net>
2019-04-10 18:43:40 +0200

    selection: ensure that last_single_id is set when appropriate.
    This is done when initializing the selection struct to restore previous
    state when starting darktable. We also keep the last_single_id when
    selecting a range.
    
    Fixes #2368.

--- src/common/selection.c
+++ src/common/selection.c
@@ -59,6 +59,15 @@ const dt_selection_t *dt_selection_new()
   /* initialize the collection copy */
   _selection_update_collection(NULL, (gpointer)s);
 
+  /* initialize last_single_id based on current database */
+  s->last_single_id = -1;
+
+  if(dt_collection_get_selected_count(darktable.collection) >= 1)
+  {
+    GList *selected_image = dt_collection_get_selected(darktable.collection, 1);
+    s->last_single_id = GPOINTER_TO_INT(selected_image->data);
+    g_list_free(selected_image);
+  }
 
   /* setup signal handler for darktable collection update
    to update the internal collection of the selection */
@@ -293,8 +302,6 @@ void dt_selection_select_range(dt_selection_t *selection, uint32_t imgid)
   dt_selection_select(selection, selection->last_single_id);
   dt_selection_select(selection, imgid);
 
-  selection->last_single_id = -1;
-  
   g_free(fullq);
 }
 
