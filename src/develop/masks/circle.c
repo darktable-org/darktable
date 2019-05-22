3dc9c1cc566de9f3ff2c5f4f17ba0ea28ad89b5f
Andreas Schneider <asn@cryptomilk.org>
2019-05-22 22:03:46 +0200

    develop:masks: Fix building circle.c with OpenMP and GCC9
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>

--- src/develop/masks/circle.c
+++ src/develop/masks/circle.c
@@ -941,7 +941,9 @@ static int dt_circle_get_mask_roi(dt_iop_module_t *module, dt_dev_pixelpipe_iop_
 
 #ifdef _OPENMP
 #if !defined(__SUNOS__) && !defined(__NetBSD__)
-#pragma omp parallel for default(none) shared(points)
+#pragma omp parallel for default(none) \
+  dt_omp_firstprivate(iscale, mh, mw, px, py) \
+  shared(points)
 #else
 #pragma omp parallel for shared(points)
 #endif
@@ -980,7 +982,9 @@ static int dt_circle_get_mask_roi(dt_iop_module_t *module, dt_dev_pixelpipe_iop_
 
 #ifdef _OPENMP
 #if !defined(__SUNOS__) && !defined(__NetBSD__)
-#pragma omp parallel for default(none) shared(points)
+#pragma omp parallel for default(none) \
+  dt_omp_firstprivate(center, mh, mw, radius2, total2) \
+  shared(points)
 #else
 #pragma omp parallel for shared(points)
 #endif
@@ -1007,7 +1011,9 @@ static int dt_circle_get_mask_roi(dt_iop_module_t *module, dt_dev_pixelpipe_iop_
 // we fill the output buffer by interpolation
 #ifdef _OPENMP
 #if !defined(__SUNOS__) && !defined(__NetBSD__)
-#pragma omp parallel for default(none) shared(points, buffer)
+#pragma omp parallel for default(none) \
+  dt_omp_firstprivate(h, mw, w) \
+  shared(points, buffer)
 #else
 #pragma omp parallel for shared(points, buffer)
 #endif
