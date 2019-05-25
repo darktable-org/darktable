3b031f087f4aa235103f13a78b3e554f3ea34559
Andreas Schneider <asn@cryptomilk.org>
2019-05-25 20:48:22 +0200

    iop: Fix building hazeremoval.c with OpenMP and GCC9
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>

--- src/iop/hazeremoval.c
+++ src/iop/hazeremoval.c
@@ -354,7 +354,9 @@ static void box_max(const gray_image img1, const gray_image img2, const int w)
   if(img1.data == img2.data)
   {
 #ifdef _OPENMP
-#pragma omp parallel default(none) private(img2_bak)
+#pragma omp parallel default(none) \
+    dt_omp_firstprivate(img1, img2, w) \
+    private(img2_bak)
 #endif
     {
       img2_bak = new_gray_image(img2.width, 1);
@@ -372,7 +374,9 @@ static void box_max(const gray_image img1, const gray_image img2, const int w)
   else
   {
 #ifdef _OPENMP
-#pragma omp parallel default(none) private(img2_bak)
+#pragma omp parallel default(none) \
+    dt_omp_firstprivate(img1, img2, w) \
+    private(img2_bak)
 #endif
     {
 #ifdef _OPENMP
@@ -383,7 +387,9 @@ static void box_max(const gray_image img1, const gray_image img2, const int w)
     }
   }
 #ifdef _OPENMP
-#pragma omp parallel default(none) private(img2_bak)
+#pragma omp parallel default(none) \
+  dt_omp_firstprivate(img1, img2, w) \
+  private(img2_bak)
 #endif
   {
     img2_bak = new_gray_image(1, img2.height);
@@ -427,7 +433,9 @@ static void box_min(const gray_image img1, const gray_image img2, const int w)
   if(img1.data == img2.data)
   {
 #ifdef _OPENMP
-#pragma omp parallel default(none) private(img2_bak)
+#pragma omp parallel default(none) \
+    dt_omp_firstprivate(img1, img2, w) \
+    private(img2_bak)
 #endif
     {
       img2_bak = new_gray_image(img2.width, 1);
@@ -445,7 +453,9 @@ static void box_min(const gray_image img1, const gray_image img2, const int w)
   else
   {
 #ifdef _OPENMP
-#pragma omp parallel default(none) private(img2_bak)
+#pragma omp parallel default(none) \
+    dt_omp_firstprivate(img1, img2, w) \
+    private(img2_bak)
 #endif
     {
 #ifdef _OPENMP
@@ -456,7 +466,9 @@ static void box_min(const gray_image img1, const gray_image img2, const int w)
     }
   }
 #ifdef _OPENMP
-#pragma omp parallel default(none) private(img2_bak)
+#pragma omp parallel default(none) \
+  dt_omp_firstprivate(img1, img2, w) \
+  private(img2_bak)
 #endif
   {
     img2_bak = new_gray_image(1, img2.height);
@@ -478,7 +490,9 @@ static void dark_channel(const const_rgb_image img1, const gray_image img2, cons
 {
   const size_t size = (size_t)img1.height * img1.width;
 #ifdef _OPENMP
-#pragma omp parallel for default(none) schedule(static)
+#pragma omp parallel for default(none) \
+  dt_omp_firstprivate(img1, img2, size) \
+  schedule(static)
 #endif
   for(size_t i = 0; i < size; i++)
   {
@@ -498,7 +512,9 @@ static void transition_map(const const_rgb_image img1, const gray_image img2, co
 {
   const size_t size = (size_t)img1.height * img1.width;
 #ifdef _OPENMP
-#pragma omp parallel for default(none) schedule(static)
+#pragma omp parallel for default(none) \
+  dt_omp_firstprivate(A0, img1, img2, size, strength) \
+  schedule(static)
 #endif
   for(size_t i = 0; i < size; i++)
   {
@@ -605,7 +621,10 @@ static float ambient_light(const const_rgb_image img, int w1, rgb_pixel *pA0)
   size_t N_bright_hazy = 0;
   const float *const data = dark_ch.data;
 #ifdef _OPENMP
-#pragma omp parallel for default(none) schedule(static) reduction(+ : N_bright_hazy, A0_r, A0_g, A0_b)
+#pragma omp parallel for default(none) \
+  dt_omp_firstprivate(crit_brightness, crit_haze_level, data, img, size) \
+  schedule(static) \
+  reduction(+ : N_bright_hazy, A0_r, A0_g, A0_b)
 #endif
   for(size_t i = 0; i < size; i++)
   {
@@ -726,7 +745,9 @@ void process(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, const 
   const float *const c_A0 = A0;
   const gray_image c_trans_map_filtered = trans_map_filtered;
 #ifdef _OPENMP
-#pragma omp parallel for default(none) schedule(static)
+#pragma omp parallel for default(none) \
+  dt_omp_firstprivate(c_A0, c_trans_map_filtered, img_in, img_out, size, t_min) \
+  schedule(static)
 #endif
   for(size_t i = 0; i < size; i++)
   {
