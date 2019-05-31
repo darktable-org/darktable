32e671e36a6e996c6e94a196cf7e85e3e427f149
Andreas Schneider <asn@cryptomilk.org>
2019-05-31 13:23:49 +0200

    chart: Fix building main.c with OpenMP and GCC9
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>

--- src/chart/main.c
+++ src/chart/main.c
@@ -1426,10 +1426,13 @@ static void get_xyz_sample_from_image(const image_t *const image, float shrink, 
   double sample_x = 0.0, sample_y = 0.0, sample_z = 0.0;
   size_t n_samples = 0;
 #ifdef _OPENMP
-#pragma omp parallel for schedule(static) default(none)                                                           \
-    shared(corners, x_start, y_start, x_end, y_end, delta_x_top, delta_y_top, delta_x_bottom, delta_y_bottom,     \
-           delta_x_left, delta_y_left, delta_x_right,                                                             \
-           delta_y_right) reduction(+ : n_samples, sample_x, sample_y, sample_z)
+#pragma omp parallel for default(none) \
+  dt_omp_firstprivate(image) \
+  shared(corners, x_start, y_start, x_end, y_end, delta_x_top, delta_y_top, \
+         delta_x_bottom, delta_y_bottom, delta_x_left, delta_y_left, \
+         delta_x_right, delta_y_right) \
+  reduction(+ : n_samples, sample_x, sample_y, sample_z) \
+  schedule(static) 
 #endif
   for(int y = y_start; y < y_end; y++)
     for(int x = x_start; x < x_end; x++)
@@ -1544,7 +1547,10 @@ static void free_image(image_t *image)
 static void image_lab_to_xyz(float *image, const int width, const int height)
 {
 #ifdef _OPENMP
-#pragma omp parallel for schedule(static) default(none) shared(image)
+#pragma omp parallel for default(none) \
+  dt_omp_firstprivate(height, width) \
+  shared(image) \
+  schedule(static)
 #endif
   for(int y = 0; y < height; y++)
     for(int x = 0; x < width; x++)
