5d02654d43dc86e407321013c13e7e122ea089e2
Andreas Schneider <asn@cryptomilk.org>
2019-05-31 13:25:18 +0200

    chart: Fix building dtcairo.c with OpenMP and GCC9
    Signed-off-by: Andreas Schneider <asn@cryptomilk.org>

--- src/chart/dtcairo.c
+++ src/chart/dtcairo.c
@@ -187,7 +187,10 @@ cairo_surface_t *cairo_surface_create_from_xyz_data(const float *const image, co
   unsigned char *rgbbuf = (unsigned char *)malloc(sizeof(unsigned char) * height * width * 4);
 
 #ifdef _OPENMP
-#pragma omp parallel for schedule(static) default(none) shared(rgbbuf)
+#pragma omp parallel for default(none) \
+  dt_omp_firstprivate(height, image, width) \
+  shared(rgbbuf) \
+  schedule(static)
 #endif
   for(int y = 0; y < height; y++)
   {
