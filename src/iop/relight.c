3779a939031325f847d377dfef581a1011170bb2
Roman Lebedev <lebedev.ri@gmail.com>
2019-06-06 20:07:16 +0300

    relight iop: fix build with clang (trunk)

--- src/iop/relight.c
+++ src/iop/relight.c
@@ -134,10 +134,8 @@ void process(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, const 
   const float c = (data->width / 10.0) / 2.0; // Width
 
 #ifdef _OPENMP
-#pragma omp parallel for default(none) \
-  dt_omp_firstprivate(b, c, ch, ivoid, ovoid, roi_out) \
-  shared(data) \
-  schedule(static)
+#pragma omp parallel for default(none) dt_omp_firstprivate(a, b, c, ch, ivoid, ovoid, roi_out) shared(data)       \
+    schedule(static)
 #endif
   for(int k = 0; k < roi_out->height; k++)
   {
