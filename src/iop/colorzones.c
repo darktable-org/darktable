9815a29e986cff39a96173e8e22402954fa2317a
Roman Lebedev <lebedev.ri@gmail.com>
2019-06-06 20:07:01 +0300

    colorzones iop: fix build with clang (trunk)

--- src/iop/colorzones.c
+++ src/iop/colorzones.c
@@ -390,9 +390,8 @@ void process(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, const 
     memcpy(ovoid, ivoid, roi_out->width * roi_out->height * ch * sizeof(float));
 
 #ifdef _OPENMP
-#pragma omp parallel for default(none) schedule(static) \
-  dt_omp_firstprivate(ch, ivoid, ovoid, roi_out, display_channel) \
-  shared(d)
+#pragma omp parallel for default(none) schedule(static)                                                           \
+    dt_omp_firstprivate(normalize_C, ch, ivoid, ovoid, roi_out, display_channel) shared(d)
 #endif
     for(size_t k = 0; k < (size_t)roi_out->width * roi_out->height; k++)
     {
@@ -436,10 +435,8 @@ void process(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, const 
   }
 
 #ifdef _OPENMP
-#pragma omp parallel for default(none) \
-  dt_omp_firstprivate(ch, ivoid, ovoid, roi_out) \
-  shared(d) \
-  schedule(static)
+#pragma omp parallel for default(none) dt_omp_firstprivate(normalize_C, ch, ivoid, ovoid, roi_out) shared(d)      \
+    schedule(static)
 #endif
   for(size_t k = 0; k < (size_t)roi_out->width * roi_out->height; k++)
   {
