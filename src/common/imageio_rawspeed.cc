96402739cecac5cf9d3ba04f11d74c17923e6c7d
hanno@schwalm-bremen.de <hanno@schwalm-bremen.de>
2019-07-28 15:01:26 +0200

    Adding support for DNGs DefaultUserCrop
    Second attempt:
    a) not using rawspped
    b) no bloating of the database
    c) neglectable performance penalty
    d) using a bit in img->flags for performance and possibly database search

--- src/common/imageio_rawspeed.cc
+++ src/common/imageio_rawspeed.cc
@@ -226,6 +226,10 @@ dt_imageio_retval_t dt_imageio_open_rawspeed(dt_image_t *img, const char *filena
     // Grab the WB
     for(int i = 0; i < 4; i++) img->wb_coeffs[i] = r->metadata.wbCoeffs[i];
 
+    // Get DefaultUserCrop
+    if (img->flags & DT_IMAGE_HAS_USERCROP)
+      dt_img_check_usercrop(img, filename);
+
     img->buf_dsc.filters = 0u;
     if(!r->isCFA)
     {
