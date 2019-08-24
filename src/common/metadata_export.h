5ede501c063cb5617deec885d3118d4945c0dbe6
phweyland <philippe.weyland@libertysurf.fr>
2019-08-24 14:30:04 -0300

    export metadata start

---
+++ src/common/metadata_export.h
@@ -0,0 +1,38 @@
+/*
+    This file is part of darktable,
+    copyright (c) 2019 philippe weyland.
+
+    darktable is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    darktable is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with darktable.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+typedef enum dt_metadata_id
+{
+  DT_META_NONE = 0,
+  DT_META_PRIVATE_TAG = 1 << 0,
+  DT_META_SYNONYMS_TAG = 1 << 1,
+  DT_META_HIERARCHICAL_TAG = 1 << 2,
+  DT_META_GEOTAG = 1 << 3,
+} dt_metadata_id;
+
+/** open dialog to configure metadata exportation */
+// char *dt_lib_export_metadata_configuration_dialog(const char *name);
+/** get the list of export metadata presets */
+GList *dt_lib_export_metadata_get_presets_list();
+// only difference with dt_lib_presets_add is writeprotect = 0
+void dt_lib_export_metadata_presets_add(const char *name, const char *plugin_name, const int32_t version, const void *params,
+                        const int32_t params_size);
+
+// modelines: These editor modelines have been set for all relevant files by tools/update_modelines.sh
+// vim: shiftwidth=2 expandtab tabstop=2 cindent
+// kate: tab-indents: off; indent-width 2; replace-tabs on; indent-mode cstyle; remove-trailing-spaces modified;
