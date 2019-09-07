56f37bab4e3ee826d0a7aa048ef32ee25d6a8af4
phweyland <philippe.weyland@libertysurf.fr>
2019-09-06 23:25:35 -0300

    integrate metadata preset into export preset

--- src/common/metadata_export.c
+++ src/common/metadata_export.c
@@ -18,18 +18,6 @@
 
 #include "common/darktable.h"
 #include "common/debug.h"
-#include "common/tags.h"
-#include "control/conf.h"
-#include "control/control.h"
-#include "dtgtk/button.h"
-#include "gui/gtk.h"
-#include "libs/lib.h"
-#include "libs/lib_api.h"
-#ifdef GDK_WINDOWING_QUARTZ
-#include "osx/osx.h"
-#endif
-#include <gdk/gdkkeysyms.h>
-#include <math.h>
 
 const char *dt_export_xmp_keys[]
     = { "Xmp.dc.creator", "Xmp.dc.publisher", "Xmp.dc.title", "Xmp.dc.description", "Xmp.dc.rights",
@@ -52,7 +40,7 @@ const char *dt_export_xmp_keys[]
         "Exif.Photo.WhiteBalance", "Exif.Photo.UserComment", "Exif.Photo.ColorSpace",
 
         "Xmp.xmp.CreateDate", "Xmp.xmp.CreatorTool", "Xmp.xmp.Identifier", "Xmp.xmp.Label", "Xmp.xmp.ModifyDate",
-        "Xmp.xmp.Nickname","Xmp.xmp.Rating",
+        "Xmp.xmp.Nickname", "Xmp.xmp.Rating",
 
         "Iptc.Application2.Subject", "Iptc.Application2.Keywords", "Iptc.Application2.LocationName",
         "Iptc.Application2.City", "Iptc.Application2.SubLocation", "Iptc.Application2.ProvinceState",
@@ -69,106 +57,6 @@ const char **dt_lib_export_metadata_get_export_keys(guint *dt_export_keys_n)
 {
   *dt_export_keys_n = dt_export_xmp_keys_n;
   return dt_export_xmp_keys;
-}
-
-GList *dt_lib_export_metadata_get_presets(const char *name, int32_t *flags)
-{
-  sqlite3_stmt *stmt;
-  *flags = 0;
-  GList *list = NULL;
-
-  DT_DEBUG_SQLITE3_PREPARE_V2(dt_database_get(darktable.db),
-    "SELECT op_params "
-    "FROM data.presets "
-    "WHERE operation='export_metadata' AND name=?1",
-     -1, &stmt, NULL);
-  DT_DEBUG_SQLITE3_BIND_TEXT(stmt, 1, name, -1, SQLITE_TRANSIENT);
-  if (sqlite3_step(stmt) == SQLITE_ROW)
-  {
-    char *params = (void *)sqlite3_column_blob(stmt, 0);
-    const int32_t params_size = sqlite3_column_bytes(stmt, 0);
-    if (params)
-    {
-      char *params_end = params + params_size;
-      *flags = *(const int *)params;
-      params += sizeof(int32_t);
-      while (params < params_end)
-      {
-        const char *tagname = params;
-        params += strlen(tagname) + 1;
-        const char *formula = params;
-        params += strlen(formula) + 1;
-        const int size = strlen(tagname) + strlen(formula) + 2;
-        char *tags = g_malloc(size);
-        memcpy(tags, tagname, size);
-        list = g_list_append(list, tags);
-      }
-    }
-  }
-  sqlite3_finalize(stmt);
-  return list;
-}
-
-void dt_lib_export_metadata_delete_presets(const char *name)
-{
-  sqlite3_stmt *stmt;
-  DT_DEBUG_SQLITE3_PREPARE_V2(dt_database_get(darktable.db),
-      "DELETE FROM data.presets "
-      "WHERE operation='export_metadata' AND name=?1",
-       -1, &stmt, NULL);
-  DT_DEBUG_SQLITE3_BIND_TEXT(stmt, 1, name, -1, SQLITE_TRANSIENT);
-  sqlite3_step(stmt);
-  sqlite3_finalize(stmt);
-}
-
-// only difference with dt_lib_presets_add is writeprotect = 0
-void dt_lib_export_metadata_presets_add(const char *name, const char *plugin_name, const int32_t version, const void *params,
-                        const int32_t params_size)
-{
-  sqlite3_stmt *stmt;
-  DT_DEBUG_SQLITE3_PREPARE_V2(dt_database_get(darktable.db),
-                              "DELETE FROM data.presets WHERE name=?1 AND operation=?2 AND op_version=?3", -1,
-                              &stmt, NULL);
-  DT_DEBUG_SQLITE3_BIND_TEXT(stmt, 1, name, -1, SQLITE_TRANSIENT);
-  DT_DEBUG_SQLITE3_BIND_TEXT(stmt, 2, plugin_name, -1, SQLITE_TRANSIENT);
-  DT_DEBUG_SQLITE3_BIND_INT(stmt, 3, version);
-  sqlite3_step(stmt);
-  sqlite3_finalize(stmt);
-  DT_DEBUG_SQLITE3_PREPARE_V2(
-      dt_database_get(darktable.db),
-      "INSERT INTO data.presets (name, description, operation, op_version, op_params, "
-      "blendop_params, blendop_version, enabled, model, maker, lens, "
-      "iso_min, iso_max, exposure_min, exposure_max, aperture_min, aperture_max, "
-      "focal_length_min, focal_length_max, writeprotect, "
-      "autoapply, filter, def, format) VALUES (?1, '', ?2, ?3, ?4, NULL, 0, 1, '%', "
-      "'%', '%', 0, 340282346638528859812000000000000000000, 0, 10000000, 0, 100000000, 0, 1000, 0, 0, 0, 0, 0)",
-      -1, &stmt, NULL);
-  DT_DEBUG_SQLITE3_BIND_TEXT(stmt, 1, name, -1, SQLITE_TRANSIENT);
-  DT_DEBUG_SQLITE3_BIND_TEXT(stmt, 2, plugin_name, -1, SQLITE_TRANSIENT);
-  DT_DEBUG_SQLITE3_BIND_INT(stmt, 3, version);
-  DT_DEBUG_SQLITE3_BIND_BLOB(stmt, 4, params, params_size, SQLITE_TRANSIENT);
-  sqlite3_step(stmt);
-  sqlite3_finalize(stmt);
-}
-
-GList *dt_lib_export_metadata_get_presets_list()
-{
-  GList *result = NULL;
-
-  sqlite3_stmt *stmt;
-  DT_DEBUG_SQLITE3_PREPARE_V2(dt_database_get(darktable.db),
-    "SELECT name "
-    "FROM data.presets "
-    "WHERE operation='export_metadata'",
-     -1, &stmt, NULL);
-  while(sqlite3_step(stmt) == SQLITE_ROW)
-  {
-    char *presets_name = g_strdup((char *)sqlite3_column_text(stmt, 0));
-    result = g_list_append(result, presets_name);
-  }
-  sqlite3_finalize(stmt);
-
-  return result;
 }
 
 // modelines: These editor modelines have been set for all relevant files by tools/update_modelines.sh
