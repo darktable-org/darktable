87083364495d0bb1271a11b326343f6127cb615c
Pascal Obry <pascal@obry.net>
2019-09-07 11:44:03 +0200

    dt_history_check_module_exists: new routine.

--- src/common/history.h
+++ src/common/history.h
@@ -70,6 +70,9 @@ GList *dt_history_get_items(int32_t imgid, gboolean enabled);
 /** get list of history items for image as a nice string */
 char *dt_history_get_items_as_string(int32_t imgid);
 
+/* check if a module exists in the history of corresponding image */
+gboolean dt_history_check_module_exists(int32_t imgid, const char *operation);
+
 // modelines: These editor modelines have been set for all relevant files by tools/update_modelines.sh
 // vim: shiftwidth=2 expandtab tabstop=2 cindent
 // kate: tab-indents: off; indent-width 2; replace-tabs on; indent-mode cstyle; remove-trailing-spaces modified;
