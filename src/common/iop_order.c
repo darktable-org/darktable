04bf5076982eaecda356f41969d3fa1b7268f3c9
Pascal Obry <pascal@obry.net>
2019-09-22 17:08:11 +0200

    toneequal: must be after distortion.
    This is checked into the module and now honored here.

--- src/common/iop_order.c
+++ src/common/iop_order.c
@@ -63,7 +63,7 @@ static int _ioppr_legacy_iop_order_step(GList **_iop_order_list, GList *history_
 
     // correct exposure in camera RGB space (otherwise, it's not really exposure)
     _ioppr_move_iop_before(_iop_order_list, "exposure", "colorin", dont_move);
-     _ioppr_insert_iop_after(_iop_order_list, history_list, "toneequal", "exposure", dont_move);
+    _ioppr_insert_iop_after(_iop_order_list, history_list, "toneequal", "clipping", dont_move);
 
     // move local distorsions/pixel shifts after general distorsions
     _ioppr_move_iop_before(_iop_order_list, "retouch", "exposure", dont_move);
