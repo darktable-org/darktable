7e32c63cc24da2929eaf68a3dc369cfc2ab4860c
AlicVB <dev@lnaa.fr>
2019-10-01 18:43:30 +0200

    culling : fine tune selection frame - hide frame in culling for darktable-elegant-* - always show frame in culling filmstrip

--- src/gui/gtk.c
+++ src/gui/gtk.c
@@ -2253,7 +2253,9 @@ void dt_gui_load_theme(const char *theme)
     [DT_GUI_COLOR_THUMBNAIL_BORDER] = { "thumbnail_border_color", { 0.1, 0.1, 0.1, 1.0 } },
     [DT_GUI_COLOR_THUMBNAIL_SELECTED_BORDER] = { "thumbnail_selected_border_color", { 0.9, 0.9, 0.9, 1.0 } },
     [DT_GUI_COLOR_FILMSTRIP_BG] = { "filmstrip_bg_color", { 0.2, 0.2, 0.2, 1.0 } },
-    [DT_GUI_COLOR_PREVIEW_BORDER] = { "preview_border_color", { 0.1, 0.1, 0.1, 1.0 } },
+    [DT_GUI_COLOR_CULLING_SELECTED_BORDER] = { "culling_selected_border_color", { 0.1, 0.1, 0.1, 1.0 } },
+    [DT_GUI_COLOR_CULLING_FILMSTRIP_SELECTED_BORDER]
+    = { "culling_filmstrip_selected_border_color", { 0.1, 0.1, 0.1, 1.0 } },
     [DT_GUI_COLOR_PREVIEW_HOVER_BORDER] = { "preview_hover_border_color", { 0.9, 0.9, 0.9, 1.0 } },
   };
 
