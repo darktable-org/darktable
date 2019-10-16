81df61fa21009baec6739e9a0a4a2dbbf3df8be4
Pascal Obry <pascal@obry.net>
2019-10-16 20:03:52 +0200

    imageop: refactor to have a more generic routine get_module_by_name()

--- src/develop/imageop.c
+++ src/develop/imageop.c
@@ -2046,11 +2046,16 @@ void dt_iop_nap(int32_t usec)
 
 dt_iop_module_t *get_colorout_module(void)
 {
+  return get_module_by_name("colorout");
+}
+
+dt_iop_module_t *get_module_by_name(const char *op)
+{
   GList *modules = darktable.develop->iop;
   while(modules)
   {
     dt_iop_module_t *module = (dt_iop_module_t *)modules->data;
-    if(!strcmp(module->op, "colorout")) return module;
+    if(!strcmp(module->op, op)) return module;
     modules = g_list_next(modules);
   }
   return NULL;
