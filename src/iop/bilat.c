darktable-org/darktable

commit ab1109c012376d91d230201e05b9e6104ee2b739

bilat (local contrast): improve slightly the default params
Taking into account that the local contrast module is placed after filmic (global tonemapping), modify the default params to restore the local contrast that might have been lost in the tone mapping.

src/iop/bilat.c
@@ -394,7 +394,7 @@ void init(dt_iop_module_t *module)
   module->params_size = sizeof(dt_iop_bilat_params_t);
   module->gui_data = NULL;
   // init defaults:
-  dt_iop_bilat_params_t tmp = (dt_iop_bilat_params_t){ s_mode_local_laplacian, 1.0, 1.0, 0.2, 0.2 };
+  dt_iop_bilat_params_t tmp = (dt_iop_bilat_params_t){ s_mode_local_laplacian, 0.5, 0.5, 0.25, 0.5 };
 
   memcpy(module->params, &tmp, sizeof(dt_iop_bilat_params_t));
   memcpy(module->default_params, &tmp, sizeof(dt_iop_bilat_params_t));

commit da297721fcea3c47090c944f72ece1263a6f09c4

bilat: fix regression introduced by ab1109c01.
We also want to ensure that when changing mode the new default are
choosen to init the module.

src/iop/bilat.c
@@ -460,8 +460,8 @@ static void mode_callback(GtkWidget *w, dt_iop_module_t *self)
     gtk_widget_set_visible(g->midtone, TRUE);
     gtk_widget_set_visible(g->range, FALSE);
     gtk_widget_set_visible(g->spatial, FALSE);
-    dt_bauhaus_slider_set(g->highlights, 100.0f);
-    dt_bauhaus_slider_set(g->shadows, 100.0f);
+    dt_bauhaus_slider_set(g->highlights, 50.0f);
+    dt_bauhaus_slider_set(g->shadows, 50.0f);
   }
   else
   {