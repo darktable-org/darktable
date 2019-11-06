b73f7a3b50d414af3ec073e9b1bed0a157391c63
Marcus Rückert <darix@nordisch.org>
2019-11-06 02:14:17 +0100

    Fix warning about missing CL_TARGET_OPENCL_VERSION.
    As the default fallback seems to be 220 I use that as a default version
    
    ```
    In file included from /usr/include/CL/cl.h:32,
                     from /home/abuild/rpmbuild/BUILD/darktable-3.0.0rc0~git23.cb0aaf0c1/src/common/dlopencl.h:27,
                     from /home/abuild/rpmbuild/BUILD/darktable-3.0.0rc0~git23.cb0aaf0c1/src/common/opencl.h:38,
                     from /home/abuild/rpmbuild/BUILD/darktable-3.0.0rc0~git23.cb0aaf0c1/src/develop/imageop.h:37,
                     from /home/abuild/rpmbuild/BUILD/darktable-3.0.0rc0~git23.cb0aaf0c1/src/develop/develop.h:30,
                     from /home/abuild/rpmbuild/BUILD/darktable-3.0.0rc0~git23.cb0aaf0c1/src/control/jobs/develop_jobs.h:22,
                     from /home/abuild/rpmbuild/BUILD/darktable-3.0.0rc0~git23.cb0aaf0c1/src/control/jobs.h:99,
                     from /home/abuild/rpmbuild/BUILD/darktable-3.0.0rc0~git23.cb0aaf0c1/src/control/control.h:32,
                     from /home/abuild/rpmbuild/BUILD/darktable-3.0.0rc0~git23.cb0aaf0c1/src/bauhaus/bauhaus.h:23,
                     from /home/abuild/rpmbuild/BUILD/darktable-3.0.0rc0~git23.cb0aaf0c1/src/bauhaus/bauhaus.c:20:
    /usr/include/CL/cl_version.h:34:9: note: #pragma message: cl_version.h: CL_TARGET_OPENCL_VERSION is not defined. Defaulting to 220 (OpenCL 2.2)
       34 | #pragma message("cl_version.h: CL_TARGET_OPENCL_VERSION is not defined. Defaulting to 220 (OpenCL 2.2)")
          |         ^~~~~~~
    In file included from /usr/include/CL/cl.h:32,
                     from /home/abuild/rpmbuild/BUILD/darktable-3.0.0rc0~git23.cb0aaf0c1/src/common/dlopencl.h:27,
                     from /home/abuild/rpmbuild/BUILD/darktable-3.0.0rc0~git23.cb0aaf0c1/src/common/opencl.h:38,
                     from /home/abuild/rpmbuild/BUILD/darktable-3.0.0rc0~git23.cb0aaf0c1/src/develop/imageop.h:37,
                     from /home/abuild/rpmbuild/BUILD/darktable-3.0.0rc0~git23.cb0aaf0c1/src/common/color_picker.c:23:
    /usr/include/CL/cl_version.h:34:9: note: #pragma message: cl_version.h: CL_TARGET_OPENCL_VERSION is not defined. Defaulting to 220 (OpenCL 2.2)
       34 | #pragma message("cl_version.h: CL_TARGET_OPENCL_VERSION is not defined. Defaulting to 220 (OpenCL 2.2)")
          |         ^~~~~~~
    ```

--- src/config.cmake.h
+++ src/config.cmake.h
@@ -68,6 +68,9 @@ static const char *dt_supported_extensions[] __attribute__((unused)) = {"@DT_SUP
 
 #cmakedefine HAVE_OMP_FIRSTPRIVATE_WITH_CONST 1
 
+// the default fallback is going for version 220 anyway.
+#define CL_TARGET_OPENCL_VERSION 220
+
 // modelines: These editor modelines have been set for all relevant files by tools/update_modelines.sh
 // vim: shiftwidth=2 expandtab tabstop=2 cindent
 // kate: tab-indents: off; indent-width 2; replace-tabs on; indent-mode cstyle; remove-trailing-spaces modified;
