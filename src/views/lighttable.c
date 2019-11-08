180adb87bcce3769e520061f2ba0ae3a9c682ef3
AlicVB <dev@lnaa.fr>
2019-11-08 20:20:39 +0100

    lighttable : avoid position 'jump' on rating/colorlabels
    using shortcuts.
    This fix #3090

--- src/views/lighttable.c
+++ src/views/lighttable.c
@@ -535,13 +535,22 @@ static void _view_lighttable_collection_listener_internal(dt_view_t *self, dt_li
 
   _update_collected_images(self);
 
-  // we ensure selection visibility if any
-  GList *first_selected = dt_collection_get_selected(darktable.collection, 1);
-  // we have at least 1 selected image
-  if(first_selected)
+  // we ensure selection/image-under-mouse visibility if any
+  // because we may have added/removed a lot of image before this one
+  int cur_id = dt_control_get_mouse_over_id();
+  if(cur_id <= 0)
   {
-    gchar *query = dt_util_dstrcat(NULL, "SELECT rowid FROM memory.collected_images WHERE imgid = %d",
-                                   GPOINTER_TO_INT(first_selected->data));
+    GList *first_selected = dt_collection_get_selected(darktable.collection, 1);
+    if(first_selected)
+    {
+      cur_id = GPOINTER_TO_INT(first_selected->data);
+      g_list_free(first_selected);
+    }
+  }
+  // we have at least 1 selected image
+  if(cur_id > 0)
+  {
+    gchar *query = dt_util_dstrcat(NULL, "SELECT rowid FROM memory.collected_images WHERE imgid = %d", cur_id);
     sqlite3_stmt *stmt;
     DT_DEBUG_SQLITE3_PREPARE_V2(dt_database_get(darktable.db), query, -1, &stmt, NULL);
     if(stmt != NULL && sqlite3_step(stmt) == SQLITE_ROW)
@@ -550,7 +559,6 @@ static void _view_lighttable_collection_listener_internal(dt_view_t *self, dt_li
     }
     if(stmt) sqlite3_finalize(stmt);
     g_free(query);
-    g_list_free(first_selected);
   }
 }
 
