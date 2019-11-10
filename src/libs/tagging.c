b110bb3736f6f26dee7e5469fb413c8ea4aa70df
Pascal Obry <pascal@obry.net>
2019-11-10 17:33:47 +0100

    Add a key to apply last tag to hovered picture.
    For #3366.

--- src/libs/tagging.c
+++ src/libs/tagging.c
@@ -39,6 +39,8 @@
 
 DT_MODULE(1)
 
+static gboolean _lib_tagging_tag_redo(GtkAccelGroup *accel_group, GObject *acceleratable, guint keyval,
+                                      GdkModifierType modifier, dt_lib_module_t *self);
 static gboolean _lib_tagging_tag_show(GtkAccelGroup *accel_group, GObject *acceleratable, guint keyval,
                                       GdkModifierType modifier, dt_lib_module_t *self);
 
@@ -59,6 +61,7 @@ typedef struct dt_lib_tagging_t
   gboolean tree_flag, suggestion_flag, sort_count_flag, hide_path_flag, dttags_flag;
   char *collection;
   GtkEntryCompletion *completion;
+  char *last_tag;
 } dt_lib_tagging_t;
 
 typedef struct dt_tag_op_t
@@ -122,6 +125,7 @@ void init_key_accels(dt_lib_module_t *self)
   dt_accel_register_lib(self, NC_("accel", "detach"), 0, 0);
   dt_accel_register_lib(self, NC_("accel", "new"), 0, 0);
   dt_accel_register_lib(self, NC_("accel", "tag"), GDK_KEY_t, GDK_CONTROL_MASK);
+  dt_accel_register_lib(self, NC_("accel", "redo last tag"), GDK_KEY_t, GDK_MOD1_MASK);
 }
 
 void connect_key_accels(dt_lib_module_t *self)
@@ -132,6 +136,7 @@ void connect_key_accels(dt_lib_module_t *self)
   dt_accel_connect_button_lib(self, "detach", d->detach_button);
   dt_accel_connect_button_lib(self, "new", d->new_button);
   dt_accel_connect_lib(self, "tag", g_cclosure_new(G_CALLBACK(_lib_tagging_tag_show), self, NULL));
+  dt_accel_connect_lib(self, "redo last tag", g_cclosure_new(G_CALLBACK(_lib_tagging_tag_redo), self, NULL));
 }
 
 static void propagate_sel_to_parents(GtkTreeModel *model, GtkTreeIter *iter)
@@ -878,6 +883,10 @@ static void attach_selected_tag(dt_lib_module_t *self, dt_lib_tagging_t *d)
     return;
   dt_tag_attach(tagid, imgsel);
 
+  /** record last tag used */
+  g_free(d->last_tag);
+  d->last_tag = g_strdup(dt_tag_get_name(tagid));
+
   init_treeview(self, 0);
   if (d->tree_flag || !d->suggestion_flag)
   {
@@ -990,6 +999,10 @@ static void pop_menu_attached_attach_to_all(GtkWidget *menuitem, dt_lib_module_t
   const int imgsel = dt_view_get_image_to_act_on();
   dt_tag_attach(tagid, imgsel);
 
+  /** record last tag used */
+  g_free(d->last_tag);
+  d->last_tag = g_strdup(dt_tag_get_name(tagid));
+
   init_treeview(self, 0);
 
   const uint32_t count = dt_tag_images_count(tagid);
@@ -1100,6 +1113,10 @@ static void new_button_clicked(GtkButton *button, dt_lib_module_t *self)
   /** attach tag to selected images  */
   dt_tag_attach_string_list(tag, -1);
   dt_image_synch_xmp(-1);
+
+  /** record last tag used */
+  g_free(d->last_tag);
+  d->last_tag = g_strdup(tag);
 
   /** clear input box */
   gtk_entry_set_text(d->entry, "");
@@ -2485,6 +2502,7 @@ void gui_init(dt_lib_module_t *self)
   dt_lib_tagging_t *d = (dt_lib_tagging_t *)malloc(sizeof(dt_lib_tagging_t));
   self->data = (void *)d;
   d->imgsel = -1;
+  d->last_tag = NULL;
 
   self->widget = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
   dt_gui_add_help_link(self->widget, dt_get_help_url(self->plugin_name));
@@ -2784,10 +2802,16 @@ static gboolean _lib_tagging_tag_key_press(GtkWidget *entry, GdkEventKey *event,
       // both these functions can deal with -1 for all selected images. no need for extra code in here!
       dt_tag_attach_string_list(tag, d->floating_tag_imgid);
       dt_image_synch_xmp(d->floating_tag_imgid);
+
+      /** record last tag used */
+      g_free(d->last_tag);
+      d->last_tag = g_strdup(tag);
+
       init_treeview(self, 0);
       init_treeview(self, 1);
       gtk_widget_destroy(d->floating_tag_window);
       dt_control_signal_raise(darktable.signals, DT_SIGNAL_TAG_CHANGED);
+
       return TRUE;
     }
   }
@@ -2798,6 +2822,24 @@ static gboolean _lib_tagging_tag_destroy(GtkWidget *widget, GdkEvent *event, gpo
 {
   gtk_widget_destroy(GTK_WIDGET(user_data));
   return FALSE;
+}
+
+static gboolean _lib_tagging_tag_redo(GtkAccelGroup *accel_group, GObject *acceleratable, guint keyval,
+                                      GdkModifierType modifier, dt_lib_module_t *self)
+{
+  dt_lib_tagging_t *d = (dt_lib_tagging_t *)self->data;
+
+  if(d->last_tag)
+  {
+    const int imgsel = dt_control_get_mouse_over_id();
+
+    dt_tag_attach_string_list(d->last_tag, imgsel);
+    dt_image_synch_xmp(imgsel);
+    init_treeview(self, 0);
+    init_treeview(self, 1);
+    dt_control_signal_raise(darktable.signals, DT_SIGNAL_TAG_CHANGED);
+  }
+  return TRUE;
 }
 
 static gboolean _lib_tagging_tag_show(GtkAccelGroup *accel_group, GObject *acceleratable, guint keyval,
