fb2e3be21234683ffd9738a69f90a7f79f856d29
Diederik ter Rahe <diederik@Terrathon>
2019-11-21 18:59:13 +0000

    Added accelerators for iop modules with sliders

--- src/iop/colorchecker.c
+++ src/iop/colorchecker.c
@@ -130,6 +130,25 @@ int default_colorspace(dt_iop_module_t *self, dt_dev_pixelpipe_t *pipe, dt_dev_p
   return iop_cs_Lab;
 }
 
+void init_key_accels(dt_iop_module_so_t *self)
+{
+  dt_accel_register_slider_iop(self, FALSE, NC_("accel", "lightness"));
+  dt_accel_register_slider_iop(self, FALSE, NC_("accel", "green-red"));
+  dt_accel_register_slider_iop(self, FALSE, NC_("accel", "blue-yellow"));
+  dt_accel_register_slider_iop(self, FALSE, NC_("accel", "saturation"));
+}
+
+void connect_key_accels(dt_iop_module_t *self)
+{
+  dt_iop_colorchecker_gui_data_t *g = (dt_iop_colorchecker_gui_data_t *)self->gui_data;
+
+  dt_accel_connect_slider_iop(self, "lightness", GTK_WIDGET(g->scale_L));
+  dt_accel_connect_slider_iop(self, "green-red", GTK_WIDGET(g->scale_a));
+  dt_accel_connect_slider_iop(self, "blue-yellow", GTK_WIDGET(g->scale_b));
+  dt_accel_connect_slider_iop(self, "saturation", GTK_WIDGET(g->scale_C));
+}
+
+
 int legacy_params(
     dt_iop_module_t  *self,
     const void *const old_params,
