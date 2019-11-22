ca7f2fff1db109cf1aafb38a988c26955a09c033
Ulrich Pegelow <ulrich.pegelow@tongareva.de>
2019-11-22 16:07:03 +0100

    blend gui: make channel view a bit less obscure
    press 'c' in a parametric mask slider to view that channel.
    press 'm' to see the mask.

--- src/develop/develop.h
+++ src/develop/develop.h
@@ -109,7 +109,8 @@ typedef enum dt_dev_pixelpipe_display_mask_t
   DT_DEV_PIXELPIPE_DISPLAY_HSL_H = 10 << 3,
   DT_DEV_PIXELPIPE_DISPLAY_HSL_S = 11 << 3,
   DT_DEV_PIXELPIPE_DISPLAY_HSL_l = 12 << 3,
-  DT_DEV_PIXELPIPE_DISPLAY_ANY = 0xff << 2
+  DT_DEV_PIXELPIPE_DISPLAY_ANY = 0xff << 2,
+  DT_DEV_PIXELPIPE_DISPLAY_STICKY = 1 << 16
 } dt_dev_pixelpipe_display_mask_t;
 
 extern const gchar *dt_dev_histogram_type_names[];
