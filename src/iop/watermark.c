530b127fe4f8c062167d1bb1933d19699b253614
Pascal Obry <pascal@obry.net>
2019-12-16 07:28:17 +0100

    Minor code clean-up.

--- src/iop/watermark.c
+++ src/iop/watermark.c
@@ -1298,9 +1298,8 @@ void cleanup_pipe(struct dt_iop_module_t *self, dt_dev_pixelpipe_t *pipe, dt_dev
 
 void gui_update(struct dt_iop_module_t *self)
 {
-  dt_iop_module_t *module = (dt_iop_module_t *)self;
   dt_iop_watermark_gui_data_t *g = (dt_iop_watermark_gui_data_t *)self->gui_data;
-  dt_iop_watermark_params_t *p = (dt_iop_watermark_params_t *)module->params;
+  dt_iop_watermark_params_t *p = (dt_iop_watermark_params_t *)self->params;
   dt_bauhaus_slider_set(g->opacity, p->opacity);
   dt_bauhaus_slider_set_soft(g->scale, p->scale);
   dt_bauhaus_slider_set(g->rotate, p->rotate);
