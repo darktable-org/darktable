cda040b6a995b9b31a4a66a3cbc97cc26038b071
Hubert Kowalski <h.kowalski@hakger.pl>
2020-02-23 14:38:24 +0100

    Make tools use same paths and options as main binary

--- tools/purge_non_existing_images.sh
+++ tools/purge_non_existing_images.sh
@@ -1,24 +1,70 @@
 #!/bin/bash
 
-DRYRUN=yes
-
-if [ "$1" = "-p" ]; then
-    DRYRUN=no
-fi
-
-DBFILE=$HOME/.config/darktable/library.db
-TMPFILE=$(mktemp -t tmp.XXXXXXXXXX)
-QUERY="select A.id,B.folder,A.filename from images as A join film_rolls as B on A.film_id = B.id"
+#
+# Usage: purge_non_existing_images [-p]
+#        -p  do the purge, otherwise only display non existing images
+#
 
 if ! which sqlite3 > /dev/null; then
-    echo error: please install sqlite3 binary.
+    echo "error: please install sqlite3 binary".
     exit 1
+fi
+
+configdir="$HOME/.config/darktable"
+DBFILE="$configdir/library.db"
+dryrun=1
+library=""
+
+# remember the command line to show it in the end when not purging
+commandline="$0 $*"
+
+# handle command line arguments
+while [ "$#" -ge 1 ] ; do
+  option="$1"
+  case ${option} in
+  -h|--help)
+    echo "Delete non existing images from darktable's database"
+    echo "Usage:   $0 [options]"
+    echo ""
+    echo "Options:"
+    echo "  -c|--configdir <path>    path to the darktable config directory"
+    echo "                           (default: '${configdir}')"
+    echo "  -l|--library <path>      path to the library.db"
+    echo "                           (default: '${DBFILE}')"
+    echo "  -p|--purge               actually delete the tags instead of just finding them"
+    exit 0
+    ;;
+  -l|--library)
+    library="$2"
+    shift
+    ;;
+  -c|--configdir)
+    configdir="$2"
+    shift
+    ;;
+  -p|--purge)
+    dryrun=0
+    ;;
+  *)
+    echo "warning: ignoring unknown option $option"
+    ;;
+  esac
+    shift
+done
+
+DBFILE="$configdir/library.db"
+
+if [ "$library" != "" ]; then
+    DBFILE="$library"
 fi
 
 if [ ! -f "$DBFILE" ]; then
-    echo error: library.db not found \($DBFILE\).
+    echo "error: library db '${DBFILE}' doesn't exist"
     exit 1
 fi
+
+TMPFILE=$(mktemp -t tmp.XXXXXXXXXX)
+QUERY="select A.id,B.folder,A.filename from images as A join film_rolls as B on A.film_id = B.id"
 
 sqlite3 $DBFILE "$QUERY" > "$TMPFILE"
 
@@ -33,7 +79,7 @@ do
   then
     echo "  $FD/$FL with ID = $ID"
 
-    if [ $DRYRUN = no ]; then
+    if [ $dryrun -eq 0 ]; then
         for table in images meta_data; do
             sqlite3 "$DBFILE" "delete from $table where id=$ID"
         done
@@ -47,7 +93,7 @@ done
 rm "$TMPFILE"
 
 
-if [ $DRYRUN = no ]; then
+if [ $dryrun -eq 0 ]; then
     # delete now-empty filmrolls
     sqlite3 "$DBFILE" "DELETE FROM film_rolls WHERE (SELECT COUNT(A.id) FROM images AS A WHERE A.film_id=film_rolls.id)=0"
     sqlite3 "$DBFILE" "VACUUM; ANALYZE"
@@ -55,10 +101,8 @@ else
     echo
     echo Remove following now-empty filmrolls:
     sqlite3 "$DBFILE" "SELECT folder FROM film_rolls WHERE (SELECT COUNT(A.id) FROM images AS A WHERE A.film_id=film_rolls.id)=0"
-fi
-
-if [ $DRYRUN = yes ]; then
+    
     echo
     echo to really remove non existing images from the database call:
-    echo "$0" -p
+    echo "${commandline} --purge"
 fi
