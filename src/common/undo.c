865a3a973d37d7585a07ebdbe1968652d80ca1e3
AlicVB <dev@lnaa.fr>
2020-02-29 16:37:20 +0100

    thumb : reload thumb after undo pasting

--- src/common/undo.c
+++ src/common/undo.c
@@ -16,12 +16,13 @@
     along with darktable.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "common/darktable.h"
-#include "common/collection.h"
-#include "common/image.h"
 #include "common/undo.h"
-#include <glib.h>    // for GList, gpointer, g_list_first, g_list_prepend
-#include <stdlib.h>  // for NULL, malloc, free
+#include "common/collection.h"
+#include "common/darktable.h"
+#include "common/image.h"
+#include "control/control.h"
+#include <glib.h>   // for GList, gpointer, g_list_first, g_list_prepend
+#include <stdlib.h> // for NULL, malloc, free
 #include <sys/time.h>
 
 const double MAX_TIME_PERIOD = 0.5; // in second
@@ -247,10 +248,9 @@ static void _undo_do_undo_redo(dt_undo_t *self, uint32_t filter, dt_undo_action_
     // udpate xmp for updated images
     for(GList *img = imgs; img != NULL; img = img->next)
       dt_image_synch_xmp(GPOINTER_TO_INT(img->data));
-    g_list_free(imgs);
   }
 
-  dt_collection_update_query(darktable.collection, DT_COLLECTION_CHANGE_RELOAD, NULL);
+  dt_collection_update_query(darktable.collection, DT_COLLECTION_CHANGE_RELOAD, imgs);
 }
 
 void dt_undo_do_redo(dt_undo_t *self, uint32_t filter)
