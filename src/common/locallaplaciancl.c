1dbdbf019ea9fdcc63bc2141254fa24041bbb385
Pascal Obry <pascal@obry.net>
2020-03-01 14:12:38 +0100

    Change copyright wording from project to developers.

--- src/common/locallaplaciancl.c
+++ src/common/locallaplaciancl.c
@@ -1,7 +1,7 @@
 #ifdef HAVE_OPENCL
 /*
     This file is part of darktable,
-    Copyright (C) 2016-2020 darktable project.
+    Copyright (C) 2016-2020 darktable developers.
 
     darktable is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
--- src/osx/osx.h
+++ src/osx/osx.h
@@ -1,6 +1,6 @@
 /*
  *   This file is part of darktable,
- *   Copyright (C) 2014-2020 darktable project.
+ *   Copyright (C) 2014-2020 darktable developers.
  * 
  *   darktable is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
