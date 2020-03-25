darktable-org/darktable

commit 190ed3116324f69e416d49103c149e0fba690607

Update wb_presets.c (Nikon Coolpix P1000) (#4335)
* Update wb_presets.c (Nikon Coolpix P1000)

Adding the whitebalance values for Nikon Coolpix P1000, firmware 1.2.

I extracted the values with the help of your Ruby script extract_wb on Windows 10 professional x64 as described on https://www.bilddateien.de/blog/2019-01-20-kamera-weissabgleichseinstellungen-fuer-darktable-ermitteln.html. 

When running the script, there was an error about something like "array not being an array" in line 164 (found_presets = Hash[found_presets.sort.zip].keys), so I commented this line and manually did the sorting and removal of duplicates in Notepad++. After that, there were some occurrences of value-duplicates (in the curly braces) with different WB-tags assigned, which I also removed from the list. If you need the original (unsorted) list, please feel free to send me an e-mail :-)

I'm wondering, why the whitebalancing shifting-values (e.g. -3...0...+3) which I applied where possible, is not found in the resulting list anymore.

src/external/wb_presets.c
@@ -64,6 +64,8 @@ static const char WhiteFluorescent[] = N_("white fluorescent");
 static const char SodiumVaporFluorescent[] = N_("sodium-vapor fluorescent");
 static const char DayWhiteFluorescent[] = N_("day white fluorescent");
 static const char HighTempMercuryVaporFluorescent[] = N_("high temp. mercury-vapor fluorescent");
+// Found in Nikon P1000
+static const char HTMercury[] = N_("high temp. mercury-vapor");
 // On Some Panasonic
 static const char D55[] = N_("D55");
 static const char ISO_Studio_Tungsten[] = N_("ISO Studio Tungsten");
@@ -2349,6 +2351,74 @@ const wb_data wb_preset[] =
   { "Nikon", "Coolpix P340", DaylightFluorescent, 0,	{ 2.093750, 1, 1.371094, 0 } },
   { "Nikon", "Coolpix P340", Cloudy, 0,			{ 1.976563, 1, 1.457031, 0 } },
   { "Nikon", "Coolpix P340", Flash, 0,			{ 2.085938, 1, 1.554688, 0 } },
+
+  // Nikon CoolPix P1000 firmware 1.2
+  { "Nikon", "COOLPIX P1000", HTMercury           , 0, { 1.76953125, 1, 1.61328125, 0 } },
+  { "Nikon", "COOLPIX P1000", DirectSunlight      , 0, { 1.4375, 1, 1.99609375, 0 } },
+  { "Nikon", "COOLPIX P1000", DirectSunlight      , 0, { 1.46875, 1, 1.94140625, 0 } },
+  { "Nikon", "COOLPIX P1000", DirectSunlight      , 0, { 1.50390625, 1, 1.88671875, 0 } },
+  { "Nikon", "COOLPIX P1000", DirectSunlight      , 0, { 1.5390625, 1, 1.8203125, 0 } },
+  { "Nikon", "COOLPIX P1000", DirectSunlight      , 0, { 1.578125, 1, 1.7578125, 0 } },
+  { "Nikon", "COOLPIX P1000", DirectSunlight      , 0, { 1.61328125, 1, 1.6953125, 0 } },
+  { "Nikon", "COOLPIX P1000", DirectSunlight      , 0, { 1.40625, 1, 2.05078125, 0 } },
+  { "Nikon", "COOLPIX P1000", Daylight            , 0, { 1.50390625, 1, 1.88671875, 0 } },
+  { "Nikon", "COOLPIX P1000", Shade               , 0, { 1.87890625, 1, 1.4140625, 0 } },
+  { "Nikon", "COOLPIX P1000", Cloudy              , 0, { 1.61328125, 1, 1.765625, 0 } },
+  { "Nikon", "COOLPIX P1000", Cloudy              , 0, { 1.65234375, 1, 1.71875, 0 } },
+  { "Nikon", "COOLPIX P1000", Cloudy              , 0, { 1.734375, 1, 1.6015625, 0 } },
+  { "Nikon", "COOLPIX P1000", Cloudy              , 0, { 1.7734375, 1, 1.546875, 0 } },
+  { "Nikon", "COOLPIX P1000", Cloudy              , 0, { 1.578125, 1, 1.8203125, 0 } },
+  { "Nikon", "COOLPIX P1000", Cloudy              , 0, { 1.54296875, 1, 1.87109375, 0 } },
+  { "Nikon", "COOLPIX P1000", Cloudy              , 0, { 1.69140625, 1, 1.66015625, 0 } },
+  { "Nikon", "COOLPIX P1000", Tungsten            , 0, { 1.015625, 1, 2.8046875, 0 } },
+  { "Nikon", "COOLPIX P1000", Incandescent        , 0, { 0.9921875, 1, 2.88671875, 0 } },
+  { "Nikon", "COOLPIX P1000", Incandescent        , 0, { 0.96875, 1, 2.96875, 0 } },
+  { "Nikon", "COOLPIX P1000", Incandescent        , 0, { 1.015625, 1, 2.8046875, 0 } },
+  { "Nikon", "COOLPIX P1000", Incandescent        , 0, { 1.0625, 1, 2.6171875, 0 } },
+  { "Nikon", "COOLPIX P1000", Incandescent        , 0, { 1.0390625, 1, 2.7109375, 0 } },
+  { "Nikon", "COOLPIX P1000", Incandescent        , 0, { 0.94921875, 1, 3.05078125, 0 } },
+  { "Nikon", "COOLPIX P1000", Incandescent        , 0, { 1.08984375, 1, 2.5234375, 0 } },
+  { "Nikon", "COOLPIX P1000", CoolWhiteFluorescent, 0, { 1.5859375, 1, 2.5, 0 } },
+  { "Nikon", "COOLPIX P1000", NeutralFluorescent  , 0, { 1.59765625, 1, 1.83203125, 0 } },
+  { "Nikon", "COOLPIX P1000", DaylightFluorescent , 0, { 1.796875, 1, 1.47265625, 0 } },
+  { "Nikon", "COOLPIX P1000", Flash               , 0, { 1.8984375, 1, 1.44921875, 0 } },
+  { "Nikon", "COOLPIX P1000", Flash               , 0, { 1.8125, 1, 1.55859375, 0 } },
+  { "Nikon", "COOLPIX P1000", Flash               , 0, { 1.65234375, 1, 1.75390625, 0 } },
+  { "Nikon", "COOLPIX P1000", Flash               , 0, { 1.69140625, 1, 1.70703125, 0 } },
+  { "Nikon", "COOLPIX P1000", Flash               , 0, { 1.73046875, 1, 1.66015625, 0 } },
+  { "Nikon", "COOLPIX P1000", Flash               , 0, { 1.76953125, 1, 1.61328125, 0 } },
+  { "Nikon", "COOLPIX P1000", Flash               , 0, { 1.85546875, 1, 1.50390625, 0 } },
+  { "Nikon", "COOLPIX P1000", "2500K"             , 0, { 1.078125, 1, 2.98828125, 0 } },
+  { "Nikon", "COOLPIX P1000", "2560K"             , 0, { 1.07421875, 1, 2.94140625, 0 } },
+  { "Nikon", "COOLPIX P1000", "2630K"             , 0, { 1.06640625, 1, 2.890625, 0 } },
+  { "Nikon", "COOLPIX P1000", "2700K"             , 0, { 1.05859375, 1, 2.83984375, 0 } },
+  { "Nikon", "COOLPIX P1000", "2780K"             , 0, { 1.0625, 1, 2.78515625, 0 } },
+  { "Nikon", "COOLPIX P1000", "2860K"             , 0, { 1.0625, 1, 2.73046875, 0 } },
+  { "Nikon", "COOLPIX P1000", "2940K"             , 0, { 1.06640625, 1, 2.67578125, 0 } },
+  { "Nikon", "COOLPIX P1000", "3030K"             , 0, { 1.0703125, 1, 2.62109375, 0 } },
+  { "Nikon", "COOLPIX P1000", "3130K"             , 0, { 1.0703125, 1, 2.5625, 0 } },
+  { "Nikon", "COOLPIX P1000", "3230K"             , 0, { 1.07421875, 1, 2.51171875, 0 } },
+  { "Nikon", "COOLPIX P1000", "3330K"             , 0, { 1.11328125, 1, 2.45703125, 0 } },
+  { "Nikon", "COOLPIX P1000", "3450K"             , 0, { 1.15234375, 1, 2.3984375, 0 } },
+  { "Nikon", "COOLPIX P1000", "3570K"             , 0, { 1.19140625, 1, 2.34375, 0 } },
+  { "Nikon", "COOLPIX P1000", "3700K"             , 0, { 1.23046875, 1, 2.28515625, 0 } },
+  { "Nikon", "COOLPIX P1000", "3850K"             , 0, { 1.25390625, 1, 2.2109375, 0 } },
+  { "Nikon", "COOLPIX P1000", "4000K"             , 0, { 1.2734375, 1, 2.13671875, 0 } },
+  { "Nikon", "COOLPIX P1000", "4170K"             , 0, { 1.31640625, 1, 2.09375, 0 } },
+  { "Nikon", "COOLPIX P1000", "4350K"             , 0, { 1.359375, 1, 2.05078125, 0 } },
+  { "Nikon", "COOLPIX P1000", "4550K"             , 0, { 1.3984375, 1, 2.00390625, 0 } },
+  { "Nikon", "COOLPIX P1000", "4760K"             , 0, { 1.44140625, 1, 1.9609375, 0 } },
+  { "Nikon", "COOLPIX P1000", "5000K"             , 0, { 1.48046875, 1, 1.91796875, 0 } },
+  { "Nikon", "COOLPIX P1000", "5260K"             , 0, { 1.5078125, 1, 1.85546875, 0 } },
+  { "Nikon", "COOLPIX P1000", "5560K"             , 0, { 1.53515625, 1, 1.79296875, 0 } },
+  { "Nikon", "COOLPIX P1000", "5880K"             , 0, { 1.5625, 1, 1.734375, 0 } },
+  { "Nikon", "COOLPIX P1000", "6250K"             , 0, { 1.5859375, 1, 1.671875, 0 } },
+  { "Nikon", "COOLPIX P1000", "6670K"             , 0, { 1.65625, 1, 1.625, 0 } },
+  { "Nikon", "COOLPIX P1000", "7140K"             , 0, { 1.73046875, 1, 1.58203125, 0 } },
+  { "Nikon", "COOLPIX P1000", "7690K"             , 0, { 1.80078125, 1, 1.5390625, 0 } },
+  { "Nikon", "COOLPIX P1000", "8330K"             , 0, { 1.88671875, 1, 1.48828125, 0 } },
+  { "Nikon", "COOLPIX P1000", "9090K"             , 0, { 1.97265625, 1, 1.4375, 0 } },
+  { "Nikon", "COOLPIX P1000", "10000K"            , 0, { 2.0703125, 1, 1.375, 0 } },
 
   // Fine tuning is from A6 to B6 on amber-blue.
   { "Nikon", "Coolpix P7100", DirectSunlight, -6, { 1.7908, 1, 1.9042, 0 } },

commit 3cc6a591891c66a0d67c649821d9b9fd49a71b9b

fix Nikon P1000 wb presets
resort wb presets with finetuning according to coefficients and
add proper finetuning options for them.

this fixes wb preset bug introduced in #4335

src/external/wb_presets.c
@@ -2470,40 +2470,40 @@ const wb_data wb_preset[] =
 
   // Nikon CoolPix P1000 firmware 1.2
   { "Nikon", "COOLPIX P1000", HTMercury           , 0, { 1.76953125, 1, 1.61328125, 0 } },
-  { "Nikon", "COOLPIX P1000", DirectSunlight      , 0, { 1.4375, 1, 1.99609375, 0 } },
-  { "Nikon", "COOLPIX P1000", DirectSunlight      , 0, { 1.46875, 1, 1.94140625, 0 } },
-  { "Nikon", "COOLPIX P1000", DirectSunlight      , 0, { 1.50390625, 1, 1.88671875, 0 } },
-  { "Nikon", "COOLPIX P1000", DirectSunlight      , 0, { 1.5390625, 1, 1.8203125, 0 } },
-  { "Nikon", "COOLPIX P1000", DirectSunlight      , 0, { 1.578125, 1, 1.7578125, 0 } },
-  { "Nikon", "COOLPIX P1000", DirectSunlight      , 0, { 1.61328125, 1, 1.6953125, 0 } },
-  { "Nikon", "COOLPIX P1000", DirectSunlight      , 0, { 1.40625, 1, 2.05078125, 0 } },
+  { "Nikon", "COOLPIX P1000", DirectSunlight      , -3, { 1.40625, 1, 2.05078125, 0 } },
+  { "Nikon", "COOLPIX P1000", DirectSunlight      , -2, { 1.4375, 1, 1.99609375, 0 } },
+  { "Nikon", "COOLPIX P1000", DirectSunlight      , -1, { 1.46875, 1, 1.94140625, 0 } },
+  { "Nikon", "COOLPIX P1000", DirectSunlight      ,  0, { 1.50390625, 1, 1.88671875, 0 } },
+  { "Nikon", "COOLPIX P1000", DirectSunlight      ,  1, { 1.5390625, 1, 1.8203125, 0 } },
+  { "Nikon", "COOLPIX P1000", DirectSunlight      ,  2, { 1.578125, 1, 1.7578125, 0 } },
+  { "Nikon", "COOLPIX P1000", DirectSunlight      ,  3, { 1.61328125, 1, 1.6953125, 0 } },
   { "Nikon", "COOLPIX P1000", Daylight            , 0, { 1.50390625, 1, 1.88671875, 0 } },
   { "Nikon", "COOLPIX P1000", Shade               , 0, { 1.87890625, 1, 1.4140625, 0 } },
-  { "Nikon", "COOLPIX P1000", Cloudy              , 0, { 1.61328125, 1, 1.765625, 0 } },
-  { "Nikon", "COOLPIX P1000", Cloudy              , 0, { 1.65234375, 1, 1.71875, 0 } },
-  { "Nikon", "COOLPIX P1000", Cloudy              , 0, { 1.734375, 1, 1.6015625, 0 } },
-  { "Nikon", "COOLPIX P1000", Cloudy              , 0, { 1.7734375, 1, 1.546875, 0 } },
-  { "Nikon", "COOLPIX P1000", Cloudy              , 0, { 1.578125, 1, 1.8203125, 0 } },
-  { "Nikon", "COOLPIX P1000", Cloudy              , 0, { 1.54296875, 1, 1.87109375, 0 } },
-  { "Nikon", "COOLPIX P1000", Cloudy              , 0, { 1.69140625, 1, 1.66015625, 0 } },
+  { "Nikon", "COOLPIX P1000", Cloudy              , -3, { 1.54296875, 1, 1.87109375, 0 } },  
+  { "Nikon", "COOLPIX P1000", Cloudy              , -2, { 1.578125, 1, 1.8203125, 0 } },  
+  { "Nikon", "COOLPIX P1000", Cloudy              , -1, { 1.61328125, 1, 1.765625, 0 } },
+  { "Nikon", "COOLPIX P1000", Cloudy              ,  0, { 1.65234375, 1, 1.71875, 0 } },
+  { "Nikon", "COOLPIX P1000", Cloudy              ,  1, { 1.69140625, 1, 1.66015625, 0 } },  
+  { "Nikon", "COOLPIX P1000", Cloudy              ,  2, { 1.734375, 1, 1.6015625, 0 } },
+  { "Nikon", "COOLPIX P1000", Cloudy              ,  3, { 1.7734375, 1, 1.546875, 0 } },
   { "Nikon", "COOLPIX P1000", Tungsten            , 0, { 1.015625, 1, 2.8046875, 0 } },
-  { "Nikon", "COOLPIX P1000", Incandescent        , 0, { 0.9921875, 1, 2.88671875, 0 } },
-  { "Nikon", "COOLPIX P1000", Incandescent        , 0, { 0.96875, 1, 2.96875, 0 } },
-  { "Nikon", "COOLPIX P1000", Incandescent        , 0, { 1.015625, 1, 2.8046875, 0 } },
-  { "Nikon", "COOLPIX P1000", Incandescent        , 0, { 1.0625, 1, 2.6171875, 0 } },
-  { "Nikon", "COOLPIX P1000", Incandescent        , 0, { 1.0390625, 1, 2.7109375, 0 } },
-  { "Nikon", "COOLPIX P1000", Incandescent        , 0, { 0.94921875, 1, 3.05078125, 0 } },
-  { "Nikon", "COOLPIX P1000", Incandescent        , 0, { 1.08984375, 1, 2.5234375, 0 } },
+  { "Nikon", "COOLPIX P1000", Incandescent        , -3, { 0.94921875, 1, 3.05078125, 0 } },  
+  { "Nikon", "COOLPIX P1000", Incandescent        , -2, { 0.96875, 1, 2.96875, 0 } },  
+  { "Nikon", "COOLPIX P1000", Incandescent        , -1, { 0.9921875, 1, 2.88671875, 0 } },
+  { "Nikon", "COOLPIX P1000", Incandescent        ,  0, { 1.015625, 1, 2.8046875, 0 } },
+  { "Nikon", "COOLPIX P1000", Incandescent        ,  1, { 1.0390625, 1, 2.7109375, 0 } },
+  { "Nikon", "COOLPIX P1000", Incandescent        ,  2, { 1.0625, 1, 2.6171875, 0 } },
+  { "Nikon", "COOLPIX P1000", Incandescent        ,  3, { 1.08984375, 1, 2.5234375, 0 } },
   { "Nikon", "COOLPIX P1000", CoolWhiteFluorescent, 0, { 1.5859375, 1, 2.5, 0 } },
   { "Nikon", "COOLPIX P1000", NeutralFluorescent  , 0, { 1.59765625, 1, 1.83203125, 0 } },
   { "Nikon", "COOLPIX P1000", DaylightFluorescent , 0, { 1.796875, 1, 1.47265625, 0 } },
-  { "Nikon", "COOLPIX P1000", Flash               , 0, { 1.8984375, 1, 1.44921875, 0 } },
-  { "Nikon", "COOLPIX P1000", Flash               , 0, { 1.8125, 1, 1.55859375, 0 } },
-  { "Nikon", "COOLPIX P1000", Flash               , 0, { 1.65234375, 1, 1.75390625, 0 } },
-  { "Nikon", "COOLPIX P1000", Flash               , 0, { 1.69140625, 1, 1.70703125, 0 } },
-  { "Nikon", "COOLPIX P1000", Flash               , 0, { 1.73046875, 1, 1.66015625, 0 } },
-  { "Nikon", "COOLPIX P1000", Flash               , 0, { 1.76953125, 1, 1.61328125, 0 } },
-  { "Nikon", "COOLPIX P1000", Flash               , 0, { 1.85546875, 1, 1.50390625, 0 } },
+  { "Nikon", "COOLPIX P1000", Flash               , -3, { 1.65234375, 1, 1.75390625, 0 } },
+  { "Nikon", "COOLPIX P1000", Flash               , -2, { 1.69140625, 1, 1.70703125, 0 } },
+  { "Nikon", "COOLPIX P1000", Flash               , -1, { 1.73046875, 1, 1.66015625, 0 } },
+  { "Nikon", "COOLPIX P1000", Flash               ,  0, { 1.76953125, 1, 1.61328125, 0 } },
+  { "Nikon", "COOLPIX P1000", Flash               ,  1, { 1.8125, 1, 1.55859375, 0 } },  
+  { "Nikon", "COOLPIX P1000", Flash               ,  2, { 1.85546875, 1, 1.50390625, 0 } },  
+  { "Nikon", "COOLPIX P1000", Flash               ,  3, { 1.8984375, 1, 1.44921875, 0 } },  
   { "Nikon", "COOLPIX P1000", "2500K"             , 0, { 1.078125, 1, 2.98828125, 0 } },
   { "Nikon", "COOLPIX P1000", "2560K"             , 0, { 1.07421875, 1, 2.94140625, 0 } },
   { "Nikon", "COOLPIX P1000", "2630K"             , 0, { 1.06640625, 1, 2.890625, 0 } },