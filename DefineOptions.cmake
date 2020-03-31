1c73bc7e26ac399943696e08a35cb9b0aaaf06f3
Tobias Ellinghaus <tobias.ellinghaus@k-lens.de>
2020-03-31 15:36:56 +0200

    Add cmake option for colored compiler output
    (cherry picked from commit 9ff9cc7480a4143197aa2125e29a016a81c5a0ea)

--- DefineOptions.cmake
+++ DefineOptions.cmake
@@ -32,6 +32,7 @@ option(BUILD_CURVE_TOOLS "Build tools for generating base and tone curves" OFF)
 option(USE_LENSFUN "Enable LensFun support" ON)
 option(USE_GMIC "Use G'MIC image processing framework." ON)
 option(USE_GAME "Build 1st April easter egg game" ON)
+option(FORCE_COLORED_OUTPUT "Always produce ANSI-colored output (GNU/Clang only)." OFF)
 
 if (USE_OPENCL)
     option(TESTBUILD_OPENCL_PROGRAMS "Test-compile opencl programs (needs llvm and clang 3.9+)" ON)
