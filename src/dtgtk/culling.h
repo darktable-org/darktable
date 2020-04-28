4ce0064bee835cffd29167bc75a42f674bdd9c2a
AlicVB <dev@lnaa.fr>
2020-04-28 17:42:48 +0200

    culling/preview : prefectch next/previous images

--- src/dtgtk/culling.h
+++ src/dtgtk/culling.h
@@ -98,9 +98,6 @@ void dt_culling_change_offset_image(dt_culling_t *table, int offset);
 void dt_culling_zoom_max(dt_culling_t *table);
 void dt_culling_zoom_fit(dt_culling_t *table);
 
-// TODO
-// prefetching
-
 // modelines: These editor modelines have been set for all relevant files by tools/update_modelines.sh
 // vim: shiftwidth=2 expandtab tabstop=2 cindent
 // kate: tab-indents: off; indent-width 2; replace-tabs on; indent-mode cstyle; remove-trailing-spaces modified;
