30df692cc527b5332e983238279c67eff92c89ef
Aurélien PIERRE <aurelien@aurelienpierre.com>
2020-05-03 20:54:05 +0200

    add new RGB channel mixer unbounded

--- src/common/illuminants.h
+++ src/common/illuminants.h
@@ -16,7 +16,13 @@
     along with darktable.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#pragma once
+
+#include "common/chromatic_adaptation.h"
 #include "common/colorspaces_inline_conversions.h"
+#include "common/image.h"
+#include "external/adobe_coeff.c"
+
 
 /* Standard CIE illuminants */
 typedef enum dt_illuminant_t
@@ -29,6 +35,8 @@ typedef enum dt_illuminant_t
   DT_ILLUMINANT_LED    = 5,    // LED
   DT_ILLUMINANT_BB     = 6,    // general black body radiator - not CIE standard
   DT_ILLUMINANT_CUSTOM = 7,    // input x and y directly - bypass search
+  DT_ILLUMINANT_DETECT = 8,    // auto-detection in image
+  DT_ILLUMINANT_CAMERA = 9,    // read RAW EXIF for WB
   DT_ILLUMINANT_LAST
 } dt_illuminant_t;
 
@@ -189,7 +197,7 @@ static inline void illuminant_xy_to_RGB(const float x, const float y, float RGB[
 
   // Fixme : convert to RGB display space instead of sRGB but first the display profile should be global in dt,
   // not confined to colorout where it gets created/destroyed all the time.
-  dt_XYZ_to_Rec709_D65(XYZ, RGB);
+  dt_XYZ_to_Rec709_D50(XYZ, RGB);
 
   // Handle gamut clipping
   const float max_RGB = fmaxf(fmaxf(RGB[0], RGB[1]), RGB[2]);
@@ -212,7 +220,12 @@ static inline void illuminant_CCT_to_RGB(const float t, float RGB[4])
 }
 
 
+// Fetch image from pipeline and read EXIF for camera RAW WB coeffs
+static inline int find_temperature_from_raw_coeffs(const dt_image_t *img, float *chroma_x, float *chroma_y);
+
+
 static int illuminant_to_xy(const dt_illuminant_t illuminant, // primary type of illuminant
+                            const dt_image_t *img,            // image container
                             float *x_out, float *y_out,       // chromaticity output
                             const float t,                    // temperature in K, if needed
                             const dt_illuminant_fluo_t fluo,  // sub-type of fluorescent illuminant, if needed
@@ -233,9 +246,9 @@ static int illuminant_to_xy(const dt_illuminant_t illuminant, // primary type of
   {
     case DT_ILLUMINANT_PIPE:
     {
-      // darktable default pipeline D65
-      x = 0.31271f;
-      y = 0.32902f;
+      // darktable default pipeline D50
+      x = 0.34567f;
+      y = 0.35850f;
       break;
     }
     case DT_ILLUMINANT_E:
@@ -285,7 +298,14 @@ static int illuminant_to_xy(const dt_illuminant_t illuminant, // primary type of
       if(y != 0.f && x != 0.f) break;
       // else t is out of bounds -> use custom/original values (next case)
     }
+    case DT_ILLUMINANT_CAMERA:
+    {
+      // Detect WB from RAW EXIF
+      if(img)
+        if(find_temperature_from_raw_coeffs(img, &x, &y)) break;
+    }
     case DT_ILLUMINANT_CUSTOM: // leave x and y as-is
+    case DT_ILLUMINANT_DETECT:
     case DT_ILLUMINANT_LAST:
     {
       return FALSE;
@@ -307,6 +327,7 @@ static inline void WB_coeffs_to_illuminant_xy(const float CAM_to_XYZ[4][3], cons
 {
   // Find the illuminant chromaticity x y from RAW WB coeffs and camera input matrice
   float XYZ[4];
+  float LMS[4];
   // Simulate white point, aka convert (1, 1, 1) in camera space to XYZ
   // warning : we multiply the transpose of CAM_to_XYZ  since the pseudoinverse transposes it
   XYZ[0] = CAM_to_XYZ[0][0] / WB[0] + CAM_to_XYZ[1][0] / WB[1] + CAM_to_XYZ[2][0] / WB[2];
@@ -314,14 +335,12 @@ static inline void WB_coeffs_to_illuminant_xy(const float CAM_to_XYZ[4][3], cons
   XYZ[2] = CAM_to_XYZ[0][2] / WB[0] + CAM_to_XYZ[1][2] / WB[1] + CAM_to_XYZ[2][2] / WB[2];
 
   // Matrices white point is D65. We need to convert it for darktable's pipe (D50)
-  /*
   static const float DT_ALIGNED_PIXEL D65[4] = { 0.941238f, 1.040633f, 1.088932f, 0.f };
-  static const float p = powf(1.088932f / 0.818155f, 0.0834f);
+  const float p = powf(1.088932f / 0.818155f, 0.0834f);
 
-  convert_XYZ_to_bradford_LMS(XYZ, XYZ);
-  bradford_adapt_D50(XYZ, D65, p, XYZ);
-  convert_bradford_LMS_to_XYZ(XYZ, XYZ);
-  */
+  convert_XYZ_to_bradford_LMS(XYZ, LMS);
+  bradford_adapt_D50(LMS, D65, p, FALSE, LMS);
+  convert_bradford_LMS_to_XYZ(LMS, XYZ);
 
   // Get white point chromaticity
   XYZ[0] /= XYZ[1];
@@ -330,6 +349,88 @@ static inline void WB_coeffs_to_illuminant_xy(const float CAM_to_XYZ[4][3], cons
 
   *x = XYZ[0] / (XYZ[0] + XYZ[1] + XYZ[2]);
   *y = XYZ[1] / (XYZ[0] + XYZ[1] + XYZ[2]);
+}
+
+
+static inline void matrice_pseudoinverse(float (*in)[3], float (*out)[3], int size)
+{
+  float work[3][6];
+
+  for(int i = 0; i < 3; i++)
+  {
+    for(int j = 0; j < 6; j++)
+      work[i][j] = j == i+3;
+    for(int j = 0; j < 3; j++)
+      for(int k = 0; k < size; k++)
+        work[i][j] += in[k][i] * in[k][j];
+  }
+  for(int i = 0; i < 3; i++)
+  {
+    float num = work[i][i];
+    for(int j = 0; j < 6; j++)
+      work[i][j] /= num;
+    for(int k = 0; k < 3; k++)
+    {
+      if(k==i) continue;
+      num = work[k][i];
+      for(int j = 0; j < 6; j++)
+        work[k][j] -= work[i][j] * num;
+    }
+  }
+  for(int i = 0; i < size; i++)
+    for(int j = 0; j < 3; j++)
+    {
+      out[i][j] = 0.0f;
+      for(int k = 0; k < 3; k++)
+        out[i][j] += work[j][k+3] * in[i][k];
+    }
+}
+
+
+static int find_temperature_from_raw_coeffs(const dt_image_t *img, float *chroma_x, float *chroma_y)
+{
+  const int is_raw = dt_image_is_matrix_correction_supported(img);
+
+  if(is_raw)
+  {
+    int has_valid_coeffs = TRUE;
+    const int num_coeffs = (img->flags & DT_IMAGE_4BAYER) ? 4 : 3;
+
+    // Check coeffs
+    for(int k = 0; has_valid_coeffs && k < num_coeffs; k++)
+      if(!isnormal(img->wb_coeffs[k]) || img->wb_coeffs[k] == 0.0f) has_valid_coeffs = FALSE;
+
+    if(has_valid_coeffs)
+    {
+      // Get white balance camera factors
+      float WB[4] = { img->wb_coeffs[0],
+                      img->wb_coeffs[1],
+                      img->wb_coeffs[2],
+                      img->wb_coeffs[3] };
+
+      // Get the camera input profile (matrice of primaries)
+      float XYZ_to_CAM[4][3];
+      XYZ_to_CAM[0][0] = NAN;
+      dt_dcraw_adobe_coeff(img->camera_makermodel, (float(*)[12])XYZ_to_CAM);
+
+      if(isnan(XYZ_to_CAM[0][0])) return FALSE;
+
+      // Bloody input matrices define XYZ -> CAM transform, as if we often needed camera profiles to output
+      // So we need to invert them. Here go your CPU cycles again.
+      float CAM_to_XYZ[4][3];
+      CAM_to_XYZ[0][0] = NAN;
+      matrice_pseudoinverse(XYZ_to_CAM, CAM_to_XYZ, 3);
+      if(isnan(CAM_to_XYZ[0][0])) return FALSE;
+
+      float x, y;
+      WB_coeffs_to_illuminant_xy(CAM_to_XYZ, WB, &x, &y);
+      *chroma_x = x;
+      *chroma_y = y;
+
+      return TRUE;
+    }
+  }
+  return FALSE;
 }
 
 
@@ -378,6 +479,21 @@ static inline float get_tint_from_tinted_xy(const float x, const float y, const 
   CCT_to_xy_blackbody(t, &x_bb, &y_bb);
   const float tint = -(y - y_bb) * norm;
   return tint;
+}
+
+
+#ifdef _OPENMP
+#pragma omp declare simd
+#endif
+static inline void xy_to_uv(const float xy[2], float uv[4])
+{
+  // Convert to CIE1960 Yuv color space, usefull to compute CCT
+  // https://en.wikipedia.org/wiki/CIE_1960_color_space
+  const float denom = 12.f * xy[1] - 1.882f * xy[0] + 2.9088f;
+  uv[0] = 5.5932f * xy[0] + 1.9116 * xy[1];
+  uv[1] = 7.8972f * xy[1];
+  uv[0] /= denom;
+  uv[1] /= denom;
 }
 
 
@@ -439,7 +555,11 @@ static inline float CCT_reverse_lookup(const float x, const float y)
 
     // Current x, y chromaticity
     float x_bb, y_bb;
-    CCT_to_xy_blackbody(T, &x_bb, &y_bb);
+
+    if(T > 4000.f)
+      CCT_to_xy_daylight(T, &x_bb, &y_bb);
+    else
+      CCT_to_xy_blackbody(T, &x_bb, &y_bb);
 
     // Compute distance between current planckian chromaticity and input
     const pair radius_tmp = { hypotf((x_bb - x), (y_bb - y)), T };
--- src/common/iop_order.c
+++ src/common/iop_order.c
@@ -104,6 +104,7 @@ const dt_iop_order_entry_t legacy_order[] = {
   { {25.0f }, "profile_gamma", 0},
   { {26.0f }, "hazeremoval", 0},
   { {27.0f }, "colorin", 0},
+  { {27.5f }, "channelmixerrgb", 0},
   { {27.5f }, "negadoctor", 0},
   { {27.5f }, "basicadj", 0},
   { {28.0f }, "colorreconstruct", 0},
@@ -186,6 +187,7 @@ const dt_iop_order_entry_t v30_order[] = {
   { {26.0f }, "profile_gamma", 0},
   { {27.0f }, "equalizer", 0},
   { {28.0f }, "colorin", 0},
+  { {28.5f }, "channelmixerrgb", 0},
   { {28.5f }, "negadoctor", 0},      // Cineon film encoding comes after scanner input color profile
   { {29.0f }, "nlmeans", 0},         // signal processing (denoising)
                                   //    -> needs a signal as scene-referred as possible (even if it works in Lab)
@@ -632,6 +634,7 @@ GList *dt_ioppr_get_iop_order_list(int32_t imgid, gboolean sorted)
           // @@_NEW_MOUDLE: For new module it is required to insert the new module name in the iop-order list here.
           //                The insertion can be done depending on the current iop-order list kind.
           _insert_before(iop_order_list, "nlmeans", "negadoctor");
+          _insert_before(iop_order_list, "negadoctor", "channelmixerrgb");
         }
       }
       else if(version == DT_IOP_ORDER_LEGACY)
