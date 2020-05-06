5cfa1f7abefb26687d0a829ca8501ca7b44a9794
Diederik ter Rahe <dterrahe@yahoo.com>
2020-05-06 14:26:01 +0100

    Infrastructure for minimalist colorpicker gui definition

--- src/gui/color_picker_proxy.h
+++ src/gui/color_picker_proxy.h
@@ -128,6 +128,9 @@ void dt_iop_color_picker_init();
 /* global cleanup */
 void dt_iop_color_picker_cleanup();
 
+/* link color picker to widget */
+GtkWidget *dt_color_picker_new(dt_iop_module_t *module, dt_iop_color_picker_kind_t kind, GtkWidget *w);
+
 // modelines: These editor modelines have been set for all relevant files by tools/update_modelines.sh
 // vim: shiftwidth=2 expandtab tabstop=2 cindent
 // kate: tab-indents: off; indent-width 2; replace-tabs on; indent-mode cstyle; remove-trailing-spaces modified;
