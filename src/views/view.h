e72e42c89417205e749c02eeac339d9e72558075
AlicVB <dev@lnaa.fr>
2020-05-09 15:24:39 +0200

    finish removing dt_view_get_image_to act_on

--- src/views/view.h
+++ src/views/view.h
@@ -187,11 +187,6 @@ typedef enum dt_view_image_over_t
   DT_VIEW_END     = 10, // placeholder for the end of the list
 } dt_view_image_over_t;
 
-/** returns -1 if the action has to be applied to the selection,
-    or the imgid otherwise
-    TODO : remove it in profit of next functions*/
-int32_t dt_view_get_image_to_act_on();
-
 // get images to act on for gloabals change (via libs or accels)
 GList *dt_view_get_images_to_act_on(gboolean only_visible);
 // get the main image to act on during global changes (libs, accels)
