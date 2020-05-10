fc218ec65af1bd702f449bf2599367ce25aa66db
Pascal Obry <pascal@obry.net>
2020-05-10 23:09:57 +0200

    paint: add some consts.

--- src/dtgtk/paint.c
+++ src/dtgtk/paint.c
@@ -1831,7 +1831,7 @@ void dtgtk_cairo_paint_overexposed(cairo_t *cr, gint x, gint y, gint w, gint h, 
   cairo_translate(cr, x + (w / 2.0) - (s / 2.0), y + (h / 2.0) - (s / 2.0));
   cairo_scale(cr, s, s);
 
-  float line_width = 0.1;
+  const float line_width = 0.1;
 
   cairo_set_line_cap(cr, CAIRO_LINE_CAP_ROUND);
   cairo_set_line_width(cr, line_width);
@@ -1854,7 +1854,7 @@ void dtgtk_cairo_paint_bulb(cairo_t *cr, gint x, gint y, gint w, gint h, gint fl
   cairo_translate(cr, x + (w / 2.0) - (s / 2.0), y + (h / 2.0) - (s / 2.0));
   cairo_scale(cr, 0.9 * s, 0.9 * s);
 
-  float line_width = 0.1;
+  const float line_width = 0.1;
 
   cairo_set_line_cap(cr, CAIRO_LINE_CAP_ROUND);
   cairo_set_line_width(cr, line_width);
@@ -1893,7 +1893,7 @@ void dtgtk_cairo_paint_rawoverexposed(cairo_t *cr, gint x, gint y, gint w, gint 
   cairo_translate(cr, x + (w / 2.0) - (s / 2.0), y + (h / 2.0) - (s / 2.0));
   cairo_scale(cr, s, s);
 
-  float line_width = 0.1;
+  const float line_width = 0.1;
 
   cairo_set_line_cap(cr, CAIRO_LINE_CAP_ROUND);
   cairo_set_line_width(cr, line_width);
