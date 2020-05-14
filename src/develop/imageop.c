90225b0e83ab349d3a67e73844d90b4eaeef2d64
Chris.Elston <chris.elston@blueyonder.co.uk>
2020-05-14 21:21:20 +0100

    fix module toggle button
    fix reversed on/off behaviour for module toggle button
    
    Resolves #5047

--- src/develop/imageop.c
+++ src/develop/imageop.c
@@ -1060,16 +1060,26 @@ static void dt_iop_gui_multiinstance_callback(GtkButton *button, GdkEventButton 
   dtgtk_button_set_active(DTGTK_BUTTON(button), FALSE);
 }
 
-static gboolean dt_iop_gui_off_callback(GtkWidget *w, GdkEventButton *e, gpointer user_data)
+static gboolean dt_iop_gui_off_button_press(GtkWidget *w, GdkEventButton *e, gpointer user_data)
 {
   dt_iop_module_t *module = (dt_iop_module_t *)user_data;
   if(!darktable.gui->reset)
   {
     if(e->state & GDK_CONTROL_MASK)
       dt_iop_request_focus(darktable.develop->gui_module == module ? NULL : module);
-    else if(gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(w)))
+    else
+      gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(w), !gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(w)));
+  }
+  return TRUE;
+}
+  
+static void dt_iop_gui_off_callback(GtkToggleButton *togglebutton, gpointer user_data)
+{
+  dt_iop_module_t *module = (dt_iop_module_t *)user_data;
+  if(!darktable.gui->reset)
+  {
+    if(gtk_toggle_button_get_active(togglebutton))
     {
-      gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(w), FALSE);
       module->enabled = 1;
 
       if(dt_conf_get_bool("darkroom/ui/scroll_to_module"))
@@ -1080,7 +1090,6 @@ static gboolean dt_iop_gui_off_callback(GtkWidget *w, GdkEventButton *e, gpointe
     }
     else
     {
-      gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(w), TRUE);
       module->enabled = 0;
 
       if(dt_conf_get_bool("darkroom/ui/activate_expand") && module->expanded)
@@ -1093,10 +1102,8 @@ static gboolean dt_iop_gui_off_callback(GtkWidget *w, GdkEventButton *e, gpointe
   snprintf(tooltip, sizeof(tooltip), module->enabled ? _("%s is switched on") : _("%s is switched off"),
            module_label);
   g_free(module_label);
-  gtk_widget_set_tooltip_text(w, tooltip);
-  gtk_widget_queue_draw(w);
-  
-  return TRUE;
+  gtk_widget_set_tooltip_text(GTK_WIDGET(togglebutton), tooltip);
+  gtk_widget_queue_draw(GTK_WIDGET(togglebutton));
 }
 
 gboolean dt_iop_so_is_hidden(dt_iop_module_so_t *module)
@@ -2028,7 +2035,8 @@ GtkWidget *dt_iop_gui_get_expander(dt_iop_module_t *module)
   g_free(module_label);
   gtk_widget_set_tooltip_text(GTK_WIDGET(hw[IOP_MODULE_SWITCH]), tooltip);
   gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(hw[IOP_MODULE_SWITCH]), module->enabled);
-  g_signal_connect(G_OBJECT(hw[IOP_MODULE_SWITCH]), "button-press-event", G_CALLBACK(dt_iop_gui_off_callback), module);
+  g_signal_connect(G_OBJECT(hw[IOP_MODULE_SWITCH]), "toggled", G_CALLBACK(dt_iop_gui_off_callback), module);
+  g_signal_connect(G_OBJECT(hw[IOP_MODULE_SWITCH]), "button-press-event", G_CALLBACK(dt_iop_gui_off_button_press), module);
   module->off = DTGTK_TOGGLEBUTTON(hw[IOP_MODULE_SWITCH]);
   gtk_widget_set_sensitive(GTK_WIDGET(hw[IOP_MODULE_SWITCH]), !module->hide_enable_button);
 
