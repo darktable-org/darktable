37aa24e371203413947f7961485aca9c0d4d30d5
Hanno Schwalm <hanno@schwalm-bremen.de>
2020-05-15 16:21:03 +0200

    Less offensive 'module-in-focus' icon (#5062)

--- src/dtgtk/paint.c
+++ src/dtgtk/paint.c
@@ -270,19 +270,18 @@ void dtgtk_cairo_paint_switch(cairo_t *cr, gint x, gint y, gint w, gint h, gint 
   const gint s = w < h ? w : h;
   cairo_translate(cr, x + (w / 2.0) - (s / 2.0), y + (h / 2.0) - (s / 2.0));
   cairo_scale(cr, s, s);
-
-  cairo_set_line_width(cr, 0.1);
   cairo_set_line_cap(cr, CAIRO_LINE_CAP_ROUND);
   if(flags & CPF_FOCUS)
   {
-    cairo_arc(cr, 0.5, 0.5, 0.66, 0, 2 * M_PI);
+    cairo_set_line_width(cr, 0.05);
+    cairo_arc(cr, 0.5, 0.5, 0.60, 0, M_PI);
     cairo_stroke(cr);
   }
+  cairo_set_line_width(cr, 0.1);
   cairo_arc(cr, 0.5, 0.5, 0.46, (-50 * 3.145 / 180), (230 * 3.145 / 180));
   cairo_move_to(cr, 0.5, 0.0);
   cairo_line_to(cr, 0.5, 0.5);
   cairo_stroke(cr);
-
   if(flags & CPF_ACTIVE) // If active add some diffuse light
   {
     cairo_arc(cr, 0.5, 0.5, 0.45, 0.0, 2*M_PI);
@@ -298,13 +297,13 @@ void dtgtk_cairo_paint_switch_on(cairo_t *cr, gint x, gint y, gint w, gint h, gi
   const gint s = w < h ? w : h;
   cairo_translate(cr, x + (w / 2.0) - (s / 2.0), y + (h / 2.0) - (s / 2.0));
   cairo_scale(cr, s, s);
-
-  cairo_set_line_width(cr, 0.1);
   if(flags & CPF_FOCUS)
   {
-    cairo_arc(cr, 0.5, 0.5, 0.7, 0, 2 * M_PI);
+    cairo_set_line_width(cr, 0.05);
+    cairo_arc(cr, 0.5, 0.5, 0.65, 0, 2 * M_PI);
     cairo_stroke(cr);
   }
+  cairo_set_line_width(cr, 0.1);
   cairo_arc(cr, 0.5, 0.5, 0.35, 0, 2 * M_PI);
   cairo_fill(cr);
 
