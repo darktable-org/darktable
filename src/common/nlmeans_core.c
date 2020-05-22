ea6415501fc28bd0db0d1675cab535f8b8120509
ralfbrown <ralfbrown@users.noreply.github.com>
2020-05-22 05:05:25 -0400

    New more-scaleable implementation of non-local means

---
+++ src/common/nlmeans_core.c
@@ -0,0 +1,300 @@
+/*
+    This file is part of darktable,
+    Copyright (C) 2011-2020 darktable developers.
+
+    darktable is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    darktable is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with darktable.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+//#include "bauhaus/bauhaus.h"
+#include "common/opencl.h"
+#include "control/control.h"
+#include "develop/imageop.h"
+#include "develop/imageop_math.h"
+#include "develop/tiling.h"
+#include "iop/iop_api.h"
+//#include <gtk/gtk.h>
+#include "common/nlmeans_core.h"
+#include <stdbool.h>
+#include <stdlib.h>
+
+#if defined(__SSE__)
+#include <xmmintrin.h>
+#endif
+
+// to avoid accumulation of rounding errors, we should do a full recomputation of the patch differences
+//   every so many rows of the image.  We'll also use that interval as the maximum chunk size for
+//   parallelization
+#define SLICE_HEIGHT 100
+
+// a structure to collect together the items which define the location of a patch relative to the pixel
+//  being denoised
+struct patch_t
+{
+  short rows;		// number of rows difference
+  short cols;		// number of columns difference
+  int offset;	  	// array distance between corresponding pixels
+};
+typedef struct patch_t patch_t;
+
+// some shorthand to make code more legible
+// if we have OpenMP simd enabled, declare a vectorizable for loop;
+// otherwise, just leave it a plain for()
+#if defined(_OPENMP) && defined(OPENMP_SIMD_)
+#define SIMD_FOR \
+  _Pragma("omp simd") \
+  for
+#else
+#define SIMD_FOR for
+#endif
+
+
+static inline float gh(const float f, const float sharpness)
+{
+  return exp2f(-f * sharpness);
+}
+
+static inline int sign(int a)
+{
+  return (a > 0) - (a < 0);
+}
+
+// map the basic row/column offset into a possible much larger offset based on a user parameter
+static int scatter(float scale, int scattering, int index1, int index2)
+{
+  // this formula is designed to
+  //  - produce an identity mapping when scattering = 0
+  //  - avoiding duplicate patches provided that 0 <= scattering <= 1
+  //  - avoiding grid artifacts by trying to take patches on various rows and columns
+  const int abs_i1 = abs(index1);
+  const int abs_i2 = abs(index2);
+  return scale * ((abs_i1 * abs_i1 * abs_i1 + 7.0 * abs_i1 * sqrt(abs_i2)) * sign(index1) * scattering / 6.0 + index1);
+}
+
+// allocate and fill an array of patch definitions
+static struct patch_t*
+define_patches(const dt_nlmeans_param_t *const params, const int stride, int *num_patches, int *max_shift)
+{
+  const int search_radius = params->search_radius;
+  const float scale = params->scale;
+  const float scattering = params->scattering;
+  int decimate = params->decimate;
+  decimate=0;//!!!
+  // determine how many patches we have
+  int n_patches = (2 * search_radius + 1) * (2 * search_radius + 1);
+  if (decimate)
+    n_patches = (n_patches + 1) / 2;
+  *num_patches = n_patches ;
+  // allocate a cacheline-aligned buffer
+  struct patch_t* patches = dt_alloc_align(64,n_patches*sizeof(struct patch_t));
+  // set up the patch offsets
+  int patch_num = 0;
+  int shift = 0;
+  for (int row_index = -search_radius; row_index <= search_radius; row_index++)
+  {
+    for (int col_index = -search_radius; col_index <= search_radius; col_index++)
+    {
+      if (decimate && (++decimate & 1)) continue; // skip every other patch
+      int r = scatter(scale,scattering,row_index,col_index);
+      int c = scatter(scale,scattering,col_index,row_index);
+      patches[patch_num].rows = r;
+      patches[patch_num].cols = c;
+      if (r > shift) shift = r;
+      else if (-r > shift) shift = -r;
+      if (c > shift) shift = c;
+      else if (-c > shift) shift = -c;
+      patches[patch_num].offset = (r * stride + c * 4);
+      patch_num++;
+    }
+  }
+  *max_shift = shift;
+  return patches;
+}
+
+// compute the channel-normed squared difference between two pixels
+static inline float pixel_difference(const float* const pix1, const float* pix2, const float norm[4])
+{
+  float dif1 = pix1[0] - pix2[0];
+  float dif2 = pix1[1] - pix2[1];
+  float dif3 = pix1[2] - pix2[2];
+  return (dif1 * dif1 * norm[0]) + (dif2 * dif2 * norm[1]) + (dif3 * dif3 * norm[2]);
+}
+
+static void init_column_sums(float *const col_sums, const patch_t *const patch, const float *const in,
+                             const int row, const int height, const int width, const int stride,
+                             const int radius, const float *const norm)
+{
+  const int srow = row + patch->rows;
+  const int scol = patch->cols;
+  const int col_min = MAX(0,-scol);        // columns left of this correspond to a patch column outside the RoI
+  const int col_max = width - MAX(0,scol); // columns right of this correspond to a patch column outside the RoI
+  const float *pixel = in + 4*col_min;
+  const float *shifted = pixel + patch->offset;
+  // Compute column sums from scratch.  Needed for the very first row, and at intervals thereafter
+  //   to limit accumulation of rounding errors
+  // adjust bounds if the patch extends past top/bottom of RoI
+  const int rmin = MAX(-radius,MAX(MIN(-srow,0),-row));
+  const int rmax = MIN(radius,MIN(height-srow,height-row)-1);
+  for (int col = 0; col < col_min; col++)
+    col_sums[col] = 0;
+  for (int col = col_min; col < col_max; col++, pixel+=4, shifted+=4)
+  {
+    float sum = 0;
+    for (int r = rmin; r <= rmax; r++)
+    {
+      sum += pixel_difference(pixel+r*stride,shifted+r*stride,norm);
+    }
+    col_sums[col] = sum;
+  }
+  // clear out any columns where the patch column would be outside the RoI, as well as our overrun area
+  for (int col = col_max; col < width + radius; col++)
+    col_sums[col] = 0;
+  return;
+}
+
+void nlmeans_denoise(const float *const inbuf, float *const outbuf,
+                     const dt_iop_roi_t *const roi_in, const dt_iop_roi_t *const roi_out,
+                     const dt_nlmeans_param_t *const params)
+{
+  // define the factors for applying blending between the original image and the denoised version
+  // if running in RGB space, 'luma' should equal 'chroma'
+  const float weight[4] = { params->luma, params->chroma, params->chroma, 1.0f };
+  const float invert[4] = { 1.0f - params->luma, 1.0f - params->chroma, 1.0f - params->chroma, 0.0f };
+
+  // define the patches to be compared when denoising a pixel
+  const size_t stride = 4 * roi_in->width;
+  int num_patches;
+  int max_shift;
+  struct patch_t* patches = define_patches(params,stride,&num_patches,&max_shift);
+  // allocate scratch space, including an overrun area on each end so we don't need a boundary check on every access
+  const int scratch_size = (roi_out->width + 2*params->patch_radius + 1) ;
+  const int padded_scratch_size = ((scratch_size+15)/16)*16; // round up to a full cache line
+  const int numthreads = dt_get_num_threads() ;
+  float *scratch_buf = dt_alloc_align(64,numthreads * padded_scratch_size * sizeof(float));
+  // zero out the overrun areas
+  memset(scratch_buf,'\0',numthreads*padded_scratch_size*sizeof(float));
+  const int chunk_size = (roi_out->height + numthreads - 1) / numthreads;
+#ifdef _OPENMP
+#pragma omp parallel for default(none) \
+      dt_omp_firstprivate(patches, num_patches, scratch_buf) \
+      schedule(static)
+#endif
+  for (int thr = 0 ; thr < numthreads; thr++)
+  {
+    // locate our scratch space within the big buffer allocated above
+    size_t tnum = dt_get_thread_num();
+    const int radius = params->patch_radius;
+    float *const col_sums = scratch_buf + tnum * padded_scratch_size + radius + 1;
+    // determine which horizontal slice of the image to process
+    const int chunk_start = thr * chunk_size;
+    const int chunk_end = MIN((thr+1)*chunk_size,roi_out->height);
+    // we want to incrementally sum results (especially weights in col[3]), so clear the output buffer to zeros
+    memset(outbuf+chunk_start*roi_out->width*4, '\0', roi_out->width * (chunk_end-chunk_start) * 4 * sizeof(float));
+    // cycle through all of the patches over our slice of the image
+    for (int p = 0; p < num_patches; p++)
+    {
+      // retrieve info about the current patch
+      const patch_t *patch = &patches[p];
+      const float sharpness = params->sharpness;
+      const int offset = patch->offset;
+      // skip any rows where the patch center would be above top of RoI or below bottom of RoI
+      const int height = roi_out->height;
+      int row_min = MAX(chunk_start,MAX(0,-patch->rows));
+      int row_max = MIN(chunk_end,height - MAX(0,patch->rows));
+      // skip any columns where the patch center would be to the left or the right of the RoI
+      const int scol = patch->cols;
+      const int col_min = MAX(0,-scol);
+      const int col_max = roi_out->width - MAX(0,scol);
+      init_column_sums(col_sums,patch,inbuf+stride*row_min,row_min,height,roi_out->width,stride,radius,params->norm);
+      for (int row = row_min; row < row_max; row++)
+      {
+        // add up the initial columns of the sliding window of total patch distortion
+        float distortion = 0.0;
+        for (int i = col_min - radius; i < col_min+radius; i++)
+          distortion += col_sums[i];
+        // now proceed down the current row of the image
+        const float *in = inbuf + stride * row + 4 * col_min;
+        float *out = outbuf + 4 * ((size_t)roi_out->width * row + col_min);
+        for (int col = col_min; col < col_max; col++, in+=4, out+=4)
+        {
+          distortion += (col_sums[col+radius] - col_sums[col-radius-1]);
+          float wt = gh(distortion, sharpness);
+          const float pixel[4] = { in[offset],  in[offset+1], in[offset+2], 1.0f };
+          SIMD_FOR (size_t c = 0; c < 4; c++)
+          {
+            out[c] += pixel[c] * wt;
+          }
+        }
+        const int row1 = row+1;
+        if (row1 < row_max)
+        {
+          const int srow = row1 + patch->rows;
+          const float *pixel = inbuf + row1*stride + 4*col_min;
+          const float *shifted = pixel + patch->offset;
+          // incrementally update the column sums
+          const float *shifted_top = shifted - (radius+1) * stride ;
+          const float *shifted_bot = shifted + radius * stride ;
+          if (srow <= radius || row1 <= radius)
+          {
+            // top edge of patch was above top of RoI, so it had a value of zero; just add in the new row
+            pixel += radius * stride;
+            for (int col = col_min; col < col_max; col++, pixel+=4, shifted_bot+=4)
+            {
+              col_sums[col] += pixel_difference(pixel,shifted_bot,params->norm);
+            }
+          }
+          else if (srow + radius >= height || row1 + radius >= height)
+          {
+            // new row of the patch is below the bottom of RoI, so its value is zero; just subtract the old row
+            pixel -= (radius+1) * stride;
+            for (int col = col_min; col < col_max; col++, pixel+=4, shifted_top+=4)
+            {
+              col_sums[col] -= pixel_difference(pixel,shifted_top,params->norm);
+            }
+          }
+          else
+          {
+            const float *pixel_top = pixel - (radius+1) * stride;
+            const float *pixel_bot = pixel + radius * stride;
+            // both prior and new positions are entirely within the RoI, so subtract the old row and add the new one
+            for (int col = col_min; col < col_max; col++, pixel_top+=4, pixel_bot+=4, shifted_top+=4, shifted_bot+=4)
+            {
+              col_sums[col] += (pixel_difference(pixel_bot,shifted_bot,params->norm)
+                                - pixel_difference(pixel_top,shifted_top,params->norm));
+            }
+          }
+        }
+      }
+    }
+    // normalize and apply chroma/luma blending
+    for (int row = chunk_start; row < chunk_end; row++)
+    {
+      const float *in = inbuf + row * stride;
+      float *out = outbuf + row * 4 * roi_out->width;
+      for (int col = 0; col < roi_out->width; col++, in+=4, out+=4)
+      {
+        SIMD_FOR(size_t c = 0; c < 4; c++)
+        {
+          out[c] = (in[c] * invert[c]) + (out[c] / out[3] * weight[c]);
+        }
+      }
+    }
+  }
+
+  // clean up: free the work space
+  dt_free_align(patches);
+  dt_free_align(scratch_buf);
+  return;
+}
---
+++ src/common/nlmeans_core.h
@@ -0,0 +1,38 @@
+#pragma once
+/*
+    This file is part of darktable,
+    Copyright (C) 2020 darktable developers.
+
+    darktable is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    darktable is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with darktable.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "iop/iop_api.h"
+
+struct dt_nlmeans_param_t
+{
+  float sharpness;	// relative weight of central pixel (preserves detail)
+  float luma;		// blend amount, L channel in Lab (set to 1.00 for RGB)
+  float chroma;		// blend amount, a/b channels (set to 1.00 for RBG)
+  float scattering;	// scattering factor for patches (default 0 = densest possible)
+  float scale;		// image scaling, affects scattering
+  int patch_radius;	// radius of patches which are compared, 1..4
+  int search_radius;	// radius around a pixel in which to compare patches (default = 7)
+  int decimate;         // set to 1 to search only half the patches in the neighborhood (default = 0)
+  const float* const norm; // array of four per-channel weight factors
+};
+typedef struct dt_nlmeans_param_t dt_nlmeans_param_t;
+
+void nlmeans_denoise(const float *const inbuf, float *const outbuf,
+                     const dt_iop_roi_t *const roi_in, const dt_iop_roi_t *const roi_out,
+                     const dt_nlmeans_param_t *const params);
