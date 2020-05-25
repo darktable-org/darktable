e6691f1ce54b029ca799ad6cddea7097bd4baf69
Diederik ter Rahe <dterrahe@yahoo.com>
2020-05-25 16:45:26 +0100

    [clahe.c] introspection, refactoring, janitorial

--- src/iop/clahe.c
+++ src/iop/clahe.c
@@ -253,7 +253,7 @@ void process(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, const 
 static void radius_callback(GtkWidget *slider, gpointer user_data)
 {
   dt_iop_module_t *self = (dt_iop_module_t *)user_data;
-  if(self->dt->gui->reset) return;
+  if(darktable.gui->reset) return;
   dt_iop_rlce_params_t *p = (dt_iop_rlce_params_t *)self->params;
   p->radius = dt_bauhaus_slider_get(slider);
   dt_dev_add_history_item(darktable.develop, self, TRUE);
@@ -262,7 +262,7 @@ static void radius_callback(GtkWidget *slider, gpointer user_data)
 static void slope_callback(GtkWidget *slider, gpointer user_data)
 {
   dt_iop_module_t *self = (dt_iop_module_t *)user_data;
-  if(self->dt->gui->reset) return;
+  if(darktable.gui->reset) return;
   dt_iop_rlce_params_t *p = (dt_iop_rlce_params_t *)self->params;
   p->slope = dt_bauhaus_slider_get(slider);
   dt_dev_add_history_item(darktable.develop, self, TRUE);
