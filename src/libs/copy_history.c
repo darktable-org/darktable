6a39c89ebb02c161017af34d198b5e5365d79e96
Hubert Kowalski <h.kowalski@hakger.pl>
2020-06-04 22:34:53 +0200

    introduce postponed_update for updating button statuses in copy_history

--- src/libs/copy_history.c
+++ src/libs/copy_history.c
@@ -45,7 +45,7 @@ typedef struct dt_lib_copy_history_t
   GtkWidget *copy_button, *delete_button, *load_button, *write_button;
   GtkWidget *copy_parts_button;
   GtkButton *compress_button;
-
+  guint timeout_handle;
 } dt_lib_copy_history_t;
 
 const char *name(dt_lib_module_t *self)
@@ -87,6 +87,22 @@ static void _update(dt_lib_module_t *self)
 
   gtk_widget_set_sensitive(GTK_WIDGET(d->paste), can_paste);
   gtk_widget_set_sensitive(GTK_WIDGET(d->paste_parts), can_paste);
+
+  if(d->timeout_handle)
+  {
+    g_source_remove(d->timeout_handle);
+    d->timeout_handle = 0;
+  }
+}
+
+static gboolean _postponed_update(gpointer data)
+{
+  dt_lib_module_t *self = (dt_lib_module_t *)data;
+
+  // timeout handle clearing is handled by update code
+  _update(self);
+
+  return FALSE;
 }
 
 static void write_button_clicked(GtkWidget *widget, dt_lib_module_t *self)
@@ -281,6 +297,20 @@ static void _collection_updated_callback(gpointer instance, dt_collection_change
   _update(self);
 }
 
+static void _mouse_over_image_callback(gpointer instance, dt_lib_module_t *self)
+{
+  dt_lib_copy_history_t *d = (dt_lib_copy_history_t *)self->data;
+  const int delay = CLAMP(darktable.develop->average_delay / 2, 10, 250);
+
+  if(d->timeout_handle)
+  {
+    // here we're making sure the event fires at last hover
+    // and we won't have avalanche of events in the mean time.
+    g_source_remove(d->timeout_handle);
+  }
+  d->timeout_handle = g_timeout_add(delay, _postponed_update, self);
+}
+
 void gui_reset(dt_lib_module_t *self)
 {
   _update(self);
@@ -296,6 +326,9 @@ void gui_init(dt_lib_module_t *self)
 {
   dt_lib_copy_history_t *d = (dt_lib_copy_history_t *)malloc(sizeof(dt_lib_copy_history_t));
   self->data = (void *)d;
+
+  d->timeout_handle = 0;
+
   self->widget = gtk_grid_new();
   GtkGrid *grid = GTK_GRID(self->widget);
   dt_gui_add_help_link(self->widget, dt_get_help_url(self->plugin_name));
@@ -368,6 +401,8 @@ void gui_init(dt_lib_module_t *self)
 
   dt_control_signal_connect(darktable.signals, DT_SIGNAL_SELECTION_CHANGED,
                             G_CALLBACK(_image_selection_changed_callback), self);
+  dt_control_signal_connect(darktable.signals, DT_SIGNAL_MOUSE_OVER_IMAGE_CHANGE,
+                            G_CALLBACK(_mouse_over_image_callback), self);
   dt_control_signal_connect(darktable.signals, DT_SIGNAL_COLLECTION_CHANGED,
                             G_CALLBACK(_collection_updated_callback), self);
 
@@ -386,6 +421,10 @@ void gui_init(dt_lib_module_t *self)
 
 void gui_cleanup(dt_lib_module_t *self)
 {
+  dt_lib_copy_history_t *d = (dt_lib_copy_history_t *)self->data;
+  if(d->timeout_handle)
+    g_source_remove(d->timeout_handle);
+
   free(self->data);
   self->data = NULL;
 }
