34779071e889bc76c77ddc8e51a89d85a5a06e32
hatsu <me@hatsunearu.xyz>
2020-06-05 03:26:04 -0400

    refactor & reorganize shutterspeed printing format

--- src/common/variables.c
+++ src/common/variables.c
@@ -204,29 +204,18 @@ static char *get_base_value(dt_variables_params_t *params, char **variable)
   }
   else if(has_prefix(variable, "EXIF_EXPOSURE"))
   {
-    /* no special chars for all jobs except infos */
+    /* use raw numbers for infos jobcode, else regular special character exposure str */
     if(g_strcmp0(params->jobcode, "infos") != 0)
+    {
       if(nearbyintf(params->data->exif_exposure) == params->data->exif_exposure)
         result = g_strdup_printf("%.0f", params->data->exif_exposure);
       else
         result = g_strdup_printf("%.1f", params->data->exif_exposure);
-    else if(params->data->exif_exposure >= 1.0f)
-      if(nearbyintf(params->data->exif_exposure) == params->data->exif_exposure)
-        result = g_strdup_printf("%.0f″", params->data->exif_exposure);
-      else
-        result = g_strdup_printf("%.1f″", params->data->exif_exposure);
-    /* want to catch everything below 0.3 seconds */
-    else if(params->data->exif_exposure < 0.29f)
-      result = g_strdup_printf("1/%.0f", 1.0 / params->data->exif_exposure);
-    /* catch 1/2, 1/3 */
-    else if(nearbyintf(1.0f / params->data->exif_exposure) == 1.0f / params->data->exif_exposure)
-      result = g_strdup_printf("1/%.0f", 1.0 / params->data->exif_exposure);
-    /* catch 1/1.3, 1/1.6, etc. */
-    else if(10 * nearbyintf(10.0f / params->data->exif_exposure)
-            == nearbyintf(100.0f / params->data->exif_exposure))
-      result = g_strdup_printf("1/%.1f", 1.0 / params->data->exif_exposure);
+    }
     else
-      result = g_strdup_printf("%.1f″", params->data->exif_exposure);
+    {
+      result = dt_format_exposure(params->data->exif_exposure);
+    }
   }
   else if(has_prefix(variable, "EXIF_APERTURE"))
     result = g_strdup_printf("%.1f", params->data->exif_aperture);
@@ -912,6 +901,35 @@ void dt_variables_reset_sequence(dt_variables_params_t *params)
 void dt_variables_set_tags_flags(dt_variables_params_t *params, uint32_t flags)
 {
   params->data->tags_flags = flags;
+}
+
+// format exposure time given in seconds to a string in a unified way
+char *dt_format_exposure(float exposuretime)
+{
+  char *result;
+  if(exposuretime >= 1.0f)
+  {
+    if(nearbyintf(exposuretime) == exposuretime)
+      result = g_strdup_printf("%.0f″", exposuretime);
+    else
+      result = g_strdup_printf("%.1f″", exposuretime);
+  }
+  /* want to catch everything below 0.3 seconds */
+  else if(exposuretime < 0.29f)
+    result = g_strdup_printf("1/%.0f", 1.0 / exposuretime);
+
+  /* catch 1/2, 1/3 */
+  else if(nearbyintf(1.0f / exposuretime) == 1.0f / exposuretime)
+    result = g_strdup_printf("1/%.0f", 1.0 / exposuretime);
+
+  /* catch 1/1.3, 1/1.6, etc. */
+  else if(10 * nearbyintf(10.0f / exposuretime) == nearbyintf(100.0f / exposuretime))
+    result = g_strdup_printf("1/%.1f", 1.0 / exposuretime);
+
+  else
+    result = g_strdup_printf("%.1f″", exposuretime);
+
+  return result;
 }
 
 // modelines: These editor modelines have been set for all relevant files by tools/update_modelines.sh
