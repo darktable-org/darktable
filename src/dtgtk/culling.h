8c7701f18f9ec956a7d0c4515dc13bf4732119e0
AlicVB <alic.vb@gmail.com>
2020-06-08 14:10:52 +0200

    thumb : allow to set tooltip visibility per size/mode (#5340)

--- src/dtgtk/culling.h
+++ src/dtgtk/culling.h
@@ -69,6 +69,7 @@ typedef struct dt_culling_t
 
   dt_thumbnail_overlay_t overlays; // overlays type
   int overlays_block_timeout;      // overlay block visibility duration
+  gboolean show_tooltips;          // are tooltips visible ?
 } dt_culling_t;
 
 dt_culling_t *dt_culling_new(dt_culling_mode_t mode);
