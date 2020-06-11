449cf9a4ffaee42e11da95ae2353413b3516302f
Mark-64 <marco.carrarini@gmail.com>
2020-06-11 07:50:37 +0200

    enable hover effect for directory icon

--- src/dtgtk/paint.c
+++ src/dtgtk/paint.c
@@ -944,7 +944,6 @@ void dtgtk_cairo_paint_directory(cairo_t *cr, gint x, gint y, gint w, gint h, gi
 {
   PREAMBLE(1, 0, 0);
 
-  cairo_set_source_rgb(cr, .8, .8, .8);
   cairo_rectangle(cr, 0., 0., 1., 1.);
   cairo_stroke(cr);
   cairo_move_to(cr, 0., .2);
