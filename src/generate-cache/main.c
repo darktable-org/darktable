75f636d75215efc21316321140a3f5441c150cc1
codingdave@gmail.com <David-Tillmann Schaefer>
2020-06-12 03:14:35 +0200

    fixes generate-cache and darktable-cli to work with relative pathes

--- src/generate-cache/main.c
+++ src/generate-cache/main.c
@@ -146,7 +146,30 @@ int main(int argc, char *arg[])
 #ifdef __APPLE__
   dt_osx_prepare_environment();
 #endif
-  bindtextdomain(GETTEXT_PACKAGE, DARKTABLE_LOCALEDIR);
+
+  // Assemble pathes
+  char* application_directory = NULL;
+  int dirname_length;
+  // calling wai_getExecutablePath twice as recommended in the docs:
+  // the first call retrieves the length of the path
+  int length = wai_getExecutablePath(NULL, 0, &dirname_length);
+  if (length > 0)
+  {
+    application_directory = (char*)malloc(length + 1);
+    // the second call retrieves the path including the executable
+    wai_getExecutablePath(application_directory, length, &dirname_length);
+    // strip of the executable name from the path to retrieve the path alone
+    application_directory[dirname_length] = '\0';
+  }
+  dt_print(DT_DEBUG_DEV, "application_directory: %s\n", application_directory);
+
+  // set up absolute pathes based on their relative value
+  dt_loc_init_localedir(application_directory, NULL);
+
+  char localedir[PATH_MAX] = { 0 };
+  dt_loc_get_localedir(localedir, sizeof(localedir));
+  bindtextdomain(GETTEXT_PACKAGE, localedir);
+
   bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
   textdomain(GETTEXT_PACKAGE);
 
