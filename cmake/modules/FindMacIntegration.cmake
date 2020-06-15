ed3092a81a812945cfed2ac9317e1cf3248207fd
David-Tillmann Schaefer <codingdave@gmail.com>
2020-06-15 08:51:07 +0200

    cmake: Silence some cmake 3.17 warnings

--- cmake/modules/FindMacIntegration.cmake
+++ cmake/modules/FindMacIntegration.cmake
@@ -1,32 +1,32 @@
 # - Find the native GtkOSXApplication includes and library
 #
 # This module defines
-#  MACINTEGRATION_INCLUDE_DIR, where to find gtkosxapplication.h, etc.
-#  MACINTEGRATION_LIBRARIES, the libraries to link against to use GtkOSXApplication.
-#  MACINTEGRATION_FOUND, If false, do not try to use GtkOSXApplication.
+#  MacIntegration_INCLUDE_DIR, where to find gtkosxapplication.h, etc.
+#  MacIntegration_LIBRARIES, the libraries to link against to use GtkOSXApplication.
+#  MacIntegration_FOUND, If false, do not try to use GtkOSXApplication.
 # also defined, but not for general use are
-#  MACINTEGRATION_LIBRARY, where to find the GtkOSXApplication library.
+#  MacIntegration_LIBRARY, where to find the GtkOSXApplication library.
 
 
 #=============================================================================
 # Copyright 2010 henrik andersson
 #=============================================================================
 
-SET(MACINTEGRATION_FIND_REQUIRED ${MacIntegration_FIND_REQUIRED})
+SET(MacIntegration_FIND_REQUIRED ${MacIntegration_FIND_REQUIRED})
 
-find_path(MACINTEGRATION_INCLUDE_DIR gtkosxapplication.h PATH_SUFFIXES gtkmacintegration gtkmacintegration-gtk3)
-mark_as_advanced(MACINTEGRATION_INCLUDE_DIR)
+find_path(MacIntegration_INCLUDE_DIR gtkosxapplication.h PATH_SUFFIXES gtkMacIntegration gtkMacIntegration-gtk3)
+mark_as_advanced(MacIntegration_INCLUDE_DIR)
 
-set(MACINTEGRATION_NAMES ${MACINTEGRATION_NAMES} gtkmacintegration libgtkmacintegration gtkmacintegration-gtk3 libgtkmacintegration-gtk3)
-find_library(MACINTEGRATION_LIBRARY NAMES ${MACINTEGRATION_NAMES})
-mark_as_advanced(MACINTEGRATION_LIBRARY)
+set(MacIntegration_NAMES ${MacIntegration_NAMES} gtkMacIntegration libgtkMacIntegration gtkMacIntegration-gtk3 libgtkMacIntegration-gtk3)
+find_library(MacIntegration_LIBRARY NAMES ${MacIntegration_NAMES})
+mark_as_advanced(MacIntegration_LIBRARY)
 
-# handle the QUIETLY and REQUIRED arguments and set MACINTEGRATION_FOUND to TRUE if
+# handle the QUIETLY and REQUIRED arguments and set MacIntegration_FOUND to TRUE if
 # all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(MACINTEGRATION DEFAULT_MSG MACINTEGRATION_LIBRARY MACINTEGRATION_INCLUDE_DIR)
+find_package_handle_standard_args(MacIntegration DEFAULT_MSG MacIntegration_LIBRARY MacIntegration_INCLUDE_DIR)
 
-IF(MACINTEGRATION_FOUND)
-  SET(MacIntegration_LIBRARIES ${MACINTEGRATION_LIBRARY})
-  SET(MacIntegration_INCLUDE_DIRS ${MACINTEGRATION_INCLUDE_DIR})
-ENDIF(MACINTEGRATION_FOUND)
+IF(MacIntegration_FOUND)
+  SET(MacIntegration_LIBRARIES ${MacIntegration_LIBRARY})
+  SET(MacIntegration_INCLUDE_DIRS ${MacIntegration_INCLUDE_DIR})
+ENDIF(MacIntegration_FOUND)
