1c6ea4dd4b4d2070d3605d8a0708a23a6d3dcd27
AlicVB <dev@lnaa.fr>
2020-06-25 00:02:11 +0200

    lib api : add fct to let lib manage its presets directly
    needed for iop_layouts

--- src/libs/ioporder.c
+++ src/libs/ioporder.c
@@ -198,12 +198,13 @@ void init_presets(dt_lib_module_t *self)
 
   list = dt_ioppr_get_iop_order_list_version(DT_IOP_ORDER_LEGACY);
   params = dt_ioppr_serialize_iop_order_list(list, &size);
-  dt_lib_presets_add(_("legacy"), self->plugin_name, self->version(), (const char *)params, (int32_t)size);
+  dt_lib_presets_add(_("legacy"), self->plugin_name, self->version(), (const char *)params, (int32_t)size, TRUE);
   free(params);
 
   list = dt_ioppr_get_iop_order_list_version(DT_IOP_ORDER_V30);
   params = dt_ioppr_serialize_iop_order_list(list, &size);
-  dt_lib_presets_add(_("v3.0 (default)"), self->plugin_name, self->version(), (const char *)params, (int32_t)size);
+  dt_lib_presets_add(_("v3.0 (default)"), self->plugin_name, self->version(), (const char *)params, (int32_t)size,
+                     TRUE);
   free(params);
 }
 
