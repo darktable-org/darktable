2952233989db96480de0965636209a7d67ef5c1c
AlicVB <dev@lnaa.fr>
2020-06-24 22:21:54 +0200

    iop_layout : adapt internal buttons handling

--- src/libs/modulegroups.c
+++ src/libs/modulegroups.c
@@ -47,7 +47,6 @@ typedef struct dt_lib_modulegroups_group_t
 typedef struct dt_lib_modulegroups_t
 {
   uint32_t current;
-  GtkWidget *buttons[DT_MODULEGROUP_SIZE];
   GtkWidget *text_entry;
   GtkWidget *hbox_buttons;
   GtkWidget *active_btn;
@@ -125,25 +124,13 @@ int position()
   return 999;
 }
 
-static int _iop_get_group_order(const int group_id, const int default_order)
+static GtkWidget *_buttons_get_from_pos(dt_lib_module_t *self, const int pos)
 {
-  if (group_id < DT_MODULEGROUP_BASIC)
-    return group_id;
-
-  gchar *key = dt_util_dstrcat(NULL, "plugins/darkroom/group_order/%d", group_id - 1);
-  int prefs = dt_conf_get_int(key);
-
-  /* if zero, not found, record it */
-  if (!prefs)
-  {
-    dt_conf_set_int(key, default_order - 1);
-    prefs = default_order;
-  }
-  else
-    prefs += 1;
-
-  g_free(key);
-  return prefs<1 ? 1 : (prefs>DT_MODULEGROUP_SIZE? DT_MODULEGROUP_SIZE: prefs);
+  dt_lib_modulegroups_t *d = (dt_lib_modulegroups_t *)self->data;
+  if(pos == 0) return d->active_btn;
+  dt_lib_modulegroups_group_t *gr = (dt_lib_modulegroups_group_t *)g_list_nth_data(d->groups, pos - 1);
+  if(gr) return gr->button;
+  return NULL;
 }
 
 static dt_lib_modulegroup_iop_visibility_type_t _get_search_iop_visibility()
@@ -311,7 +298,7 @@ void gui_init(dt_lib_module_t *self)
   gtk_box_pack_start(GTK_BOX(self->widget), d->hbox_buttons, TRUE, TRUE, 0);
   gtk_box_pack_start(GTK_BOX(self->widget), d->hbox_search_box, TRUE, TRUE, 0);
 
-  gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(d->buttons[d->current]), TRUE);
+  gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(d->active_btn), TRUE);
   if(d->current == DT_MODULEGROUP_NONE) _lib_modulegroups_update_iop_visibility(self);
   gtk_widget_show_all(self->widget);
   gtk_widget_show_all(d->hbox_buttons);
@@ -390,7 +377,7 @@ static gboolean _lib_modulegroups_test_internal(dt_lib_module_t *self, uint32_t 
 
 static gboolean _lib_modulegroups_test(dt_lib_module_t *self, uint32_t group, uint32_t iop_group)
 {
-  return _lib_modulegroups_test_internal(self, _iop_get_group_order(group, group), iop_group);
+  return _lib_modulegroups_test_internal(self, group, iop_group);
 }
 
 static void _lib_modulegroups_update_iop_visibility(dt_lib_module_t *self)
@@ -408,16 +395,19 @@ static void _lib_modulegroups_update_iop_visibility(dt_lib_module_t *self)
   /* only show module group as selected if not currently searching */
   if(visibility != DT_MODULEGROUP_SEARCH_IOP_TEXT_VISIBLE && d->current != DT_MODULEGROUP_NONE)
   {
-    const int cb = _lib_modulegroups_get(self);
-    /* toggle button visibility without executing callback */
-    g_signal_handlers_block_matched(d->buttons[cb], G_SIGNAL_MATCH_FUNC, 0, 0, NULL, _lib_modulegroups_toggle, NULL);
+    GtkWidget *bt = _buttons_get_from_pos(self, d->current);
+    if(bt)
+    {
+      /* toggle button visibility without executing callback */
+      g_signal_handlers_block_matched(bt, G_SIGNAL_MATCH_FUNC, 0, 0, NULL, _lib_modulegroups_toggle, NULL);
 
-    if(text_entered && text_entered[0] != '\0')
-      gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(d->buttons[cb]), FALSE);
-    else
-      gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(d->buttons[cb]), TRUE);
+      if(text_entered && text_entered[0] != '\0')
+        gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(bt), FALSE);
+      else
+        gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(bt), TRUE);
 
-    g_signal_handlers_unblock_matched(d->buttons[cb], G_SIGNAL_MATCH_FUNC, 0, 0, NULL, _lib_modulegroups_toggle, NULL);
+      g_signal_handlers_unblock_matched(bt, G_SIGNAL_MATCH_FUNC, 0, 0, NULL, _lib_modulegroups_toggle, NULL);
+    }
   }
 
   GList *modules = darktable.develop->iop;
@@ -566,22 +556,18 @@ static void _lib_modulegroups_toggle(GtkWidget *button, gpointer user_data)
                                   : NULL;
 
   /* block all button callbacks */
-  for(int k = 0; k < DT_MODULEGROUP_SIZE; k++)
-    g_signal_handlers_block_matched(d->buttons[k], G_SIGNAL_MATCH_FUNC, 0, 0, NULL, _lib_modulegroups_toggle,
-                                    NULL);
+  for(int k = 0; k <= g_list_length(d->groups); k++)
+    g_signal_handlers_block_matched(_buttons_get_from_pos(self, k), G_SIGNAL_MATCH_FUNC, 0, 0, NULL,
+                                    _lib_modulegroups_toggle, NULL);
 
   /* deactivate all buttons */
-  uint32_t cb = 0;
   int gid = 0;
-  for(int k = 0; k < DT_MODULEGROUP_SIZE; k++)
+  for(int k = 0; k <= g_list_length(d->groups); k++)
   {
+    const GtkWidget *bt = _buttons_get_from_pos(self, k);
     /* store toggled modulegroup */
-    if(d->buttons[k] == button)
-    {
-      cb = k;
-      gid = _iop_get_group_order(k, k);
-    }
-    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(d->buttons[k]), FALSE);
+    if(bt == button) gid = k;
+    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(bt), FALSE);
   }
 
   /* only deselect button if not currently searching else re-enable module */
@@ -590,12 +576,12 @@ static void _lib_modulegroups_toggle(GtkWidget *button, gpointer user_data)
   else
   {
     d->current = gid;
-    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(d->buttons[cb]), TRUE);
+    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(_buttons_get_from_pos(self, gid)), TRUE);
   }
 
   /* unblock all button callbacks */
-  for(int k = 0; k < DT_MODULEGROUP_SIZE; k++)
-    g_signal_handlers_unblock_matched(d->buttons[k], G_SIGNAL_MATCH_FUNC, 0, 0, NULL,
+  for(int k = 0; k <= g_list_length(d->groups); k++)
+    g_signal_handlers_unblock_matched(_buttons_get_from_pos(self, k), G_SIGNAL_MATCH_FUNC, 0, 0, NULL,
                                       _lib_modulegroups_toggle, NULL);
 
   /* clear search text */
@@ -620,11 +606,9 @@ static gboolean _lib_modulegroups_set_gui_thread(gpointer user_data)
 {
   _set_gui_thread_t *params = (_set_gui_thread_t *)user_data;
 
-  dt_lib_modulegroups_t *d = (dt_lib_modulegroups_t *)params->self->data;
-
   /* set current group and update visibility */
-  if(params->group < DT_MODULEGROUP_SIZE && GTK_IS_TOGGLE_BUTTON(d->buttons[params->group]))
-    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(d->buttons[params->group]), TRUE);
+  GtkWidget *bt = _buttons_get_from_pos(params->self, params->group);
+  if(bt) gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(bt), TRUE);
   _lib_modulegroups_update_iop_visibility(params->self);
 
   free(params);
@@ -702,13 +686,7 @@ static void _lib_modulegroups_switch_group(dt_lib_module_t *self, dt_iop_module_
 static uint32_t _lib_modulegroups_get(dt_lib_module_t *self)
 {
   dt_lib_modulegroups_t *d = (dt_lib_modulegroups_t *)self->data;
-
-  for(int k = 0; k < DT_MODULEGROUP_SIZE; k++)
-  {
-    if (d->current == _iop_get_group_order(k, k))
-      return k;
-  }
-  return DT_MODULEGROUP_NONE;
+  return d->current;
 }
 
 static gchar *_preset_to_string(GList *groups)
@@ -793,7 +771,19 @@ int set_params(dt_lib_module_t *self, const void *params, int size)
   if(!params) return 1;
 
   dt_lib_modulegroups_t *d = (dt_lib_modulegroups_t *)self->data;
-  // TODO : cleanup existing groups
+
+  // cleanup existing groups
+  GList *l = d->groups;
+  while(l)
+  {
+    dt_lib_modulegroups_group_t *gr = (dt_lib_modulegroups_group_t *)l->data;
+    g_free(gr->name);
+    g_free(gr->icon);
+    g_list_free_full(gr->modules, g_free);
+    l = g_list_next(l);
+  }
+  g_list_free_full(d->groups, g_free);
+
   d->groups = _preset_from_string((char *)params);
   _buttons_update(self);
   return 0;
