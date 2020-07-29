2e26557f9c727a697312358a2e4aeebe4944f46c
bobobo1618 <bobobo1618@users.noreply.github.com>
2020-07-29 18:25:45 +0200

    Add docs for running multiple instances of darktable-cli (#5736)

--- src/cli/main.c
+++ src/cli/main.c
@@ -68,7 +68,8 @@ static void usage(const char *progname)
   fprintf(stderr, "   --export_masks <0|1|false|true>, default: false\n");
   fprintf(stderr, "   --style <style name>\n");
   fprintf(stderr, "   --style-overwrite\n");
-  fprintf(stderr, "   --apply-custom-presets <0|1|false|true>, default: true\n");
+  fprintf(stderr, "   --apply-custom-presets <0|1|false|true>, default: true\n"
+  fprintf(stderr, "                          disable for multiple instances\n");
   fprintf(stderr, "   --verbose\n");
   fprintf(stderr, "   --help,-h\n");
   fprintf(stderr, "   --version\n");
