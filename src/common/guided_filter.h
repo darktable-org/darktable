92401e6f53f4bc7dd9cf1771cfd3f1a8a7a6883f
ralfbrown <ralfbrown@users.noreply.github.com>
2020-08-23 21:38:44 -0400

    make tile size more easily configurable
    Also adds code to dynamically tweak the tile size if it would leave a sliver in the last row or column.
    However, this did not result in any speedup greater than the timing uncertainty, so it is left disabled
    as it also causes a handfull of pixels to round off differently.

--- src/common/guided_filter.h
+++ src/common/guided_filter.h
@@ -75,7 +75,16 @@ static inline float Kahan_sum(const float m, float *c, const float add)
    return t2;
 }
 
-
+#ifdef __SSE2__
+// vectorized Kahan summation algorithm
+static inline __m128 Kahan_sum_sse(const __m128 m, __m128 *c, const __m128 add)
+{
+   const __m128 t1 = add - (*c);
+   const __m128 t2 = m + t1;
+   *c = (t2 - m) - t1;
+   return t2;
+}
+#endif /* __SSE2__ */
 
 void guided_filter(const float *guide, const float *in, float *out, int width, int height, int ch, int w,
                    float sqrt_eps, float guide_weight, float min, float max);
