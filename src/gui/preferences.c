40a2c93b5832f724af413489d2a2f351ca957660
hanno@schwalm-bremen.de <hanno@schwalm-bremen.de>
2020-10-01 20:27:04 +0200

    Simplyfied approach for `screen_ppd_overwrite` and `ui/cairo_filter`
    The PPD (pixels-per-dot) value is read by default from OS setting resulting in values
    of either 1.0f or 2.0f for normal / high-resolution monitors. Or you could specify it via
    the general->preferences.
    
    The tooltip here suggested to increase the value for better visual output regarding thumbs & previews,
    or decrease it for better performance. In fact, the first suggestion is not true any more since
    the thumbnail size is calculated correctly. The second suggestion (decreasing) indeed leads to
    increased performance but also lead to blurring in gtk/bauhaus elements. (To test this on a normal
    monitor, set it to 0.5f, go to darkroom and look at the iops)
    
    Another option was the hidden `ui/cairo_filter` option. It was introduced a while ago for more 'crisp'
    display of thumbs & previews when in hires mode, this is not necessary any more.
    
    **But** both above ways would be fine for slow computers to have faster thumb&preview rendering.
    
    What has been done in this pr?
    
    1) Both config options have been removed and were replaced by a drop down menu selecting between
       system(default), standard monitor(ppd==1) and hires monitor(ppd==2). For both specified monitor
       types we can select between normal vs. slow computer mode.
       "default"
       "standard monitor"
       "standard monitor, (slow)"
       "high resolution monitor"
       "high resolution monitor, (slow)"
    
    2) As we want the thumbs to be displayed with different ppd than the GUI ppd we add `ppd_thb` to
       `dt_gui_gtk_t`, this is used in all thumbs drawing code recently introduced when fixing the hires
       impaired output.
    
    3) When selecting one of non-slow modes ppd_thb is the same as ppd and rendering is cairo_good,
       if we use one of the slow modes we select cairo_fast and a smaller ppd_thb to be smaller.
       This results in an overall > 4-fold better performance for the slow modes.

--- src/gui/preferences.c
+++ src/gui/preferences.c
@@ -208,12 +208,20 @@ static void font_size_changed_callback(GtkWidget *widget, gpointer user_data)
   dt_bauhaus_load_theme();
 }
 
-static void gui_scaling_changed_callback(GtkWidget *widget, gpointer user_data)
+static void display_callback(GtkWidget *widget, gpointer user_data)
 {
-  float ppd = gtk_spin_button_get_value(GTK_SPIN_BUTTON(widget));
-  if(ppd > 0.0) ppd = fmax(0.5, ppd); // else <= 0 -> use system default
-  dt_conf_set_float("screen_ppd_overwrite", ppd);
-  restart_required = TRUE;
+  int old = dt_conf_get_int("ui/monitor");
+  const int selected = gtk_combo_box_get_active(GTK_COMBO_BOX(widget));
+  dt_conf_set_int("ui/monitor", selected);
+  gboolean restart = TRUE;
+
+  if(((old == 1) && (selected == 2)) ||
+     ((old == 2) && (selected == 1)) ||
+     ((old == 3) && (selected == 4)) ||
+     ((old == 4) && (selected == 3)) ||
+      (old == selected))
+    restart = FALSE;
+  restart_required = restart;
   dt_configure_ppd_dpi(darktable.gui);
   dt_bauhaus_load_theme();
 }
@@ -392,6 +400,28 @@ static void init_tab_general(GtkWidget *dialog, GtkWidget *stack, dt_gui_themetw
   g_signal_connect(G_OBJECT(widget), "changed", G_CALLBACK(theme_callback), 0);
   gtk_widget_set_tooltip_text(widget, _("set the theme for the user interface"));
 
+  label = gtk_label_new(_("display"));
+  gtk_widget_set_halign(label, GTK_ALIGN_START);
+  widget = gtk_combo_box_text_new();
+  labelev = gtk_event_box_new();
+  gtk_widget_add_events(labelev, GDK_BUTTON_PRESS_MASK);
+  gtk_container_add(GTK_CONTAINER(labelev), label);
+  gtk_grid_attach(GTK_GRID(grid), labelev, 0, line++, 1, 1);
+  gtk_grid_attach_next_to(GTK_GRID(grid), widget, labelev, GTK_POS_RIGHT, 1, 1);
+
+  gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(widget), "default");
+  gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(widget), "standard monitor");
+  gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(widget), "standard monitor, (slow)");
+  gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(widget), "high resolution monitor");
+  gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(widget), "high resolution monitor, (slow)");
+
+  selected = dt_conf_get_int("ui/monitor");
+  gtk_combo_box_set_active(GTK_COMBO_BOX(widget), selected);
+  g_signal_connect(G_OBJECT(widget), "changed", G_CALLBACK(display_callback), 0);
+  gtk_widget_set_tooltip_text(widget, _("set according to your display type and computer performance.\n"
+                                        "slow modes increase performance but decrease visual quality of thumbs and previews."
+                                        "this needs a restart to apply changes."));
+
   //Font size check and spin buttons
   GtkWidget *usesysfont = gtk_check_button_new();
   GtkWidget *fontsize = gtk_spin_button_new_with_range(5.0f, 30.0f, 0.2f);
@@ -428,22 +458,6 @@ static void init_tab_general(GtkWidget *dialog, GtkWidget *stack, dt_gui_themetw
   gtk_widget_set_tooltip_text(fontsize, _("font size in points"));
   gtk_spin_button_set_value(GTK_SPIN_BUTTON(fontsize), dt_conf_get_float("font_size"));
   g_signal_connect(G_OBJECT(fontsize), "value_changed", G_CALLBACK(font_size_changed_callback), 0);
-
-  GtkWidget *screen_ppd_overwrite = gtk_spin_button_new_with_range(-1.0f, 8.0f, 0.2f);
-  label = gtk_label_new(_("GUI thumbs and previews DPI scaling factor"));
-  gtk_widget_set_halign(label, GTK_ALIGN_START);
-  labelev = gtk_event_box_new();
-  gtk_widget_add_events(labelev, GDK_BUTTON_PRESS_MASK);
-  gtk_container_add(GTK_CONTAINER(labelev), label);
-  gtk_grid_attach(GTK_GRID(grid), labelev, 0, line++, 1, 1);
-  gtk_grid_attach_next_to(GTK_GRID(grid), screen_ppd_overwrite, labelev, GTK_POS_RIGHT, 1, 1);
-  gtk_widget_set_tooltip_text(screen_ppd_overwrite, _("scale the thumbnails and previews resolutions for high DPI screens.\n"
-                                                      "increase if thumbnails look blurry, decrease if lighttable is too slow.\n"
-                                                      "set to -1.0 to use the system-defined global scaling.\n"
-                                                      "default is 1.0 on most systems, or 2.0 when using resolutions above 1920×1080 px.\n"
-                                                      "this needs a restart to apply changes."));
-  gtk_spin_button_set_value(GTK_SPIN_BUTTON(screen_ppd_overwrite), dt_conf_get_float("screen_ppd_overwrite"));
-  g_signal_connect(G_OBJECT(screen_ppd_overwrite), "value_changed", G_CALLBACK(gui_scaling_changed_callback), 0);
 
   GtkWidget *screen_dpi_overwrite = gtk_spin_button_new_with_range(-1.0f, 360, 1.f);
   label = gtk_label_new(_("GUI controls and text DPI"));
