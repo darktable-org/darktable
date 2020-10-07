4bcd3e8d4862ec3614d52b2eb52fd80c60c22e5b
hanno@schwalm-bremen.de <hanno@schwalm-bremen.de>
2020-10-07 19:29:59 +0200

    Remove some irrelevant debug info

--- src/common/camera_control.c
+++ src/common/camera_control.c
@@ -1009,9 +1009,7 @@ void dt_camctl_import(const dt_camctl_t *c, const dt_camera_t *cam, GList *image
 {
   _camctl_lock(c, cam);
   const gboolean sdcard = !strncmp(c->active_camera->port, "disk:", 5);
-  if(sdcard)
-    dt_print(DT_DEBUG_CAMCTL, "[dt_camctl_import] started an import session from `%s`\n", c->active_camera->port);
-
+    
   GList *ifile = g_list_first(images);
   if(ifile) do
   {
