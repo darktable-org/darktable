d7737b56df362d98e36b6c0b2299ca43a1204ace
David-Tillmann Schaefer <codingdave@gmail.com>
2020-10-12 09:48:03 +0200

    renames REL_BIN_TO_LIB to REL_BIN_TO_LIBDIR

--- src/config.cmake.h
+++ src/config.cmake.h
@@ -19,7 +19,7 @@ static const char *dt_supported_extensions[] __attribute__((unused)) = {"@DT_SUP
 #define GETTEXT_PACKAGE "darktable"
 
 #cmakedefine DARKTABLE_LOCALEDIR "@REL_BIN_TO_LOCALEDIR@"
-#cmakedefine DARKTABLE_LIBDIR    "@REL_BIN_TO_LIB@"
+#cmakedefine DARKTABLE_LIBDIR    "@REL_BIN_TO_LIBDIR@"
 #cmakedefine DARKTABLE_DATADIR   "@REL_BIN_TO_DATADIR@"
 #cmakedefine DARKTABLE_SHAREDIR  "@REL_BIN_TO_DATADIR@"
 
