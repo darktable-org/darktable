6db0afdf26229b85086ac83f43d3bfc0354143be
David-Tillmann Schaefer <codingdave@gmail.com>
2020-10-20 01:26:49 +0200

    do not override CMAKE_BUILD_TYPE, GENERATOR, VERBOSE, KEEPGOING if already set

--- .ci/ci-script.sh
+++ .ci/ci-script.sh
@@ -27,8 +27,21 @@
 
 set -exv
 
+if [[ -z "$CMAKE_BUILD_TYPE" ]]; then
+  CMAKE_BUILD_TYPE="RelWithDebInfo"
+fi;
+
+if [[ -z "$GENERATOR" ]]; then
+  GENERATOR="Ninja"
+fi;
+
+if [[ -z "$VERBOSE" ]]; then
 VERBOSE="-v"
+fi;
+
+if [[ -z "$KEEPGOING" ]]; then
 KEEPGOING="-k0"
+fi;
 
 if [ "$GENERATOR" = "Unix Makefiles" ];
 then
