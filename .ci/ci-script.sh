c2fd67b25f910c5fe4140bfbc7c291c8e5a1225d
David-Tillmann Schaefer <codingdave@gmail.com>
2020-10-20 03:09:30 +0200

    removes quotes to enable keepgoing to not fail compilation if it is empty

--- .ci/ci-script.sh
+++ .ci/ci-script.sh
@@ -67,12 +67,12 @@ target_build()
 {
   echo "target_build"
   # to get as much of the issues into the log as possible
-  cmake --build "$BUILD_DIR" $MAKEFLAGS || cmake --build "$BUILD_DIR" -j1 "$VERBOSE" "$KEEPGOING"
+  cmake --build "$BUILD_DIR" $MAKEFLAGS || cmake --build "$BUILD_DIR" -j1 "$VERBOSE" $KEEPGOING
 
   ctest --output-on-failure || ctest --rerun-failed -V -VV
 
   # and now check that it installs where told and only there.
-  cmake --build "$BUILD_DIR" --target install $MAKEFLAGS || cmake --build "$BUILD_DIR" --target install -j1 "$VERBOSE" "$KEEPGOING"
+  cmake --build "$BUILD_DIR" --target install $MAKEFLAGS || cmake --build "$BUILD_DIR" --target install -j1 "$VERBOSE" $KEEPGOING
 }
 
 target_notest()
@@ -80,10 +80,10 @@ target_notest()
   echo "target_notest"
 
   # to get as much of the issues into the log as possible
-  cmake --build "$BUILD_DIR" $MAKEFLAGS || cmake --build "$BUILD_DIR" -j1 "$VERBOSE" "$KEEPGOING"
+  cmake --build "$BUILD_DIR" $MAKEFLAGS || cmake --build "$BUILD_DIR" -j1 "$VERBOSE" $KEEPGOING
 
   # and now check that it installs where told and only there.
-  cmake --build "$BUILD_DIR" --target install $MAKEFLAGS || cmake --build "$BUILD_DIR" --target install -j1 "$VERBOSE" "$KEEPGOING"
+  cmake --build "$BUILD_DIR" --target install $MAKEFLAGS || cmake --build "$BUILD_DIR" --target install -j1 "$VERBOSE" $KEEPGOING
 }
 
 target_usermanual()
