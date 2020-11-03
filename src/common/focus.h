5ab3ef21fedcd356dd4fdc802f248930232e3c6c
hanno@schwalm-bremen.de <hanno@schwalm-bremen.de>
2020-11-03 20:58:41 +0100

    Fix focus display for preview_downsampling
    This is not so much a ppd_thb issue but more of a remaining preview_downsampling issue

--- src/common/focus.h
+++ src/common/focus.h
@@ -279,7 +279,8 @@ static void dt_focus_draw_clusters(cairo_t *cr, int width, int height, int imgid
   }
 
   const int32_t tb = darktable.develop->border_size;
-  const float scale = fminf((width - 2 * tb) / (float)wd, (height - 2 * tb) / (float)ht) * full_zoom;
+  const float prev_scale = darktable.develop->preview_downsampling;
+  const float scale = fminf((width - 2 * tb) / (float)wd, (height - 2 * tb) / (float)ht) * full_zoom / prev_scale;
   cairo_scale(cr, scale, scale);
   float fx = 0.0f;
   float fy = 0.0f;
@@ -294,7 +295,7 @@ static void dt_focus_draw_clusters(cairo_t *cr, int width, int height, int imgid
     if(ht * scale <= height) fy = 0;
   }
 
-  cairo_translate(cr, -wd / 2.0f + fx / scale * darktable.gui->ppd_thb, -ht / 2.0f + fy / scale * darktable.gui->ppd_thb);
+  cairo_translate(cr, -wd / 2.0f * prev_scale + fx / scale * darktable.gui->ppd_thb, -ht / 2.0f * prev_scale + fy / scale * darktable.gui->ppd_thb);
 
   cairo_rectangle(cr, 0, 0, wd, ht);
   cairo_clip(cr);
