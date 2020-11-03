0c010d81564478843e19630ee680f0fe1335f6ac
Pascal Obry <pascal@obry.net>
2020-11-03 14:24:26 +0100

    integration driver: rework the run script driver for tests.
    It is now possible to run all tests, a single one or a list of
    tests passed on the command line.
    
    The environment variable DARKTABLE_CLI can be used to specify the
    full pathname of darktable-cli tool.
    
    Also, two new options are added:
    
    1. --disable-opencl : do not run the OpenCL test.
    2. --no-deltae      : do not use Delta-E Python module for diff.
    
    When in no-deltae mode a simple diff is done using the compare script.
    This is not a precise check but it is faster and does not require the
    Delta-E Python support module. This mode is mostly expected to be used
    by the CI runners.

--- src/tests/integration/run.sh
+++ src/tests/integration/run.sh
@@ -1,21 +1,60 @@
 #!/bin/bash
 
+# To run darktable-cli must be found, either
+#
+#   1. Put darktable-cli in the PATH
+#   2. Set DARKTABLE_CLI to the full pathname of darktable-cli executable
+#
+# To run the test suite:
+#
+#   ./run.sh               - will run all tests
+#   ./run.sh 0001-exposure - will run the single tests 0001-exposure
+#
+# Options:
+#
+#   --disable-opencl   - do not run the OpenCL path
+#   --no-deltae        - do a light check not requiring Delta-E module
+
 CDPATH=
 
-PATTERN="[0-9]*"
-
-[ ! -z $1 ] && PATTERN="$(basename $1)"
-
-CLI=darktable-cli
+CLI=${DARKTABLE_CLI:-darktable-cli}
 TEST_IMAGES=$PWD/images
 
+TESTS=""
 TEST_COUNT=0
 TEST_ERROR=0
 COMPARE=$(which compare)
+DO_OPENCL=yes
+DO_DELTAE=yes
 
 [ -z $(which $CLI) ] && echo Make sure $CLI is in the path && exit 1
 
-for dir in $(ls -d $PATTERN); do
+set -- $(getopt -q -u -o : -l disable-opencl,no-deltae -- $*)
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        --disable-opencl)
+            DO_OPENCL=no
+            ;;
+        --no-deltae)
+            DO_DELTAE=no
+            ;;
+        (--)
+            ;;
+        (*)
+            TESTS="$TESTS $(basename $1)"
+            ;;
+        (-*)
+            echo "$0: error - unrecognized option $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+[ -z "$TESTS" ] && TESTS="$(ls -d [0-9]*)"
+
+for dir in $TESTS; do
     echo Test $dir
     TEST_COUNT=$((TEST_COUNT + 1))
 
@@ -78,17 +117,19 @@ for dir in $(ls -d $PATTERN); do
 
             res=$?
 
-            $CLI --width 2048 --height 2048 \
-                 --hq true --apply-custom-presets false \
-                 "$TEST_IMAGES/$IMAGE" "$TEST.xmp" output-cl.png \
-                 --core $CORE_OPTIONS 1> /dev/null 2> /dev/null
+            if [ $DO_OPENCL == yes ]; then
+                $CLI --width 2048 --height 2048 \
+                     --hq true --apply-custom-presets false \
+                     "$TEST_IMAGES/$IMAGE" "$TEST.xmp" output-cl.png \
+                     --core $CORE_OPTIONS 1> /dev/null 2> /dev/null
 
-            res=$((res + $?))
+                res=$((res + $?))
+            fi
 
             # If all ok, check Delta-E
 
             if [ $res -eq 0 ]; then
-                if [ ! -z $COMPARE ]; then
+                if [ ! -z "$COMPARE" -a $DO_OPENCL == yes ]; then
                     diffcount="$(compare output.png output-cl.png -metric ae diff-cl.png 2>&1 )"
 
                     if [ $? -ne 0 ]; then
@@ -96,30 +137,45 @@ for dir in $(ls -d $PATTERN); do
                     fi
                 fi
 
-                if [ -f expected.png ]; then
-                    ../deltae expected.png output.png
+                if [ $DO_DELTAE == yes ]; then
+                    if [ -f expected.png ]; then
+                        ../deltae expected.png output.png
+                    else
+                        false
+                    fi
+
+                    res=$?
+
+                    if [ $res -lt 2 ]; then
+                        echo "  OK"
+                        if [ $res = 1 ]; then
+                            diffcount="$(compare expected.png output.png -metric ae diff-ok.png 2>&1 )"
+                        fi
+                        res=0
+
+                    else
+                        echo "  FAILS: image visually changed"
+                        if [ ! -z $COMPARE -a -f expected.png ]; then
+                            diffcount="$(compare expected.png output.png -metric ae diff.png 2>&1 )"
+                            echo "         see diff.png for visual difference"
+			    echo "         (${diffcount} pixels changed)"
+                        fi
+                    fi
                 else
-                    false
-                fi
-
-                res=$?
-
-                if [ $res -lt 2 ]; then
-                    echo "  OK"
-                    if [ $res = 1 ]; then
+                    if [ -z $COMPARE ]; then
+                        echo "no delta-e mode : required compare tool not found."
+                        res=1
+                    else
                         diffcount="$(compare expected.png output.png -metric ae diff-ok.png 2>&1 )"
-                    fi
-                    res=0
-
-                else
-                    echo "  FAILS: image visually changed"
-                    if [ ! -z $COMPARE -a -f expected.png ]; then
-                        diffcount="$(compare expected.png output.png -metric ae diff.png 2>&1 )"
-                        echo "         see diff.png for visual difference"
-			echo "         (${diffcount} pixels changed)"
+                        if [ $diffcount -lt 2000 ]; then
+                            echo "      Light check : OK"
+                            res=0
+                        else
+                            echo "      Light check : NOK"
+                            res=1
+                        fi
                     fi
                 fi
-
             else
                 echo "  FAILS : darktable-cli errored"
             fi
