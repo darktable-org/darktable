92c5c183b3b801335ce60d06cbe8b2d50bdd009e
Aurélien PIERRE <aurelien@aurelienpierre.com>
2020-11-06 22:28:26 +0100

    fix #6766, handle input color profile correctly

--- src/develop/format.h
+++ src/develop/format.h
@@ -60,10 +60,7 @@ typedef struct dt_iop_buffer_dsc_t
 
   /** colorspace of the image */
   int cst;
-  /** work profile info of the image */
-  struct dt_iop_order_iccprofile_info_t *work_profile_info;
-  /** input profile info **/
-  struct dt_iop_order_iccprofile_info_t *input_profile_info;
+
 } dt_iop_buffer_dsc_t;
 
 size_t dt_iop_buffer_dsc_to_bpp(const struct dt_iop_buffer_dsc_t *dsc);
