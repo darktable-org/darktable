7c34a7a7fe4618a38396cfb11ff484d2781241ab
rawfiner <rawfiner@gmail.com>
2020-11-08 11:35:15 +0100

    add new module cacorrectrgb based on useless.c
    no functionnality yet, code is taken from useless.c

---
+++ src/iop/cacorrectrgb.c
@@ -0,0 +1,153 @@
+/*
+    This file is part of darktable,
+    Copyright (C) 2010-2020 darktable developers.
+
+    darktable is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    darktable is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with darktable.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+// our includes go first:
+#include "bauhaus/bauhaus.h"
+#include "develop/imageop.h"
+#include "develop/imageop_gui.h"
+#include "gui/color_picker_proxy.h"
+#include "gui/gtk.h"
+#include "iop/iop_api.h"
+
+#include <gtk/gtk.h>
+#include <stdlib.h>
+
+// This is an example implementation of an image operation module that does nothing useful.
+// It demonstrates how the different functions work together. To build your own module,
+// take all of the functions that are mandatory, stripping them of comments.
+// Then add only the optional functions that are required to implement the functionality
+// you need. Don't copy default implementations (hint: if you don't need to change or add
+// anything, you probably don't need the copy). Make sure you choose descriptive names
+// for your fields and variables. The ones given here are just examples; rename them.
+//
+// To have your module compile and appear in darkroom, add it to CMakeLists.txt, with
+//  add_iop(useless "useless.c")
+// and to iop_order.c, in the initialisation of legacy_order & v30_order with:
+//  { {XX.0f }, "useless", 0},
+
+// This is the version of the module's parameters,
+// and includes version information about compile-time dt.
+// The first released version should be 1.
+DT_MODULE_INTROSPECTION(1, dt_iop_cacorrectrgb_params_t)
+
+// TODO: some build system to support dt-less compilation and translation!
+
+// Enums used in params_t can have $DESCRIPTIONs that will be used to
+// automatically populate a combobox with dt_bauhaus_combobox_from_params.
+// They are also used in the history changes tooltip.
+// Combobox options will be presented in the same order as defined here.
+// These numbers must not be changed when a new version is introduced.
+typedef enum dt_iop_cacorrectrgb_guide_channel_t
+{
+  DT_CACORRECT_RGB_R = 0,    // $DESCRIPTION: "red"
+  DT_CACORRECT_RGB_G = 1,    // $DESCRIPTION: "green"
+  DT_CACORRECT_RGB_B = 2     // $DESCRIPTION: "blue"
+} dt_iop_cacorrectrgb_guide_channel_t;
+
+typedef struct dt_iop_cacorrectrgb_params_t
+{
+  dt_iop_cacorrectrgb_guide_channel_t guide_channel; // $DEFAULT: DT_CACORRECT_RGB_G $DESCRIPTION: "guide"
+  int radius; // $MIN: 1 $MAX: 20 $DEFAULT: 1 $DESCRIPTION: "radius"
+} dt_iop_cacorrectrgb_params_t;
+
+typedef struct dt_iop_cacorrectrgb_gui_data_t
+{
+  GtkWidget *guide_channel, *radius;
+} dt_iop_cacorrectrgb_gui_data_t;
+
+// this returns a translatable name
+const char *name()
+{
+  // make sure you put all your translatable strings into _() !
+  return _("chromatic aberrations rgb");
+}
+
+// some additional flags (self explanatory i think):
+int flags()
+{
+  return IOP_FLAGS_INCLUDE_IN_STYLES | IOP_FLAGS_SUPPORTS_BLENDING;
+}
+
+// where does it appear in the gui?
+int default_group()
+{
+  return IOP_GROUP_CORRECT | IOP_GROUP_TECHNICAL;
+}
+
+int default_colorspace(dt_iop_module_t *self, dt_dev_pixelpipe_t *pipe, dt_dev_pixelpipe_iop_t *piece)
+{
+  return iop_cs_rgb;
+}
+
+void commit_params(dt_iop_module_t *self, dt_iop_params_t *p1, dt_dev_pixelpipe_t *pipe, dt_dev_pixelpipe_iop_t *piece)
+{
+  memcpy(piece->data, p1, self->params_size);
+}
+
+/** process, all real work is done here. */
+void process(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, const void *const ivoid, void *const ovoid,
+             const dt_iop_roi_t *const roi_in, const dt_iop_roi_t *const roi_out)
+{
+  // this is called for preview and full pipe separately, each with its own pixelpipe piece.
+  // get our data struct:
+  //dt_iop_cacorrectrgb_params_t *d = (dt_iop_cacorrectrgb_params_t *)piece->data;
+  // the total scale is composed of scale before input to the pipeline (iscale),
+  // and the scale of the roi.
+  //const float scale = piece->iscale / roi_in->scale;
+  // how many colors in our buffer?
+  const int ch = piece->colors;
+
+  memcpy(ovoid, ivoid, sizeof(float) * ch * roi_out->height * roi_in->width);
+}
+
+/** gui setup and update, these are needed. */
+void gui_update(dt_iop_module_t *self)
+{
+  dt_iop_cacorrectrgb_gui_data_t *g = (dt_iop_cacorrectrgb_gui_data_t *)self->gui_data;
+  dt_iop_cacorrectrgb_params_t *p = (dt_iop_cacorrectrgb_params_t *)self->params;
+
+  dt_bauhaus_combobox_set_from_value(g->guide_channel, p->guide_channel);
+  dt_bauhaus_slider_set(g->radius, p->radius);
+}
+
+/** optional: if this exists, it will be called to init new defaults if a new image is
+ * loaded from film strip mode. */
+void reload_defaults(dt_iop_module_t *module)
+{
+  dt_iop_cacorrectrgb_params_t *d = (dt_iop_cacorrectrgb_params_t *)module->default_params;
+
+  d->guide_channel = DT_CACORRECT_RGB_G;
+  d->radius = 1;
+
+  dt_iop_cacorrectrgb_gui_data_t *g = (dt_iop_cacorrectrgb_gui_data_t *)module->gui_data;
+  if(g)
+  {
+    dt_bauhaus_combobox_set_default(g->guide_channel, d->guide_channel);
+    dt_bauhaus_slider_set_default(g->radius, d->radius);
+  }
+}
+
+void gui_init(dt_iop_module_t *self)
+{
+  dt_iop_cacorrectrgb_gui_data_t *g = IOP_GUI_ALLOC(cacorrectrgb);
+  self->widget = gtk_box_new(GTK_ORIENTATION_VERTICAL, DT_BAUHAUS_SPACE);
+  g->guide_channel = dt_bauhaus_combobox_from_params(self, "guide_channel");
+  g->radius = dt_bauhaus_slider_from_params(self, "radius");
+}
