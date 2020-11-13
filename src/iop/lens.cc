7d74b57340af3245fba310f5dd2b7c178443612e
Aurélien PIERRE <aurelienpierre@users.noreply.github.com>
2020-11-13 18:10:44 +0100

    lens.cc : add aliases

--- src/iop/lens.cc
+++ src/iop/lens.cc
@@ -147,6 +147,11 @@ const char *name()
 {
   return _("lens correction");
 }
+    
+const char *aliases()
+{
+  return _("vignette|chromatic aberrations|distortion");
+}
 
 const char *description(struct dt_iop_module_t *self)
 {
