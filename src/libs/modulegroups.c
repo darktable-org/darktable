darktable-org/darktable

commit fd1f1c5ddd24a2ee8d754318c21e43fd3c7e9c1f

modulegroups: set current group when entering darkroom
resolves issue whereby, when exiting darkroom with "active" group
selected, reentering darkroom changed the group to "none" (i.e. show
all)

src/libs/modulegroups.c
@@ -220,6 +220,9 @@ void view_enter(dt_lib_module_t *self, dt_view_t *old_view, dt_view_t *new_view)
     if(!dt_lib_presets_apply(preset, self->plugin_name, self->version()))
       dt_lib_presets_apply(_(FALLBACK_PRESET_NAME), self->plugin_name, self->version());
     g_free(preset);
+
+    //and set the current group
+    d->current = dt_conf_get_int("plugins/darkroom/groups");
   }
 }
 

commit e765c61dfc29b6aa2152901b1aad0b0ec3879556

modulegroups: fix regression caused by #6889

src/libs/modulegroups.c
@@ -342,6 +342,7 @@ void gui_init(dt_lib_module_t *self)
   gtk_box_pack_start(GTK_BOX(self->widget), d->hbox_search_box, TRUE, TRUE, 0);
 
   gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(d->active_btn), TRUE);
+  d->current = dt_conf_get_int("plugins/darkroom/groups");
   if(d->current == DT_MODULEGROUP_NONE) _lib_modulegroups_update_iop_visibility(self);
   gtk_widget_show_all(self->widget);
   gtk_widget_show_all(d->hbox_buttons);