fc5de3c66fab157d996c60c1282c5f287e109b4c
Aurélien PIERRE <aurelienpierre@users.noreply.github.com>
2020-11-20 03:29:12 +0100

    filmicrgb.c : be more gently on noise default

--- src/iop/filmicrgb.c
+++ src/iop/filmicrgb.c
@@ -173,7 +173,7 @@ typedef struct dt_iop_filmicrgb_params_t
   float contrast;           // $MIN: 0 $MAX: 5 $DEFAULT: 1.35
   float saturation;         // $MIN: -50 $MAX: 200 $DEFAULT: 10 $DESCRIPTION: "extreme luminance saturation"
   float balance;            // $MIN: -50 $MAX: 50 $DEFAULT: 0.0 $DESCRIPTION: "shadows/highlights balance"
-  float noise_level;        // $MIN: 0.0 $MAX: 6.0 $DEFAULT: 0.5f $DESCRIPTION: "add noise in highlights"
+  float noise_level;        // $MIN: 0.0 $MAX: 6.0 $DEFAULT: 0.2f $DESCRIPTION: "add noise in highlights"
   dt_iop_filmicrgb_methods_type_t preserve_color; // $DEFAULT: DT_FILMIC_METHOD_POWER_NORM $DESCRIPTION: "preserve chrominance"
   dt_iop_filmicrgb_colorscience_type_t version; // $DEFAULT: DT_FILMIC_COLORSCIENCE_V2 $DESCRIPTION: "color science"
   gboolean auto_hardness;                       // $DEFAULT: TRUE $DESCRIPTION: "auto adjust hardness"
