2e82e986270d974ec6c8b8c4675b3e8aff681192
Aurélien PIERRE <aurelien@aurelienpierre.com>
2020-11-29 23:15:52 +0100

    DNG: fix white balance coeffs and embedded matrices
    For DNG files from known cameras, colorin defaults to the embedded matrix which is good. However, the D65 WB coeffs are computed using the standard Adobe matrice, which is inconsistent. A set of WB coeff is tied to a particular matrice.
    
    For DNG files from unknown cameras, colorin defaults to the embedded profile too. But the D65 coeffs cannot be computed at all, since there is no Adobe standard matrix.
    
    This changes the behaviour to always use the embedded DNG matrix if any. It makes white balance and color calibration modules support properly DNG files for default_params.

--- src/iop/demosaic.c
+++ src/iop/demosaic.c
@@ -4926,7 +4926,7 @@ void commit_params(struct dt_iop_module_t *self, dt_iop_params_t *params, dt_dev
 
     // Get and store the matrix to go from camera to RGB for 4Bayer images
     char *camera = self->dev->image_storage.camera_makermodel;
-    if (!dt_colorspaces_conversion_matrices_rgb(camera, NULL, d->CAM_to_RGB, NULL))
+    if (!dt_colorspaces_conversion_matrices_rgb(camera, NULL, d->CAM_to_RGB, self->dev->image_storage.d65_color_matrix, NULL))
     {
       fprintf(stderr, "[colorspaces] `%s' color matrix not found for 4bayer image!\n", camera);
       dt_control_log(_("`%s' color matrix not found for 4bayer image!"), camera);
