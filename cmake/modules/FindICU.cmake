4d10a8bb7cb85c12b9e4fa067f00cfdf2329b8bc
phweyland <philippe.weyland@laposte.net>
2020-12-10 17:04:38 -0300

    collect: set insensitive pref as enum; integrate sqliteicu interface to libicui18n.so

--- cmake/modules/FindICU.cmake
+++ cmake/modules/FindICU.cmake
@@ -3,28 +3,27 @@
 #
 
 # This module defines
-# ICU_INCLUDE_DIR, where to find *.h etc
-# ICU_LIBRARY, the libraries
+# ICU_LIBRARIES, the libraries
 # ICU_FOUND, If false, do not try to use ICU.
 
 include(LibFindMacros)
 
 # Use pkg-config to get hints about paths
-libfind_pkg_check_modules(ICU_PKGCONF ICU)
+libfind_pkg_check_modules(ICU_PKGCONF icu-i18n)
 
-# Find the library
+# Find the libraries
 find_library(ICU_LIBRARY
-  NAMES libicu.so
+  NAMES icui18n
   HINTS ${ICU_PKGCONF_LIBRARY_DIRS}
 )
 
 if(ICU_LIBRARY)
-  # Set the libraries and let libfind_process do the rest.
-  # NOTE: Singular variables for this library, plural for libraries this lib depends on.
   set(ICU_PROCESS_LIBS ${ICU_LIBRARY})
   libfind_process(ICU)
 endif(ICU_LIBRARY)
 
-if(NOT ICU_FOUND)
+if(ICU_FOUND)
+  set(ICU_LIBRARIES ${ICU_LIBRARY})
+else()
   message(STATUS "ICU not found")
-endif(NOT ICU_FOUND)
+endif(ICU_FOUND)
