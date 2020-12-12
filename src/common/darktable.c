6a39bade412953d81e8af6f84f51754c9e9157c3
hanno@schwalm-bremen.de <hanno@schwalm-bremen.de>
2020-12-12 17:21:28 +0100

    Integrate low_quality_thumbnails preference into ui/performance
    As described in #7158 there is the hidden preference setting 'low_quality_thumbnails'
    which has been set by 'dt_configure_performance' while installing dt.
    
    It visually changes the output of thumbnails (&previews) for slow machines but the
    reason for the decreased quality is not obvious for the user.
    
    As we alrready have the option 'performance versus quality' and the tooltip tells the user
    what the implication is i think we can remove that preference and use the 'ui/performance'
    setting to change the behaviour of 'dt_imageio_export_with_flags'.
    
    Fixes #7158

--- src/common/darktable.c
+++ src/common/darktable.c
@@ -1464,7 +1464,6 @@ void dt_configure_performance()
     dt_conf_set_int("singlebuffer_limit", MAX(16, dt_conf_get_int("singlebuffer_limit")));
     if(demosaic_quality == NULL || !strcmp(demosaic_quality, "always bilinear (fast)"))
       dt_conf_set_string("plugins/darkroom/demosaic/quality", "at most PPG (reasonable)");
-    dt_conf_set_bool("plugins/lighttable/low_quality_thumbnails", FALSE);
     dt_conf_set_bool("ui/performance", FALSE);
   }
   else if(mem > (2lu << 20) && threads >= 4 && atom_cores == 0)
@@ -1478,7 +1477,6 @@ void dt_configure_performance()
     dt_conf_set_int("singlebuffer_limit", MAX(16, dt_conf_get_int("singlebuffer_limit")));
     if(demosaic_quality == NULL ||!strcmp(demosaic_quality, "always bilinear (fast)"))
       dt_conf_set_string("plugins/darkroom/demosaic/quality", "at most PPG (reasonable)");
-    dt_conf_set_bool("plugins/lighttable/low_quality_thumbnails", FALSE);
     dt_conf_set_bool("ui/performance", FALSE);
   }
   else if(mem < (1lu << 20) || threads <= 2 || atom_cores > 0)
@@ -1490,7 +1488,6 @@ void dt_configure_performance()
     dt_conf_set_int("host_memory_limit", 500);
     dt_conf_set_int("singlebuffer_limit", 8);
     dt_conf_set_string("plugins/darkroom/demosaic/quality", "always bilinear (fast)");
-    dt_conf_set_bool("plugins/lighttable/low_quality_thumbnails", TRUE);
     dt_conf_set_bool("ui/performance", TRUE);
   }
   else
@@ -1502,7 +1499,6 @@ void dt_configure_performance()
     dt_conf_set_int("host_memory_limit", 1500);
     dt_conf_set_int("singlebuffer_limit", 16);
     dt_conf_set_string("plugins/darkroom/demosaic/quality", "at most PPG (reasonable)");
-    dt_conf_set_bool("plugins/lighttable/low_quality_thumbnails", FALSE);
     dt_conf_set_bool("ui/performance", FALSE);
   }
 
