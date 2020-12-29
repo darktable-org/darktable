82638aba57a99936adb4968a88d5dc7ebf07e02a
ralfbrown <ralfbrown@users.noreply.github.com>
2020-12-29 16:08:22 -0500

    add dt_iop_gui_data_t to .cc files, correct calls to dt_iop_alloc_image_buffers

--- src/iop/tonemap.cc
+++ src/iop/tonemap.cc
@@ -61,6 +61,7 @@ typedef struct dt_iop_tonemapping_params_t
 
 typedef struct dt_iop_tonemapping_gui_data_t
 {
+  struct dt_iop_gui_data_t common; // contains required fields: lock, warning_label
   GtkWidget *contrast, *Fsize;
 } dt_iop_tonemapping_gui_data_t;
 
