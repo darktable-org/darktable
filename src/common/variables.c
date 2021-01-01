d51d1d0ee7cb9fd4834fb091080d97597bbf366f
Diederik ter Rahe <dterrahe@yahoo.com>
2021-01-01 12:10:52 +0000

    only create warning widget when needed + red markup

--- src/common/variables.c
+++ src/common/variables.c
@@ -383,7 +383,9 @@ static char *get_base_value(dt_variables_params_t *params, char **variable)
         break;
     }
   }
-  else if(has_prefix(variable, "LABELS_ICONS") && g_strcmp0(params->jobcode, "infos") == 0)
+  else if((has_prefix(variable, "LABELS_ICONS") ||
+           has_prefix(variable, "LABELS_COLORICONS"))
+          && g_strcmp0(params->jobcode, "infos") == 0)
   {
     escape = FALSE;
     GList *res = dt_metadata_get(params->imgid, "Xmp.darktable.colorlabels", NULL);
@@ -392,82 +394,10 @@ static char *get_base_value(dt_variables_params_t *params, char **variable)
     {
       do
       {
-        const char *lb = (char *)(dt_colorlabels_to_string(GPOINTER_TO_INT(res->data)));
-        if(g_strcmp0(lb, "red") == 0)
-        {
-          const GdkRGBA c = darktable.bauhaus->colorlabels[DT_COLORLABELS_RED];
-          result = dt_util_dstrcat(result,
-                                   "<span foreground=\"#%02x%02x%02x\">⬤ </span>",
-                                   R(c), G(c), B(c));
-        }
-        else if(g_strcmp0(lb, "yellow") == 0)
-        {
-          const GdkRGBA c = darktable.bauhaus->colorlabels[DT_COLORLABELS_YELLOW];
-          result = dt_util_dstrcat(result, "<span foreground=\"#%02x%02x%02X\">⬤ </span>",
-                                   R(c), G(c), B(c));
-        }
-        else if(g_strcmp0(lb, "green") == 0)
-        {
-          const GdkRGBA c = darktable.bauhaus->colorlabels[DT_COLORLABELS_GREEN];
-          result = dt_util_dstrcat(result, "<span foreground=\"#%02x%02x%02X\">⬤ </span>",
-                                   R(c), G(c), B(c));
-        }
-        else if(g_strcmp0(lb, "blue") == 0)
-        {
-          const GdkRGBA c = darktable.bauhaus->colorlabels[DT_COLORLABELS_BLUE];
-          result = dt_util_dstrcat(result, "<span foreground=\"#%02x%02x%02X\">⬤ </span>",
-                                   R(c), G(c), B(c));
-        }
-        else if(g_strcmp0(lb, "purple") == 0)
-        {
-          const GdkRGBA c = darktable.bauhaus->colorlabels[DT_COLORLABELS_PURPLE];
-          result = dt_util_dstrcat(result, "<span foreground=\"#%02x%02x%02X\">⬤ </span>",
-                                   R(c), G(c), B(c));
-        }
-      } while((res = g_list_next(res)) != NULL);
-    }
-    g_list_free(res);
-  }
-  else if(has_prefix(variable, "LABELS_COLORICONS") && g_strcmp0(params->jobcode, "infos") == 0)
-  {
-    escape = FALSE;
-    GList *res = dt_metadata_get(params->imgid, "Xmp.darktable.colorlabels", NULL);
-    res = g_list_first(res);
-    if(res != NULL)
-    {
-      do
-      {
-        const char *lb = (char *)(dt_colorlabels_to_string(GPOINTER_TO_INT(res->data)));
-        if(g_strcmp0(lb, "red") == 0)
-        {
-          const GdkRGBA c = darktable.bauhaus->colorlabels[DT_COLORLABELS_RED];
-          result = dt_util_dstrcat(result, "<span foreground=\"#%02x%02x%02x\">🔴 </span>",
-                                   R(c), G(c), B(c));
-        }
-        else if(g_strcmp0(lb, "yellow") == 0)
-        {
-          const GdkRGBA c = darktable.bauhaus->colorlabels[DT_COLORLABELS_YELLOW];
-          result = dt_util_dstrcat(result, "<span foreground=\"#%02x%02x%02X\">🟡 </span>",
-                                   R(c), G(c), B(c));
-        }
-        else if(g_strcmp0(lb, "green") == 0)
-        {
-          const GdkRGBA c = darktable.bauhaus->colorlabels[DT_COLORLABELS_GREEN];
-          result = dt_util_dstrcat(result, "<span foreground=\"#%02x%02x%02X\">🟢 </span>",
-                                   R(c), G(c), B(c));
-        }
-        else if(g_strcmp0(lb, "blue") == 0)
-        {
-          const GdkRGBA c = darktable.bauhaus->colorlabels[DT_COLORLABELS_BLUE];
-          result = dt_util_dstrcat(result, "<span foreground=\"#%02x%02x%02X\">🔵 </span>",
-                                   R(c), G(c), B(c));
-        }
-        else if(g_strcmp0(lb, "purple") == 0)
-        {
-          const GdkRGBA c = darktable.bauhaus->colorlabels[DT_COLORLABELS_PURPLE];
-          result = dt_util_dstrcat(result, "<span foreground=\"#%02x%02x%02X\">🟣 </span>",
-                                   R(c), G(c), B(c));
-        }
+        const GdkRGBA c = darktable.bauhaus->colorlabels[GPOINTER_TO_INT(res->data)];
+        result = dt_util_dstrcat(result,
+                                  "<span foreground='#%02x%02x%02x'>⬤ </span>",
+                                  R(c), G(c), B(c));
       } while((res = g_list_next(res)) != NULL);
     }
     g_list_free(res);
