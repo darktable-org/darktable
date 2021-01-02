41d1eeee7aeaecf9185bbd7838e34d8ff97b0c28
Hubert Kowalski <h.kowalski@hakger.pl>
2021-01-02 15:21:28 +0100

    [masks] beautify hinter, add moar toasts

--- src/develop/masks/circle.c
+++ src/develop/masks/circle.c
@@ -84,6 +84,7 @@ static int dt_circle_events_mouse_scrolled(struct dt_iop_module_t *module, float
         dt_conf_set_float("plugins/darkroom/spots/circle_border", masks_border);
       else
         dt_conf_set_float("plugins/darkroom/masks/circle/border", masks_border);
+      dt_toast_log(_("feather size: %3.2f%%"), masks_border*100.0f);
     }
     else if(state == 0)
     {
@@ -103,6 +104,7 @@ static int dt_circle_events_mouse_scrolled(struct dt_iop_module_t *module, float
         dt_conf_set_float("plugins/darkroom/spots/circle_size", masks_size);
       else
         dt_conf_set_float("plugins/darkroom/masks/circle/size", masks_size);
+      dt_toast_log(_("size: %3.2f%%"), masks_size*100.0f);
     }
     return 1;
   }
@@ -139,6 +141,7 @@ static int dt_circle_events_mouse_scrolled(struct dt_iop_module_t *module, float
           dt_conf_set_float("plugins/darkroom/spots/circle_border", circle->border);
         else
           dt_conf_set_float("plugins/darkroom/masks/circle/border", circle->border);
+        dt_toast_log(_("feather size: %3.2f%%"), circle->border*100.0f);
       }
       else if(gui->edit_mode == DT_MASKS_EDIT_FULL)
       {
@@ -155,6 +158,7 @@ static int dt_circle_events_mouse_scrolled(struct dt_iop_module_t *module, float
           dt_conf_set_float("plugins/darkroom/spots/circle_size", circle->radius);
         else
           dt_conf_set_float("plugins/darkroom/masks/circle/size", circle->radius);
+        dt_toast_log(_("size: %3.2f%%"), circle->radius*100.0f);
       }
       else
       {
