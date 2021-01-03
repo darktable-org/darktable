ecd056cec951d95f64ca905563f0444b712d88c5
phweyland <philippe.weyland@laposte.net>
2021-01-03 11:26:09 -0300

    add preferences to presets menu in metadata info, metadata editor and export selected

--- src/gui/gtk.h
+++ src/gui/gtk.h
@@ -365,6 +365,7 @@ static inline GtkWidget *dt_ui_label_new(const gchar *str)
 {
   GtkWidget *label = gtk_label_new(str);
   gtk_widget_set_halign(label, GTK_ALIGN_START);
+  gtk_label_set_xalign (GTK_LABEL(label), 0.0f);
   gtk_label_set_ellipsize(GTK_LABEL(label), PANGO_ELLIPSIZE_END);
   return label;
 };
