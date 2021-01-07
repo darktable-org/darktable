111dc8fb699daed938e2ed5af6ec8e96467492be
Hubert Kowalski <h.kowalski@hakger.pl>
2021-01-07 20:26:55 +0100

    [threadsafety] localtime is not threadsafe, use localtime_r

--- src/libs/metadata_view.c
+++ src/libs/metadata_view.c
@@ -294,8 +294,9 @@ static void _metadata_update_tooltip(const int i, const char *tooltip, dt_lib_mo
 static void _metadata_update_timestamp(const int i, const time_t *value, dt_lib_module_t *self)
 {
   char datetime[200];
+  struct tm tm_val;
   // just %c is too long and includes a time zone that we don't know from exif
-  const size_t datetime_len = strftime(datetime, sizeof(datetime), "%a %x %X", localtime(value));
+  const size_t datetime_len = strftime(datetime, sizeof(datetime), "%a %x %X", localtime_r(value, &tm_val));
   if(datetime_len > 0)
   {
     const gboolean valid_utf = g_utf8_validate(datetime, datetime_len, NULL);
