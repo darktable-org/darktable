e9e8a82b6757c697ef201a58aec9d18e9c20356b
Pascal Obry <pascal@obry.net>
2021-01-08 22:41:56 +0100

    liquify: fix hit point sensitivity on HiDPI sreeen.

--- src/iop/liquify.c
+++ src/iop/liquify.c
@@ -2360,7 +2360,7 @@ static dt_liquify_hit_t _hit_paths(dt_iop_module_t *module,
     }
   }
 
-  if(distance < 15)
+  if(distance < DT_PIXEL_APPLY_DPI(15))
     return hit;
   else
     return NOWHERE;
