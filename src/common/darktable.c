c199b975715896166db0a80a2911e255e8717322
Hanno Schwalm <hanno@schwalm-bremen.de>
2021-01-12 16:53:36 +0100

    Debug option -d demosaic (#7796)
    * Adding debugging option -d demosaic
    
    As there is continuing work on improving the demosaicer status it seems to be
    better having a debugging option that can be switched on by a user for reporting
    instead of the compile-time option we have so far.
    
    * Make color smoothing menu introspection supported
    
    Also the -d demosaic option tells what parateters are passed. This seems to be not necessary,
    will help for more complicated stuff. BTW it allows to reproduce reported issues.

--- src/common/darktable.c
+++ src/common/darktable.c
@@ -120,7 +120,7 @@ static int usage(const char *argv0)
   printf("  --conf <key>=<value>\n");
   printf("  --configdir <user config directory>\n");
   printf("  -d {all,cache,camctl,camsupport,control,dev,fswatch,imageio,input,\n");
-  printf("      ioporder,lighttable,lua,masks,memory,nan,opencl,params,perf,\n");
+  printf("      ioporder,lighttable,lua,masks,memory,nan,opencl,params,perf,demosaic\n");
   printf("      pwstorage,print,signal,sql,undo}\n");
   printf("  --d-signal <signal> \n");
   printf("  --d-signal-act <all,raise,connect,disconnect");
@@ -616,6 +616,8 @@ int dt_init(int argc, char *argv[], const gboolean init_gui, const gboolean load
           darktable.unmuted |= DT_DEBUG_SIGNAL; // signal information on console
         else if(!strcmp(argv[k + 1], "params"))
           darktable.unmuted |= DT_DEBUG_PARAMS; // iop module params checks on console
+        else if(!strcmp(argv[k + 1], "demosaic"))
+          darktable.unmuted |= DT_DEBUG_DEMOSAIC;
         else
           return usage(argv[0]);
         k++;
