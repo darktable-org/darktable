eff1b47da60992c364720b59cf2d085eaedeae96
Pascal Obry <pascal@obry.net>
2021-01-20 18:52:30 +0100

    Fix some glib critical messages for blending modules having no masks.
    (darktable:1003798): Gtk-CRITICAL **: 18:46:31.257: gtk_toggle_button_set_active: assertion 'GTK_IS_TOGGLE_BUTTON (toggle_button)' failed
    
    The retouch & spot removal modules have no blending masks support.
    Activating one of the blending mode was displaying a critial message
    on the console as trying to deactivate the edit_masks button which
    is not created for such modules.

--- src/develop/masks/masks.c
+++ src/develop/masks/masks.c
@@ -1992,8 +1992,9 @@ void dt_masks_set_edit_mode(struct dt_iop_module_t *module, dt_masks_edit_mode_t
   else
     dt_dev_masks_selection_change(darktable.develop, 0, FALSE);
 
-  gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(bd->masks_edit),
-                               value == DT_MASKS_EDIT_OFF ? FALSE : TRUE);
+  if(bd->masks_support)
+    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(bd->masks_edit),
+                                 value == DT_MASKS_EDIT_OFF ? FALSE : TRUE);
 
   dt_control_queue_redraw_center();
 }
