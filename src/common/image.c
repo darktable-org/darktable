darktable-org/darktable

commit f41cbba6df6f673c5a949a5ddd84d5af8cee1af8

geotagging on map
allow to
- preview on map the different gpx tracks
- select meaningful tracks
- tune images datetime / offset to match some accurate position on tracks
- apply geo positions from gpx file
also
- zoom in/out keeping stable the location under the mouse (more or less)

src/common/image.c
@@ -572,8 +572,6 @@ void dt_image_set_locations(const GList *img, const dt_image_geoloc_t *geoloc, c
     }
 
     DT_DEBUG_CONTROL_SIGNAL_RAISE(darktable.signals, DT_SIGNAL_MOUSE_OVER_IMAGE_CHANGE);
-    // imgs is freed by the destinee
-    DT_DEBUG_CONTROL_SIGNAL_RAISE(darktable.signals, DT_SIGNAL_GEOTAG_CHANGED, imgs, 0);
   }
 }
 

src/libs/image.c
@@ -347,6 +347,8 @@ static void _execute_metadata(dt_lib_module_t *self, const int action)
       else
         dt_image_get_location(imageid, geoloc);
       dt_image_set_locations(imgs, geoloc, TRUE);
+      DT_DEBUG_CONTROL_SIGNAL_RAISE(darktable.signals, DT_SIGNAL_GEOTAG_CHANGED,
+                                    g_list_copy((GList *)imgs), 0);
       g_free(geoloc);
     }
     if(dttag_flag)

data/darktableconfig.xml.in
@@ -2570,6 +2570,13 @@
     <longdescription>show the name of the module of each basic widget</longdescription>
   </dtconfig>
   <dtconfig>
+    <name>plugins/lighttable/geotagging/heighttracklist</name>
+    <type>int</type>
+    <default>50</default>
+    <shortdescription>height of the track list</shortdescription>
+    <longdescription>maximum height the track list will grow to before scrolling</longdescription>
+  </dtconfig>
+  <dtconfig>
     <name>plugins/map/geotagging_search_url</name>
     <type>string</type>
     <default>https://nominatim.openstreetmap.org/search/%s?format=xml&amp;limit=%d&amp;polygon_text=1</default>

data/themes/darktable.css
@@ -887,6 +887,12 @@ dialog scrolledwindow
   font-style: italic;
 }
 
+#right #lib-plugin-ui #gpx_list checkbutton check
+{
+  margin: 1px;
+  padding: 0px;
+}
+
 /* Set import from camera or card dialog window */
 dialog .dialog-vbox notebook stack /* set top margin ; between tabs and text below */
 {

src/common/geo.h
@@ -23,7 +23,8 @@ typedef enum dt_geo_map_display_t
   MAP_DISPLAY_NONE,
   MAP_DISPLAY_POINT,
   MAP_DISPLAY_TRACK,
-  MAP_DISPLAY_POLYGON
+  MAP_DISPLAY_POLYGON,
+  MAP_DISPLAY_THUMB
 } dt_geo_map_display_t;
 
 typedef struct dt_geo_map_display_point_t

src/common/gpx.c
@@ -16,15 +16,10 @@
     along with darktable.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "common/gpx.h"
+#include "common/geo.h"
 #include "common/darktable.h"
 #include <glib.h>
 #include <inttypes.h>
-
-typedef struct _gpx_track_point_t
-{
-  gdouble longitude, latitude, elevation;
-  GTimeVal time;
-} _gpx_track_point_t;
 
 /* GPX XML parser */
 typedef enum _gpx_parser_element_t
@@ -32,20 +27,23 @@ typedef enum _gpx_parser_element_t
   GPX_PARSER_ELEMENT_NONE = 0,
   GPX_PARSER_ELEMENT_TRKPT = 1 << 0,
   GPX_PARSER_ELEMENT_TIME = 1 << 1,
-  GPX_PARSER_ELEMENT_ELE = 1 << 2
+  GPX_PARSER_ELEMENT_ELE = 1 << 2,
+  GPX_PARSER_ELEMENT_NAME = 1 << 3
 } _gpx_parser_element_t;
 
 typedef struct dt_gpx_t
 {
   /* the list of track records parsed */
-  GList *track;
+  GList *trkpts;
+  GList *trksegs;
 
   /* currently parsed track point */
-  _gpx_track_point_t *current_track_point;
+  dt_gpx_track_point_t *current_track_point;
   _gpx_parser_element_t current_parser_element;
   gboolean invalid_track_point;
   gboolean parsing_trk;
-
+  uint32_t segid;
+  char *seg_name;
 } dt_gpx_t;
 
 static void _gpx_parser_start_element(GMarkupParseContext *ctx, const gchar *element_name,
@@ -62,10 +60,18 @@ static GMarkupParser _gpx_parser
 
 static gint _sort_track(gconstpointer a, gconstpointer b)
 {
-  const _gpx_track_point_t *pa = (const _gpx_track_point_t *)a;
-  const _gpx_track_point_t *pb = (const _gpx_track_point_t *)b;
-  glong diff = pa->time.tv_sec - pb->time.tv_sec;
+  const dt_gpx_track_point_t *pa = (const dt_gpx_track_point_t *)a;
+  const dt_gpx_track_point_t *pb = (const dt_gpx_track_point_t *)b;
+  const glong diff = pa->time.tv_sec - pb->time.tv_sec;
   return diff != 0 ? diff : pa->time.tv_usec - pb->time.tv_usec;
+}
+
+static gint _sort_segment(gconstpointer a, gconstpointer b)
+{
+  const dt_gpx_track_segment_t *pa = (const dt_gpx_track_segment_t *)a;
+  const dt_gpx_track_segment_t *pb = (const dt_gpx_track_segment_t *)b;
+  const glong diff = pa->start_dt.tv_sec - pb->start_dt.tv_sec;
+  return diff != 0 ? diff : pa->start_dt.tv_usec - pb->start_dt.tv_usec;
 }
 
 dt_gpx_t *dt_gpx_new(const gchar *filename)
@@ -104,8 +110,8 @@ dt_gpx_t *dt_gpx_new(const gchar *filename)
   g_markup_parse_context_free(ctx);
   g_mapped_file_unref(gpxmf);
 
-  /* safeguard against corrupt gpx files that have the points not ordered by time */
-  gpx->track = g_list_sort(gpx->track, _sort_track);
+  gpx->trkpts = g_list_sort(gpx->trkpts, _sort_track);
+  gpx->trksegs = g_list_sort(gpx->trksegs, _sort_segment);
 
   return gpx;
 
@@ -125,11 +131,18 @@ error:
   return NULL;
 }
 
+void _track_seg_free(dt_gpx_track_segment_t *trkseg)
+{
+  g_free(trkseg->name);
+  g_free(trkseg);
+}
+
 void dt_gpx_destroy(struct dt_gpx_t *gpx)
 {
   g_assert(gpx != NULL);
 
-  if(gpx->track) g_list_free_full(gpx->track, g_free);
+  if(gpx->trkpts) g_list_free_full(gpx->trkpts, g_free);
+  if(gpx->trksegs) g_list_free_full(gpx->trksegs, (GDestroyNotify)_track_seg_free);
 
   g_free(gpx);
 }
@@ -138,7 +151,7 @@ gboolean dt_gpx_get_location(struct dt_gpx_t *gpx, GTimeVal *timestamp, dt_image
 {
   g_assert(gpx != NULL);
 
-  GList *item = g_list_first(gpx->track);
+  GList *item = g_list_first(gpx->trkpts);
 
   /* verify that we got at least 2 trackpoints */
   if(!item || !item->next) return FALSE;
@@ -146,7 +159,7 @@ gboolean dt_gpx_get_location(struct dt_gpx_t *gpx, GTimeVal *timestamp, dt_image
   do
   {
 
-    _gpx_track_point_t *tp = (_gpx_track_point_t *)item->data;
+    dt_gpx_track_point_t *tp = (dt_gpx_track_point_t *)item->data;
 
     /* if timestamp is out of time range return false but fill
        closest location value start or end point */
@@ -160,7 +173,7 @@ gboolean dt_gpx_get_location(struct dt_gpx_t *gpx, GTimeVal *timestamp, dt_image
 
     /* check if timestamp is within current and next trackpoint */
     if(timestamp->tv_sec >= tp->time.tv_sec
-       && timestamp->tv_sec <= ((_gpx_track_point_t *)item->next->data)->time.tv_sec)
+       && timestamp->tv_sec <= ((dt_gpx_track_point_t *)item->next->data)->time.tv_sec)
     {
       geoloc->longitude = tp->longitude;
       geoloc->latitude = tp->latitude;
@@ -209,7 +222,8 @@ void _gpx_parser_start_element(GMarkupParseContext *ctx, const gchar *element_na
 
     if(*attribute_name)
     {
-      gpx->current_track_point = g_malloc0(sizeof(_gpx_track_point_t));
+      gpx->current_track_point = g_malloc0(sizeof(dt_gpx_track_point_t));
+      gpx->current_track_point->segid = gpx->segid;
 
       /* initialize with NAN for validation check */
       gpx->current_track_point->longitude = NAN;
@@ -252,6 +266,18 @@ void _gpx_parser_start_element(GMarkupParseContext *ctx, const gchar *element_na
 
     gpx->current_parser_element = GPX_PARSER_ELEMENT_ELE;
   }
+  else if(strcmp(element_name, "name") == 0)
+  {
+    gpx->current_parser_element = GPX_PARSER_ELEMENT_NAME;
+  }
+  else if(strcmp(element_name, "trkseg") == 0)
+  {
+    dt_gpx_track_segment_t *ts = g_malloc0(sizeof(dt_gpx_track_segment_t));
+    ts->name = gpx->seg_name;
+    ts->id = gpx->segid;
+    gpx->seg_name = NULL;
+    gpx->trksegs = g_list_prepend(gpx->trksegs, ts);
+  }
 
 end:
 
@@ -276,11 +302,15 @@ void _gpx_parser_end_element(GMarkupParseContext *context, const gchar *element_
     else if(strcmp(element_name, "trkpt") == 0)
     {
       if(!gpx->invalid_track_point)
-        gpx->track = g_list_append(gpx->track, gpx->current_track_point);
+        gpx->trkpts = g_list_prepend(gpx->trkpts, gpx->current_track_point);
       else
         g_free(gpx->current_track_point);
 
       gpx->current_track_point = NULL;
+    }
+    else if(strcmp(element_name, "trkseg") == 0)
+    {
+      gpx->segid++;
     }
 
     /* clear current parser element */
@@ -293,19 +323,62 @@ void _gpx_parser_text(GMarkupParseContext *context, const gchar *text, gsize tex
 {
   dt_gpx_t *gpx = (dt_gpx_t *)user_data;
 
+  if(gpx->current_parser_element == GPX_PARSER_ELEMENT_NAME)
+  {
+    if(gpx->seg_name) g_free(gpx->seg_name);
+    gpx->seg_name =  g_strdup(text);
+  }
+
   if(!gpx->current_track_point) return;
 
   if(gpx->current_parser_element == GPX_PARSER_ELEMENT_TIME)
   {
-
     if(!g_time_val_from_iso8601(text, &gpx->current_track_point->time))
     {
       gpx->invalid_track_point = TRUE;
       fprintf(stderr, "broken gpx file, failed to pars is8601 time '%s' for trackpoint\n", text);
     }
+    dt_gpx_track_segment_t *ts = (dt_gpx_track_segment_t *)gpx->trksegs->data;
+    if(ts)
+    {
+      ts->nb_trkpt++;
+      if(!ts->start_dt.tv_sec)
+      {
+        ts->start_dt.tv_sec = gpx->current_track_point->time.tv_sec;
+        ts->start_dt.tv_usec = gpx->current_track_point->time.tv_usec;
+        ts->trkpt = gpx->current_track_point;
+      }
+      ts->end_dt.tv_sec = gpx->current_track_point->time.tv_sec;
+      ts->end_dt.tv_usec = gpx->current_track_point->time.tv_usec;
+    }
   }
   else if(gpx->current_parser_element == GPX_PARSER_ELEMENT_ELE)
     gpx->current_track_point->elevation = g_ascii_strtod(text, NULL);
+}
+
+GList *dt_gpx_get_trkseg(struct dt_gpx_t *gpx)
+{
+  return gpx->trksegs;
+}
+
+GList *dt_gpx_get_trkpts(struct dt_gpx_t *gpx, const guint segid)
+{
+  GList *pts = NULL;
+  GList *ts = g_list_nth(gpx->trksegs, segid);
+  if(!ts) return pts;
+  dt_gpx_track_segment_t *tsd = (dt_gpx_track_segment_t *)ts->data;
+  GList *tps = g_list_find(gpx->trkpts, tsd->trkpt);
+  if(!tps) return pts;
+  for(GList *tp = tps; tp; tp = g_list_next(tp))
+  {
+    dt_gpx_track_point_t *tpd = (dt_gpx_track_point_t *)tp->data;
+    if(tpd->segid != segid) return pts;
+    dt_geo_map_display_point_t *p = g_malloc0(sizeof(dt_geo_map_display_point_t));
+    p->lat = tpd->latitude;
+    p->lon = tpd->longitude;
+    pts = g_list_prepend(pts, p);
+  }
+  return pts;
 }
 
 // modelines: These editor modelines have been set for all relevant files by tools/update_modelines.sh

src/common/gpx.h
@@ -23,14 +23,37 @@
 
 struct dt_gpx_t;
 
+typedef struct dt_gpx_track_point_t
+{
+  gdouble longitude, latitude, elevation;
+  GTimeVal time;
+  uint32_t segid;
+} dt_gpx_track_point_t;
+
+typedef struct dt_gpx_track_segment_t
+{
+  guint id;
+  GTimeVal start_dt;
+  GTimeVal end_dt;
+  char *name;
+  dt_gpx_track_point_t *trkpt;
+  uint32_t nb_trkpt;
+} dt_gpx_track_segment_t;
+
 /* loads and parses a gpx track file */
 struct dt_gpx_t *dt_gpx_new(const gchar *filename);
-void dt_gpx_destroy(struct dt_gpx_t *);
+void dt_gpx_destroy(struct dt_gpx_t *gpx);
 
 /* fetch the lon,lat coords for time t, if within time range
   of gpx record return TRUE, FALSE is returned if out of time frame
   and closest record of lon,lat is filled */
 gboolean dt_gpx_get_location(struct dt_gpx_t *, GTimeVal *timestamp, dt_image_geoloc_t *geoloc);
+
+// get the list of track segments
+GList *dt_gpx_get_trkseg(struct dt_gpx_t *gpx);
+
+// get the list of track points for a track segment
+GList *dt_gpx_get_trkpts(struct dt_gpx_t *gpx, const guint segid);
 
 // modelines: These editor modelines have been set for all relevant files by tools/update_modelines.sh
 // vim: shiftwidth=2 expandtab tabstop=2 cindent

src/common/undo.h
@@ -39,7 +39,7 @@ typedef enum dt_undo_type_t
   DT_UNDO_DEVELOP     = DT_UNDO_HISTORY | DT_UNDO_MASK | DT_UNDO_TAGS | DT_UNDO_RATINGS | DT_UNDO_COLORLABELS,
   DT_UNDO_LIGHTTABLE  = DT_UNDO_RATINGS | DT_UNDO_COLORLABELS | DT_UNDO_TAGS | DT_UNDO_METADATA
                         | DT_UNDO_LT_HISTORY | DT_UNDO_GEOTAG | DT_UNDO_FLAGS | DT_UNDO_DATETIME,
-  DT_UNDO_MAP         = DT_UNDO_GEOTAG | DT_UNDO_TAGS,
+  DT_UNDO_MAP         = DT_UNDO_GEOTAG | DT_UNDO_TAGS | DT_UNDO_DATETIME,
   DT_UNDO_ALL         = DT_UNDO_GEOTAG | DT_UNDO_DEVELOP | DT_UNDO_LIGHTTABLE
 } dt_undo_type_t;
 

commit 12369bdc96b0571f086d2085c2372553a7bd81a2

image: fix memory leak in dt_image_set_locations (introduced in #8106)

src/common/image.c
@@ -556,15 +556,14 @@ void _image_set_location(GList *imgs, const dt_image_geoloc_t *geoloc, GList **u
   }
 }
 
-void dt_image_set_locations(const GList *img, const dt_image_geoloc_t *geoloc, const gboolean undo_on)
+void dt_image_set_locations(const GList *imgs, const dt_image_geoloc_t *geoloc, const gboolean undo_on)
 {
-  GList *imgs = g_list_copy((GList *)img);
   if(imgs)
   {
     GList *undo = NULL;
     if(undo_on) dt_undo_start_group(darktable.undo, DT_UNDO_GEOTAG);
 
-    _image_set_location(imgs, geoloc, &undo, undo_on);
+    _image_set_location((GList *)imgs, geoloc, &undo, undo_on);
 
     if(undo_on)
     {
@@ -1191,7 +1190,7 @@ GList* dt_image_find_duplicates(const char* filename)
   {
     // the default sidecar exists and is readable, so add it to the list
     files = g_list_prepend(files, g_strdup(pattern));
-  } 
+  }
 
   // now collect all file_N*N.ext.xmp matches
   static const char glob_pattern[] = "_[0-9]*[0-9]";