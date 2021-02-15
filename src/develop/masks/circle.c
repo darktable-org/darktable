262110e382f752d1cb03b69db809387bf68e7ee7
Pascal Obry <pascal@obry.net>
2021-02-15 22:34:13 +0100

    brush,circle: fix as (use for detection of collision with mouse).
    Use definition as in ellispse, paths & gradient. This makes the
    use of handles in the brush lot easier as not requiring too
    much precision.

--- src/develop/masks/circle.c
+++ src/develop/masks/circle.c
@@ -452,10 +452,10 @@ static int dt_circle_events_mouse_moved(struct dt_iop_module_t *module, float pz
   }
   else if(!gui->creation)
   {
-    float as = 0.005f / zoom_scale * darktable.develop->preview_pipe->backbuf_width;
     const dt_dev_zoom_t zoom = dt_control_get_dev_zoom();
     const int closeup = dt_control_get_dev_closeup();
     const float zoom_scale = dt_dev_get_zoom_scale(darktable.develop, zoom, 1<<closeup, 1);
+    const float as = DT_PIXEL_APPLY_DPI(5) / zoom_scale;
     int in, inb, near, ins;
     dt_circle_get_distance(pzx * darktable.develop->preview_pipe->backbuf_width,
                            pzy * darktable.develop->preview_pipe->backbuf_height, as, gui, index, &in, &inb,
