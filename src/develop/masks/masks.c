a230332908bb5d11ca0c9aaed659510ae90d44d1
ralfbrown <ralfbrown@users.noreply.github.com>
2021-02-20 17:35:18 -0500

    split circle.c into separate compilation unit

--- src/develop/masks/masks.c
+++ src/develop/masks/masks.c
@@ -28,7 +28,6 @@
 #pragma GCC diagnostic ignored "-Wshadow"
 
 // clang-format off
-#include "develop/masks/circle.c"
 #include "develop/masks/path.c"
 #include "develop/masks/brush.c"
 #include "develop/masks/gradient.c"
