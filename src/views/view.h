edb591a9d910cc8e868f0105f2c76a2798cbd304
ralfbrown <ralfbrown@users.noreply.github.com>
2021-02-25 00:00:12 -0500

    add functions for mouse_action_t creation

--- src/views/view.h
+++ src/views/view.h
@@ -420,6 +420,12 @@ void dt_view_manager_module_toolbox_add(dt_view_manager_t *vm, GtkWidget *tool, 
 void dt_view_set_scrollbar(dt_view_t *view, float hpos, float hscroll_lower, float hsize, float hwinsize,
                            float vpos, float vscroll_lower, float vsize, float vwinsize);
 
+/** add mouse action record to list of mouse actions */
+GSList *dt_mouse_action_create_simple(GSList *actions, dt_mouse_action_type_t type, GdkModifierType accel,
+                                      const char *const description);
+GSList *dt_mouse_action_create_format(GSList *actions, dt_mouse_action_type_t type, GdkModifierType accel,
+                                      const char *const format_string, const char *const replacement);
+
 /*
  * Tethering View PROXY
  */
