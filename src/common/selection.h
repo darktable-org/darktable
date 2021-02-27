53750fc972d8456050b22cd5cbfe5239b1aac1ca
AlicVB <dev@lnaa.fr>
2021-02-27 14:51:26 +0100

    metadata lib : speedup _update routine

--- src/common/selection.h
+++ src/common/selection.h
@@ -53,6 +53,8 @@ const struct dt_collection_t *dt_selection_get_collection(struct dt_selection_t 
 /** get the list of selected images */
 GList *dt_selection_get_list(struct dt_selection_t *selection, const gboolean only_visible,
                              const gboolean ordering);
+gchar *dt_selection_get_list_query(struct dt_selection_t *selection, const gboolean only_visible,
+                                   const gboolean ordering);
 // modelines: These editor modelines have been set for all relevant files by tools/update_modelines.sh
 // vim: shiftwidth=2 expandtab tabstop=2 cindent
 // kate: tab-indents: off; indent-width 2; replace-tabs on; indent-mode cstyle; remove-trailing-spaces modified;
