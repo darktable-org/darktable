4c078015b5ed258953a516d06843a1256a1bca6b
lrkwz <luca.orlandi@gmail.com>
2021-02-28 14:51:26 +0100

    Fixed: ReadLink was not correctly set in GNU, Replaced alias with a variable

--- tools/common.sh
+++ tools/common.sh
@@ -3,17 +3,19 @@
 # Partly stolen from http://imagejdocu.tudor.lu/doku.php?id=diverse:commandline:imagej
 # Detect readlink or realpath version
 # if GNU readlink is known to be installed, this code can be replaced by "alias ReadLink='readlink -f'"
-alias ReadLink='readlink' # Default
+ReadLink='readlink' # Default
 OS=$(uname)
-if [ "$OS" = "Darwin" -o "$OS" = "FreeBSD" ] && which greadlink >/dev/null 2>&1 ; then
+if [ "$OS" = "Darwin" -o "$OS" = "FreeBSD" ] && which greadlink >/dev/null 2>&1; then
         # Using GNU readlink on MacOS X or FreeBSD
-        alias ReadLink='greadlink -f'
-elif [ "$OS" = "Darwin" -o "$OS" = "FreeBSD" -o "$OS" = "Linux" ] && which realpath >/dev/null 2>&1 ; then
-        # Using realpath on MacOS X or FreeBSD
-        alias ReadLink='realpath'
-elif [ -f "$(which readlink)" ] && readlink --version | grep coreutils >/dev/null 2>&1 ; then
-        alias ReadLink='readlink -f'  # use GNU readlink
+        ReadLink='greadlink -f'
+elif [ "$OS" = "Darwin" -o "$OS" = "FreeBSD" -o "$OS" = "Linux" ] && which realpath >/dev/null 2>&1; then
+        if [ -f "$(which readlink)" ] && readlink --version | grep coreutils >/dev/null 2>&1; then
+                ReadLink='readlink -f' # use GNU readlink
+        else
+                # Using realpath on MacOS X or FreeBSD
+                ReadLink='realpath'
+        fi
 else
-        alias ReadLink='echo'
+        ReadLink='echo'
         echo "Please install realpath or GNU readlink. Symbolic links may not be resolved properly" >&2
 fi
