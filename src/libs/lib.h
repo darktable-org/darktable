fdf40c48de2b71a0c86741c10d9f59f3850e8823
dterrahe <dterrahe@yahoo.com>
2021-03-01 16:58:06 +0000

    reorganise module api specification and loading (#8336)

--- src/libs/lib.h
+++ src/libs/lib.h
@@ -80,7 +80,8 @@ typedef struct dt_lib_t
 
 typedef struct dt_lib_module_t
 {
-  // !!! MUST BE KEPT IN SYNC WITH src/libs/lib_api.h !!!
+#define INCLUDE_API_FROM_MODULE_H
+#include "libs/lib_api.h"
 
   /** opened module. */
   GModule *module;
@@ -96,59 +97,6 @@ typedef struct dt_lib_module_t
   void (*_postponed_update)(struct dt_lib_module_t *self);
   /** ID of timer for delayed callback */
   guint timeout_handle;
-
-  /** version */
-  int (*version)(void);
-  /** get name of the module, to be translated. */
-  const char *(*name)(struct dt_lib_module_t *self);
-  /** get the views which the module should be loaded in. */
-  const char **(*views)(struct dt_lib_module_t *self);
-  /** get the container which the module should be placed in */
-  uint32_t (*container)(struct dt_lib_module_t *self);
-  /** check if module should use a expander or not, default implementation
-      will make the module expandable and storing the expanding state,
-      if not the module will always be shown without the expander. */
-  int (*expandable)(struct dt_lib_module_t *self);
-
-  /** constructor */
-  void (*init)(struct dt_lib_module_t *self);
-  /** callback methods for gui. */
-  /** construct widget. */
-  void (*gui_init)(struct dt_lib_module_t *self);
-  /** destroy widget. */
-  void (*gui_cleanup)(struct dt_lib_module_t *self);
-  /** reset to defaults. */
-  void (*gui_reset)(struct dt_lib_module_t *self);
-
-  /** entering a view, only called if lib is displayed on the new view */
-  void (*view_enter)(struct dt_lib_module_t *self,struct dt_view_t *old_view,struct dt_view_t *new_view);
-  /** entering a view, only called if lib is displayed on the old view */
-  void (*view_leave)(struct dt_lib_module_t *self,struct dt_view_t *old_view,struct dt_view_t *new_view);
-
-  /** optional event callbacks for big center widget. */
-  /** optional method called after lighttable expose. */
-  void (*gui_post_expose)(struct dt_lib_module_t *self, cairo_t *cr, int32_t width, int32_t height,
-                          int32_t pointerx, int32_t pointery);
-  int (*mouse_leave)(struct dt_lib_module_t *self);
-  int (*mouse_moved)(struct dt_lib_module_t *self, double x, double y, double pressure, int which);
-  int (*button_released)(struct dt_lib_module_t *self, double x, double y, int which, uint32_t state);
-  int (*button_pressed)(struct dt_lib_module_t *self, double x, double y, double pressure, int which,
-                        int type, uint32_t state);
-  int (*scrolled)(struct dt_lib_module_t *self, double x, double y, int up);
-  void (*configure)(struct dt_lib_module_t *self, int width, int height);
-  int (*position)(const struct dt_lib_module_t *self);
-  /** implement these three if you want customizable presets to be stored in db. */
-  /** legacy_params can run in iterations, just return to what version you updated the preset. */
-  void *(*legacy_params)(struct dt_lib_module_t *self, const void *const old_params,
-                         const size_t old_params_size, const int old_version, int *new_version, size_t *new_size);
-  void *(*get_params)(struct dt_lib_module_t *self, int *size);
-  int (*set_params)(struct dt_lib_module_t *self, const void *params, int size);
-  void (*init_presets)(struct dt_lib_module_t *self);
-  void (*manage_presets)(struct dt_lib_module_t *self);
-  void (*set_preferences)(void *menu, struct dt_lib_module_t *self);
-  /** Optional callbacks for keyboard accelerators */
-  void (*init_key_accels)(struct dt_lib_module_t *self);
-  void (*connect_key_accels)(struct dt_lib_module_t *self);
 
   GSList *accel_closures;
   GtkWidget *reset_button;
