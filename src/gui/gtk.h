c149ffd5621123c2fa6cd30c33f6ea64956c92f4
dterrahe <dterrahe@yahoo.com>
2021-03-01 16:15:44 +0000

    reorder blending buttons to save 2 lines (#8359)

--- src/gui/gtk.h
+++ src/gui/gtk.h
@@ -349,8 +349,8 @@ void dt_ellipsize_combo(GtkComboBox *cbox);
 static inline void dt_ui_section_label_set(GtkWidget *label)
 {
   gtk_widget_set_halign(label, GTK_ALIGN_FILL); // make it span the whole available width
+  gtk_label_set_xalign (GTK_LABEL(label), 0.0f);
   gtk_label_set_ellipsize(GTK_LABEL(label), PANGO_ELLIPSIZE_END); // ellipsize labels
-  g_object_set(G_OBJECT(label), "xalign", 0.0, (gchar *)0);    // make the text left aligned
   gtk_widget_set_name(label, "section_label"); // make sure that we can style these easily
 }
 
