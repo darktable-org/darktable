bdaf2181d1545cce9e74a115a5292bdb91c3482c
lhietal <62325790+lhietal@users.noreply.github.com>
2021-03-09 17:40:00 +0200

    Add deltas scaling on macOS

--- src/gui/gtk.c
+++ src/gui/gtk.c
@@ -592,8 +592,13 @@ gboolean dt_gui_get_scroll_deltas(const GdkEventScroll *event, gdouble *delta_x,
     case GDK_SCROLL_SMOOTH:
       if((delta_x && event->delta_x != 0) || (delta_y && event->delta_y != 0))
       {
+#ifdef GDK_WINDOWING_QUARTZ // on macOS deltas need to be scaled
+        if(delta_x) *delta_x = event->delta_x / 100;
+        if(delta_y) *delta_y = event->delta_y / 100;
+#else
         if(delta_x) *delta_x = event->delta_x;
         if(delta_y) *delta_y = event->delta_y;
+ #endif
         handled = TRUE;
       }
     default:
