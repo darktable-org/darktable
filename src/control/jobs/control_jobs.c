7742a680e73ffce98fe726eec5ad3f8b09810ea8
Pascal Obry <pascal@obry.net>
2021-04-09 21:20:02 +0200

    Ensure that a duplicate gets the change_timestamp of the source image.
    Fix White Balance for old edits that are duplicated before enterring
    the darkroom.
    
    Continued work for #8599.

--- src/control/jobs/control_jobs.c
+++ src/control/jobs/control_jobs.c
@@ -567,6 +567,9 @@ static int32_t dt_control_duplicate_images_job_run(dt_job_t *job)
     if(newimgid != -1)
     {
       dt_history_copy_and_paste_on_image(imgid, newimgid, FALSE, NULL, TRUE, TRUE);
+      // a duplicate should keep the change time stamp of the original
+      dt_image_cache_set_change_timestamp_from_image(darktable.image_cache, newimgid, imgid);
+
       dt_collection_update_query(darktable.collection, DT_COLLECTION_CHANGE_RELOAD, NULL);
     }
     t = g_list_next(t);
