1203473518705fc5f423f380c7bbeda58c9a8f8e
Pascal Obry <pascal@obry.net>
2021-05-04 22:44:34 +0200

    Remove one superfluous translation.

--- src/develop/blend_gui.c
+++ src/develop/blend_gui.c
@@ -3042,7 +3042,7 @@ void dt_iop_gui_init_blending(GtkWidget *iopw, dt_iop_module_t *module)
 
     bd->details_slider = dt_bauhaus_slider_new_with_range(module, -1.0f, 1.0f, .01f, 0.0f, 2);
     dt_bauhaus_widget_set_label(bd->details_slider, N_("blend"), N_("details threshold"));
-    dt_bauhaus_slider_set_format(bd->details_slider, _("%.2f"));
+    dt_bauhaus_slider_set_format(bd->details_slider, "%.2f");
     gtk_widget_set_tooltip_text(bd->details_slider, _("adjust the threshold for the details mask (using raw data), "
                                                       "\npositive values selects areas with strong details, "
                                                       "\nnegative values select flat areas"));
