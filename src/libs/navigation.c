78901a73c59531de2a352b923c2f67ff0ab7d0dd
hanno@schwalm-bremen.de <hanno@schwalm-bremen.de>
2021-05-05 16:54:25 +0200

    Fix scaling indicator depending on ppd

--- src/libs/navigation.c
+++ src/libs/navigation.c
@@ -249,7 +249,7 @@ static gboolean _lib_navigation_draw_callback(GtkWidget *widget, cairo_t *crf, g
       cairo_set_line_join(cr, CAIRO_LINE_JOIN_ROUND);
 
       char zoomline[5];
-      snprintf(zoomline, sizeof(zoomline), "%.0f%%", cur_scale * 100);
+      snprintf(zoomline, sizeof(zoomline), "%.0f%%", cur_scale * 100 * darktable.gui->ppd);
 
       pango_layout_set_text(layout, zoomline, -1);
       pango_layout_get_pixel_extents(layout, &ink, NULL);
