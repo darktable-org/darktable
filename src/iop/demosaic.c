2b9424a326b584dff4a5aa64f4bee83c7126cbd9
hanno@schwalm-bremen.de <hanno@schwalm-bremen.de>
2021-05-09 14:08:23 +0200

    Writing the details mask does not support tiling
    Write out of the preliminary details mask is done inside the process function of the module,
    this prohibits tiling here.
    
    Doing it in the pipeline dt_dev_pixelpipe_process_rec checking for these special conditions would
    require some major mods not desired.

--- src/iop/demosaic.c
+++ src/iop/demosaic.c
@@ -5542,8 +5542,14 @@ void commit_params(struct dt_iop_module_t *self, dt_iop_params_t *params, dt_dev
   }
 
   // green-equilibrate over full image excludes tiling
-  if(d->green_eq == DT_IOP_GREEN_EQ_FULL || d->green_eq == DT_IOP_GREEN_EQ_BOTH) piece->process_tiling_ready = 0;
-
+  // The details mask is written inside process, this does not allow tiling.
+  if((d->green_eq == DT_IOP_GREEN_EQ_FULL || d->green_eq == DT_IOP_GREEN_EQ_BOTH) ||
+     ((use_method & DEMOSAIC_DUAL) && (d->dual_thrs > 0.0f)) ||
+     (piece->pipe->want_detail_mask == (DT_DEV_DETAIL_MASK_REQUIRED | DT_DEV_DETAIL_MASK_DEMOSAIC)))
+  {
+    piece->process_tiling_ready = 0;
+  }
+ 
   if (self->dev->image_storage.flags & DT_IMAGE_4BAYER)
   {
     // 4Bayer images not implemented in OpenCL yet
