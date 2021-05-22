darktable-org/darktable

commit a8c82c7934c4f9bcbd066230d794831edf576d52

histogram: make process know about input & output colorspace
Don't assume output colorspace is histogram profile -- in some cases
for tether this will not be so.

This is to clear the way for doing colorspace conversion during
histogram process, saving a temp buffer and maybe time.

Also, use dt_iop_order_iccprofile_info_t rather than type & filename
to represent profiles.

src/views/tethering.c
@@ -300,6 +300,8 @@ static void _expose_tethered_mode(dt_view_t *self, cairo_t *cr, int32_t width, i
         // can't handle when not in darkroom view. Plus we can do an
         // in-place conversion which saves allocating an extra buffer.
         const dt_iop_order_iccprofile_info_t *profile_to;
+        const dt_iop_order_iccprofile_info_t *const srgb_profile =
+          dt_ioppr_add_profile_info_to_list(dev, DT_COLORSPACE_SRGB, "", DT_INTENT_RELATIVE_COLORIMETRIC);
         if(darktable.color_profiles->histogram_type == DT_COLORSPACE_WORK)
         {
           // The work profile of a SOC JPEG is nonsensical. So that
@@ -311,24 +313,18 @@ static void _expose_tethered_mode(dt_view_t *self, cairo_t *cr, int32_t width, i
         else if(darktable.color_profiles->histogram_type == DT_COLORSPACE_EXPORT)
         {
           // don't touch the image
-          profile_to = NULL;
+          profile_to = srgb_profile;
         }
         else
         {
           profile_to = dt_ioppr_get_histogram_profile_info(dev);
+          if(!profile_to)
+            profile_to = srgb_profile;
         }
 
-        if(profile_to)
-        {
-          // FIXME: if liveview image is tagged and we can read its colorspace, use that
-          const dt_iop_order_iccprofile_info_t *const profile_from
-              = dt_ioppr_add_profile_info_to_list(dev, DT_COLORSPACE_SRGB, "", INTENT_PERCEPTUAL);
-          dt_ioppr_transform_image_colorspace_rgb(tmp_f, tmp_f, pw, ph, profile_from, profile_to,
-                                                  "live view histogram");
-        }
-
+        // FIXME: if liveview image is tagged and we can read its colorspace, use that
         darktable.lib->proxy.histogram.process(darktable.lib->proxy.histogram.module, tmp_f, pw, ph,
-                                               DT_COLORSPACE_NONE, "");
+                                               srgb_profile, profile_to);
         dt_control_queue_redraw_widget(darktable.lib->proxy.histogram.module->widget);
         dt_free_align(tmp_f);
       }
@@ -398,8 +394,10 @@ static void _expose_tethered_mode(dt_view_t *self, cairo_t *cr, int32_t width, i
                                      FALSE, FALSE, FALSE, FALSE, NULL, FALSE, FALSE, icc_type, icc_filename,
                                      DT_INTENT_PERCEPTUAL, NULL, NULL, 1, 1, NULL))
     {
+      const dt_iop_order_iccprofile_info_t *const histogram_profile =
+        dt_ioppr_get_histogram_profile_info(darktable.develop);
       darktable.lib->proxy.histogram.process(darktable.lib->proxy.histogram.module, dat.buf, dat.head.width,
-                                             dat.head.height, DT_COLORSPACE_NONE, "");
+                                             dat.head.height, histogram_profile, histogram_profile);
       dt_control_queue_redraw_widget(darktable.lib->proxy.histogram.module->widget);
       free(dat.buf);
     }
@@ -407,8 +405,7 @@ static void _expose_tethered_mode(dt_view_t *self, cairo_t *cr, int32_t width, i
   else // not in live view, no image selected
   {
     // if we just left live view, blank out its histogram
-    darktable.lib->proxy.histogram.process(darktable.lib->proxy.histogram.module, NULL, 0, 0, DT_COLORSPACE_NONE,
-                                           "");
+    darktable.lib->proxy.histogram.process(darktable.lib->proxy.histogram.module, NULL, 0, 0, NULL, NULL);
     dt_control_queue_redraw_widget(darktable.lib->proxy.histogram.module->widget);
   }
 }

src/develop/pixelpipe_hb.c
@@ -2160,6 +2160,11 @@ post_process_collect_info:
     }
     if(dev->gui_attached && !dev->gui_leaving && pipe == dev->preview_pipe && (strcmp(module->op, "gamma") == 0))
     {
+      // FIXME: read this from dt_ioppr_get_pipe_output_profile_info()?
+      const dt_iop_order_iccprofile_info_t *const display_profile
+        = dt_ioppr_add_profile_info_to_list(dev, darktable.color_profiles->display_type,
+                                            darktable.color_profiles->display_filename, INTENT_RELATIVE_COLORIMETRIC);
+
       // Since histogram is being treated as the second-to-last link
       // in the pixelpipe and has a "process" call, why not treat it
       // as an iop? Granted, other views such as tether may also
@@ -2181,7 +2186,7 @@ post_process_collect_info:
           }
           darktable.lib->proxy.histogram.process(darktable.lib->proxy.histogram.module, buf,
                                                  roi_out->width, roi_out->height,
-                                                 darktable.color_profiles->display_type, darktable.color_profiles->display_filename);
+                                                 display_profile, dt_ioppr_get_histogram_profile_info(dev));
           dt_free_align(buf);
         }
       }
@@ -2189,7 +2194,7 @@ post_process_collect_info:
       {
         darktable.lib->proxy.histogram.process(darktable.lib->proxy.histogram.module, input,
                                                roi_in.width, roi_in.height,
-                                               darktable.color_profiles->display_type, darktable.color_profiles->display_filename);
+                                               display_profile, dt_ioppr_get_histogram_profile_info(dev));
       }
     }
   }

src/libs/histogram.c
@@ -279,7 +279,7 @@ static void _lib_histogram_process_waveform(dt_lib_histogram_t *const d, const f
   }
 }
 
-static void _lib_histogram_hue_ring(dt_lib_histogram_t *d, dt_iop_order_iccprofile_info_t *vs_prof)
+static void _lib_histogram_hue_ring(dt_lib_histogram_t *d, const dt_iop_order_iccprofile_info_t *vs_prof)
 {
   // FIXME: as in colorbalancergb, repack matrix for SEE?
   // Calculate "hue ring" by tracing along the edges of the "RGB cube"
@@ -348,6 +348,7 @@ static void _lib_histogram_process_vectorscope(dt_lib_histogram_t *d, const floa
 
   // FIXME: is this available from caller?
   // FIXME: if we do convert to histogram RGB, should it be an absolute colorimetric conversion (would mean knowing the histogram profile whitepoint and un-adapting its matrices) and then we have a meaningful whitepoint and could plot spectral locus -- or the reverse, adapt the spectral locus to the histogram profile PCS (always D50)?
+  // NOTE: this may be different from the output profile for _lib_histogram_process()
   dt_iop_order_iccprofile_info_t *vs_prof = dt_ioppr_get_histogram_profile_info(darktable.develop);
   if(!vs_prof) return;
   if(vs_prof->type != d->hue_ring_prof)
@@ -526,11 +527,11 @@ static void _lib_histogram_process_vectorscope(dt_lib_histogram_t *d, const floa
 
 static void dt_lib_histogram_process(struct dt_lib_module_t *self, const float *const input,
                                      int width, int height,
-                                     dt_colorspaces_color_profile_type_t in_profile_type, const gchar *in_profile_filename)
+                                     const dt_iop_order_iccprofile_info_t *const profile_info_from,
+                                     const dt_iop_order_iccprofile_info_t *const profile_info_to)
 {
   dt_lib_histogram_t *d = (dt_lib_histogram_t *)self->data;
   dt_develop_t *dev = darktable.develop;
-  float *img_display = NULL;
 
   // special case, clear the scopes
   if(!input)
@@ -543,6 +544,7 @@ static void dt_lib_histogram_process(struct dt_lib_module_t *self, const float *
     return;
   }
 
+  // FIXME: do clocking in this function rather than in per-scope type functions
   // FIXME: scope goes black when click histogram lib colorpicker on -- is this meant to happen?
   // FIXME: scope doesn't redraw when click histogram lib colorpicker off -- is this meant to happen?
   dt_histogram_roi_t roi = { .width = width, .height = height,
@@ -579,46 +581,30 @@ static void dt_lib_histogram_process(struct dt_lib_module_t *self, const float *
   // caller.
   // FIXME: do conversion in-place in the processing to save an extra buffer? -- at least for waveform, which already has to touch each pixel -- will need logic from _transform_matrix_rgb() -- or better yet a per-pixel callback within _transform_matrix_rgb()-ish code
   // FIXME: in case of vectorscope, it needs XYZ data, so skip this conversion and instead it's enough that it has input & in_profile_type -- though then we don't see the result of a relative colorimetric conversion to the histogram profile...
-  if(in_profile_type != DT_COLORSPACE_NONE)
-  {
-    const dt_iop_order_iccprofile_info_t *const profile_info_from
-      = dt_ioppr_add_profile_info_to_list(dev, in_profile_type, in_profile_filename, INTENT_PERCEPTUAL);
-
-    dt_colorspaces_color_profile_type_t out_profile_type;
-    const char *out_profile_filename;
-    dt_ioppr_get_histogram_profile_type(&out_profile_type, &out_profile_filename);
-    if(out_profile_type != DT_COLORSPACE_NONE)
-    {
-      const dt_iop_order_iccprofile_info_t *const profile_info_to =
-        dt_ioppr_add_profile_info_to_list(dev, out_profile_type, out_profile_filename, DT_INTENT_RELATIVE_COLORIMETRIC);
-      img_display = dt_alloc_align_float((size_t)4 * width * height);
-      if(!img_display) return;
-      dt_ioppr_transform_image_colorspace_rgb(input, img_display, width, height, profile_info_from,
-                                              profile_info_to, "final histogram");
-    }
-  }
+  float *img_display = dt_alloc_align_float((size_t)4 * width * height);
+  if(!img_display) return;
+  dt_ioppr_transform_image_colorspace_rgb(input, img_display, width, height,
+                                          profile_info_from, profile_info_to, "final histogram");
 
   dt_pthread_mutex_lock(&d->lock);
   switch(d->scope_type)
   {
     case DT_LIB_HISTOGRAM_SCOPE_HISTOGRAM:
-      _lib_histogram_process_histogram(d, img_display ? img_display : input, &roi);
+      _lib_histogram_process_histogram(d, img_display, &roi);
       break;
     case DT_LIB_HISTOGRAM_SCOPE_WAVEFORM:
-      _lib_histogram_process_waveform(d, img_display ? img_display : input, &roi);
+      _lib_histogram_process_waveform(d, img_display, &roi);
       break;
     case DT_LIB_HISTOGRAM_SCOPE_VECTORSCOPE:
       // FIXME: only work on roi
-      _lib_histogram_process_vectorscope(d, img_display ? img_display : input, &roi);
+      _lib_histogram_process_vectorscope(d, img_display, &roi);
       break;
     case DT_LIB_HISTOGRAM_SCOPE_N:
       // FIXME: use dt_unreachable_codepath_with_desc()
       g_assert_not_reached();
   }
   dt_pthread_mutex_unlock(&d->lock);
-
-  if(img_display)
-    dt_free_align(img_display);
+  dt_free_align(img_display);
 }
 
 
@@ -768,6 +754,7 @@ static void _lib_histogram_draw_vectorscope(dt_lib_histogram_t *d, cairo_t *cr,
   }
 
   // vectorscope graph
+  // FIXME: use cairo_pattern_set_filter()?
   cairo_set_operator(cr, CAIRO_OPERATOR_ADD);
   const int stride = cairo_format_stride_for_width(CAIRO_FORMAT_RGB24, diam_px);
   // FIXME: if use cairo_mask() could calculate a false color vectorscope and draw with appropriate hues?

src/libs/lib.h
@@ -73,7 +73,8 @@ typedef struct dt_lib_t
       struct dt_lib_module_t *module;
       void (*process)(struct dt_lib_module_t *self, const float *const input,
                       int width, int height,
-                      dt_colorspaces_color_profile_type_t icc_type, const gchar *icc_filename);
+                      const dt_iop_order_iccprofile_info_t *const profile_info_from,
+                      const dt_iop_order_iccprofile_info_t *const profile_info_to);
       // FIXME: now that PR #5532 is merged, define this as dt_atomic_int and include "common/atomic.h" and use dt_atomic_set_int() and dt_atomic_get_int()
       gboolean is_linear;
     } histogram;

commit 17e684c05a0ef926bb08292c249860a85ab4abf4

tether: fix histogram crash
When drawing the current image (not in live view), when the histogram
profile was either work or export, darktable would crash. This is
because of a call to dt_ioppr_get_histogram_profile_info() which can
only retrieve these values when in darkroom view.

Instead, do some work by hand, including catching these cases and use
dt_colorspaces_get_{work,output}_profile() to retrieve the appropriate
profile.

Also update a comment as profile conversion is now happening in
histogram process, not in live view code.

Fixes bug introduced in a8c82c7934c4f9bcbd066230d794831edf576d52.

TODO: vectorscope may still crash when in tether view and histogram
profile is work or export.

src/views/tethering.c
@@ -294,11 +294,9 @@ static void _expose_tethered_mode(dt_view_t *self, cairo_t *cr, int32_t width, i
             tmp_f[p + k] = dt_noise_generator(DT_NOISE_UNIFORM, p_buf[p + k], 0.5f, 0, state) / 255.0f;
         }
 
-        // Do colorspace conversion here rather than having histogram
-        // process do it. We need to do special cases for work/export
-        // colorspace which dt_ioppr_get_histogram_profile_type()
-        // can't handle when not in darkroom view. Plus we can do an
-        // in-place conversion which saves allocating an extra buffer.
+        // We need to do special cases for work/export colorspace
+        // which dt_ioppr_get_histogram_profile_type() can't handle
+        // when not in darkroom view.
         const dt_iop_order_iccprofile_info_t *profile_to;
         const dt_iop_order_iccprofile_info_t *const srgb_profile =
           dt_ioppr_add_profile_info_to_list(dev, DT_COLORSPACE_SRGB, "", DT_INTENT_RELATIVE_COLORIMETRIC);
@@ -367,35 +365,39 @@ static void _expose_tethered_mode(dt_view_t *self, cairo_t *cr, int32_t width, i
     dat.head.max_height = darktable.mipmap_cache->max_height[DT_MIPMAP_F];
     dat.head.style[0] = '\0';
 
-    dt_colorspaces_color_profile_type_t icc_type;
-    const char *icc_filename;
-    const char _icc_filename[1] = { 0 };
+    dt_colorspaces_color_profile_type_t histogram_type;
+    const char *histogram_filename;
     if(darktable.color_profiles->histogram_type == DT_COLORSPACE_WORK)
     {
       const dt_colorspaces_color_profile_t *work_profile = dt_colorspaces_get_work_profile(lib->image_id);
-      icc_type = work_profile->type;
-      icc_filename = work_profile->filename;
+      histogram_type = work_profile->type;
+      histogram_filename = work_profile->filename;
     }
     else if(darktable.color_profiles->histogram_type == DT_COLORSPACE_EXPORT)
     {
-      icc_type = DT_COLORSPACE_NONE; // use the colorout profile
-      icc_filename = _icc_filename;
+      const dt_colorspaces_color_profile_t *export_profile = dt_colorspaces_get_output_profile(lib->image_id, DT_COLORSPACE_NONE, NULL);
+      histogram_type = export_profile->type;
+      histogram_filename = export_profile->filename;
     }
     else
     {
-      dt_ioppr_get_histogram_profile_type(&icc_type, &icc_filename);
+      // special cases above as this can't handle work/export profile
+      // when not in darkroom view
+      dt_ioppr_get_histogram_profile_type(&histogram_type, &histogram_filename);
     }
 
     // this uses the export rather than thumbnail pipe -- slower, but
     // as we're not competing with the full pixelpipe, it's a
     // reasonable trade-off for a histogram which matches that in
     // darkroom view
+    // FIXME: instead export image in work profile, then pass that to histogram process as well as converting to display profile for output, eliminating dt_view_image_get_surface() above
     if(!dt_imageio_export_with_flags(lib->image_id, "unused", &format, (dt_imageio_module_data_t *)&dat, TRUE,
-                                     FALSE, FALSE, FALSE, FALSE, NULL, FALSE, FALSE, icc_type, icc_filename,
+                                     FALSE, FALSE, FALSE, FALSE, NULL, FALSE, FALSE, histogram_type, histogram_filename,
                                      DT_INTENT_PERCEPTUAL, NULL, NULL, 1, 1, NULL))
     {
       const dt_iop_order_iccprofile_info_t *const histogram_profile =
-        dt_ioppr_get_histogram_profile_info(darktable.develop);
+        dt_ioppr_add_profile_info_to_list(darktable.develop, histogram_type, histogram_filename,
+                                          DT_INTENT_RELATIVE_COLORIMETRIC);
       darktable.lib->proxy.histogram.process(darktable.lib->proxy.histogram.module, dat.buf, dat.head.width,
                                              dat.head.height, histogram_profile, histogram_profile);
       dt_control_queue_redraw_widget(darktable.lib->proxy.histogram.module->widget);