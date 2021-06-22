81495a80ac60438be3f40e7bdfdcf6aafa0737cf
ralfbrown <ralfbrown@users.noreply.github.com>
2021-06-22 02:41:42 -0400

    use dt_conf_get_conststring where possible, ensure return from dt_conf_get_string freed

--- src/common/collection.c
+++ src/common/collection.c
@@ -1926,14 +1926,13 @@ int dt_collection_serialize(char *buf, int bufsize)
     buf += c;
     bufsize -= c;
     snprintf(confname, sizeof(confname), "plugins/lighttable/collect/string%1d", k);
-    gchar *str = dt_conf_get_string(confname);
+    const char *str = dt_conf_get_conststring(confname);
     if(str && (str[0] != '\0'))
       c = snprintf(buf, bufsize, "%s$", str);
     else
       c = snprintf(buf, bufsize, "%%$");
     buf += c;
     bufsize -= c;
-    g_free(str);
   }
   return 0;
 }
