ec9443343ba45ad1017a1388d612d5a7e2384b4b
Diederik ter Rahe <dterrahe@yahoo.com>
2021-07-03 08:46:10 +0100

    add permanent search entries for shortcuts dialog

--- src/gui/accelerators.c
+++ src/gui/accelerators.c
@@ -1413,6 +1413,23 @@ gboolean shortcut_selection_function(GtkTreeSelection *selection,
 
 static dt_action_t *_selected_action = NULL;
 
+static gboolean _start_search(GtkWidget *widget, GdkEventKey *event, GtkSearchEntry *entry)
+{
+  if(gtk_search_entry_handle_event(entry, (GdkEvent *)event))
+  {
+    gtk_entry_grab_focus_without_selecting(GTK_ENTRY(entry));
+    return TRUE;
+  }
+
+  return FALSE;
+}
+
+static void _stop_search(GtkSearchEntry *entry, GtkWidget *tree_view)
+{
+  gtk_entry_set_text(GTK_ENTRY(entry), "");
+  gtk_widget_grab_focus(tree_view);
+}
+
 static gboolean _action_view_click(GtkWidget *widget, GdkEventButton *event, gpointer data)
 {
   GtkTreeView *view = GTK_TREE_VIEW(widget);
@@ -1622,12 +1639,19 @@ GtkWidget *dt_shortcuts_prefs(GtkWidget *widget)
   g_object_unref(G_OBJECT(filtered_shortcuts));
   gtk_tree_view_set_search_column(shortcuts_view, 0); // fake column for _search_func
   gtk_tree_view_set_search_equal_func(shortcuts_view, _search_func, shortcuts_view, NULL);
+  GtkWidget *search_shortcuts = gtk_search_entry_new();
+  gtk_entry_set_placeholder_text(GTK_ENTRY(search_shortcuts), _("search shortcuts"));
+  gtk_widget_set_tooltip_text(GTK_WIDGET(search_shortcuts), "incrementally search the list of shortcuts\npress up or down keys to cycle through matches");
+  g_signal_connect(G_OBJECT(search_shortcuts), "stop-search", G_CALLBACK(_stop_search), shortcuts_view);
+  gtk_tree_view_set_search_entry(shortcuts_view, GTK_ENTRY(search_shortcuts));
+
   gtk_tree_selection_set_select_function(gtk_tree_view_get_selection(shortcuts_view),
                                          shortcut_selection_function, NULL, NULL);
   g_object_set(shortcuts_view, "has-tooltip", TRUE, NULL);
   g_signal_connect(G_OBJECT(shortcuts_view), "query-tooltip", G_CALLBACK(_shortcut_tooltip_callback), GINT_TO_POINTER(TRUE));
   g_signal_connect(G_OBJECT(shortcuts_view), "row-activated", G_CALLBACK(_shortcut_row_activated), filtered_shortcuts);
   g_signal_connect(G_OBJECT(shortcuts_view), "key-press-event", G_CALLBACK(_shortcut_key_pressed), NULL);
+  g_signal_connect(G_OBJECT(shortcuts_view), "key-press-event", G_CALLBACK(_start_search), search_shortcuts);
   g_signal_connect(G_OBJECT(shortcuts_store), "row-inserted", G_CALLBACK(_shortcut_row_inserted), shortcuts_view);
 
   // Setting up the cell renderers
@@ -1705,10 +1729,17 @@ GtkWidget *dt_shortcuts_prefs(GtkWidget *widget)
   g_object_unref(actions_store);
   gtk_tree_view_set_search_column(actions_view, 1); // fake column for _search_func
   gtk_tree_view_set_search_equal_func(actions_view, _search_func, actions_view, NULL);
+  GtkWidget *search_actions = gtk_search_entry_new();
+  gtk_entry_set_placeholder_text(GTK_ENTRY(search_actions), _("search actions"));
+  gtk_widget_set_tooltip_text(GTK_WIDGET(search_actions), "incrementally search the list of actions\npress up or down keys to cycle through matches");
+  g_signal_connect(G_OBJECT(search_actions), "stop-search", G_CALLBACK(_stop_search), actions_view);
+  gtk_tree_view_set_search_entry(actions_view, GTK_ENTRY(search_actions));
+
   g_object_set(actions_view, "has-tooltip", TRUE, NULL);
   g_signal_connect(G_OBJECT(actions_view), "query-tooltip", G_CALLBACK(_shortcut_tooltip_callback), NULL);
   g_signal_connect(G_OBJECT(actions_view), "row-activated", G_CALLBACK(_action_row_activated), actions_store);
   g_signal_connect(G_OBJECT(actions_view), "button-press-event", G_CALLBACK(_action_view_click), actions_store);
+  g_signal_connect(G_OBJECT(actions_view), "key-press-event", G_CALLBACK(_start_search), search_actions);
   g_signal_connect(G_OBJECT(gtk_tree_view_get_selection(actions_view)), "changed",
                    G_CALLBACK(_action_selection_changed), shortcuts_view);
 
@@ -1745,7 +1776,31 @@ GtkWidget *dt_shortcuts_prefs(GtkWidget *widget)
   if(split_position) gtk_paned_set_position(GTK_PANED(container), split_position);
   g_signal_connect(G_OBJECT(shortcuts_view), "size-allocate", G_CALLBACK(_resize_shortcuts_view), container);
 
-  return container;
+  GtkWidget *button_bar = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
+  gtk_widget_set_name(button_bar, "shortcut_controls");
+  gtk_box_pack_start(GTK_BOX(button_bar), search_shortcuts, FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(button_bar), search_actions, FALSE, FALSE, 0);
+
+  GtkWidget *defaults_button = gtk_button_new_with_label("defaults");
+  gtk_widget_set_sensitive(defaults_button, FALSE);
+  gtk_widget_set_tooltip_text(defaults_button, "to be implemented");
+  gtk_box_pack_end(GTK_BOX(button_bar), defaults_button, FALSE, FALSE, 0);
+
+  GtkWidget *export_button = gtk_button_new_with_label("export...");
+  gtk_widget_set_sensitive(export_button, FALSE);
+  gtk_widget_set_tooltip_text(export_button, "to be implemented");
+  gtk_box_pack_end(GTK_BOX(button_bar), export_button, FALSE, FALSE, 0);
+
+  GtkWidget *import_button = gtk_button_new_with_label("import...");
+  gtk_widget_set_sensitive(import_button, FALSE);
+  gtk_widget_set_tooltip_text(import_button, "to be implemented");
+  gtk_box_pack_end(GTK_BOX(button_bar), import_button, FALSE, FALSE, 0);
+
+  GtkWidget *top_level = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+  gtk_box_pack_start(GTK_BOX(top_level), container, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(top_level), button_bar, FALSE, FALSE, 0);
+
+  return top_level;
 }
 
 void dt_shortcuts_save(gboolean backup)
@@ -2692,9 +2747,9 @@ gboolean dt_shortcut_dispatcher(GtkWidget *w, GdkEvent *event, gpointer user_dat
   switch(event->type)
   {
   case GDK_KEY_PRESS:
-    if(event->key.is_modifier || 
-       event->key.keyval == GDK_KEY_VoidSymbol || 
-       event->key.keyval == GDK_KEY_Meta_L || event->key.keyval == GDK_KEY_Meta_R || 
+    if(event->key.is_modifier ||
+       event->key.keyval == GDK_KEY_VoidSymbol ||
+       event->key.keyval == GDK_KEY_Meta_L || event->key.keyval == GDK_KEY_Meta_R ||
        event->key.keyval == GDK_KEY_ISO_Level3_Shift) return FALSE;
 
     _sc.mods = event->key.state;
