d133cee5759119cc7ece6e624a0b6ff8af8b3f30
phweyland <philippe.weyland@laposte.net>
2021-07-04 12:59:38 -0300

    import: fix already imported flag in subfolders
    fixes #9392

--- src/common/image.c
+++ src/common/image.c
@@ -1656,8 +1656,10 @@ static uint32_t _image_import_internal(const int32_t film_id, const char *filena
   return id;
 }
 
-gboolean dt_images_already_imported(const gchar *folder, const gchar *filename)
+gboolean dt_images_already_imported(const gchar *filename)
 {
+  gchar *dir = g_path_get_dirname(filename);
+  gchar *file = g_path_get_basename(filename);
   sqlite3_stmt *stmt;
   DT_DEBUG_SQLITE3_PREPARE_V2(dt_database_get(darktable.db),
                               "SELECT *"
@@ -1666,10 +1668,12 @@ gboolean dt_images_already_imported(const gchar *folder, const gchar *filename)
                               "       AND images.film_id = film_rolls.id"
                               "       AND images.filename = ?2",
                               -1, &stmt, NULL);
-  DT_DEBUG_SQLITE3_BIND_TEXT(stmt, 1, folder, -1, SQLITE_STATIC);
-  DT_DEBUG_SQLITE3_BIND_TEXT(stmt, 2, filename, -1, SQLITE_STATIC);
+  DT_DEBUG_SQLITE3_BIND_TEXT(stmt, 1, dir, -1, SQLITE_STATIC);
+  DT_DEBUG_SQLITE3_BIND_TEXT(stmt, 2, file, -1, SQLITE_STATIC);
   const gboolean result = sqlite3_step(stmt) == SQLITE_ROW;
   sqlite3_finalize(stmt);
+  g_free(dir);
+  g_free(file);
 
   return result;
 }
