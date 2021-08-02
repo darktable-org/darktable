f6016034dd3ee4f5362683d38c805b27d6097b4d
Hubert Kowalski <johnny-bit@users.noreply.github.com>
2021-08-02 17:03:57 +0200

    InputNG: add keyboard shortcut icon (#9689)

--- src/dtgtk/paint.c
+++ src/dtgtk/paint.c
@@ -2850,6 +2850,67 @@ void dtgtk_cairo_paint_link(cairo_t *cr, gint x, gint y, gint w, gint h, gint fl
   FINISH
 }
 
+void dtgtk_cairo_paint_shortcut(cairo_t *cr, gint x, gint y, gint w, gint h, gint flags, void *data)
+{
+  PREAMBLE(1.15, 0, 0)
+
+  //keyboard outline
+  cairo_set_line_width(cr, .05);
+  cairo_set_line_join(cr, CAIRO_LINE_JOIN_ROUND);
+
+  cairo_move_to(cr, .9, .27);
+  cairo_line_to(cr, .1, .27);
+  cairo_line_to(cr, .1, .73);
+  cairo_line_to(cr, .9, .73);
+  cairo_line_to(cr, .9, .27);
+
+  cairo_stroke(cr);
+
+  //keyboard buttons
+
+  const double cr_linewidth=.04;
+  const int toprow_keycount = 7;
+
+  cairo_set_line_width(cr, cr_linewidth);
+  cairo_set_line_join(cr, CAIRO_LINE_JOIN_MITER);
+  cairo_set_line_cap(cr, CAIRO_LINE_CAP_ROUND);
+
+  const double kheight = .73-.27;
+  const double kwidth = .9 - .1;
+  const double rspace = (kheight - cr_linewidth*3.)/4.;
+  const double keylength = (kwidth-(cr_linewidth*2.0))/((double)toprow_keycount+((double)toprow_keycount/0.9));
+  const double keyspace = keylength / 0.9;
+  const double spacelength = kwidth / 2.0;
+
+  // top row
+  double keyrowwidth = keylength * toprow_keycount + keyspace*(toprow_keycount-1);
+  double rowstartpos = .1 + (kwidth - keyrowwidth)/2;
+  for(int i=0; i < 7; i++)
+  {
+    cairo_move_to(cr, rowstartpos + i*(keylength+keyspace), .27+rspace+cr_linewidth);
+    cairo_line_to(cr, rowstartpos + i*(keylength+keyspace)+keylength, .27+rspace+cr_linewidth);
+  }
+
+  // middle row
+  keyrowwidth = keylength * (toprow_keycount-1) + keyspace*(toprow_keycount-2);
+  rowstartpos = .1 + (kwidth - keyrowwidth)/2;
+  for(int i=0; i < 6; i++)
+  {
+    cairo_move_to(cr, rowstartpos + i*(keylength+keyspace), .27+(rspace+cr_linewidth)*2);
+    cairo_line_to(cr, rowstartpos + i*(keylength+keyspace)+keylength, .27+(rspace+cr_linewidth)*2);
+  }
+
+  // 3rd (space) row
+  keyrowwidth = spacelength;
+  rowstartpos = .1 + (kwidth - keyrowwidth)/2;
+  cairo_move_to(cr, rowstartpos , .27+(rspace+cr_linewidth)*3);
+  cairo_line_to(cr, rowstartpos + spacelength, .27+(rspace+cr_linewidth)*3);
+
+  cairo_stroke(cr);
+
+  FINISH
+}
+
 // modelines: These editor modelines have been set for all relevant files by tools/update_modelines.sh
 // vim: shiftwidth=2 expandtab tabstop=2 cindent
 // kate: tab-indents: off; indent-width 2; replace-tabs on; indent-mode cstyle; remove-trailing-spaces modified;
