1d12ac5cb086dba4e1d9d48bdbc2a4da71b6291e
parafin <parafin@paraf.in>
2021-09-03 09:37:28 +0200

    be more specific with Xcode 11.3.1 compilation fix

--- src/common/eaw.c
+++ src/common/eaw.c
@@ -470,7 +470,7 @@ void eaw_dn_decompose(float *const restrict out, const float *const restrict in,
 
   _aligned_pixel sum_sq = { .v = { 0.0f } };
 
-#ifndef __APPLE__ //makes Xcode 11.3.1 compiler crash
+#if !(defined(__apple_build_version__) && __apple_build_version__ < 11030000) //makes Xcode 11.3.1 compiler crash
 #ifdef _OPENMP
 #pragma omp parallel for default(none) \
   dt_omp_firstprivate(detail, filter, height, in, inv_sigma2, mult, boundary, out, width) \
@@ -567,7 +567,7 @@ void eaw_dn_decompose_sse(float *const restrict out, const float *const restrict
 
   _aligned_pixel sum_sq = { .v = { 0.0f } };
 
-#ifndef __APPLE__ //makes Xcode 11.3.1 compiler crash
+#if !(defined(__apple_build_version__) && __apple_build_version__ < 11030000) //makes Xcode 11.3.1 compiler crash
 #ifdef _OPENMP
 #pragma omp parallel for default(none) \
   dt_omp_firstprivate(detail, filter, height, in, inv_sigma2, mult, boundary, out, width) \
