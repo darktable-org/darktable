c49ec15a953548a76f8bb3f8134a99c2bfc63247
Marco Carrarini <marco.carrarini@gmail.com>
2021-09-05 20:35:27 +0200

    fix import presets always returning failure message

--- src/common/presets.c
+++ src/common/presets.c
@@ -277,7 +277,7 @@ int dt_presets_import_from_file(const char *preset_path)
   DT_DEBUG_SQLITE3_BIND_INT(stmt, 24, def);
   DT_DEBUG_SQLITE3_BIND_INT(stmt, 25, format);
 
-  result = (sqlite3_step(stmt) == SQLITE_OK);
+  result = (sqlite3_step(stmt) == SQLITE_DONE);
 
   sqlite3_finalize(stmt);
 
