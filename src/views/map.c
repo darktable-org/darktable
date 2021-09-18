4d2217c273656a8f230c392fcab61bcb062790b8
Paolo Benvenuto <paolobenve@gmail.com>
2021-09-18 13:52:55 -0400

    gpx interpolation: produce at least 2 segments

--- src/views/map.c
+++ src/views/map.c
@@ -2220,7 +2220,7 @@ static void _track_add_point(OsmGpsMapTrack *track, OsmGpsMapPoint *point, OsmGp
     /* the line must be splitted in order to seek the geodesic line */
     OsmGpsMapPoint *ith_point;
     double f, ith_lat, ith_lon;
-    int n_segments = d / DT_MINIMUM_DISTANCE_FOR_GEODESIC;
+    int n_segments = ceil(d / DT_MINIMUM_DISTANCE_FOR_GEODESIC);
     gboolean first_time = TRUE;
     for (int i = 1; i < n_segments; i ++)
     {
