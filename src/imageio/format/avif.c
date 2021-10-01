47c61c62b6da82810c01bffd2f28f3124c036ed7
Miloš Komarčević <4973094+kmilos@users.noreply.github.com>
2021-10-01 10:38:25 +0200

    avif: fix matrix coefficients used on sRGB export

--- src/imageio/format/avif.c
+++ src/imageio/format/avif.c
@@ -294,7 +294,7 @@ int write_image(struct dt_imageio_module_data_t *data,
       case DT_COLORSPACE_SRGB:
           image->colorPrimaries = AVIF_COLOR_PRIMARIES_BT709;
           image->transferCharacteristics = AVIF_TRANSFER_CHARACTERISTICS_SRGB;
-          image->matrixCoefficients = AVIF_MATRIX_COEFFICIENTS_BT709;
+          image->matrixCoefficients = AVIF_MATRIX_COEFFICIENTS_BT470BG;
           break;
       case DT_COLORSPACE_REC709:
           image->colorPrimaries = AVIF_COLOR_PRIMARIES_BT709;
