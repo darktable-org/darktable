fed9b4bafbf26453d12fff8391e28c7ecc3acec0
Eric <dev@derewonko.com>
2021-10-10 23:00:51 -0700

    add common.cmd and custom.cmd for Windows
    This files are for Windows.

---
+++ tools/common.cmd
@@ -0,0 +1,6 @@
+set configdir=%LOCALAPPDATA%\darktable
+set cachedir=%LOCALAPPDATA%\Microsoft\Windows\INetCache\darktable
+set library=%configdir%\library.db
+set "PATH=%PATH%;%~dp0..\..\..\bin"
+set min-mip=0
+set max-mip=4
---
+++ tools/custom.cmd
@@ -0,0 +1,7 @@
+:: This is an example for those who use specific paths. Use example: cachedir could be a RAMDISK. 
+:: Simply uncomment the lines that need to be different from common.bat
+REM set configdir=K:\config
+REM set cachedir=K:\cache
+REM set library=%configdir%\library.db
+REM set min-mip=0
+REM set max-mip=5
