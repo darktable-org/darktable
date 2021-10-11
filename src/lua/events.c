c64903999cca5f6d411f5bf52d87517ffb22252e
Bill Ferguson <wpferguson@gmail.com>
2021-10-11 13:17:53 -0400

    [lua/events.c] added new event darkroom-image-history-changed
    [views/darkroom.c] added event trigger to notify lua events that
                       the darkroom image history was changed.  This
                       trigger only executes when changing to another
                       image or exiting darkroom view.

--- src/lua/events.c
+++ src/lua/events.c
@@ -659,6 +659,12 @@ int dt_lua_init_events(lua_State *L)
   lua_pushcfunction(L, dt_lua_event_multiinstance_destroy);
   lua_pushcfunction(L, dt_lua_event_multiinstance_trigger);
   dt_lua_event_add(L,"selection-changed");
+
+  lua_pushcfunction(L, dt_lua_event_multiinstance_register);
+  lua_pushcfunction(L, dt_lua_event_multiinstance_destroy);
+  lua_pushcfunction(L, dt_lua_event_multiinstance_trigger);
+  dt_lua_event_add(L, "darkroom-image-history-changed");
+
   return 0;
 }
 // modelines: These editor modelines have been set for all relevant files by tools/update_modelines.sh
