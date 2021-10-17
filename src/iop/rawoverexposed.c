19b3265328d29bdf4040f1bd2b491af1677d80d2
Aurélien PIERRE <aurelien@aurelienpierre.com>
2021-10-17 13:27:34 +0200

    pipeline: relax fences to allow framing module to move

--- src/iop/rawoverexposed.c
+++ src/iop/rawoverexposed.c
@@ -76,7 +76,7 @@ int default_group()
 
 int flags()
 {
-  return IOP_FLAGS_ALLOW_TILING | IOP_FLAGS_HIDDEN | IOP_FLAGS_ONE_INSTANCE | IOP_FLAGS_NO_HISTORY_STACK | IOP_FLAGS_FENCE;
+  return IOP_FLAGS_ALLOW_TILING | IOP_FLAGS_HIDDEN | IOP_FLAGS_ONE_INSTANCE | IOP_FLAGS_NO_HISTORY_STACK;
 }
 
 int default_colorspace(dt_iop_module_t *self, dt_dev_pixelpipe_t *pipe, dt_dev_pixelpipe_iop_t *piece)
