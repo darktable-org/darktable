3b9ce481f9e848e0ca65aa194f5f268dea0e89e4
Pascal Obry <pascal@obry.net>
2021-10-21 19:04:41 +0200

    Add option to invert the behavior of mouse scroll up/down.
    At the same time all checks have been changed to be "positive"
    and make the conditional easier to read. This is especially
    important since the default behavior is now scroll-up = increase
    mask parameters.

--- src/develop/imageop_gui.c
+++ src/develop/imageop_gui.c
@@ -442,6 +442,12 @@ GtkWidget *dt_iop_button_new(dt_iop_module_t *self, const gchar *label,
   return button;
 }
 
+gboolean dt_mask_scroll_increases(int up)
+{
+  const gboolean mask_down = dt_conf_get_bool("masks_scroll_down_increases");
+  return up ? !mask_down : mask_down;
+}
+
 // modelines: These editor modelines have been set for all relevant files by tools/update_modelines.sh
 // vim: shiftwidth=2 expandtab tabstop=2 cindent
 // kate: tab-indents: off; indent-width 2; replace-tabs on; indent-mode cstyle; remove-trailing-spaces modified;
