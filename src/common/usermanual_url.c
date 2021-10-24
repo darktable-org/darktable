8a60e0c89e96607550175dcb52a757642159d332
AlicVB <dev@lnaa.fr>
2021-10-24 20:27:11 +0200

    fix module headers help links

--- src/common/usermanual_url.c
+++ src/common/usermanual_url.c
@@ -78,7 +78,7 @@ dt_help_url urls_db[] =
   {"lighttable_filemanager",     "lighttable/lighttable-modes/filemanager/"},
   {"lighttable_zoomable",        "lighttable/lighttable-modes/zoomable-lighttable/"},
   {"darkroom_bottom_panel",      "darkroom/darkroom-view-layout/#bottom-panel"},
-  {"module_interacting",         "darkroom/interacting-with-modules/"},
+  {"module_header",              "darkroom/processing-modules/module-header/"},
   {"tethering_session",          "module-reference/utility-modules/tethering/session/"},
   {"tethering_live_view",        "module-reference/utility-modules/tethering/live-view/"},
   {"module_toolbox",             NULL},
