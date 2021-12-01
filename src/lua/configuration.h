0d33fffd73780957f0389780ad73209ac47930a4
Bill Ferguson <wpferguson@gmail.com>
2021-11-30 19:06:27 -0500

    [RELEASE_NOTES.md] Added Lua section
    [lua/configuration.h] removed dev from the API version number

--- src/lua/configuration.h
+++ src/lua/configuration.h
@@ -35,6 +35,7 @@
 // 2.4.0 was 5.0.0 (going to lua 5.3 is a major API bump)
 // 3.2.0 was 6.0.0 (removed facebook, flickr, and picasa from types.dt_imageio_storage_module_t)
 // 3.6.0 was 7.0.0 (added naming to events, selections, and actions)
+// 3.8.0 was 8.0.0 (moved to lua 5.4 and added some events)
 /* incompatible API change */
 #define LUA_API_VERSION_MAJOR 8
 /* backward compatible API change */
@@ -42,7 +43,7 @@
 /* bugfixes that should not change anything to the API */
 #define LUA_API_VERSION_PATCH 0
 /* suffix for unstable version */
-#define LUA_API_VERSION_SUFFIX "dev"
+#define LUA_API_VERSION_SUFFIX ""
 
 /** initialize lua stuff at DT start time */
 int dt_lua_init_configuration(lua_State *L);
