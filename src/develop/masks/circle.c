1a7e2e259743475a830d06bb6dbc1be5248cde3b
Pascal Obry <pascal@obry.net>
2021-12-02 20:10:09 +0100

    masks: make sure just created forms is selected in mask manager.
    Fixes #7723.

--- src/develop/masks/circle.c
+++ src/develop/masks/circle.c
@@ -281,6 +281,7 @@ static int _circle_events_button_pressed(struct dt_iop_module_t *module, float p
       else if(!gui->creation_continuous)
         dt_masks_set_edit_mode(crea_module, DT_MASKS_EDIT_FULL);
       dt_masks_iop_update(crea_module);
+      dt_dev_masks_selection_change(darktable.develop, crea_module, form->formid, TRUE);
       gui->creation_module = NULL;
     }
     else
