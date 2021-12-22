d563a5899a84a07992aabde7ef0a331e320d7553
Miloš Komarčević <miloskomarcevic@aim.com>
2021-12-22 16:29:44 +0100

    Enable CLANG64 build on Win

--- src/win/win.h
+++ src/win/win.h
@@ -1,14 +1,7 @@
 #pragma once
 
-#ifdef __MSVCRT_VERSION__
-#undef __MSVCRT_VERSION__
-#endif
-#define __MSVCRT_VERSION__ 0x0700
-
 #define XMD_H
 
-#undef _WIN32_WINNT
-#define _WIN32_WINNT 0x0600
 #include <winsock2.h>
 #include <windows.h>
 #include <psapi.h>
