f5d4a93ecd1f8b254988c862c236565aa7f62302
Diederik ter Rahe <dterrahe@yahoo.com>
2022-01-22 19:43:55 -0500

    make color calibration white balance picker shortcuttable

--- src/iop/channelmixerrgb.c
+++ src/iop/channelmixerrgb.c
@@ -3864,6 +3864,7 @@ void gui_init(struct dt_iop_module_t *self)
   gtk_box_pack_start(GTK_BOX(hbox), g->illum_color, TRUE, TRUE, 0);
 
   g->color_picker = dt_color_picker_new(self, DT_COLOR_PICKER_AREA, hbox);
+  dt_action_define_iop(self, NULL, N_("picker"), g->color_picker, &dt_action_def_toggle);
   gtk_widget_set_tooltip_text(g->color_picker, _("set white balance to detected from area"));
 
   gtk_box_pack_start(GTK_BOX(self->widget), GTK_WIDGET(hbox), FALSE, FALSE, 0);
