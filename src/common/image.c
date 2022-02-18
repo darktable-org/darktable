1d61aa53fb0fb92e0fe1f6523f3524c05bbe2181
Pascal Obry <pascal@obry.net>
2022-02-18 23:40:40 +0100

    Revert 20c657c which introduced a nested transaction.

--- src/common/image.c
+++ src/common/image.c
@@ -1490,8 +1490,6 @@ static uint32_t _image_import_internal(const int32_t film_id, const char *filena
     g_free(extra_file);
   }
 
-  dt_database_start_transaction(darktable.db);
-
   //insert a v0 record (which may be updated later if no v0 xmp exists)
   DT_DEBUG_SQLITE3_PREPARE_V2
     (dt_database_get(darktable.db),
@@ -1649,8 +1647,6 @@ static uint32_t _image_import_internal(const int32_t film_id, const char *filena
   //synch database entries to xmp
   if(xmp_mode == DT_WRITE_XMP_ALWAYS)
     dt_image_synch_all_xmp(normalized_filename);
-
-  dt_database_release_transaction(darktable.db);
 
   g_free(imgfname);
   g_free(basename);
