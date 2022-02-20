74486bf71f45e897a72b7282c67901e115310574
hanno@schwalm-bremen.de <hanno@schwalm-bremen.de>
2022-02-20 19:37:38 +0100

    remove the --enforce-tiling option
    - was not as usefull as intended
    - the new minimal system emulation works by far better and can be tested at runtime

--- src/common/darktable.h
+++ src/common/darktable.h
@@ -268,8 +268,7 @@ typedef enum dt_debug_thread_t
   DT_DEBUG_SIGNAL         = 1 << 20,
   DT_DEBUG_PARAMS         = 1 << 21,
   DT_DEBUG_DEMOSAIC       = 1 << 22,
-  DT_DEBUG_TILING         = 1 << 23,
-  DT_DEBUG_ACT_ON         = 1 << 24
+  DT_DEBUG_ACT_ON         = 1 << 23
 } dt_debug_thread_t;
 
 typedef struct dt_codepath_t
