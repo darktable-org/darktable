cc193bb7e887856d9c22c3416c351085152ca2a6
phweyland <philippe.weyland@laposte.net>
2022-02-28 15:53:10 +0100

    watermark: do provide full image path to variables

--- src/iop/watermark.c
+++ src/iop/watermark.c
@@ -507,7 +507,10 @@ static gchar *_watermark_get_svgdoc(dt_iop_module_t *self, dt_iop_watermark_data
     const int32_t flags = dt_lib_export_metadata_get_conf_flags();
     dt_variables_params_t *params;
     dt_variables_params_init(&params);
-    params->filename = image->filename;
+    gboolean from_cache = FALSE;
+    char image_path[PATH_MAX] = { 0 };
+    dt_image_full_path(image->id, image_path, sizeof(image_path), &from_cache);
+    params->filename = image_path;
     params->jobcode = "infos";
     params->sequence = 0;
     params->imgid = image->id;
