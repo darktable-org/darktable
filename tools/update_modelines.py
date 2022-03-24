693c8a1df4eb225a74c1549cc9377a406f45abce
maruncz <marun1@email.cz>
2022-03-24 06:55:04 +0100

    modelines: refactored script

--- tools/update_modelines.py
+++ tools/update_modelines.py
@@ -16,70 +16,79 @@ class estate(Enum):
 	inside=3
 	end=4
 
-state = estate.none
-begin = -1
-end = -1
-
-lines = []
-
-def process_line(line, lineno):
-	global state
-	global begin
-	global end
-	if(state == estate.none):
-		if(line.startswith(CLANG_OFF)):
-			state=estate.start
-		return
-	elif(state == estate.start):
-		if(line.startswith('// modelines:')):
-			state=estate.inside
-			begin = lineno-1
-		else:
-			state=estate.none
-		return
-	elif(state == estate.inside):
-		if(line.startswith(CLANG_ON)):
-			state=estate.end
-			end = lineno
-		return
-	elif(state == estate.end):
-		if(not line.strip()):
-			end = lineno
-		else:
-			state=estate.none
-		return
+class modelines_updater_t:
+	def __init__(self, filename):
+		self.filename = filename
+		self.state = estate.none
+		self.begin = -1
+		self.end = -1
+		self.lines = []
 		
-def remove_lines():
-	with open(sys.argv[1],'w') as file:
-		for lineno,line in enumerate(lines):
-			if((lineno >= begin) and (lineno <= end)):
-				continue
-			if(line.startswith('// modelines')):
-				continue
-			if(line.startswith('// vim')):
-				continue
-			if(line.startswith('// kate')):
-				continue
-			file.write(line)
+	def load_lines(self):
+		with open(self.filename,'r') as file:
+			self.lines = file.readlines()
+		
+	def process_line(self, line, lineno):
+		if(self.state == estate.none):
+			if(line.startswith(CLANG_OFF)):
+				self.state=estate.start
+			return
+		elif(self.state == estate.start):
+			if(line.startswith('// modelines:')):
+				self.state=estate.inside
+				self.begin = lineno-1
+			else:
+				self.state=estate.none
+			return
+		elif(self.state == estate.inside):
+			if(line.startswith(CLANG_ON)):
+				self.state=estate.end
+				self.end = lineno
+			return
+		elif(self.state == estate.end):
+			if(not line.strip()):
+				self.end = lineno
+			else:
+				self.state=estate.none
+			return
+		
+	def remove_lines(self):
+		with open(sys.argv[1],'w') as file:
+			for lineno,line in enumerate(self.lines):
+				if((lineno >= self.begin) and (lineno <= self.end)):
+					continue
+				if(line.startswith('// modelines')):
+					continue
+				if(line.startswith('// vim')):
+					continue
+				if(line.startswith('// kate')):
+					continue
+				file.write(line)
+				
+	def write_file(self):
+		with open(self.filename,'a') as file:
+			file.write(CLANG_OFF)
+			file.write(NOTIFICATION_LINE)
+			file.write(VIM_MODELINE)
+			file.write(KATE_MODELINE)
+			file.write(CLANG_ON)
+			file.write('\n')
+			
+	def update(self):
+		print('parsing {}'.format(self.filename));
+		self.load_lines()
+		for lineno,line in enumerate(self.lines):
+			self.process_line(line, lineno)
+		if((self.begin == -1) != (self.end == -1)):
+			raise RuntimeError("parsing error")
+		print('removing old modelines')
+		self.remove_lines()
+		print('writing file')
+		self.write_file()
+			
+
 
 if __name__ == "__main__":
-	if(sys.argv[1] is None):
-		raise RuntimeError("no input file")
-	print('parsing {}'.format(sys.argv[1]));
-	with open(sys.argv[1],'r') as file:
-		lines = file.readlines()
-	for lineno,line in enumerate(lines):
-		process_line(line, lineno)
-	if((begin == -1) != (end == -1)):
-		raise RuntimeError("parsing error")
-	print('removing old modelines');
-	remove_lines()
-	with open(sys.argv[1],'a') as file:
-		print('adding new modelines');
-		file.write(CLANG_OFF)
-		file.write(NOTIFICATION_LINE)
-		file.write(VIM_MODELINE)
-		file.write(KATE_MODELINE)
-		file.write(CLANG_ON)
-		file.write('\n')
+	updater = modelines_updater_t(sys.argv[1])
+	updater.update()
 
