darktable-org/darktable

commit 41052e900def5fdfa55b525d6a990b3939c0ccb9

image flags : set image type from the beginning
using extension first. This will eventually be changed once the image is load for real

src/common/imageio.c
@@ -78,6 +78,45 @@
 #include "lua/image.h"
 #endif
 
+// note `dng` is not included anywhere as it can be anything. For this images we'll need to open it for "real"
+static const gchar *_supported_raw[]
+    = { "3fr", "ari", "arw", "bay", "cr2", "cr3", "crw", "dc2", "dcr", "erf", "fff",
+        "ia",  "iiq", "k25", "kc2", "kdc", "mdc", "mef", "mos", "mrw", "nef", "nrw",
+        "orf", "pef", "raf", "raw", "rw2", "rwl", "sr2", "srf", "srw", "sti", "x3f" };
+static const gchar *_supported_ldr[]
+    = { "bmp",  "bmq", "cap", "cine", "cs1", "dcm", "gif", "gpr", "j2c", "j2k", "jng", "jp2", "jpc", "jpeg", "jpg",
+        "miff", "mng", "ori", "pbm",  "pfm", "pgm", "png", "pnm", "ppm", "pxn", "qtk", "rdc", "tif", "tiff" };
+static const gchar *_supported_hdr[] = { "avif", "exr", "hdr", "heic", "heif", "hif", "pfm" };
+
+// get the type of image from its extension
+dt_image_flags_t dt_imageio_get_type_from_extension(const char *extension)
+{
+  const char *ext = g_str_has_prefix(extension, ".") ? extension + 1 : extension;
+  for(const char **i = _supported_raw; *i != NULL; i++)
+  {
+    if(!g_ascii_strncasecmp(ext, *i, strlen(*i)))
+    {
+      return DT_IMAGE_RAW;
+    }
+  }
+  for(const char **i = _supported_hdr; *i != NULL; i++)
+  {
+    if(!g_ascii_strncasecmp(ext, *i, strlen(*i)))
+    {
+      return DT_IMAGE_HDR;
+    }
+  }
+  for(const char **i = _supported_ldr; *i != NULL; i++)
+  {
+    if(!g_ascii_strncasecmp(ext, *i, strlen(*i)))
+    {
+      return DT_IMAGE_LDR;
+    }
+  }
+  // default to 0
+  return 0;
+}
+
 // load a full-res thumbnail:
 int dt_imageio_large_thumbnail(const char *filename, uint8_t **buffer, int32_t *width, int32_t *height,
                                dt_colorspaces_color_profile_type_t *color_space)

src/common/database.c
@@ -49,7 +49,7 @@
 
 // whenever _create_*_schema() gets changed you HAVE to bump this version and add an update path to
 // _upgrade_*_schema_step()!
-#define CURRENT_DATABASE_VERSION_LIBRARY 35
+#define CURRENT_DATABASE_VERSION_LIBRARY 36
 #define CURRENT_DATABASE_VERSION_DATA     9
 
 // #define USE_NESTED_TRANSACTIONS
@@ -2115,6 +2115,44 @@ static int _upgrade_library_schema_step(dt_database_t *db, int version)
     sqlite3_exec(db->handle, "COMMIT", NULL, NULL, NULL);
     sqlite3_exec(db->handle, "PRAGMA foreign_keys = ON", NULL, NULL, NULL);
     new_version = 35;
+  }
+  else if(version == 35)
+  {
+    TRY_EXEC("CREATE TABLE main.images_new (id INTEGER, filename VARCHAR, flags INTEGER)",
+             "[init] can't create new images table\n");
+
+    gchar *query = g_strdup_printf("INSERT INTO `images_new` "
+                                   "SELECT id, filename, flags"
+                                   " FROM images"
+                                   " WHERE (flags & %d == 0)",
+                                   DT_IMAGE_RAW | DT_IMAGE_LDR | DT_IMAGE_HDR);
+    TRY_EXEC(query, "[init] can't copy back from images\n");
+
+    TRY_PREPARE(stmt, "SELECT id, filename, flags FROM `images_new`",
+                "[init] can't prepare selecting images flags\n");
+
+    while(sqlite3_step(stmt) == SQLITE_ROW)
+    {
+      sqlite3_stmt *stmt2;
+      sqlite3_prepare_v2(db->handle,
+                         "UPDATE `images` SET"
+                         " (flags) = "
+                         " (?2) WHERE id = ?1",
+                         -1, &stmt2, NULL);
+      sqlite3_bind_int(stmt2, 1, sqlite3_column_int(stmt, 0));
+
+      dt_image_flags_t flags = sqlite3_column_int(stmt, 2);
+      gchar *ext = g_strrstr((const char *)sqlite3_column_text(stmt, 1), ".");
+      flags |= dt_imageio_get_type_from_extension(ext);
+      sqlite3_bind_int(stmt2, 2, flags);
+
+      TRY_STEP(stmt2, SQLITE_DONE, "[init] can't update flags\n");
+      sqlite3_finalize(stmt2);
+    }
+    sqlite3_finalize(stmt);
+
+    TRY_EXEC("DROP TABLE `images_new`", "[init] can't drop temp images table\n");
+    new_version = 36;
   }
   else
     new_version = version; // should be the fallback so that calling code sees that we are in an infinite loop

src/common/image.c
@@ -1505,6 +1505,9 @@ static uint32_t _image_import_internal(const int32_t film_id, const char *filena
   // also need to set the no-legacy bit, to make sure we get the right presets (new ones)
   uint32_t flags = dt_conf_get_int("ui_last/import_initial_rating");
   flags |= DT_IMAGE_NO_LEGACY_PRESETS;
+  // and we set the type of image flag (from extension for now)
+  gchar *extension = g_strrstr(imgfname, ".");
+  flags |= dt_imageio_get_type_from_extension(extension);
   // set the bits in flags that indicate if any of the extra files (.txt, .wav) are present
   char *extra_file = dt_image_get_audio_path_from_path(normalized_filename);
   if(extra_file)

src/common/imageio.h
@@ -114,6 +114,9 @@ gboolean dt_imageio_lookup_makermodel(const char *maker, const char *model,
                                       char *mk, int mk_len, char *md, int md_len,
                                       char *al, int al_len);
 
+// get the type of image from its extension
+dt_image_flags_t dt_imageio_get_type_from_extension(const char *extension);
+
 // clang-format off
 // modelines: These editor modelines have been set for all relevant files by tools/update_modelines.py
 // vim: shiftwidth=2 expandtab tabstop=2 cindent

commit 6baf817426ac326a679e3aba1ca9fd01a9ddbd9b

fix a crash caused by #11572, fixes #11894

src/common/imageio.c
@@ -82,11 +82,11 @@
 static const gchar *_supported_raw[]
     = { "3fr", "ari", "arw", "bay", "cr2", "cr3", "crw", "dc2", "dcr", "erf", "fff",
         "ia",  "iiq", "k25", "kc2", "kdc", "mdc", "mef", "mos", "mrw", "nef", "nrw",
-        "orf", "pef", "raf", "raw", "rw2", "rwl", "sr2", "srf", "srw", "sti", "x3f" };
+        "orf", "pef", "raf", "raw", "rw2", "rwl", "sr2", "srf", "srw", "sti", "x3f", NULL };
 static const gchar *_supported_ldr[]
     = { "bmp",  "bmq", "cap", "cine", "cs1", "dcm", "gif", "gpr", "j2c", "j2k", "jng", "jp2", "jpc", "jpeg", "jpg",
-        "miff", "mng", "ori", "pbm",  "pfm", "pgm", "png", "pnm", "ppm", "pxn", "qtk", "rdc", "tif", "tiff" };
-static const gchar *_supported_hdr[] = { "avif", "exr", "hdr", "heic", "heif", "hif", "pfm" };
+        "miff", "mng", "ori", "pbm",  "pfm", "pgm", "png", "pnm", "ppm", "pxn", "qtk", "rdc", "tif", "tiff", NULL };
+static const gchar *_supported_hdr[] = { "avif", "exr", "hdr", "heic", "heif", "hif", "pfm", NULL };
 
 // get the type of image from its extension
 dt_image_flags_t dt_imageio_get_type_from_extension(const char *extension)