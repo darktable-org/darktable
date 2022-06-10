5b6a7b42a2d9c3a5f6182aa3f84bdeaeda827afa
Aurélien PIERRE <aurelien@aurelienpierre.com>
2022-06-10 23:41:55 +0200

    rework scene-referred defaults
    add +0.7 EV exposure (better for Fuji/Nikon)
    use contrast = 1 and 4th order polynomial in filmic

--- src/iop/exposure.c
+++ src/iop/exposure.c
@@ -278,7 +278,7 @@ void init_presets (dt_iop_module_so_t *self)
   dt_gui_presets_add_generic(_("scene-referred default"), self->op, self->version(),
                              &(dt_iop_exposure_params_t){.mode = EXPOSURE_MODE_MANUAL,
                                                          .black = -0.000244140625f,
-                                                         .exposure = 0.5f,
+                                                         .exposure = 0.7f,
                                                          .deflicker_percentile = 50.0f,
                                                          .deflicker_target_level = -4.0f,
                                                          .compensate_exposure_bias = TRUE},
