64f17240e95b2fd10e06105bfe56beda001e7bee
Pascal Obry <pascal@obry.net>
2022-10-25 17:37:44 +0200

    dt_imageio_export_with_flags: Add support for history_end.
    If history_end set to -1, the develop use the whole history.

--- src/common/imageio.c
+++ src/common/imageio.c
@@ -731,7 +731,7 @@ int dt_imageio_export(const int32_t imgid, const char *filename, dt_imageio_modu
 
     return dt_imageio_export_with_flags(imgid, filename, format, format_params, FALSE, FALSE, high_quality, upscale, is_scaling,
                                         FALSE, NULL, copy_metadata, export_masks, icc_type, icc_filename, icc_intent,
-                                        storage, storage_params, num, total, metadata);
+                                        storage, storage_params, num, total, metadata, -1);
   }
 }
 
@@ -744,7 +744,7 @@ int dt_imageio_export_with_flags(const int32_t imgid, const char *filename,
                                  dt_colorspaces_color_profile_type_t icc_type, const gchar *icc_filename,
                                  dt_iop_color_intent_t icc_intent, dt_imageio_module_storage_t *storage,
                                  dt_imageio_module_data_t *storage_params, int num, int total,
-                                 dt_export_metadata_t *metadata)
+                                 dt_export_metadata_t *metadata, const int history_end)
 {
   dt_develop_t dev;
   dt_dev_init(&dev, FALSE);
@@ -783,6 +783,7 @@ int dt_imageio_export_with_flags(const int32_t imgid, const char *filename,
     goto error;
   }
 
+  const int final_history_end = history_end == -1 ? dev.history_end : history_end;
   const gboolean use_style = !thumbnail_export && format_params->style[0] != '\0';
   const gboolean appending = format_params->style_append != FALSE;
   //  If a style is to be applied during export, add the iop params into the history
@@ -797,7 +798,7 @@ int dt_imageio_export_with_flags(const int32_t imgid, const char *filename,
 
     GList *modules_used = NULL;
 
-    dt_dev_pop_history_items_ext(&dev, appending ? dev.history_end : 0);
+    dt_dev_pop_history_items_ext(&dev, appending ? final_history_end : 0);
     dt_ioppr_update_for_style_items(&dev, style_items, appending);
 
     for(GList *st_items = style_items; st_items; st_items = g_list_next(st_items))
@@ -809,6 +810,8 @@ int dt_imageio_export_with_flags(const int32_t imgid, const char *filename,
     g_list_free(modules_used);
     g_list_free_full(style_items, dt_style_item_free);
   }
+  else if(history_end != -1)
+    dt_dev_pop_history_items_ext(&dev, final_history_end);
 
   dt_ioppr_resync_modules_order(&dev);
 
