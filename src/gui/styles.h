6f635bd066ce6dbf849a2065d28e268fb682a382
Pascal Obry <pascal@obry.net>
2022-10-30 21:47:12 +0100

    gui/styles: Add widget to create a style preview plus information.

--- src/gui/styles.h
+++ src/gui/styles.h
@@ -18,15 +18,20 @@
 
 #pragma once
 
+#include <cairo.h>
+
 /** shows a dialog for creating a new style */
 void dt_gui_styles_dialog_new(int imgid);
 
 /** shows a dialog for editing existing style */
 void dt_gui_styles_dialog_edit(const char *name);
 
+cairo_surface_t *dt_gui_get_style_preview(const uint32_t imgid, const char *name);
+
+GtkWidget *dt_gui_style_content_dialog(char *name, const int imgid);
+
 // clang-format off
 // modelines: These editor modelines have been set for all relevant files by tools/update_modelines.py
 // vim: shiftwidth=2 expandtab tabstop=2 cindent
 // kate: tab-indents: off; indent-width 2; replace-tabs on; indent-mode cstyle; remove-trailing-spaces modified;
 // clang-format on
-
