a0ad2e2d1799405a6a70a61325498752c7d2896b
Hanno Schwalm <hanno@schwalm-bremen.de>
2022-11-15 10:01:20 +0100

    Minor bugfixes for `DT_DEV_PIXELPIPE_PREVIEW2` (#12904)
    * Minor bugfixes for `DT_DEV_PIXELPIPE_PREVIEW2`
    
    In a few modules we do some special treatment for preview pixelpipes for performance.
    In a few places there we no checks for preview2
    
    - clipping.c
    - dither.c
    - nlmeans.c

--- src/iop/dither.c
+++ src/iop/dither.c
@@ -327,7 +327,7 @@ static int get_dither_parameters(const dt_iop_dither_data_t *const data, const d
           break;
       }
       // no automatic dithering for preview and thumbnail
-      if(piece->pipe->type & (DT_DEV_PIXELPIPE_PREVIEW | DT_DEV_PIXELPIPE_THUMBNAIL))
+      if(piece->pipe->type & (DT_DEV_PIXELPIPE_PREVIEW | DT_DEV_PIXELPIPE_PREVIEW2 | DT_DEV_PIXELPIPE_THUMBNAIL))
       {
         graymode = -1;
       }
