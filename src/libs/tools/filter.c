eea143bfaf275a2d77de924913ed3cf5e651ce71
Victor Forsiuk <vvforce@gmail.com>
2022-11-19 19:12:07 +0200

    Fix grammar in the GUI string

--- src/libs/tools/filter.c
+++ src/libs/tools/filter.c
@@ -102,7 +102,7 @@ void gui_init(dt_lib_module_t *self)
   gtk_widget_set_valign(self->widget, GTK_ALIGN_CENTER);
 
   GtkWidget *bt = dtgtk_button_new(dtgtk_cairo_paint_presets, 0, NULL);
-  gtk_widget_set_tooltip_text(bt, _("filters preferences"));
+  gtk_widget_set_tooltip_text(bt, _("filter preferences"));
   g_signal_connect(G_OBJECT(bt), "button-press-event", G_CALLBACK(_pref_show), self);
   gtk_box_pack_start(GTK_BOX(self->widget), bt, FALSE, TRUE, 0);
 
