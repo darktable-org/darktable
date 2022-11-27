1f342c2bbd64e57e40b706f715f1495c069191a5
hanno@schwalm-bremen.de <hanno@schwalm-bremen.de>
2022-11-27 19:51:15 +0100

    Bugfix: non-raw highlights reconstruction
    Fixes #12993
    
    See the lengthy discussion in #12998 what was wrong and what needed to be fixed.
    
    A summary of what was wrong: (non-raws are all images with no demosaicer following)
    
    1. non-raws of course don't have the scaling applied by the demosaicer module so we can't set roi_in scale to 1.0 here
    2. so if i expand roi_in to full image data in hlr reconstruction for those images we don't take image data from the
       pixelpipe input data for processing but take image data and crop & scale to what we seem to need.
    3. That step (2) is
       a) bad for quality
       b) is done at a part of the pipeline without OpenCL - so very slow -
       c) takes an extra cacheline without good reason so using more memory
    4. we want a scale of 1.0f here to avoid issues with buffer sizes later in the pipeline
    
    Replaces #12998 as that pr was not readable any more :-)

--- src/iop/highlights.c
+++ src/iop/highlights.c
@@ -1962,19 +1962,36 @@ void modify_roi_in(dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, const d
   *roi_in = *roi_out;
 
   dt_iop_highlights_data_t *d = (dt_iop_highlights_data_t *)piece->data;
-  dt_iop_highlights_gui_data_t *g = (dt_iop_highlights_gui_data_t *)self->gui_data;
-  const gboolean fullpipe = piece->pipe->type & DT_DEV_PIXELPIPE_FULL;
-  const gboolean fullclipped = (g != NULL) ? (g->hlr_mask_mode == DT_HIGHLIGHTS_MASK_CLIPPED) && fullpipe : FALSE;
   const gboolean use_opposing = (d->mode == DT_IOP_HIGHLIGHTS_OPPOSED) || (d->mode == DT_IOP_HIGHLIGHTS_SEGMENTS);
-  
-  if(fullclipped || !use_opposing)
+  const gboolean fullpipe = piece->pipe->type & DT_DEV_PIXELPIPE_FULL;
+  /* When do we need to expand the roi to maximum of the full input data?
+     1. Certainly not if
+       a) any other than opposed or segmentation based algo is used.
+       b) we have another pipe than fullpipe as they all have roi correctly set
+  */
+  if(!use_opposing || !fullpipe)
+    return;
+
+  dt_iop_highlights_gui_data_t *g = (dt_iop_highlights_gui_data_t *)self->gui_data;
+  const gboolean clipmask = (g != NULL) ? (g->hlr_mask_mode == DT_HIGHLIGHTS_MASK_CLIPPED) : FALSE;
+  /*
+       c) Certainly not if we show the clipped mask as that is also safe with current roi
+       d) linear raws all miss the automatic downscaler provided by the demosaicer stage, so
+          the expanding to full image data does not work as we do a downscaling very early in
+          the pixelpipe. So - no quality achieved but really bad performance.
+          FIXME For dt 4.4 we might want to implement a downscaling step for this modules in these cases.
+            For now we don't expand and keep the linear opposed simple.
+          See #12998 and #12993 for lengthy discussions
+  */
+  if((fullpipe && clipmask) || piece->pipe->dsc.filters == 0)
     return;
 
   roi_in->x = 0;
   roi_in->y = 0;
   roi_in->width = piece->buf_in.width;
   roi_in->height = piece->buf_in.height;
-}
+  roi_in->scale = 1.0f;
+ }
 
 void process(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, const void *const ivoid,
              void *const ovoid, const dt_iop_roi_t *const roi_in, const dt_iop_roi_t *const roi_out)
@@ -2036,7 +2053,7 @@ void process(struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, const 
     }
     else
     {
-      _process_linear_opposed(self, piece, ivoid, ovoid, roi_in, roi_out, data, high_quality);
+      _process_linear_opposed(self, piece, ivoid, ovoid, roi_in, roi_out, data);
     }
     return;
   }
@@ -2173,17 +2190,18 @@ void commit_params(struct dt_iop_module_t *self, dt_iop_params_t *p1, dt_dev_pix
   if((d->mode == DT_IOP_HIGHLIGHTS_SEGMENTS) || (d->mode == DT_IOP_HIGHLIGHTS_OPPOSED))
     piece->process_tiling_ready = 0;
 
+  const gboolean fullpipe = piece->pipe->type & DT_DEV_PIXELPIPE_FULL;
   dt_iop_highlights_gui_data_t *g = (dt_iop_highlights_gui_data_t *)self->gui_data;
   if(g)
   {
     const gboolean linear = piece->pipe->dsc.filters == 0;
-    const gboolean fullpipe = piece->pipe->type & DT_DEV_PIXELPIPE_FULL;
     if((g->hlr_mask_mode == DT_HIGHLIGHTS_MASK_CLIPPED) && linear && fullpipe)
       piece->process_cl_ready = FALSE;
   }
   // check for heavy computing here to give an iop cache hint
   const gboolean heavy = (((d->mode == DT_IOP_HIGHLIGHTS_LAPLACIAN) && ((d->iterations * 1<<(2+d->scales)) >= 256))
-                        || (d->mode == DT_IOP_HIGHLIGHTS_SEGMENTS));
+                        || (d->mode == DT_IOP_HIGHLIGHTS_SEGMENTS)
+                        || ((d->mode == DT_IOP_HIGHLIGHTS_OPPOSED) && fullpipe && (piece->pipe->dsc.filters == 0)));
   self->cache_next_important = heavy;
 }
 
