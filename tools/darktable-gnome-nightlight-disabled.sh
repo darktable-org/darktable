67fadd06c5eed90292cad6f2f72d986ab6bd4ae3
Victor Forsiuk <vvforce@gmail.com>
2022-12-29 20:16:53 +0200

    Fix desktop file name in comments and in message string

--- tools/darktable-gnome-nightlight-disabled.sh
+++ tools/darktable-gnome-nightlight-disabled.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # command line: put this file in path before darktable as: /usr/local/bin/darktable 
-# desktop icon: edit /usr/share/applications/darktable.desktop: Exec and TryExec pointing to /usr/local/bin/darktable  
+# desktop icon: edit /usr/share/applications/org.darktable.darktable.desktop: Exec and TryExec pointing to /usr/local/bin/darktable  
 # (ubuntu18.04, dt from git)
 #
 
