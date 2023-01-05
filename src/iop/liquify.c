a0e9226973d15b8dcf6cbd8f6718e07664326c10
Diederik ter Rahe <dterrahe@yahoo.com>
2023-01-05 09:40:11 -0500

    remove unnecessary default implementations of modify_roi_out

--- src/iop/liquify.c
+++ src/iop/liquify.c
@@ -1231,17 +1231,6 @@ static void _build_global_distortion_map(struct dt_iop_module_t *module,
   g_list_free_full(interpolated, free);
 }
 
-// 1st pass: how large would the output be, given this input roi?
-// this is always called with the full buffer before processing.
-void modify_roi_out(struct dt_iop_module_t *module,
-                     struct dt_dev_pixelpipe_iop_t *piece,
-                     dt_iop_roi_t *roi_out,
-                     const dt_iop_roi_t *roi_in)
-{
-  // output is same size as input
-  *roi_out = *roi_in;
-}
-
 // 2nd pass: which roi would this operation need as input to fill the given output region?
 void modify_roi_in(struct dt_iop_module_t *module,
                     struct dt_dev_pixelpipe_iop_t *piece,
