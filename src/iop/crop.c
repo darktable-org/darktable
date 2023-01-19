7f2f6d3396680aefc93321e5d4aa18ad2dcc675e
Diederik ter Rahe <dterrahe@yahoo.com>
2023-01-18 19:41:33 -0500

    in crop module overlay always show crop handle areas

--- src/iop/crop.c
+++ src/iop/crop.c
@@ -108,6 +108,7 @@ typedef struct dt_iop_crop_gui_data_t
   gboolean shift_hold;
   gboolean ctrl_hold;
   gboolean preview_ready;
+  gint64 focus_time;
   dt_gui_collapsible_section_t cs;
 } dt_iop_crop_gui_data_t;
 
@@ -383,6 +384,7 @@ void commit_params(struct dt_iop_module_t *self, dt_iop_params_t *p1, dt_dev_pix
   }
 }
 
+
 static void _event_preview_updated_callback(gpointer instance, dt_iop_module_t *self)
 {
   dt_iop_crop_gui_data_t *g = (dt_iop_crop_gui_data_t *)self->gui_data;
@@ -429,6 +431,8 @@ void gui_focus(struct dt_iop_module_t *self, gboolean in)
   }
   else if(in)
     g->preview_ready = TRUE;
+
+  g->focus_time = g_get_monotonic_time();
 }
 
 void init_pipe(struct dt_iop_module_t *self, dt_dev_pixelpipe_t *pipe, dt_dev_pixelpipe_iop_t *piece)
@@ -1329,12 +1333,25 @@ void gui_post_expose(struct dt_iop_module_t *self, cairo_t *cr, int32_t width, i
     g_object_unref(layout);
   }
 
+  cairo_set_line_width(cr, DT_PIXEL_APPLY_DPI(2.0) / zoom_scale);
+  double alpha = CLAMP(1.0 - (g_get_monotonic_time() - g->focus_time) / 2e6f, 0.0, 1.0);
+  dt_draw_set_color_overlay(cr, TRUE, alpha);
+  const int border = DT_PIXEL_APPLY_DPI(30.0) / zoom_scale;
+
+  cairo_move_to(cr, g->clip_x * wd + border, g->clip_y * ht);
+  cairo_line_to(cr, g->clip_x * wd + border, (g->clip_y + g->clip_h) * ht);
+  cairo_move_to(cr, (g->clip_x + g->clip_w) * wd - border, g->clip_y * ht);
+  cairo_line_to(cr, (g->clip_x + g->clip_w) * wd - border, (g->clip_y + g->clip_h) * ht);
+  cairo_move_to(cr, g->clip_x * wd, g->clip_y * ht + border);
+  cairo_line_to(cr, (g->clip_x + g->clip_w) * wd, g->clip_y * ht + border);
+  cairo_move_to(cr, g->clip_x * wd, (g->clip_y + g->clip_h) * ht - border);
+  cairo_line_to(cr, (g->clip_x + g->clip_w) * wd, (g->clip_y + g->clip_h) * ht - border);
+  cairo_stroke(cr);
+
   // draw crop area guides
   dt_guides_draw(cr, g->clip_x * wd, g->clip_y * ht, g->clip_w * wd, g->clip_h * ht, zoom_scale);
 
-  cairo_set_line_width(cr, DT_PIXEL_APPLY_DPI(2.0) / zoom_scale);
-  dt_draw_set_color_overlay(cr, FALSE, 1.0);
-  const int border = DT_PIXEL_APPLY_DPI(30.0) / zoom_scale;
+  dt_draw_set_color_overlay(cr, TRUE, 1.0);
 
   const _grab_region_t grab = g->cropping ? g->cropping : _gui_get_grab(pzx, pzy, g, border, wd, ht);
 
