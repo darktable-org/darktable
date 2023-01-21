1a4e4a4555c2dc804767ad02494a574e0901486c
hanno@schwalm-bremen.de <hanno@schwalm-bremen.de>
2023-01-21 12:14:23 +0100

    Fix dt_print() verbose checks
    There was a bug leading to console printing if there was -d verbose as the only
    option.

--- src/common/darktable.c
+++ src/common/darktable.c
@@ -1545,9 +1545,8 @@ void dt_cleanup()
 */
 void dt_print(dt_debug_thread_t thread, const char *msg, ...)
 {
-  if(((thread & DT_DEBUG_VERBOSE) && ((darktable.unmuted & DT_DEBUG_VERBOSE) == 0))
-    || !(darktable.unmuted & thread))
-    return;
+  if(((darktable.unmuted & thread) & ~DT_DEBUG_VERBOSE) == 0) return;
+  if((thread & DT_DEBUG_VERBOSE) && !(darktable.unmuted & DT_DEBUG_VERBOSE)) return;
 
   char buf[128];
   char vbuf[2048];
@@ -1564,9 +1563,8 @@ void dt_print(dt_debug_thread_t thread, const char *msg, ...)
 
 void dt_print_nts(dt_debug_thread_t thread, const char *msg, ...)
 {
-  if(((thread & DT_DEBUG_VERBOSE) && ((darktable.unmuted & DT_DEBUG_VERBOSE) == 0))
-    || !(darktable.unmuted & thread))
-    return;
+  if(((darktable.unmuted & thread) & ~DT_DEBUG_VERBOSE) == 0) return;
+  if((thread & DT_DEBUG_VERBOSE) && !(darktable.unmuted & DT_DEBUG_VERBOSE)) return;
 
   char vbuf[2048];
   va_list ap;
