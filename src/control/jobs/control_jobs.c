70ddfbade89996a8f1c9abb1aca5299fb252ec09
Paolo Benvenuto <paolobenve@gmail.com>
2023-01-26 16:41:11 -0500

    revert back change to message

--- src/control/jobs/control_jobs.c
+++ src/control/jobs/control_jobs.c
@@ -1363,7 +1363,10 @@ static int32_t dt_control_export_job_run(dt_job_t *job)
     h = sh < fh ? sh : fh;
 
   const guint total = g_list_length(t);
-  dt_control_log(ngettext("exporting %d image..", "exporting %d images..", total), total);
+  if(total > 0)
+    dt_control_log(ngettext("exporting %d image..", "exporting %d images..", total), total);
+  else
+    dt_control_log(_("no image to export"));
 
   double fraction = 0;
 
