aedc9d438759ccd197b445d56cea89a5af53ee44
Pascal Obry <pascal@obry.net>
2023-02-01 09:19:41 +0100

    Make levels & contraste-brightness-saturation deprecated.

--- src/iop/levels.c
+++ src/iop/levels.c
@@ -104,6 +104,11 @@ typedef struct dt_iop_levels_global_data_t
 } dt_iop_levels_global_data_t;
 
 
+const char *deprecated_msg()
+{
+  return _("this module is deprecated. please use the RGB levels module instead.");
+}
+
 const char *name()
 {
   return _("levels");
@@ -116,7 +121,7 @@ int default_group()
 
 int flags()
 {
-  return IOP_FLAGS_SUPPORTS_BLENDING;
+  return IOP_FLAGS_SUPPORTS_BLENDING | IOP_FLAGS_DEPRECATED;
 }
 
 int default_colorspace(dt_iop_module_t *self, dt_dev_pixelpipe_t *pipe, dt_dev_pixelpipe_iop_t *piece)
@@ -987,4 +992,3 @@ static void dt_iop_levels_autoadjust_callback(GtkRange *range, dt_iop_module_t *
 // vim: shiftwidth=2 expandtab tabstop=2 cindent
 // kate: tab-indents: off; indent-width 2; replace-tabs on; indent-mode cstyle; remove-trailing-spaces modified;
 // clang-format on
-
