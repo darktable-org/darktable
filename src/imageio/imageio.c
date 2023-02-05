a40e5c00c787672f073865d325cd526e928da1e2
Victor Forsiuk <vvforce@gmail.com>
2023-02-05 20:00:48 +0200

    Move setting image flags to the png loader from where it is called

--- src/imageio/imageio.c
+++ src/imageio/imageio.c
@@ -650,17 +650,7 @@ dt_imageio_retval_t dt_imageio_open_ldr(dt_image_t *img,
 #endif
 
   ret = dt_imageio_open_png(img, filename, buf);
-  if(ret == DT_IMAGEIO_OK || ret == DT_IMAGEIO_CACHE_FULL)
-  {
-    img->buf_dsc.cst = IOP_CS_RGB; // png is always RGB
-    img->buf_dsc.filters = 0u;
-    img->flags &= ~DT_IMAGE_RAW;
-    img->flags &= ~DT_IMAGE_S_RAW;
-    img->flags &= ~DT_IMAGE_HDR;
-    img->flags |= DT_IMAGE_LDR;
-    img->loader = LOADER_PNG;
-    return ret;
-  }
+  if(ret == DT_IMAGEIO_OK || ret == DT_IMAGEIO_CACHE_FULL) return ret;
 
 #ifdef HAVE_OPENJPEG
   ret = dt_imageio_open_j2k(img, filename, buf);
