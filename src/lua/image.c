735c008e31b0b214bfe99e8d1a49219cfd27cae5
Romuald du Song <rdusong@tique.org>
2023-02-20 21:40:11 +0100

    only expose aspect_ratio and only in read-only mode

--- src/lua/image.c
+++ src/lua/image.c
@@ -509,8 +509,7 @@ int dt_lua_init_image(lua_State *L)
   luaA_struct_member(L, dt_image_t, filename, const char_filename_length);
   luaA_struct_member(L, dt_image_t, width, const int32_t);
   luaA_struct_member(L, dt_image_t, height, const int32_t);
-  luaA_struct_member(L, dt_image_t, orientation, const int32_t);
-  luaA_struct_member(L, dt_image_t, aspect_ratio, float);
+  luaA_struct_member(L, dt_image_t, aspect_ratio, const float);
 
   luaA_struct_member_name(L, dt_image_t, geoloc.longitude, protected_double, longitude); // set to NAN if value is not set
   luaA_struct_member_name(L, dt_image_t, geoloc.latitude, protected_double, latitude); // set to NAN if value is not set
