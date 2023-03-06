ab37011bddd9f40727cd61b877c8f7b53c2355d9
ralfbrown <ralfbrown@users.noreply.github.com>
2023-03-05 22:17:23 -0500

    remove declaration of eaw_dn_decompose_sse

--- src/common/eaw.c
+++ src/common/eaw.c
@@ -1,6 +1,6 @@
 /*
     This file is part of darktable,
-    Copyright (C) 2017-2021 darktable developers.
+    Copyright (C) 2017-2023 darktable developers.
 
     darktable is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
