dd12b6cd9cca7cc006a4de0799781ef314c7a6de
Victor Forsiuk <vvforce@gmail.com>
2023-03-16 17:41:14 +0200

    Reword and correct comments

--- .ci/ci-script.sh
+++ .ci/ci-script.sh
@@ -16,13 +16,13 @@
 #    You should have received a copy of the GNU General Public License
 #    along with darktable.  If not, see <http://www.gnu.org/licenses/>.
 
-# it is supposed to be run by travis-ci
-# expects a few env variables to be set:
-#   BUILD_DIR - the working directory, where to build
-#   INSTALL_DIR - the installation prefix.
-#   SRC_DIR - read-only directory with git checkout to compile
-#   CC, CXX, CFLAGS, CXXFLAGS are not required, should make sense too
-#   TARGET - either build, skiptest, nofeatures or usermanual
+# This script is supposed to be run by Travis CI or GitHub workflow.
+# It expects a few env variables to be set:
+#   BUILD_DIR - the working directory where the program will be built
+#   INSTALL_DIR - the installation prefix
+#   SRC_DIR - directory with the source code to be compiled
+#   CC, CXX, CFLAGS, CXXFLAGS are optional, but make sense for build
+#   TARGET - either build, skiptest, nofeatures or nofeatures_nosse
 #   ECO - some other flags for cmake
 
 set -ex
