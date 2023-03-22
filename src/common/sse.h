721da990d3214c13282002c4e1fae7f6ea8ea84a
ralfbrown <ralfbrown@users.noreply.github.com>
2023-03-22 01:26:17 -0400

    remove unused functions

--- src/common/sse.h
+++ src/common/sse.h
@@ -124,22 +124,6 @@ static inline __m128 _mm_pow_ps1(__m128 x, float y)
   return _mm_exp2_ps(_mm_mul_ps(_mm_log2_ps(x), _mm_set1_ps(y)));
 }
 
-
-/**
- * Allow access of the content of a SSE vector
- **/
-
-static inline float _mm_vectorGetByIndex( __m128 V, unsigned int i)
-{
-  union {
-    __m128 v;
-    float a[4];
-  } converter;
-
-  converter.v = V;
-  return converter.a[i];
-}
-
 // clang-format off
 // modelines: These editor modelines have been set for all relevant files by tools/update_modelines.py
 // vim: shiftwidth=2 expandtab tabstop=2 cindent
