b23ccdf8ae14f58d1fe37cbb70ff1de5ea3e9d72
ralfbrown <ralfbrown@users.noreply.github.com>
2023-03-22 20:39:55 -0400

    dt_fast_expf_4wide -> dt_vector_exp

--- src/common/math.h
+++ src/common/math.h
@@ -331,27 +331,6 @@ static inline float dt_fast_expf(const float x)
   return u.f;
 }
 
-static inline void dt_fast_expf_4wide(const float x[4], float result[4])
-{
-  // meant for the range [-100.0f, 0.0f]. largest error ~ -0.06 at 0.0f.
-  // will get _a_lot_ worse for x > 0.0f (9000 at 10.0f)..
-  const int i1 = 0x3f800000u;
-  // e^x, the comment would be 2^x
-  const int i2 = 0x402DF854u; // 0x40000000u;
-  // const int k = CLAMPS(i1 + x * (i2 - i1), 0x0u, 0x7fffffffu);
-  // without max clamping (doesn't work for large x, but is faster):
-  union float_int u[4];
-#ifdef _OPENMP
-#pragma omp simd aligned(x, result)
-#endif
-  for(size_t c = 0; c < 4; c++)
-  {
-    const int k0 = i1 + (int)(x[c] * (i2 - i1));
-    u[c].k = k0 > 0 ? k0 : 0;
-    result[c] = u[c].f;
-  }
-}
-
 // fast approximation of 2^-x for 0<x<126
 /****** if you change this function, you need to make the same change in data/kernels/{denoiseprofile,nlmeans}.cl ***/
 static inline float dt_fast_mexp2f(const float x)
