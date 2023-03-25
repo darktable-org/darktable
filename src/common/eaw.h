902e0da07130136cb38a0c9aad243171efe5fad5
ralfbrown <ralfbrown@users.noreply.github.com>
2023-03-24 20:44:50 -0400

    remove SSE code

--- src/common/eaw.h
+++ src/common/eaw.h
@@ -35,9 +35,6 @@ void eaw_synthesize(float *const restrict out, const float *const restrict in, c
                     const float *const restrict thrsf, const float *const restrict boostf,
                     const int32_t width, const int32_t height);
 
-void eaw_decompose_sse2(float *const restrict out, const float *const restrict in, float *const restrict detail,
-                        const int scale, const float sharpen, const int32_t width, const int32_t height);
-
 typedef void((*eaw_dn_decompose_t)(float *const restrict out, const float *const restrict in, float *const restrict detail,
                                    dt_aligned_pixel_t sum_squared, const int scale, const float inv_sigma2,
                                    const int32_t width, const int32_t height));
