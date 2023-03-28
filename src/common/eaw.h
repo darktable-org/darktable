57b9cdebea66f29f9d54e64e967c78c95898d0e6
ralfbrown <ralfbrown@users.noreply.github.com>
2023-03-28 01:06:00 -0400

    optimize edge-aware wavelets for contrast equalizer
    Merge the decompose and synthesize passes, since nothing is done in
    between.  This reduces memory traffic which helps performance at
    higher thread counts.
    
    Perform a number of other optimizations, as well as tweaks to keep the
    compiler from spilling values to RAM.
    
    Passes integration test 0024.
    
    Thr	Master	PR
    1	4300.99	4306.58	 +0.1%
    2	2180.76 2170.35	 -0.4%
    4	1128.66 1094.23	 -3.0%
    8	 599.00  558.62	 -6.7%
    16	 339.51  291.06	-14.2%
    32	 226.11  170.30	-24.6%
    64	 220.58	 153.69	-30.3%

--- src/common/eaw.h
+++ src/common/eaw.h
@@ -29,11 +29,22 @@ typedef void((*eaw_synthesize_t)(float *const out, const float *const in, const 
                                  const float *const restrict thrsf, const float *const restrict boostf,
                                  const int32_t width, const int32_t height));
 
-void eaw_decompose(float *const restrict out, const float *const restrict in, float *const restrict detail,
-                   const int scale, const float sharpen, const int32_t width, const int32_t height) ;
-void eaw_synthesize(float *const restrict out, const float *const restrict in, const float *const restrict detail,
-                    const float *const restrict thrsf, const float *const restrict boostf,
-                    const int32_t width, const int32_t height);
+void eaw_decompose_and_synthesize(float *const restrict out,
+                                  const float *const restrict in,
+                                  float *const restrict accum,
+                                  const int scale,
+                                  const float sharpen,
+                                  const dt_aligned_pixel_t threshold,
+                                  const dt_aligned_pixel_t boost,
+                                  const ssize_t width,
+                                  const ssize_t height) ;
+void eaw_synthesize(float *const restrict out,
+                    const float *const restrict in,
+                    const float *const restrict detail,
+                    const float *const restrict thrsf,
+                    const float *const restrict boostf,
+                    const int32_t width,
+                    const int32_t height);
 
 typedef void((*eaw_dn_decompose_t)(float *const restrict out, const float *const restrict in, float *const restrict detail,
                                    dt_aligned_pixel_t sum_squared, const int scale, const float inv_sigma2,
