58498350dcad1bec3530ab76064564b74b983c48
ralfbrown <ralfbrown@users.noreply.github.com>
2023-03-30 01:32:07 -0400

    channelmixerrgb: adjust position of GCC optimization pragma
    The local optimization flags improve performance, but not as much as
    they could because they are placed after include files with quite a
    few inline functions used by the main file.  Putting the pragma before
    the includes yields a 12-17% speedup compared to current master,
    depending on the selected adaptation method.
    
    Spot checks with removing the local optimization flags instead results
    in a comparable slowdown compared to master.
    
    Linear Bradford
    Thr     Master  OptFlags
    1       2595.51 2226.28 -14.2%
    2       1299.22 1113.71 -14.2%
    4        650.05  556.91 -14.3%
    8        325.05  278.61 -14.2%
    16       162.76  139.53 -14.2%
    32        81.95   69.84 -14.7%
    64        48.18   40.97 -14.9%
    
    CIECAM16
    Thr     Master  OptFlags
    1       2585.85 2231.41 -13.7%
    2       1294.87 1116.45 -13.7%
    4        648.23  558.41 -13.8%
    8        324.13  279.45 -13.7%
    16       162.44  139.87 -13.8%
    32        81.40   70.24 -13.7%
    64        48.07   41.50 -13.6%
    
    Non-linear Bradford
    Thr     Master  OptFlags
    1       2906.99 2538.79 -12.6%
    2       1458.17 1269.01 -12.9%
    4        728.68  635.13 -12.8%
    8        365.26  317.71 -13.0%
    16       182.99  158.84 -13.1%
    32        91.59   79.55 -13.1%
    64        52.51   46.47 -11.5%
    
    XYZ
    Thr     Master  OptFlags
    1       2191.88 1799.36 -17.9%
    2       1095.94  901.09 -17.7%
    4        548.93  450.60 -17.9%
    8        274.53  225.73 -17.7%
    16       137.34  113.24 -17.5%
    32        68.74   57.50 -16.3%
    64        41.46   34.04 -17.8%

--- src/iop/channelmixerrgb.c
+++ src/iop/channelmixerrgb.c
@@ -16,6 +16,23 @@
   along with darktable.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/** Note :
+ * we use finite-math-only and fast-math because divisions by zero are manually avoided in the code
+ * fp-contract=fast enables hardware-accelerated Fused Multiply-Add
+ * the rest is loop reorganization and vectorization optimization
+ **/
+#if defined(__GNUC__)
+#pragma GCC optimize ("unroll-loops", "tree-loop-if-convert", \
+                      "tree-loop-distribution", "no-strict-aliasing", \
+                      "loop-interchange", "loop-nest-optimize", "tree-loop-im", \
+                      "unswitch-loops", "tree-loop-ivcanon", "ira-loop-pressure", \
+                      "split-ivs-in-unroller", "variable-expansion-in-unroller", \
+                      "split-loops", "ivopts", "predictive-commoning",\
+                      "tree-loop-linear", "loop-block", "loop-strip-mine", \
+                      "finite-math-only", "fp-contract=fast", "fast-math", \
+                      "tree-vectorize", "no-math-errno")
+#endif
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
@@ -48,24 +65,6 @@
 #include <time.h>
 
 DT_MODULE_INTROSPECTION(3, dt_iop_channelmixer_rgb_params_t)
-
-/** Note :
- * we use finite-math-only and fast-math because divisions by zero are manually avoided in the code
- * fp-contract=fast enables hardware-accelerated Fused Multiply-Add
- * the rest is loop reorganization and vectorization optimization
- **/
-#if defined(__GNUC__)
-#pragma GCC optimize ("unroll-loops", "tree-loop-if-convert", \
-                      "tree-loop-distribution", "no-strict-aliasing", \
-                      "loop-interchange", "loop-nest-optimize", "tree-loop-im", \
-                      "unswitch-loops", "tree-loop-ivcanon", "ira-loop-pressure", \
-                      "split-ivs-in-unroller", "variable-expansion-in-unroller", \
-                      "split-loops", "ivopts", "predictive-commoning",\
-                      "tree-loop-linear", "loop-block", "loop-strip-mine", \
-                      "finite-math-only", "fp-contract=fast", "fast-math", \
-                      "tree-vectorize", "no-math-errno")
-#endif
-
 
 #define CHANNEL_SIZE 4
 #define INVERSE_SQRT_3 0.5773502691896258f
