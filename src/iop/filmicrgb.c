8f24448b5fa9edd36110d4d6038885e6d9d40895
Pascal Obry <github@obry.net>
2023-04-26 10:46:08 +0200

    filmicrgb.c: Fix typo.
    Co-authored-by: Chris Elston <elstoc@users.noreply.github.com>

--- src/iop/filmicrgb.c
+++ src/iop/filmicrgb.c
@@ -4568,7 +4568,7 @@ void gui_changed(dt_iop_module_t *self, GtkWidget *w, void *previous)
     {
       dt_bauhaus_widget_set_label(g->saturation, NULL, N_("highlights saturation mix"));
       gtk_widget_set_tooltip_text(g->saturation, _("positive values ensure saturation is kept unchanged over the whole range.\n"
-                                                   "negative values bleache highlights at constant hue and luminance.\n"
+                                                   "negative values bleach highlights at constant hue and luminance.\n"
                                                    "zero is an equal mix of both strategies."));
       gtk_widget_set_visible(GTK_WIDGET(g->preserve_color), FALSE);
     }
