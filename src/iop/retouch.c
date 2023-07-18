bed3085674684f36e7a24cfaefe6df7a1d9cc73a
Pascal Obry <pascal@obry.net>
2023-07-18 17:18:55 +0200

    retouch: Use blend data for piece.
    Fixes #14888.

--- src/iop/retouch.c
+++ src/iop/retouch.c
@@ -2530,7 +2530,7 @@ static void rt_compute_roi_in(struct dt_iop_module_t *self,
                               int *_roiy)
 {
   dt_iop_retouch_params_t *p = (dt_iop_retouch_params_t *)piece->data;
-  dt_develop_blend_params_t *bp = self->blend_params;
+  dt_develop_blend_params_t *bp = piece->blendop_data;
 
   int roir = *_roir;
   int roib = *_roib;
@@ -2635,7 +2635,7 @@ static void rt_extend_roi_in_from_source_clones(struct dt_iop_module_t *self,
                                                 int *_roiy)
 {
   dt_iop_retouch_params_t *p = (dt_iop_retouch_params_t *)piece->data;
-  dt_develop_blend_params_t *bp = self->blend_params;
+  dt_develop_blend_params_t *bp = piece->blendop_data;
 
   int roir = *_roir;
   int roib = *_roib;
@@ -2728,7 +2728,7 @@ static void rt_extend_roi_in_for_clone(struct dt_iop_module_t *self,
                                        int *_roiy)
 {
   dt_iop_retouch_params_t *p = (dt_iop_retouch_params_t *)piece->data;
-  dt_develop_blend_params_t *bp = self->blend_params;
+  dt_develop_blend_params_t *bp = piece->blendop_data;
 
   int roir = *_roir;
   int roib = *_roib;
