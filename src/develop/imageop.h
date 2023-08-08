2b928edbc0d91dfc3bce809d3c49163af85178a9
Pascal Obry <pascal@obry.net>
2023-08-08 22:33:16 +0200

    Refactor iop legacy_params handling.
    Preparatory work for supporting incremental migration of iop
    legacy params.

--- src/develop/imageop.h
+++ src/develop/imageop.h
@@ -349,6 +349,12 @@ gboolean dt_iop_load_module(dt_iop_module_t *module,
                        struct dt_develop_t *dev);
 /** calls module->cleanup and closes the dl connection. */
 void dt_iop_cleanup_module(dt_iop_module_t *module);
+/** migrate legacy params */
+int dt_iop_legacy_params(dt_iop_module_t *module,
+                         const void *const old_params,
+                         const int old_version,
+                         void *new_params,
+                         const int new_version);
 /** initialize pipe. */
 void dt_iop_init_pipe(struct dt_iop_module_t *module,
                       struct dt_dev_pixelpipe_t *pipe,
