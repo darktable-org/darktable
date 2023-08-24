95eada1a35d4744fc271aaeee034d651836af551
Mario Zimmermann <mail@zisoft.de>
2023-08-24 18:04:46 +0200

    separate about dialog

--- src/libs/tools/darktable.c
+++ src/libs/tools/darktable.c
@@ -27,9 +27,7 @@
 #include "gui/gtk.h"
 #include "libs/lib.h"
 #include "libs/lib_api.h"
-#ifdef GDK_WINDOWING_QUARTZ
-#include "osx/osx.h"
-#endif
+#include "gui/about.h"
 
 #include <librsvg/rsvg.h>
 // ugh, ugly hack. why do people break stuff all the time?
@@ -57,8 +55,6 @@ static gboolean _lib_darktable_draw_callback(GtkWidget *widget, cairo_t *cr, gpo
 /* button press callback */
 static gboolean _lib_darktable_button_press_callback(GtkWidget *widget, GdkEventButton *event,
                                                      gpointer user_data);
-/* show the about dialog */
-static void _lib_darktable_show_about_dialog();
 
 const char *name(dt_lib_module_t *self)
 {
@@ -268,47 +264,8 @@ static gboolean _lib_darktable_button_press_callback(GtkWidget *widget, GdkEvent
                                                      gpointer user_data)
 {
   /* show about box */
-  _lib_darktable_show_about_dialog();
+  darktable_show_about_dialog();
   return TRUE;
-}
-
-static void _lib_darktable_show_about_dialog()
-{
-  GtkWidget *dialog = gtk_about_dialog_new();
-  gtk_widget_set_name (dialog, "about-dialog");
-#ifdef GDK_WINDOWING_QUARTZ
-  dt_osx_disallow_fullscreen(dialog);
-#endif
-  gtk_about_dialog_set_program_name(GTK_ABOUT_DIALOG(dialog), PACKAGE_NAME);
-  gtk_about_dialog_set_version(GTK_ABOUT_DIALOG(dialog), darktable_package_version);
-  char *copyright = g_strdup_printf(_("copyright (c) the authors 2009-%s"), darktable_last_commit_year);
-  gtk_about_dialog_set_copyright(GTK_ABOUT_DIALOG(dialog), copyright);
-  g_free(copyright);
-  gtk_about_dialog_set_comments(GTK_ABOUT_DIALOG(dialog),
-                                _("organize and develop images from digital cameras"));
-  gtk_about_dialog_set_website(GTK_ABOUT_DIALOG(dialog), "https://www.darktable.org/");
-  gtk_about_dialog_set_website_label(GTK_ABOUT_DIALOG(dialog), "website");
-  dt_logo_season_t season = dt_util_get_logo_season();
-  char *icon;
-  if(season != DT_LOGO_SEASON_NONE)
-    icon = g_strdup_printf("darktable-%d", (int)season);
-  else
-    icon = g_strdup("darktable");
-  gtk_about_dialog_set_logo_icon_name(GTK_ABOUT_DIALOG(dialog), icon);
-  g_free(icon);
-
-  const char *str = _("all those of you that made previous releases possible");
-
-#include "tools/darktable_authors.h"
-
-  const char *final[] = {str, NULL };
-  gtk_about_dialog_add_credit_section (GTK_ABOUT_DIALOG(dialog), _("and..."), final);
-
-  gtk_about_dialog_set_translator_credits(GTK_ABOUT_DIALOG(dialog), _("translator-credits"));
-
-  gtk_window_set_transient_for(GTK_WINDOW(dialog), GTK_WINDOW(dt_ui_main_window(darktable.gui->ui)));
-  gtk_dialog_run(GTK_DIALOG(dialog));
-  gtk_widget_destroy(dialog);
 }
 // clang-format off
 // modelines: These editor modelines have been set for all relevant files by tools/update_modelines.py
