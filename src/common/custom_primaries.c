b57ff71b5897bd10324b5ac12e0d25af5024abe2
Pascal Obry <pascal@obry.net>
2023-09-03 21:57:17 +0200

    Code reformatting.

--- src/common/custom_primaries.c
+++ src/common/custom_primaries.c
@@ -20,24 +20,37 @@
 #include <float.h>
 
 
-static inline float _determinant(const float a, const float b, const float c, const float d)
+static inline float _determinant(const float a,
+                                 const float b,
+                                 const float c,
+                                 const float d)
 {
   return a * d - b * c;
 }
 
-static inline float _intersect_line_segments(const float x1, const float y1, const float x2, const float y2,
-                                             const float x3, const float y3, const float x4, const float y4)
+static inline float _intersect_line_segments(const float x1,
+                                             const float y1,
+                                             const float x2,
+                                             const float y2,
+                                             const float x3,
+                                             const float y3,
+                                             const float x4,
+                                             const float y4)
 {
   const float denominator = _determinant(x1 - x2, x3 - x4, y1 - y2, y3 - y4);
-  if(denominator == 0.f) return FLT_MAX; // lines don't intersect
+  if(denominator == 0.f)
+    return FLT_MAX; // lines don't intersect
 
   const float t = _determinant(x1 - x3, x3 - x4, y1 - y3, y3 - y4) / denominator;
-  if(t >= 0.f) return t;
+  if(t >= 0.f)
+    return t;
   return FLT_MAX; // intersection is in the wrong direction
 }
 
-static inline float _find_distance_to_edge(const dt_iop_order_iccprofile_info_t *const profile,
-                                           const float cos_angle, const float sin_angle)
+static inline float _find_distance_to_edge
+  (const dt_iop_order_iccprofile_info_t *const profile,
+   const float cos_angle,
+   const float sin_angle)
 {
   const float x1 = profile->whitepoint[0];
   const float y1 = profile->whitepoint[1];
@@ -53,14 +66,18 @@ static inline float _find_distance_to_edge(const dt_iop_order_iccprofile_info_t 
     const float x4 = profile->primaries[next_i][0];
     const float y4 = profile->primaries[next_i][1];
     const float distance = _intersect_line_segments(x1, y1, x2, y2, x3, y3, x4, y4);
-    if(distance < distance_to_edge) distance_to_edge = distance;
+    if(distance < distance_to_edge)
+      distance_to_edge = distance;
   }
 
   return distance_to_edge;
 }
 
-void dt_rotate_and_scale_primary(const dt_iop_order_iccprofile_info_t *const profile, const float scaling,
-                                 const float rotation, size_t primary_index, float new_xy[2])
+void dt_rotate_and_scale_primary(const dt_iop_order_iccprofile_info_t *const profile,
+                                 const float scaling,
+                                 const float rotation,
+                                 const size_t primary_index,
+                                 float new_xy[2])
 {
   // Generate a custom set of tone mapping primaries by scaling
   // and rotating the primaries of the given profile.
