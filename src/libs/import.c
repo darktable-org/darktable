5127141c8b478de223403e569bc880a839127bff
Pascal Obry <pascal@obry.net>
2023-09-10 20:54:42 +0200

    imports: Minor reformatting.

--- src/libs/import.c
+++ src/libs/import.c
@@ -682,7 +682,8 @@ static guint _import_set_file_list(const gchar *folder, const int folder_lgth,
       if (is_hidden)
         continue;
 
-      const time_t datetime = g_file_info_get_attribute_uint64(info, G_FILE_ATTRIBUTE_TIME_MODIFIED);
+      const time_t datetime =
+        g_file_info_get_attribute_uint64(info, G_FILE_ATTRIBUTE_TIME_MODIFIED);
       GDateTime *dt_datetime = g_date_time_new_from_unix_local(datetime);
       gchar *dt_txt = g_date_time_format(dt_datetime, "%x %X");
       const GFileType filetype = g_file_info_get_file_type(info);
