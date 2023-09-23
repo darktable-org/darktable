0688e9ea9e60748389ab7e1f46e4b8eb7ed4680f
fabiosirna <fabio@fabiosirna.com>
2023-09-23 14:37:29 +0200

    15120 typo

--- src/common/darktable.c
+++ src/common/darktable.c
@@ -693,7 +693,7 @@ char *version = g_strdup_printf("darktable %s\nCopyright (C) 2012-%s Johannes Ha
 #ifdef HAVE_GMIC
                "  GMIC                   -> ENABLED  - Compressed LUTs supported\n",
 #else
-               "  GMIC                   -> \033[38;5;244mDISABLED\033[0m - Compressed LUTs NOT be supported\n",
+               "  GMIC                   -> \033[38;5;244mDISABLED\033[0m - Compressed LUTs NOT supported\n",
 #endif
 
 #ifdef HAVE_GRAPHICSMAGICK
