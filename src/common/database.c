acf6485379a31bae58c384766f41a3fefa1759f8
Pascal Obry <pascal@obry.net>
2023-10-14 19:27:09 +0200

    Some more legacy_presets removal.
    This is a left over of 98ba6930e82ab0cdea80cf6be5fc0295bf6e6647.

--- src/common/database.c
+++ src/common/database.c
@@ -48,7 +48,7 @@
 
 // whenever _create_*_schema() gets changed you HAVE to bump this version and add an update path to
 // _upgrade_*_schema_step()!
-#define CURRENT_DATABASE_VERSION_LIBRARY 45
+#define CURRENT_DATABASE_VERSION_LIBRARY 46
 #define CURRENT_DATABASE_VERSION_DATA    10
 
 // #define USE_NESTED_TRANSACTIONS
@@ -2597,6 +2597,13 @@ static int _upgrade_library_schema_step(dt_database_t *db, int version)
     sqlite3_exec(db->handle, "PRAGMA foreign_keys = ON", NULL, NULL, NULL);
 
     new_version = 45;
+  }
+  else if(version == 45)
+  {
+    TRY_EXEC("DROP TABLE legacy_presets",
+              "[init] can't drop legacy_presets\n");
+
+    new_version = 46;
   }
   else
     new_version = version; // should be the fallback so that calling code sees that we are in an infinite loop
