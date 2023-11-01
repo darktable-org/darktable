bf7dd8ef4e8a5fb86fe229c0e04803422bec68e8
Roman Lebedev <lebedev.ri@gmail.com>
2023-11-02 02:00:14 +0300

    CMake: sync compiler-versions from RawSpeed

--- cmake/compiler-versions.cmake
+++ cmake/compiler-versions.cmake
@@ -19,16 +19,16 @@
 #    * and the newest one that is available in *both* the
 #      debian stable *and* the latest ubuntu LTS
 #
-# As of the time of writing (2023-03-14), the next (spring) darktable release
-# will happen in 2023-06 ish. By that time:
+# As of the time of writing (2023-10-22), the next (winter) darktable release
+# will happen in 2023-12 ish. By that time:
 # * debian 12 (Bookworm) will have been released,
 #   coming with gcc-12 and LLVM15
 # * Ubuntu 22.04.1 LTS (Jammy Jellyfish) will be the newest LTS,
 #   coming with gcc-12 and LLVM15
-# * macOS 11 (Big Sur) be the oldest supported macOS version,
-#   with the newest supported Xcode version being 13.2 (LLVM12-based !)
+# * macOS 12 (Big Sur) be the oldest supported macOS version,
+#   with the newest supported Xcode version being 14.2 (LLVM14-based !)
 #
-# Therefore, we require GCC12, macOS 11 + Xcode 13.2, and LLVM12.
+# Therefore, we require GCC12, macOS 12 + Xcode 14.2, and LLVM14.
 #
 # The next+1 (fall) darktable release will happen 2023-12 ish,
 # and by that time, macOS 11 will have EOL'd on 2023-10 ish,
@@ -43,21 +43,21 @@ if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_L
   message(SEND_ERROR "GNU C++ compiler version ${CMAKE_CXX_COMPILER_VERSION} is too old and is unsupported. Version 12+ is required.")
 endif()
 
-if(CMAKE_C_COMPILER_ID STREQUAL "Clang" AND CMAKE_C_COMPILER_VERSION VERSION_LESS 12)
-  message(SEND_ERROR "LLVM Clang C compiler version ${CMAKE_C_COMPILER_VERSION} is too old and is unsupported. Version 12+ is required.")
+if(CMAKE_C_COMPILER_ID STREQUAL "Clang" AND CMAKE_C_COMPILER_VERSION VERSION_LESS 14)
+  message(SEND_ERROR "LLVM Clang C compiler version ${CMAKE_C_COMPILER_VERSION} is too old and is unsupported. Version 14+ is required.")
 endif()
-if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 12)
-  message(SEND_ERROR "LLVM Clang C++ compiler version ${CMAKE_CXX_COMPILER_VERSION} is too old and is unsupported. Version 12+ is required.")
-endif()
-
-# XCode 13.2 (apple clang 1300.0.29.3) is based on LLVM12
-if(CMAKE_C_COMPILER_ID STREQUAL "AppleClang" AND CMAKE_C_COMPILER_VERSION VERSION_LESS 13.0.0.13000029)
-  message(SEND_ERROR "XCode (Apple clang) C compiler version ${CMAKE_C_COMPILER_VERSION} is too old and is unsupported. XCode version 13.2+ is required.")
-endif()
-if(CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 13.0.0.13000029)
-  message(SEND_ERROR "XCode (Apple clang) C++ compiler version ${CMAKE_CXX_COMPILER_VERSION} is too old and is unsupported. XCode version 13.2+ is required.")
+if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 14)
+  message(SEND_ERROR "LLVM Clang C++ compiler version ${CMAKE_CXX_COMPILER_VERSION} is too old and is unsupported. Version 14+ is required.")
 endif()
 
-if(CMAKE_OSX_DEPLOYMENT_TARGET AND CMAKE_OSX_DEPLOYMENT_TARGET VERSION_LESS 11.3)
-  message(SEND_ERROR "Targeting OSX version ${CMAKE_OSX_DEPLOYMENT_TARGET} older than 11.3 is unsupported.")
+# XCode 14.2 (apple clang 1400.0.29.202) is based on LLVM14
+if(CMAKE_C_COMPILER_ID STREQUAL "AppleClang" AND CMAKE_C_COMPILER_VERSION VERSION_LESS 14.0.0.14000029)
+  message(SEND_ERROR "XCode (Apple clang) C compiler version ${CMAKE_C_COMPILER_VERSION} is too old and is unsupported. XCode version 14.2+ is required.")
+endif()
+if(CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang" AND CMAKE_CXX_COMPILER_VERSION VERSION_LESS 14.0.0.14000029)
+  message(SEND_ERROR "XCode (Apple clang) C++ compiler version ${CMAKE_CXX_COMPILER_VERSION} is too old and is unsupported. XCode version 14.2+ is required.")
+endif()
+
+if(CMAKE_OSX_DEPLOYMENT_TARGET AND CMAKE_OSX_DEPLOYMENT_TARGET VERSION_LESS 12.5)
+  message(SEND_ERROR "Targeting OSX version ${CMAKE_OSX_DEPLOYMENT_TARGET} older than 12.5 is unsupported.")
 endif()
