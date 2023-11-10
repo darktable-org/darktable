8a7c6439c6cfd966c2b91b76673981e83f6cd965
Diederik ter Rahe <dterrahe@yahoo.com>
2023-11-10 14:16:48 -0500

    switch vignetting module off when crop focused

--- src/iop/vignette.c
+++ src/iop/vignette.c
@@ -134,6 +134,11 @@ int default_group()
   return IOP_GROUP_EFFECT | IOP_GROUP_EFFECTS;
 }
 
+int operation_tags()
+{
+  return IOP_TAG_DECORATION;
+}
+
 dt_iop_colorspace_type_t default_colorspace(dt_iop_module_t *self,
                                             dt_dev_pixelpipe_t *pipe,
                                             dt_dev_pixelpipe_iop_t *piece)
