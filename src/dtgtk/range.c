af26502ceca2990bb42235e17650740f5f706c31
AlicVB <dev@lnaa.fr>
2023-11-25 21:02:00 +0100

    daterange : fix wrong date casting in windows

--- src/dtgtk/range.c
+++ src/dtgtk/range.c
@@ -225,7 +225,7 @@ static gchar *_default_print_date_func(const double value, const gboolean detail
 }
 static gboolean _default_decode_date_func(const gchar *text, double *value)
 {
-  long val = dt_datetime_exif_to_gtimespan(text);
+  GTimeSpan val = dt_datetime_exif_to_gtimespan(text);
   if(val > 0)
   {
     *value = val;
@@ -1748,7 +1748,7 @@ GtkWidget *dtgtk_range_select_new(const gchar *property, const gboolean show_ent
   DT_DEBUG_CONTROL_SIGNAL_CONNECT(darktable.signals, DT_SIGNAL_PREFERENCES_CHANGE, G_CALLBACK(_dt_pref_changed),
                                   range);
   gtk_widget_set_name((GtkWidget *)range, "dt-range");
-  
+
   return (GtkWidget *)range;
 }
 
