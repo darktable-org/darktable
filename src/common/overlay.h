ebbfea6c90a26ffd923c7f1937b4e27f21ac3277
Pascal Obry <pascal@obry.net>
2023-12-23 19:12:50 +0100

    common/overlay: New unit to handle overlay data (db + tag).

---
+++ src/common/overlay.h
@@ -0,0 +1,58 @@
+/*
+    This file is part of darktable,
+    Copyright (C) 2023 darktable developers.
+
+    darktable is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    darktable is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with darktable.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <glib.h>
+#include <stdint.h>
+
+#include "common/darktable.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/*
+  Record overlay_id as being used as overlay in imgid
+*/
+void dt_overlay_record(const dt_imgid_t imgid, const dt_imgid_t overlay_id);
+
+/*
+  Remove overlay_id as being used as overlay in imgid
+*/
+void dt_overlay_remove(const dt_imgid_t imgid, const dt_imgid_t overlay_id);
+
+/*
+  Get list of all images used as overlay in imgid
+*/
+GList *dt_overlay_get_imgs(const dt_imgid_t imgid);
+
+/*
+  Get list of all images where overlay_id is used as overlay
+*/
+GList *dt_overlay_get_used_in_imgs(const dt_imgid_t overlay_id);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif /* __cplusplus */
+
+// clang-format off
+// modelines: These editor modelines have been set for all relevant files by tools/update_modelines.py
+// vim: shiftwidth=2 expandtab tabstop=2 cindent
+// kate: tab-indents: off; indent-width 2; replace-tabs on; indent-mode cstyle; remove-trailing-spaces modified;
+// clang-format on
