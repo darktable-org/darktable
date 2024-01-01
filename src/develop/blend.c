bce83d51a9aa2a3e15b7909796d756033a683535
hanno@schwalm-bremen.de <hanno@schwalm-bremen.de>
2024-01-02 00:30:39 +0100

    Fix details mask on low opencl memory
    Due to the design of the details threshold mask we precalculate the scharr mask in demosaic
    module but we do **not** support doing that in tiling mode.
    
    In #15999 and related reports in pixls and on github we ran into problems because of tiling
    the generated scharr mask did not cover the full roi of demosaic but just a part of it, this lead
    to crashes.
    
    Due to the way we commit parameters while preparing the pipeline we have to make sure about no
    tiling while committing params and also make sure in process and the cl variant.

--- src/develop/blend.c
+++ src/develop/blend.c
@@ -840,7 +840,7 @@ static void _refine_with_detail_mask_cl(struct dt_iop_module_t *self,
 
   dt_print_pipe(DT_DEBUG_PIPE,
        "refine_detail_mask on GPU",
-       piece->pipe, self, roi_in, roi_out, "\n");
+       piece->pipe, self, roi_in, roi_out, "scharr %ix%i\n", iwidth, iheight);
 
   lum = dt_alloc_align_float((size_t)iwidth * iheight);
   out = dt_opencl_alloc_device_buffer(devid, sizeof(float) * iwidth * iheight);
@@ -878,6 +878,8 @@ static void _refine_with_detail_mask_cl(struct dt_iop_module_t *self,
 
   dt_opencl_release_mem_object(blur);
   dt_opencl_release_mem_object(out);
+  out = NULL;
+  blur = NULL;
 
   // here we have the slightly blurred full detail mask available
   float *warp_mask = dt_dev_distort_detail_mask(p, lum, self);
