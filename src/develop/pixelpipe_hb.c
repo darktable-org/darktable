901c3f4726d09036ddbb8c3d4e259f6272f21fad
hanno@schwalm-bremen.de <hanno@schwalm-bremen.de>
2024-01-02 00:30:39 +0100

    Fix details mask on low opencl memory
    Due to the design of the details threshold mask we precalculate the scharr mask in demosaic
    module but we do **not** support doing that in tiling mode.
    
    In #15999 and related reports in pixls and on github we ran into problems because of tiling
    the generated scharr mask did not cover the full roi of demosaic but just a part of it, this lead
    to crashes.
    
    Due to the way we commit parameters while preparing the pipeline we have to make sure about no
    tiling while committing params and also make sure in process and the cl variant.

--- src/develop/pixelpipe_hb.c
+++ src/develop/pixelpipe_hb.c
@@ -2971,6 +2971,8 @@ gboolean dt_dev_write_scharr_mask(dt_dev_pixelpipe_iop_t *piece,
 {
   dt_dev_pixelpipe_t *p = piece->pipe;
   dt_dev_clear_scharr_mask(p);
+  if(piece->pipe->tiling)
+    goto error;
 
   const int width = roi_in->width;
   const int height = roi_in->height;
@@ -3011,6 +3013,9 @@ int dt_dev_write_scharr_mask_cl(dt_dev_pixelpipe_iop_t *piece,
 {
   dt_dev_pixelpipe_t *p = piece->pipe;
   dt_dev_clear_scharr_mask(p);
+
+  if(piece->pipe->tiling)
+    return DT_OPENCL_PROCESS_CL;
 
   const int width = roi_in->width;
   const int height = roi_in->height;
