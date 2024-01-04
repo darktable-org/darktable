ea0eda79e0255f08a956022f94e073bd75004ad8
Pascal Obry <pascal@obry.net>
2024-01-04 18:51:46 +0100

    Minor code reformatting.

--- src/gui/styles.h
+++ src/gui/styles.h
@@ -21,7 +21,7 @@
 #include <cairo.h>
 
 /** shows a dialog for creating a new style */
-void dt_gui_styles_dialog_new(dt_imgid_t imgid);
+void dt_gui_styles_dialog_new(const dt_imgid_t imgid);
 
 /** shows a dialog for editing existing style */
 void dt_gui_styles_dialog_edit(const char *name);
