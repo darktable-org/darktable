e33a1b82666219c7ad94a0d95a613ba35815d3d7
hanno@schwalm-bremen.de <hanno@schwalm-bremen.de>
2024-01-14 11:33:08 +0100

    Fixes for gamut_lut for colorbalance rgb (4.6.x)
    Backport from #16093
    
        remove chroma_LUT as it is not used at all
        use public D65xyY for initializing instead of specific declaration
        ensure complete initializing of gamut_lut in UCS mode
        always smooth gamut_lut
        use proper reduction for max while in OpenMP

--- src/common/colorspaces_inline_conversions.h
+++ src/common/colorspaces_inline_conversions.h
@@ -1355,9 +1355,9 @@ static inline float dt_UCS_L_star_to_Y(const float L_star)
 
 // L_star upper limit is 2.098883786377 truncated to 32-bit float and last decimal removed.
 // By clipping L_star to this limit, we ensure dt_UCS_L_star_to_Y() doesn't divide by zero.
-static const float DT_UCS_L_STAR_UPPER_LIMIT = 2.098883f;
+static const float DT_UCS_L_STAR_UPPER_LIMIT = 2.09885f;
 // Y upper limit is calculated from the above L star upper limit.
-static const float DT_UCS_Y_UPPER_LIMIT = 13237757000.f;
+static const float DT_UCS_Y_UPPER_LIMIT = 1e8f;
 
 
 #ifdef _OPENMP
