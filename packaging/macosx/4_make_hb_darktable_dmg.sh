19f7bbcf0be64318c2b0d65094ca5ba688c7a821
Mario Zimmermann <mail@zisoft.de>
2024-03-01 09:13:51 +0100

    make several retries of hdiutil

--- packaging/macosx/4_make_hb_darktable_dmg.sh
+++ packaging/macosx/4_make_hb_darktable_dmg.sh
@@ -19,14 +19,44 @@ cd "$buildDir"/
 # Generate symlink to applications folder for easier drag & drop within dmg
 ln -s /Applications package/ || true
 
-if [ -n "${GITHUB_RUN_ID}" ]; then
-  # see https://github.com/actions/runner-images/issues/7522
-  echo "Killing XProtect..."; sudo pkill -9 XProtect >/dev/null || true;
+# When building on github runner, 'hdiutil create' occasionally fails (resource busy)
+# so we make several retries
+try_count=0
+hdiutil_success=0
+
+while [ $hdiutil_success -ne 1 -a $try_count -lt 8 ]; do
+	# Create temporary rw image
+    if hdiutil create -srcfolder package -volname "${PROGN}" -fs HFS+ \
+		-fsargs "-c c=64,a=16,e=16" -format UDRW pack.temp.dmg
+    then
+        hdiutil_success=1
+        break
+    fi
+    try_count=$(( $try_count + 1 ))
+	echo "'hdiutil create' failed (attempt ${try_count}). Retrying..."
+    sleep 1
+done
+
+if [ $hdiutil_success -ne 1 -a -n "${GITHUB_RUN_ID}" ]; then
+	# Still no success after 8 attempts.
+	# If we are on github runner, kill the Xprotect service and make one
+	# final attempt.
+    # see https://github.com/actions/runner-images/issues/7522
+	echo "Killing XProtect..."
+	sudo pkill -9 XProtect >/dev/null || true;
+	sleep 3
+
+    if hdiutil create -srcfolder package -volname "${PROGN}" -fs HFS+ \
+		-fsargs "-c c=64,a=16,e=16" -format UDRW pack.temp.dmg
+    then
+        hdiutil_success=1
+	fi
 fi
 
-# Create temporary rw image
-hdiutil create -srcfolder package -volname "${PROGN}" -fs HFS+ \
-	-fsargs "-c c=64,a=16,e=16" -format UDRW pack.temp.dmg
+if [ $hdiutil_success -ne 1 ]; then
+    echo "FATAL: 'hdiutil create' FAILED!"
+    exit 1
+fi 
 
 # Mount image without autoopen to create window style params
 device=$(hdiutil attach -readwrite -noverify -autoopen "pack.temp.dmg" |
