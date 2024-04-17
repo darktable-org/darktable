8d4237760c0a6d3a2bcc396e7603f13409e7a2cc
Aurelien Pierre <aurelien@aurelienpierre.com>
2024-04-17 18:05:54 +0200

    Took from ansel code

--- src/common/focus_peaking.h
+++ src/common/focus_peaking.h
@@ -2,6 +2,7 @@
     This file is part of darktable,
     Copyright (C) 2019-2024 darktable developers.
 
+    (Took from ansel code)
     darktable is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -39,7 +40,6 @@ static inline uint8_t float_to_uint8(const float i)
   return (uint8_t)(i * 255.0f);
 }
 
-
 DT_OMP_DECLARE_SIMD(aligned(image, index:64) uniform(image))
 static inline float laplacian(const float *const image, const size_t index[8])
 {
@@ -75,11 +75,15 @@ static inline void get_indices(const size_t i, const size_t j, const size_t widt
   index[7] = lower_line + right_row;      // south east
 }
 
-static inline void dt_focuspeaking(cairo_t *cr, const int buf_width, const int buf_height,
-                                   uint8_t *const restrict image)
+// static inline void dt_focuspeaking(cairo_t *cr, const int buf_width, const int buf_height,
+//                                   uint8_t *const restrict image)
+
+static inline void dt_focuspeaking(cairo_t *cr, int width, int height,
+                                   uint8_t *const restrict image,
+                                   const int buf_width, const int buf_height)
 {
   float *const restrict luma = dt_alloc_align_float((size_t)buf_width * buf_height);
-  uint8_t *const restrict focus_peaking = dt_alloc_align_uint8(buf_width * buf_height * 4);
+  uint8_t *const restrict focus_peaking = dt_alloc_align(64, sizeof(uint8_t) * buf_width * buf_height * 4);
 
   const size_t npixels = (size_t)buf_height * buf_width;
   // Create a luma buffer as the euclidian norm of RGB channels
@@ -99,77 +103,141 @@ static inline void dt_focuspeaking(cairo_t *cr, const int buf_width, const int b
   // Prefilter noise
   fast_surface_blur(luma, buf_width, buf_height, 12, 0.00001f, 4, DT_GF_BLENDING_LINEAR, 1, 0.0f, exp2f(-8.0f), 1.0f);
 
-  // Compute the gradients magnitudes
+  // Compute the laplacian of a gaussian
   float *const restrict luma_ds =  dt_alloc_align_float((size_t)buf_width * buf_height);
   DT_OMP_FOR(collapse(2))
   for(size_t i = 0; i < buf_height; ++i)
     for(size_t j = 0; j < buf_width; ++j)
     {
       size_t index = i * buf_width + j;
-      if (i < 2 || i >= buf_height - 2 || j < 2 || j > buf_width -2)
+      if(i < 8 || i >= buf_height - 8 || j < 8 || j > buf_width - 8)
+      {
         // ensure defined value for borders
         luma_ds[index] = 0.0f;
+      }
       else
       {
-        size_t DT_ALIGNED_ARRAY index_close[8];
-        get_indices(i, j, buf_width, buf_height, 1, index_close);
+        // Laplacian of a Gaussian kernel with sigma = 1.05
+        static const float kernel[7][7]
+            = { { 0.00053449f, 0.00352729f,  0.00992912f,  0.01362207f,  0.00992912f, 0.00352729f, 0.00053449f },
+                { 0.00352729f, 0.01828379f,  0.03437727f,  0.03474665f,  0.03437727f, 0.01828379f, 0.00352729f },
+                { 0.00992912f, 0.03437727f, -0.00982925f, -0.09093110f, -0.00982925f, 0.03437727f, 0.00992912f },
+                { 0.01362207f, 0.03474665f, -0.09093110f, -0.26187433f, -0.09093110f, 0.03474665f, 0.01362207f },
+                { 0.00992912f, 0.03437727f, -0.00982925f, -0.09093110f, -0.00982925f, 0.03437727f, 0.00992912f },
+                { 0.00352729f, 0.01828379f,  0.03437727f,  0.03474665f,  0.03437727f, 0.01828379f, 0.00352729f },
+                { 0.00053449f, 0.00352729f,  0.00992912f,  0.01362207f,  0.00992912f, 0.00352729f, 0.00053449f } };
 
-        size_t DT_ALIGNED_ARRAY index_far[8];
-        get_indices(i, j, buf_width, buf_height, 2, index_far);
+        // The close laplacian is the local-local contrast
+        // The far laplacian is the far local contrast, sampled 2 times farther in an a-trous fashion.
+        // If far / 2 = close, we are on a slowly-varying gradient, aka on a contrasted edge that is not sharp.
+        float laplacian_close = 0.f;
+        float laplacian_far = 0.f;
 
-        // Computing the gradient on the closest neighbours gives us the rate of variation, but doesn't say if we are
-        // looking at local contrast or optical sharpness.
-        // so we compute again the gradient on neighbours a bit further.
-        // if both gradients have the same magnitude, it means we have no sharpness but just a big step in intensity,
-        // aka local contrast. If the closest is higher than the farthest, is means we have indeed a sharp something,
-        // either noise or edge. To mitigate that, we just subtract half the farthest gradient but add a noise threshold
-        luma_ds[index] = laplacian(luma, index_close) - 0.67f * (laplacian(luma, index_far) - 0.00390625f);
+        for(int ii = 0; ii < 7; ii++)
+          for(int jj = 0; jj < 7; jj++)
+          {
+            laplacian_close += luma[(i - 3 + ii) * buf_width + (j - 3 + jj)] * kernel[ii][jj];
+            laplacian_far += luma[(i + (-3 + ii) * 2) * buf_width + (j + (-3 + jj) * 2)] * kernel[ii][jj];
+          }
+
+        // gradient on principal directions
+        const float gradient_1_y = (luma[(i - 2) * buf_width + (j)] - luma[(i + 2) * buf_width + (j)]) / 4.f;
+        const float gradient_1_x = (luma[(i) * buf_width + (j - 2)] - luma[(i) * buf_width + (j + 2)]) / 4.f;
+        const float TV_1 = dt_fast_hypotf(gradient_1_x, gradient_1_y);
+
+        // gradient on diagonals
+        const float gradient_2_y = (luma[(i - 2) * buf_width + (j - 2)] - luma[(i + 2) * buf_width + (j + 2)]) / (2.f * sqrtf(2.f));
+        const float gradient_2_x = (luma[(i - 2) * buf_width + (j + 2)] - luma[(i + 2) * buf_width + (j - 2)]) / (2.f * sqrtf(2.f));
+        const float TV_2 = dt_fast_hypotf(gradient_2_x, gradient_2_y);
+
+        // gradient on principal directions
+        const float gradient_3_y = (luma[(i - 1) * buf_width + (j)] - luma[(i + 1) * buf_width + (j)]) / 2.f;
+        const float gradient_3_x = (luma[(i) * buf_width + (j - 1)] - luma[(i) * buf_width + (j + 1)]) / 2.f;
+        const float TV_3 = dt_fast_hypotf(gradient_3_x, gradient_3_y);
+
+        // gradient on diagonals
+        const float gradient_4_y = (luma[(i - 1) * buf_width + (j - 1)] - luma[(i + 1) * buf_width + (j + 1)]) / (sqrtf(2.f));
+        const float gradient_4_x = (luma[(i - 1) * buf_width + (j + 1)] - luma[(i + 1) * buf_width + (j - 1)]) / (sqrtf(2.f));
+        const float TV_4 = dt_fast_hypotf(gradient_4_x, gradient_4_y);
+
+        // Total Variation = norm(grad_x, grad_y). We use it as a metric of global contrast since it doesn't use the current pixel.
+        // Laplacian = div(grad). We use it as a metric of local contrast, aka difference with current pixel and local average value.
+        // The ratio of both is meant to catch local contrast NOT correlated with global contrast, aka sharp edges.
+        // The TV is averaged from both directions, its coeff is made-up to balance local contrast detection.
+        const float TV = 100.f * (TV_1 + TV_2 + TV_3 + TV_4) / 4.f;
+        luma_ds[index] = (laplacian_close > 1e-9f) ? fmaxf(fabsf(laplacian_close) - 0.5f * fabsf(laplacian_far), 0.f) / (TV + 1.f) : 0.f;
+      }
+    }
+
+  // Dilate the mask to improve connectivity
+#ifdef _OPENMP
+#pragma omp parallel for default(none) \
+dt_omp_firstprivate(luma, luma_ds, buf_height, buf_width) \
+schedule(static) collapse(2)
+#endif
+  for(size_t i = 0; i < buf_height; ++i)
+    for(size_t j = 0; j < buf_width; ++j)
+    {
+      size_t index = i * buf_width + j;
+      if(i < 8 || i >= buf_height - 8 || j < 8 || j > buf_width - 8)
+      {
+        // ensure defined value for borders
+        luma[index] = 0.0f;
+      }
+      else
+      {
+        // Dilating kernel
+        static const float kernel[3][3] = { { 1.f } };
+        luma[index] = 0.f;
+        for(int ii = 0; ii < 3; ii++)
+          for(int jj = 0; jj < 3; jj++)
+            luma[index] += luma_ds[(i - 1 + ii) * buf_width + (j - 1 + jj)] * kernel[ii][jj];
       }
     }
 
   // Anti-aliasing
-  dt_box_mean(luma_ds, buf_height, buf_width, 1, 2, 1);
+  dt_box_mean(luma, buf_height, buf_width, 1, 3, 1);
 
-  // Compute the gradient mean over the picture
+  // Postfilter to connect isolated dots and draw lines
+  fast_surface_blur(luma, buf_width, buf_height, 12, 0.0000001f, 1, DT_GF_BLENDING_LINEAR, 1, 0.0f, exp2f(-8.0f), 1.0f);
+
+  // Compute the laplacian mean over the picture
   float TV_sum = 0.0f;
 
   DT_OMP_FOR_SIMD(collapse(2) aligned(luma_ds:64) reduction(+:TV_sum))
   for(size_t i = 2; i < buf_height - 2; ++i)
     for(size_t j = 2; j < buf_width - 2; ++j)
-      TV_sum += luma_ds[i * buf_width + j];
+      TV_sum += luma[i * buf_width + j];
 
-  TV_sum /= (float)(buf_height - 4) * (float)(buf_width - 4);
+  TV_sum /= (float)(buf_height - 16) * (float)(buf_width - 16);
 
-  // Compute the predicator of the hyper-laplacian distribution
-  // (similar to the standard deviation if we had a gaussian distribution)
+  // Compute the standard deviation
   float sigma = 0.0f;
 
   DT_OMP_FOR_SIMD(collapse(2) aligned(focus_peaking, luma_ds:64) reduction(+:sigma))
   for(size_t i = 2; i < buf_height - 2; ++i)
     for(size_t j = 2; j < buf_width - 2; ++j)
-       sigma += fabsf(luma_ds[i * buf_width + j] - TV_sum);
+       sigma += sqf(luma[i * buf_width + j] - TV_sum);
 
-  sigma /= (float)(buf_height - 4) * (float)(buf_width - 4);
+  sigma /= (float)(buf_height - 16) * (float)(buf_width - 16);
+  sigma = sqrtf(sigma);
 
   // Set the sharpness thresholds
-  const float six_sigma = TV_sum + 10.0f * sigma;
-  const float four_sigma = TV_sum + 5.0f * sigma;
-  const float two_sigma = TV_sum + 2.5f * sigma;
-
-  // Postfilter to connect isolated dots and draw lines
-  fast_surface_blur(luma_ds, buf_width, buf_height, 12, 0.00001f, 4, DT_GF_BLENDING_LINEAR, 1, 0.0f, exp2f(-8.0f), 1.0f);
+  const float six_sigma = TV_sum + 4.f * sigma;
+  const float four_sigma = TV_sum + 3.f * sigma;
+  const float two_sigma = TV_sum + 2.f * sigma;
 
   // Prepare the focus-peaking image overlay
   DT_OMP_FOR(collapse(2))
   for(size_t i = 0; i < buf_height; ++i)
     for(size_t j = 0; j < buf_width; ++j)
     {
-      static const uint8_t yellow[4] = { 0/*B*/, 255/*G*/, 255/*R*/, 255/*alpha*/ };
-      static const uint8_t green[4] = { 0, 255, 0, 255 };
-      static const uint8_t blue[4] = { 255, 0, 0, 255 };
+      static const uint8_t yellow[4] = { 0, 255, 255, 255 };
+      static const uint8_t green[4]  = { 0, 255,   0, 255 };
+      static const uint8_t blue[4]   = { 255, 0,   0, 255 };
 
       const size_t index = (i * buf_width + j) * 4;
-      const float TV = luma_ds[index / 4];
+      const float TV = luma[(i * buf_width + j)];
 
       if(TV > six_sigma)
       {
@@ -188,7 +256,7 @@ static inline void dt_focuspeaking(cairo_t *cr, const int buf_width, const int b
       }
       else
       {
-        // Not sharp enough : paint 0
+        // Not sharp enough : paint 0
         for_four_channels(c) focus_peaking[index + c] = 0;
       }
     }
