884709bc10538a27a7c676a9db283e233d97782e
ralfbrown <ralfbrown@users.noreply.github.com>
2024-09-04 14:40:55 -0400

    refactor building hierarchical popup menus for style selection

---
+++ src/dtgtk/stylemenu.h
@@ -0,0 +1,36 @@
+/*
+    This file is part of darktable,
+    Copyright (C) 2024 darktable developers.
+
+    darktable is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    darktable is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with darktable.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+typedef void dtgtk_menuitem_activate_callback_fn(GtkMenuItem *menuitem, gchar *name);
+typedef gboolean dtgtk_menuitem_button_callback_fn(GtkMenuItem *, GdkEventButton *event, gchar *name);
+
+GtkMenuShell *dtgtk_build_style_menu_hierarchy(gboolean allow_none,
+                                               dtgtk_menuitem_activate_callback_fn *activate_callback,
+                                               dtgtk_menuitem_button_callback_fn *button_callback);
+
+G_END_DECLS
+
+// clang-format off
+// modelines: These editor modelines have been set for all relevant files by tools/update_modelines.py
+// vim: shiftwidth=2 expandtab tabstop=2 cindent
+// kate: tab-indents: off; indent-width 2; replace-tabs on; indent-mode cstyle; remove-trailing-spaces modified;
+// clang-format on
