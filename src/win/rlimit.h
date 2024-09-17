b803888b976ff2c12af614080ed61309669e70d0
Victor Forsiuk <vvforce@gmail.com>
2024-09-17 09:28:52 +0300

    Optimize comments

--- src/win/rlimit.h
+++ src/win/rlimit.h
@@ -20,34 +20,25 @@
 
 #include <stdio.h>
 
-#define RLIMIT_CPU 0    /* limit on CPU time per process */
-#define RLIMIT_FSIZE 1  /* limit on file size */
-#define RLIMIT_DATA 2   /* limit on data segment size */
-#define RLIMIT_STACK 3  /* limit on process stack size */
-#define RLIMIT_CORE 4   /* limit on size of core dump file */
-#define RLIMIT_NOFILE 5 /* limit on number of open files */
-#define RLIMIT_AS 6     /* limit on process total address space size */
+#define RLIMIT_CPU 0         // limit on CPU time per process
+#define RLIMIT_FSIZE 1       // limit on file size
+#define RLIMIT_DATA 2        // limit on data segment size
+#define RLIMIT_STACK 3       // limit on process stack size
+#define RLIMIT_CORE 4        // limit on size of core dump file
+#define RLIMIT_NOFILE 5      // limit on number of open files
+#define RLIMIT_AS 6          // limit on process total address space size
 #define RLIMIT_VMEM RLIMIT_AS
 #define RLIM_NLIMITS 7
 #define RLIM_INFINITY (~0UL)
 
-/*
- * process resource limits definitions
- */
-
 struct rlimit
 {
-  //        LARGE_INTEGER  rlim_cur;
-  //        LARGE_INTEGER  rlim_max;
-  __int64 rlim_cur;
-  __int64 rlim_max;
+  __int64 rlim_cur;  // current (soft) limit
+  __int64 rlim_max;  // maximum value for rlim_cur (hard limit)
 };
 
 typedef struct rlimit rlimit_t;
 
-/*
- * Prototypes
- */
 int getrlimit(int resource, struct rlimit *);
 int setrlimit(int resource, const struct rlimit *);
 
