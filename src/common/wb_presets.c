6eab9a5f2ecca4e29a1b3f8935a849a6fc8197d4
ralfbrown <ralfbrown@users.noreply.github.com>
2024-09-22 15:05:10 -0400

    fix memory leaks

--- src/common/wb_presets.c
+++ src/common/wb_presets.c
@@ -1,4 +1,22 @@
 /*
+    This file is part of darktable,
+    Copyright (C) 2013-2024 darktable developers.
+
+    darktable is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    darktable is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with darktable.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
  * UFRaw - Unidentified Flying Raw converter for digital camera images
  *
  * wb_presets.c - White balance preset values for various cameras
@@ -284,6 +302,7 @@ void dt_wb_presets_init(const char *alternative)
            wb_presets_count);
 
 end:
+  if(parser) g_object_unref(parser);
   if(reader) g_object_unref(reader);
   if(!valid) exit(1);
   return;
