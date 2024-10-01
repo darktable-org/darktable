1da83db9e2a77c926c8cb72a1547920fd3e67814
ralfbrown <ralfbrown@users.noreply.github.com>
2024-10-01 13:52:43 -0400

    add allocation-failure check to GraphicsMagick loader

--- src/imageio/imageio_gm.c
+++ src/imageio/imageio_gm.c
@@ -1,6 +1,6 @@
 /*
     This file is part of darktable,
-    Copyright (C) 2012-2023 darktable developers.
+    Copyright (C) 2012-2024 darktable developers.
 
     darktable is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -127,7 +127,8 @@ dt_imageio_retval_t dt_imageio_open_gm(dt_image_t *img, const char *filename, dt
   {
     img->profile_size = profile_length;
     img->profile = (uint8_t *)g_malloc0(profile_length);
-    memcpy(img->profile, profile_data, profile_length);
+    if(img->profile)
+      memcpy(img->profile, profile_data, profile_length);
   }
 
   if(image) DestroyImage(image);
