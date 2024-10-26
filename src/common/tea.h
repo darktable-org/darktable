f6b816ec5066d05592fb6435590802526041d286
Victor Forsiuk <vvforce@gmail.com>
2024-10-26 19:35:27 +0300

    Minor reformatting of common/tea.h

--- src/common/tea.h
+++ src/common/tea.h
@@ -1,6 +1,6 @@
 /*
     This file is part of darktable,
-    Copyright (C) 2010-2020 darktable developers.
+    Copyright (C) 2010-2024 darktable developers.
 
     darktable is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -18,11 +18,12 @@
 
 #pragma once
 
-// Allocate a buffer for storing the internal state of 'numthreads' parallel instances of the Tiny Encryption
-// Algorithm.  We need to ensure that each state falls in a separate cache line, or all threads sharing a
-// cache line will be running in lock-step as the cache line bounces back and forth between them, effectively
-// cutting throughput by a factor equal to the number of threads sharing a cache line (8 with a 64-byte cache
-// line and 32-bit ints)
+// Allocate a buffer for storing the internal state of 'numthreads' parallel
+// instances of the Tiny Encryption Algorithm.  We need to ensure that each
+// state falls in a separate cache line, or all threads sharing a cache line
+// will be running in lock-step as the cache line bounces back and forth
+// between them, effectively cutting throughput by a factor equal to the number
+// of threads sharing a cache line (8 with 64-byte cache line and 32-bit ints)
 #define TEA_STATE_SIZE (MAX(DT_CACHELINE_BYTES, 2*sizeof(unsigned int)))
 static inline unsigned int* alloc_tea_states(size_t numthreads)
 {
@@ -31,9 +32,10 @@ static inline unsigned int* alloc_tea_states(size_t numthreads)
   return states;
 }
 
-// retrieve the state for the instance in the given thread from the array of states previously allocated with
-// alloc_tea_states()
-static inline unsigned int* get_tea_state(unsigned int* const states, int threadnum)
+// Retrieve the state for the instance in the given thread from the array of
+// states previously allocated with alloc_tea_states()
+static inline unsigned int* get_tea_state(unsigned int* const states,
+                                          int threadnum)
 {
   return states + threadnum * TEA_STATE_SIZE/sizeof(states[0]);
 }
@@ -46,8 +48,9 @@ static inline void free_tea_states(unsigned int* states)
 // How many rounds of the mixing function to run for one encryption
 #define TEA_ROUNDS 8
 
-// Run the encryption mixing function using and updating the given internal state.  For use as a PRNG, you can
-// set arg[0] to the random-number seed, then read out the value of arg[0] after each call to this function.
+// Run the encryption mixing function using and updating the given internal
+// state. For use as a PRNG, you can set arg[0] to the random-number seed,
+// then read out the value of arg[0] after each call to this function.
 static inline void encrypt_tea(unsigned int *arg)
 {
   const unsigned int key[] = { 0xa341316c, 0xc8013ea4, 0xad90777d, 0x7e95761e };
@@ -68,7 +71,9 @@ static inline float tpdf(unsigned int urandom)
 {
   float frandom = (float)urandom / (float)0xFFFFFFFFu;
 
-  return (frandom < 0.5f ? (sqrtf(2.0f * frandom) - 1.0f) : (1.0f - sqrtf(2.0f * (1.0f - frandom))));
+  return (frandom < 0.5f
+          ? (sqrtf(2.0f * frandom) - 1.0f)
+          : (1.0f - sqrtf(2.0f * (1.0f - frandom))));
 }
 
 
