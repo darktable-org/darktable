aa48235da35224f66c1babf06f6787bc58fdf423
Bill Ferguson <wpferguson@gmail.com>
2024-10-31 14:35:20 -0400

    lua/util.c - added gen_uuid function to generate a UUID and return              the resulting UUID string.

--- src/lua/util.c
+++ src/lua/util.c
@@ -21,6 +21,17 @@
 #include <glib.h>
 #include <stdlib.h>
 #include <string.h>
+#include <uuid.h>
+
+static int gen_uuid(lua_State *L)
+{
+  uuid_t uuid;
+  char uuid_string[37];
+  uuid_generate(uuid);
+  uuid_unparse_upper(uuid, uuid_string);
+  lua_pushstring(L, uuid_string);
+  return 1;
+}
 
 static int message(lua_State *L)
 {
@@ -44,6 +55,9 @@ int dt_lua_init_util(lua_State *L)
 {
   dt_lua_push_darktable_lib(L);
   dt_lua_goto_subtable(L, "util");
+
+  lua_pushcfunction(L, gen_uuid);
+  lua_setfield(L, -2, "gen_uuid");
 
   lua_pushcfunction(L, message);
   lua_setfield(L, -2, "message");
