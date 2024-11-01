4a80b01b43fe53243c5f646a8f6c393bc7e1e96d
Victor Forsiuk <vvforce@gmail.com>
2024-11-01 15:53:32 +0200

    No need to check the signature, this is already done by the calling code

--- src/imageio/imageio_qoi.c
+++ src/imageio/imageio_qoi.c
@@ -59,28 +59,7 @@ dt_imageio_retval_t dt_imageio_open_qoi(dt_image_t *img,
     return DT_IMAGEIO_LOAD_FAILED;
   }
 
-  // Let's check whether the entire content of the file should be read into the buffer.
-  // If we see that it's a non-QOI file, we'll save time by avoiding unnecessary reading
-  // of a potentially large file into the buffer.
-  if(fread(read_buffer, 1, 4, f) != 4)
-  {
-    fclose(f);
-    g_free(read_buffer);
-    dt_print(DT_DEBUG_ALWAYS, "[qoi_open] failed to read from %s", filename);
-    // if we can't read even first 4 bytes, it's more like file disappeared
-    return DT_IMAGEIO_FILE_CORRUPTED;
-  }
-
-  if(memcmp(read_buffer, "qoif", 4) != 0)
-  {
-    fclose(f);
-    g_free(read_buffer);
-    dt_print(DT_DEBUG_ALWAYS,
-             "[qoi_open] no proper file header in %s", filename);
-    return DT_IMAGEIO_UNSUPPORTED_FORMAT;
-  }
-
-  if(fread(read_buffer+4, 1, filesize-4, f) != filesize-4)
+  if(fread(read_buffer, 1, filesize, f) != filesize)
   {
     fclose(f);
     g_free(read_buffer);
