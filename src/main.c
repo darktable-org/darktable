7c980fff4aa207215d53b8b6ef0543a45bc919ef
esq4 <esq654@gmail.com>
2024-12-06 21:22:57 +0700

    logdir in cli help

--- src/main.c
+++ src/main.c
@@ -67,6 +67,8 @@ int main(int argc, char *argv[])
 
   if(redirect_output)
   {
+    // NOTE! If the log file path is changed, reflect the changes at least
+    // in the console message in 'usage()' of 'common\darktable.c'
     char *logdir = g_build_filename(g_get_home_dir(), "Documents", "Darktable", NULL);
     char *logfile = g_build_filename(logdir, "darktable-log.txt", NULL);
 
