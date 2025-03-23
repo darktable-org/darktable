8c3fa866d74c30eef69f7c348b24d1bc59d78fb7
Victor Forsiuk <vvforce@gmail.com>
2025-03-23 10:47:09 +0200

    Remove long unused srgb_tone_curve_values.h

--- src/common/colorspaces.c
+++ src/common/colorspaces.c
@@ -1,6 +1,6 @@
 /*
     This file is part of darktable,
-    Copyright (C) 2010-2024 darktable developers.
+    Copyright (C) 2010-2025 darktable developers.
 
     darktable is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -25,7 +25,6 @@
 #include "common/file_location.h"
 #include "common/math.h"
 #include "common/matrices.h"
-#include "common/srgb_tone_curve_values.h"
 #include "common/utility.h"
 #include "control/conf.h"
 #include "control/control.h"
