062ce0c8e4944aef030233e68305779de5801fd9
Victor Forsiuk <vvforce@gmail.com>
2025-04-04 17:57:00 +0300

    Add our standard copyright line

--- tools/basecurve/exif-wrapper.cpp
+++ tools/basecurve/exif-wrapper.cpp
@@ -1,6 +1,7 @@
 /*
     This file is part of darktable,
                   2014 Edouard Gomez
+    Copyright (C) 2025 darktable developers.
 
     darktable is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
