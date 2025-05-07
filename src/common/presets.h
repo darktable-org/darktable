5d906c8910e05b48687f849455397f01dcdaf670
Pascal Obry <pascal@obry.net>
2025-05-07 22:23:41 +0200

    Never translate a preset's multi_name.
    We want to keep the untranslated name as-is. The translation if needed is
    down on display only. This ensure that a preset applied using dt with
    French language will be properly displayed in English when dt is started
    in English or in any other language dt is started.

--- src/common/presets.h
+++ src/common/presets.h
@@ -42,7 +42,9 @@ char *dt_presets_get_module_label(const char *module_name,
    and the recorded preset's multi_name. This depends on the preference
    darkroom/ui/auto_module_name_update
 */
-const char *dt_presets_get_multi_name(const char *name, const char *multi_name);
+const char *dt_presets_get_multi_name(const char *name,
+                                      const char *multi_name,
+                                      const gboolean localize);
 
 /** get currently active preset name for the module */
 gchar *dt_get_active_preset_name(dt_iop_module_t *module, gboolean *writeprotect);
