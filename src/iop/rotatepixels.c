8b518f72df5f6c43425851bac8cd13c098f5ee49
Victor Forsiuk <vvforce@gmail.com>
2025-06-05 17:15:40 +0300

    Remove all GUI related code

--- src/iop/rotatepixels.c
+++ src/iop/rotatepixels.c
@@ -349,26 +349,6 @@ void reload_defaults(dt_iop_module_t *self)
   *d = (dt_iop_rotatepixels_params_t){ .rx = 0u, .ry = image->fuji_rotation_pos, .angle = -45.0f };
 
   self->default_enabled = ((d->rx != 0u) || (d->ry != 0u));
-
-  // FIXME: does not work.
-  self->hide_enable_button = !self->default_enabled;
-
-  if(self->widget)
-    gtk_label_set_text(GTK_LABEL(self->widget), self->default_enabled
-                       ? _("automatic pixel rotation")
-                       : _("automatic pixel rotation\nonly works for the sensors that need it."));
-}
-
-void gui_update(dt_iop_module_t *self)
-{
-}
-void gui_init(dt_iop_module_t *self)
-{
-  IOP_GUI_ALLOC(rotatepixels);
-
-  self->widget = dt_ui_label_new("");
-  gtk_label_set_line_wrap(GTK_LABEL(self->widget), TRUE);
-
 }
 
 // clang-format off
