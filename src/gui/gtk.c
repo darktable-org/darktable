6a619002006de901cbb12ec9aba73dee91800803
Kofa <kofa@kovacs-telekes.org>
2025-07-19 11:21:08 +0200

    Replaced deg2rad conversions of 0, 90, 180, 270, 360 degrees with constants 0, M_PI / 2.0, M_PI, 1.5 * M_PI, 2.0 * M_PI

--- src/gui/gtk.c
+++ src/gui/gtk.c
@@ -4231,10 +4231,10 @@ void dt_gui_draw_rounded_rectangle(cairo_t *cr,
 {
   const float radius = height / 5.0f;
   cairo_new_sub_path(cr);
-  cairo_arc(cr, x + width - radius, y + radius, radius, deg2rad(-90), deg2rad(0));
-  cairo_arc(cr, x + width - radius, y + height - radius, radius, deg2rad(0), deg2rad(90));
-  cairo_arc(cr, x + radius, y + height - radius, radius, deg2rad(90), deg2rad(180));
-  cairo_arc(cr, x + radius, y + radius, radius, deg2rad(180), deg2rad(270));
+  cairo_arc(cr, x + width - radius, y + radius, radius, -M_PI / 2.0, 0);
+  cairo_arc(cr, x + width - radius, y + height - radius, radius, 0, M_PI / 2.0);
+  cairo_arc(cr, x + radius, y + height - radius, radius, M_PI / 2.0, M_PI);
+  cairo_arc(cr, x + radius, y + radius, radius, M_PI, 1.5 * M_PI);
   cairo_close_path(cr);
   cairo_fill(cr);
 }
