a600a78259ce54d4a84ed7ab460096e03d7bc2dd
Kofa <kofa@kovacs-telekes.org>
2025-08-26 20:22:59 +0200

    Revert "agx: calculate matrices only in process(), not in commit_params()"
    This reverts commit 13706207483ee72829432f8bfa1e8c0ff64fff9c.

--- src/iop/agx.c
+++ src/iop/agx.c
@@ -245,6 +245,10 @@ typedef struct primaries_params_t
 typedef struct dt_iop_agx_data_t
 {
   tone_mapping_params_t tone_mapping_params;
+  dt_colormatrix_t pipe_to_base_transposed;
+  dt_colormatrix_t base_to_rendering_transposed;
+  dt_colormatrix_t rendering_to_pipe_transposed;
+  dt_iop_order_iccprofile_info_t rendering_profile;
 } dt_iop_agx_data_t;
 
 // Primaries preset deduplication: hashtable key type, hash and equality functions
@@ -1286,39 +1290,10 @@ void process(dt_iop_module_t *self,
     return;
   }
 
-  const dt_iop_agx_params_t *const p = self->params;
-
-  const dt_iop_agx_data_t *const processing_params = piece->data;
+  const dt_iop_agx_data_t *processing_params = piece->data;
   const float *const in = ivoid;
   float *const out = ovoid;
   const size_t n_pixels = (size_t)roi_in->width * roi_in->height;
-
-  // Get profiles and create matrices
-  const dt_iop_order_iccprofile_info_t *const pipe_work_profile = dt_ioppr_get_pipe_work_profile_info(piece->pipe);
-  const dt_iop_order_iccprofile_info_t *const base_profile =
-      _agx_get_base_profile(self->dev, pipe_work_profile, p->base_primaries);
-
-  if(!base_profile)
-  {
-    dt_print(DT_DEBUG_ALWAYS,
-             "[agx commit_params] Failed to obtain a valid base profile. Module will not run correctly.");
-    return;
-  }
-
-  dt_colormatrix_t pipe_to_base_transposed;
-  dt_colormatrix_t base_to_rendering_transposed;
-  dt_colormatrix_t rendering_to_pipe_transposed;
-  dt_iop_order_iccprofile_info_t rendering_profile;
-
-  _create_matrices(p, pipe_work_profile, base_profile,
-                   rendering_profile.matrix_in_transposed,
-                   pipe_to_base_transposed, base_to_rendering_transposed,
-                   rendering_to_pipe_transposed);
-
-  dt_colormatrix_transpose(rendering_profile.matrix_in,
-                           rendering_profile.matrix_in_transposed);
-  rendering_profile.nonlinearlut = FALSE; // no LUT for this linear transform
-
 
   DT_OMP_FOR()
   for(size_t k = 0; k < 4 * n_pixels; k += 4)
@@ -1328,19 +1303,19 @@ void process(dt_iop_module_t *self,
 
     // Convert from pipe working space to base space
     dt_aligned_pixel_t base_rgb = { 0.f };
-    dt_apply_transposed_color_matrix(pix_in, pipe_to_base_transposed, base_rgb);
+    dt_apply_transposed_color_matrix(pix_in, processing_params->pipe_to_base_transposed, base_rgb);
 
     _compress_into_gamut(base_rgb);
 
     dt_aligned_pixel_t rendering_rgb = { 0.f };
-    dt_apply_transposed_color_matrix(base_rgb, base_to_rendering_transposed, rendering_rgb);
+    dt_apply_transposed_color_matrix(base_rgb, processing_params->base_to_rendering_transposed, rendering_rgb);
 
     // Apply the tone mapping curve and look adjustments
     _agx_tone_mapping(rendering_rgb, &processing_params->tone_mapping_params,
-                      rendering_profile.matrix_in_transposed);
+                      processing_params->rendering_profile.matrix_in_transposed);
 
     // Convert from internal rendering space back to pipe working space
-    dt_apply_transposed_color_matrix(rendering_rgb, rendering_to_pipe_transposed, pix_out);
+    dt_apply_transposed_color_matrix(rendering_rgb, processing_params->rendering_to_pipe_transposed, pix_out);
 
     // Copy over the alpha channel
     pix_out[3] = pix_in[3];
@@ -2425,4 +2400,25 @@ void commit_params(dt_iop_module_t *self,
 
   // Calculate curve parameters once
   processing_params->tone_mapping_params = _calculate_tone_mapping_params(p);
+
+  // Get profiles and create matrices
+  const dt_iop_order_iccprofile_info_t *const pipe_work_profile = dt_ioppr_get_pipe_work_profile_info(piece->pipe);
+  const dt_iop_order_iccprofile_info_t *const base_profile =
+      _agx_get_base_profile(self->dev, pipe_work_profile, p->base_primaries);
+
+  if(!base_profile)
+  {
+    dt_print(DT_DEBUG_ALWAYS,
+             "[agx commit_params] Failed to obtain a valid base profile. Module will not run correctly.");
+    return;
+  }
+
+  _create_matrices(p, pipe_work_profile, base_profile,
+                   processing_params->rendering_profile.matrix_in_transposed,
+                   processing_params->pipe_to_base_transposed, processing_params->base_to_rendering_transposed,
+                   processing_params->rendering_to_pipe_transposed);
+
+  dt_colormatrix_transpose(processing_params->rendering_profile.matrix_in,
+                           processing_params->rendering_profile.matrix_in_transposed);
+  processing_params->rendering_profile.nonlinearlut = FALSE; // no LUT for this linear transform
 }
