0cf0da4983d715a2e8127cf61bf0a4226b73c58b
Bill Ferguson <wpferguson@gmail.com>
2025-08-28 12:04:48 -0400

    lua/configuration.h - bumped API to 9.6.0

--- src/lua/configuration.h
+++ src/lua/configuration.h
@@ -42,10 +42,11 @@
 // 4.8.0 was 9.3.0 (added button and box widget enhancements)
 // 5.0.0 was 9.4.0 (added group events and uuid)
 // 5.2.0 was 9.5.0 (added apply_sidecar to image)
+// 5.4.0 was 9.6.0 (added event querying)
 /* incompatible API change */
 #define LUA_API_VERSION_MAJOR 9
 /* backward compatible API change */
-#define LUA_API_VERSION_MINOR 5
+#define LUA_API_VERSION_MINOR 6
 /* bugfixes that should not change anything to the API */
 #define LUA_API_VERSION_PATCH 0
 /* suffix for unstable version */
