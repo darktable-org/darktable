5d2e0cc5f842c1b852d399f415519937cdb10bca
Bill Ferguson <wpferguson@gmail.com>
2025-09-01 15:24:50 -0400

    common/collection.c - added collection-changed event to the Lua API lua/events.c          so that scripts have a way to tell when a lighttable                       collection changes

--- src/common/collection.c
+++ src/common/collection.c
@@ -41,6 +41,11 @@
 #endif
 
 
+#ifdef USE_LUA
+#include "lua/call.h"
+#include "lua/events.h"
+#endif
+
 #define SELECT_QUERY "SELECT DISTINCT * FROM %s"
 #define LIMIT_QUERY "LIMIT ?1, ?2"
 
@@ -2689,6 +2694,12 @@ void dt_collection_update_query(const dt_collection_t *collection,
     DT_CONTROL_SIGNAL_RAISE(DT_SIGNAL_COLLECTION_CHANGED,
                             query_change, changed_property,
                             list, next);
+#ifdef USE_LUA
+    dt_lua_async_call_alien(dt_lua_event_trigger_wrapper,
+        0, NULL, NULL,
+        LUA_ASYNC_TYPENAME, "const char*", "collection-changed",
+        LUA_ASYNC_DONE);
+#endif
   }
 }
 
