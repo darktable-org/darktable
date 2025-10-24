395cb921fcbc74d04ee822ae7264ceaa455159c0
Pascal Obry <pascal@obry.net>
2025-10-24 17:58:10 +0200

    Use workspace instead of multiple-db (closer to what is supported now).
    Also add support for deleting databases with a dialog and a warning
    message.
    
    For #19516.

--- src/gui/workspace.c
+++ src/gui/workspace.c
@@ -20,7 +20,7 @@
 #include "common/utility.h"
 #include "gui/gtk.h"
 #include "dtgtk/button.h"
-#include "dbsession.h"
+#include "workspace.h"
 #ifdef GDK_WINDOWING_QUARTZ
 #include "osx/osx.h"
 #endif
@@ -28,61 +28,101 @@
 // override window manager's title bar?
 #define USE_HEADER_BAR
 
-typedef struct _dbsession_t {
+typedef struct _workspace_t {
   GtkWidget *db_screen;
   GtkWidget *entry;
   const char *datadir;
-} dt_dbsession_t;
+} dt_workspace_t;
 
-static void _dbsession_screen_destroy(dt_dbsession_t *session)
+static void _workspace_screen_destroy(dt_workspace_t *session)
 {
   if(session->db_screen)
     gtk_widget_destroy(session->db_screen);
   session->db_screen = NULL;
 }
 
-static void _dbsession_select_db(GtkWidget *button, dt_dbsession_t *session)
+static void _workspace_delete_db(GtkWidget *button, dt_workspace_t *session)
+{
+  GtkWidget *b = g_object_get_data(G_OBJECT(button), "db");
+  const gchar *label = gtk_button_get_label(GTK_BUTTON(b));
+
+  if(dt_gui_show_yes_no_dialog(_("delete workspace"),
+                               _("WARNING\n\ndo you really want to delete the '%s' workspace?"
+                                 "\n\nif XMP writing is not activated, the editing work will be lost."),
+                               label))
+  {
+    char FILE[PATH_MAX] = { 0 };
+
+    // db file
+    snprintf(FILE, sizeof(FILE),
+             "%s/library-%s.db", session->datadir, label);
+
+    if(g_file_test(FILE, G_FILE_TEST_EXISTS))
+    {
+      GFile *gf = g_file_new_for_path(FILE);
+      g_file_delete(gf, NULL, NULL);
+      g_object_unref(gf);
+    }
+
+    // resource file
+    snprintf(FILE, sizeof(FILE),
+             "%s/darktablerc-%s", session->datadir, label);
+
+    if(g_file_test(FILE, G_FILE_TEST_EXISTS))
+    {
+      GFile *gf = g_file_new_for_path(FILE);
+      g_file_delete(gf, NULL, NULL);
+      g_object_unref(gf);
+    }
+
+    // and now, remove/disable the buttons
+    gtk_widget_hide(button);
+    gtk_widget_hide(b);
+  }
+}
+
+static void _workspace_select_db(GtkWidget *button, dt_workspace_t *session)
 {
   const gchar *label = gtk_button_get_label(GTK_BUTTON(button));
 
   if(strcmp(label, _("default")) == 0)
   {
     dt_conf_set_string("database", "library.db");
-    dt_conf_set_string("database/label", "");
+    dt_conf_set_string("workspace/label", "");
   }
   else
   {
     char *dbname = g_strdup_printf("library-%s.db", label);
     dt_conf_set_string("database", dbname);
-    dt_conf_set_string("database/label", label);
+    dt_conf_set_string("workspace/label", label);
     g_free(dbname);
   }
 
-  _dbsession_screen_destroy(session);
+  _workspace_screen_destroy(session);
 }
 
-static void _dbsession_new_db(GtkWidget *button, dt_dbsession_t *session)
+static void _workspace_new_db(GtkWidget *button, dt_workspace_t *session)
 {
   const gchar *label = gtk_entry_get_text(GTK_ENTRY(session->entry));
 
   char *dbname = g_strdup_printf("library-%s.db", label);
   dt_conf_set_string("database", dbname);
-  dt_conf_set_string("database/label", label);
+  dt_conf_set_string("workspace/label", label);
   g_free(dbname);
 
-  _dbsession_screen_destroy(session);
+  _workspace_screen_destroy(session);
 }
 
-void dt_dbsession_create(const char *datadir)
+void dt_workspace_create(const char *datadir)
 {
   if(dt_check_gimpmode("file")
      || dt_check_gimpmode("thumb")
-     || !dt_conf_get_bool("database/multiple_db"))
+     || !dt_conf_get_bool("database/multiple_workspace"))
   {
     return;
   }
 
-  dt_dbsession_t *session = g_malloc(sizeof(dt_dbsession_t));
+  dt_workspace_t *session = g_malloc(sizeof(dt_workspace_t));
   session->datadir = datadir;
 
   // a simple gtk_dialog_new() leaves us unable to setup the header
@@ -95,7 +135,7 @@ void dt_dbsession_create(const char *datadir)
   GtkDialogFlags flags = GTK_DIALOG_DESTROY_WITH_PARENT;
 #endif
   session->db_screen =
-    gtk_dialog_new_with_buttons(_("darktable - select a database"),
+    gtk_dialog_new_with_buttons(_("darktable - select a workspace"),
                                 NULL, flags,
                                 NULL,
                                 GTK_RESPONSE_NONE, // <-- fake button list for compiler
@@ -105,9 +145,8 @@ void dt_dbsession_create(const char *datadir)
 
   GList *dbs = dt_read_file_pattern(datadir, "library*.db");
 
-  GtkWidget *l1 = gtk_label_new(_("select an existing database"));
-  GtkBox *vbox = GTK_BOX(dt_gui_vbox());
-  dt_gui_box_add(vbox, l1);
+  GtkWidget *l1 = gtk_label_new(_("select an existing workspace"));
+  dt_gui_dialog_add(session->db_screen, l1);
 
   const char *current_db =  dt_conf_get_string("database");
   gboolean current_db_found = FALSE;
@@ -116,8 +155,9 @@ void dt_dbsession_create(const char *datadir)
   {
     char *name = (char *)l->data;
     GtkWidget *b = NULL;
+    const gboolean is_default = strcmp(name, "library.db") == 0;
 
-    if(strcmp(name, "library.db") == 0)
+    if(is_default)
     {
       b = gtk_button_new_with_label(_("default"));
     }
@@ -133,12 +173,27 @@ void dt_dbsession_create(const char *datadir)
       b = gtk_button_new_with_label(f);
     }
 
+    gtk_widget_set_hexpand(GTK_WIDGET(b), TRUE);
+
     if(strcmp(name, current_db) == 0)
       current_db_found = TRUE;
 
     g_signal_connect(G_OBJECT(b), "clicked",
-                     G_CALLBACK(_dbsession_select_db), session);
-    dt_gui_box_add(vbox, b);
+                     G_CALLBACK(_workspace_select_db), session);
+
+    GtkBox *box = GTK_BOX(dt_gui_hbox());
+    dt_gui_box_add(box, b);
+
+    if(!is_default)
+    {
+      GtkWidget *del = dtgtk_button_new(dtgtk_cairo_paint_remove, 0, NULL);
+      g_signal_connect(G_OBJECT(del), "clicked",
+                       G_CALLBACK(_workspace_delete_db), session);
+      g_object_set_data(G_OBJECT(del), "db", b);
+      dt_gui_box_add(box, del);
+    }
+
+    dt_gui_dialog_add(session->db_screen, box);
   }
   g_list_free_full(dbs, g_free);
 
@@ -155,19 +210,18 @@ void dt_dbsession_create(const char *datadir)
   session->entry = gtk_entry_new();
   GtkWidget *create = gtk_button_new_with_label(_("create"));
   g_signal_connect(G_OBJECT(create), "clicked",
-                   G_CALLBACK(_dbsession_new_db), session);
+                   G_CALLBACK(_workspace_new_db), session);
   dt_gui_box_add(box, session->entry, create);
 
-  GtkBox *content = GTK_BOX(gtk_dialog_get_content_area(GTK_DIALOG(session->db_screen)));
-  dt_gui_box_add(vbox, l2, box);
+  dt_gui_dialog_add(session->db_screen, l2, box);
 
-  dt_gui_box_add(content, vbox);
-  gtk_widget_set_name(GTK_WIDGET(vbox), "multiple-db");
+  GtkWidget *content = gtk_dialog_get_content_area(GTK_DIALOG(session->db_screen));
+  gtk_widget_set_name(content, "workspace");
 
   gtk_widget_show_all(session->db_screen);
   while(gtk_dialog_run(GTK_DIALOG(session->db_screen)) == GTK_RESPONSE_ACCEPT);
 
-  _dbsession_screen_destroy(session);
+  _workspace_screen_destroy(session);
   g_free(session);
 }
 
