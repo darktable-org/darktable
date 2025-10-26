darktable-org/darktable

commit f55fe25c81b7b8b5aa0f1dcdce335e116563f79f

Move history stack copy mode (append / overwrite) into GUI dialog.
The default mode is now the append mode.

To use the overwrite mode (rarely used) one need now to do a selective
paste. A button in the dialog can be ticked to use the overwrite
paste mode.

As for the style the overwrite vs append mode status is now taken once
and used for the whole paste session. It was possible previously
possible to change the mode while a paste session was active in
background.

Closes #19510.

src/control/jobs/control_jobs.c
@@ -1576,11 +1576,13 @@ static int32_t _control_paste_history_job_run(dt_job_t *job)
   dt_stop_backthumbs_crawler(FALSE);
   dt_control_image_enumerator_t *params =
     (dt_control_image_enumerator_t *)dt_control_job_get_params(job);
-  GList *t = params->data;
+  _images_job_data_t *paste_data = params->data;
+  GList *t = paste_data->imgs;
+
   const guint total = g_list_length(t);
   double fraction = 0.0;
-  const int mode = dt_conf_get_int("plugins/lighttable/copy_history/pastemode");
-  const gboolean merge = (mode == 0) ? TRUE : FALSE;
+  const int mode = paste_data->overwrite;
+  const gboolean merge = (mode == DT_HISTORY_COPY_APPEND) ? TRUE : FALSE;
 
   dt_control_job_set_progress_message(job,
                                       ngettext("pasting history to %d image",
@@ -2303,15 +2305,17 @@ static void _add_history_job(GList *imgs,
     // remove the image in darkroom center view from the list of
     // images to be processed, and run it synchronously by itself
     imgs = g_list_remove_link(imgs, link);
-    if(images_job_data && images_job_data->styles)
+
+    if(images_job_data)
       images_job_data->imgs = link;
+
     dt_control_add_job(DT_JOB_QUEUE_SYNCHRONOUS,
                        _control_generic_images_job_create(execute, title, 0,
                                                           images_job_data
                                                           ? (gpointer)images_job_data
                                                           : (gpointer)link,
                                                           PROGRESS_BLOCKING, FALSE));
-    if(images_job_data && images_job_data->styles)
+    if(images_job_data)
       images_job_data->imgs = imgs;
   }
   // if there are any images left in the list after removing the
@@ -2335,7 +2339,18 @@ void dt_control_paste_history(GList *imgs)
     g_list_free(imgs);
     return;
   }
-  _add_history_job(imgs, N_("paste history"), &_control_paste_history_job_run, NULL);
+
+  _images_job_data_t *images_job_data = g_malloc(sizeof(_images_job_data_t));
+  if(images_job_data)
+  {
+    images_job_data->imgs = imgs;
+    images_job_data->styles = NULL;
+    images_job_data->duplicate = FALSE;
+    images_job_data->overwrite = FALSE;
+  }
+
+  _add_history_job(imgs, N_("paste history"),
+                   &_control_paste_history_job_run, images_job_data);
 }
 
 void dt_control_paste_parts_history(GList *imgs)
@@ -2353,7 +2368,17 @@ void dt_control_paste_parts_history(GList *imgs)
 
   if(res == GTK_RESPONSE_OK)
   {
-    _add_history_job(imgs, N_("paste history"), &_control_paste_history_job_run, NULL);
+    _images_job_data_t *images_job_data = g_malloc(sizeof(_images_job_data_t));
+    if(images_job_data)
+    {
+      images_job_data->imgs = imgs;
+      images_job_data->styles = NULL;
+      images_job_data->duplicate = FALSE;
+      images_job_data->overwrite = darktable.view_manager->copy_paste.is_overwrite_set;
+    }
+
+    _add_history_job(imgs, N_("paste history"),
+                     &_control_paste_history_job_run, images_job_data);
   }
   else
     g_list_free(imgs);

src/common/history.h
@@ -53,13 +53,21 @@ typedef struct dt_history_hash_values_t
   int current_len;
 } dt_history_hash_values_t;
 
+typedef enum dt_history_copy_mode_t
+{
+  DT_HISTORY_COPY_APPEND    = 0,
+  DT_HISTORY_COPY_OVERWRITE = 1
+} dt_history_copy_mode_t;
+
 typedef struct dt_history_copy_item_t
 {
   GList *selops;
   GtkTreeView *items;
+  GtkWidget *overwrite;
   dt_imgid_t copied_imageid;
   gboolean full_copy;
   gboolean copy_iop_order;
+  gboolean is_overwrite_set;
 } dt_history_copy_item_t;
 
 /** helper function to free a GList of dt_history_item_t */

src/gui/hist_dialog.c
@@ -130,6 +130,10 @@ static void _gui_hist_copy_response(GtkDialog *dialog,
     case GTK_RESPONSE_OK:
       g->selops = _gui_hist_get_active_items(g);
       g->copy_iop_order = _gui_hist_is_copy_module_order_set(g);
+      if(g->overwrite)
+      {
+        g->is_overwrite_set = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(g->overwrite));
+      }
       break;
   }
 }
@@ -207,6 +211,13 @@ void tree_on_row_activated(GtkTreeView       *treeview,
     // and finally close the dialog
     g_signal_emit_by_name(dialog, "response", GTK_RESPONSE_OK, NULL);
   }
+}
+
+static void _overwrite_mode_clicked_callback(GtkWidget *widget,
+                                             dt_history_copy_item_t *d)
+{
+  const gboolean flag = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(d->overwrite));
+  dt_conf_set_bool("ui_last/history_overwrite", flag);
 }
 
 int dt_gui_hist_dialog_new(dt_history_copy_item_t *d,
@@ -346,6 +357,24 @@ int dt_gui_hist_dialog_new(dt_history_copy_item_t *d,
     dt_control_log(_("can't copy history out of unaltered image"));
     return GTK_RESPONSE_CANCEL;
   }
+
+  /* overwrite/append mode */
+
+  if(!iscopy)
+  {
+    d->overwrite = gtk_check_button_new_with_label(_("overwrite mode"));
+    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(d->overwrite),
+                                 dt_conf_get_bool("ui_last/history_overwrite"));
+    gtk_widget_set_tooltip_text(d->overwrite,
+                                _("clear history stack before pasting the copied history.\n"
+                                  "required modules will be added with default values."));
+    g_signal_connect(G_OBJECT(d->overwrite), "clicked",                 \
+                     G_CALLBACK(_overwrite_mode_clicked_callback), d);
+
+    dt_gui_dialog_add(GTK_DIALOG(dialog), d->overwrite);
+  }
+  else
+    d->overwrite = NULL;
 
   g_signal_connect(GTK_TREE_VIEW(d->items), "row-activated",
                    (GCallback)tree_on_row_activated, GTK_WIDGET(dialog));

src/libs/copy_history.c
@@ -40,20 +40,12 @@
 
 DT_MODULE(1)
 
-typedef enum dt_history_copy_mode_t
-{
-  DT_COPY_HISTORY_APPEND    = 0,
-  DT_COPY_HISTORY_OVERWRITE = 1
-} dt_history_copy_mode_t;
-
 typedef struct dt_lib_copy_history_t
 {
-  GtkWidget *pastemode;
   GtkWidget *paste, *paste_parts;
   GtkWidget *copy_button, *discard_button, *load_button, *write_button;
   GtkWidget *copy_parts_button;
   GtkWidget *compress_button;
-  gboolean is_full_copy;
 } dt_lib_copy_history_t;
 
 const char *name(dt_lib_module_t *self)
@@ -208,26 +200,20 @@ static void compress_button_clicked(GtkWidget *widget, gpointer user_data)
 
 static void copy_button_clicked(GtkWidget *widget, dt_lib_module_t *self)
 {
-  dt_lib_copy_history_t *d = self->data;
-
   const dt_imgid_t id = dt_act_on_get_main_image();
 
   if(dt_is_valid_imgid(id) && dt_history_copy(id))
   {
-    d->is_full_copy = TRUE;
     dt_lib_gui_queue_update(self);
   }
 }
 
 static void copy_parts_button_clicked(GtkWidget *widget, dt_lib_module_t *self)
 {
-  dt_lib_copy_history_t *d = self->data;
-
   const dt_imgid_t id = dt_act_on_get_main_image();
 
   if(dt_is_valid_imgid(id) && dt_history_copy_parts(id))
   {
-    d->is_full_copy = FALSE;
     dt_lib_gui_queue_update(self);
   }
 }
@@ -256,23 +242,9 @@ static void discard_button_clicked(GtkWidget *widget, gpointer user_data)
 
 static void paste_button_clicked(GtkWidget *widget, dt_lib_module_t *self)
 {
-  dt_lib_copy_history_t *d = self->data;
-
-  const int current_mode = dt_bauhaus_combobox_get(d->pastemode);
-
-  /* get paste mode and store, overwrite / merge */
-  const int mode = d->is_full_copy
-    ? DT_COPY_HISTORY_OVERWRITE
-    : current_mode;
-
-  dt_conf_set_int("plugins/lighttable/copy_history/pastemode", mode);
-
   /* copy history from previously copied image and paste onto selection */
   GList *imgs = dt_act_on_get_images(TRUE, TRUE, FALSE);
   dt_control_paste_history(imgs);
-
-  // restore mode
-  dt_conf_set_int("plugins/lighttable/copy_history/pastemode", current_mode);
 }
 
 static void paste_parts_button_clicked(GtkWidget *widget, gpointer user_data)
@@ -280,14 +252,6 @@ static void paste_parts_button_clicked(GtkWidget *widget, gpointer user_data)
   /* copy history from previously copied image and paste onto selection */
   GList *imgs = dt_act_on_get_images(TRUE, TRUE, FALSE);
   dt_control_paste_parts_history(imgs);
-}
-
-static void pastemode_combobox_changed(GtkWidget *widget, gpointer user_data)
-{
-  const int mode = dt_bauhaus_combobox_get(widget);
-  dt_conf_set_int("plugins/lighttable/copy_history/pastemode", mode);
-
-  dt_lib_gui_queue_update((dt_lib_module_t *)user_data);
 }
 
 static void _image_selection_changed_callback(gpointer instance, dt_lib_module_t *self)
@@ -365,16 +329,6 @@ void gui_init(dt_lib_module_t *self)
     (self, N_("discard history"), discard_button_clicked, self,
      _("discard history stack of\nall selected images"), 0, 0);
   gtk_grid_attach(grid, d->discard_button, 3, line++, 3, 1);
-
-  DT_BAUHAUS_COMBOBOX_NEW_FULL
-    (d->pastemode, self, NULL, N_("mode"),
-     _("how to handle existing history"),
-     dt_conf_get_int("plugins/lighttable/copy_history/pastemode"),
-     pastemode_combobox_changed, self,
-     N_("append"),     // DT_COPY_HISTORY_APPEND
-     N_("overwrite")); // DT_COPY_HISTORY_OVERWRITE
-  dt_gui_add_help_link(d->pastemode, "history");
-  gtk_grid_attach(grid, d->pastemode, 0, line++, 6, 1);
 
   d->load_button = dt_action_button_new
     (self, N_("load sidecar file..."), load_button_clicked, self,

commit a3f61c5c6b9af0b8b6d541bf834974a0953e0162

Fix query update list of imgids.
Fix crash introduced in f55fe25c81b

src/control/jobs/control_jobs.c
@@ -1619,7 +1619,7 @@ static int32_t _control_paste_history_job_run(dt_job_t *job)
 
   dt_collection_update_query(darktable.collection,
                              DT_COLLECTION_CHANGE_RELOAD, DT_COLLECTION_PROP_UNDEF,
-                             (GList*)params->data); // frees list of images
+                             (GList*)paste_data->imgs); // frees list of images
   g_free(params->data);
   params->data = NULL;
 