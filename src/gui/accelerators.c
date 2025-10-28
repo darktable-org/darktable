darktable-org/darktable

commit 2774fec95c00ab91cf2c5ff53df735a664a9d83c

Allow yes/no dialog to be named.
Use this to ensure the delete workspace dialog is having
a red background.

For #19516.

src/gui/accelerators.c
@@ -1458,6 +1458,7 @@ static gboolean _insert_shortcut(dt_shortcut_t *shortcut,
              && (!confirm ||
                  dt_gui_show_yes_no_dialog
                    (_("shortcut for move exists with single effect"),
+                    "",
                     _("%s\n\n(assign %s to %s)"),
                     _("create separate shortcuts for up and down move?"),
                     _action_description(e, 2),
@@ -1503,8 +1504,9 @@ static gboolean _insert_shortcut(dt_shortcut_t *shortcut,
                     e->instance != s->instance )
             {
               if(!confirm ||
-                 dt_gui_show_yes_no_dialog(_("shortcut exists with different settings"),
-                                           _("reset the settings of the shortcut?")))
+                 dt_gui_show_yes_no_dialog
+                 (_("shortcut exists with different settings"), "",
+                  _("reset the settings of the shortcut?")))
               {
                 *e = *s;
               }
@@ -1513,7 +1515,7 @@ static gboolean _insert_shortcut(dt_shortcut_t *shortcut,
             {
               // there should be no other clashes because same mapping already existed
               if(confirm &&
-                 dt_gui_show_yes_no_dialog(_("shortcut already exists"),
+                 dt_gui_show_yes_no_dialog(_("shortcut already exists"), "",
                                            e->is_default ?
                                            _("disable this default shortcut?") :
                                            _("remove the shortcut?")))
@@ -1556,9 +1558,9 @@ static gboolean _insert_shortcut(dt_shortcut_t *shortcut,
 
     if(existing_labels)
     {
-      remove_existing = dt_gui_show_yes_no_dialog(_("clashing shortcuts exist"), "%s\n%s",
+      remove_existing = dt_gui_show_yes_no_dialog(_("clashing shortcuts exist"), "%s\n%s", "",
                                                   _("remove these existing shortcuts?"),
-                                                  existing_labels);
+         existing_labels);
       g_free(existing_labels);
 
       if(!remove_existing)
@@ -2024,7 +2026,7 @@ static gboolean _view_key_pressed(GtkWidget *widget,
         // GDK_KEY_BackSpace moves to parent in tree
         if(event->keyval == GDK_KEY_Delete || event->keyval == GDK_KEY_KP_Delete)
         {
-          if(dt_gui_show_yes_no_dialog(_("removing shortcut"),
+          if(dt_gui_show_yes_no_dialog(_("removing shortcut"), "",
                                        s->is_default ? s->views ?
                                        _("disable the selected default shortcut?") :
                                        _("restore the selected default shortcut?") :

data/themes/darktable.css
@@ -2353,6 +2353,11 @@ Details :
     margin: 0.5em;
 }
 
+#wpdialog
+{
+    background-color: #852A2A;
+}
+
 #workspace .dt_module_btn
 {
     background-color: #852A2A;

src/control/jobs/control_jobs.c
@@ -2066,7 +2066,7 @@ gboolean dt_control_remove_images()
     }
 
     if(!dt_gui_show_yes_no_dialog(
-          ngettext(_("remove image?"), _("remove images?"), number),
+          ngettext(_("remove image?"), _("remove images?"), number), "",
           ngettext("do you really want to remove %d image from darktable\n(without deleting file on disk)?",
                    "do you really want to remove %d images from darktable\n(without deleting files on disk)?", number),
           number))
@@ -2099,7 +2099,7 @@ void dt_control_delete_images()
     }
 
     if(!dt_gui_show_yes_no_dialog(
-          ngettext(_("delete image?"), _("delete images?"), number),
+          ngettext(_("delete image?"), _("delete images?"), number), "",
           send_to_trash ? ngettext("do you really want to physically delete %d image\n(using trash if possible)?",
                                    "do you really want to physically delete %d images\n(using trash if possible)?", number)
                         : ngettext("do you really want to physically delete %d image from disk?",
@@ -2136,7 +2136,7 @@ void dt_control_delete_duplicate(const dt_imgid_t imgid)
     }
 
     if(!dt_gui_show_yes_no_dialog(
-          _("delete duplicate?"),
+          _("delete duplicate?"), "",
           _("do you really want to delete the duplicate "
             "(without deleting the source image file on disk)?")))
     {
@@ -2186,7 +2186,7 @@ void dt_control_move_images()
   if(dt_conf_get_bool("ask_before_move"))
   {
     if(!dt_gui_show_yes_no_dialog(
-          ngettext("move image?", "move images?", number),
+          ngettext("move image?", "move images?", number), "",
           ngettext("do you really want to physically move %d image to %s?\n"
                    "(all duplicates will be moved along)",
                    "do you really want to physically move %d images to %s?\n"
@@ -2246,7 +2246,7 @@ void dt_control_copy_images()
   if(dt_conf_get_bool("ask_before_copy"))
   {
     if(!dt_gui_show_yes_no_dialog(
-          ngettext("copy image?", "copy images?", number),
+          ngettext("copy image?", "copy images?", number), "",
           ngettext("do you really want to physically copy %d image to %s?",
                    "do you really want to physically copy %d images to %s?", number),
           number, dir))

src/dtgtk/thumbtable.c
@@ -1714,7 +1714,7 @@ static void _thumbs_ask_for_discard(dt_thumbtable_t *table)
 
     dt_util_str_cat(&txt, _("do you want to do that now?"));
 
-    if(dt_gui_show_yes_no_dialog(_("cached thumbnails invalidation"),
+    if(dt_gui_show_yes_no_dialog(_("cached thumbnails invalidation"), "",
                                  "%s", txt))
     {
       DT_DEBUG_SQLITE3_PREPARE_V2(dt_database_get(darktable.db),

src/gui/gtk.c
@@ -1975,7 +1975,7 @@ void dt_ui_toggle_panels_visibility(const struct dt_ui_t *ui)
   else
   {
     if(!dt_conf_get_bool("collapse_help_shown") &&
-       !dt_gui_show_yes_no_dialog(_("collapsing panels"),
+       !dt_gui_show_yes_no_dialog(_("collapsing panels"), "",
                                   _("this is the first time you pressed the shortcut\n"
                                     "to collapse all side and top/bottom panels.\n"
                                     "by default this is the TAB key.\n"
@@ -3125,6 +3125,7 @@ char *dt_gui_show_standalone_string_dialog(const char *title,
 }
 
 gboolean dt_gui_show_yes_no_dialog(const char *title,
+                                   const char *wname,
                                    const char *format, ...)
 {
   va_list ap;
@@ -3146,6 +3147,8 @@ gboolean dt_gui_show_yes_no_dialog(const char *title,
                                              GTK_MESSAGE_QUESTION,
                                              GTK_BUTTONS_NONE,
                                              "%s", question);
+  GtkWidget *content = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
+  gtk_widget_set_name(content, wname);
   gtk_dialog_add_buttons(GTK_DIALOG(dialog),
                          _("_yes"), GTK_RESPONSE_YES,
                          _("_no"), GTK_RESPONSE_NO,
@@ -3264,7 +3267,7 @@ void dt_gui_show_help(GtkWidget *widget)
       last_base_url = base_url;
 
       // ask the user if darktable.org may be accessed
-      if(dt_gui_show_yes_no_dialog(_("access the online user manual?"),
+      if(dt_gui_show_yes_no_dialog(_("access the online user manual?"), "",
                                     _("do you want to access `%s'?"), last_base_url))
       {
         dt_conf_set_string("context_help/last_url", last_base_url);

src/gui/gtk.h
@@ -444,6 +444,7 @@ char *dt_gui_show_standalone_string_dialog(const char *title,
 
 // returns TRUE if YES was answered, FALSE otherwise
 gboolean dt_gui_show_yes_no_dialog(const char *title,
+                                   const char *wname,
                                    const char *format, ...);
 
 void dt_gui_add_help_link(GtkWidget *widget,

src/gui/preferences.c
@@ -301,7 +301,7 @@ static gboolean _remove_panel_config(gpointer key,
 static void _reset_panels_clicked(GtkButton *button, gpointer user_data)
 {
   if(!dt_gui_show_yes_no_dialog
-     (_("reset panels in all views"),
+     (_("reset panels in all views"), "",
       _("are you sure?\n\n"
         "you will not be able to restore your current panel layout and module selection.")))
     return;

src/gui/presets.c
@@ -156,7 +156,7 @@ static void _menuitem_delete_preset(GtkMenuItem *menuitem,
   }
 
   if(!dt_conf_get_bool("plugins/lighttable/preset/ask_before_delete_preset")
-     || dt_gui_show_yes_no_dialog(_("delete preset?"),
+     || dt_gui_show_yes_no_dialog(_("delete preset?"), "",
                                   _("do you really want to delete the preset `%s'?"), name))
   {
     dt_action_rename_preset(&module->so->actions, name, NULL);
@@ -240,7 +240,7 @@ static void _edit_preset_response(GtkDialog *dialog,
         // if result is BUTTON_NO or ESCAPE keypress exit without
         // destroying dialog, to permit other name
         if(dt_gui_show_yes_no_dialog
-           (_("overwrite preset?"),
+           (_("overwrite preset?"), "",
             _("preset `%s' already exists.\ndo you want to overwrite?"), name))
         {
           // we remove the preset that will be overwrite
@@ -462,7 +462,7 @@ gboolean dt_gui_presets_confirm_and_delete(const char *name,
 {
   if(!module_name) return FALSE;
 
-  if(dt_gui_show_yes_no_dialog(_("delete preset?"),
+  if(dt_gui_show_yes_no_dialog(_("delete preset?"), "",
                                _("do you really want to delete the preset `%s'?"), name))
   {
     // deregistering accel...
@@ -990,7 +990,7 @@ static void _menuitem_update_preset(GtkMenuItem *menuitem, dt_iop_module_t *modu
   gchar *name = g_object_get_data(G_OBJECT(menuitem), "dt-preset-name");
 
   if(!dt_conf_get_bool("plugins/lighttable/preset/ask_before_delete_preset")
-     || dt_gui_show_yes_no_dialog(_("update preset?"),
+     || dt_gui_show_yes_no_dialog(_("update preset?"), "",
                                   _("do you really want to update the preset `%s'?"),
                                   name))
   {

commit fd1693a038589bcefa3671eccb6fb6b424368803

Fix a yes/no dialog parameter order.
Issues introduced in 2774fec.

src/gui/accelerators.c
@@ -1558,7 +1558,7 @@ static gboolean _insert_shortcut(dt_shortcut_t *shortcut,
 
     if(existing_labels)
     {
-      remove_existing = dt_gui_show_yes_no_dialog(_("clashing shortcuts exist"), "%s\n%s", "",
+      remove_existing = dt_gui_show_yes_no_dialog(_("clashing shortcuts exist"), "", "%s\n%s",
                                                   _("remove these existing shortcuts?"),
          existing_labels);
       g_free(existing_labels);