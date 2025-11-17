darktable-org/darktable

commit a506ec8e456fe0513adab5a9a4408b722470e226

Fix liquify OpenCL code
1. Don't clamp liquify mixed output but just make sure we have non-negative output as we have in cpu code.
2. Make sure we can't have div-by-zero
3. the internal interpolators should behave as cpu dt_interpolation_compute_pixel4c() ensuring non-negative input signals.

data/kernels/liquify.cl
@@ -1,6 +1,6 @@
 /*
     This file is part of darktable,
-    copyright (c) 2014 Marcello Perathoner.
+    Copyright (C) 2020-2024 darktable developers.
 
     darktable is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -72,13 +72,13 @@ float kmix (global const float *k,
 
 kernel void
 warp_kernel (read_only image2d_t in,
-	     write_only image2d_t out,
-	     global dt_iop_roi_t *roi_in,
-	     global dt_iop_roi_t *roi_out,
-	     global float2 *map,
-	     global cairo_rectangle_int_t *map_extent,
-	     global dt_liquify_kernel_descriptor_t *kdesc,
-	     global float *k)
+	           write_only image2d_t out,
+	           global dt_iop_roi_t *roi_in,
+	           global dt_iop_roi_t *roi_out,
+	           global float2 *map,
+	           global cairo_rectangle_int_t *map_extent,
+	           global dt_liquify_kernel_descriptor_t *kdesc,
+	           global float *k)
 {
   int2 pos = (int2) (get_global_id (0), get_global_id (1));
 
@@ -123,11 +123,9 @@ warp_kernel (read_only image2d_t in,
   // loop over support region (eg. 6x6 pixels for lanczos3)
   for (sample_pos.y = 1 - a; sample_pos.y <= a; ++sample_pos.y)
     for (sample_pos.x = 1 - a; sample_pos.x <= a; ++sample_pos.x)
-      Sxy += (read_imagef (in, sampleri, in_pos + convert_float2 (sample_pos))
-	      * lk[sample_pos.x].x * lk[sample_pos.y].y);
+      Sxy += fmax(0.0f, (read_imagef (in, sampleri, in_pos + convert_float2 (sample_pos))
+	      * lk[sample_pos.x].x * lk[sample_pos.y].y));
 
-  Sxy = Sxy / (norm.x * norm.y);
-  Sxy = clamp (Sxy, 0.0f, 1.0f);
-
+  Sxy = fmax(0.0f, Sxy / fmax(1e-7f, norm.x * norm.y));
   write_imagef (out, pos - roi_out_origin, Sxy);
 }

commit 23a64f27e920cfa78060496df6419957ade7a39c

Fix OpenCL liquify
1. There was a bug introduced in a506ec8e456fe0513adab5a9a4408b722470e226, we should just
   make sure we don't read NaN from the pipe.
2. If we use an unsupported interpolator we should return an error code instead of false.

data/kernels/liquify.cl
@@ -34,15 +34,15 @@ typedef struct {
 } dt_liquify_kernel_descriptor_t;
 
 
-float kmix (global const float *k,
-	    global const dt_liquify_kernel_descriptor_t* kdesc,
-	    float t)
+float kmix(global const float *k,
+	         global const dt_liquify_kernel_descriptor_t* kdesc,
+	         float t)
 {
-  t = fabs (t * kdesc->resolution);
+  t = fabs(t * kdesc->resolution);
   float flor;
-  t = fract (t, &flor);
+  t = fract(t, &flor);
   int i = (int) flor;
-  return mix (k[i], k[i+1], t);
+  return mix(k[i], k[i+1], t);
 }
 
 /**
@@ -80,7 +80,7 @@ warp_kernel (read_only image2d_t in,
 	           global dt_liquify_kernel_descriptor_t *kdesc,
 	           global float *k)
 {
-  int2 pos = (int2) (get_global_id (0), get_global_id (1));
+  int2 pos = (int2) (get_global_id(0), get_global_id(1));
 
   // stop surplus workers in the last workgroup
   if (pos.x >= map_extent->width || pos.y >= map_extent->height)
@@ -102,7 +102,7 @@ warp_kernel (read_only image2d_t in,
   const int2 roi_in_origin  = (int2) (roi_in->x,    roi_in->y);
   const int2 roi_out_origin = (int2) (roi_out->x,   roi_out->y);
 
-  float2 in_pos = convert_float2 (pos - roi_in_origin) + warp;
+  float2 in_pos = convert_float2(pos - roi_in_origin) + warp;
 
   const int a = kdesc->size;      // half the kernel width
 
@@ -111,11 +111,11 @@ warp_kernel (read_only image2d_t in,
   float2 norm = (float2) 0.0f;
   for (int i = 1 - a; i <= a; ++i)
   {
-    norm.x += (lk[i].x = kmix (k, kdesc, in_pos.x - floor (in_pos.x) - i));
-    norm.y += (lk[i].y = kmix (k, kdesc, in_pos.y - floor (in_pos.y) - i));
+    norm.x += (lk[i].x = kmix(k, kdesc, in_pos.x - floor(in_pos.x) - i));
+    norm.y += (lk[i].y = kmix(k, kdesc, in_pos.y - floor(in_pos.y) - i));
   }
 
-  in_pos = floor (in_pos);
+  in_pos = floor(in_pos);
 
   int2 sample_pos;
   float4 Sxy = (float4) 0.0f;
@@ -123,9 +123,9 @@ warp_kernel (read_only image2d_t in,
   // loop over support region (eg. 6x6 pixels for lanczos3)
   for (sample_pos.y = 1 - a; sample_pos.y <= a; ++sample_pos.y)
     for (sample_pos.x = 1 - a; sample_pos.x <= a; ++sample_pos.x)
-      Sxy += fmax(0.0f, (read_imagef (in, sampleri, in_pos + convert_float2 (sample_pos))
-	      * lk[sample_pos.x].x * lk[sample_pos.y].y));
+      Sxy += fmax(0.0f, read_imagef(in, sampleri, in_pos + convert_float2(sample_pos)))
+	      * lk[sample_pos.x].x * lk[sample_pos.y].y;
 
   Sxy = fmax(0.0f, Sxy / fmax(1e-7f, norm.x * norm.y));
-  write_imagef (out, pos - roi_out_origin, Sxy);
+  write_imagef(out, pos - roi_out_origin, Sxy);
 }

src/iop/liquify.c
@@ -1494,7 +1494,7 @@ static cl_int_t _apply_global_distortion_map_cl(const dt_iop_module_t *self,
          k[i] = lanczos(3, (float) i / kdesc.resolution);
        break;
      default:
-       return FALSE;
+       return DT_OPENCL_PROCESS_CL;
   }
 
   const cl_mem_t dev_roi_in = dt_opencl_copy_host_to_device_constant