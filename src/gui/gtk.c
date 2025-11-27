c8bc3a26166b3f939edb8be3d2e88f8aa7595164
Diederik ter Rahe <dterrahe@yahoo.com>
2025-11-27 13:11:02 +0100

    minimal compile time check for wrong signal handler return type

--- src/gui/gtk.c
+++ src/gui/gtk.c
@@ -1378,10 +1378,10 @@ int dt_gui_gtk_init(dt_gui_gtk_t *gui)
   gtkosx_application_set_window_menu(OSXApp, GTK_MENU_ITEM(window_root_menu));
   gtkosx_application_set_help_menu(OSXApp, GTK_MENU_ITEM(help_root_menu));
 
-  g_signal_connect(G_OBJECT(OSXApp), "NSApplicationBlockTermination",
-                   G_CALLBACK(_osx_quit_callback), NULL);
-  g_signal_connect(G_OBJECT(OSXApp), "NSApplicationOpenFile",
-                   G_CALLBACK(_osx_openfile_callback), NULL);
+  g_signal_connect_data(G_OBJECT(OSXApp), "NSApplicationBlockTermination",
+                        G_CALLBACK(_osx_quit_callback), NULL, NULL, 0);
+  g_signal_connect_data(G_OBJECT(OSXApp), "NSApplicationOpenFile",
+                        G_CALLBACK(_osx_openfile_callback), NULL, NULL, 0);
 #endif
 
 #ifdef _WIN32
@@ -4465,10 +4465,10 @@ GtkGestureSingle *(dt_gui_connect_click)(GtkWidget *widget,
   // GTK4 GtkGesture *gesture = gtk_gesture_click_new();
   //      gtk_widget_add_controller(widget, GTK_EVENT_CONTROLLER(gesture));
 
-  if(pressed) g_signal_connect(gesture, "pressed", pressed, data);
+  if(pressed) g_signal_connect(gesture, "pressed", G_CALLBACK(pressed), data);
   if(released)
   {
-    g_signal_connect(gesture, "released", released, data);
+    g_signal_connect(gesture, "released", G_CALLBACK(released), data);
     g_signal_connect(gesture, "cancel", G_CALLBACK(_gesture_cancel), NULL);
   }
 
@@ -4488,9 +4488,9 @@ GtkEventController *(dt_gui_connect_motion)(GtkWidget *widget,
 
   gtk_widget_add_events(widget, GDK_ENTER_NOTIFY_MASK | GDK_LEAVE_NOTIFY_MASK); // still needed for now by _main_do_event_keymap
 
-  if(motion) g_signal_connect(controller, "motion", motion, data);
-  if(enter) g_signal_connect(controller, "enter", enter, data);
-  if(leave) g_signal_connect(controller, "leave", leave, data);
+  if(motion) g_signal_connect(controller, "motion", G_CALLBACK(motion), data);
+  if(enter) g_signal_connect(controller, "enter", G_CALLBACK(enter), data);
+  if(leave) g_signal_connect(controller, "leave", G_CALLBACK(leave), data);
 
   return controller;
 }
