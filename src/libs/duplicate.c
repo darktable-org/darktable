darktable-org/darktable

commit 7bc039ff0ff021fd31c98bf71602578b7c45c736

Duplicate: Do not display cursor on duplicates.
Closes #19866.

src/libs/duplicate.c
@@ -294,7 +294,7 @@ static void _lib_duplicate_init_callback(gpointer instance, dt_lib_module_t *sel
                                              imgid,
                                              -1,
                                              DT_THUMBNAIL_OVERLAYS_ALWAYS_NORMAL,
-                                             DT_THUMBNAIL_CONTAINER_LIGHTTABLE,
+                                             DT_THUMBNAIL_CONTAINER_DUPLICATE,
                                              TRUE,
                                              DT_THUMBNAIL_SELECTION_UNSELECTED);
     thumb->sel_mode = DT_THUMBNAIL_SEL_MODE_DISABLED;
@@ -334,7 +334,7 @@ static void _lib_duplicate_init_callback(gpointer instance, dt_lib_module_t *sel
     gtk_grid_attach(GTK_GRID(hb), lb, 1, 0, 1, 1);
     gtk_grid_attach(GTK_GRID(hb), tb, 1, 1, 2, 1);
 
-    gtk_widget_show_all(hb);
+    gtk_widget_show(hb);
 
     dt_gui_box_add(d->duplicate_box, hb);
     d->thumbs = g_list_append(d->thumbs, thumb);

src/dtgtk/thumbnail.c
@@ -899,7 +899,8 @@ static void _thumb_update_icons(dt_thumbnail_t *thumb)
     show_active = (thumb->imgid == activeid);
   }
   const gboolean show_cursor = show_active
-    && !(thumb->container == DT_THUMBNAIL_CONTAINER_CULLING)
+    && (thumb->container != DT_THUMBNAIL_CONTAINER_CULLING)
+    && (thumb->container != DT_THUMBNAIL_CONTAINER_DUPLICATE)
     && (dt_view_get_current() == DT_VIEW_DARKROOM);
   gtk_widget_set_visible(thumb->w_cursor, show_cursor);
 

src/dtgtk/thumbnail.h
@@ -52,7 +52,8 @@ typedef enum dt_thumbnail_container_t
 {
   DT_THUMBNAIL_CONTAINER_LIGHTTABLE,
   DT_THUMBNAIL_CONTAINER_CULLING,
-  DT_THUMBNAIL_CONTAINER_PREVIEW
+  DT_THUMBNAIL_CONTAINER_PREVIEW,
+  DT_THUMBNAIL_CONTAINER_DUPLICATE
 } dt_thumbnail_container_t;
 
 typedef enum dt_thumbnail_selection_mode_t

commit e5bae12a8c97510ff81007f93e4a9330b4cf3dee

Fix duplicate manager widgets visibility.
This issue has been introduced in 7bc039f.

Closes #19893.

src/libs/duplicate.c
@@ -335,6 +335,9 @@ static void _lib_duplicate_init_callback(gpointer instance, dt_lib_module_t *sel
     gtk_grid_attach(GTK_GRID(hb), tb, 1, 1, 2, 1);
 
     gtk_widget_show(hb);
+    gtk_widget_show(lb);
+    gtk_widget_show(tb);
+    gtk_widget_show(bt);
 
     dt_gui_box_add(d->duplicate_box, hb);
     d->thumbs = g_list_append(d->thumbs, thumb);