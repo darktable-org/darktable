5a302764c959328a3c7c4d0ec483f8c99d892c99
ralfbrown <ralfbrown@users.noreply.github.com>
2026-01-08 09:27:14 -0500

    use count() instead of messing with size()

--- src/common/exif.cc
+++ src/common/exif.cc
@@ -1216,7 +1216,7 @@ static void _check_highlight_preservation(Exiv2::ExifData &exifData,
       const long state1 = pos->toLong(0);
       const long state2 = pos->toLong(1);
       const long state3 = pos->toLong(2);
-      const long state4 = pos->size() > (int)(3*sizeof(short)) ? pos->toLong(3) : 0;
+      const long state4 = pos->count() > 3 ? pos->toLong(3) : 0;
       if(state4 == 1) // Auto mode is reported to apply a -0.3EV exposure compensation
 	 img->exif_highlight_preservation = 0.33f;
       else if(state1 == -1 && state2 == -1 && state3 == 1) // low key, reported -1.0EV exposure compensation
