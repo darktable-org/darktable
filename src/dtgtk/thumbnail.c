a6b7817a67062544df5ec99754bbf5789e373282
Bill Ferguson <wpferguson@gmail.com>
2026-01-24 14:27:05 -0500

    dtgtk/thumbnail.c - Added check for extended overlays to force                     reloading of thumbnail data when image information                     changed.  This was necessary to update variable                     values that changed while in darkroom.

--- src/dtgtk/thumbnail.c
+++ src/dtgtk/thumbnail.c
@@ -1791,9 +1791,16 @@ void dt_thumbnail_destroy(dt_thumbnail_t *thumb)
 void dt_thumbnail_update_infos(dt_thumbnail_t *thumb)
 {
   if(!thumb) return;
-  _image_get_infos(thumb);
-  _thumb_write_extension(thumb);
-  _thumb_update_icons(thumb);
+  if(thumb->over == DT_THUMBNAIL_OVERLAYS_ALWAYS_EXTENDED
+       || thumb->over == DT_THUMBNAIL_OVERLAYS_HOVER_EXTENDED
+       || thumb->over == DT_THUMBNAIL_OVERLAYS_MIXED)
+    dt_thumbnail_reload_infos(thumb);
+  else
+  {
+    _image_get_infos(thumb);
+    _thumb_write_extension(thumb);
+    _thumb_update_icons(thumb);
+  }
   gtk_widget_queue_draw(thumb->w_main);
 }
 
