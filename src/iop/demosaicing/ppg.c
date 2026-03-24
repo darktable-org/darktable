a94c42f4da0c5f5300b8ee6c77e624f58528d0e3
Hanno Schwalm <hanno@schwalm-bremen.de>
2026-03-24 05:08:56 +0100

    Remaining work on RCD, LMMSE and PPG demosaicers
    1. To avoid border remaining CPU/GPU differences we have to increase the outermost margin to 9 as we did for the border
    2. As the RCD-internal border algorithms was basically a PPG variant that code got deduplicated and we have that now all
       in ppg code.
    3. Also some PPG CPU/GPU differences got fixed.

--- src/iop/demosaicing/ppg.c
+++ src/iop/demosaicing/ppg.c
@@ -1,6 +1,6 @@
 /*
     This file is part of darktable,
-    Copyright (C) 2010-2024 darktable developers.
+    Copyright (C) 2010-2026 darktable developers.
 
     darktable is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -22,9 +22,10 @@ static void demosaic_ppg(float *const out,
                          const int width,
                          const int height,
                          const uint32_t filters,
-                         const float thrs)
+                         const float thrs,
+                         const int margin)
 {
-  // border interpolate
+  // preliminary border interpolate for outermost 3 pixels
   float sum[8];
   for(int j = 0; j < height; j++)
     for(int i = 0; i < width; i++)
@@ -38,7 +39,7 @@ static void demosaic_ppg(float *const out,
           if((y >= 0) && (x >= 0) && (y < height) && (x < width))
           {
             const int f = FC(y, x, filters);
-            sum[f] += in[(size_t)y * width + x];
+            sum[f] += fmaxf(0.0f, in[(size_t)y * width + x]);
             sum[f + 4]++;
           }
         }
@@ -60,8 +61,11 @@ static void demosaic_ppg(float *const out,
     pre_median(med_in, in, width, height, filters, 1, thrs);
     input = med_in;
   }
-// for all pixels except those in the 3 pixel border:
-// interpolate green from input into out float array, or copy color.
+
+  const int border = margin + 3;
+  // for all pixels except those in the 3 pixel border:
+  // interpolate green from input into out float array, or copy color.
+  // avoid processing if outside of given border
   DT_OMP_FOR()
   for(int j = 3; j < height - 3; j++)
   {
@@ -69,6 +73,14 @@ static void demosaic_ppg(float *const out,
     const float *buf_in = input + (size_t)width * j + 3;
     for(int i = 3; i < width - 3; i++)
     {
+      if(i == border && j >= border && j < height - border)
+      {
+        i = width - border;
+        buf = out + (size_t)4 * width * j + 4 * i;
+        buf_in = in + (size_t)width * j + i;
+      }
+      if(i == width) break;
+
       const int c = FC(j, i, filters);
       dt_aligned_pixel_t color;
       const float pc = buf_in[0];
@@ -123,77 +135,85 @@ static void demosaic_ppg(float *const out,
     }
   }
 
-// for all pixels except the outermost row/column:
-// interpolate colors using out as input into float out array
   DT_OMP_FOR()
-  for(int j = 1; j < height - 1; j++)
+  for(int j = 0; j < height; j++)
   {
-    float *buf = out + (size_t)4 * width * j + 4;
-    for(int i = 1; i < width - 1; i++)
+    float *buf = out + (size_t)4 * width * j;
+    for(int i = 0; i < width; i++)
     {
+      if(i == margin && j >= margin && j < height - margin)
+      {
+        i = width - margin;
+        buf = out + (size_t)4 * (width * j + i);
+      }
       // also prefetch direct nbs top/bottom
-      const int c = FC(j, i, filters);
       dt_aligned_pixel_t color = { buf[0], buf[1], buf[2], buf[3] };
 
-      // fill all four pixels with correctly interpolated stuff: r/b for green1/2
-      // b for r and r for b
-      if(__builtin_expect(c & 1, 1)) // c == 1 || c == 3)
+      // for all pixels except the outermost row/column:
+      // interpolate colors using out as input into float out array
+      // the outermost are untouched
+      // but both final results avoid negatives
+      if(j > 0 && i > 0 && i < width - 1 && j < height - 1)
       {
-        // calculate red and blue for green pixels:
-        // need 4-nbhood:
-        const float *nt = buf - 4 * width;
-        const float *nb = buf + 4 * width;
-        const float *nl = buf - 4;
-        const float *nr = buf + 4;
-        if(FC(j, i + 1, filters) == 0) // red nb in same row
+        const int c = FC(j, i, filters);
+        // fill all four pixels with correctly interpolated stuff: r/b for green1/2
+        // b for r and r for b
+        if(__builtin_expect(c & 1, 1)) // c == 1 || c == 3)
         {
-          color[2] = (nt[2] + nb[2] + 2.0f * color[1] - nt[1] - nb[1]) * .5f;
-          color[0] = (nl[0] + nr[0] + 2.0f * color[1] - nl[1] - nr[1]) * .5f;
+          // calculate red and blue for green pixels:
+          // need 4-nbhood:
+          const float *nt = buf - 4 * width;
+          const float *nb = buf + 4 * width;
+          const float *nl = buf - 4;
+          const float *nr = buf + 4;
+          if(FC(j, i + 1, filters) == 0) // red nb in same row
+          {
+            color[2] = (nt[2] + nb[2] + 2.0f * color[1] - nt[1] - nb[1]) * .5f;
+            color[0] = (nl[0] + nr[0] + 2.0f * color[1] - nl[1] - nr[1]) * .5f;
+          }
+          else
+          {
+            // blue nb
+            color[0] = (nt[0] + nb[0] + 2.0f * color[1] - nt[1] - nb[1]) * .5f;
+            color[2] = (nl[2] + nr[2] + 2.0f * color[1] - nl[1] - nr[1]) * .5f;
+          }
         }
         else
         {
-          // blue nb
-          color[0] = (nt[0] + nb[0] + 2.0f * color[1] - nt[1] - nb[1]) * .5f;
-          color[2] = (nl[2] + nr[2] + 2.0f * color[1] - nl[1] - nr[1]) * .5f;
+          // get 4-star-nbhood:
+          const float *ntl = buf - 4 - 4 * width;
+          const float *ntr = buf + 4 - 4 * width;
+          const float *nbl = buf - 4 + 4 * width;
+          const float *nbr = buf + 4 + 4 * width;
+          if(c == 0)
+          {
+            // red pixel, fill blue:
+            const float diff1 = fabsf(ntl[2] - nbr[2]) + fabsf(ntl[1] - color[1]) + fabsf(nbr[1] - color[1]);
+            const float guess1 = ntl[2] + nbr[2] + 2.0f * color[1] - ntl[1] - nbr[1];
+            const float diff2 = fabsf(ntr[2] - nbl[2]) + fabsf(ntr[1] - color[1]) + fabsf(nbl[1] - color[1]);
+            const float guess2 = ntr[2] + nbl[2] + 2.0f * color[1] - ntr[1] - nbl[1];
+            if(diff1 > diff2)
+              color[2] = guess2 * .5f;
+            else if(diff1 < diff2)
+              color[2] = guess1 * .5f;
+            else
+              color[2] = (guess1 + guess2) * .25f;
+          }
+          else // c == 2, blue pixel, fill red:
+          {
+            const float diff1 = fabsf(ntl[0] - nbr[0]) + fabsf(ntl[1] - color[1]) + fabsf(nbr[1] - color[1]);
+            const float guess1 = ntl[0] + nbr[0] + 2.0f * color[1] - ntl[1] - nbr[1];
+            const float diff2 = fabsf(ntr[0] - nbl[0]) + fabsf(ntr[1] - color[1]) + fabsf(nbl[1] - color[1]);
+            const float guess2 = ntr[0] + nbl[0] + 2.0f * color[1] - ntr[1] - nbl[1];
+            if(diff1 > diff2)
+              color[0] = guess2 * .5f;
+            else if(diff1 < diff2)
+              color[0] = guess1 * .5f;
+            else
+              color[0] = (guess1 + guess2) * .25f;
+          }
         }
       }
-      else
-      {
-        // get 4-star-nbhood:
-        const float *ntl = buf - 4 - 4 * width;
-        const float *ntr = buf + 4 - 4 * width;
-        const float *nbl = buf - 4 + 4 * width;
-        const float *nbr = buf + 4 + 4 * width;
-
-        if(c == 0)
-        {
-          // red pixel, fill blue:
-          const float diff1 = fabsf(ntl[2] - nbr[2]) + fabsf(ntl[1] - color[1]) + fabsf(nbr[1] - color[1]);
-          const float guess1 = ntl[2] + nbr[2] + 2.0f * color[1] - ntl[1] - nbr[1];
-          const float diff2 = fabsf(ntr[2] - nbl[2]) + fabsf(ntr[1] - color[1]) + fabsf(nbl[1] - color[1]);
-          const float guess2 = ntr[2] + nbl[2] + 2.0f * color[1] - ntr[1] - nbl[1];
-          if(diff1 > diff2)
-            color[2] = guess2 * .5f;
-          else if(diff1 < diff2)
-            color[2] = guess1 * .5f;
-          else
-            color[2] = (guess1 + guess2) * .25f;
-        }
-        else // c == 2, blue pixel, fill red:
-        {
-          const float diff1 = fabsf(ntl[0] - nbr[0]) + fabsf(ntl[1] - color[1]) + fabsf(nbr[1] - color[1]);
-          const float guess1 = ntl[0] + nbr[0] + 2.0f * color[1] - ntl[1] - nbr[1];
-          const float diff2 = fabsf(ntr[0] - nbl[0]) + fabsf(ntr[1] - color[1]) + fabsf(nbl[1] - color[1]);
-          const float guess2 = ntr[0] + nbl[0] + 2.0f * color[1] - ntr[1] - nbl[1];
-          if(diff1 > diff2)
-            color[0] = guess2 * .5f;
-          else if(diff1 < diff2)
-            color[0] = guess1 * .5f;
-          else
-            color[0] = (guess1 + guess2) * .25f;
-        }
-      }
-
       for_each_channel(k,aligned(buf,color:16)
         dt_omp_nontemporal(buf)) buf[k] = fmaxf(0.0f, color[k]);
 
