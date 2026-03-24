darktable-org/darktable

commit 11e5ad4cbfc222ba06203c46c431d1163b32edc9

exposure/bauhaus;scopes: use GTK4-ready scrolling behavior
Convert bauhaus _widget_scroll() to use GtkEventControllerScroll
instead of GdkEventScroll.

- If ignoring scroll, pass it on to the scrolled window.
- Use delta from event controller, not from raw event
- In scopes use discrete scroll events from controller, which aligns
  with how bauhaus does things and gives fine results

Pass a GtkEventController to handle_event aka
dt_dev_exposure_handle_event().

Also: Fix bauhaus memory leak by freeing events returned by
gtk_get_current_event(). See:
https://docs.gtk.org/gtk3/func.get_current_event.html, "The caller of
the function takes ownership of the data, and is responsible for
freeing it."

Also: new dt_gui_connect_scroll() helper function.

src/bauhaus/bauhaus.c
@@ -3071,41 +3071,54 @@ static void _slider_add_step(GtkWidget *widget,
     dt_bauhaus_slider_set(widget, CLAMP(value + delta, d->min, d->max));
 }
 
-static gboolean _widget_scroll(GtkWidget *widget,
-                               GdkEventScroll *event)
+static void _widget_scroll(GtkEventControllerScroll *controller,
+                           double dx, double dy,
+                           GtkWidget *widget)
 {
-  if(dt_gui_ignore_scroll(event)) return FALSE;
-
-  // handle speed adjustment in mapping mode in dispatcher
-  if(darktable.control->mapping_widget)
-    return dt_shortcut_dispatcher(widget, (GdkEvent*)event, NULL);
-
-  gtk_widget_grab_focus(widget);
-
-  int delta_y = 0;
-  if(dt_gui_get_scroll_unit_delta(event, &delta_y))
+  GdkEvent *event = gtk_get_current_event();
+  if(!event || gdk_event_get_event_type(event) != GDK_SCROLL)
   {
-    if(delta_y == 0) return TRUE;
+    // NOP, should not happen
+  }
+  if(dt_gui_ignore_scroll(&event->scroll))
+  {
+    // hack: GTK3 event controller handlers can't return GDK_EVENT_PROPAGATE
+    GtkWidget *sw = gtk_widget_get_ancestor(widget, GTK_TYPE_SCROLLED_WINDOW);
+    if(sw)
+      gtk_widget_event(sw, event);
+  }
+  else if(darktable.control->mapping_widget)
+  {
+    // handle speed adjustment in mapping mode in dispatcher
+    dt_shortcut_dispatcher(widget, (GdkEvent*)event, NULL);
+  }
+  else
+  {
+    gtk_widget_grab_focus(widget);
 
-    dt_bauhaus_widget_t *w = (dt_bauhaus_widget_t *)widget;
-    _request_focus(w);
-
-    if(w->type == DT_BAUHAUS_SLIDER)
+    int delta = dx + dy;
+    if(delta != 0)
     {
-      const gboolean force = darktable.control->element == DT_ACTION_ELEMENT_FORCE
-                          && event->window == gtk_widget_get_window(widget);
-      if(force && dt_modifier_is(event->state, GDK_SHIFT_MASK | GDK_CONTROL_MASK))
+      dt_bauhaus_widget_t *w = (dt_bauhaus_widget_t *)widget;
+      _request_focus(w);
+
+      if(w->type == DT_BAUHAUS_SLIDER)
       {
-        _slider_zoom_range(w, delta_y);
-        _slider_zoom_toast(w);
+        const gboolean force = darktable.control->element == DT_ACTION_ELEMENT_FORCE
+                            && event->scroll.window == gtk_widget_get_window(widget);
+        if(force && dt_modifier_is(event->scroll.state, GDK_SHIFT_MASK | GDK_CONTROL_MASK))
+        {
+          _slider_zoom_range(w, delta);
+          _slider_zoom_toast(w);
+        }
+        else
+          _slider_add_step(widget, - delta, event->scroll.state, force);
       }
       else
-        _slider_add_step(widget, - delta_y, event->state, force);
+        _combobox_next_sensitive(w, delta, 0, FALSE);
     }
-    else
-      _combobox_next_sensitive(w, delta_y, 0, FALSE);
   }
-  return TRUE; // Ensure that scrolling the combobox cannot move side panel
+  gdk_event_free(event);
 }
 
 static gboolean _widget_key_press(GtkWidget *widget, GdkEventKey *event)
@@ -3566,8 +3579,9 @@ static void _widget_button_press(GtkGestureSingle *gesture,
   }
   else if(button == GDK_BUTTON_SECONDARY || w->type == DT_BAUHAUS_COMBOBOX)
   {
-    // FIXME: free event result of gtk_get_current_event(), see https://docs.gtk.org/gtk3/func.get_current_event.html, "The caller of the function takes ownership of the data, and is responsible for freeing it."  
-    bh->opentime = gdk_event_get_time(gtk_get_current_event());
+    GdkEvent *const event = gtk_get_current_event();
+    bh->opentime = gdk_event_get_time(event);
+    gdk_event_free(event);
     bh->mouse_x = x;
     bh->mouse_y = y;
     _popup_show(widget);
@@ -3650,9 +3664,10 @@ static void _widget_motion(GtkEventControllerMotion *controller,
       const float scaled_step =
         width * dt_bauhaus_slider_get_step(widget) / (d->max - d->min);
       const float steps = floorf((x - bh->mouse_x) / scaled_step);
-      // FIXME: free event result of gtk_get_current_event(), see https://docs.gtk.org/gtk3/func.get_current_event.html, "The caller of the function takes ownership of the data, and is responsible for freeing it."  
-      GdkEventMotion *event = (GdkEventMotion *)gtk_get_current_event(); // TODO cleanup
-      _slider_add_step(widget, copysignf(1, d->factor) * steps, event->state, FALSE);
+      GdkEvent *const event = gtk_get_current_event(); // TODO cleanup
+      _slider_add_step(widget, copysignf(1, d->factor) * steps,
+                       event->motion.state, FALSE);
+      gdk_event_free(event);
 
       bh->mouse_x += steps * scaled_step;
     }
@@ -3683,7 +3698,6 @@ static void dt_bh_class_init(DtBauhausWidgetClass *class)
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS(class);
   widget_class->draw = _widget_draw;
   // widget_class->snapshot = _widget_snapshot;
-  widget_class->scroll_event = _widget_scroll;
   widget_class->key_press_event = _widget_key_press;
   widget_class->get_preferred_width = _widget_get_preferred_width;
   widget_class->get_preferred_height = _widget_get_preferred_height;
@@ -3711,7 +3725,8 @@ static void dt_bh_init(DtBauhausWidget *w)
   w->show_label = TRUE;
 
   GtkWidget *widget = GTK_WIDGET(w);
-  gtk_widget_add_events(widget, GDK_FOCUS_CHANGE_MASK | darktable.gui->scroll_mask);
+  // FIXME: needed?
+  gtk_widget_add_events(widget, GDK_FOCUS_CHANGE_MASK);
 
   GtkGestureSingle *gesture =
     dt_gui_connect_click(w, _widget_button_press, _widget_button_release, widget);
@@ -3720,9 +3735,9 @@ static void dt_bh_init(DtBauhausWidget *w)
 
   dt_gui_connect_motion(w, _widget_motion, _widget_enter, _widget_leave, widget);
 
-  // GtkEventController *controller = gtk_event_controller_scroll_new(GTK_EVENT_CONTROLLER_SCROLL_BOTH_AXES | GTK_EVENT_CONTROLLER_SCROLL_DISCRETE);
-  // gtk_widget_add_controller(GTK_WIDGET(w), GTK_EVENT_CONTROLLER(controller));
-  // g_signal_connect(controller, "scroll", G_CALLBACK(_widget_scroll), w);
+  dt_gui_connect_scroll(w, GTK_EVENT_CONTROLLER_SCROLL_BOTH_AXES
+                           | GTK_EVENT_CONTROLLER_SCROLL_DISCRETE,
+                        _widget_scroll, widget);
 
   gtk_widget_set_can_focus(widget, TRUE);
   dt_gui_add_class(widget, "dt_bauhaus");

src/bauhaus/bauhaus.h
@@ -159,7 +159,7 @@ typedef struct dt_bauhaus_t
   void (*press)(GtkGestureSingle*, int, double, double, GtkWidget*);
   void (*release)(GtkGestureSingle*, int, double, double, GtkWidget*);
   void (*motion)(GtkEventControllerMotion*, double, double, GtkWidget*);
-  gboolean (*scroll)(GtkWidget*, GdkEventScroll*);
+  void (*scroll)(GtkEventControllerScroll*, double, double, GtkWidget*);
 } dt_bauhaus_t;
 
 #define DT_BAUHAUS_SPACE 0

src/develop/develop.c
@@ -3325,10 +3325,13 @@ float dt_dev_exposure_get_black(dt_develop_t *dev)
   return instance && instance->get_black  && instance->module->enabled ? instance->get_black(instance->module) : 0.0f;
 }
 
-void dt_dev_exposure_handle_event(gpointer controller, int n_press, gdouble x, const gboolean blackwhite)
+void dt_dev_exposure_handle_event(GtkEventController *controller,
+                                  int n_press, gdouble x, gdouble delta,
+                                  const gboolean blackwhite)
 {
   if(darktable.develop->proxy.exposure.handle_event)
-    darktable.develop->proxy.exposure.handle_event(controller, n_press, x, blackwhite);
+    darktable.develop->proxy.exposure.handle_event(controller, n_press, x, delta,
+                                                   blackwhite);
 }
 
 void dt_dev_modulegroups_set(dt_develop_t *dev,

src/develop/develop.h
@@ -103,7 +103,7 @@ typedef struct dt_dev_proxy_exposure_t
   float (*get_exposure)(struct dt_iop_module_t *exp);
   float (*get_effective_exposure)(struct dt_iop_module_t *exp);
   float (*get_black)(struct dt_iop_module_t *exp);
-  void (*handle_event)(gpointer, int, gdouble, const gboolean);
+  void (*handle_event)(GtkEventController*, int, gdouble, gdouble, const gboolean);
 } dt_dev_proxy_exposure_t;
 
 struct dt_dev_pixelpipe_t;
@@ -495,7 +495,9 @@ float dt_dev_exposure_get_effective_exposure(dt_develop_t *dev);
 /** get exposure black level */
 float dt_dev_exposure_get_black(dt_develop_t *dev);
 
-void dt_dev_exposure_handle_event(gpointer controller, int n_press, gdouble x, const gboolean blackwhite);
+void dt_dev_exposure_handle_event(GtkEventController *controller,
+                                  int n_press, gdouble x, gdouble delta,
+                                  const gboolean blackwhite);
 
 /*
  * modulegroups plugin hooks

src/gui/gtk.c
@@ -4610,6 +4610,19 @@ GtkEventController *(dt_gui_connect_motion)(GtkWidget *widget,
   return controller;
 }
 
+GtkEventController *(dt_gui_connect_scroll)(GtkWidget *widget,
+                                            GtkEventControllerScrollFlags flags,
+                                            GCallback scroll,
+                                            gpointer data)
+{
+  GtkEventController *controller = gtk_event_controller_scroll_new(widget, flags);
+  gtk_event_controller_set_propagation_phase(controller, GTK_PHASE_TARGET);
+  g_object_weak_ref(G_OBJECT (widget), (GWeakNotify) g_object_unref, controller);
+  // GTK4 gtk_widget_add_controller(widget, GTK_EVENT_CONTROLLER(controller));
+  if(scroll) g_signal_connect(controller, "scroll", G_CALLBACK(scroll), data);
+  return controller;
+}
+
 
 static int busy_nest_count = 0;
 static GdkCursor* busy_prev_cursor = NULL;

src/gui/gtk.h
@@ -567,6 +567,14 @@ GtkEventController *(dt_gui_connect_motion)(GtkWidget *widget,
   ASSERT_FUNC_TYPE(leave, void(*)(GtkEventControllerMotion *, __typeof__(data))), \
   dt_gui_connect_motion(GTK_WIDGET(widget), G_CALLBACK(motion), G_CALLBACK(enter), G_CALLBACK(leave), (data)))
 
+GtkEventController *(dt_gui_connect_scroll)(GtkWidget *widget,
+					    GtkEventControllerScrollFlags flags,
+                                            GCallback scroll,
+                                            gpointer data);
+#define dt_gui_connect_scroll(widget, flags, scroll, data) ( \
+  ASSERT_FUNC_TYPE(scroll, void(*)(GtkEventControllerScroll *, double, double, __typeof__(data))), \
+  dt_gui_connect_scroll(GTK_WIDGET(widget), (flags), G_CALLBACK(scroll), (data)))
+
 // GTK4 gtk_event_controller_get_current_event_state(GTK_EVENT_CONTROLLER(controller));
 #define dt_modifier_eq(controller, mask)\
   dt_modifier_is(dt_key_modifier_state(), mask)

src/iop/exposure.c
@@ -825,9 +825,9 @@ static float _exposure_proxy_get_effective_exposure(dt_iop_module_t *self)
   return g->effective_exposure;
 }
 
-static void _exposure_proxy_handle_event(gpointer controller,
+static void _exposure_proxy_handle_event(GtkEventController *controller,
                                          int n_press,
-                                         double x,
+                                         gdouble x, gdouble delta,
                                          const gboolean blackwhite)
 {
   dt_iop_module_t *self = darktable.develop->proxy.exposure.module;
@@ -847,7 +847,7 @@ static void _exposure_proxy_handle_event(gpointer controller,
                         p->mode == EXPOSURE_MODE_DEFLICKER
                       ? g->deflicker_target_level : g->exposure;
     if(!n_press)
-      darktable.bauhaus->scroll(widget, controller);
+      darktable.bauhaus->scroll(GTK_EVENT_CONTROLLER_SCROLL(controller), 0.0, delta, widget);
     else
     {
       // ignores the quad width, but is accurate enough
@@ -862,11 +862,11 @@ static void _exposure_proxy_handle_event(gpointer controller,
       x = x * slider_width / scope_width;
       if(GTK_IS_GESTURE_SINGLE(controller))
         if(n_press > 0)
-          darktable.bauhaus->press(controller, n_press, x, 0, widget);
+          darktable.bauhaus->press(GTK_GESTURE_SINGLE(controller), n_press, x, 0, widget);
         else
-          darktable.bauhaus->release(controller, -n_press, x, 0, widget);
+          darktable.bauhaus->release(GTK_GESTURE_SINGLE(controller), -n_press, x, 0, widget);
       else
-        darktable.bauhaus->motion(controller, x, 0, widget);
+        darktable.bauhaus->motion(GTK_EVENT_CONTROLLER_MOTION(controller), x, 0, widget);
     }
 
     gchar *text = dt_bauhaus_slider_get_text(widget, dt_bauhaus_slider_get(widget));

src/libs/histogram.c
@@ -302,7 +302,7 @@ static void _drawable_button_press(GtkGestureSingle *gesture,
      && dt_scopes_func_exists(s->cur_mode, get_exposure_pos))
   {
     const double pos = dt_scopes_call(s->cur_mode, get_exposure_pos, x, y);
-    dt_dev_exposure_handle_event(gesture, n_press, pos,
+    dt_dev_exposure_handle_event(GTK_EVENT_CONTROLLER(gesture), n_press, pos, 0.0,
                                  s->highlight == DT_SCOPES_HIGHLIGHT_BLACK_POINT);
   }
 }
@@ -314,7 +314,7 @@ static void _drawable_button_release(GtkGestureSingle *gesture,
                                      dt_scopes_t *s)
 {
   if(s->highlight != DT_SCOPES_HIGHLIGHT_NONE)
-    dt_dev_exposure_handle_event(gesture, -n_press, x, FALSE);
+    dt_dev_exposure_handle_event(GTK_EVENT_CONTROLLER(gesture), -n_press, x, 0.0, FALSE);
 }
 
 static void _drawable_motion(GtkEventControllerMotion *controller,
@@ -328,7 +328,7 @@ static void _drawable_motion(GtkEventControllerMotion *controller,
      && dt_scopes_func_exists(cur_mode, get_exposure_pos))
   {
     const double pos = dt_scopes_call(cur_mode, get_exposure_pos, x, y);
-    dt_dev_exposure_handle_event(controller, 1, pos, FALSE);
+    dt_dev_exposure_handle_event(GTK_EVENT_CONTROLLER(controller), 1, pos, 0.0, FALSE);
   }
   else
   {
@@ -447,18 +447,11 @@ static void _eventbox_scroll_callback(GtkEventControllerScroll* self,
       // FIXME: should handle horizontal scrolling as well?
       // FIXME: should scrolling of scope be handled in the drawable rather than the eventbox
       const gboolean black = s->highlight == DT_SCOPES_HIGHLIGHT_BLACK_POINT;
-      if(black)
-        event->scroll.delta_y *= -1;
-      // awkwardly, _exposure_proxy_handle_event() expects
-      // EventController, but passes scroll events on to bauhaus
-      // _widget_scroll() which expects an GdkEventScroll -- fix this
-      // when bauhaus can handle GtkEventControllerScroll
-      // FIXME: add handling of GtkEventControllerScroll to bauhaus, this code may be the thing holding that back
-      dt_dev_exposure_handle_event(&event->scroll, 0, 0, black);
+      dt_dev_exposure_handle_event(GTK_EVENT_CONTROLLER(self), 0, 0, black ? -dy : dy, black);
     }
     else
     {
-      // FIXME: should scrolling of scope be handled in the drawable rather than the eventbox
+      // FIXME: should scrolling of scope be handled in the drawable rather than the eventbox? right now can scroll on buttons and it will change the vectorscope!
       dt_scopes_call_if_exists(s->cur_mode, eventbox_scroll,
                                event->scroll.x, event->scroll.y,
                                dx, dy, event->scroll.state);
@@ -768,13 +761,10 @@ void gui_init(dt_lib_module_t *self)
   dt_gui_connect_motion(s->scope_draw, _drawable_motion, NULL,
                         _drawable_leave, s);
 
-  //gtk_widget_add_events(eventbox, darktable.gui->scroll_mask);
-  // FIXME: make dt_gui_connect_scroll() function
-  GtkEventController *scroll_controller =
-    gtk_event_controller_scroll_new(eventbox, GTK_EVENT_CONTROLLER_SCROLL_VERTICAL);
-  g_object_weak_ref(G_OBJECT (eventbox), (GWeakNotify) g_object_unref, scroll_controller);
-  g_signal_connect(scroll_controller, "scroll", G_CALLBACK(_eventbox_scroll_callback), s);
-
+  // FIXME: scope implementation didn't setprop phase, maybe defaulted to bubble -- do we need to set this here?
+  dt_gui_connect_scroll(eventbox, GTK_EVENT_CONTROLLER_SCROLL_VERTICAL
+                                  | GTK_EVENT_CONTROLLER_SCROLL_DISCRETE,
+                        _eventbox_scroll_callback, s);
   // FIXME: add (optional) propagation phase argument to dt_gui_connect_motion()
   GtkEventController *motion_controller =
     dt_gui_connect_motion(eventbox, _eventbox_motion_notify_callback,

commit d7fb620b4b0677f8302ce37ac10bef1b9623252d

bauhaus: receive scroll events propagated from other widgets
Switching bauhaus to GTK4-ready scrolling behavior in
11e5ad4cbfc222ba06203c46c431d1163b32edc9 caused bauhaus to no longer
handle synthetic scroll events sent from other widgets. Setting the
scroll event controller to receive events in GTK_PHASE_BUBBLE fixes
this.

The colorequal iop synthesizes scroll events from its graph widget to
the appropriate bauhaus slider. This commit restores the ability of
the bauhaus sliders to capture these events.

Fixes: #20641.

src/bauhaus/bauhaus.c
@@ -3738,9 +3738,12 @@ static void dt_bh_init(DtBauhausWidget *w)
 
   dt_gui_connect_motion(w, _widget_motion, _widget_enter, _widget_leave, widget);
 
-  dt_gui_connect_scroll(w, GTK_EVENT_CONTROLLER_SCROLL_BOTH_AXES
-                           | GTK_EVENT_CONTROLLER_SCROLL_DISCRETE,
-                        _widget_scroll, widget);
+  GtkEventController *scroll_controller =
+    dt_gui_connect_scroll(w, GTK_EVENT_CONTROLLER_SCROLL_BOTH_AXES
+                             | GTK_EVENT_CONTROLLER_SCROLL_DISCRETE,
+                          _widget_scroll, widget);
+  // allows for capturing propagated events from other widgets
+  gtk_event_controller_set_propagation_phase(scroll_controller, GTK_PHASE_BUBBLE);
 
   gtk_widget_set_can_focus(widget, TRUE);
   dt_gui_add_class(widget, "dt_bauhaus");