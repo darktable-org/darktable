d75585d0397e78a334c8a2d31afb08ed455f24c0
Andrii Ryzhkov <andrii.ryzhkov@pm.me>
2026-05-27 10:16:53 +0200

    Make the AI registry an internal singleton

--- src/develop/masks/object.c
+++ src/develop/masks/object.c
@@ -1983,10 +1983,10 @@ const dt_masks_functions_t dt_masks_functions_object = {
 
 gboolean dt_masks_object_available(void)
 {
-  if(!dt_ai_registry_is_enabled(darktable.ai_registry))
+  if(!dt_ai_registry_is_enabled())
     return FALSE;
   char *model_id = dt_ai_models_get_active_for_task("mask");
-  dt_ai_model_t *model = dt_ai_models_get_by_id(darktable.ai_registry, model_id);
+  dt_ai_model_t *model = dt_ai_models_get_by_id(model_id);
   g_free(model_id);
   const gboolean available = model && model->status == DT_AI_MODEL_DOWNLOADED;
   dt_ai_model_free(model);
