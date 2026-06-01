bded383ef28d8fac1deb131a395342c117f1f1bb
Victor Forsiuk <vvforce@gmail.com>
2026-06-01 19:45:52 +0300

    Add editor modelines to color harmonizer source code

--- src/iop/colorharmonizer.c
+++ src/iop/colorharmonizer.c
@@ -1665,3 +1665,9 @@ void gui_cleanup(dt_iop_module_t *self)
   }
   dt_iop_default_cleanup(self);
 }
+
+// clang-format off
+// modelines: These editor modelines have been set for all relevant files by tools/update_modelines.py
+// vim: shiftwidth=2 expandtab tabstop=2 cindent
+// kate: tab-indents: off; indent-width 2; replace-tabs on; indent-mode cstyle; remove-trailing-spaces modified;
+// clang-format on
