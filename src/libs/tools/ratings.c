041133f00f3074a283a1e4a60dbec0a93c24de98
deekayhd <53017684+deekayhd@users.noreply.github.com>
2026-06-03 15:55:57 +0200

    clean up code

--- src/libs/tools/ratings.c
+++ src/libs/tools/ratings.c
@@ -215,8 +215,13 @@ static void _lib_ratings_button_press_callback(GtkGestureSingle *gesture, int n_
   dt_ratings_apply_on_list(imgs, d->current, TRUE);
   dt_collection_update_query(darktable.collection, DT_COLLECTION_CHANGE_RELOAD, DT_COLLECTION_PROP_RATING_RANGE, imgs);
 
-    dt_control_queue_redraw_center();
-  }
+  GList *imgs = dt_act_on_get_images(FALSE, TRUE, FALSE);
+  dt_ratings_apply_on_list(imgs, d->current, TRUE);
+  dt_collection_update_query(darktable.collection, DT_COLLECTION_CHANGE_RELOAD, DT_COLLECTION_PROP_RATING_RANGE, imgs);
+
+  dt_control_queue_redraw_center();
+  
+  return TRUE;
 }
 
 static void _lib_ratings_button_release_callback(GtkGestureSingle *gesture, int n_press,
