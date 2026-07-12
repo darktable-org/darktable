cef0940d4f5ceadef0f3a29cf8e0637833ff711c
piratenpanda <benjamin@lebsanft.org>
2026-07-12 09:41:13 +0200

    initial darktable spektrafilm commit

--- src/common/iop_order.c
+++ src/common/iop_order.c
@@ -145,6 +145,7 @@ const dt_iop_order_entry_t legacy_order[] = {
   { {45.5f }, "agx", 0},
   { {46.0f }, "filmic", 0},
   { {46.5f }, "filmicrgb", 0},
+  { { 46.7f }, "spektrafilm", 0 },
   { {47.0f }, "colisa", 0},
   { {48.0f }, "zonesystem", 0},
   { {49.0f }, "tonecurve", 0},
@@ -260,6 +261,7 @@ const dt_iop_order_entry_t v30_order[] = {
   { {45.3f }, "sigmoid", 0},
   { {45.5f }, "agx", 0},
   { {46.0f }, "filmicrgb", 0},       // same, upgraded
+  { { 46.7f }, "spektrafilm", 0 },
   { {36.0f }, "lut3d", 0},           // apply a creative style or film emulation, possibly non-linear
   { {47.0f }, "colisa", 0},          // edit contrast while damaging colour
   { {48.0f }, "tonecurve", 0},       // same
@@ -380,6 +382,7 @@ const dt_iop_order_entry_t v50_order[] = {
   { {45.3f }, "sigmoid", 0},
   { {45.5f }, "agx", 0},
   { {46.0f }, "filmicrgb", 0},       // same, upgraded
+  { { 46.7f }, "spektrafilm", 0 },
   { {36.0f }, "lut3d", 0},           // apply a creative style or film emulation, possibly non-linear
   { {47.0f }, "colisa", 0},          // edit contrast while damaging colour
   { {48.0f }, "tonecurve", 0},       // same
@@ -501,6 +504,7 @@ const dt_iop_order_entry_t v30_jpg_order[] = {
   { {45.5f }, "agx", 0},
   { { 45.3f }, "sigmoid", 0},
   { { 46.0f }, "filmicrgb", 0 },     // same, upgraded
+  { { 46.7f }, "spektrafilm", 0 },
   { { 36.0f }, "lut3d", 0 },         // apply a creative style or film emulation, possibly non-linear
   { { 47.0f }, "colisa", 0 },        // edit contrast while damaging colour
   { { 48.0f }, "tonecurve", 0 },     // same
@@ -624,6 +628,7 @@ const dt_iop_order_entry_t v50_jpg_order[] = {
   { { 45.3f }, "sigmoid", 0},
   { {45.5f }, "agx", 0},
   { { 46.0f }, "filmicrgb", 0 },     // same, upgraded
+  { { 46.7f }, "spektrafilm", 0 },
   { { 36.0f }, "lut3d", 0 },         // apply a creative style or film emulation, possibly non-linear
   { { 47.0f }, "colisa", 0 },        // edit contrast while damaging colour
   { { 48.0f }, "tonecurve", 0 },     // same
