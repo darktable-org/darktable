947c59bbb950d29afe154a53d6ea8d056d781b57
yuri <1969yuri1969@gmail.com>
2026-07-29 00:11:39 +0200

    fix(ui): add spektrafilm to modules list
    The new module was missing from the `all` modules group listing.

--- src/libs/modulegroups.c
+++ src/libs/modulegroups.c
@@ -1666,6 +1666,7 @@ void init_presets(dt_lib_module_t *self)
   AM("colorzones");
   AM("colorequal");
   AM("colorharmonizer");
+  AM("spektrafilm");
   AM("lut3d");
   AM("monochrome");
   AM("profile");
