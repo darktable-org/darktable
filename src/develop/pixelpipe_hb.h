480da15e2568a71e0bd83eb4c9f7f49b4b5fa5ba
Hanno Schwalm <hanno@schwalm-bremen.de>
2026-08-02 09:15:33 +0200

    Pixelpipe 5.8 overhaul for efficacy
    A short summary: How does current dt pixelpipe work?
    Pixelpipe processing happens in _dev_pixelpipe_process_rec(), it is responsible for preparing
    zoom scale and area, piece roi_in&out, cacheline allocations and invalidations, importance hints,
    cacheline integrity after colorspace conversions, mask blending, process a piece in tiling mode
    and some special cases as fast blending, mask visualizing and providing picker and histogram data.
    This is valid for all sort of pipes - some pipes modify stuff for performance (preview and thumbs) or
    have to respect visualizing (like for masks).
    
    1. In it's first part - terminated by _dev_pixelpipe_early_exit() it checks for
       position and output dimension of the pipe.
    2. We recursively traverse the module list from last to first preparing roi_in
       for each piece and as the last step read the pipe input data.
    3. After that, we walk & process the prepared list of pieces.
    
    The UI resonsiveness in darkroom mode is determined by three factors:
    1. an efficient and reliable caching of in- and output data (besides that we have special caches
       for mipmaps, raster&detail masks and for fast blending).
       If a pixelpipe is started, it tries to find a starting point by a cacheline hit instead of
       costly processing all modules.
    2. a pipe shutdown mechanism for a running pipe due to user actions in above 1-3 phases.
       When handling such a shutdown request, we must ensure integrity of cachelines so only valid
       cachelines are used as a pixelpipe starting point.
    3. If we process the pipe via OpenCL, we minimize host/device transfers.
       As cachelines are currently only in host memory, we have to balance costly transfers vs hit
       rate, thus we have an importance strategy.
       Important cachelines should always be on host and have a smaller chance of being evicted.
    
    **About cachelines**
    Until now we had to invalidate input cachelines after colorspace transforms requested by
    the module for stable results as we transformed the cacheline data "inspace".
    This possibly came with a hefty UI responsiveness penalty as cache hits for a reprocessed pipe
    never happened for modules that had to transform input colorspace, this depends of course on
    the selection of used modules.
    This commit modifies the host-data bound processing - both CPU processed modules and OpenCL
    modules in tiling mode - for the screen pipes.
    We now keep input cacheline intact by using a temporary buffer for colorspace transformed data.
    So - cacheline invalidations are now required only for some rare conditions.
    Because of invalidation importance the related logs have been improved.
    Also fixed a bug in pixelpipe cache cleanup leading to stale cachelines.
    
    **About shutdown**
    In 5.6 code we only used the shutdown mechanism for changing pipe nodes or toggling the HQ
    processing mode. This commit introduces these new modes:
      DT_DEV_PIXELPIPE_PROCESSING
      DT_DEV_PIXELPIPE_STOP_ZOOM
      DT_DEV_PIXELPIPE_STOP_DATA
    all served by _module_pipe_stop() and it's callers.
    Please note that the ZOOM and DATA shutdowns are not used yet but are fully functional.
    We use dt_atomic_CAS_int to set shutdown so there is only one shutdown mode per pixelpipe run.
    
    We had 'processing' as a gboolean flag in the pipe struct.
    Now we use DT_DEV_PIXELPIPE_PROCESSING in pipe->shutdown instead,
    - this allows us to distinguish between a processing and a stale pipe
    - we have an atomic state switch including processing state
    - dt_pipe_started() and dt_pipe_processing() are provided helpers.
    
    Clear the history_last_module hint for important cachelines after the full pipe has been
    fully processed instead of doing so while processing the pipe for less interference for
    both preview pipe caches.
    
    Some comments about pixelpipe workflow have been added/corrected.
    Some log improvements for shutdown mode handling and cacheline invalidations.

--- src/develop/pixelpipe_hb.h
+++ src/develop/pixelpipe_hb.h
@@ -31,8 +31,6 @@ G_BEGIN_DECLS
 
 #define DT_PIPECACHE_MIN 2
 
-// #define DT_PIPE_CAS_SHUTDOWN
-
 /** cached distorted mask at a geometric module's output boundary.
  *  used to avoid re-distorting masks from scratch when multiple
  *  downstream modules request the same mask type. */
@@ -100,46 +98,51 @@ typedef enum dt_dev_pixelpipe_status_t
   DT_DEV_PIXELPIPE_INVALID = 3  // pixelpipe has finished; invalid result
 } dt_dev_pixelpipe_status_t;
 
-/* dt_dev_pixelpipe_stopper_t is used as shutdown in dt_dev_pixelpipe_t.
+/* dt_dev_pixelpipe_stopper_t is used for shutdown in dt_dev_pixelpipe_t.
     By design we can write atomically on a pipe->shutdown to request an early exit
     of the pixepipe process _dev_pixelpipe_process_rec().
 
-    This requires special care in
-      - _dev_pixelpipe_process_rec()
-      - dt_dev_process_image_job()
-    possibly invalidating wrong module input/output data in the pixelpipe cache,
-    ensure either an immediate restart of the pipe or exit of dt_dev_process_image_job()
-    with an error flag.
-    A reminder, when setting pipe->shutdown we might have to do that via dt_atomic_CAS_int()
-    with wxpected DT_DEV_PIXELPIPE_STOP_NO to avoid overwriting an earlier shutdown writing.
+    When setting pipe->shutdown we should use dt_dev_pixelpipe_set_shutdown(),
+    it uses dt_atomic_CAS_int so having only one shutdown request per pipe run.
+    The "expected" state is DT_DEV_PIXELPIPE_PROCESSING, in other states the
+    set_shutdown() request is ignored.
+    As we never have valid output data we always invalidate output cacheline.
 
-    A summary about how these shutdown modes are supposed to work.
+    A summary about how these shutdown modes are supposed to work:
 
     DT_DEV_PIXELPIPE_STOP_NO
-    Set whenever a pipe is started in _dev_pixelpipe_process_rec() as default.
+    Set whenever we initialize or clean up the pipe, means "pipe is idle"
+
+    DT_DEV_PIXELPIPE_PROCESSING
+    Set whenever a pipe is started making this different from idling mode STOP_NO.
+    Please note there is a very small timelap after the pipe thread has started.
 
     DT_DEV_PIXELPIPE_STOP_NODES
-    Set if the pipe should stop as the pipe nodes are changed so a restart is desired asap.
-    As nodes are recreated, we don't have to fiddle with pixelpipe cache.
+    Set if the pipe should stop as the pipe nodes are changed.
 
     DT_DEV_PIXELPIPE_STOP_HQ
     Used to switch between darkroom HQ modes.
-    Requires a restart of the pipe but pixelpipe cache can stay.
 
-    DT_DEV_PIXELPIPE_STOP_LAST
-    If the shutdown value is >= DT_DEV_PIXELPIPE_STOP_LAST it is understood as the iop_order
-    of a module.
-    Any module might set pipe->shutdown to it's iop_order, this is checked while processing
-    the pipe and if detected the piece input data and all pipe cachelines with at least
-    this iop_order will be invalidated.
+    DT_DEV_PIXELPIPE_STOP_ZOOM
+    A request to restart with different darkroom position or scale.
+    We might get back to last zoom setting pretty soon so we keep cachlines
+    as we do for above shutdown modes.
+
+    DT_DEV_PIXELPIPE_STOP_DATA
+    A request to restart with different module parameters.
+    As we missed processing the correct output and all following modules
+    will give different results accordingly we clear cachelines for following
+    modules (possibly writing back input cl_mem to host for a faster restart).
 */
 
 typedef enum dt_dev_pixelpipe_stopper_t
 {
   DT_DEV_PIXELPIPE_STOP_NO = 0,
+  DT_DEV_PIXELPIPE_PROCESSING,
   DT_DEV_PIXELPIPE_STOP_NODES,
   DT_DEV_PIXELPIPE_STOP_HQ,
-  DT_DEV_PIXELPIPE_STOP_LAST,
+  DT_DEV_PIXELPIPE_STOP_ZOOM,
+  DT_DEV_PIXELPIPE_STOP_DATA,
 } dt_dev_pixelpipe_stopper_t;
 
 typedef struct dt_dev_detail_mask_t
@@ -211,9 +214,7 @@ typedef struct dt_dev_pixelpipe_t
   gboolean nocache;
 
   dt_imgid_t output_imgid;
-  // working?
-  gboolean processing;
-  /* shutting down?
+  /* Testing for shutting down and a running pixelpipe
      can be used in various ways defined in dt_dev_pixelpipe_stopper_t, in all cases the
        running pipe is stopped asap
      If we don't use one of the enum values this is interpreted as the iop_order of the module
@@ -317,14 +318,21 @@ static inline gboolean dt_pipe_mask_display(const dt_dev_pixelpipe_t *pipe)
 {
   return pipe->mask_display != DT_DEV_PIXELPIPE_DISPLAY_NONE;
 }
+static inline gboolean dt_pipe_processing(dt_dev_pixelpipe_t *pipe)
+{
+  return dt_atomic_get_int(&pipe->shutdown) == DT_DEV_PIXELPIPE_PROCESSING;
+}
+static inline gboolean dt_pipe_started(dt_dev_pixelpipe_t *pipe)
+{
+  return dt_atomic_get_int(&pipe->shutdown) >= DT_DEV_PIXELPIPE_PROCESSING;
+}
 
 // report pipe->type as textual string
 const char *dt_dev_pixelpipe_type_to_str(const dt_dev_pixelpipe_type_t pipe_type);
 // return pipe->shutdown as textual
 const char *dt_dev_pixelpipe_shutdown_to_str(const dt_dev_pixelpipe_stopper_t stopper);
 
-// sets pipe->shutdown in atomic mode
-// If DT_PIPE_CAS_SHUTDOWN is defined do that only if shutdown was DT_DEV_PIXELPIPE_STOP_NO
+// sets pipe->shutdown in atomic CAS mode so only one mode is possible per pipe run
 void dt_dev_pixelpipe_set_shutdown(dt_dev_pixelpipe_t *pipe, const dt_dev_pixelpipe_stopper_t stopper);
 
 // inits the pixelpipe with plain passthrough input/output and empty input and default caching settings.
