f4a963cfb7010cf75080280c5f0b459f4b313785
Hanno Schwalm <hanno@schwalm-bremen.de>
2026-08-08 06:34:24 +0200

    Fixes for dt_opencl_image_fits_device()
    We need input and output bpp to correctly calculate the tiling requirements.

--- src/develop/pixelpipe_hb.c
+++ src/develop/pixelpipe_hb.c
@@ -2361,7 +2361,7 @@ static gboolean _dev_pixelpipe_process_rec(dt_dev_pixelpipe_t *pipe,
 
     const dt_opencl_tilemode_t fitter =
       dt_opencl_image_fits_device(pipe->devid, m_width, m_height,
-                                  m_bpp, tiling.factor_cl, tiling.overhead, tiling.overlap);
+                                  in_bpp, bpp, tiling.factor_cl, tiling.overhead, tiling.overlap);
 
     const gboolean no_tiling = fitter == DT_OPENCL_NO_TILING;
     const gboolean fast_tiling = fitter == DT_OPENCL_FAST_TILING
