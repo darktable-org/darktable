9c6d27b6eecb864d6b5eb1fe5326e6470de72af0
Martin Straeten <39386816+MStraeten@users.noreply.github.com>
2026-08-14 22:09:12 +0200

    add satcurvergb module

--- src/libs/modulegroups.c
+++ src/libs/modulegroups.c
@@ -1658,6 +1658,7 @@ void init_presets(dt_lib_module_t *self)
   AM("monochrome");
   AM("profile");
   AM("primaries");
+  AM("satcurvergb");
   AM("gamma");
   AM("velvia");
 
@@ -1820,6 +1821,7 @@ void init_presets(dt_lib_module_t *self)
   AM("colorequal");
   AM("colorharmonizer");
   AM("primaries");
+  AM("satcurvergb");
 
   SMG(C_("modulegroup", "correct"), "correct");
   AM("cacorrect");
