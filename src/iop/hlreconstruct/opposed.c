b75a5caa53aa0829c5ea8e20eb4c1ad2f6cfabec
Hanno Schwalm <hanno@schwalm-bremen.de>
2026-08-14 07:29:56 +0200

    Let's remove some superfluous log messages

--- src/iop/hlreconstruct/opposed.c
+++ src/iop/hlreconstruct/opposed.c
@@ -187,7 +187,7 @@ static void _process_linear_opposed(dt_iop_module_t *self,
           img_opphash = opphash;
           img_oppclipped = anyclipped;
         }
-        dt_print_pipe(DT_DEBUG_PIPE,
+        dt_print_pipe(DT_DEBUG_PIPE | DT_DEBUG_VERBOSE,
             "opposed chroma", piece->pipe, self, DT_DEVICE_CPU, roi_in, NULL,
             "RGB %3.4f %3.4f %3.4f%s%s",
             chrominance[0], chrominance[1], chrominance[2],
@@ -344,7 +344,7 @@ static float *_process_opposed(dt_iop_module_t *self,
         img_oppclipped = anyclipped;
       }
 
-      dt_print_pipe(DT_DEBUG_PIPE,
+      dt_print_pipe(DT_DEBUG_PIPE | DT_DEBUG_VERBOSE,
           "opposed chroma", piece->pipe, self, DT_DEVICE_CPU, NULL, NULL,
            "%12.7f (%d)%12.7f (%d)%12.7f (%d)%s%s",
           chrominance[0], (int)cnts[0],
@@ -544,7 +544,7 @@ static cl_int process_opposed_cl(dt_iop_module_t *self,
       img_oppclipped = clipped > 0.0f;
     }
 
-    dt_print_pipe(DT_DEBUG_PIPE,
+    dt_print_pipe(DT_DEBUG_PIPE | DT_DEBUG_VERBOSE,
         "opposed chroma", piece->pipe, self, piece->pipe->devid, NULL, NULL,
         "%12.7f (%d)%12.7f (%d)%12.7f (%d)%s%s",
         chrominance[0], (int)cnts[0],
