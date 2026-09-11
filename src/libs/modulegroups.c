dff2a25c22e6052708e2fc3aa87e5e5ff5eacda0
Hanno Schwalm <hanno@schwalm-bremen.de>
2026-09-11 19:11:39 +0200

    Post demosaicer downscaling
    As the demosaicer now always process full image data we downscale to the required roi
    in another module enabled for all demosaiced images.
    
    Pro and against arguments for using this strategy:
    
    About cost vs. efficacy:
    1. As we now always demosaic full image data the pipe is principally slower if parameters
       change before/inside demosaic and we use a smaller roi than full data (like when using crop).
       This affects all pipe types so exports will be somewhat slower especially if we crop,
       either by crop module or ashift.
    2. Also we have to process more when zooming or dragging the roi when zoomed in.
    3. As demosaic does no internal crop&scale anymore, the memory footprint is lower thus internal
       tilesizes can be smaller with a possible perf gain.
    4. BUT: in all desktop pipes we avoid processing all modules before the demosaic downscaler
       after the first pipe run as we have cached data and just have to crop&scale which is much
       faster than re-processing from pipe start.
    
    About quality & results:
    All demosaicers have inferior interpolators at the border regions, depending on the demosaicer
    algorithm this affects the outermost 2-4 photosites.
    With the old method we had these minor quality pixels always on the cropped roi.
    As we now calculate all pixels we crop on the better data thus results will be somewhat better
    at the outermost areas.

--- src/libs/modulegroups.c
+++ src/libs/modulegroups.c
@@ -1638,6 +1638,7 @@ void init_presets(dt_lib_module_t *self)
   AM("demosaic");
   AM("exposure");
   AM("finalscale");
+  AM("demosaicscale");
   AM("flip");
   AM("highlights");
   AM("negadoctor");
