c0348f7353977e63f46398f327872f2d050be3b9
Mario Zimmermann <mail@zisoft.de>
2026-09-11 13:33:55 +0200

    migrate styles popover menu

--- src/libs/print_settings.c
+++ src/libs/print_settings.c
@@ -1614,37 +1614,45 @@ static void _update_style(const dt_stylemenu_data_t *menu_data)
   _update_style_label(menu_data->user_data, menu_data->name);
 }
 
-static void _apply_style_activate_callback(GtkMenuItem *menuitem,
-                                           const dt_stylemenu_data_t *menu_data)
+static void _apply_style_activate_callback(GSimpleAction *action,
+                                           GVariant *parameter,
+                                           gpointer user_data)
 {
-  if(dt_gui_menuitem_activated_by_keyboard(GTK_WIDGET(menuitem)))
-    _update_style(menu_data);
-}
+  const dt_stylemenu_data_t *menu_data = (dt_stylemenu_data_t *)g_variant_get_uint64(parameter);
 
-static void _apply_style_button_callback(GtkGestureSingle *gesture,
-                                         gint n_press,
-                                         gdouble x,
-                                         gdouble y,
-                                         const dt_stylemenu_data_t *menu_data)
-{
-  if(gtk_gesture_single_get_current_button(gesture) == GDK_BUTTON_PRIMARY)
-    _update_style(menu_data);
-  else
-  {
-    //??? dt_shortcut_copy_lua(NULL, name);
-  }
+  _update_style(menu_data);
+  dtgtk_stylemenu_free_menu_data();
 }
 
 static void _style_popupmenu_callback(GtkWidget *w, gpointer user_data)
 {
+  GActionGroup *action_group = gtk_widget_get_action_group(w, "styles");
+  if(action_group == NULL)
+  {
+    GActionEntry action_entries[] =
+    {
+      { "activate", _apply_style_activate_callback, "t", NULL },
+    };
+
+    action_group = G_ACTION_GROUP(g_simple_action_group_new());
+    g_action_map_add_action_entries(G_ACTION_MAP(action_group),
+                                    action_entries,
+                                    G_N_ELEMENTS(action_entries),
+                                    NULL);
+    gtk_widget_insert_action_group(w, 
+                                   "styles",
+                                   G_ACTION_GROUP(action_group));
+  }
+
   /* if we got any styles, lets popup menu for selection */
-  GtkMenuShell *menu = dtgtk_build_style_menu_hierarchy(TRUE,
-                                                        _apply_style_activate_callback,
-                                                        _apply_style_button_callback,
+  GMenu *menu = dtgtk_build_style_menu_hierarchy(TRUE,
                                                         user_data);
   if(menu)
   {
-    dt_gui_menu_popup(GTK_MENU(menu), w, GDK_GRAVITY_SOUTH_WEST, GDK_GRAVITY_NORTH_WEST);
+    // popup the menu
+    GtkWidget *popover_menu = dt_gui_popover_menu_from_model(w, menu);
+    g_object_unref(menu);
+    gtk_popover_popup(GTK_POPOVER(popover_menu));
   }
   else
     dt_control_log(_("no styles have been created yet"));
