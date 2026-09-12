421db430e5aa5134c12f99faedd11f386c846613
Hanno Schwalm <hanno@schwalm-bremen.de>
2026-09-12 09:32:07 +0200

    Collection of various minor fixes
    1. dt_print_pipe() does better roi reporting like "none" or "same" and some callers got corrected or simplified.
    2. reporting module names instead of non human-readable pointers while changing CAT.
    3. corrected switch to fast approximating demosaicers
       - the preview pipe got it's data already downscaled so stay with normal but fast demosaicing mode.
       - for thumbs it should be chosen depending on final dimension instead of roi (respecting crops ...)

--- src/iop/channelmixerrgb.c
+++ src/iop/channelmixerrgb.c
@@ -1206,8 +1206,12 @@ static void _declare_cat_on_pipe(dt_iop_module_t *self, const gboolean preset)
   }
 
   if(origcat != chr->adaptation)
-    dt_print(DT_DEBUG_PIPE, "changed CAT for %s%s from %p to %p",
-      self->op, dt_iop_get_instance_id(self), origcat, chr->adaptation);
+    dt_print(DT_DEBUG_PIPE, "changed CAT for %s%s from %s%s to %s%s",
+      self->op, dt_iop_get_instance_id(self),
+      origcat ? origcat->name() : "none",
+      origcat ? dt_iop_get_instance_id(origcat) : "",
+      chr->adaptation ? chr->adaptation->op : "none",
+      chr->adaptation ? dt_iop_get_instance_id(chr->adaptation) : "");
 }
 
 static void _update_illuminants(const dt_iop_module_t *self);
